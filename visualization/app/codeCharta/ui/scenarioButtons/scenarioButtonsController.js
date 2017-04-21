c3bcb6c410b750fa7b8c880cf1a32b0277747886
mrpatpat <adrian.endrich@googlemail.com>
2017-04-21 12:40:20 +0200

    adding "controller as" syntax

--- visualization/app/codeCharta/ui/scenarioButtons/scenarioButtonsController.js
+++ visualization/app/codeCharta/ui/scenarioButtons/scenarioButtonsController.js
@@ -5,11 +5,24 @@ class ScenarioButtonsController {
     /* @ngInject */
     constructor(scenarioService) {
 
+
         /**
          *
          * @type {ScenarioService}
          */
         this.scenarioService = scenarioService;
+
+        /**
+         *
+         * @type {string}
+         */
+        this.controllerAs = "ctrl";
+
+        /**
+         *
+         * @type {boolean}
+         */
+        this.bindToController = true;
 
     }
 
