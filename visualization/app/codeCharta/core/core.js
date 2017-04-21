6ef3fb1d21815445a41ea2723493aa00dd30937e
mrpatpat <adrian.endrich@googlemail.com>
2017-04-21 11:47:54 +0200

    fixing a bug

--- visualization/app/codeCharta/core/core.js
+++ visualization/app/codeCharta/core/core.js
@@ -4,5 +4,6 @@ import "./data/data.js";
 import "./settings/settings.js";
 import "./url/url.js";
 import "./treemap/treemap.js";
+import "./scenario/scenario.js";
 
-angular.module("app.codeCharta.core",["app.codeCharta.core.data", "app.codeCharta.core.settings", "app.codeCharta.core.url", "app.codeCharta.core.treemap"]);
+angular.module("app.codeCharta.core",["app.codeCharta.core.data", "app.codeCharta.core.settings", "app.codeCharta.core.url", "app.codeCharta.core.treemap","app.codeCharta.core.scenario"]);
