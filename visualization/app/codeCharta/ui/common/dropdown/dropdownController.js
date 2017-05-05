cba62b2ca09a95e0741f81360469b3e081e5768f
FernandoES <fernan_1989@hotmail.com>
2017-05-05 16:47:27 +0200

    Adding comments

--- visualization/app/codeCharta/ui/common/dropdown/dropdownController.js
+++ visualization/app/codeCharta/ui/common/dropdown/dropdownController.js
@@ -1,12 +1,23 @@
 "use strict";
-
+/**
+ * Controller for the DropdownDirective
+ */
 class DropdownController{
 
     /* @ngInject */
+    /**
+     *
+     * @param {TooltipService} tooltipService
+     */
     constructor(tooltipService) {
         this.tooltipService = tooltipService;
     }
 
+    /**
+     * returns the tooltip description related to the given key
+     * @param {String} key
+     * @returns {string} description
+     */
     getTooltipTextByKey(key) {
         return this.tooltipService.getTooltipTextByKey(key);
     }
