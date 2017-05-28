6d007d58406a340ec71d736023ac043a42e2382e
Dominik Ufer <dominik.ufer@maibornwolff.de>
2017-05-28 23:06:46 +0200

    Added global configuration parameter

--- analysis/import/CSVImporter/src/main/java/de/maibornwolff/codecharta/importer/csv/CSVImporter.java
+++ analysis/import/CSVImporter/src/main/java/de/maibornwolff/codecharta/importer/csv/CSVImporter.java
@@ -4,21 +4,27 @@ import com.google.common.collect.ImmutableList;
 import de.maibornwolff.codecharta.serialization.ProjectSerializer;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CSVImporter {
-    public static void main(String... args) throws IOException {
-        CSVImporterParameter callParameter = new CSVImporterParameter(3,4);
 
-        CSVProjectAdapter project = new CSVProjectAdapter("test", callParameter);
-        getInputStreamsFromArgs(args).forEach(project::addProjectFromCsv);
-        ProjectSerializer.serializeProject(project, new OutputStreamWriter(System.out));
+    public static final String STANDARD_PROJECT_NAME = "test";
+
+    public static void main(String... args) throws IOException {
+        CSVImporterParameter callParameter = new CSVImporterParameter(args);
+
+        if (callParameter.isHelp()) {
+            callParameter.printUsage();
+        } else {
+            CSVProjectAdapter project = new CSVProjectAdapter(STANDARD_PROJECT_NAME, callParameter.getPathSeparator(), callParameter.getCsvDelimiter());
+            getInputStreamsFromArgs(callParameter.getFiles()).forEach(project::addProjectFromCsv);
+            ProjectSerializer.serializeProject(project, new OutputStreamWriter(System.out));
+        }
     }
 
-    private static List<InputStream> getInputStreamsFromArgs(String[] args) {
-        List<InputStream> fileList = Arrays.stream(args).map(CSVImporter::createFileInputStream).collect(Collectors.toList());
+    private static List<InputStream> getInputStreamsFromArgs(List<String> files) {
+        List<InputStream> fileList = files.stream().map(CSVImporter::createFileInputStream).collect(Collectors.toList());
         return fileList.isEmpty() ? ImmutableList.of(System.in) : fileList;
     }
 
