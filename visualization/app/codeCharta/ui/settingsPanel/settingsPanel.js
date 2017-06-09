9df7d8cf5164c35b15c5ab3a38c08a3e492b215c
mrpatpat <adrian.endrich@googlemail.com>
2017-06-09 12:38:00 +0200

    adding lib

--- visualization/app/codeCharta/ui/settingsPanel/settingsPanel.js
+++ visualization/app/codeCharta/ui/settingsPanel/settingsPanel.js
@@ -3,13 +3,14 @@
 import "../../ui/ui.js";
 import "../../core/core.js";
 import "../../codeMap/codeMap.js";
+import "angularjs-slider";
 
 import {SettingsPanelDirective} from "./settingsPanelDirective.js";
 import {SettingsPanelController} from "./settingsPanelController.js";
 
 angular.module(
     "app.codeCharta.ui.settingsPanel",
-    ["app.codeCharta.ui", "app.codeCharta.core", "app.codeCharta.codeMap"]
+    ["app.codeCharta.ui", "app.codeCharta.core", "app.codeCharta.codeMap", "rzModule"]
 );
 
 angular.module("app.codeCharta.ui.settingsPanel").directive(
