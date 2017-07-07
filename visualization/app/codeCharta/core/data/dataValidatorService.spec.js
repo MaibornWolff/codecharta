12268c72fd1c07cabd28e70fc90aa5260df3b816
FernandoES <fernan_1989@hotmail.com>
2017-07-07 15:55:55 +0200

    In test id and timestamp deleting and root by nodes[0] substitution

--- visualization/app/codeCharta/core/data/dataValidatorService.spec.js
+++ visualization/app/codeCharta/core/data/dataValidatorService.spec.js
@@ -13,7 +13,7 @@ describe("app.codeCharta.core.data.dataValidatorService", function() {
     beforeEach(angular.mock.inject(function (_dataValidatorService_, _$httpBackend_) {
         dataValidatorService = _dataValidatorService_;
 
-        //bind ressource to httpBackend
+        //bind resource to httpBackend
         _$httpBackend_
             .when("GET", "schema.json")
             .respond(200, require("./schema.json"));
@@ -26,40 +26,33 @@ describe("app.codeCharta.core.data.dataValidatorService", function() {
 
         file = {
 
-            "name": "Sample Map",
+            "projectName": "Sample Map",
 
-            "timestamp": "0",
-
-            "root": {
+            "nodes": [{
                 "name": "root",
-                "id":20,
                 "attributes": {},
                 "children": [
                     {
                         "name": "big leaf",
-                        "id":0,
                         "attributes": {"RLOC": 100, "Functions": 10, "MCC": 1},
                         "link": "http://www.google.de"
                     },
                     {
                         "name": "Parent Leaf",
-                        "id":1,
                         "attributes": {},
                         "children": [
                             {
                                 "name": "small leaf",
-                                "id":2,
                                 "attributes": {"RLOC": 30, "Functions": 100, "MCC": 100}
                             },
                             {
                                 "name": "other small leaf",
-                                "id":3,
                                 "attributes": {"RLOC": 70, "Functions": 1000, "MCC": 10}
                             }
                         ]
                     }
                 ]
-            }
+            }]
 
         };
 
@@ -73,7 +66,7 @@ describe("app.codeCharta.core.data.dataValidatorService", function() {
         $httpBackend.flush();
     });
 
-    it("should reject when no timestamp is given", (done)=>{
+    xit("should reject when no timestamp is given", (done)=>{
         file.timestamp = null;
         dataValidatorService.validate(file).then(
             ()=>{done("should reject")},
@@ -83,8 +76,8 @@ describe("app.codeCharta.core.data.dataValidatorService", function() {
     });
 
     it("should reject when children are not unique in name ", (done)=>{
-        file.root.children[0].name = "same";
-        file.root.children[1].name = "same";
+        file.nodes[0].children[0].name = "same";
+        file.nodes[0].children[1].name = "same";
         dataValidatorService.validate(file).then(
             ()=>{done("should reject")},
             ()=>{done();}
@@ -92,13 +85,98 @@ describe("app.codeCharta.core.data.dataValidatorService", function() {
         $httpBackend.flush();
     });
 
-    //TODO more tests
-    it("should accept when everything is ok", (done)=>{
+    xit("should reject when children are not unique in id ", (done)=>{
+        file.root.children[0].id = 0;
+        file.root.children[1].id = 0;
         dataValidatorService.validate(file).then(
-            ()=>{done()},
-            ()=>{done("should reject");}
+            ()=>{done("should reject")},
+            ()=>{done();}
         );
         $httpBackend.flush();
     });
+
+    it("should reject when children are empty", (done)=>{
+        file.nodes[0].children = [];
+        dataValidatorService.validate(file).then(
+            ()=>{done("should reject")},
+            ()=>{done();}
+        );
+        $httpBackend.flush();
+    });
+
+    xit("root should be node and therefore have name and id", (done)=>{
+        file.root = {
+            name: "name",
+            id: 0
+        };
+        dataValidatorService.validate(file).then(
+            ()=>{done();},
+            ()=>{done("should resolve");}
+        );
+        $httpBackend.flush();
+    });
+
+    xit("should reject if root is not a node and therefore has no name or id", (done)=>{
+        file.root = {
+            name: "name"
+        };
+        dataValidatorService.validate(file).then(
+            ()=>{done("should reject");},
+            ()=>{done();}
+        );
+        $httpBackend.flush();
+    });
+
+    xit("attributes should not allow whitespaces", (done)=>{
+        file.nodes[0].attributes = {
+            "tes t1": 0
+        };
+        dataValidatorService.validate(file).then(
+            ()=>{done("should reject");},
+            ()=>{done();}
+        );
+        $httpBackend.flush();
+    });
+
+    xit("attributes should not allow special characters", (done)=>{
+        file.nodes[0].attributes = {
+            "tes)t1": 0
+        };
+        dataValidatorService.validate(file).then(
+            ()=>{done("should reject");},
+            ()=>{done();}
+        );
+        $httpBackend.flush();
+    });
+
+    xit("id should not be a floating point number", (done)=>{
+        file.root.children[1].id = 1.6;
+        dataValidatorService.validate(file).then(
+            ()=>{done("should reject");},
+            ()=>{done();}
+        );
+        $httpBackend.flush();
+    });
+
+    xit("id should not be negative", (done)=>{
+        file.root.children[1].id = -1;
+        dataValidatorService.validate(file).then(
+            ()=>{done("should reject");},
+            ()=>{done();}
+        );
+        $httpBackend.flush();
+    });
+
+
+    xit("id should be unique in whole map", (done)=>{
+        file.root.children[1].id = 1;
+        file.root.children[1].children[0].id = 1;
+        dataValidatorService.validate(file).then(
+            ()=>{done("should reject");},
+            ()=>{done();}
+        );
+        $httpBackend.flush();
+    });
+
 });
 
