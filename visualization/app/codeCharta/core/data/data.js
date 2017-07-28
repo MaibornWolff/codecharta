3e3bfc2a8bdf9b55c4140e0f3058fb88aed55251
mrpatpat <adrian.endrich@googlemail.com>
2017-07-28 10:07:05 +0200

    adding unary metric

--- visualization/app/codeCharta/core/data/data.js
+++ visualization/app/codeCharta/core/data/data.js
@@ -4,8 +4,14 @@ import {DataService} from "./dataService.js";
 import {DataValidatorService} from "./dataValidatorService.js";
 import {DataLoadingService} from "./dataLoadingService.js";
 import {DeltaCalculatorService} from "./deltaCalculatorService.js";
+import {DataDecoratorService} from "./dataDecoratorService.js";
+
 
 angular.module("app.codeCharta.core.data",[]);
+
+angular.module("app.codeCharta.core.data").service(
+    "dataDecoratorService", DataDecoratorService
+);
 
 angular.module("app.codeCharta.core.data").service(
     "dataLoadingService", DataLoadingService
@@ -23,3 +29,4 @@ angular.module("app.codeCharta.core.data").service(
 angular.module("app.codeCharta.core.data").service(
     "deltaCalculatorService", DeltaCalculatorService
 );
+
