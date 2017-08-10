af849265b0d2913a8c0b42784ab87153282756bc
mrpatpat <adrian.endrich@googlemail.com>
2017-08-10 16:09:00 +0200

    wrapping decorator services into promises, to make them chainable and make future webworker usage (multithreading) easier

--- visualization/app/codeCharta/ui/ui.js
+++ visualization/app/codeCharta/ui/ui.js
@@ -8,6 +8,7 @@ import "./legendPanel/legendPanel.js";
 import "./fileChooser/fileChooser.js";
 import "./detailPanel/detailPanel.js";
 import "./scenarioButtons/scenarioButtons.js";
+import "./loadingPanel/loadingPanel.js";
 
-angular.module("app.codeCharta.ui", ["app.codeCharta.ui.common", "app.codeCharta.ui.fabBar", "app.codeCharta.ui.settingsPanel", "app.codeCharta.ui.revisionChooser", "app.codeCharta.ui.legendPanel", "app.codeCharta.ui.fileChooser", "app.codeCharta.ui.detailPanel", "app.codeCharta.ui.scenarioButtons"]);
+angular.module("app.codeCharta.ui", ["app.codeCharta.ui.common", "app.codeCharta.ui.fabBar", "app.codeCharta.ui.settingsPanel", "app.codeCharta.ui.revisionChooser", "app.codeCharta.ui.legendPanel", "app.codeCharta.ui.fileChooser", "app.codeCharta.ui.detailPanel", "app.codeCharta.ui.scenarioButtons", "app.codeCharta.ui.loadingPanel"]);
 
