05e718cd514e9173545ef7cec842adf65f8ed3c5
mrpatpat <adrian.endrich@googlemail.com>
2017-08-18 15:00:15 +0200

    making the module more lean

--- visualization/app/codeCharta/ui/settingsPanel/index.js
+++ visualization/app/codeCharta/ui/settingsPanel/index.js
@@ -8,7 +8,6 @@ import "angularjs-slider";
 import {settingsPanelComponent, SettingsPanelController} from "./settingsPanel";
 
 angular.module("app.codeCharta.ui.settingsPanel", ["app.codeCharta.ui", "app.codeCharta.core", "rzModule"])
-    .controller("settingsPanelController", SettingsPanelController) // for test TODO create artificial module in test just for this purpose, since we dont need this controller exposed outside of module
     .component(settingsPanelComponent.selector, settingsPanelComponent);
 
 
