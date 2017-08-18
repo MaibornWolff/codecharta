ec74d6c52aca2984dc448b40518f404838e8206d
mrpatpat <adrian.endrich@googlemail.com>
2017-08-18 15:48:52 +0200

    preparing ui for beta release

--- visualization/app/codeCharta/ui/revisionChooser/revisionChooserDirective.js
+++ visualization/app/codeCharta/ui/revisionChooser/revisionChooserDirective.js
@@ -47,6 +47,34 @@ class RevisionChooserDirective {
          */
         this.bindToController = true;
 
+        /**
+         * visibility
+         * @type {boolean}
+         */
+        this.visible = false;
+    }
+
+    /**
+     * Links the click Handler
+     * @param {Scope} scope
+     * @param {object} element dom element
+     */
+    link(scope, element) {
+        element.find("#revisionButton").bind("click", this.toggle);
+        element.find("#mapButton").bind("click", this.toggle);
+    }
+
+    /**
+     * Toggles the visibility
+     */
+    toggle(){
+        if (this.visible) {
+            $("#revisionChooser").animate({left: -500 + "px"});
+            this.visible = false;
+        } else {
+            $("#revisionChooser").animate({left: 2.8+"em"});
+            this.visible = true;
+        }
     }
     
 }
