c97637e1a2acb687fffbc790ef44bdb8b08aa1f1
mrpatpat <adrian.endrich@googlemail.com>
2017-08-18 14:51:35 +0200

    refactoring old directive syntax to newer/shorter component syntax in settingsPanel

--- visualization/app/codeCharta/ui/ui.js
+++ visualization/app/codeCharta/ui/ui.js
@@ -2,7 +2,7 @@
 
 import "./common/common.js";
 import "./fabBar/fabBar.js";
-import "./settingsPanel/settingsPanel.js";
+import "./settingsPanel/index.js";
 import "./revisionChooser/revisionChooser.js";
 import "./legendPanel/legendPanel.js";
 import "./fileChooser/fileChooser.js";
