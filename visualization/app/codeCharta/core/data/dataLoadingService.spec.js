5549d3135677cab466fe0d606d1302ba0093d244
FernandoES <fernan_1989@hotmail.com>
2017-09-01 14:36:34 +0200

    comments

--- visualization/app/codeCharta/core/data/dataLoadingService.spec.js
+++ visualization/app/codeCharta/core/data/dataLoadingService.spec.js
@@ -5,42 +5,27 @@ require("./data.js");
  */
 describe("app.codeCharta.core.data.dataLoadingService", function() {
 
-    var firstFile;
-    var secondFile;
     var dataLoadingService;
+    var dataValidatorService;
 
     beforeEach(angular.mock.module("app.codeCharta.core.data"));
-    beforeEach(angular.mock.inject(function (_dataLoadingService_) {dataLoadingService = _dataLoadingService_;}));
+    beforeEach(angular.mock.inject(function (_dataLoadingService_, _dataValidatorService_) {
+        dataLoadingService = _dataLoadingService_;
+        dataValidatorService = _dataValidatorService_;
+    }));
 
-    beforeEach(function(){
-
-        firstFile = {
-            name: "name",
-            timestamp: "0",
-            root:{
-                name: "myRoot",
-                id: "anId",
-                attributes: {anAttribute: 12}
-            }
+    it("should resolve valid file",()=>{
+        dataValidatorService.validate = () => {
+            return new Promise((resolve,reject)=>{resolve();});
         };
-
-        secondFile = {
-            name: "name",
-            root:{
-                name: "myRoot",
-                id: "anId",
-                attributes: {anAttribute: 12}
-            }
-        };
-
+        return dataLoadingService.loadMapFromFileContent("valid");
     });
 
-    xit("should resolve firstFile",()=>{
-        return dataLoadingService.loadMapFromFileContent(firstFile);
-    });
-
-    xit("should reject secondFile",(done)=>{
-        dataLoadingService.loadMapFromFileContent(secondFile).then(
+    it("should reject invalid file",(done)=>{
+        dataValidatorService.validate = () => {
+            return new Promise((resolve,reject)=>{reject();});
+        };
+        dataLoadingService.loadMapFromFileContent("invalid").then(
             ()=>{
                 done("should reject");
             },
