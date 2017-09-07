a8b1abee765340a634cf61dc0350006794e56eef
Bartlomiej Pohl <bartlomiej.pohl@gmail.com>
2017-09-07 13:04:44 +0200

    added TypeScript Support to Project

--- visualization/conf/webpack.config.js
+++ visualization/conf/webpack.config.js
@@ -50,7 +50,15 @@ module.exports = {
                 use: [
                     'file-loader'
                 ]
-            }
+            },
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader', 'ts-loader'
+                ],
+                
+              }
         ]
     },
     plugins: [
