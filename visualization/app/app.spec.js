939350e0966c356d7a013cd2fc1d76485f838db8
mrpatpat <adrian.endrich@googlemail.com>
2017-10-17 11:34:57 +0200

    removing ts test files

--- visualization/app/app.spec.js
+++ visualization/app/app.spec.js
@@ -1,3 +1,3 @@
 // this file can stay js. no context problems with webpack and jasmine
-const context = require.context('./codeCharta', true, /\.(ts|tsx)$/);
+const context = require.context('./codeCharta', true, /\.(js|ts|tsx)$/);
 context.keys().forEach(context);
