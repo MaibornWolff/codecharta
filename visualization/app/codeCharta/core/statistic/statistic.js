d9b4de52890c4555a6633383cb7914634414d8c1
FernandoES <fernan_1989@hotmail.com>
2017-10-23 15:56:36 +0200

    comments

---
+++ visualization/app/codeCharta/core/statistic/statistic.js
@@ -0,0 +1,10 @@
+import {StatisticMapService} from "./statisticMapService.js";
+
+angular.module(
+    "app.codeCharta.core.statistic", []
+);
+
+angular.module("app.codeCharta.core.statistic").service(
+    "statisticMapService", StatisticMapService
+);
+
