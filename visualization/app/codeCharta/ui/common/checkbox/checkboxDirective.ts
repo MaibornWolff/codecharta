cd185d8fe62d5757d20ae1da71336281713e48ca
mrpatpat <adrian.endrich@googlemail.com>
2017-10-24 17:24:00 +0200

    fixing directive

--- visualization/app/codeCharta/ui/common/checkbox/checkboxDirective.ts
+++ visualization/app/codeCharta/ui/common/checkbox/checkboxDirective.ts
@@ -1,17 +1,17 @@
-"use strict";
+import {IDirective} from "angular";
 
 /**
  * A simple checkbox customized for CodeCharta. Watches for model changes and calls the bound change-function
  */
-class CheckboxDirective {
+class CheckboxDirective implements IDirective{
 
-    private template: string = "<div class='input-field col s12'><input type='checkbox' ng-model='model' id='checkbox-directive-{{::$id}}'><label for='checkbox-directive-{{::$id}}'>{{::label}}</label></div>";
-    private scope: any = {
+    template: string = "<div class='input-field col s12'><input type='checkbox' ng-model='model' id='checkbox-directive-{{::$id}}'><label for='checkbox-directive-{{::$id}}'>{{::label}}</label></div>";
+    scope: any = {
         model: "=",
         label: "@",
         change: "&"
     };
-    private restrict: string = "E";
+    restrict: string = "E";
 
     constructor() {}
 
