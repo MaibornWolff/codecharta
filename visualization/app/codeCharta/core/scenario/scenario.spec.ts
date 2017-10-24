5773519c681dd5a2881009bde8774fd8724ee42e
mrpatpat <adrian.endrich@googlemail.com>
2017-10-24 10:52:34 +0200

    ref scenario

--- visualization/app/codeCharta/core/scenario/scenario.spec.ts
+++ visualization/app/codeCharta/core/scenario/scenario.spec.ts
@@ -1,76 +1,41 @@
 import "./scenario.module.ts";
 import angular from "angular";
-import sinon from "sinon";
+import {ScenarioService, Scenario} from "./scenario.service.ts";
+import {SettingsService} from "../settings/settings.service.ts";
+import {createDefaultScenario} from "./scenario.data.ts";
 /**
  * @test {ScenatioService}
  */
-describe("app.codeCharta.core.scenarioService", function() {
+describe("app.codeCharta.core.scenarioService", function () {
 
-    var scenarioService, $scope, sandbox, settingsService, scenario;
+    let scenarioService: ScenarioService,
+        $scope,
+        scenario: Scenario,
+        defaultScenario: Scenario,
+        settingsService: SettingsService;
 
     //noinspection TypeScriptUnresolvedVariable
     beforeEach(angular.mock.module("app.codeCharta.core.scenario"));
 
     //noinspection TypeScriptUnresolvedVariable
-    beforeEach(angular.mock.inject((_scenarioService_, _settingsService_, _$rootScope_)=>{
+    beforeEach(angular.mock.inject((_scenarioService_, _settingsService_, _$rootScope_)=> {
         scenarioService = _scenarioService_;
         settingsService = _settingsService_;
         $scope = _$rootScope_;
-
-        let r = {
-            from: 20,
-            to: 40,
-            flipped: false
-        };
-
-        let s = {
-            x: 1, y: 1, z: 1
-        };
-
-        let c = {
-            x: 0, y: 300, z: 1000
-        };
-
-        scenario = {
-            map: {},
-            neutralColorRange: r,
-            areaMetric: "rloc",
-            heightMetric: "mcc",
-            colorMetric: "mcc",
-            deltas: false,
-            amountOfTopLabels: 1,
-            scaling: s,
-            camera: c,
-            margin: 1
-        };
-
+        scenario = {name: "testScenario", settings: settingsService.settings};
+        defaultScenario = createDefaultScenario(settingsService.settings.map);
     }));
 
-    beforeEach(()=>{
-        sandbox = sinon.sandbox.create();
-    });
-
-    afterEach(()=>{
-        sandbox.restore();
-    });
-
-    /**
-     * @test {ScenarioService#applyScenario}
-     */
-    it("should apply the settings from a given scenario", ()=>{
-        settingsService.applySettings = sinon.spy();
+    it("should apply the settings from a given scenario", ()=> {
         scenarioService.applyScenario(scenario);
-        sinon.assert.calledWith(settingsService.applySettings, sinon.match.same(scenario.settings));
+        expect(settingsService.settings).toBe(scenario.settings);
     });
 
-    /**
-     * @test {ScenarioService#getDefaultScenario}
-     */
-    it("default scenario should be rloc/mcc/mcc", ()=>{
+    it("default scenario should be rloc/mcc/mcc", ()=> {
         var scenario = scenarioService.getDefaultScenario();
-        expect(scenario.settings.areaMetric).toBe("rloc");
-        expect(scenario.settings.heightMetric).toBe("mcc");
-        expect(scenario.settings.colorMetric).toBe("mcc");
+        expect(scenario.settings.areaMetric).toBe(defaultScenario.settings.areaMetric);
+        expect(scenario.settings.heightMetric).toBe(defaultScenario.settings.heightMetric);
+        expect(scenario.settings.colorMetric).toBe(defaultScenario.settings.colorMetric);
     });
 
 });
