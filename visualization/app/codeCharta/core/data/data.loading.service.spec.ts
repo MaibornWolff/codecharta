f31e0d37e0c4234b288c8c48edc2a4f83ec543c3
mrpatpat <adrian.endrich@googlemail.com>
2017-10-24 11:08:56 +0200

    refactoring loading tests, deleting obsolete test

--- visualization/app/codeCharta/core/data/data.loading.service.spec.ts
+++ visualization/app/codeCharta/core/data/data.loading.service.spec.ts
@@ -1,117 +1,32 @@
 import "./data.module.ts";
 import angular from "angular";
+import {DataLoadingService} from "./data.loading.service.ts";
+import {TEST_FILE_CONTENT} from "./data.mocks.ts";
 
 /**
- * TODO: refactor
  * @test {DataLoadingService}
  */
-describe("app.codeCharta.core.data.dataLoadingService", function() {
+describe("app.codeCharta.core.data.dataLoadingService", function () {
 
-    var firstFile;
-    var secondFile;
-    var dataLoadingService;
-    var $httpBackend;
+    let dataLoadingService: DataLoadingService,
+        validFileContent;
 
     //noinspection TypeScriptUnresolvedVariable
     beforeEach(angular.mock.module("app.codeCharta.core.data"));
     //noinspection TypeScriptUnresolvedVariable
-    beforeEach(angular.mock.inject(function (_dataLoadingService_, _$httpBackend_) {
+    beforeEach(angular.mock.inject(function (_dataLoadingService_) {
         dataLoadingService = _dataLoadingService_;
-        $httpBackend = _$httpBackend_;
+        validFileContent = TEST_FILE_CONTENT;
     }));
 
-    beforeEach(function() {
-
-        firstFile = {
-            "projectName": "Sample Project",
-            "nodes": [{
-                "name": "root",
-                "attributes": {},
-                "children": [
-                    {
-                        "name": "big leaf",
-                        "attributes": {"rloc": 100, "functions": 10, "mcc": 1},
-                        "link": "http://www.google.de"
-                    },
-                    {
-                        "name": "Parent Leaf",
-                        "attributes": {},
-                        "children": [
-                            {
-                                "name": "small leaf",
-                                "attributes": {"rloc": 30, "functions": 100, "mcc": 100}
-                            },
-                            {
-                                "name": "other small leaf",
-                                "attributes": {"rloc": 70, "functions": 1000, "mcc": 10}
-                            }
-                        ]
-                    }
-                ]
-            }
-            ]
-
-        };
-
-        secondFile = {
-
-            "nodes": [{
-                "name": "root",
-                "id": 20,
-                "attributes": {},
-                "children": [
-                    {
-                        "name": "big leaf",
-                        "id": 0,
-                        "attributes": {"rloc": 120, "functions": 10, "mcc": 1},
-                        "link": "http://www.google.de"
-                    },
-                    {
-                        "name": "Parent Leaf",
-                        "id": 1,
-                        "attributes": {},
-                        "children": [
-                            {
-                                "name": "small leaf",
-                                "id": 2,
-                                "attributes": {"rloc": 30, "functions": 101, "mcc": 80}
-                            },
-                            {
-                                "name": "other small leaf",
-                                "id": 3,
-                                "attributes": {"rloc": 70, "functions": 10, "mcc": 100}
-                            }
-                        ]
-                    }
-                ]
-            }]
-
-        };
+    it("should resolve valid file", (done)=> {
+        dataLoadingService.loadMapFromFileContent("file.json", validFileContent, 0).then(done, done.fail);
     });
 
-    xit("should resolve firstFile",(done)=>{
-        dataLoadingService.loadMapFromFileContent("file1.json", firstFile, 0).then(
-            ()=>{
-                done();
-            },
-            ()=>{
-                done.fail("should succeed");
-            }
-        );
-        $httpBackend.flush();
-    });
-
-    xit("should reject secondFile",(done)=>{
-        dataLoadingService.loadMapFromFileContent("file2.json", secondFile, 1).then(
-            ()=>{
-                done.fail("should reject");
-            },
-            ()=>{
-                done();
-            }
-        );
-        $httpBackend.flush();
-
+    it("should reject invalid file", (done)=> {
+        let invalidFileContent = validFileContent;
+        delete invalidFileContent.projectName;
+        dataLoadingService.loadMapFromFileContent("file.json", invalidFileContent, 0).then(()=>{done.fail()}, done);
     });
 
 });
