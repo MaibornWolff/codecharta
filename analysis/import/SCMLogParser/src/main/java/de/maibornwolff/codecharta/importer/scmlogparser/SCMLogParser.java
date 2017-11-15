1af0a93237c40b646155170097fac01efdc9cff7
Dominik Ufer <dominik.ufer@maibornwolff.de>
2017-11-15 18:58:02 +0100

    Changed implementation of argument parsing in scmlogparser to JCommander

--- analysis/import/SCMLogParser/src/main/java/de/maibornwolff/codecharta/importer/scmlogparser/SCMLogParser.java
+++ analysis/import/SCMLogParser/src/main/java/de/maibornwolff/codecharta/importer/scmlogparser/SCMLogParser.java
@@ -16,68 +16,38 @@ import java.util.stream.Stream;
 public class SCMLogParser {
 
     public static void main(String[] args) throws IOException {
-        if (args.length >= 1) {
-            if (args[0].equals("-h") || args[0].equals("--help")) {
-                showHelpAndTerminate();
-            }
-        }
-        if (args.length >= 2) {
-            String pathToLog = args[0];
-            String gitOrSvn = args[1];
+        SCMLogParserParameter callParameter = new SCMLogParserParameter(args);
 
-            String outputFile = null;
-
+        if (callParameter.isHelp() || callParameter.getFiles().size() != 1) {
+            callParameter.printUsage();
+        } else {
+            String pathToLog = callParameter.getFiles().get(0);
+            SCMLogParserParameter.SCM gitOrSvn = callParameter.getSCM();
+            boolean addAuthor = callParameter.isAddAuthor();
             boolean containsAuthors = false;
+            String outputFile = callParameter.getOutputFile();
 
-            if (args.length >= 3) {
-                if ("--a".equals(args[2])) {
-                    containsAuthors = true;
-                    if (args.length >= 4) {
-                        outputFile = args[3];
-                    }
-                } else {
-                    outputFile = args[2];
-                }
-            }
-
-            Project project = parseDataFromLog(pathToLog, gitOrSvn, containsAuthors);
-            if (outputFile != null) {
+            Project project = parseDataFromLog(pathToLog, gitOrSvn, addAuthor);
+            if (outputFile != null && !outputFile.isEmpty()) {
                 ProjectSerializer.serializeProjectAndWriteToFile(project, outputFile);
             } else {
                 ProjectSerializer.serializeProject(project, new OutputStreamWriter(System.out));
             }
-        } else {
-            showErrorAndTerminate();
         }
     }
 
-    private static Project parseDataFromLog(String pathToLog, String gitOrSvn, boolean containsAuthors) throws IOException {
+    private static Project parseDataFromLog(String pathToLog, SCMLogParserParameter.SCM scm, boolean containsAuthors) throws IOException {
         LogParserStrategy parserStrategy = null;
-        switch (gitOrSvn) {
-            case "--git":
+        switch (scm) {
+            default:
+            case GIT:
                 parserStrategy = new GitLogParserStrategy();
                 break;
-            case "--svn":
+            case SVN:
                 parserStrategy = new SVNLogParserStrategy();
                 break;
-            default:
-                showErrorAndTerminate();
         }
         Stream<String> lines = Files.lines(Paths.get(pathToLog));
         return new LogParser(parserStrategy, containsAuthors).parse(lines);
     }
-
-    private static void showErrorAndTerminate() {
-        System.out.println("Invalid arguments!\n");
-        showHelpAndTerminate();
-    }
-
-    private static void showHelpAndTerminate() {
-        System.out.println("Please use the following syntax\n\"SCMLogParser-x.x.jar <pathToLogFile> --git/--svn [--a] [<pathToOutputfile>]\"\n" +
-                "The log file must have been created by using \"svn log --verbose\" or \"git log --name-status\"\n" +
-                "Set the --a parameter to add an array of authors to every file of your output\n" +
-                "If no output file was specified, the output will be piped to standard out");
-        System.exit(0);
-    }
-
 }
