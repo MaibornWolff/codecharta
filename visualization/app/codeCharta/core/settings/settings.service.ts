9e43d37495d437e8aa44d3e8f176e1799394b56a
mrpatpat <adrian.endrich@googlemail.com>
2018-01-24 16:14:14 +0100

    adding visibility to nodes, tree view uses it as visibility parameter

--- visualization/app/codeCharta/core/settings/settings.service.ts
+++ visualization/app/codeCharta/core/settings/settings.service.ts
@@ -149,7 +149,7 @@ export class SettingsService implements DataServiceSubscriber, CameraChangeSubsc
      * Broadcasts a settings-changed event with the new {Settings} object as a payload
      * @emits {settings-changed} on call
      */
-    private onSettingsChanged() {
+    public onSettingsChanged() {
 
         if (this._lastDeltaState && !this._settings.deltas) {
             this._lastDeltaState = false;
