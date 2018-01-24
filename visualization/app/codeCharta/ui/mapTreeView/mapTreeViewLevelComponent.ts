73bea9483ee8e0226f0bfa36c79125a0690e2d5f
mrpatpat <adrian.endrich@googlemail.com>
2018-01-24 16:05:24 +0100

    adding visibility markers and buttons

--- visualization/app/codeCharta/ui/mapTreeView/mapTreeViewLevelComponent.ts
+++ visualization/app/codeCharta/ui/mapTreeView/mapTreeViewLevelComponent.ts
@@ -7,14 +7,29 @@ export class MapTreeViewLevelController {
     public node: CodeMapNode = null;
     public depth: number = 0;
     public collapsed: boolean = true;
+    public visible: boolean = true;
 
     /* @ngInject */
-    constructor(private $timeout: ITimeoutService) {
-
+    constructor(private $timeout: ITimeoutService, private $scope) {
+        this.$scope.$on("tree-parent-visibility-changed", (event, visibility)=>{
+            this.visible = visibility;
+        });
     }
 
     onLabelClick() {
         this.collapsed = !this.collapsed;
+    }
+
+    onEyeClick() {
+        this.setAndBroadcastVisibility(!this.visible);
+    }
+
+    setAndBroadcastVisibility(value: boolean) {
+        this.visible = value;
+        if(!this.isLeaf()) {
+            //broadcast to children
+            this.$scope.$broadcast("tree-parent-visibility-changed", value);
+        }
     }
 
     isLeaf(): boolean {
