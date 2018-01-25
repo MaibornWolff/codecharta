2c532f7619b898922b4ef5cc27574d25654bf401
mrpatpat <adrian.endrich@googlemail.com>
2018-01-25 14:54:26 +0100

    testing

--- visualization/script/travis-build.sh
+++ visualization/script/travis-build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-echo "TEST"
-echo $NPM_TOKEN
-echo "TEST"
-
 cd visualization
 npm install
 npm run build
+
+echo "makeshifting .npmrc with Token from Travis settings"
+./node_modules/.bin/makeshift -t $NPM_TOKEN
+echo "npm whoami for check"
+npm whoami
+
 npm run test
 sonar-scanner || true
 npm run doc
