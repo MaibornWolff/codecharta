07aa9fde902065c58e6ca173bd47d2e8b735a0ea
Dominik Ufer <dominik.ufer@maibornwolff.de>
2018-01-25 17:39:43 +0100

    Added Metrics

---
+++ analysis/import/SCMLogParser/src/main/java/de/maibornwolff/codecharta/importer/scmlogparser/input/metrics/HighlyCoupledFiles.java
@@ -0,0 +1,57 @@
+package de.maibornwolff.codecharta.importer.scmlogparser.input.metrics;
+
+import de.maibornwolff.codecharta.importer.scmlogparser.input.Commit;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * experimental -> therefore no tests
+ */
+public class HighlyCoupledFiles implements Metric {
+    public static final double HIGH_COUPLING_VALUE = .35;
+    public static final long MIN_NO_COMMITS_FOR_HIGH_COUPLING = 5L;
+
+    private final Map<String, Long> simultaneouslyCommitedFiles = new HashMap<>();
+    private final List<Integer> numberCommitedFiles = new ArrayList<>();
+    private long numberOfCommits = 0;
+
+    @Override
+    public String description() {
+        return "Highly Coupled Files: Number of highly coupled files with this file.";
+    }
+
+    @Override
+    public String metricName() {
+        return "highly_coupled_files";
+    }
+
+    @Override
+    public Number value() {
+        return simultaneouslyCommitedFiles.values().stream()
+                .filter(this::isHighlyCoupled)
+                .count();
+    }
+
+    private boolean isHighlyCoupled(long val) {
+        if (numberOfCommits > MIN_NO_COMMITS_FOR_HIGH_COUPLING) {
+            return (double) val / (double) numberOfCommits > HIGH_COUPLING_VALUE;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void registerCommit(Commit commit) {
+        numberOfCommits++;
+        commit.getModifications()
+                .forEach(
+                        mod -> {
+                            simultaneouslyCommitedFiles.merge(mod.getFilename(), 1L, (x, y) -> x + y);
+                        }
+                );
+        numberCommitedFiles.add(commit.getModifications().size() - 1);
+    }
+}
