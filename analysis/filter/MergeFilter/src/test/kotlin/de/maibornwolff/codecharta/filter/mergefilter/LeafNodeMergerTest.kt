4e9e785ccf997d582bc2df680981e46a2533ce23
Dominik Ufer <dominik.ufer@maibornwolff.de>
2018-02-14 15:56:09 +0100

    testing spek as testing framework

--- analysis/filter/MergeFilter/src/test/kotlin/de/maibornwolff/codecharta/filter/mergefilter/LeafNodeMergerTest.kt
+++ analysis/filter/MergeFilter/src/test/kotlin/de/maibornwolff/codecharta/filter/mergefilter/LeafNodeMergerTest.kt
@@ -32,129 +32,128 @@ package de.maibornwolff.codecharta.filter.mergefilter
 import de.maibornwolff.codecharta.model.Node
 import de.maibornwolff.codecharta.model.NodeType
 import de.maibornwolff.codecharta.model.Path
-import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.hasSize
-import org.junit.Assert.assertThat
-import org.junit.Test
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 
-class LeafNodeMergerTest {
-    private val fittingMerger = LeafNodeMergerStrategy(false)
+class LeafNodeMergerTest : Spek({
+    describe("a fitting merger") {
+        val fittingMerger = LeafNodeMergerStrategy(false)
 
-    @Test
-    fun should_merge_nodes_with_same_name() {
-        // given
-        val node1 = Node("Name", NodeType.File)
-        val node2 = Node("Name", NodeType.Folder)
+        on("merging nodes with same name") {
+            val node1 = Node("Name", NodeType.File)
+            val node2 = Node("Name", NodeType.Folder)
 
-        // when
-        val nodeList = fittingMerger.mergeNodeLists(listOf(listOf(node1), listOf(node2)))
+            val nodeList = fittingMerger.mergeNodeLists(listOf(listOf(node1), listOf(node2)))
 
-        // then
-        assertThat(nodeList, hasSize(1))
+            it("should return merged node") {
+                assertThat(nodeList, hasSize(1))
+            }
+        }
+
+        on("merging nodes with children and with same name") {
+            val child1 = Node("child1", NodeType.File)
+            val child2 = Node("child2", NodeType.Folder)
+            val child1_littleBitDifferent =
+                    Node("child1", NodeType.File, mapOf(Pair("someAttribute", 1.0f)), "", listOf())
+            val node1 = Node("Name", NodeType.File, mapOf(), "", listOf(child1_littleBitDifferent))
+            val node2 = Node("Name", NodeType.File, mapOf(), "", listOf(child1, child2))
+
+            val newNode = fittingMerger.mergeNodeLists(listOf(listOf(node2), listOf(node1)))[0]
+
+            it("should merge children") {
+                assertThat(newNode.children, hasSize(2))
+                assertThat(newNode.children[0].name, `is`(child1.name))
+                assertThat(newNode.children[0].type, `is`(child1.type))
+                assertThat(newNode.children[1].name, `is`(child2.name))
+                assertThat(newNode.children[1].type, `is`(child2.type))
+            }
+
+            it("should merge attributes") {
+                assertThat(newNode.children[0].attributes.size, `is`(1))
+            }
+        }
+
+        on("merging nodes with children and with same name") {
+            // given
+            val childA = Node("A", NodeType.File, mapOf("a" to 0))
+            val childB = Node("B", NodeType.Folder, mapOf(), "", listOf(Node("A", NodeType.File)))
+            val root1 = Node("root", NodeType.Folder, mapOf(), "", listOf(childA, childB))
+
+            val childA2 = Node("A", NodeType.File)
+            val childB2 = Node("B", NodeType.Folder, mapOf(), "", listOf(Node("A", NodeType.File)))
+            val root2 = Node("root", NodeType.Folder, mapOf(), "", listOf(childA2, childB2))
+
+            // when
+            val newRoot = fittingMerger.mergeNodeLists(listOf(listOf(root1), listOf(root2)))[0]
+
+            it("should merge children") {
+                assertThat(newRoot.children, hasSize(2))
+                assertThat((newRoot.getNodeBy(Path(listOf("B", "A"))) as Node).attributes.size, `is`(0))
+            }
+        }
+
+        it("should merge leafs") {
+            // given
+            val child1_littleBitDifferent =
+                    Node("child1", NodeType.File, mapOf(Pair("someAttribute", 1.0f)), "", listOf())
+            val node1 = Node("root", NodeType.File, mapOf(), "", listOf(child1_littleBitDifferent))
+            val child1 = Node("child1", NodeType.File)
+            val intermediateNode = Node("intermediateNode", NodeType.File, mapOf(), "", listOf(child1))
+            val node2 = Node("root", NodeType.File, mapOf(), "", listOf(intermediateNode))
+
+            // when
+            val newNode = fittingMerger.mergeNodeLists(listOf(listOf(node1), listOf(node2)))[0]
+
+            // then
+            assertThat(newNode.children, hasSize(1))
+            assertThat(newNode.children[0].name, `is`(child1.name))
+            assertThat(newNode.children[0].type, `is`(child1.type))
+        }
+
+
+        on("merging empty nodes") {
+            val nodeList = fittingMerger.mergeNodeLists(listOf())
+
+            it("should return empty node list") {
+                assertThat(nodeList, hasSize(0))
+            }
+        }
+
+        on("merging single node list") {
+            val nodeList = listOf(Node("node", NodeType.File, mapOf()))
+            val actualNodeList = fittingMerger.mergeNodeLists(listOf(nodeList))
+
+            it("should return same node list") {
+                assertThat(actualNodeList, `is`(nodeList))
+            }
+        }
     }
 
-    @Test
-    fun merging_nodes_should_merge_children() {
+    describe("a misfitting merger") {
         val misfittingMerger = LeafNodeMergerStrategy(true)
+        on("merging nodes with children and with same name") {
+            val child1 = Node("child1", NodeType.File)
+            val child2 = Node("child2", NodeType.Folder)
+            val child1_littleBitDifferent =
+                    Node("child1", NodeType.File, mapOf(Pair("someAttribute", 1.0f)), "", listOf())
+            val node1 = Node("Name", NodeType.File, mapOf(), "", listOf(child1_littleBitDifferent))
+            val node2 = Node("Name", NodeType.File, mapOf(), "", listOf(child1, child2))
 
-        // given
-        val child1 = Node("child1", NodeType.File)
-        val child2 = Node("child2", NodeType.Folder)
-        val child1_littleBitDifferent = Node("child1", NodeType.File, mapOf(Pair("someAttribute", 1.0f)), "", listOf())
-        val node1 = Node("Name", NodeType.File, mapOf(), "", listOf(child1_littleBitDifferent))
-        val node2 = Node("Name", NodeType.File, mapOf(), "", listOf(child1, child2))
+            val newNode = misfittingMerger.mergeNodeLists(listOf(listOf(node1), listOf(node2)))[0]
 
-        // when
-        val newNode = misfittingMerger.mergeNodeLists(listOf(listOf(node1), listOf(node2)))[0]
+            it("should merge children") {
+                assertThat(newNode.children, hasSize(2))
+                assertThat(newNode.children[0].name, `is`(child1.name))
+                assertThat(newNode.children[0].type, `is`(child1.type))
+                assertThat(newNode.children[1].name, `is`(child2.name))
+                assertThat(newNode.children[1].type, `is`(child2.type))
+            }
+        }
 
-        // then
-        assertThat(newNode.children, hasSize(2))
-        assertThat(newNode.children[0].name, `is`(child1.name))
-        assertThat(newNode.children[0].type, `is`(child1.type))
-        assertThat(newNode.children[1].name, `is`(child2.name))
-        assertThat(newNode.children[1].type, `is`(child2.type))
     }
-
-    @Test
-    fun merging_nodes_should_merge_children2() {
-        // given
-        val child1 = Node("child1", NodeType.File)
-        val child2 = Node("child2", NodeType.Folder)
-        val child1_littleBitDifferent = Node("child1", NodeType.File, mapOf(Pair("someAttribute", 1.0f)), "", listOf())
-        val node1 = Node("Name", NodeType.File, mapOf(), "", listOf(child1_littleBitDifferent))
-        val node2 = Node("Name", NodeType.File, mapOf(), "", listOf(child1, child2))
-
-        // when
-        val newNode = fittingMerger.mergeNodeLists(listOf(listOf(node2), listOf(node1)))[0]
-
-        // then
-        assertThat(newNode.children, hasSize(2))
-        assertThat(newNode.children[0].name, `is`(child1.name))
-        assertThat(newNode.children[0].type, `is`(child1.type))
-        assertThat(newNode.children[0].attributes.size, `is`(1))
-        assertThat(newNode.children[1].name, `is`(child2.name))
-        assertThat(newNode.children[1].type, `is`(child2.type))
-    }
-
-    @Test
-    fun merging_nodes_should_merge_leafs() {
-        // given
-        val child1_littleBitDifferent = Node("child1", NodeType.File, mapOf(Pair("someAttribute", 1.0f)), "", listOf())
-        val node1 = Node("root", NodeType.File, mapOf(), "", listOf(child1_littleBitDifferent))
-        val child1 = Node("child1", NodeType.File)
-        val intermediateNode = Node("intermediateNode", NodeType.File, mapOf(), "", listOf(child1))
-        val node2 = Node("root", NodeType.File, mapOf(), "", listOf(intermediateNode))
-
-        // when
-        val newNode = fittingMerger.mergeNodeLists(listOf(listOf(node1), listOf(node2)))[0]
-
-        // then
-        assertThat(newNode.children, hasSize(1))
-        assertThat(newNode.children[0].name, `is`(child1.name))
-        assertThat(newNode.children[0].type, `is`(child1.type))
-    }
-
-    @Test
-    fun merging_empty_nodes_should_return_empty_nodelist() {
-        // given
-        val emptyNodeList = listOf<List<Node>>()
-
-        // when
-        val nodeList = fittingMerger.mergeNodeLists(emptyNodeList)
-
-        // then
-        assertThat(nodeList, hasSize(0))
-    }
-
-    @Test
-    fun merging_single_nodeList_should_return_same_nodelist() {
-        // given
-        val nodeList = listOf(Node("node", NodeType.File, mapOf()))
-
-        // when
-        val actualNodeList = fittingMerger.mergeNodeLists(listOf(nodeList))
-
-        // then
-        assertThat(actualNodeList, `is`(nodeList))
-    }
-
-
-    @Test
-    fun merging_nodes_should_merge_children3(){
-        // given
-        val childA = Node("A", NodeType.File, mapOf("a" to 0))
-        val childB = Node("B", NodeType.Folder, mapOf(), "", listOf(Node("A", NodeType.File)))
-        val root1 = Node("root", NodeType.Folder, mapOf(), "", listOf(childA, childB))
-
-        val childA2 = Node("A", NodeType.File)
-        val childB2 = Node("B", NodeType.Folder, mapOf(), "", listOf(Node("A", NodeType.File)))
-        val root2 = Node("root", NodeType.Folder, mapOf(), "", listOf(childA2, childB2))
-
-        // when
-        val newRoot = fittingMerger.mergeNodeLists(listOf(listOf(root1), listOf(root2)))[0]
-
-        // then
-        assertThat(newRoot.children, hasSize(2))
-        assertThat((newRoot.getNodeBy(Path(listOf("B", "A"))) as Node).attributes.size, `is`(0))
-    }
-}
+})
