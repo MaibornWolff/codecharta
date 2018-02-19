973cd03c2a2285dd01ed27bdf7169f3617f8ef3e
Dominik Ufer <dominik.ufer@maibornwolff.de>
2018-02-19 21:34:09 +0100

    fixes in MergeFilter

--- analysis/filter/MergeFilter/src/main/kotlin/de/maibornwolff/codecharta/filter/mergefilter/NodeMergerStrategy.kt
+++ analysis/filter/MergeFilter/src/main/kotlin/de/maibornwolff/codecharta/filter/mergefilter/NodeMergerStrategy.kt
@@ -32,5 +32,5 @@ package de.maibornwolff.codecharta.filter.mergefilter
 import de.maibornwolff.codecharta.model.Node
 
 interface NodeMergerStrategy {
-    fun mergeNodeLists(lists: List<List<Node>>?): List<Node>
+    fun mergeNodeLists(lists: List<List<Node>>): List<Node>
 }
--- analysis/filter/MergeFilter/src/main/kotlin/de/maibornwolff/codecharta/filter/mergefilter/RecursiveNodeMergerStrategy.kt
+++ analysis/filter/MergeFilter/src/main/kotlin/de/maibornwolff/codecharta/filter/mergefilter/RecursiveNodeMergerStrategy.kt
@@ -34,23 +34,28 @@ import de.maibornwolff.codecharta.model.Node
 /**
  * merges nodes recursively if their paths coincide
  */
-class RecursiveNodeMergerStrategy : NodeMergerStrategy {
+class RecursiveNodeMergerStrategy(ignoreCase: Boolean = false) : NodeMergerStrategy {
     private val flatNodeMerger = NodeMerger()
 
-    private val mergeConditionSatisfied = { n1: Node, n2: Node -> n1.name == n2.name }
+    private val mergeConditionSatisfied: (Node, Node) -> Boolean
 
-    override fun mergeNodeLists(lists: List<List<Node>>?): List<Node> {
-        return lists!!.fold(listOf(), { nodes, actual -> reduceNodeList(nodes, actual) })
+    init {
+        mergeConditionSatisfied = if (ignoreCase) { n1: Node, n2: Node -> n1.name.toUpperCase() == n2.name.toUpperCase() }
+        else { n1: Node, n2: Node -> n1.name == n2.name }
     }
 
-    private fun reduceNodeList(total: List<Node>, actual: List<Node>): List<Node> {
-        return actual.fold(total, { t: List<Node>, a: Node ->
-            t.map { if (mergeConditionSatisfied(it, a)) merge(it, a) else listOf(it) }
-            when {
-                t.filter { mergeConditionSatisfied(it, a) }.count() > 0 -> t.map { if (mergeConditionSatisfied(it, a)) merge(it, a) else it }
-                else -> t + a
-            }
-        })
+    override fun mergeNodeLists(lists: List<List<Node>>): List<Node> {
+        return if (lists.isEmpty()) listOf()
+        else lists.reduce { total, next ->
+            next.fold(total, { t: List<Node>, a: Node ->
+                t.map { if (mergeConditionSatisfied(it, a)) merge(it, a) else it }.map { listOf(it) }
+                when {
+                    t.filter { mergeConditionSatisfied(it, a) }.count() > 0 ->
+                        t.map { if (mergeConditionSatisfied(it, a)) merge(it, a) else it }
+                    else -> t + a
+                }
+            })
+        }
     }
 
     private fun merge(vararg nodes: Node): Node {
