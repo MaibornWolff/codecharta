300b02b4187bbc60381f311d77ed98c2aff31049
mrpatpat <adrian.endrich@googlemail.com>
2018-02-27 22:28:09 +0100

    adding sass support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/)
 
 ## [unreleased]
 ### Added
+- SASS support 
 
 ### Changed
 - Clean up UI #86
--- visualization/conf/webpack.loaders.js
+++ visualization/conf/webpack.loaders.js
@@ -14,6 +14,10 @@ module.exports = {
             use: ['style-loader', 'css-loader']
         },
         {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
             test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']
         },
         {
