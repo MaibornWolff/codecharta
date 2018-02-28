9e63470983e7d8be3e7432d399ac5850f58f91cc
mrpatpat <adrian.endrich@googlemail.com>
2018-02-28 01:17:56 +0100

    adding angular material

--- visualization/app/app.ts
+++ visualization/app/app.ts
@@ -1,11 +1,14 @@
 import angular from "angular";
+import "angular-animate";
+import "angular-aria";
+import "angular-material";
 import "script-loader!jquery";
 import 'script-loader!materialize-css/dist/js/materialize.min.js'
 import "./codeCharta/codeCharta";
 import "./assets/icon.ico";
 import "./app.scss";
 
-angular.module("app", ["app.codeCharta"]);
+angular.module("app", ["app.codeCharta", "ngMaterial"]);
 
 angular.module("app").config(["$locationProvider", function($locationProvider) {
     $locationProvider.hashPrefix("");
