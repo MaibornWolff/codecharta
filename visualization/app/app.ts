0f99cb33f5bb2775869a29617da9e4b0025d9d83
mrpatpat <adrian.endrich@googlemail.com>
2018-02-28 20:06:43 +0100

    removing materialize

--- visualization/app/app.ts
+++ visualization/app/app.ts
@@ -2,8 +2,6 @@ import angular from "angular";
 import "angular-animate";
 import "angular-aria";
 import "angular-material";
-import "script-loader!jquery";
-import 'script-loader!materialize-css/dist/js/materialize.min.js'
 import "./codeCharta/codeCharta";
 import "./assets/icon.ico";
 import "./app.scss";
