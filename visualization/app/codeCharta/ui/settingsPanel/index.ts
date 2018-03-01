38775655a0104d45becb7a733f7a7d2942bf23ae
mrpatpat <adrian.endrich@googlemail.com>
2018-03-01 16:30:27 +0100

    adding collapsible panels to settings

--- visualization/app/codeCharta/ui/settingsPanel/index.ts
+++ visualization/app/codeCharta/ui/settingsPanel/index.ts
@@ -4,11 +4,13 @@ import "../../ui/ui";
 import "../../core/core.module";
 import "../codeMap/codeMap";
 
+import "angular-material-expansion-panel";
+
 import angular from "angular";
 
 import {settingsPanelComponent} from "./settingsPanel";
 
-angular.module("app.codeCharta.ui.settingsPanel", ["app.codeCharta.ui", "app.codeCharta.core"])
+angular.module("app.codeCharta.ui.settingsPanel", ["app.codeCharta.ui", "app.codeCharta.core", "material.components.expansionPanels"])
     .component(settingsPanelComponent.selector, settingsPanelComponent);
 
 
