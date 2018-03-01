ac069c15f01e75f760d9371534a72f82f42b86e1
mrpatpat <adrian.endrich@googlemail.com>
2018-03-01 16:43:27 +0100

    rzSlider update on expansion

--- visualization/app/codeCharta/ui/settingsPanel/settingsPanel.ts
+++ visualization/app/codeCharta/ui/settingsPanel/settingsPanel.ts
@@ -1,74 +1,27 @@
 "use strict";
-import {STATISTIC_OPS, StatisticMapService} from "../../core/statistic/statistic.service";
-import {SettingsService, Settings, SettingsServiceSubscriber} from "../../core/settings/settings.service";
-import {DataService, DataServiceSubscriber, DataModel} from "../../core/data/data.service";
-import {TreeMapService} from "../../core/treemap/treemap.service";
 import "./settingsPanel.scss";
 
 /**
  * Controls the settingsPanel
  */
-export class SettingsPanelController implements DataServiceSubscriber, SettingsServiceSubscriber{
-
-    public settings: Settings;
-    public metrics: string[];
-    public data: DataModel;
-    public STATISTIC_OPS = STATISTIC_OPS;
-
+export class SettingsPanelController{
 
     /* @ngInject */
     constructor(
-        private settingsService: SettingsService,
-        private dataService: DataService,
-        private statisticMapService: StatisticMapService
+        private $scope,
+        private $timeout
     ) {
-
-        this.settings = settingsService.settings;
-        this.data = dataService.data;
-        this.STATISTIC_OPS = STATISTIC_OPS;
-
-        this.metrics = this.sortStringArrayAlphabetically(dataService.data.metrics);
-
-        this.dataService.subscribe(this);
-        this.settingsService.subscribe(this);
-
     }
 
     /**
-     * called on data change.
-     * @param {DataModel} data
+     * This is necessary to update the rzSlider on panel expansion
+     * @param $panel
      */
-    onDataChanged(data: DataModel) {
-        this.metrics = this.sortStringArrayAlphabetically(data.metrics);
-        this.onStatisticsChange();
-    }
-
-    onSettingsChanged(settings: Settings, event: Event) {
-        this.settings = settings;
-    }
-
-    /**
-     * Notifies the settings service about changes.
-     */
-    notify() {
-        this.settingsService.applySettings();
-    }
-
-    /**
-     * Sorts a simple string array in alphabetic order.
-     * @param {string[]} arr
-     * @returns {string[]} sortedArr
-     */
-    sortStringArrayAlphabetically(arr: string[]): string[] {
-        return arr.sort();
-    }
-
-    /**
-     * Updates the map before broadcasting the update of the settings
-     */
-    onStatisticsChange(){
-        this.settings.map = this.statisticMapService.unifyMaps(this.data, this.settings);
-        this.notify();
+    collapseAndUpdateChildRzSlider($panel) {
+        $panel.collapse();
+        this.$timeout(() => {
+            this.$scope.$broadcast('rzSliderForceRender');
+        },50)
     }
 
 }
