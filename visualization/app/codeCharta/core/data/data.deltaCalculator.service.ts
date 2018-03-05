9773fe07f476b8a1112d818740b836a3f84e229a
mrpatpat <adrian.endrich@googlemail.com>
2018-03-05 01:52:48 +0100

    propably working ?

--- visualization/app/codeCharta/core/data/data.deltaCalculator.service.ts
+++ visualization/app/codeCharta/core/data/data.deltaCalculator.service.ts
@@ -19,41 +19,13 @@ export class DeltaCalculatorService {
 
     }
 
-    public makeDeltasHappen(firstMap: CodeMap, secondMap: CodeMap) {
-
-        //make hashmaps with paths as indices
-        let firstLeafHashMap = new Map<string, CodeMapNode>();
-        d3.hierarchy(firstMap.root).leaves().forEach((node: HierarchyNode<CodeMapNode>) => {
-            firstLeafHashMap.set(node.data.path, node.data);
-        });
-
-        let secondLeafHashMap = new Map<string, CodeMapNode>();
-        d3.hierarchy(secondMap.root).leaves().forEach((node: HierarchyNode<CodeMapNode>) => {
-            secondLeafHashMap.set(node.data.path, node.data);
-        });
-
-        // iterate over maps and insert nodes
-        this.insertNodes(firstLeafHashMap, secondLeafHashMap, firstMap, secondMap);
-        this.insertNodes(secondLeafHashMap, firstLeafHashMap, secondMap, firstMap);
-
-        //console.log(Array.from(firstHashMap.keys()));
-        //console.log(Array.from(secondHashMap.keys()));
-
-        // add nodes from the other trees
-        //let mergedMaps = this.fillMapsWithNonExistingNodesFromOtherMap(firstMap, secondMap);
-        //let mergedFirstMap = mergedMaps.leftMap;
-        //let mergedSecondMap = mergedMaps.rightMap;
-
-    }
-
-    private insertNodes(firstLeafHashMap: Map<string, CodeMapNode>, secondLeafHashMap: Map<string, CodeMapNode>, firstMap: CodeMap, secondMap: CodeMap) {
+    private insertNodesIntoMapsAndHashmaps(firstLeafHashMap: Map<string, CodeMapNode>, secondLeafHashMap: Map<string, CodeMapNode>, firstMap: CodeMap, secondMap: CodeMap) {
         firstLeafHashMap.forEach((node, path) => {
             if (!secondLeafHashMap.has(path)) {
                 // insert node into secondHashMap and secondMap
-                //secondHashMap.set(path, node); //TODO brauchen wir das ? wenn ja dann neue leere node
                 let addedNode = this.deepcopy2(node);
+                secondLeafHashMap.set(path, addedNode);
                 this.insertNodeIntoMapByPath(addedNode, secondMap);
-                //secondLeafHashMap.get(insertPath).children.push(addedNode); // TODO das ist der triviale fall für blatt inserts und nicht folder
             }
         });
     }
@@ -113,142 +85,45 @@ export class DeltaCalculatorService {
 
     }
 
-    /**
-     * This requires the map to have paths. e.g.
-     *
-     * let dds = new DataDecoratorService();
-     * dds.decorateMapWithPathAttribute(a);
-     * dds.decorateMapWithPathAttribute(b);
-     *
-     * TODO ensure paths through typings
-     *
-     * @param {CodeMap} firstMap
-     * @param {CodeMap} secondMap
-     */
-    public decorateMapsWithDeltas(firstMap: CodeMap, secondMap: CodeMap) {
+    public fillMapsWithNonExistingNodesFromOtherMap(leftMap: CodeMap, rightMap: CodeMap) {
 
-        if (firstMap && secondMap && firstMap.root && secondMap.root) {
-            let firstRoot = d3.hierarchy<CodeMapNode>(firstMap.root);
-            let firstLeaves: HierarchyNode<CodeMapNode>[] = firstRoot.leaves();
-            let secondRoot = d3.hierarchy(secondMap.root);
-            let secondLeaves: HierarchyNode<CodeMapNode>[] = secondRoot.leaves();
+        //remove cross origin nodes from maps
+        this.removeCrossOriginNodes(leftMap);
+        this.removeCrossOriginNodes(rightMap);
 
-            for (var j = 0; j < firstLeaves.length; j++) {
-                for (var k = 0; k < secondLeaves.length; k++) {
+        //build hash maps for fast search indices
+        let firstLeafHashMap = new Map<string, CodeMapNode>();
+        d3.hierarchy(leftMap.root).leaves().forEach((node: HierarchyNode<CodeMapNode>) => {
+            firstLeafHashMap.set(node.data.path, node.data);
+        });
 
-                    let fl: HierarchyNode<CodeMapNode> = firstLeaves[j];
-                    let sl: HierarchyNode<CodeMapNode> = secondLeaves[k];
+        let secondLeafHashMap = new Map<string, CodeMapNode>();
+        d3.hierarchy(rightMap.root).leaves().forEach((node: HierarchyNode<CodeMapNode>) => {
+            secondLeafHashMap.set(node.data.path, node.data);
+        });
 
-                    if (fl.data.path === sl.data.path) {
-                        //calculate delta for those nodes attributes and push it to the second leave
-                        let firstDeltas = this.calculateAttributeListDelta(sl.data.attributes, fl.data.attributes);
-                        let secondDeltas = this.calculateAttributeListDelta(fl.data.attributes, sl.data.attributes);
+        //insert nodes from the other map
+        this.insertNodesIntoMapsAndHashmaps(firstLeafHashMap, secondLeafHashMap, leftMap, rightMap);
+        this.insertNodesIntoMapsAndHashmaps(secondLeafHashMap, firstLeafHashMap, rightMap, leftMap);
 
-                        firstLeaves[j].data.deltas = firstDeltas;
-                        secondLeaves[k].data.deltas = secondDeltas;
-
-                    }
-                }
-            }
-
-        }
+        //calculate deltas between leaves
+        firstLeafHashMap.forEach((node, path) => {
+            let otherNode = secondLeafHashMap.get(path);
+            otherNode.deltas = this.calculateAttributeListDelta(node.attributes, otherNode.attributes);
+            node.deltas = this.calculateAttributeListDelta(otherNode.attributes, node.attributes);
+        });
 
     }
 
-    /**
-     * Unifies both maps and inserts unique nodes into the other map. The inserted nodes have all metrics set to zero.
-     * This will calculate deltas correctly between versions and add "empty" buildings to ensure all revisions have an
-     * equal building pool. These empty buildings should be viewed with the unary metric as area
-     * @param {CodeMap} leftMap
-     * @param {CodeMap} rightMap
-     * @returns {{leftMap: CodeMap; rightMap: CodeMap}}
-     */
-    public fillMapsWithNonExistingNodesFromOtherMap(leftMap: CodeMap, rightMap: CodeMap): { leftMap: CodeMap; rightMap: CodeMap; } {
+    private removeCrossOriginNodes(map: CodeMap) {
 
-        if (leftMap && rightMap && leftMap.root && rightMap.root) {
-            //make hashmaps with paths as indices
-            let firstLeafHashMap = new Map<string, CodeMapNode>();
-            d3.hierarchy(leftMap.root).leaves().forEach((node: HierarchyNode<CodeMapNode>) => {
-                firstLeafHashMap.set(node.data.path, node.data);
-            });
-
-            let secondLeafHashMap = new Map<string, CodeMapNode>();
-            d3.hierarchy(rightMap.root).leaves().forEach((node: HierarchyNode<CodeMapNode>) => {
-                secondLeafHashMap.set(node.data.path, node.data);
-            });
-
-            // iterate over maps and insert nodes
-            this.insertNodes(firstLeafHashMap, secondLeafHashMap, leftMap, rightMap);
-            this.insertNodes(secondLeafHashMap, firstLeafHashMap, rightMap, leftMap);
-
-
-        }
-
-
-        return {leftMap: leftMap, rightMap: rightMap};
-
-    }
-
-    public removeCrossOriginNodes(map: CodeMap): CodeMap {
-        if (map && map.root) {
-
-            let mapCopy: CodeMap = deepcopy.default(map);
-
-            let mapRoot = d3.hierarchy<CodeMapNode>(mapCopy.root);
+            let mapRoot = d3.hierarchy<CodeMapNode>(map.root);
             mapRoot.each((node) => {
                 if (node.data.children) {
-                    node.data.children = node.data.children.filter(x => (x.origin === mapCopy.fileName));
+                    node.data.children = node.data.children.filter(x => (x.origin === map.fileName));
                 }
             });
 
-            return mapCopy;
-
-        } else {
-            return map;
-        }
-    }
-
-    private insertLeftIntoRightWithZeroMetrics(left: HierarchyNode<CodeMapNode>, right: HierarchyNode<CodeMapNode>) {
-
-        // are these nodes mergable (same name and have children)?
-        if (left.data.name === right.data.name && left.children != undefined && right.children != undefined && left.children.length > 0 && right.children.length > 0) {
-
-            left.children.forEach((leftChild) => {
-
-                let leftChildExistsInRight = this.childExistsInHierarchy(right, leftChild);
-
-                //if left child does not exist in the right node's children, insert it as a new valueless node into rights children
-                if (!leftChildExistsInRight) {
-                    let copy = this.deepcopy(leftChild);
-                    right.data.children.push(copy.data);
-                } else {
-
-                    // if left child exists in right nodes children, skip, since we only look for direct children
-                    right.children.forEach((rightChildInner) => {
-                        left.children.forEach((leftChildInner) => {
-                            //step in with pre-filtering
-                            if (leftChildInner.data.name !== leftChild.data.name) {
-                                if (leftChildInner.data.name === rightChildInner.data.name && leftChildInner.children != undefined && rightChildInner.children != undefined && leftChildInner.children.length > 0 && rightChildInner.children.length > 0) {
-                                    this.insertLeftIntoRightWithZeroMetrics(leftChildInner, rightChildInner);
-                                }
-                            }
-                        });
-                    });
-                }
-
-            });
-
-        }
-    }
-
-    private childExistsInHierarchy(hierarchy: HierarchyNode<CodeMapNode>, child: HierarchyNode<CodeMapNode>): boolean {
-        let tmp = false;
-        hierarchy.children.forEach((rightChild) => {
-            if (child.data.name === rightChild.data.name) {
-                tmp = true;
-            }
-        });
-        return tmp;
     }
 
     private deepcopy(nodes: HierarchyNode<CodeMapNode>): HierarchyNode<CodeMapNode> {
@@ -283,7 +158,7 @@ export class DeltaCalculatorService {
 
     }
 
-    calculateAttributeListDelta(first: KVObject, second: KVObject) {
+    private calculateAttributeListDelta(first: KVObject, second: KVObject) {
         let deltas = {};
         for (var key in second) {
             if (key) {
