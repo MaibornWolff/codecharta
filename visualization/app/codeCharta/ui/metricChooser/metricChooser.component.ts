5d24427eceff070832e492db20cd667400bf5fb7
mrpatpat <adrian.endrich@googlemail.com>
2018-04-13 14:36:27 +0200

    implementing #128

--- visualization/app/codeCharta/ui/metricChooser/metricChooser.component.ts
+++ visualization/app/codeCharta/ui/metricChooser/metricChooser.component.ts
@@ -1,19 +1,29 @@
 import {DataServiceSubscriber, DataService, DataModel} from "../../core/data/data.service";
 import {SettingsService} from "../../core/settings/settings.service";
-import {IAngularEvent} from "angular";
+import {IAngularEvent, IRootScopeService} from "angular";
 import "./metricChooser.component.scss";
+import {
+    CodeMapBuildingTransition, CodeMapMouseEventService,
+    CodeMapMouseEventServiceSubscriber
+} from "../codeMap/codeMap.mouseEvent.service";
 
-export class MetricChooserController implements DataServiceSubscriber{
+export class MetricChooserController implements DataServiceSubscriber, CodeMapMouseEventServiceSubscriber{
 
     public metrics: string[];
+
+    public hoveredAreaValue: number;
+    public hoveredHeightValue: number;
+    public hoveredColorValue: number;
 
     /* @ngInject */
     constructor(
         private dataService: DataService,
-        private settingsService: SettingsService
+        private settingsService: SettingsService,
+        private $rootScope: IRootScopeService
     ) {
         this.metrics = dataService.data.metrics.sort();
         this.dataService.subscribe(this);
+        CodeMapMouseEventService.subscribe($rootScope, this);
     }
 
     onDataChanged(data: DataModel, event: IAngularEvent) {
@@ -24,11 +34,32 @@ export class MetricChooserController implements DataServiceSubscriber{
         this.settingsService.applySettings();
     }
 
+    onBuildingHovered(data: CodeMapBuildingTransition, event: angular.IAngularEvent) {
+        if(data && data.to && data.to.node && data.to.node.attributes) {
+            this.hoveredAreaValue = data.to.node.attributes[this.settingsService.settings.areaMetric];
+            this.hoveredColorValue = data.to.node.attributes[this.settingsService.settings.colorMetric];
+            this.hoveredHeightValue = data.to.node.attributes[this.settingsService.settings.heightMetric];
+        } else {
+            this.hoveredAreaValue = null;
+            this.hoveredColorValue = null;
+            this.hoveredHeightValue = null;
+        }
+    }
+
+    onBuildingSelected(data: CodeMapBuildingTransition, event: angular.IAngularEvent) {
+    }
+
 }
 
 export const metricChooserComponent = {
     selector: "metricChooserComponent",
     template: require("./metricChooser.component.html"),
+    controller: MetricChooserController
+};
+
+export const horizontalMetricChooserComponent = {
+    selector: "horizontalMetricChooserComponent",
+    template: require("./metricChooser.horizontal.component.html"),
     controller: MetricChooserController
 };
 
