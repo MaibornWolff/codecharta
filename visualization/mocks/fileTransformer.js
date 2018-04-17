40e267f8184c797576df24ba1ddad97a8684820f
mrpatpat <adrian.endrich@googlemail.com>
2018-04-17 13:34:55 +0200

    rewriting some test stuff

---
+++ visualization/mocks/fileTransformer.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {
+    process(src, filename, config, options) {
+        const formatted = src.replace(/["]/g, "'").replace(/[\n]/g, "");
+        return "module.exports = \""+formatted+"\";";
+    },
+};
