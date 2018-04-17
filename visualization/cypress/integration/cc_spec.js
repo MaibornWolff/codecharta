24c86c743f7576bed9c052d298e5bc9158e5063d
mrpatpat <adrian.endrich@googlemail.com>
2018-04-17 14:20:44 +0200

    basic cypress/e2e setup

--- visualization/cypress/integration/cc_spec.js
+++
@@ -1,6 +0,0 @@
-describe('My First Test', function() {
-    it('finds the content "type"', function() {
-        cy.visit("dist/webpack/index.html")
-        cy.contains('type')
-    })
-})
--- visualization/cypress/support/index.js
+++ visualization/cypress/support/index.js
@@ -12,9 +12,8 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-
 // Import commands.js using ES2015 syntax:
-import './commands'
+require('./commands')
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
