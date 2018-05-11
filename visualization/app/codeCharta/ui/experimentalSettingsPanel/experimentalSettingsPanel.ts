b2fdcffb98903aa3e7608cec05c0b251eb4602f5
AdrianE <adrian.endrich@maibornwolff.de>
2018-05-11 14:16:17 +0200

    adding switcher to experimental features #141

--- visualization/app/codeCharta/ui/experimentalSettingsPanel/experimentalSettingsPanel.ts
+++ visualization/app/codeCharta/ui/experimentalSettingsPanel/experimentalSettingsPanel.ts
@@ -1,12 +1,13 @@
 import "../rangeSlider/rangeSlider";
 import "../statisticSettingsPanel/statisticSettingsPanel";
 import "../../core/core.module";
+import "../layoutSwitcher/layoutSwitcher";
 
 import angular from "angular";
 
 import {experimentalSettingsPanelComponent} from "./experimentalSettingsPanel.component";
 
-angular.module("app.codeCharta.ui.experimentalSettingsPanel", ["app.codeCharta.ui.rangeSlider", "app.codeCharta.ui.statisticSettingsPanel", "app.codeCharta.core"])
+angular.module("app.codeCharta.ui.experimentalSettingsPanel", ["app.codeCharta.ui.rangeSlider", "app.codeCharta.ui.statisticSettingsPanel", "app.codeCharta.core", "app.codeCharta.ui.layoutSwitcher"])
     .component(experimentalSettingsPanelComponent.selector, experimentalSettingsPanelComponent);
 
 
