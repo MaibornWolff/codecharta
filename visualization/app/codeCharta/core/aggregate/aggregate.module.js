2aefd1c9c842792e5af9583bfed814600bb0a799
FernandoES <fernan_1989@hotmail.com>
2018-05-29 18:35:31 +0200

    test correcture

--- visualization/app/codeCharta/core/aggregate/aggregate.module.js
+++ visualization/app/codeCharta/core/aggregate/aggregate.module.js
@@ -1,6 +1,6 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var angular_1 = require("angular");
-var aggregate_service_1 = require("./aggregate.service");
-require("../../ui/dialog/dialog");
-angular_1.default.module("app.codeCharta.core.aggregate", ["app.codeCharta.ui.dialog"]).service(aggregate_service_1.AggregateMapService.SELECTOR, aggregate_service_1.AggregateMapService);
+import angular from "angular";
+import { AggregateMapService } from "./aggregate.service";
+import "../../ui/dialog/dialog";
+angular.module("app.codeCharta.core.aggregate", ["app.codeCharta.ui.dialog"]).service(AggregateMapService.SELECTOR, AggregateMapService);
+//# sourceMappingURL=aggregate.module.js.map
