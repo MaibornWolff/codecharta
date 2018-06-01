4b8d377b5037ff806c2f09f0d228c5918d8082aa
FernandoES <fernan_1989@hotmail.com>
2018-06-01 14:58:53 +0200

    js files deletion

--- visualization/app/codeCharta/ui/aggregateSettingsPanel/aggregateSettingsPanel.js
+++
@@ -1,7 +0,0 @@
-import "../rangeSlider/rangeSlider";
-import "../../core/core.module";
-import angular from "angular";
-import { aggregateSettingsPanelComponent } from "./aggregateSettingsPanel.component";
-angular.module("app.codeCharta.ui.aggregateSettingsPanel", ["app.codeCharta.ui.rangeSlider", "app.codeCharta.core"])
-    .component(aggregateSettingsPanelComponent.selector, aggregateSettingsPanelComponent);
-//# sourceMappingURL=aggregateSettingsPanel.js.map
