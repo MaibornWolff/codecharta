c0af7fcbb224716fb83bd72eaae01f67af1e680f
FernandoES <fernan_1989@hotmail.com>
2018-06-01 14:54:04 +0200

    js files dele

--- visualization/app/codeCharta/core/aggregate/aggregate.module.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var angular_1 = require("angular");
-var aggregate_service_1 = require("./aggregate.service");
-require("../../ui/dialog/dialog");
-angular_1.default.module("app.codeCharta.core.aggregate", ["app.codeCharta.ui.dialog"]).service(aggregate_service_1.AggregateMapService.SELECTOR, aggregate_service_1.AggregateMapService);
