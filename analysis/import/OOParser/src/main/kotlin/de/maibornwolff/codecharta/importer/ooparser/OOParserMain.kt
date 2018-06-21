14ceffad1974ab69df1825c0a6449525864aed19
alschmut <aschmutz1@web.de>
2018-06-21 18:01:08 +0200

    Add OOParserMain

---
+++ analysis/import/OOParser/src/main/kotlin/de/maibornwolff/codecharta/importer/ooparser/OOParserMain.kt
@@ -0,0 +1,83 @@
+package de.maibornwolff.codecharta.importer.ooparser
+
+import de.maibornwolff.codecharta.importer.ooparser.antlr.java.JavaLexer
+import de.maibornwolff.codecharta.importer.ooparser.antlr.java.JavaParser
+import org.antlr.v4.runtime.CharStream
+import picocli.CommandLine
+import java.io.*
+import java.util.concurrent.Callable
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+
+@CommandLine.Command(name = "parse", description = ["generates cc.json from projectpath"], footer = ["Copyright(c) 2018, MaibornWolff GmbH"])
+class OOParserMain : Callable<Void> {
+
+    @CommandLine.Option(names = ["-h", "--help"], usageHelp = true, description = ["displays this help and exits"])
+    private var help = false
+
+    @CommandLine.Option(names = ["-p", "--projectName"], description = ["project name"])
+    private var projectName = "OOParserMain"
+
+    @CommandLine.Option(names = ["--pathSeparator"], description = ["path separator (default = '/')"])
+    private var pathSeparator = '/'
+
+    @CommandLine.Option(names = ["-o", "--outputFile"], description = ["output File (or empty for stdout)"])
+    private var outputFile: File? = null
+
+    @CommandLine.Parameters(arity = "1..*", paramLabel = "FILE", description = ["project file"])
+    private var files: List<File> = mutableListOf()
+
+    @Throws(IOException::class)
+    override fun call(): Void? {
+
+        /*val file =  files.get(0)
+        print(file)
+        readFile(file)*/
+
+        val charStream = CharStreams.fromString("Hallo John!")
+
+        val filePath = files.get(0).absolutePath
+        val fileCharStream: CharStream = CharStreams.fromFileName(filePath)
+        val lexer = JavaLexer(fileCharStream)
+        val tokens = CommonTokenStream(lexer)
+        val parser = JavaParser(tokens)
+
+        /*parse(parser.compilationUnit())*/
+        println("--------------------")
+        System.out.println(parser.compilationUnit().toStringTree())
+        println("--------------------")
+        System.out.println(parser.compilationUnit().start.getLine())
+
+
+        return null
+    }
+
+    /**
+     * Kotlin Example to read contents of file line by line
+     */
+    fun readFile(file: File) {
+
+        var i: Int = 1
+
+        file.readLines().forEach {
+            print((i++))
+            println(": "+it)
+        }
+    }
+
+    private fun writer(): Writer {
+        return if (outputFile == null) {
+            OutputStreamWriter(System.out)
+        } else {
+            BufferedWriter(FileWriter(outputFile))
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            CommandLine.call(OOParserMain(), System.out, *args)
+        }
+    }
+}
+
--- analysis/tools/ccsh/src/main/kotlin/de/maibornwolff/codecharta/tools/ccsh/Ccsh.kt
+++ analysis/tools/ccsh/src/main/kotlin/de/maibornwolff/codecharta/tools/ccsh/Ccsh.kt
@@ -37,6 +37,7 @@ import de.maibornwolff.codecharta.importer.scmlogparser.SCMLogParser
 import de.maibornwolff.codecharta.importer.sonar.SonarImporterMain
 import de.maibornwolff.codecharta.tools.validation.ValidationTool
 import de.maibornwolff.codecharta.importer.crococosmo.CrococosmoImporter
+import de.maibornwolff.codecharta.importer.ooparser.OOParserMain
 import de.maibornwolff.codecharta.importer.understand.UnderstandImporter
 import picocli.CommandLine
 import java.util.concurrent.Callable
@@ -55,7 +56,8 @@ import java.util.concurrent.Callable
             Installer::class,
             CSVExporter::class,
             CrococosmoImporter::class,
-            UnderstandImporter::class
+            UnderstandImporter::class,
+            OOParserMain::class
         ],
         versionProvider = Ccsh.ManifestVersionProvider::class,
         footer = ["Copyright(c) 2018, MaibornWolff GmbH"]
