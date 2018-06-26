0dc0ec26fd521367a7866af0d8db074e969061e9
alschmut <aschmutz1@web.de>
2018-06-26 11:21:35 +0200

    Add converted java files

--- analysis/import/OOParser/src/test/kotlin/de/maibornwolff/codecharta/importer/ooparser/RealLinesOfCodeParsingTest.kt
+++ analysis/import/OOParser/src/test/kotlin/de/maibornwolff/codecharta/importer/ooparser/RealLinesOfCodeParsingTest.kt
@@ -3,8 +3,9 @@ package de.maibornwolff.codecharta.importer.ooparser
 import org.junit.Test
 import java.io.IOException
 
-
+import de.maibornwolff.codecharta.importer.ooparser.OOParser
 import org.assertj.core.api.Assertions.assertThat
+import java.io.File
 
 class RealLinesOfCodeParsingTest {
 
@@ -12,11 +13,15 @@ class RealLinesOfCodeParsingTest {
     @Throws(IOException::class)
     fun parsesFourRealLines() {
 
-        val ooparser: OOParser = OOParser()
-        ooparser.getRealLinesOfCode()
+        val file = File(javaClass.classLoader.getResource("de/maibornwolff/codecharta/importer/ooparser/RealLinesShort.java")!!.file)
 
-        //assertThat(parser.getRealLinesOfCode()).isEqualTo(5)
+        val ooparser = OOParser(file)
+        ooparser.realLinesOfCode()
 
-        assertThat(true).isEqualTo(true)
+        print(ooparser.realLinesOfCode())
+
+        assertThat(ooparser.realLinesOfCode()).isEqualTo(5)
+
+        //assertThat(true).isEqualTo(true)
     }
 }
