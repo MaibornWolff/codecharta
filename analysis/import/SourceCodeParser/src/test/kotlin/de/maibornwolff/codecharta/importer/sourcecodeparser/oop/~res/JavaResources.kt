5f566d98d1bb6de498bd90a92f084bd0df0015dd
Richard Gross <Richard.Gross@maibornwolff.de>
2018-07-04 10:16:36 +0200

    adds some tests; moves test resources to correct folder #162

--- analysis/import/SourceCodeParser/src/test/kotlin/de/maibornwolff/codecharta/importer/sourcecodeparser/oop/~res/JavaResources.kt
+++ analysis/import/SourceCodeParser/src/test/kotlin/de/maibornwolff/codecharta/importer/sourcecodeparser/oop/~res/JavaResources.kt
@@ -1,5 +1,5 @@
 // NOTE: I like to put technical details at the bottom of packages, that's what the tilde is for
 package de.maibornwolff.codecharta.importer.sourcecodeparser.oop.`~res`
 
-val javaBaseFolder = "de/maibornwolff/codecharta/importer/sourcecodeparser/oop/java"
+val javaBaseFolder = "de/maibornwolff/codecharta/importer/sourcecodeparser/oop/core/extract/java"
 
