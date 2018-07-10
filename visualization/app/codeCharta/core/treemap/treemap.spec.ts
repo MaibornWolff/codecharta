d7a2cb2625048f360714f248f49dd9769165026a
mrpatpat <adrian.endrich@googlemail.com>
2018-07-10 17:25:38 +0200

    add tests

--- visualization/app/codeCharta/core/treemap/treemap.spec.ts
+++ visualization/app/codeCharta/core/treemap/treemap.spec.ts
@@ -1,4 +1,4 @@
-import {SquarifiedValuedCodeMapNode, TreeMapService, ValuedCodeMapNode} from "./treemap.service";
+import {SquarifiedValuedCodeMapNode, TreeMapService, TreeMapSettings, ValuedCodeMapNode} from "./treemap.service";
 import {CodeMapNode} from "../data/model/CodeMap";
 import {node} from "../../ui/codeMap/rendering/node";
 import {DataService} from "../data/data.service";
@@ -8,68 +8,78 @@ describe("tdd", ()=> {
     let treeMapService: TreeMapService;
     let root: CodeMapNode;
     let dataServiceMock: DataService;
+    let treemapSettings: TreeMapSettings;
 
     beforeEach(()=>{
+
         dataServiceMock = {
             getMaxMetricInAllRevisions: jest.fn()
         };
+
+        dataServiceMock.getMaxMetricInAllRevisions.mockReturnValue(1000);
+
         treeMapService = new TreeMapService(dataServiceMock);
-        root = new CodeMapNode("root");
+
+        root = { name: "root" };
         root.attributes = {
             "mcc": 1,
             "fun": 10,
             "lin": 100,
         };
         root.path = "/";
+
+        treemapSettings = {
+            size: 200,
+            areaKey: "mcc",
+            heightKey: "fun",
+            margin: 0,
+            invertHeight: false
+        };
+
     });
 
-    xit("only root node", ()=> {
-        dataServiceMock.getMaxMetricInAllRevisions.mockReturnValue(1000);
-        let node: node = treeMapService.createTreemapNodes(root, 100, 200, 0, "fun", "mcc");
+    it("only root node", ()=> {
+        let node: node = treeMapService.createTreemapNodes(root, treemapSettings);
+        expect(node).toMatchSnapshot();
     });
 
-    xit("root node with two direct children", ()=> {
+    it("root node with two direct children", ()=> {
 
-        dataServiceMock.getMaxMetricInAllRevisions.mockReturnValue(1000);
-
-        let firstChild = new CodeMapNode("firstChild");
+        let firstChild = { name: "firstChild" };
         firstChild.attributes = root.attributes;
 
-        let secondChild = new CodeMapNode("secondChild");
+        let secondChild = { name: "secondChild" };
         secondChild.attributes = root.attributes;
 
         root.children = [firstChild, secondChild];
 
-        let node: node = treeMapService.createTreemapNodes(root, 100, 200, 0, "fun", "mcc")
+        let node: node = treeMapService.createTreemapNodes(root, treemapSettings)
 
-        console.log(node);
+        expect(node).toMatchSnapshot();
 
     });
 
-    it("value+squarify root node with two direct children and some grand children", ()=> {
+    it("root node with two direct children and some grand children", ()=> {
 
-        dataServiceMock.getMaxMetricInAllRevisions.mockReturnValue(1000);
-
-        let firstChild = new CodeMapNode("firstChild");
+        let firstChild = { name: "firstChild" };
         firstChild.attributes = root.attributes;
 
-        let firstGrandChild = new CodeMapNode("firstGrandChild");
+        let firstGrandChild = { name: "firstGrandChild" };
         firstGrandChild.attributes = root.attributes;
 
-        let secondGrandChild = new CodeMapNode("secondGrandChild");
+        let secondGrandChild = { name: "secondGrandChild" };
         secondGrandChild.attributes = root.attributes;
 
         firstChild.children = [firstGrandChild, secondGrandChild];
 
-        let secondChild = new CodeMapNode("secondChild");
+        let secondChild = { name: "secondChild" };
         secondChild.attributes = root.attributes;
 
         root.children = [firstChild, secondChild];
 
-        let valued: ValuedCodeMapNode = treeMapService.valueCodeMapNodes(root, "fun");
-        let squarified: SquarifiedValuedCodeMapNode = treeMapService.squarify(valued, 6, 6);
+        let node: node = treeMapService.createTreemapNodes(root, treemapSettings);
 
-        console.log(squarified);
+        expect(node).toMatchSnapshot();
 
     });
 
@@ -78,51 +88,40 @@ describe("tdd", ()=> {
         it("attribute exists, no children", ()=> {
             let root = {name: "root"};
             root.attributes = {a:100};
-            expect(root.calculateValue("a")).toBe(100);
+            let node: node = treeMapService.createTreemapNodes(root, treemapSettings);
+            expect(node.attributes["a"]).toBe(100);
         });
 
         it("attribute do not exists, no children", ()=> {
             let root = {name: "root"};
-            root.attributes = {a:100};
-            expect(root.calculateValue("b")).toBe(0);
+            root.attributes = {};
+            let node: node = treeMapService.createTreemapNodes(root, treemapSettings);
+            expect(node.attributes["b"]).toBe(undefined);
         });
 
         it("attribute do not exists, multiple children with non existant attributes", ()=> {
             let root = {name: "root"};
+            root.attributes = {};
             let firstChild = {name: "firstChild"};
+            firstChild.attributes = {};
             let secondChild = {name: "secondChild"};
+            secondChild.attributes = {};
             root.children = [firstChild, secondChild];
-            expect(root.calculateValue("a")).toBe(0);
+            treemapSettings.heightKey = "b";
+            treemapSettings.areaKey = "b";
+            let node: node = treeMapService.createTreemapNodes(root, treemapSettings);
+            expect(node.attributes["b"]).toBe(undefined);
         });
 
-        it("attribute do not exists, multiple children with existant attributes", ()=> {
+        it("attribute do not exists, multiple children with existant attributes. should be undefined since it is not the job of the treemap service to add non existant attributes", ()=> {
             let root = {name: "root"};
+            root.attributes = {};
             let firstChild = {name: "firstChild"};
             firstChild.attributes = {a:100};
             let secondChild = {name: "secondChild"};
             secondChild.attributes = {a:200};
             root.children = [firstChild, secondChild];
-            expect(root.calculateValue("a")).toBe(300);
-        });
-
-        it("attribute do not exists, multiple children with some existant attributes", ()=> {
-            let root = {name: "root"};
-            let firstChild = {name: "firstChild"};
-            firstChild.attributes = {a:100};
-            let secondChild = {name: "secondChild"};
-            root.children = [firstChild, secondChild];
-            expect(root.calculateValue("a")).toBe(100);
-        });
-
-        it("attribute exists, multiple children with existant attributes", ()=> {
-            let root = {name: "root"};
-            root.attributes = {a:99};
-            let firstChild = {name: "firstChild"};
-            firstChild.attributes = {a:100};
-            let secondChild = {name: "secondChild"};
-            secondChild.attributes = {a:200};
-            root.children = [firstChild, secondChild];
-            expect(root.calculateValue("a")).toBe(300);
+            expect(root.attributes["a"]).toBe(undefined);
         });
 
     });
