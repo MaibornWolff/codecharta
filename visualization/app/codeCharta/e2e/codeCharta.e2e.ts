344036aaa7df4ebd684df48960f57a0224579c9c
mrpatpat <adrian.endrich@googlemail.com>
2018-07-14 13:44:47 +0200

    force mkdirp update to prevent octal literal error. fix linting errors

--- visualization/app/codeCharta/e2e/codeCharta.e2e.ts
+++ visualization/app/codeCharta/e2e/codeCharta.e2e.ts
@@ -7,16 +7,16 @@ export class LogoPageObject {
     constructor(private page) {}
 
     async getVersion() {
-        const versionString = await this.page.evaluate(() => document.querySelector('#title > div > h2 > span').innerText);
+        const versionString = await this.page.evaluate(() => document.querySelector('#title > div > h2 > span')["innerText"]);
         return versionString.split(" ")[1];
     }
 
     async getLink() {
-        return await this.page.evaluate(() => document.querySelector('#title > div > a').href);
+        return await this.page.evaluate(() => document.querySelector('#title > div > a')["href"]);
     }
 
     async getImageSrc() {
-        return await this.page.evaluate(() => document.querySelector('#title > div > a > img').src);
+        return await this.page.evaluate(() => document.querySelector('#title > div > a > img')["src"]);
     }
 
 }
