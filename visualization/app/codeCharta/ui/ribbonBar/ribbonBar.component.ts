a87f18979e0c3ad92eaba6ba0f57e387537cd543
Adrian Endrich <adrian.endrich@maibornwolff.de>
2018-08-15 16:40:06 +0200

    add button to toggle

--- visualization/app/codeCharta/ui/ribbonBar/ribbonBar.component.ts
+++ visualization/app/codeCharta/ui/ribbonBar/ribbonBar.component.ts
@@ -3,21 +3,20 @@ import $ from "jquery";
 
 export class RibbonBarController {
 
-    private heightExpanded = RibbonBarController.getExpandedHeight();
-    private heightCollapsed = $("ribbon-bar-component #header").height();
     private collapsingElements = $("ribbon-bar-component #header, ribbon-bar-component .section-body");
+    private toggleElement = $("ribbon-bar-component #toggle-button");
 
     private isExpanded: boolean = false;
 
     /* @ngInject */
     constructor() {
-        $(document).on("mousemove", this.onMouseMove.bind(this));
+        this.toggleElement.on("click", this.toggle.bind(this));
     }
 
-    private onMouseMove(e) {
-        if (e.pageY <= this.heightCollapsed && !this.isExpanded) {
+    private toggle(e) {
+        if (!this.isExpanded) {
             this.expand();
-        } else if (e.pageY > this.heightExpanded && this.isExpanded) {
+        } else {
             this.collapse();
         }
     }
@@ -30,16 +29,6 @@ export class RibbonBarController {
     public collapse() {
         this.isExpanded = false;
         this.collapsingElements.removeClass("expanded");
-    }
-
-    private static getExpandedHeight() {
-        const $inspector = $("<ribbon-bar-component><div id='header' class='expanded'></div></ribbon-bar-component>").css('display', 'none');
-        $("body").append($inspector);
-        try {
-            return $inspector.find(".expanded").css("height").replace("px","");
-        } finally {
-            $inspector.remove();
-        }
     }
 
 }
