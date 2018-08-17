489fe863b0a1cd7807d16b750e4d7a77a54e0dfc
Adrian Endrich <adrian.endrich@maibornwolff.de>
2018-08-17 16:54:25 +0200

    fix e2e test

--- visualization/app/codeCharta/e2e/logo.po.ts
+++ visualization/app/codeCharta/e2e/logo.po.ts
@@ -3,16 +3,16 @@ export class LogoPageObject {
     constructor(private page) {}
 
     async getVersion() {
-        const versionString = await this.page.evaluate(() => document.querySelector('#title > div > h2 > span')["innerText"]);
+        const versionString = await this.page.evaluate(() => document.querySelector('#mw-logo > div > h2 > span')["innerText"]);
         return versionString.split(" ")[1];
     }
 
     async getLink() {
-        return await this.page.evaluate(() => document.querySelector('#title > div > a')["href"]);
+        return await this.page.evaluate(() => document.querySelector('#mw-logo > div > a')["href"]);
     }
 
     async getImageSrc() {
-        return await this.page.evaluate(() => document.querySelector('#title > div > a > img')["src"]);
+        return await this.page.evaluate(() => document.querySelector('#mw-logo > div > a > img')["src"]);
     }
 
 }
