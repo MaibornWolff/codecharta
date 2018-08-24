b3832daddcf100e9e2e88f2af9f3fed606fb587a
FelixH <xherbst@gmail.com>
2018-08-24 12:49:26 +0200

    fix scenario test

--- visualization/app/codeCharta/core/scenario/scenario.spec.ts
+++ visualization/app/codeCharta/core/scenario/scenario.spec.ts
@@ -45,7 +45,7 @@ describe("app.codeCharta.core.scenarioService", function () {
                         "y": 1,
                         "z": 1},
                 "showDependencies": false,
-                "showRibbonBar": false,
+                "showRibbonBar": true,
                 "isWhiteBackground": false
             },
 
