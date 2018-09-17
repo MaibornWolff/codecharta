29f1cc979a6798f4523d5ba017b60ee84a3f6b0b
alschmut <aschmutz1@web.de>
2018-09-17 15:57:49 +0100

    Add Button in Sidebar to download current JSON file

--- visualization/app/codeCharta/ui/sidenav/sidenav.component.ts
+++ visualization/app/codeCharta/ui/sidenav/sidenav.component.ts
@@ -1,12 +1,15 @@
 "use strict";
 import "./sidenav.component.scss";
 import { DialogService } from "../dialog/dialog.service";
+import {SettingsService} from "../../core/settings/settings.service";
+import any = jasmine.any;
 
 export class SidenavController {
 
     /* @ngInject */
     constructor(
         private dialogService: DialogService,
+        private settingsService: SettingsService,
         private $mdSidenav: any
     ) {
     }
@@ -19,6 +22,41 @@ export class SidenavController {
         this.$mdSidenav(navID).toggle();
     }
 
+    downloadCurrentJson() {
+
+        const filename: string = this.settingsService.settings.map.fileName;
+        var map: any = this.settingsService.settings.map;
+        var userSettings = this.settingsService.settings;
+        delete userSettings.map;
+
+        let data = {
+            fileName: map.fileName,
+            projectName: map.projectName,
+            apiVersion: map.apiVersion,
+            nodes: [map.root],
+            edges: map.edges,
+            attributeTypes: map.attributeTypes,
+            userSettings: userSettings,
+            blacklist: map.blacklist || [],
+        };
+        this.downloadData(data, filename);
+    }
+
+    downloadData(data, filename) {
+        if (typeof data === "object") {
+            data = JSON.stringify(data, undefined, 4);
+        }
+
+        const blob = new Blob([data], {type: "text/json"});
+        const e = document.createEvent("MouseEvents");
+        const a = document.createElement("a");
+
+        a.download = filename;
+        a.href = window.URL.createObjectURL(blob);
+        a.dataset.downloadurl = ["text/json", a.download, a.href].join(":");
+        e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        a.dispatchEvent(e);
+    }
 }
 
 export const sidenavComponent = {
