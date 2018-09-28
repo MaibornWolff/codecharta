bbf663f608f0e95ba00412af2b1a70c656480530
alschmut <aschmutz1@web.de>
2018-09-28 14:12:23 +0100

    Move save-file button into ribbonBar

--- visualization/app/codeCharta/ui/ribbonBar/ribbonBar.component.ts
+++ visualization/app/codeCharta/ui/ribbonBar/ribbonBar.component.ts
@@ -37,6 +37,42 @@ export class RibbonBarController {
         this.collapsingElements.removeClass("expanded");
     }
 
+    downloadCurrentJson() {
+        const filename: string = this.settingsService.settings.map.fileName;
+        var map: any = this.settingsService.settings.map;
+        var blacklist = this.settingsService.settings.blacklist;
+        var userSettings = this.settingsService.settings;
+        delete userSettings.map;
+        delete userSettings.blacklist;
+
+        let data = {
+            fileName: map.fileName,
+            projectName: map.projectName,
+            apiVersion: map.apiVersion,
+            nodes: [map.root],
+            edges: map.edges,
+            attributeTypes: map.attributeTypes,
+            userSettings: userSettings,
+            blacklist: blacklist,
+        };
+        this.downloadData(data, filename);
+    }
+
+    downloadData(data, filename) {
+        if (typeof data === "object") {
+            data = JSON.stringify(data, undefined, 4);
+        }
+
+        const blob = new Blob([data], {type: "text/json"});
+        const e = document.createEvent("MouseEvents");
+        const a = document.createElement("a");
+
+        a.download = filename;
+        a.href = window.URL.createObjectURL(blob);
+        a.dataset.downloadurl = ["text/json", a.download, a.href].join(":");
+        e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        a.dispatchEvent(e);
+    }
 }
 
 export const ribbonBarComponent = {
