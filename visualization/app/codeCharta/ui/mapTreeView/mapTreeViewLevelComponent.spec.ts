738f7a14ff3b690d48372308468772cfe2d880f8
FernandoES <fernan_1989@hotmail.com>
2018-11-30 17:25:43 +0100

    test #285

--- visualization/app/codeCharta/ui/mapTreeView/mapTreeViewLevelComponent.spec.ts
+++ visualization/app/codeCharta/ui/mapTreeView/mapTreeViewLevelComponent.spec.ts
@@ -1,16 +1,18 @@
-import {MapTreeViewLevelController} from "./MapTreeViewLevelComponent";
+import {MapTreeViewHoverEventSubscriber, MapTreeViewLevelController} from "./MapTreeViewLevelComponent";
 import {CodeMapActionsService} from "../codeMap/codeMap.actions.service";
 import {SettingsService} from "../../core/settings/settings.service";
 import {CodeMapUtilService} from "../codeMap/codeMap.util.service";
-import {CodeMapNode} from "../../core/data/model/CodeMap";
-import {CodeMapMouseEventService} from "../codeMap/codeMap.mouseEvent.service";
-//jest.mock("../codeMap/codeMap.mouseEvent.service");
+import {CodeMapNode, ExcludeType} from "../../core/data/model/CodeMap";
 
 describe("MapTreeViewLevelController", () => {
 
     let mapTreeViewLevelController: MapTreeViewLevelController;
     let $rootScope;
     let node;
+    let $event;
+    let threeOrbitControlsService;
+    let $timeout;
+
 
     let codeMapActionsService: CodeMapActionsService;
     let settingsServiceMock: SettingsService;
@@ -21,15 +23,25 @@ describe("MapTreeViewLevelController", () => {
 
         $rootScope = jest.fn();
 
+        $timeout = jest.fn();
+
         $rootScope = {
-            $broadcast: jest.fn()}
+            $broadcast: jest.fn(),
+            $on: jest.fn()
+        };
+
+        $event = {
+            clientX: jest.fn(),
+            clientY: jest.fn()
+        };
 
         const SettingsServiceMock = jest.fn<SettingsService>(() => ({
             subscribe: jest.fn(),
             applySettings: jest.fn(),
             settings: {
                 map: {
-                    root: null
+                    root: null,
+                    blacklist: {},
                 }
             }
         }));
@@ -50,12 +62,6 @@ describe("MapTreeViewLevelController", () => {
                     attributes: {},
                     children: [
                         {
-                            name: "aa",
-                            type: "File",
-                            path: "/root/a/aa",
-                            attributes: {},
-                        },
-                        {
                             name: "ab",
                             type: "Folder",
                             path: "/root/a/ab",
@@ -70,12 +76,6 @@ describe("MapTreeViewLevelController", () => {
                             ]
                         },
                     ]
-                },
-                {
-                    name: "b",
-                    type: "File",
-                    path: "/root/b",
-                    attributes: {},
                 }
             ]
         };
@@ -84,6 +84,9 @@ describe("MapTreeViewLevelController", () => {
         codeMapUtilService = new CodeMapUtilService(settingsServiceMock);
 
         settingsServiceMock.settings.map.root = simpleHierarchy;
+
+
+        codeMapActionsService = new CodeMapActionsService(settingsServiceMock,threeOrbitControlsService,$timeout);
 
         mapTreeViewLevelController = new MapTreeViewLevelController($rootScope,codeMapActionsService , settingsServiceMock, codeMapUtilService);
     }
@@ -99,9 +102,18 @@ describe("MapTreeViewLevelController", () => {
         });
 
         it("Return the color defined in the folder ", () => {
+
             mapTreeViewLevelController.node = codeMapUtilService.getCodeMapNodeFromPath("/root/a/ab", "Folder");
             mapTreeViewLevelController.node.markingColor = "0xff3210";
+
             expect(mapTreeViewLevelController.getFolderColor()).toBe("#ff3210");
+        });
+
+        it("Return black if no markingColor in node ", () => {
+
+            mapTreeViewLevelController.node = codeMapUtilService.getCodeMapNodeFromPath("/root/a/ab", "Folder");
+
+            expect(mapTreeViewLevelController.getFolderColor()).toBe("#000");
         });
 
     });
@@ -118,13 +130,122 @@ describe("MapTreeViewLevelController", () => {
     });
 
 
-    describe("", () => {
-        it("", () => {
+    describe("Clicks behaviour", () => {
+        it("Right click", () => {
+
+
+            mapTreeViewLevelController.node = codeMapUtilService.getCodeMapNodeFromPath("/root/a/ab", "Folder");
+            let context = {"path": mapTreeViewLevelController.node.path,"type": mapTreeViewLevelController.node.type,"x": $event.clientX,"y": $event.clientY};
+            mapTreeViewLevelController.onRightClick($event);
+
+            expect($rootScope.$broadcast).toBeCalledWith("hide-node-context-menu");
+            expect($rootScope.$broadcast).toBeCalledWith("show-node-context-menu",context);
 
         });
-        it("", () => {
+        it("Folder click collapse", () => {
+            mapTreeViewLevelController.collapsed = true;
+            mapTreeViewLevelController.onFolderClick();
+            expect(mapTreeViewLevelController.collapsed).toBeFalsy();
+        });
+
+
+        it("Folder click uncollapse", () => {
+            mapTreeViewLevelController.collapsed = false;
+
+            mapTreeViewLevelController.onFolderClick();
+
+            expect(mapTreeViewLevelController.collapsed).toBeTruthy();
+        });
+
+        it("Label click", () => {
+
+            mapTreeViewLevelController.node = codeMapUtilService.getCodeMapNodeFromPath("/root/a/", "Folder");
+            mapTreeViewLevelController.codeMapActionsService.isolateNode = jest.fn();
+            mapTreeViewLevelController.onLabelClick();
+
+            expect(mapTreeViewLevelController.codeMapActionsService.isolateNode).toHaveBeenCalledWith(mapTreeViewLevelController.node);
 
         });
+
+        it("Eye click", () => {
+
+            mapTreeViewLevelController.node = codeMapUtilService.getCodeMapNodeFromPath("/root/a/", "Folder");
+            mapTreeViewLevelController.codeMapActionsService.toggleNodeVisibility = jest.fn();
+            mapTreeViewLevelController.onEyeClick();
+
+            expect(mapTreeViewLevelController.codeMapActionsService.toggleNodeVisibility).toHaveBeenCalledWith(mapTreeViewLevelController.node);
+        });
+
+
+        it("Is leaf", () => {
+
+            mapTreeViewLevelController.node = codeMapUtilService.getCodeMapNodeFromPath("/root/a/ab/aba", "Folder");
+
+            expect(mapTreeViewLevelController.isLeaf()).toBeTruthy();
+        });
+
+        it("Is not leaf", () => {
+
+            mapTreeViewLevelController.node = codeMapUtilService.getCodeMapNodeFromPath("/root/a/ab", "Folder");
+
+            expect(mapTreeViewLevelController.isLeaf(mapTreeViewLevelController.node)).toBeFalsy();
+        });
+
+
+        it("Is blacklisted", () => {
+
+            mapTreeViewLevelController.node = codeMapUtilService.getCodeMapNodeFromPath("/root/a/ab", "Folder");
+
+            CodeMapUtilService.isBlacklisted = jest.fn();
+            mapTreeViewLevelController.isBlacklisted(mapTreeViewLevelController.node);
+
+            expect(CodeMapUtilService.isBlacklisted).
+            toHaveBeenCalledWith(mapTreeViewLevelController.node, settingsServiceMock.settings.blacklist, ExcludeType.exclude);
+        });
+
+        it("Not blacklisted, not exist", () => {
+
+            CodeMapUtilService.isBlacklisted = jest.fn();
+            let blacklisted = mapTreeViewLevelController.isBlacklisted(mapTreeViewLevelController.node);
+
+            expect(blacklisted).toBeFalsy();
+        });
+
+
+
+
+        it("Sort leaf", () => {
+
+            mapTreeViewLevelController.node = codeMapUtilService.getCodeMapNodeFromPath("/root/a/ab/aba", "Folder");
+
+            let sortValue = mapTreeViewLevelController.sortByFolder(mapTreeViewLevelController.node);
+
+            expect(sortValue).toBe(0);
+        });
+
+        it("Sort not a leaf", () => {
+
+            mapTreeViewLevelController.node = codeMapUtilService.getCodeMapNodeFromPath("/root/a/ab", "Folder");
+
+            let sortValue = mapTreeViewLevelController.sortByFolder(mapTreeViewLevelController.node);
+
+            expect(sortValue).toBe(1);
+        });
+
+        it("Subscribe Hover", () => {
+
+            let subscriber: MapTreeViewHoverEventSubscriber;
+
+            MapTreeViewLevelController.subscribeToHoverEvents($rootScope, subscriber);
+
+            let hoverFunction = function(event, args){subscriber.onShouldHoverNode(args)};
+            let unhoverFunction = function(event, args){subscriber.onShouldUnhoverNode(args)};
+
+            expect($rootScope.$on).toBeCalledWith("should-hover-node", expect.any(Function));
+            expect($rootScope.$on).toBeCalledWith("should-unhover-node", expect.any(Function));
+
+        });
+
     });
 
 
