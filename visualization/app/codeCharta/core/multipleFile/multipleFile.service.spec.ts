12440183bac7761faf129d6a0588de346aadf2b9
alschmut <aschmutz1@web.de>
2019-01-20 18:53:11 +0000

    Remove unnecessary comments #313

--- visualization/app/codeCharta/core/multipleFile/multipleFile.service.spec.ts
+++ visualization/app/codeCharta/core/multipleFile/multipleFile.service.spec.ts
@@ -2,10 +2,6 @@ import "./multipleFile.module";
 import {NGMock} from "../../../../mocks/ng.mockhelper";
 import {AttributeType, CodeMap} from "../data/model/CodeMap";
 
-
-/**
- * @test {Multiple}
- */
 describe("app.codeCharta.core.multiple", function() {
 
     const file1: CodeMap = {
@@ -255,34 +251,22 @@ describe("app.codeCharta.core.multiple", function() {
 
     describe("multipleService", function() {
 
-        /**
-         * @test {multipleFileService}
-         */
         it("instance", NGMock.mock.inject(function (multipleFileService) {
             expect(multipleFileService).not.toBe(undefined);
         }));
 
-        /**
-         * @test {multipleFileService}
-         */
         it("aggregation of two maps", NGMock.mock.inject(function (multipleFileService) {
             let multiple: CodeMap;
             multiple = multipleFileService.aggregateMaps([file1, file2]);
             expect(multiple).toEqual(file_aggregation_two);
         }));
 
-        /**
-         * @test {multipleFileService}
-         */
         it("aggregation of four maps", NGMock.mock.inject(function (multipleFileService) {
             let multiple: CodeMap;
             multiple = multipleFileService.aggregateMaps([file1, file2, file1, file2]);
             expect(multiple).toEqual(file_aggregation_four);
         }));
 
-        /**
-         * @test {multipleFileService}
-         */
         it("aggregation one map", NGMock.mock.inject(function (multipleFileService) {
             let multiple: CodeMap;
             multiple = multipleFileService.aggregateMaps([file1]);
@@ -333,30 +317,21 @@ describe("app.codeCharta.core.multiple", function() {
             }
         ];
 
-        /**
-         * @test {multipleFileService}
-         */
-        it ( "aggregation of two maps with empty edges should result in empty edges",NGMock.mock.inject(function(multipleFileService){
+        it("aggregation of two maps with empty edges should result in empty edges",NGMock.mock.inject(function(multipleFileService){
             file1.edges = [];
             file2.edges = [];
             let multiple: CodeMap = multipleFileService.aggregateMaps([file1,file2]);
             expect(multiple.edges).toEqual([]);
         }));
 
-        /**
-         * @test {multipleFileService}
-         */
-        it ( "aggregation of two maps with given edges should result in summarized edges",NGMock.mock.inject(function(multipleFileService){
+        it("aggregation of two maps with given edges should result in summarized edges",NGMock.mock.inject(function(multipleFileService){
             file1.edges = edges1;
             file2.edges = edges2;
             let multiple = multipleFileService.aggregateMaps([file1,file2]);
             expect(multiple.edges).toEqual(aggEdges);
         }));
 
-        /**
-         * @test {multipleFileService}
-         */
-        it ( "aggregation of one map with edges and other without should result in merged edges",NGMock.mock.inject(function(multipleFileService){
+        it("aggregation of one map with edges and other without should result in merged edges",NGMock.mock.inject(function(multipleFileService){
             file1.edges = [edges1[0], edges2[0]];
             file2.edges = null;
             const expectedEdges = aggEdges;
@@ -414,20 +389,14 @@ describe("app.codeCharta.core.multiple", function() {
             }
         };
 
-        /**
-         * @test {multipleFileService}
-         */
-        it ( "aggregation of two maps with different key attributes should result in all keys aggregated map",NGMock.mock.inject(function(multipleFileService){
+        it("aggregation of two maps with different key attributes should result in all keys aggregated map",NGMock.mock.inject(function(multipleFileService){
             file1.attributeTypes = attribute1;
             file2.attributeTypes = attribute2;
             let multiple = multipleFileService.aggregateMaps([file1,file2]);
             expect(multiple.attributeTypes).toEqual(aggAttributes);
         }));
 
-        /**
-         * @test {multipleFileService}
-         */
-        it ( "aggregation of one map with attributes and other without should result in merged with same attributes as the one with them",NGMock.mock.inject(function(multipleFileService){
+        it("aggregation of one map with attributes and other without should result in merged with same attributes as the one with them",NGMock.mock.inject(function(multipleFileService){
             file1.attributeTypes = attribute1;
             file2.attributeTypes = {};
             let multiple = multipleFileService.aggregateMaps([file1,file2]);
