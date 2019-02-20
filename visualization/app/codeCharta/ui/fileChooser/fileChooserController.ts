e9002e8ef4f9e9b950ce11f952d366a35582d354
alschmut <aschmutz1@web.de>
2019-02-20 14:26:29 +0000

    Set empty markedPackages onNewFileImported #337

--- visualization/app/codeCharta/ui/fileChooser/fileChooserController.ts
+++ visualization/app/codeCharta/ui/fileChooser/fileChooserController.ts
@@ -90,7 +90,8 @@ class FileChooserController {
                 ctx.scenarioService.applyScenarioOnce(this.scenarioService.getDefaultScenario());
                 ctx.dataService.setComparisonMap(revision);
                 ctx.dataService.setReferenceMap(revision);
-                ctx.settingsService.applySettings();
+                ctx.settingsService.settings.markedPackages = [];
+                ctx.settingsService.applySettings(ctx.settingsService.settings);
 
                 if(!ctx.$scope.$$phase || !ctx.$scope.$root.$$phase) {
                     ctx.$scope.$digest();
