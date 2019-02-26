bc856f50d0515242896b648c7c3c63d8030a6a0d
alexhunziker <alexhunziker91@gmail.com>
2019-02-26 16:26:55 +0100

    Added test cases for multipleFilePanel #390

--- visualization/app/codeCharta/ui/multipleFilePanel/multipleFilePanelComponent.ts
+++ visualization/app/codeCharta/ui/multipleFilePanel/multipleFilePanelComponent.ts
@@ -1,75 +1,70 @@
-import {Settings, SettingsService, SettingsServiceSubscriber} from "../../core/settings/settings.service";
-import "./multipleFilePanel.component.scss";
-import {DataModel, DataService, DataServiceSubscriber} from "../../core/data/data.service";
-import {CodeMap} from "../../core/data/model/CodeMap";
-import {MultipleFileService} from "../../core/multipleFile/multipleFile.service";
+import { Settings, SettingsService, SettingsServiceSubscriber } from "../../core/settings/settings.service"
+import "./multipleFilePanel.component.scss"
+import { DataModel, DataService, DataServiceSubscriber } from "../../core/data/data.service"
+import { CodeMap } from "../../core/data/model/CodeMap"
+import { MultipleFileService } from "../../core/multipleFile/multipleFile.service"
 
-export class MultipleFilePanelController implements DataServiceSubscriber, SettingsServiceSubscriber{
+export class MultipleFilePanelController implements DataServiceSubscriber, SettingsServiceSubscriber {
+	public settings: Settings
+	public data: DataModel
+	public revisions: CodeMap[]
+	public selectedMapIndices: number[]
+	public mapsToAggregate: CodeMap[]
 
-    public settings: Settings;
-    public data: DataModel;
-    public revisions: CodeMap[];
-    public selectedMapIndices : number[];
-    public mapsToAggregate : CodeMap[];
+	/* @ngInject */
+	constructor(
+		private settingsService: SettingsService,
+		private dataService: DataService,
+		private multipleFileService: MultipleFileService
+	) {
+		this.revisions = dataService.data.revisions
+		this.settings = settingsService.settings
+		this.data = dataService.data
+		this.dataService.subscribe(this)
+		this.settingsService.subscribe(this)
+		this.mapsToAggregate = [] as CodeMap[]
+		this.updateSelectedMapIndices()
+	}
 
-    /* @ngInject */
-    constructor(
-        private settingsService: SettingsService,
-        private dataService: DataService,
-        private multipleFileService: MultipleFileService,
-    ) {
-        this.revisions = dataService.data.revisions;
-        this.settings = settingsService.settings;
-        this.data = dataService.data;
-        this.dataService.subscribe(this);
-        this.settingsService.subscribe(this);
-        this.mapsToAggregate = [] as CodeMap[];
-        this.updateSelectedMapIndices();
-    }
+	public onMultipleChange() {
+		this.selectMapsToAggregate()
 
-    public onMultipleChange(){
+		let newMap = this.multipleFileService.aggregateMaps(JSON.parse(JSON.stringify(this.mapsToAggregate)))
 
-        this.selectMapsToAggregate();
+		this.settings.map = newMap
+		this.settings.blacklist = newMap.blacklist
 
-        let newMap = this.multipleFileService.aggregateMaps(JSON.parse(JSON.stringify(this.mapsToAggregate)));
+		this.settingsService.applySettings(this.settings)
+	}
 
-        this.settings.map = newMap;
-        this.settings.blacklist = newMap.blacklist;
+	public onDataChanged(data: DataModel) {
+		this.data = data
+		this.revisions = data.revisions
+		this.updateSelectedMapIndices()
+	}
 
-        this.settingsService.applySettings(this.settings);
-    }
+	public onSettingsChanged(settings: Settings, event: Event) {
+		this.settings = settings
+	}
 
-    public onDataChanged(data: DataModel) {
-        this.data = data;
-        this.revisions = data.revisions;
-        this.updateSelectedMapIndices();
-    }
+	private updateSelectedMapIndices() {
+		this.selectedMapIndices = []
+		for (let i = 0; i < this.revisions.length; i++) {
+			this.selectedMapIndices.push(i)
+		}
+	}
 
-    public onSettingsChanged(settings: Settings, event: Event) {
-        this.settings = settings;
-    }
-
-    public updateSelectedMapIndices(){
-        this.selectedMapIndices = [];
-        this.selectedMapIndices.push(this.revisions.length-1);
-        for(let i=0; i<this.revisions.length; i++){
-            this.selectedMapIndices.push(i)
-        }
-    }
-
-
-    private selectMapsToAggregate(){
-        this.mapsToAggregate = [] as CodeMap[];
-
-        for(let position of this.selectedMapIndices){
-            let currentCodeMap = this.revisions[position];
-            this.mapsToAggregate.push(currentCodeMap);
-        }
-    }
+	private selectMapsToAggregate() {
+		this.mapsToAggregate = [] as CodeMap[]
+		for (let position of this.selectedMapIndices) {
+			let currentCodeMap = this.revisions[position]
+			this.mapsToAggregate.push(currentCodeMap)
+		}
+	}
 }
 
 export const multipleFilePanelComponent = {
-    selector: "multipleFilePanelComponent",
-    template: require("./multipleFilePanel.component.html"),
-    controller: MultipleFilePanelController
-};
+	selector: "multipleFilePanelComponent",
+	template: require("./multipleFilePanel.component.html"),
+	controller: MultipleFilePanelController
+}
