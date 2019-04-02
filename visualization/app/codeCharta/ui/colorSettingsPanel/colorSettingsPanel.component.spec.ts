874d6cd1fb15ff6e29ea7fb496a8ea913b0944be
alexhunziker <alexhunziker91@gmail.com>
2019-04-02 17:12:40 +0200

    added colorSettingsPanel tests #178

--- visualization/app/codeCharta/ui/colorSettingsPanel/colorSettingsPanel.component.spec.ts
+++ visualization/app/codeCharta/ui/colorSettingsPanel/colorSettingsPanel.component.spec.ts
@@ -1,49 +1,145 @@
-import {ColorSettingsPanelController} from "./colorSettingsPanel.component";
-import {IRootScopeService} from "angular";
-import {SettingsService} from "../../state/settings.service";
-import {FileStateService} from "../../state/fileState.service";
-import {MetricService} from "../../state/metric.service";
-import {getService, instantiateModule} from "../../../../mocks/ng.mockhelper";
-import {TEST_FILE_DATA} from "../../util/dataMocks";
+import "./colorSettingsPanel.module"
+
+import { ColorSettingsPanelController } from "./colorSettingsPanel.component"
+import { IRootScopeService } from "angular"
+import { SettingsService } from "../../state/settings.service"
+import { FileStateService } from "../../state/fileState.service"
+import { MetricService } from "../../state/metric.service"
+import { getService, instantiateModule } from "../../../../mocks/ng.mockhelper"
+import { TEST_FILE_DATA, SETTINGS } from "../../util/dataMocks"
+import { Settings, FileSelectionState, FileState } from "../../codeCharta.model"
+import { services } from "@uirouter/core"
+import { settings } from "cluster"
 
 describe("ColorSettingsPanelController", () => {
+	let colorSettingsPanelController: ColorSettingsPanelController
+	let $rootScope: IRootScopeService
+	let settingsService: SettingsService
+	let fileStateService: FileStateService
+	let metricService: MetricService
 
-    let colorSettingsPanelController: ColorSettingsPanelController
-    let $rootScope: IRootScopeService
-    let settingsService: SettingsService
-    let fileStateService: FileStateService
-    let metricService: MetricService
+	beforeEach(() => {
+		restartSystem()
+		rebuildController()
+	})
 
-    beforeEach(() => {
-        restartSystem()
-        rebuildController()
-    })
+	function restartSystem() {
+		instantiateModule("app.codeCharta.ui.colorSettingsPanel")
 
-    function restartSystem() {
-        instantiateModule("app.codeCharta.ui.colorSettingsPanel")
+		$rootScope = getService<IRootScopeService>("$rootScope")
+		settingsService = getService<SettingsService>("settingsService")
+		fileStateService = getService<FileStateService>("fileStateService")
+		metricService = getService<MetricService>("metricService")
+	}
 
-        $rootScope = getService<IRootScopeService>("$rootScope")
-        settingsService = getService<SettingsService>("settingsService")
-        fileStateService = getService<FileStateService>("fileStateService")
-        metricService = getService<MetricService>("metricService")
-    }
+	function withMockedSettingsService() {
+		settingsService = colorSettingsPanelController["settingsService"] = jest.fn(() => {
+			return {
+				updateSettings: jest.fn(),
+				getSettings: jest.fn().mockReturnValue(SETTINGS)
+			}
+		})()
+	}
 
-    function rebuildController() {
-        colorSettingsPanelController = new ColorSettingsPanelController(
-            $rootScope,
-            settingsService,
-            fileStateService,
-            metricService
-        );
-    }
+	function rebuildController() {
+		colorSettingsPanelController = new ColorSettingsPanelController($rootScope, settingsService, fileStateService, metricService)
+	}
 
-    it("set adapted ColorRange in thirds for given metricValues", () => {
-        settingsService.updateSettings({dynamicSettings: { colorMetric: "rloc"}})
-        fileStateService.addFile(TEST_FILE_DATA)
-        fileStateService.setSingle(TEST_FILE_DATA)
-        colorSettingsPanelController.adaptColorRange(settingsService.getSettings())
+	it("set delta color flipped flag", () => {
+		let settings = { appSettings: { deltaColorFlipped: true }, dynamicSettings: {} } as Settings
 
-        expect(settingsService.updateSettings).toHaveBeenCalledWith({flipped: false, from: 33.33, to: 66.66})
-    });
+		colorSettingsPanelController.onSettingsChanged(settings, null)
 
-});
+		expect(colorSettingsPanelController["_viewModel"].deltaColorFlipped).toBe(true)
+	})
+
+	it("set white color buildings", () => {
+		let settings = { appSettings: { whiteColorBuildings: true }, dynamicSettings: {} } as Settings
+
+		colorSettingsPanelController.onSettingsChanged(settings, null)
+
+		expect(colorSettingsPanelController["_viewModel"].whiteColorBuildings).toBeTruthy()
+	})
+
+	it("set neutralColorRangeFlipped", () => {
+		let settings = { dynamicSettings: { neutralColorRange: { flipped: true } }, appSettings: {} } as Settings
+
+		colorSettingsPanelController.onSettingsChanged(settings, null)
+
+		expect(colorSettingsPanelController["_viewModel"].neutralColorRangeFlipped).toBeTruthy()
+	})
+
+	it("detects delta mode selection", () => {
+		let fileStates = [
+			{ file: {}, selectedAs: FileSelectionState.Comparison },
+			{ file: {}, selectedAs: FileSelectionState.Reference }
+		] as FileState[]
+
+		colorSettingsPanelController.onFileSelectionStatesChanged(fileStates, null)
+
+		expect(colorSettingsPanelController["_viewModel"].isDeltaState).toBeTruthy()
+	})
+
+	it("detects not delta mode selection", () => {
+		let fileStates = [
+			{ file: {}, selectedAs: FileSelectionState.None },
+			{ file: {}, selectedAs: FileSelectionState.Partial }
+		] as FileState[]
+
+		colorSettingsPanelController.onFileSelectionStatesChanged(fileStates, null)
+
+		expect(colorSettingsPanelController["_viewModel"].isDeltaState).toBeFalsy()
+	})
+
+	it("apply settings calls update settings correctly", () => {
+		settingsService.updateSettings = jest.fn()
+		colorSettingsPanelController["_viewModel"].neutralColorRangeFlipped = false
+		colorSettingsPanelController["_viewModel"].deltaColorFlipped = true
+		colorSettingsPanelController["_viewModel"].whiteColorBuildings = true
+
+		colorSettingsPanelController.applySettings()
+
+		expect(settingsService.updateSettings).toHaveBeenCalledWith({
+			dynamicSettings: {
+				neutralColorRange: {
+					flipped: false
+				}
+			},
+			appSettings: {
+				deltaColorFlipped: true,
+				whiteColorBuildings: true
+			}
+		})
+	})
+
+	it("only adapt color range if color metric is not the same ", () => {
+		settingsService.updateSettings = jest.fn()
+		let settings = { dynamicSettings: { colorMetric: "foo" }, appSettings: {} } as Settings
+
+		colorSettingsPanelController.onSettingsChanged(settings, null)
+		colorSettingsPanelController.onSettingsChanged(settings, null)
+
+		expect(settingsService.updateSettings).toHaveBeenCalledTimes(1)
+		expect(colorSettingsPanelController["lastColorMetric"]).toBe("foo")
+	})
+
+	it("correct metric max is retrieved for range calculation ", () => {
+		metricService.getMaxMetricByMetricName = jest.fn()
+		let settings = { dynamicSettings: { colorMetric: "rloc" }, appSettings: {} } as Settings
+
+		colorSettingsPanelController.onSettingsChanged(settings, null)
+
+		expect(metricService.getMaxMetricByMetricName).toHaveBeenCalledWith("rloc")
+	})
+
+	it("set adapted ColorRange in thirds for given metricValues", () => {
+		withMockedSettingsService()
+		metricService.getMaxMetricByMetricName = jest.fn(() => 100)
+
+		colorSettingsPanelController.onSettingsChanged(settingsService.getSettings(), null)
+
+		expect(settingsService.updateSettings).toHaveBeenCalledWith({
+			dynamicSettings: { neutralColorRange: { flipped: false, from: 33.33, to: 66.66 } }
+		})
+	})
+})
--- visualization/app/codeCharta/ui/colorSettingsPanel/colorSettingsPanel.component.ts
+++ visualization/app/codeCharta/ui/colorSettingsPanel/colorSettingsPanel.component.ts
@@ -1,96 +1,90 @@
-import {SettingsService, SettingsServiceSubscriber} from "../../state/settings.service";
-import "./colorSettingsPanel.component.scss";
-import {FileState, Settings} from "../../codeCharta.model";
-import {IRootScopeService} from "angular";
-import {FileStateService, FileStateServiceSubscriber} from "../../state/fileState.service";
-import {MetricService} from "../../state/metric.service";
-import {FileStateHelper} from "../../util/fileStateHelper";
+import { SettingsService, SettingsServiceSubscriber } from "../../state/settings.service"
+import "./colorSettingsPanel.component.scss"
+import { FileState, Settings } from "../../codeCharta.model"
+import { IRootScopeService } from "angular"
+import { FileStateService, FileStateServiceSubscriber } from "../../state/fileState.service"
+import { MetricService } from "../../state/metric.service"
+import { FileStateHelper } from "../../util/fileStateHelper"
 
 export class ColorSettingsPanelController implements SettingsServiceSubscriber, FileStateServiceSubscriber {
+	private lastColorMetric = null
 
-    private lastColorMetric = null;
+	private _viewModel: {
+		neutralColorRangeFlipped: boolean
+		deltaColorFlipped: boolean
+		whiteColorBuildings: boolean
+		isDeltaState: boolean
+	} = {
+		neutralColorRangeFlipped: null,
+		deltaColorFlipped: null,
+		whiteColorBuildings: null,
+		isDeltaState: null
+	}
 
-    private _viewModel: {
-        neutralColorRangeFlipped: boolean,
-        deltaColorFlipped: boolean,
-        whiteColorBuildings: boolean,
-        isDeltaState: boolean,
-    } = {
-        neutralColorRangeFlipped: null,
-        deltaColorFlipped: null,
-        whiteColorBuildings: null,
-        isDeltaState: null,
-    };
+	/* @ngInject */
+	constructor(
+		private $rootScope: IRootScopeService,
+		private settingsService: SettingsService,
+		private fileStateService: FileStateService,
+		private metricService: MetricService
+	) {
+		SettingsService.subscribe(this.$rootScope, this)
+		FileStateService.subscribe(this.$rootScope, this)
+	}
 
-    /* @ngInject */
-    constructor(
-        private $rootScope: IRootScopeService,
-        private settingsService: SettingsService,
-        private fileStateService: FileStateService,
-        private metricService: MetricService
-    ) {
-        SettingsService.subscribe(this.$rootScope, this);
-        FileStateService.subscribe(this.$rootScope, this);
-    }
+	public onSettingsChanged(settings: Settings, event: angular.IAngularEvent) {
+		this._viewModel.deltaColorFlipped = settings.appSettings.deltaColorFlipped
+		this._viewModel.whiteColorBuildings = settings.appSettings.whiteColorBuildings
 
-    public onSettingsChanged(settings: Settings, event: angular.IAngularEvent) {
-        this._viewModel.deltaColorFlipped = settings.appSettings.deltaColorFlipped;
-        this._viewModel.whiteColorBuildings = settings.appSettings.whiteColorBuildings;
+		if (this.lastColorMetric != settings.dynamicSettings.colorMetric) {
+			this.lastColorMetric = settings.dynamicSettings.colorMetric
+			this.adaptColorRange(settings)
+		} else if (settings.dynamicSettings.neutralColorRange) {
+			this._viewModel.neutralColorRangeFlipped = settings.dynamicSettings.neutralColorRange.flipped
+		}
+	}
 
-        if (this.lastColorMetric != settings.dynamicSettings.colorMetric) {
-            this.lastColorMetric = settings.dynamicSettings.colorMetric
-            this.adaptColorRange(settings)
-        } else if (settings.dynamicSettings.neutralColorRange) {
-            this._viewModel.neutralColorRangeFlipped = settings.dynamicSettings.neutralColorRange.flipped;
-        }
-    }
+	public onFileSelectionStatesChanged(fileStates: FileState[], event: angular.IAngularEvent) {
+		this._viewModel.isDeltaState = FileStateHelper.isDeltaState(fileStates)
+	}
 
-    public onFileSelectionStatesChanged(fileStates: FileState[], event: angular.IAngularEvent) {
-        this._viewModel.isDeltaState = FileStateHelper.isDeltaState(fileStates)
-    }
+	public onImportedFilesChanged(fileStates: FileState[], event: angular.IAngularEvent) {}
 
-    public onImportedFilesChanged(fileStates: FileState[], event: angular.IAngularEvent) {
-    }
+	public applySettings() {
+		this.settingsService.updateSettings({
+			dynamicSettings: {
+				neutralColorRange: {
+					flipped: this._viewModel.neutralColorRangeFlipped
+				}
+			},
+			appSettings: {
+				deltaColorFlipped: this._viewModel.deltaColorFlipped,
+				whiteColorBuildings: this._viewModel.whiteColorBuildings
+			}
+		})
+	}
 
-    public applySettings() {
-        this.settingsService.updateSettings({
-            dynamicSettings: {
-                neutralColorRange: {
-                    flipped: this._viewModel.neutralColorRangeFlipped
-                }
-            },
-            appSettings: {
-                deltaColorFlipped: this._viewModel.deltaColorFlipped,
-                whiteColorBuildings: this._viewModel.whiteColorBuildings
-            }
-        })
-    }
+	private adaptColorRange(s: Settings) {
+		const maxMetricValue = this.metricService.getMaxMetricByMetricName(s.dynamicSettings.colorMetric)
 
-    private adaptColorRange(s: Settings) {
-        const maxMetricValue = this.metricService.getMaxMetricByMetricName(s.dynamicSettings.colorMetric)
+		const flipped = s.dynamicSettings.neutralColorRange ? s.dynamicSettings.neutralColorRange.flipped : false
+		const firstThird = Math.round((maxMetricValue / 3) * 100) / 100
+		const secondThird = Math.round(firstThird * 2 * 100) / 100
 
-        const flipped = (s.dynamicSettings.neutralColorRange) ? s.dynamicSettings.neutralColorRange.flipped : false
-        const firstThird = Math.round((maxMetricValue / 3) * 100) / 100
-        const secondThird = Math.round(firstThird * 2 * 100) / 100
-
-        this.settingsService.updateSettings({
-            dynamicSettings: {
-                neutralColorRange: {
-                    flipped: flipped,
-                    from: firstThird,
-                    to: secondThird
-                }
-            }
-        })
-    }
-
+		this.settingsService.updateSettings({
+			dynamicSettings: {
+				neutralColorRange: {
+					flipped: flipped,
+					from: firstThird,
+					to: secondThird
+				}
+			}
+		})
+	}
 }
 
 export const colorSettingsPanelComponent = {
-    selector: "colorSettingsPanelComponent",
-    template: require("./colorSettingsPanel.component.html"),
-    controller: ColorSettingsPanelController
-};
-
-
-
+	selector: "colorSettingsPanelComponent",
+	template: require("./colorSettingsPanel.component.html"),
+	controller: ColorSettingsPanelController
+}
