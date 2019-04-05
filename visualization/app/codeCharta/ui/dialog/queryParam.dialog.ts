10350073eb329ed45d3f6083b9d134c8f83da902
alschmut <aschmutz1@web.de>
2019-04-05 15:20:21 +0100

    Update content of URL Parameter button in sidebar #178

--- visualization/app/codeCharta/ui/dialog/queryParam.dialog.ts
+++ visualization/app/codeCharta/ui/dialog/queryParam.dialog.ts
@@ -1,38 +1,12 @@
-import {SettingsService} from "../../state/settings.service";
 
 export class QueryParamDialogController {
 
-    private _viewModel = {
-        queryParams: ""
-    }
-
-    constructor(private settingsService: SettingsService, private $mdDialog) {
-        this._viewModel.queryParams = this.getSettingsAsUrlParameterString();
+    constructor(private $mdDialog) {
     }
 
     public hide() {
         this.$mdDialog.hide();
     }
-
-    private getSettingsAsUrlParameterString(): string {
-
-        let result = "";
-
-        let iterateProperties = (obj, prefix) => {
-            for(let key in obj) {
-                if(typeof obj[key] === "object") {
-                    iterateProperties(obj[key], prefix + key + ".");
-                } else {
-                    result += "&" + prefix + key + "=" + obj[key];
-                }
-            }
-        };
-
-        iterateProperties(this.settingsService.getSettings(), "");
-
-        return "?" + result.substring(1).replace(new RegExp("&", "g"),"\n&");
-    }
-
 }
 
 export const queryParamDialog = {
