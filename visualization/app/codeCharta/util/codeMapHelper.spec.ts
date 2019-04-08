e148f09f12c2686374dd1bda0edd148d22d5c4fe
alexhunziker91@gmail.com <alexhunziker91@gmail.com>
2019-04-08 16:15:56 +0200

    mocked TransformPath in some codeMapHelper tests (since path.relative() works different on windows (only in unit tests)) #178

--- visualization/app/codeCharta/util/codeMapHelper.spec.ts
+++ visualization/app/codeCharta/util/codeMapHelper.spec.ts
@@ -1,263 +1,280 @@
 import { CodeMapHelper } from "./codeMapHelper"
 import { BlacklistItem, BlacklistType, CodeMapNode, MarkedPackage } from "../codeCharta.model"
 import * as path from "path"
-import {instantiateModule} from "../../../mocks/ng.mockhelper";
-import {TEST_FILE_WITH_PATHS} from "./dataMocks";
+import { instantiateModule } from "../../../mocks/ng.mockhelper"
+import { TEST_FILE_WITH_PATHS } from "./dataMocks"
 
 describe("codeMapHelper", () => {
+	let testRoot: CodeMapNode
+	let blacklist: BlacklistItem[]
+	let transformPath
 
-    let testRoot: CodeMapNode
-    let blacklist : BlacklistItem[]
+	beforeEach(() => {
+		restartSystem()
+	})
 
-    beforeEach(() => {
-        restartSystem()
-    })
+	afterEach(() => {
+		jest.resetAllMocks()
+	})
 
-    afterEach(() => {
-        jest.resetAllMocks()
-    })
+	function restartSystem() {
+		instantiateModule("app.codeCharta.ui.codeMap")
 
-    function restartSystem() {
-        instantiateModule("app.codeCharta.ui.codeMap")
+		testRoot = JSON.parse(JSON.stringify(TEST_FILE_WITH_PATHS.map))
+	}
 
-        testRoot = JSON.parse(JSON.stringify(TEST_FILE_WITH_PATHS.map))
-    }
+	function addRootToBlacklist() {
+		blacklist.push({ path: testRoot.path, type: BlacklistType.exclude })
+	}
 
-    function addRootToBlacklist() {
-        blacklist.push({path: testRoot.path, type : BlacklistType.exclude})
-    }
+	function addSubNodeToBlacklist() {
+		blacklist.push({ path: testRoot.children[0].path, type: BlacklistType.exclude })
+	}
 
-    function addSubNodeToBlacklist() {
-        blacklist.push({path: testRoot.children[0].path, type : BlacklistType.exclude})
-    }
+	function mockTransformPath() {
+		transformPath = CodeMapHelper.transformPath
+		CodeMapHelper.transformPath = jest.fn((path: string) => {
+			return path.substr(1)
+		})
+	}
 
-    describe("getCodeMapNodeFromPath", () => {
-        it("should return the root if path matches path of root", () => {
-            const expected = testRoot
+	function unmockTransformPath() {
+		CodeMapHelper.transformPath = transformPath
+	}
 
-            const result = CodeMapHelper.getCodeMapNodeFromPath("/root", "Folder", testRoot)
+	describe("getCodeMapNodeFromPath", () => {
+		it("should return the root if path matches path of root", () => {
+			const expected = testRoot
 
-            expect(result).toEqual(expected)
-        })
+			const result = CodeMapHelper.getCodeMapNodeFromPath("/root", "Folder", testRoot)
 
-        it("should return the node that matches path and type", () => {
-            const expected = testRoot.children[1]
+			expect(result).toEqual(expected)
+		})
 
-            const result = CodeMapHelper.getCodeMapNodeFromPath("/root/Parent Leaf", "Folder", testRoot)
+		it("should return the node that matches path and type", () => {
+			const expected = testRoot.children[1]
 
-            expect(result).toEqual(expected)
-        })
+			const result = CodeMapHelper.getCodeMapNodeFromPath("/root/Parent Leaf", "Folder", testRoot)
 
-        it("should return null if no node matches path and type", () => {
-            const result = CodeMapHelper.getCodeMapNodeFromPath("/root/Uncle Leaf", "Folder", testRoot)
+			expect(result).toEqual(expected)
+		})
 
-            expect(result).toBeNull()
-        })
+		it("should return null if no node matches path and type", () => {
+			const result = CodeMapHelper.getCodeMapNodeFromPath("/root/Uncle Leaf", "Folder", testRoot)
 
-        it("should return null if a node only matches path", () => {
-            const result = CodeMapHelper.getCodeMapNodeFromPath("/root/Parent Leaf", "File", testRoot)
+			expect(result).toBeNull()
+		})
 
-            expect(result).toBeNull()
-        })
-    })
+		it("should return null if a node only matches path", () => {
+			const result = CodeMapHelper.getCodeMapNodeFromPath("/root/Parent Leaf", "File", testRoot)
 
-    describe("getAnyCodeMapNodeFromPath", () => {
-        it("should call getCodeMapNodeFromPath with type File at the beginning of call", () => {
-            CodeMapHelper.getCodeMapNodeFromPath = jest.fn()
+			expect(result).toBeNull()
+		})
+	})
 
-            CodeMapHelper.getAnyCodeMapNodeFromPath("/root", testRoot)
+	describe("getAnyCodeMapNodeFromPath", () => {
+		it("should call getCodeMapNodeFromPath with type File at the beginning of call", () => {
+			CodeMapHelper.getCodeMapNodeFromPath = jest.fn()
 
-            expect(CodeMapHelper.getCodeMapNodeFromPath).toHaveBeenCalledWith("/root", "File", testRoot)
-        })
+			CodeMapHelper.getAnyCodeMapNodeFromPath("/root", testRoot)
 
-        it("should call getCodeMapNodeFromPath with type Folder when no file was found and return null", () => {
-            CodeMapHelper.getCodeMapNodeFromPath = jest.fn().mockReturnValue(null)
+			expect(CodeMapHelper.getCodeMapNodeFromPath).toHaveBeenCalledWith("/root", "File", testRoot)
+		})
 
-            const result = CodeMapHelper.getAnyCodeMapNodeFromPath("/root", testRoot)
+		it("should call getCodeMapNodeFromPath with type Folder when no file was found and return null", () => {
+			CodeMapHelper.getCodeMapNodeFromPath = jest.fn().mockReturnValue(null)
 
-            expect(CodeMapHelper.getCodeMapNodeFromPath).toHaveBeenCalledWith("/root", "Folder", testRoot)
-            expect(result).toBeNull()
-        })
+			const result = CodeMapHelper.getAnyCodeMapNodeFromPath("/root", testRoot)
 
-        it("should return the first file found by getCodeMapNodeFromPath", () => {
-            CodeMapHelper.getCodeMapNodeFromPath = jest.fn().mockReturnValue(testRoot)
+			expect(CodeMapHelper.getCodeMapNodeFromPath).toHaveBeenCalledWith("/root", "Folder", testRoot)
+			expect(result).toBeNull()
+		})
 
-            const result = CodeMapHelper.getAnyCodeMapNodeFromPath("/root", testRoot)
+		it("should return the first file found by getCodeMapNodeFromPath", () => {
+			CodeMapHelper.getCodeMapNodeFromPath = jest.fn().mockReturnValue(testRoot)
 
-            expect(result).toEqual(testRoot)
-        })
-    })
+			const result = CodeMapHelper.getAnyCodeMapNodeFromPath("/root", testRoot)
 
-    describe("transformPath", () => {
-        it("should remove / from path", () => {
-            const result = CodeMapHelper.transformPath("/root")
+			expect(result).toEqual(testRoot)
+		})
+	})
 
-            expect(result).toBe("root")
-        })
+	describe("transformPath", () => {
+		it("should remove / from path", () => {
+			const result = CodeMapHelper.transformPath("/root")
 
-        it("should remove ./ from path and return path", () => {
-            const result = CodeMapHelper.transformPath("./root/Big Leaf")
+			expect(result).toBe("root")
+		})
 
-            expect(result).toBe(path.relative("/", "root/Big Leaf"))
-        })
-    })
+		it("should remove ./ from path and return path", () => {
+			const result = CodeMapHelper.transformPath("./root/Big Leaf")
 
-    describe("getNodesByGitignorePath", () => {
-        it("should return the ignored leaf if parent folder is provided", () => {
-            const expected = [testRoot.children[1].children[1]]
+			expect(result).toBe(path.relative("/", "root/Big Leaf"))
+		})
+	})
 
-            const result = CodeMapHelper.getNodesByGitignorePath(testRoot.children[1].children, "/root/Parent Leaf/other small leaf")
+	describe("getNodesByGitignorePath", () => {
+		it("should return the ignored leaf if parent folder is provided", () => {
+			mockTransformPath()
+			const expected = [testRoot.children[1].children[1]]
 
-            expect(result).toEqual(expected)
-        })
+			const result = CodeMapHelper.getNodesByGitignorePath(testRoot.children[1].children, "/root/Parent Leaf/other small leaf")
 
-        it("should return an empty array if no direct children are found with path", () => {
-            const result = CodeMapHelper.getNodesByGitignorePath(testRoot.children, "/root/Parent Leaf/other small leaf")
+			expect(result).toEqual(expected)
+			unmockTransformPath()
+		})
 
-            expect(result).toEqual([])
-        })
+		it("should return an empty array if no direct children are found with path", () => {
+			const result = CodeMapHelper.getNodesByGitignorePath(testRoot.children, "/root/Parent Leaf/other small leaf")
 
-        it("should return all children if root is ignored", () => {
-            const expected = testRoot.children
+			expect(result).toEqual([])
+		})
 
-            const result = CodeMapHelper.getNodesByGitignorePath(testRoot.children, "/root")
+		it("should return all children if root is ignored", () => {
+			const expected = testRoot.children
 
-            expect(result).toEqual(expected)
-        })
+			const result = CodeMapHelper.getNodesByGitignorePath(testRoot.children, "/root")
 
-        it("should return all children of subfolder if root/subfolder is ignored", () => {
-            const expected = [testRoot.children[1]]
+			expect(result).toEqual(expected)
+		})
 
-            const result = CodeMapHelper.getNodesByGitignorePath(testRoot.children, "/root/Parent Leaf")
+		it("should return all children of subfolder if root/subfolder is ignored", () => {
+			mockTransformPath()
+			const expected = [testRoot.children[1]]
 
-            expect(result).toEqual(expected)
-        })
-    })
+			const result = CodeMapHelper.getNodesByGitignorePath(testRoot.children, "/root/Parent Leaf")
 
-    describe("numberOfBlacklistedNodes", () => {
-        beforeEach(() => {
-            blacklist = []
-        })
+			expect(result).toEqual(expected)
+			unmockTransformPath()
+		})
+	})
 
-        it("should return 0 if no blacklist is provided", () => {
-            const result = CodeMapHelper.numberOfBlacklistedNodes(testRoot.children, null)
+	describe("numberOfBlacklistedNodes", () => {
+		beforeEach(() => {
+			blacklist = []
+		})
 
-            expect(result).toBe(0)
-        })
+		it("should return 0 if no blacklist is provided", () => {
+			const result = CodeMapHelper.numberOfBlacklistedNodes(testRoot.children, null)
 
-        it("should return 0 if empty blacklist is provided", () => {
-            const result = CodeMapHelper.numberOfBlacklistedNodes(testRoot.children, blacklist)
+			expect(result).toBe(0)
+		})
 
-            expect(result).toBe(0)
-        })
+		it("should return 0 if empty blacklist is provided", () => {
+			const result = CodeMapHelper.numberOfBlacklistedNodes(testRoot.children, blacklist)
 
-        it("should return 1 if only one node is blacklisted and provided for nodes", () => {
-            addRootToBlacklist()
+			expect(result).toBe(0)
+		})
 
-            const result = CodeMapHelper.numberOfBlacklistedNodes([testRoot], blacklist)
+		it("should return 1 if only one node is blacklisted and provided for nodes", () => {
+			addRootToBlacklist()
 
-            expect(result).toBe(1)
-        })
+			const result = CodeMapHelper.numberOfBlacklistedNodes([testRoot], blacklist)
 
-        it("should return 1 if only one sub-node is blacklisted and but root and sub-node are provided for nodes", () => {
-            addSubNodeToBlacklist()
+			expect(result).toBe(1)
+		})
 
-            const result = CodeMapHelper.numberOfBlacklistedNodes([testRoot, testRoot.children[0]], blacklist)
+		it("should return 1 if only one sub-node is blacklisted and but root and sub-node are provided for nodes", () => {
+			mockTransformPath()
+			addSubNodeToBlacklist()
 
-            expect(result).toBe(1)
-        })
-    })
+			const result = CodeMapHelper.numberOfBlacklistedNodes([testRoot, testRoot.children[0]], blacklist)
 
-    describe("isBlacklisted", () => {
-        beforeEach(() => {
-            blacklist = []
-        })
+			expect(result).toBe(1)
+			unmockTransformPath()
+		})
+	})
 
-        it("should return false if blacklist is empty", () => {
-            const result = CodeMapHelper.isBlacklisted(testRoot, blacklist, BlacklistType.exclude)
+	describe("isBlacklisted", () => {
+		beforeEach(() => {
+			blacklist = []
+		})
 
-            expect(result).toBeFalsy()
-        })
+		it("should return false if blacklist is empty", () => {
+			const result = CodeMapHelper.isBlacklisted(testRoot, blacklist, BlacklistType.exclude)
 
-        it("should return false if node does not exist in blacklist", () => {
-            addSubNodeToBlacklist()
+			expect(result).toBeFalsy()
+		})
 
-            const result = CodeMapHelper.isBlacklisted(testRoot, blacklist, BlacklistType.exclude)
+		it("should return false if node does not exist in blacklist", () => {
+			addSubNodeToBlacklist()
 
-            expect(result).toBeFalsy()
-        })
+			const result = CodeMapHelper.isBlacklisted(testRoot, blacklist, BlacklistType.exclude)
 
-        it("should return false if node exists in blacklist, but does not match BlacklistType", () => {
-            addRootToBlacklist()
+			expect(result).toBeFalsy()
+		})
 
-            const result = CodeMapHelper.isBlacklisted(testRoot, blacklist, BlacklistType.hide)
+		it("should return false if node exists in blacklist, but does not match BlacklistType", () => {
+			addRootToBlacklist()
 
-            expect(result).toBeFalsy()
-        })
+			const result = CodeMapHelper.isBlacklisted(testRoot, blacklist, BlacklistType.hide)
 
-        it("should return true if node exists in blacklist and matches BlacklistType", () => {
-            addRootToBlacklist()
+			expect(result).toBeFalsy()
+		})
 
-            const result = CodeMapHelper.isBlacklisted(testRoot, blacklist, BlacklistType.exclude)
+		it("should return true if node exists in blacklist and matches BlacklistType", () => {
+			addRootToBlacklist()
 
-            expect(result).toBeTruthy()
-        })
+			const result = CodeMapHelper.isBlacklisted(testRoot, blacklist, BlacklistType.exclude)
 
-        it("should return true if node exists in blacklist and sub-node is provided as node", () => {
-            addRootToBlacklist()
+			expect(result).toBeTruthy()
+		})
 
-            const result = CodeMapHelper.isBlacklisted(testRoot.children[0], blacklist, BlacklistType.exclude)
+		it("should return true if node exists in blacklist and sub-node is provided as node", () => {
+			addRootToBlacklist()
 
-            expect(result).toBeTruthy()
-        })
-    })
+			const result = CodeMapHelper.isBlacklisted(testRoot.children[0], blacklist, BlacklistType.exclude)
 
-    describe("getMarkingColor", () => {
-        let markedPackages : MarkedPackage[]
+			expect(result).toBeTruthy()
+		})
+	})
 
-        beforeEach(() => {
-            markedPackages = []
-        })
+	describe("getMarkingColor", () => {
+		let markedPackages: MarkedPackage[]
 
-        function addRootToMarkedPackages() {
-            markedPackages.push({path: "/root", color: "0x000000", attributes: {}})
-        }
+		beforeEach(() => {
+			markedPackages = []
+		})
 
-        function addSubNodesToMarkedPackages() {
-            markedPackages.push({path: "/root/Parent Leaf", color: "0x000001", attributes: {}})
-            markedPackages.push({path: "/root/big leaf", color: "0x000002", attributes: {}})
-        }
+		function addRootToMarkedPackages() {
+			markedPackages.push({ path: "/root", color: "0x000000", attributes: {} })
+		}
 
-        it("should return null if no markedPackages are provided", () => {
-            const result = CodeMapHelper.getMarkingColor(testRoot, null)
+		function addSubNodesToMarkedPackages() {
+			markedPackages.push({ path: "/root/Parent Leaf", color: "0x000001", attributes: {} })
+			markedPackages.push({ path: "/root/big leaf", color: "0x000002", attributes: {} })
+		}
 
-            expect(result).toBeNull()
-        })
+		it("should return null if no markedPackages are provided", () => {
+			const result = CodeMapHelper.getMarkingColor(testRoot, null)
 
-        it("should return null if no node does not exist in markedPackages", () => {
-            const result = CodeMapHelper.getMarkingColor(testRoot, markedPackages)
+			expect(result).toBeNull()
+		})
 
-            expect(result).toBeNull()
-        })
+		it("should return null if no node does not exist in markedPackages", () => {
+			const result = CodeMapHelper.getMarkingColor(testRoot, markedPackages)
 
-        it("should return node color if node exists in markedPackages", () => {
-            addRootToMarkedPackages()
-            const expected = "0x000000"
+			expect(result).toBeNull()
+		})
 
-            const result = CodeMapHelper.getMarkingColor(testRoot, markedPackages)
+		it("should return node color if node exists in markedPackages", () => {
+			addRootToMarkedPackages()
+			const expected = "0x000000"
 
-            expect(result).toBe(expected)
-        })
+			const result = CodeMapHelper.getMarkingColor(testRoot, markedPackages)
 
-        it("should return sorted sub-node color if sub-nodes exist in markedPackages", () => {
-            addRootToMarkedPackages()
-            addSubNodesToMarkedPackages()
+			expect(result).toBe(expected)
+		})
 
-            const expected = "0x000002"
+		it("should return sorted sub-node color if sub-nodes exist in markedPackages", () => {
+			addRootToMarkedPackages()
+			addSubNodesToMarkedPackages()
 
-            const result = CodeMapHelper.getMarkingColor(testRoot.children[0], markedPackages)
+			const expected = "0x000002"
 
-            expect(result).toBe(expected)
-        })
-    })
-});
+			const result = CodeMapHelper.getMarkingColor(testRoot.children[0], markedPackages)
+
+			expect(result).toBe(expected)
+		})
+	})
+})
