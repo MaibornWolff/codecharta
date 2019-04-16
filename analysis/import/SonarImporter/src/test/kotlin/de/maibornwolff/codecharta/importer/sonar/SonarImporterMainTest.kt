7546bdad22804535bba61377b29dbe464faa48d7
alexhunziker <alexhunziker91@gmail.com>
2019-04-16 17:32:40 +0200

    added unit test for trailing slash in url parameter #356

---
+++ analysis/import/SonarImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/sonar/SonarImporterMainTest.kt
@@ -0,0 +1,57 @@
+package de.maibornwolff.codecharta.importer.sonar
+
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.junit.WireMockRule
+import de.maibornwolff.codecharta.importer.sonar.SonarImporterMain.Companion.main
+import de.maibornwolff.codecharta.importer.sonar.dataaccess.SonarMetricsAPIDatasource
+
+import org.junit.Rule
+import org.junit.Test
+
+class SonarImporterMainTest {
+    companion object {
+        private const val PORT = 8089
+
+        private fun METRIC_LIST_URL_PATH(page: Int): String {
+            return "/api/metrics/search?f=hidden,decimalScale&p=$page&ps=${SonarMetricsAPIDatasource.PAGE_SIZE}"
+        }
+
+    }
+
+    @JvmField
+    @Rule
+    var wireMockRule = WireMockRule(PORT)
+
+    @Test
+    @Throws(Exception::class)
+    fun `should call correct url with trailing backslash in URL parameter`() {
+        WireMock.stubFor(
+                WireMock.get(WireMock.urlEqualTo(METRIC_LIST_URL_PATH(1)))
+                        .willReturn(WireMock.aResponse()
+                                .withStatus(200)
+                                .withHeader("Content-Type", "application/json")
+                                .withBody("{\"metrics\": [],\"total\": 0, \"p\": 0, \"ps\": 0}")))
+
+
+        main(arrayOf("http://localhost:8089/",  "someproject"))
+
+        verify ( 1, getRequestedFor(urlEqualTo("/api/metrics/search?f=hidden,decimalScale&p=1&ps=500")) )
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `should call correct url without trailing backslash in URL parameter`() {
+        WireMock.stubFor(
+                WireMock.get(WireMock.urlEqualTo(METRIC_LIST_URL_PATH(1)))
+                        .willReturn(WireMock.aResponse()
+                                .withStatus(200)
+                                .withHeader("Content-Type", "application/json")
+                                .withBody("{\"metrics\": [],\"total\": 0, \"p\": 0, \"ps\": 0}")))
+
+
+        main(arrayOf("http://localhost:8089",  "someproject"))
+
+        verify ( 1, getRequestedFor(urlEqualTo("/api/metrics/search?f=hidden,decimalScale&p=1&ps=500")) )
+    }
+}
