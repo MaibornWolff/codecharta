e0e93da4c002e2d75a7cc44a81cbf6eb71782507
JaiJoPa <jaison_palakat@gmx.de>
2019-05-14 12:50:09 +0200

    Added Component in metricChooser module #495

--- visualization/app/codeCharta/ui/metricChooser/metricChooser.module.ts
+++ visualization/app/codeCharta/ui/metricChooser/metricChooser.module.ts
@@ -1,21 +1,17 @@
-import angular from "angular";
-import "../../state/state.module";
+import angular from "angular"
+import "../../state/state.module"
 import {
-    areaMetricChooserComponent,
-    colorMetricChooserComponent,
-    heightMetricChooserComponent
-} from "./metricChooser.component";
+	areaMetricChooserComponent,
+	colorMetricChooserComponent,
+	heightMetricChooserComponent,
+	distribitionMetricChooserComponent
+} from "./metricChooser.component"
 
-angular.module("app.codeCharta.ui.metricChooser",["app.codeCharta.state"]);
+angular.module("app.codeCharta.ui.metricChooser", ["app.codeCharta.state"])
 
-angular.module("app.codeCharta.ui.metricChooser").component(
-    areaMetricChooserComponent.selector,
-    areaMetricChooserComponent
-).component(
-    heightMetricChooserComponent.selector,
-    heightMetricChooserComponent
-).component(
-    colorMetricChooserComponent.selector,
-    colorMetricChooserComponent
-);
-
+angular
+	.module("app.codeCharta.ui.metricChooser")
+	.component(areaMetricChooserComponent.selector, areaMetricChooserComponent)
+	.component(heightMetricChooserComponent.selector, heightMetricChooserComponent)
+	.component(colorMetricChooserComponent.selector, colorMetricChooserComponent)
+	.component(distribitionMetricChooserComponent.selector, distribitionMetricChooserComponent)
