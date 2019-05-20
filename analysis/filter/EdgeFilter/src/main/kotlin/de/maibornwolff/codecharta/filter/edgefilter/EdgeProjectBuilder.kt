22f18400bc02534583f3c8ba5e31fae3beb9d6f9
Alex Hunziker <alexhunziker91@gmail.com>
2019-05-20 19:23:11 +0200

    Revert "reverted commit d42c since it broke edge filter #465"
    This reverts commit 57d007ceae14e89908933263f9b69f99269078f1.

--- analysis/filter/EdgeFilter/src/main/kotlin/de/maibornwolff/codecharta/filter/edgefilter/EdgeProjectBuilder.kt
+++ analysis/filter/EdgeFilter/src/main/kotlin/de/maibornwolff/codecharta/filter/edgefilter/EdgeProjectBuilder.kt
@@ -31,7 +31,6 @@ package de.maibornwolff.codecharta.filter.edgefilter
 
 import de.maibornwolff.codecharta.model.*
 import mu.KotlinLogging
-import javax.xml.stream.events.Attribute
 import kotlin.math.max
 
 class EdgeProjectBuilder(private val project: Project, private val pathSeparator: Char) {
@@ -161,9 +160,9 @@ class EdgeProjectBuilder(private val project: Project, private val pathSeparator
 
     private fun getAttributeTypeByKey(key: String): AttributeType {
         if (!project.attributeTypes.isEmpty()) {
-            project.attributeTypes["edges"]?.forEach {
-                return AttributeType.valueOf(it[key]?.toString() ?: AttributeType.absolute.toString())
-            } ?: AttributeType.absolute
+            project.attributeTypes["edges"]!!.forEach {
+                if (it.containsKey(key)) return it[key]!!
+            }
         }
         return AttributeType.absolute
     }
