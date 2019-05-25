5bf74e10c3bbf18be1581eb68a3875ebad96c055
alschmut <aschmutz1@web.de>
2019-05-25 14:45:47 +0100

    Replace snapshot test with expected object #495

--- visualization/app/codeCharta/util/fileExtensionCalculator.spec.ts
+++ visualization/app/codeCharta/util/fileExtensionCalculator.spec.ts
@@ -23,8 +23,13 @@ describe("FileExtensionCalculator", () => {
 
 		it("should get correct relative distribution of file-extensions for given metric", () => {
 			const result: MetricDistribution[] = FileExtensionCalculator.getMetricDistribution(map, "RLOC")
-
-			expect(result).toMatchSnapshot()
+			const expected: MetricDistribution[] = [
+				{ fileExtension: "java", absoluteMetricValue: 162, relativeMetricValue: 42.97082228116711, color: null },
+				{ fileExtension: "jpg", absoluteMetricValue: 130, relativeMetricValue: 34.48275862068966, color: null },
+				{ fileExtension: "json", absoluteMetricValue: 70, relativeMetricValue: 18.56763925729443, color: null },
+				{ fileExtension: "None", absoluteMetricValue: 15, relativeMetricValue: 3.978779840848806, color: null }
+			]
+			expect(result).toEqual(expected)
 		})
 	})
 
