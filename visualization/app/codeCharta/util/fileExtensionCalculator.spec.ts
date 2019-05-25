563a3a1ac5d8d3d2d8099987a0c4b51be03ceb80
alschmut <aschmutz1@web.de>
2019-05-25 14:56:06 +0100

    Update test fileExtensionCalculator #495

--- visualization/app/codeCharta/util/fileExtensionCalculator.spec.ts
+++ visualization/app/codeCharta/util/fileExtensionCalculator.spec.ts
@@ -31,6 +31,30 @@ describe("FileExtensionCalculator", () => {
 			]
 			expect(result).toEqual(expected)
 		})
+
+		it("should get correct relative distribution of file-extensions for given metric", () => {
+			const additionalChildren: CodeMapNode[] = [
+				{ name: "child1.txt", type: "File", path: "/root/child1.txt", attributes: { RLOC: 2 } },
+				{ name: "child2.kt", type: "File", path: "/root/child2.kt", attributes: { RLOC: 4 } },
+				{ name: "child3.ts", type: "File", path: "/root/child3.ts", attributes: { RLOC: 6 } },
+				{ name: "child4.xml", type: "File", path: "/root/child4.xml", attributes: { RLOC: 8 } },
+			]
+			map.children.push(...additionalChildren)
+
+			const result: MetricDistribution[] = FileExtensionCalculator.getMetricDistribution(map, "RLOC")
+			
+			const expected: MetricDistribution[] = [
+				{ fileExtension: "java", absoluteMetricValue: 162, relativeMetricValue: 40.80604534005038, color: null },
+				{ fileExtension: "jpg", absoluteMetricValue: 130, relativeMetricValue: 32.7455919395466, color: null },
+				{ fileExtension: "json", absoluteMetricValue: 70, relativeMetricValue: 17.632241813602015, color: null },
+				{ fileExtension: "None", absoluteMetricValue: 15, relativeMetricValue: 3.7783375314861463, color: null }
+				{ fileExtension: "xml", absoluteMetricValue: 8, relativeMetricValue: 2.0151133501259446, color: null },
+				{ fileExtension: "ts", absoluteMetricValue: 6, relativeMetricValue: 1.5113350125944585, color: null },
+				{ fileExtension: "kt", absoluteMetricValue: 4, relativeMetricValue: 1.0075566750629723, color: null },
+				{ fileExtension: "txt", absoluteMetricValue: 2, relativeMetricValue: 0.5037783375314862, color: null },
+			]
+			expect(result).toEqual(expected)
+		})
 	})
 
 	describe("estimateFileExtension", () => {
