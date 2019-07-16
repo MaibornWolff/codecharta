58987e88da6c51b14964831147829e0d206a4b3b
Ben Willenbring <ben-willenbring@t-online.de>
2019-07-16 12:32:19 +0200

    added get methods to codemapbuilding #576

--- visualization/app/codeCharta/ui/codeMap/rendering/codeMapBuilding.ts
+++ visualization/app/codeCharta/ui/codeMap/rendering/codeMapBuilding.ts
@@ -4,54 +4,68 @@ import { ColorConverter } from "../../../util/colorConverter"
 import convert from "color-convert"
 
 export class CodeMapBuilding {
-	public id: number
-	public boundingBox: Box3
-	public color: string
-	public defaultColor: string
-	public node: Node
-	public parent?: CodeMapBuilding
-	public name?: string
+	private readonly _id: number
+	private readonly _boundingBox: Box3
+	private _color: string
+	private readonly _defaultColor: string
+	private readonly _node: Node
 
 	constructor(id: number, box: Box3, node: Node, color: string) {
-		this.id = id
-		this.boundingBox = box
-		this.color = color
-		this.defaultColor = color
-		this.node = node
+		this._id = id
+		this._boundingBox = box
+		this._color = color
+		this._defaultColor = color
+		this._node = node
 	}
 
 	public getCenterPoint(mapSize: number): Vector3 {
 		return new Vector3(
-			this.node.x0 - mapSize * 0.5 + this.node.width / 2,
-			this.node.z0 + this.node.height,
-			this.node.y0 - mapSize * 0.5 + this.node.length / 2
+			this._node.x0 - mapSize * 0.5 + this._node.width / 2,
+			this._node.z0 + this._node.height,
+			this._node.y0 - mapSize * 0.5 + this._node.length / 2
 		)
 	}
 
 	public decreaseLightness(value: number) {
-		const hsl = convert.hex.hsl(this.defaultColor)
+		const hsl = convert.hex.hsl(this._defaultColor)
 		hsl[2] -= value
 		hsl[2] = hsl[2] < 10 ? 10 : hsl[2]
-		this.color = ColorConverter.convertNumberToHex(convert.hsl.hex([hsl[0], hsl[1], hsl[2]]))
+		this._color = ColorConverter.convertNumberToHex(convert.hsl.hex([hsl[0], hsl[1], hsl[2]]))
 	}
 
 	public getColorVector(): Vector3 {
-		return ColorConverter.colorToVector3(this.color)
+		return ColorConverter.colorToVector3(this._color)
 	}
 
 	public getDefaultColorVector(): Vector3 {
-		return ColorConverter.colorToVector3(this.defaultColor)
+		return ColorConverter.colorToVector3(this._defaultColor)
 	}
 
 	public resetColor() {
-		this.color = this.defaultColor
-	}
-
-	public setColor(color: string) {
-		this.color = color
+		this._color = this._defaultColor
 	}
 
 	public equals(building: CodeMapBuilding) {
-		return this.id === building.id
+		return this._id === building._id
+	}
+
+	public get id(): number {
+		return this._id
+	}
+
+	public get boundingBox(): Box3 {
+		return this._boundingBox
+	}
+
+	public get color(): string {
+		return this._color
+	}
+
+	public get node(): Node {
+		return this._node
+	}
+
+	public setColor(color: string) {
+		this._color = color
 	}
 }
