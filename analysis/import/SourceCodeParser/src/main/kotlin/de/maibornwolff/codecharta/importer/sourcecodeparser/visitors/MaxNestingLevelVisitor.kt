9efec4624a5fb31b41362f074e9c133466ed66ff
Ben Willenbring <ben-willenbring@t-online.de>
2019-07-30 16:31:29 +0200

    added some first tests to check how we can access blocks in AST #659

---
+++ analysis/import/SourceCodeParser/src/main/kotlin/de/maibornwolff/codecharta/importer/sourcecodeparser/visitors/MaxNestingLevelVisitor.kt
@@ -0,0 +1,69 @@
+package de.maibornwolff.codecharta.importer.sourcecodeparser.visitors
+
+import org.sonar.java.ast.visitors.ComplexityVisitor
+import org.sonar.plugins.java.api.tree.*
+
+class MaxNestingLevelVisitor : ComplexityVisitor() {
+    private var maxNestingLevel = 0
+    private var currentMaxNestingLevel = 0
+
+    fun getMaxNestingLevel(tree: Tree): Int {
+        scan(tree)
+        return maxNestingLevel
+    }
+
+    override fun visitMethod(tree: MethodTree) {
+        println("visit method")
+        if(maxNestingLevel < currentMaxNestingLevel) {
+            maxNestingLevel = currentMaxNestingLevel
+        }
+        currentMaxNestingLevel = 0
+        super.visitMethod(tree)
+    }
+
+    override fun visitBlock(tree: BlockTree) {
+        currentMaxNestingLevel = 0
+        println("this is a new block")
+        super.visitBlock(tree)
+    }
+
+    override fun visitIfStatement(tree: IfStatementTree) {
+        currentMaxNestingLevel++
+        super.visitIfStatement(tree)
+    }
+
+    override fun visitDoWhileStatement(tree: DoWhileStatementTree) {
+        currentMaxNestingLevel++
+        super.visitDoWhileStatement(tree)
+    }
+
+    override fun visitConditionalExpression(tree: ConditionalExpressionTree) {
+        currentMaxNestingLevel++
+        super.visitConditionalExpression(tree)
+    }
+
+    override fun visitForEachStatement(tree: ForEachStatement) {
+        currentMaxNestingLevel++
+        super.visitForEachStatement(tree)
+    }
+
+    override fun visitForStatement(tree: ForStatementTree) {
+        currentMaxNestingLevel++
+        super.visitForStatement(tree)
+    }
+
+    override fun visitWhileStatement(tree: WhileStatementTree) {
+        currentMaxNestingLevel++
+        super.visitWhileStatement(tree)
+    }
+
+    override fun visitSwitchExpression(tree: SwitchExpressionTree) {
+        currentMaxNestingLevel++
+        super.visitSwitchExpression(tree)
+    }
+
+    override fun visitTryStatement(tree: TryStatementTree) {
+        currentMaxNestingLevel++
+        super.visitTryStatement(tree)
+    }
+}
---
+++ analysis/import/SourceCodeParser/src/test/kotlin/de/maibornwolff/codecharta/importer/sourcecodeparser/visitors/MaxNestingLevelVisitorTest.kt
@@ -0,0 +1,39 @@
+package de.maibornwolff.codecharta.importer.sourcecodeparser.visitors
+
+import junit.framework.Assert.assertEquals
+import org.junit.jupiter.api.Test
+import org.sonar.api.batch.fs.InputFile
+import org.sonar.api.batch.fs.internal.TestInputFileBuilder
+
+import org.sonar.java.ast.parser.JavaParser
+import org.sonar.java.model.DefaultJavaFileScannerContext
+import org.sonar.java.model.JavaVersionImpl
+import org.sonar.plugins.java.Java
+import org.sonar.plugins.java.api.tree.CompilationUnitTree
+import java.io.File
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+
+internal class MaxNestingLevelVisitorTest {
+    private val baseDir = File("src/test/resources/sampleproject").absoluteFile
+
+    @Test
+    fun getMaxNestingLevel() {
+        val file = File("src/test/resources/sampleproject/foo.java")
+        val inputFile = TestInputFileBuilder.create("moduleKey", "foo.java")
+                .setModuleBaseDir(baseDir.toPath())
+                .setCharset(StandardCharsets.UTF_8)
+                .setType(InputFile.Type.MAIN)
+                .setLanguage(Java.KEY)
+                .initMetadata(String(Files.readAllBytes(File("$baseDir/foo.java").toPath()), StandardCharsets.UTF_8))
+                .build()
+
+        val compilationUnitTree = JavaParser.createParser().parse(file) as CompilationUnitTree
+        val defaultJavaFileScannerContext = DefaultJavaFileScannerContext(
+                compilationUnitTree, inputFile, null, null, JavaVersionImpl(), true)
+
+        val maxNestingLevel = MaxNestingLevelVisitor().getMaxNestingLevel(defaultJavaFileScannerContext.tree)
+
+        assertEquals(maxNestingLevel, 1)
+    }
+}
