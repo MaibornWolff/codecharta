cd258e9f4bddd7407a350f2894cd0df83025ae2c
Ben Willenbring <ben-willenbring@t-online.de>
2019-07-31 14:45:59 +0200

    implemented a solution, but it creates stackoverflow #659

--- analysis/import/SourceCodeParser/src/main/kotlin/de/maibornwolff/codecharta/importer/sourcecodeparser/visitors/MaxNestingLevelVisitor.kt
+++ analysis/import/SourceCodeParser/src/main/kotlin/de/maibornwolff/codecharta/importer/sourcecodeparser/visitors/MaxNestingLevelVisitor.kt
@@ -3,69 +3,92 @@ package de.maibornwolff.codecharta.importer.sourcecodeparser.visitors
 import org.sonar.java.ast.visitors.ComplexityVisitor
 import org.sonar.plugins.java.api.tree.*
 
-class MaxNestingLevelVisitor : ComplexityVisitor() {
+class MaxNestingLevelVisitor: ComplexityVisitor() {
     private var maxNestingLevel = 0
-    private var currentMaxNestingLevel = 0
+    private var forwardNesting = 0
+
 
     fun getMaxNestingLevel(tree: Tree): Int {
         super.getNodes(tree)
+
         return maxNestingLevel
     }
 
-    override fun visitMethod(tree: MethodTree) {
-        println("visit method")
-        currentMaxNestingLevel = 0
-        super.visitMethod(tree)
-        if (maxNestingLevel < currentMaxNestingLevel) {
-            println(currentMaxNestingLevel)
-            maxNestingLevel = currentMaxNestingLevel
+    private fun checkForChildrenAndUpdateMax(tree: Tree) {
+        if(super.getNodes(tree).isEmpty()) {
+            if(maxNestingLevel < forwardNesting) {
+                maxNestingLevel = forwardNesting
+            }
+            forwardNesting = 0
         }
     }
 
-    override fun visitBlock(tree: BlockTree) {
-        //currentMaxNestingLevel = 0
-        println("this is a new block")
-        super.visitBlock(tree)
+    override fun visitMethod(tree: MethodTree) {
+        forwardNesting = 0
+        super.visitMethod(tree)
     }
 
     override fun visitIfStatement(tree: IfStatementTree) {
-        currentMaxNestingLevel++
-        println("if_statement")
+        forwardNesting++
+
+        checkForChildrenAndUpdateMax(tree)
+
         super.visitIfStatement(tree)
     }
 
     override fun visitDoWhileStatement(tree: DoWhileStatementTree) {
-        currentMaxNestingLevel++
+        forwardNesting++
+
+        checkForChildrenAndUpdateMax(tree)
+
         super.visitDoWhileStatement(tree)
     }
 
     override fun visitConditionalExpression(tree: ConditionalExpressionTree) {
-        currentMaxNestingLevel++
+        forwardNesting++
+
+        checkForChildrenAndUpdateMax(tree)
+
         super.visitConditionalExpression(tree)
     }
 
     override fun visitForEachStatement(tree: ForEachStatement) {
-        currentMaxNestingLevel++
+        forwardNesting++
+
+        checkForChildrenAndUpdateMax(tree)
+
         super.visitForEachStatement(tree)
     }
 
     override fun visitForStatement(tree: ForStatementTree) {
-        currentMaxNestingLevel++
+        forwardNesting++
+
+        checkForChildrenAndUpdateMax(tree)
+
         super.visitForStatement(tree)
     }
 
     override fun visitWhileStatement(tree: WhileStatementTree) {
-        currentMaxNestingLevel++
+        forwardNesting++
+
+        checkForChildrenAndUpdateMax(tree)
+
         super.visitWhileStatement(tree)
     }
 
     override fun visitSwitchExpression(tree: SwitchExpressionTree) {
-        currentMaxNestingLevel++
+        forwardNesting++
+
+        checkForChildrenAndUpdateMax(tree)
+
         super.visitSwitchExpression(tree)
     }
 
     override fun visitTryStatement(tree: TryStatementTree) {
-        currentMaxNestingLevel++
+        forwardNesting++
+
+        checkForChildrenAndUpdateMax(tree)
+
         super.visitTryStatement(tree)
     }
 }
