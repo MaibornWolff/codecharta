a293013365332298d61920d8c7ad7bff6acb803d
alschmut <aschmutz1@web.de>
2019-08-22 14:14:15 +0200

    WIP: Add AttributeSideBar test content #527

--- visualization/app/codeCharta/ui/attributeSideBar/attributeSideBar.component.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeSideBar.component.ts
@@ -1,8 +1,26 @@
 import "./attributeSideBar.component.scss"
+import { ITimeoutService, IRootScopeService } from "angular"
+import { CodeMapBuilding } from "../codeMap/rendering/codeMapBuilding"
+import {
+	CodeMapMouseEventService,
+	BuildingSelectedEventSubscriber,
+	BuildingDeselectedEventSubscriber
+} from "../codeMap/codeMap.mouseEvent.service"
 
-export class AttributeSideBarController {
+export class AttributeSideBarController implements BuildingSelectedEventSubscriber, BuildingDeselectedEventSubscriber {
 	/* @ngInject */
-	constructor() {}
+	constructor(private $rootScope: IRootScopeService, private $mdSidenav: any, private $timeout: ITimeoutService) {
+		this.$timeout(() => {
+			this.$mdSidenav("right").toggle()
+		}, 200)
+
+		CodeMapMouseEventService.subscribeToBuildingSelectedEvents(this.$rootScope, this)
+		CodeMapMouseEventService.subscribeToBuildingDeselectedEvents(this.$rootScope, this)
+	}
+
+	public onBuildingSelected(selectedBuilding: CodeMapBuilding) {}
+
+	public onBuildingDeselected() {}
 }
 
 export const attributeSideBarComponent = {
