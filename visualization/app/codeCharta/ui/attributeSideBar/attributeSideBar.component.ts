11eb76895ec962be98142a45fd5a28fe86debda1
alschmut <aschmutz1@web.de>
2019-08-22 14:39:57 +0200

    Toggle AttributeSideBar and show metrics #527

--- visualization/app/codeCharta/ui/attributeSideBar/attributeSideBar.component.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeSideBar.component.ts
@@ -6,21 +6,41 @@ import {
 	BuildingSelectedEventSubscriber,
 	BuildingDeselectedEventSubscriber
 } from "../codeMap/codeMap.mouseEvent.service"
+import { KeyValuePair } from "../../codeCharta.model"
+import _ from "lodash"
 
 export class AttributeSideBarController implements BuildingSelectedEventSubscriber, BuildingDeselectedEventSubscriber {
+	private _viewModel: {
+		attributeKeys: string[]
+		attributes: KeyValuePair
+	} = {
+		attributeKeys: null,
+		attributes: null
+	}
+
 	/* @ngInject */
 	constructor(private $rootScope: IRootScopeService, private $mdSidenav: any, private $timeout: ITimeoutService) {
-		this.$timeout(() => {
-			this.$mdSidenav("right").toggle()
-		}, 200)
+		//this.toggleAttributeSideBar()
 
 		CodeMapMouseEventService.subscribeToBuildingSelectedEvents(this.$rootScope, this)
 		CodeMapMouseEventService.subscribeToBuildingDeselectedEvents(this.$rootScope, this)
 	}
 
-	public onBuildingSelected(selectedBuilding: CodeMapBuilding) {}
+	public onBuildingSelected(selectedBuilding: CodeMapBuilding) {
+		this._viewModel.attributeKeys = _.keys(selectedBuilding.node.attributes)
+		this._viewModel.attributes = selectedBuilding.node.attributes
+		this.toggleAttributeSideBar()
+	}
 
-	public onBuildingDeselected() {}
+	public onBuildingDeselected() {
+		this.toggleAttributeSideBar()
+	}
+
+	public toggleAttributeSideBar() {
+		this.$timeout(() => {
+			this.$mdSidenav("right").toggle()
+		}, 200)
+	}
 }
 
 export const attributeSideBarComponent = {
