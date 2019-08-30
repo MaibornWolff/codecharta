fefd0471758292a2580bb701f9e7fd0dad545ffc
alschmut <aschmutz1@web.de>
2019-08-30 12:23:54 +0200

    Add tests edgeSettingsPanel #529

--- visualization/app/codeCharta/ui/edgeSettingsPanel/edgeSettingsPanel.component.spec.ts
+++ visualization/app/codeCharta/ui/edgeSettingsPanel/edgeSettingsPanel.component.spec.ts
@@ -5,6 +5,9 @@ import { IRootScopeService } from "angular"
 import { SettingsService } from "../../state/settingsService/settings.service"
 import { EdgeMetricService } from "../../state/edgeMetric.service"
 import { CodeMapActionsService } from "../codeMap/codeMap.actions.service"
+import { DEFAULT_SETTINGS } from "../../util/dataMocks"
+import _ from "lodash"
+import { Settings } from "../../codeCharta.model"
 
 describe("EdgeSettingsPanelController", () => {
 	let edgeSettingsPanelController: EdgeSettingsPanelController
@@ -13,9 +16,12 @@ describe("EdgeSettingsPanelController", () => {
 	let edgeMetricService: EdgeMetricService
 	let codeMapActionsService: CodeMapActionsService
 
+	let settings: Settings
+
 	beforeEach(() => {
 		restartSystem()
 		rebuildController()
+		withMockedSettingsService()
 	})
 
 	function restartSystem() {
@@ -25,16 +31,47 @@ describe("EdgeSettingsPanelController", () => {
 		settingsService = getService<SettingsService>("settingsService")
 		edgeMetricService = getService<EdgeMetricService>("edgeMetricService")
 		codeMapActionsService = getService<CodeMapActionsService>("codeMapActionsService")
+
+		settings = _.cloneDeep(DEFAULT_SETTINGS)
 	}
 
 	function rebuildController() {
 		edgeSettingsPanelController = new EdgeSettingsPanelController($rootScope, settingsService, edgeMetricService, codeMapActionsService)
 	}
 
+	function withMockedEvents() {
+		SettingsService.subscribe = jest.fn()
+		SettingsService.subscribeToEdgeMetric = jest.fn()
+	}
+
+	function withMockedSettingsService() {
+		settingsService = edgeSettingsPanelController["settingsService"] = jest.fn<SettingsService>(() => {
+			return {
+				getDefaultSettings: jest.fn().mockReturnValue(settings),
+				updateSettings: jest.fn()
+			}
+		})()
+	}
+
+	function withMockedEdgeMetricService(amountOfAffectedBuildings: number) {
+		edgeMetricService = edgeSettingsPanelController["edgeMetricService"] = jest.fn<EdgeMetricService>(() => {
+			return {
+				getAmountOfAffectedBuildings: jest.fn().mockReturnValue(amountOfAffectedBuildings)
+			}
+		})()
+	}
+
+	function withMockedCodeMapActionsService() {
+		codeMapActionsService = edgeSettingsPanelController["codeMapActionsService"] = jest.fn<CodeMapActionsService>(() => {
+			return {
+				updateEdgePreviews: jest.fn()
+			}
+		})()
+	}
+
 	describe("constructor", () => {
 		beforeEach(() => {
-			SettingsService.subscribe = jest.fn()
-			SettingsService.subscribeToEdgeMetric = jest.fn()
+			withMockedEvents()
 		})
 
 		it("should subscribe to SettingsService", () => {
@@ -47,6 +84,128 @@ describe("EdgeSettingsPanelController", () => {
 			rebuildController()
 
 			expect(SettingsService.subscribeToEdgeMetric).toHaveBeenCalledWith($rootScope, edgeSettingsPanelController)
+		})
+	})
+
+	describe("onSettingsChanged", () => {
+		beforeEach(() => {
+			withMockedCodeMapActionsService()
+		})
+
+		it("should update viewModel amountOfEdgePreviews and call codeMapActionService", () => {
+			rebuildController()
+			const update = { appSettings: { amountOfEdgePreviews: 42 } }
+
+			edgeSettingsPanelController.onSettingsChanged(settings, update)
+
+			expect(edgeSettingsPanelController["_viewModel"].amountOfEdgePreviews).toBe(42)
+			expect(codeMapActionsService.updateEdgePreviews).toHaveBeenCalled()
+		})
+
+		it("should update viewModel edgeHeight and call codeMapActionService", () => {
+			rebuildController()
+			const update = { appSettings: { edgeHeight: 7 } }
+
+			edgeSettingsPanelController.onSettingsChanged(settings, update)
+
+			expect(edgeSettingsPanelController["_viewModel"].edgeHeight).toBe(7)
+			expect(codeMapActionsService.updateEdgePreviews).toHaveBeenCalled()
+		})
+
+		it("should update viewModel showOnlyBuildingsWithEdges and call codeMapActionService", () => {
+			rebuildController()
+			const update = { appSettings: { showOnlyBuildingsWithEdges: true } }
+
+			edgeSettingsPanelController.onSettingsChanged(settings, update)
+
+			expect(edgeSettingsPanelController["_viewModel"].showOnlyBuildingsWithEdges).toBe(true)
+			expect(codeMapActionsService.updateEdgePreviews).toHaveBeenCalled()
+		})
+
+		it("should not update viewModel and not call codeMapActionService", () => {
+			rebuildController()
+			const update: Settings = ({ appSettings: { otherSettings: true } } as any) as Settings
+			edgeSettingsPanelController["_viewModel"].amountOfEdgePreviews = 42
+			edgeSettingsPanelController["_viewModel"].edgeHeight = 7
+			edgeSettingsPanelController["_viewModel"].showOnlyBuildingsWithEdges = true
+
+			edgeSettingsPanelController.onSettingsChanged(settings, update)
+
+			expect(edgeSettingsPanelController["_viewModel"].amountOfEdgePreviews).toBe(42)
+			expect(edgeSettingsPanelController["_viewModel"].edgeHeight).toBe(7)
+			expect(edgeSettingsPanelController["_viewModel"].showOnlyBuildingsWithEdges).toBe(true)
+			expect(codeMapActionsService.updateEdgePreviews).not.toHaveBeenCalled()
+		})
+	})
+
+	describe("onEdgeMetricChanged", () => {
+		it("should get 0 totalAffectedBuildings", () => {
+			rebuildController()
+			withMockedEdgeMetricService(0)
+
+			edgeSettingsPanelController.onEdgeMetricChanged("anyMetricName")
+
+			expect(edgeSettingsPanelController["_viewModel"].totalAffectedBuildings).toBe(0)
+		})
+
+		it("should get 42 totalAffectedBuildings", () => {
+			rebuildController()
+			withMockedEdgeMetricService(42)
+
+			edgeSettingsPanelController.onEdgeMetricChanged("anyMetricName")
+
+			expect(edgeSettingsPanelController["_viewModel"].totalAffectedBuildings).toBe(42)
+		})
+
+		it("should get amountOfEdgePreviews from settings", () => {
+			rebuildController()
+			withMockedEdgeMetricService(0)
+
+			edgeSettingsPanelController.onEdgeMetricChanged("anyMetricName")
+
+			expect(edgeSettingsPanelController["_viewModel"].amountOfEdgePreviews).toBe(settings.appSettings.amountOfEdgePreviews)
+		})
+
+		it("should get 0 amountOfEdgePreviews for metricName None", () => {
+			rebuildController()
+			withMockedEdgeMetricService(0)
+
+			edgeSettingsPanelController.onEdgeMetricChanged("None")
+
+			expect(edgeSettingsPanelController["_viewModel"].amountOfEdgePreviews).toBe(0)
+		})
+	})
+
+	describe("applySettingsAmountOfEdgePreviews", () => {
+		it("should call updateSettings", () => {
+			rebuildController()
+			edgeSettingsPanelController["_viewModel"].amountOfEdgePreviews = 42
+
+			edgeSettingsPanelController.applySettingsAmountOfEdgePreviews()
+
+			expect(settingsService.updateSettings).toHaveBeenCalledWith({ appSettings: { amountOfEdgePreviews: 42 } })
+		})
+	})
+
+	describe("applySettingsEdgeHeight", () => {
+		it("should call updateSettings", () => {
+			rebuildController()
+			edgeSettingsPanelController["_viewModel"].edgeHeight = 21
+
+			edgeSettingsPanelController.applySettingsEdgeHeight()
+
+			expect(settingsService.updateSettings).toHaveBeenCalledWith({ appSettings: { edgeHeight: 21 } })
+		})
+	})
+
+	describe("applyShowOnlyBuildingsWithEdges", () => {
+		it("should call updateSettings", () => {
+			rebuildController()
+			edgeSettingsPanelController["_viewModel"].showOnlyBuildingsWithEdges = false
+
+			edgeSettingsPanelController.applyShowOnlyBuildingsWithEdges()
+
+			expect(settingsService.updateSettings).toHaveBeenCalledWith({ appSettings: { showOnlyBuildingsWithEdges: false } })
 		})
 	})
 })
