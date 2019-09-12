6f90ec065f1bfc67989a110fe1efcf73472b93a5
alschmut <aschmutz1@web.de>
2019-09-12 13:24:46 +0200

    Fix test #527

--- visualization/app/codeCharta/ui/attributeSideBar/attributeSideBar.component.spec.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeSideBar.component.spec.ts
@@ -1,5 +1,7 @@
 import "./attributeSideBar.module"
 import { AttributeSideBarController } from "./attributeSideBar.component"
+import { AttributeSideBarService } from "./attributeSideBar.service"
+
 import { instantiateModule, getService } from "../../../../mocks/ng.mockhelper"
 import { IRootScopeService } from "angular"
 import { CodeMapMouseEventService } from "../codeMap/codeMap.mouseEvent.service"
@@ -7,6 +9,7 @@ import { CodeMapMouseEventService } from "../codeMap/codeMap.mouseEvent.service"
 describe("AttributeSideBarController", () => {
 	let attributeSideBarController: AttributeSideBarController
 	let $rootScope: IRootScopeService
+	let attributeSideBarService: AttributeSideBarService
 
 	beforeEach(() => {
 		restartSystem()
@@ -17,10 +20,11 @@ describe("AttributeSideBarController", () => {
 		instantiateModule("app.codeCharta.ui.attributeSideBar")
 
 		$rootScope = getService<IRootScopeService>("$rootScope")
+		attributeSideBarService = getService<AttributeSideBarService>("attributeSideBarService")
 	}
 
 	function rebuildController() {
-		attributeSideBarController = new AttributeSideBarController($rootScope)
+		attributeSideBarController = new AttributeSideBarController($rootScope, attributeSideBarService)
 	}
 
 	describe("constructor", () => {
