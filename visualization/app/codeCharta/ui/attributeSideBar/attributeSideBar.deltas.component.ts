ec89dacbde3569b96f05f9b9e6c63b36d22cb580
alschmut <aschmutz1@web.de>
2019-09-14 19:59:03 +0200

    Set background olor for AttributeSideBar metric values #527

--- visualization/app/codeCharta/ui/attributeSideBar/attributeSideBar.deltas.component.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeSideBar.deltas.component.ts
@@ -1,28 +1,62 @@
 import "./attributeSideBar.component.scss"
-import { KeyValuePair } from "../../codeCharta.model"
+import { Settings, RecursivePartial } from "../../codeCharta.model"
 import { CodeMapBuilding } from "../codeMap/rendering/codeMapBuilding"
 import { BuildingSelectedEventSubscriber, CodeMapMouseEventService } from "../codeMap/codeMap.mouseEvent.service"
-import { IRootScopeService } from "angular"
+import { IRootScopeService, ITimeoutService } from "angular"
 import { ThreeSceneService } from "../codeMap/threeViewer/threeSceneService"
+import { SettingsService } from "../../state/settingsService/settings.service"
+import { SettingsServiceSubscriber } from "../../state/settingsService/settings.service.events"
 
-export class AttributeSideBarDeltasController implements BuildingSelectedEventSubscriber {
-	// tslint:disable-next-line
-	private attributekey: string // variable set throught component binding
+export class AttributeSideBarDeltasController implements BuildingSelectedEventSubscriber, SettingsServiceSubscriber {
+	private static TIME_TO_INIT_BINDING: number = 50
+	private attributekey: string
 
 	private _viewModel: {
-		deltas: KeyValuePair
+		deltaValue: number
+		colorClass: string
+		attributekey: string
 	} = {
-		deltas: null
+		deltaValue: null,
+		colorClass: null,
+		attributekey: null
 	}
 
-	constructor(private $rootScope: IRootScopeService, private threeSceneService: ThreeSceneService) {
+	constructor(
+		private $rootScope: IRootScopeService,
+		private $timeout: ITimeoutService,
+		private threeSceneService: ThreeSceneService,
+		private settingsService: SettingsService
+	) {
 		CodeMapMouseEventService.subscribeToBuildingSelectedEvents(this.$rootScope, this)
-		this.onBuildingSelected(this.threeSceneService.getSelectedBuilding())
+		SettingsService.subscribe(this.$rootScope, this)
+		this.$timeout(() => {
+			this.onBuildingSelected(this.threeSceneService.getSelectedBuilding())
+		}, AttributeSideBarDeltasController.TIME_TO_INIT_BINDING)
 	}
 
 	public onBuildingSelected(selectedBuilding: CodeMapBuilding) {
+		this.setDeltaValue(selectedBuilding)
+		this.setDeltaColorClass(this.settingsService.getSettings())
+	}
+
+	public onSettingsChanged(settings: Settings, update: RecursivePartial<Settings>) {
+		if (update.appSettings && update.appSettings.invertDeltaColors !== undefined) {
+			this.setDeltaColorClass(settings)
+		}
+	}
+
+	private setDeltaValue(selectedBuilding: CodeMapBuilding) {
 		if (selectedBuilding) {
-			this._viewModel.deltas = selectedBuilding.node.deltas
+			const deltas = selectedBuilding.node.deltas
+			this._viewModel.deltaValue = deltas ? deltas[this.attributekey] : null
+		}
+	}
+
+	private setDeltaColorClass(settings: Settings) {
+		if (settings.appSettings.invertDeltaColors) {
+			this._viewModel.colorClass = this._viewModel.deltaValue > 0 ? "red" : "green"
+		} else {
+			this._viewModel.colorClass = this._viewModel.deltaValue > 0 ? "green" : "red"
 		}
 	}
 }
