7f9f626649b850f333f0cea0a1c7503ab223b0be
alschmut <aschmutz1@web.de>
2019-09-29 17:18:05 +0200

    Add test mapTreeView component

---
+++ visualization/app/codeCharta/ui/mapTreeView/mapTreeView.component.spec.ts
@@ -0,0 +1,50 @@
+import "./mapTreeView.module"
+import { MapTreeViewController } from "./mapTreeView.component"
+import { CodeMapPreRenderService } from "../codeMap/codeMap.preRender.service"
+import { IRootScopeService } from "angular"
+import { instantiateModule, getService } from "../../../../mocks/ng.mockhelper"
+import { CodeMapNode } from "../../codeCharta.model"
+import { VALID_NODE_WITH_PATH } from "../../util/dataMocks"
+import _ from "lodash"
+
+describe("MapTreeViewController", () => {
+	let mapTreeViewController: MapTreeViewController
+	let $rootScope: IRootScopeService
+	let map: CodeMapNode
+
+	beforeEach(() => {
+		restartSystem()
+		rebuildController()
+	})
+
+	function restartSystem() {
+		instantiateModule("app.codeCharta.ui.mapTreeView")
+
+		$rootScope = getService<IRootScopeService>("$rootScope")
+		map = _.cloneDeep(VALID_NODE_WITH_PATH)
+	}
+
+	function rebuildController() {
+		mapTreeViewController = new MapTreeViewController($rootScope)
+	}
+
+	describe("constructor", () => {
+		it("should subscribe to CodeMapPreRenderService", () => {
+			CodeMapPreRenderService.subscribe = jest.fn()
+
+			rebuildController()
+
+			expect(CodeMapPreRenderService.subscribe).toHaveBeenCalledWith($rootScope, mapTreeViewController)
+		})
+	})
+
+	describe("onRenderMapChanged", () => {
+		it("should update viewModel.rootNode", () => {
+			mapTreeViewController["_viewModel"] = { rootNode: null }
+
+			mapTreeViewController.onRenderMapChanged(map)
+
+			expect(mapTreeViewController["_viewModel"].rootNode).toBe(map)
+		})
+	})
+})
