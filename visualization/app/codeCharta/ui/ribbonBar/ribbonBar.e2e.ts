12c9cd3f6c744b943593b340a5891640e035b469
Ben Willenbring <ben-willenbring@t-online.de>
2019-10-18 14:26:36 +0200

    added tests for opening and closing individually

--- visualization/app/codeCharta/ui/ribbonBar/ribbonBar.e2e.ts
+++ visualization/app/codeCharta/ui/ribbonBar/ribbonBar.e2e.ts
@@ -1,4 +1,4 @@
-import { goto, puppeteer } from "../../../puppeteer.helper"
+import { goto, launch } from "../../../puppeteer.helper"
 import { RibbonBarPageObject } from "./ribbonBar.po"
 import { SearchPanelPageObject } from "../searchPanel/searchPanel.po"
 import { Browser, Page } from "puppeteer"
@@ -9,11 +9,11 @@ describe("RibbonBar", () => {
 	let browser: Browser
 	let page: Page
 
-	let settingsPanel: SearchPanelPageObject
+	let searchPanel: SearchPanelPageObject
 	let ribbonBar: RibbonBarPageObject
 
 	beforeAll(async () => {
-		browser = await puppeteer.launch({ headless: true })
+		browser = await launch()
 	})
 
 	afterAll(async () => {
@@ -23,29 +23,85 @@ describe("RibbonBar", () => {
 	beforeEach(async () => {
 		page = await browser.newPage()
 
-		settingsPanel = new SearchPanelPageObject(page)
+		searchPanel = new SearchPanelPageObject(page)
 		ribbonBar = new RibbonBarPageObject(page)
 
 		await goto(page)
 	})
 
 	it("hovering over a folder should display the sum of metric of all children", async () => {
-		await settingsPanel.toggleTreeViewMode()
-		await settingsPanel.hoverRootNodeInTreeViewSearchPanel()
+		await searchPanel.toggleTreeViewMode()
+		await searchPanel.hoverRootNodeInTreeViewSearchPanel()
 
 		const actual = await ribbonBar.getAreaMetricValue()
 		expect(actual).toBe("600")
 	})
 
-	it("should toggle ribbonBar onClick toggle button", async () => {
-		expect((await ribbonBar.getRibbonBarClassList()).split(" ")).not.toContain("expanded")
+	it("should open and close the searchPanel", async () => {
+		expect(await searchPanel.isOpen()).toBeFalsy()
 
-		await ribbonBar.toggle()
+		await searchPanel.toggle()
 
-		expect((await ribbonBar.getRibbonBarClassList()).split(" ")).toContain("expanded")
+		expect(await searchPanel.isOpen()).toBeTruthy()
 
-		await ribbonBar.toggle()
+		await searchPanel.toggle()
 
-		expect((await ribbonBar.getRibbonBarClassList()).split(" ")).not.toContain("expanded")
+		expect(await searchPanel.isOpen()).toBeFalsy()
+	})
+
+	it("should open and close the height-metric cad", async () => {
+		const panel = "height-metric"
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeFalsy()
+
+		await ribbonBar.togglePanel(panel)
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeTruthy()
+
+		await ribbonBar.togglePanel(panel)
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeFalsy()
+	})
+
+	it("should open and close the area-metric card", async () => {
+		const panel = "area-metric"
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeFalsy()
+
+		await ribbonBar.togglePanel(panel)
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeTruthy()
+
+		await ribbonBar.togglePanel(panel)
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeFalsy()
+	})
+
+	it("should open and close the color-metric card", async () => {
+		const panel = "color-metric"
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeFalsy()
+
+		await ribbonBar.togglePanel(panel)
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeTruthy()
+
+		await ribbonBar.togglePanel(panel)
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeFalsy()
+	})
+
+	it("should open and close the edge-metric", async () => {
+		const panel = "edge-metric"
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeFalsy()
+
+		await ribbonBar.togglePanel(panel)
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeTruthy()
+
+		await ribbonBar.togglePanel(panel)
+
+		expect(await ribbonBar.isPanelOpen(panel)).toBeFalsy()
 	})
 })
