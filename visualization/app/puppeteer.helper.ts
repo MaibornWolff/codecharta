d37f7bfd9f3858330b6389baa14d1ebb301403fc
Ben Willenbring <ben-willenbring@t-online.de>
2019-10-18 14:26:19 +0200

    puppeteer will launch in 1920x1080 now

--- visualization/app/puppeteer.helper.ts
+++ visualization/app/puppeteer.helper.ts
@@ -1,5 +1,5 @@
 import * as path from "path"
-import { Page } from "puppeteer"
+import { Browser, Page } from "puppeteer"
 
 export const puppeteer = require("puppeteer")
 export const CC_URL = `file:${path.join(__dirname, "../dist/webpack/index.html")}`
@@ -13,4 +13,11 @@ export const delay = timeout => {
 export const goto = async (page: Page): Promise<void> => {
 	await page.goto(CC_URL)
 	await delay(3000)
+}
+
+export const launch = async (): Promise<Browser> => {
+	return await puppeteer.launch({
+		defaultViewport: { width: 1920, height: 1080 },
+		headless: false
+	})
 }
