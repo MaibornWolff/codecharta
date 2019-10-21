4305eff3ac6057b0620c3d66f822a2c71fd72303
alexhunziker91@gmail.com <alexhunziker@sunrise.ch>
2019-10-21 12:49:49 +0200

    addded tests for onBuildingHovered #607

--- visualization/app/codeCharta/ui/toolBar/toolBar.component.spec.ts
+++ visualization/app/codeCharta/ui/toolBar/toolBar.component.spec.ts
@@ -2,8 +2,11 @@ import "./toolBar.module"
 import { ToolBarController } from "./toolBar.component"
 import { instantiateModule, getService } from "../../../../mocks/ng.mockhelper"
 import { DialogService } from "../dialog/dialog.service"
+import { IRootScopeService } from "angular"
+import { CodeMapBuildingTransition } from "../codeMap/codeMap.mouseEvent.service"
 
 describe("ToolBarController", () => {
+	let $rootScope: IRootScopeService
 	let toolBarController: ToolBarController
 	let dialogService: DialogService
 
@@ -16,11 +19,12 @@ describe("ToolBarController", () => {
 	function restartSystem() {
 		instantiateModule("app.codeCharta.ui.toolBar")
 
+		$rootScope = getService<IRootScopeService>("$rootScope")
 		dialogService = getService<DialogService>("dialogService")
 	}
 
 	function rebuildController() {
-		toolBarController = new ToolBarController(dialogService)
+		toolBarController = new ToolBarController($rootScope, dialogService)
 	}
 
 	function withMockedDialogService() {
@@ -43,6 +47,24 @@ describe("ToolBarController", () => {
 			toolBarController.showGlobalSettings()
 
 			expect(dialogService.showGlobalSettingsDialog).toHaveBeenCalled()
+		})
+	})
+
+	describe("onBuildingHovered", () => {
+		it("should set nodeHovered to true if node is hovered", () => {
+			const dataHovered = ({ to: { node: {} } } as unknown) as CodeMapBuildingTransition
+
+			toolBarController.onBuildingHovered(dataHovered)
+
+			expect(toolBarController["_viewModel"].nodeHovered).toBe(true)
+		})
+
+		it("should set nodeHovered to false if no node is hovered", () => {
+			const dataHovered = ({ to: null } as unknown) as CodeMapBuildingTransition
+
+			toolBarController.onBuildingHovered(dataHovered)
+
+			expect(toolBarController["_viewModel"].nodeHovered).toBe(false)
 		})
 	})
 })
