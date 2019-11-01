b80288efde96700d0fa5f986b7c35f41ced126ef
Alex Hunziker <alexhunziker91@gmail.com>
2019-11-01 16:03:35 +0100

    remove unwanted attributes from nodes on download #679

--- visualization/app/codeCharta/util/fileDownloader.ts
+++ visualization/app/codeCharta/util/fileDownloader.ts
@@ -3,6 +3,7 @@ import * as d3 from "d3"
 import { CodeMapNode, BlacklistType, BlacklistItem, FileSettings, ExportCCFile, FileMeta, AttributeTypes } from "../codeCharta.model"
 import { DownloadCheckboxNames } from "../ui/dialog/dialog.download.component"
 import { CodeChartaService } from "../codeCharta.service"
+import { stringify } from "querystring"
 
 export class FileDownloader {
 	public static downloadCurrentMap(
@@ -26,7 +27,7 @@ export class FileDownloader {
 		return {
 			projectName: fileMeta.projectName,
 			apiVersion: fileMeta.apiVersion,
-			nodes: [this.removeJsonHashkeysAndVisibleAttribute(map)],
+			nodes: [this.undecorateMap(map)],
 			attributeTypes: this.getAttributeTypesForJSON(fileSettings.attributeTypes),
 			edges: downloadSettingsNames.includes(DownloadCheckboxNames.edges) ? fileSettings.edges : [],
 			markedPackages: downloadSettingsNames.includes(DownloadCheckboxNames.markedPackages) ? fileSettings.markedPackages : [],
@@ -59,28 +60,36 @@ export class FileDownloader {
 		return blacklist.filter(x => x.type == type)
 	}
 
-	private static removeJsonHashkeysAndVisibleAttribute(map: CodeMapNode) {
+	private static undecorateMap(map: CodeMapNode) {
 		let copy: CodeMapNode = JSON.parse(JSON.stringify(map))
 		d3.hierarchy(copy).each(node => {
 			delete node.data.visible
+			delete node.data.origin
+			delete node.data.edgeAttributes
+			delete node.data.path
+			if (node.data.type === "Folder") {
+				node.data.attributes = {}
+			} else {
+				delete node.data.attributes["unary"]
+			}
 		})
 		return copy
 	}
 
-	private static downloadData(data, fileName: string) {
-		let dataJson = data
+	private static downloadData(data: ExportCCFile, fileName: string) {
+		let dataJson = stringify(data)
 		if (typeof data === "object") {
 			dataJson = angular.toJson(data, 4)
 		}
 
 		const blob = new Blob([dataJson], { type: "text/json" })
-		const e = document.createEvent("MouseEvents")
-		const a = document.createElement("a")
+		const mouseEvent = document.createEvent("MouseEvents")
+		const link = document.createElement("a")
 
-		a.download = fileName
-		a.href = window.URL.createObjectURL(blob)
-		a.dataset.downloadurl = ["text/json", a.download, a.href].join(":")
-		e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
-		a.dispatchEvent(e)
+		link.download = fileName
+		link.href = window.URL.createObjectURL(blob)
+		link.dataset.downloadurl = ["text/json", link.download, link.href].join(":")
+		mouseEvent.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+		link.dispatchEvent(mouseEvent)
 	}
 }
