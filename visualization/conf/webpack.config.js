9a3ee8a4efaed1ac8c963d9f0b28bad1c7824d25
Ben Willenbring <ben-willenbring@t-online.de>
2019-11-15 12:44:29 +0100

    setup new webpack config

--- visualization/conf/webpack.config.js
+++ visualization/conf/webpack.config.js
@@ -1,10 +1,6 @@
-const webpack = require("webpack")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-const BrowserSyncPlugin = require("browser-sync-webpack-plugin")
 const path = require("path")
-const CleanWebpackPlugin = require("clean-webpack-plugin")
-
-const dist = path.resolve(__dirname, "../dist/webpack")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
 	entry: "./app/app.ts",
@@ -14,18 +10,21 @@ module.exports = {
 	},
 	module: require("./webpack.loaders.js"),
 	plugins: [
-		new CleanWebpackPlugin([dist], { verbose: true, root: dist + "/.." }),
+		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			template: "./app/index.html"
-		}),
-		new BrowserSyncPlugin({
-			host: "localhost",
-			port: 3000,
-			server: { baseDir: [dist] }
 		})
 	],
 	devtool: "source-map",
 	resolve: {
 		extensions: [".ts", ".tsx", ".js"]
+	},
+	devServer: {
+		contentBase: "./dist",
+		compress: true, // enable gzip compression
+		historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+		hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+		https: false, // true for self-signed, object for cert authority
+		noInfo: true // only errors & warns on hot reload
 	}
 }
