4f6698716cca854060e81501715e40e638b498fd
Ben Willenbring <ben-willenbring@t-online.de>
2019-11-15 10:35:08 +0100

    fixed wrong module name injection

--- visualization/app/codeCharta/ui/rangeSlider/rangeSlider.module.ts
+++ visualization/app/codeCharta/ui/rangeSlider/rangeSlider.module.ts
@@ -4,6 +4,6 @@ import "../../codeCharta.module"
 import { rangeSliderComponent } from "./rangeSlider.component"
 import "angularjs-slider"
 
-angular.module("app.codeCharta.ui.rangeSlider", ["app.codeCharta.state", "rzModule", "app.codeCharta"])
+angular.module("app.codeCharta.ui.rangeSlider", ["app.codeCharta.state", "rzSlider", "app.codeCharta"])
 
 angular.module("app.codeCharta.ui.rangeSlider").component(rangeSliderComponent.selector, rangeSliderComponent)
