8841a7fa218d955e59a5e4c00379882fbf3fe648
alschmut <aschmutz1@web.de>
2019-11-22 18:25:22 +0100

    Broadcast store-changed event #698

--- visualization/app/codeCharta/state/store.service.ts
+++ visualization/app/codeCharta/state/store.service.ts
@@ -5,7 +5,12 @@ import { IRootScopeService } from "angular"
 import { BlacklistSubscriber, SettingsEvents } from "./settingsService/settings.service.events"
 import { BlacklistActions } from "./store/fileSettings/blacklist/blacklist.actions"
 
+export interface StoreSubscriber {
+	onStoreChanged(actionType: string)
+}
+
 export class StoreService {
+	private static STORE_CHANGED_EVENT = "store-changed"
 	private store: Store
 
 	/* @ngInject */
@@ -19,14 +24,25 @@ export class StoreService {
 		if (Object.values(BlacklistActions).includes(action.type)) {
 			this.notifyBlacklistSubscribers()
 		}
+		this.notify(action.type)
 	}
 
 	public getState(): State {
 		return this.store.getState()
 	}
 
+	private notify(actionType: string) {
+		this.$rootScope.$broadcast(StoreService.STORE_CHANGED_EVENT, { actionType: actionType })
+	}
+
 	private notifyBlacklistSubscribers() {
 		this.$rootScope.$broadcast(SettingsEvents.BLACKLIST_CHANGED_EVENT, { blacklist: this.getState().fileSettings.blacklist })
+	}
+
+	public static subscribeToStore($rootScope: IRootScopeService, subscriber: StoreSubscriber) {
+		$rootScope.$on(StoreService.STORE_CHANGED_EVENT, (event, data) => {
+			subscriber.onStoreChanged(data.actionType)
+		})
 	}
 
 	public static subscribeToBlacklist($rootScope: IRootScopeService, subscriber: BlacklistSubscriber) {
