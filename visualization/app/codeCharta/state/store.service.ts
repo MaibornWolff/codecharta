53d20cf4a10492e8569afe816364232949d99a72
alschmut <aschmutz1@web.de>
2019-11-22 19:19:47 +0100

    Remove blacklist broadcast inside storeService #698

--- visualization/app/codeCharta/state/store.service.ts
+++ visualization/app/codeCharta/state/store.service.ts
@@ -2,8 +2,6 @@ import { createStore, Store, Action } from "redux"
 import rootReducer from "./store/reducer"
 import { State } from "../codeCharta.model"
 import { IRootScopeService } from "angular"
-import { BlacklistSubscriber, SettingsEvents } from "./settingsService/settings.service.events"
-import { BlacklistActions } from "./store/fileSettings/blacklist/blacklist.actions"
 
 export interface StoreSubscriber {
 	onStoreChanged(actionType: string)
@@ -20,10 +18,6 @@ export class StoreService {
 
 	public dispatch(action: Action) {
 		this.store.dispatch(action)
-		// Broadcast event that fits the action?
-		if (Object.values(BlacklistActions).includes(action.type)) {
-			this.notifyBlacklistSubscribers()
-		}
 		this.notify(action.type)
 	}
 
@@ -35,19 +29,9 @@ export class StoreService {
 		this.$rootScope.$broadcast(StoreService.STORE_CHANGED_EVENT, { actionType: actionType })
 	}
 
-	private notifyBlacklistSubscribers() {
-		this.$rootScope.$broadcast(SettingsEvents.BLACKLIST_CHANGED_EVENT, { blacklist: this.getState().fileSettings.blacklist })
-	}
-
 	public static subscribeToStore($rootScope: IRootScopeService, subscriber: StoreSubscriber) {
 		$rootScope.$on(StoreService.STORE_CHANGED_EVENT, (event, data) => {
 			subscriber.onStoreChanged(data.actionType)
-		})
-	}
-
-	public static subscribeToBlacklist($rootScope: IRootScopeService, subscriber: BlacklistSubscriber) {
-		$rootScope.$on(SettingsEvents.BLACKLIST_CHANGED_EVENT, (event, data) => {
-			subscriber.onBlacklistChanged(data.blacklist)
 		})
 	}
 }
