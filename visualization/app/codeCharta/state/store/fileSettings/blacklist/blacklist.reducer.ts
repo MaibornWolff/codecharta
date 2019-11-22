47ea5279ef10fc3373093f0a09ee16f12ff48464
alschmut <aschmutz1@web.de>
2019-11-22 18:24:19 +0100

    Fix removingBlacklistItem #698

--- visualization/app/codeCharta/state/store/fileSettings/blacklist/blacklist.reducer.ts
+++ visualization/app/codeCharta/state/store/fileSettings/blacklist/blacklist.reducer.ts
@@ -1,6 +1,7 @@
 import _ from "lodash"
 import { BlacklistItem } from "../../../../codeCharta.model"
 import { BlacklistAction, BlacklistActions } from "./blacklist.actions"
+import angular from "angular"
 
 export function blacklist(state: BlacklistItem[] = [], action: BlacklistAction): BlacklistItem[] {
 	switch (action.type) {
@@ -9,7 +10,7 @@ export function blacklist(state: BlacklistItem[] = [], action: BlacklistAction):
 		case BlacklistActions.REMOVE_BLACKLIST_ITEM:
 			return removeBlacklistItem(state, action.payload)
 		case BlacklistActions.LOAD_BLACKLIST:
-			return _.cloneDeep(action.payload)
+			return [...action.payload]
 		default:
 			return state
 	}
@@ -17,6 +18,6 @@ export function blacklist(state: BlacklistItem[] = [], action: BlacklistAction):
 
 function removeBlacklistItem(blacklist: BlacklistItem[], item: BlacklistItem): BlacklistItem[] {
 	return blacklist.filter(x => {
-		return item.path !== x.path && item.type === x.type
+		return JSON.stringify(angular.toJson(x)) !== JSON.stringify(angular.toJson(item))
 	})
 }
