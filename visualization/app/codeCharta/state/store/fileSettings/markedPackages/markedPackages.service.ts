14f7e45f89cebfc77bd7fc1b766e566a850868e7
Ben Willenbring <ben-willenbring@t-online.de>
2019-11-27 15:46:57 +0100

    imported missing type #804

--- visualization/app/codeCharta/state/store/fileSettings/markedPackages/markedPackages.service.ts
+++ visualization/app/codeCharta/state/store/fileSettings/markedPackages/markedPackages.service.ts
@@ -2,6 +2,7 @@ import { StoreService, StoreSubscriber } from "../../../store.service"
 import { IRootScopeService } from "angular"
 import { MarkedPackagesActions } from "./markedPackages.actions"
 import _ from "lodash"
+import { MarkedPackage } from "../../../../codeCharta.model"
 
 export interface MarkedPackagesSubscriber {
 	onMarkedPackagesChanged(markedPackages: MarkedPackage[])
