8febe04fb3a140aa6f017fd046d0920729dc7f02
JaiJoPa <jaison_palakat@gmx.de>
2019-11-27 14:46:36 +0100

    Added tests for threesSceneService #545

--- visualization/app/codeCharta/ui/codeMap/threeViewer/threeSceneService.spec.ts
+++ visualization/app/codeCharta/ui/codeMap/threeViewer/threeSceneService.spec.ts
@@ -1,13 +1,14 @@
 import "./threeViewer.module"
 import { getService, instantiateModule } from "../../../../../mocks/ng.mockhelper"
 import { SettingsService } from "../../../state/settingsService/settings.service"
-import { VALID_NODE, CODE_MAP_BUILDING } from "../../../util/dataMocks"
+import { VALID_NODE, CODE_MAP_BUILDING, TEST_NODES } from "../../../util/dataMocks"
 import { CodeMapNode } from "../../../codeCharta.model"
 import { CodeMapPreRenderService } from "../codeMap.preRender.service"
 import { CodeMapBuilding } from "../rendering/codeMapBuilding"
 import { ThreeSceneService } from "./threeSceneService"
 import { IRootScopeService } from "angular"
 import _ from "lodash"
+import { CodeMapMesh } from "../rendering/codeMapMesh"
 
 describe("ThreeSceneService", () => {
 	let threeSceneService: ThreeSceneService
@@ -35,6 +36,10 @@ describe("ThreeSceneService", () => {
 		threeSceneService = new ThreeSceneService($rootScope, settingsService)
 	}
 
+	beforeEach(() => {
+		threeSceneService["mapMesh"] = new CodeMapMesh(TEST_NODES, settingsService.getSettings(), false)
+	})
+
 	describe("constructor", () => {
 		it("should subscribe renderMap", () => {
 			CodeMapPreRenderService.subscribe = jest.fn()
@@ -53,6 +58,55 @@ describe("ThreeSceneService", () => {
 			threeSceneService.onRenderMapChanged(map)
 
 			expect(threeSceneService["reselectBuilding"]).toHaveBeenCalled()
+		})
+	})
+
+	describe("highlightBuildings", () => {
+		it("should call highlightMultipleBuildings", () => {
+			threeSceneService["highlightedBuildings"] = [CODE_MAP_BUILDING]
+
+			threeSceneService["mapMesh"].highlightMultipleBuildings = jest.fn()
+
+			threeSceneService.highlightBuildings()
+
+			expect(threeSceneService["mapMesh"].highlightMultipleBuildings).toHaveBeenCalledWith(
+				threeSceneService["highlightedBuildings"],
+				null,
+				settingsService.getSettings()
+			)
+		})
+	})
+
+	describe("addBuildingToHighlightingList", () => {
+		it("should add the given building to the HighlightingList ", () => {
+			threeSceneService["highlightedBuildings"] = []
+
+			threeSceneService.addBuildingToHighlightingList(CODE_MAP_BUILDING)
+
+			expect(threeSceneService["highlightedBuildings"]).toEqual([CODE_MAP_BUILDING])
+		})
+	})
+
+	describe("highlightBuilding", () => {
+		it("should add a building to the highlighting list and call the highlight function", () => {
+			threeSceneService.addBuildingToHighlightingList = jest.fn()
+			threeSceneService.highlightBuildings = jest.fn()
+			threeSceneService["highlightedBuildings"] = []
+
+			threeSceneService.highlightBuilding(CODE_MAP_BUILDING)
+
+			expect(threeSceneService.addBuildingToHighlightingList).toHaveBeenCalled()
+			expect(threeSceneService.highlightBuildings).toHaveBeenCalled()
+		})
+	})
+
+	describe("clearHighlight", () => {
+		it("should clear the highlighting list", () => {
+			threeSceneService["highlightedBuildings"] = [CODE_MAP_BUILDING]
+
+			threeSceneService.clearHighlight()
+
+			expect(threeSceneService["highlightedBuildings"]).toHaveLength(0)
 		})
 	})
 })
