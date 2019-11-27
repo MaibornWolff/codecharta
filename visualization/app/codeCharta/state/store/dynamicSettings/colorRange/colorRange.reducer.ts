2ce7078bd6c4e09d0bec38fa896a09b009e1477a
Ben Willenbring <ben-willenbring@t-online.de>
2019-11-27 12:24:38 +0100

    use deepClone instead of {...} #804

--- visualization/app/codeCharta/state/store/dynamicSettings/colorRange/colorRange.reducer.ts
+++ visualization/app/codeCharta/state/store/dynamicSettings/colorRange/colorRange.reducer.ts
@@ -1,10 +1,11 @@
 import { ColorRangeAction, ColorRangeActions } from "./colorRange.actions"
 import { ColorRange } from "../../../../codeCharta.model"
+import _ from "lodash"
 
 export function colorRange(state: ColorRange = { from: null, to: null }, action: ColorRangeAction): ColorRange {
 	switch (action.type) {
 		case ColorRangeActions.SET_COLOR_RANGE:
-			return { ...action.payload }
+			return _.cloneDeep(action.payload)
 		default:
 			return state
 	}
