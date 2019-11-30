424a6b7989ffdacc8cb9694cb9dbf7b6bf7c3fb2
alschmut <aschmutz1@web.de>
2019-11-30 12:49:00 +0000

    Adapt rootRecucer and fileSettings reducer to extract parent actions #804

--- visualization/app/codeCharta/state/store/fileSettings/fileSettings.reducer.ts
+++ visualization/app/codeCharta/state/store/fileSettings/fileSettings.reducer.ts
@@ -3,14 +3,31 @@ import { markedPackages } from "./markedPackages/markedPackages.reducer"
 import { edges } from "./edges/edges.reducer"
 import { attributeTypes } from "./attributeTypes/attributeTypes.reducer"
 import { blacklist } from "./blacklist/blacklist.reducer"
-import { combineReducers } from "redux"
+import { Action } from "redux"
+import { setMarkedPackages } from "./markedPackages/markedPackages.actions"
+import { setEdges } from "./edges/edges.actions"
+import { setAttributeTypes } from "./attributeTypes/attributeTypes.actions"
+import { setBlacklist } from "./blacklist/blacklist.actions"
+import { FileSettings } from "../../../codeCharta.model"
+import { FileSettingsActions } from "./fileSettings.actions"
 
-const fileSettings = combineReducers({
-	// Plop: Append reducer usage here
-	markedPackages,
-	edges,
-	attributeTypes,
-	blacklist
-})
+export function fileSettings(state: FileSettings, action: Action) {
+	switch (action.type) {
+		case FileSettingsActions.SET_FILE_SETTINGS:
+			return {
+				markedPackages: markedPackages(state.markedPackages, setMarkedPackages(action.payload.markedPackages)),
+				edges: edges(state.edges, setEdges(action.payload.edges)),
+				attributeTypes: attributeTypes(state.attributeTypes, setAttributeTypes(action.payload.attributeTypes)),
+				blacklist: blacklist(state.blacklist, setBlacklist(action.payload.blacklist))
+			}
+		default:
+			return {
+				markedPackages: markedPackages(state.markedPackages, action),
+				edges: edges(state.edges, action),
+				attributeTypes: attributeTypes(state.attributeTypes, action),
+				blacklist: blacklist(state.blacklist, action)
+			}
+	}
+}
 
 export default fileSettings
--- visualization/app/codeCharta/state/store/reducer.ts
+++ visualization/app/codeCharta/state/store/reducer.ts
@@ -1,10 +1,26 @@
 import appSettings from "./appSettings/appSettings.reducer"
 import fileSettings from "./fileSettings/fileSettings.reducer"
 import dynamicSettings from "./dynamicSettings/dynamicSettings.reducer"
-import { combineReducers } from "redux"
+import { Action } from "redux"
+import { State } from "../../codeCharta.model"
+import { StateActions } from "./state.actions"
+import { setFileSettings } from "./fileSettings/fileSettings.actions"
+import { setAppSettings } from "./appSettings/appSettings.actions"
+import { setDynamicSettings } from "./dynamicSettings/dynamicSettings.actions"
 
-export const rootReducer = combineReducers({
-	fileSettings,
-	appSettings,
-	dynamicSettings
-})
+export function rootReducer(state: State, action: Action) {
+	switch (action.type) {
+		case StateActions.SET_STATE:
+			return {
+				fileSettings: fileSettings(state.fileSettings, setFileSettings(action.payload.fileSettings)),
+				appSettings: appSettings(state.appSettings, setAppSettings(action.payload.appSettings)),
+				dynamicSettings: dynamicSettings(state.dynamicSettings, setDynamicSettings(action.payload.dynamicSettings))
+			}
+		default:
+			return {
+				fileSettings: fileSettings(state.fileSettings, action),
+				appSettings: appSettings(state.appSettings, action),
+				dynamicSettings: dynamicSettings(state.dynamicSettings, action)
+			}
+	}
+}
