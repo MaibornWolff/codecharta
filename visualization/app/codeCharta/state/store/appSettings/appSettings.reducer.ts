c8095e5b5e0731dab6d99fce9d4c80cd1cde0cdd
alschmut <aschmutz1@web.de>
2019-12-01 18:05:35 +0000

    Adapt appSettings and dynamicSettings reducer to forward parent actions #804

--- visualization/app/codeCharta/state/store/appSettings/appSettings.reducer.ts
+++ visualization/app/codeCharta/state/store/appSettings/appSettings.reducer.ts
@@ -15,26 +15,78 @@ import { edgeHeight } from "./edgeHeight/edgeHeight.reducer"
 import { amountOfEdgePreviews } from "./amountOfEdgePreviews/amountOfEdgePreviews.reducer"
 import { amountOfTopLabels } from "./amountOfTopLabels/amountOfTopLabels.reducer"
 import { isPresentationMode } from "./isPresentationMode/isPresentationMode.reducer"
-import { combineReducers } from "redux"
+import { AppSettings, CCAction } from "../../../codeCharta.model"
+import { AppSettingsActions } from "./appSettings.actions"
+import { setMapColors } from "./mapColors/mapColors.actions"
+import { setResetCameraIfNewFileIsLoaded } from "./resetCameraIfNewFileIsLoaded/resetCameraIfNewFileIsLoaded.actions"
+import { setShowOnlyBuildingsWithEdges } from "./showOnlyBuildingsWithEdges/showOnlyBuildingsWithEdges.actions"
+import { setWhiteColorBuildings } from "./whiteColorBuildings/whiteColorBuildings.actions"
+import { setIsWhiteBackground } from "./isWhiteBackground/isWhiteBackground.actions"
+import { setDynamicMargin } from "./dynamicMargin/dynamicMargin.actions"
+import { setInvertHeight } from "./invertHeight/invertHeight.actions"
+import { setInvertDeltaColors } from "./invertDeltaColors/invertDeltaColors.actions"
+import { setInvertColorRange } from "./invertColorRange/invertColorRange.actions"
+import { setHideFlatBuildings } from "./hideFlatBuildings/hideFlatBuildings.actions"
+import { setCamera } from "./camera/camera.actions"
+import { setScaling } from "./scaling/scaling.actions"
+import { setEdgeHeight } from "./edgeHeight/edgeHeight.actions"
+import { setAmountOfEdgePreviews } from "./amountOfEdgePreviews/amountOfEdgePreviews.actions"
+import { setAmountOfTopLabels } from "./amountOfTopLabels/amountOfTopLabels.actions"
+import { setPresentationMode } from "./isPresentationMode/isPresentationMode.actions"
 
-const appSettings = combineReducers({
-	// Plop: Append reducer usage here
-	mapColors,
-	resetCameraIfNewFileIsLoaded,
-	showOnlyBuildingsWithEdges,
-	whiteColorBuildings,
-	isWhiteBackground,
-	dynamicMargin,
-	invertHeight,
-	invertDeltaColors,
-	invertColorRange,
-	hideFlatBuildings,
-	camera,
-	scaling,
-	edgeHeight,
-	amountOfEdgePreviews,
-	amountOfTopLabels,
-	isPresentationMode
-})
+export function appSettings(state: AppSettings, action: CCAction): AppSettings {
+	switch (action.type) {
+		case AppSettingsActions.SET_APP_SETTINGS:
+			return {
+				mapColors: mapColors(state.mapColors, setMapColors(action.payload.mapColors)),
+				resetCameraIfNewFileIsLoaded: resetCameraIfNewFileIsLoaded(
+					state.resetCameraIfNewFileIsLoaded,
+					setResetCameraIfNewFileIsLoaded(action.payload.resetCameraIfNewFileIsLoaded)
+				),
+				showOnlyBuildingsWithEdges: showOnlyBuildingsWithEdges(
+					state.showOnlyBuildingsWithEdges,
+					setShowOnlyBuildingsWithEdges(action.payload.showOnlyBuildingsWithEdges)
+				),
+				whiteColorBuildings: whiteColorBuildings(
+					state.whiteColorBuildings,
+					setWhiteColorBuildings(action.payload.whiteColorBuildings)
+				),
+				isWhiteBackground: isWhiteBackground(state.isWhiteBackground, setIsWhiteBackground(action.payload.isWhiteBackground)),
+				dynamicMargin: dynamicMargin(state.dynamicMargin, setDynamicMargin(action.payload.dynamicMargin)),
+				invertHeight: invertHeight(state.invertHeight, setInvertHeight(action.payload.invertHeight)),
+				invertDeltaColors: invertDeltaColors(state.invertDeltaColors, setInvertDeltaColors(action.payload.invertDeltaColors)),
+				invertColorRange: invertColorRange(state.invertColorRange, setInvertColorRange(action.payload.invertColorRange)),
+				hideFlatBuildings: hideFlatBuildings(state.hideFlatBuildings, setHideFlatBuildings(action.payload.hideFlatBuildings)),
+				camera: camera(state.camera, setCamera(action.payload.camera)),
+				scaling: scaling(state.scaling, setScaling(action.payload.scaling)),
+				edgeHeight: edgeHeight(state.edgeHeight, setEdgeHeight(action.payload.edgeHeight)),
+				amountOfEdgePreviews: amountOfEdgePreviews(
+					state.amountOfEdgePreviews,
+					setAmountOfEdgePreviews(action.payload.amountOfEdgePreviews)
+				),
+				amountOfTopLabels: amountOfTopLabels(state.amountOfTopLabels, setAmountOfTopLabels(action.payload.amountOfTopLabels)),
+				isPresentationMode: isPresentationMode(state.isPresentationMode, setPresentationMode(action.payload.isPresentationMode))
+			}
+		default:
+			return {
+				mapColors: mapColors(state.mapColors, action),
+				resetCameraIfNewFileIsLoaded: resetCameraIfNewFileIsLoaded(state.resetCameraIfNewFileIsLoaded, action),
+				showOnlyBuildingsWithEdges: showOnlyBuildingsWithEdges(state.showOnlyBuildingsWithEdges, action),
+				whiteColorBuildings: whiteColorBuildings(state.whiteColorBuildings, action),
+				isWhiteBackground: isWhiteBackground(state.isWhiteBackground, action),
+				dynamicMargin: dynamicMargin(state.dynamicMargin, action),
+				invertHeight: invertHeight(state.invertHeight, action),
+				invertDeltaColors: invertDeltaColors(state.invertDeltaColors, action),
+				invertColorRange: invertColorRange(state.invertColorRange, action),
+				hideFlatBuildings: hideFlatBuildings(state.hideFlatBuildings, action),
+				camera: camera(state.camera, action),
+				scaling: scaling(state.scaling, action),
+				edgeHeight: edgeHeight(state.edgeHeight, action),
+				amountOfEdgePreviews: amountOfEdgePreviews(state.amountOfEdgePreviews, action),
+				amountOfTopLabels: amountOfTopLabels(state.amountOfTopLabels, action),
+				isPresentationMode: isPresentationMode(state.isPresentationMode, action)
+			}
+	}
+}
 
 export default appSettings
--- visualization/app/codeCharta/state/store/dynamicSettings/dynamicSettings.reducer.ts
+++ visualization/app/codeCharta/state/store/dynamicSettings/dynamicSettings.reducer.ts
@@ -9,20 +9,48 @@ import { heightMetric } from "./heightMetric/heightMetric.reducer"
 import { distributionMetric } from "./distributionMetric/distributionMetric.reducer"
 import { colorMetric } from "./colorMetric/colorMetric.reducer"
 import { areaMetric } from "./areaMetric/areaMetric.reducer"
-import { combineReducers } from "redux"
+import { DynamicSettingsActions } from "./dynamicSettings.actions"
+import { DynamicSettings, CCAction } from "../../../codeCharta.model"
+import { setEdgeMetric } from "./edgeMetric/edgeMetric.actions"
+import { setColorRange } from "./colorRange/colorRange.actions"
+import { setMargin } from "./margin/margin.actions"
+import { setSearchPattern } from "./searchPattern/searchPattern.actions"
+import { setSearchedNodePaths } from "./searchedNodePaths/searchedNodePaths.actions"
+import { setDistributionMetric } from "./distributionMetric/distributionMetric.actions"
+import { setColorMetric } from "./colorMetric/colorMetric.actions"
+import { setAreaMetric } from "./areaMetric/areaMetric.actions"
+import { setHeightMetric } from "./heightMetric/heightMetric.actions"
+import { focusNode } from "./focusedNodePath/focusedNodePath.actions"
 
-const dynamicSettings = combineReducers({
-	// Plop: Append reducer usage here
-	edgeMetric,
-	colorRange,
-	margin,
-	searchPattern,
-	searchedNodePaths,
-	focusedNodePath,
-	heightMetric,
-	distributionMetric,
-	colorMetric,
-	areaMetric
-})
+export function dynamicSettings(state: DynamicSettings, action: CCAction): DynamicSettings {
+	switch (action.type) {
+		case DynamicSettingsActions.SET_DYNAMIC_SETTINGS:
+			return {
+				edgeMetric: edgeMetric(state.edgeMetric, setEdgeMetric(action.payload.edgeMetric)),
+				colorRange: colorRange(state.colorRange, setColorRange(action.payload.colorRange)),
+				margin: margin(state.margin, setMargin(action.payload.margin)),
+				searchPattern: searchPattern(state.searchPattern, setSearchPattern(action.payload.searchPattern)),
+				searchedNodePaths: searchedNodePaths(state.searchedNodePaths, setSearchedNodePaths(action.payload.searchedNodePaths)),
+				focusedNodePath: focusedNodePath(state.focusedNodePath, focusNode(action.payload.focusedNodePath)),
+				heightMetric: heightMetric(state.heightMetric, setHeightMetric(action.payload.heightMetric)),
+				distributionMetric: distributionMetric(state.distributionMetric, setDistributionMetric(action.payload.distributionMetric)),
+				colorMetric: colorMetric(state.colorMetric, setColorMetric(action.payload.colorMetric)),
+				areaMetric: areaMetric(state.areaMetric, setAreaMetric(action.payload.areaMetric))
+			}
+		default:
+			return {
+				edgeMetric: edgeMetric(state.edgeMetric, action),
+				colorRange: colorRange(state.colorRange, action),
+				margin: margin(state.margin, action),
+				searchPattern: searchPattern(state.searchPattern, action),
+				searchedNodePaths: searchedNodePaths(state.searchedNodePaths, action),
+				focusedNodePath: focusedNodePath(state.focusedNodePath, action),
+				heightMetric: heightMetric(state.heightMetric, action),
+				distributionMetric: distributionMetric(state.distributionMetric, action),
+				colorMetric: colorMetric(state.colorMetric, action),
+				areaMetric: areaMetric(state.areaMetric, action)
+			}
+	}
+}
 
 export default dynamicSettings
