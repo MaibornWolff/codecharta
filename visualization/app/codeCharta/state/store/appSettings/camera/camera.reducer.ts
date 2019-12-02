dcc2ab3c95f3a14792c7cf68742b51b32d3515aa
alschmut <aschmutz1@web.de>
2019-12-02 19:34:23 +0000

    Reducer return current state if payload is not defined #804

--- visualization/app/codeCharta/state/store/appSettings/camera/camera.reducer.ts
+++ visualization/app/codeCharta/state/store/appSettings/camera/camera.reducer.ts
@@ -2,6 +2,10 @@ import { CameraAction, CameraActions } from "./camera.actions"
 import { Vector3 } from "three"
 
 export function camera(state: Vector3 = new Vector3(0, 300, 1000), action: CameraAction): Vector3 {
+	if (action.payload === undefined) {
+		return state
+	}
+
 	switch (action.type) {
 		case CameraActions.SET_CAMERA:
 			return action.payload.clone()
--- visualization/app/codeCharta/state/store/appSettings/scaling/scaling.reducer.ts
+++ visualization/app/codeCharta/state/store/appSettings/scaling/scaling.reducer.ts
@@ -2,6 +2,10 @@ import { ScalingAction, ScalingActions } from "./scaling.actions"
 import { Vector3 } from "three"
 
 export function scaling(state: Vector3 = new Vector3(1, 1, 1), action: ScalingAction): Vector3 {
+	if (action.payload === undefined) {
+		return state
+	}
+
 	switch (action.type) {
 		case ScalingActions.SET_SCALING:
 			return action.payload.clone()
