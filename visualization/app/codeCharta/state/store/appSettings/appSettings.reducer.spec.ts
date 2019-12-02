4c923914fc8fe96bec1380b0f59ab6dfc157dd0e
alschmut <aschmutz1@web.de>
2019-12-02 19:34:38 +0000

    Add test for appSettings reducer #804

---
+++ visualization/app/codeCharta/state/store/appSettings/appSettings.reducer.spec.ts
@@ -0,0 +1,43 @@
+import { AppSettingsAction, setAppSettings } from "./appSettings.actions"
+import appSettings from "./appSettings.reducer"
+import { AppSettings } from "../../../codeCharta.model"
+import { Vector3 } from "three"
+
+describe("appSettings", () => {
+	describe("Default State", () => {
+		it("should initialize the default state", () => {
+			const result = appSettings(undefined, {} as AppSettingsAction)
+
+			expect(result).toEqual({})
+		})
+	})
+
+	describe("Action: SET_APP_SETTINGS", () => {
+		it("should set invertHeight boolean in new appSettings", () => {
+			const partialAppSettings = {
+				invertHeight: true
+			} as AppSettings
+			const result = appSettings({} as AppSettings, setAppSettings(partialAppSettings))
+
+			expect(result.invertHeight).toEqual(true)
+		})
+
+		it("should set edgeHeight number in new appSettings", () => {
+			const partialAppSettings = {
+				edgeHeight: 42
+			} as AppSettings
+			const result = appSettings({} as AppSettings, setAppSettings(partialAppSettings))
+
+			expect(result.edgeHeight).toEqual(42)
+		})
+
+		it("should set camera object in new appSettings", () => {
+			const partialAppSettings = {
+				camera: new Vector3(0, 300, 1000)
+			} as AppSettings
+			const result = appSettings({} as AppSettings, setAppSettings(partialAppSettings))
+
+			expect(result.camera).toEqual(new Vector3(0, 300, 1000))
+		})
+	})
+})
