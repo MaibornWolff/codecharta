aa36cfc3608d8867b7e2d8d0124e83a86dffc258
Ben Willenbring <ben-willenbring@t-online.de>
2019-12-04 17:13:07 +0100

    add default actions for margin #804

--- visualization/app/codeCharta/state/store/dynamicSettings/margin/margin.actions.ts
+++ visualization/app/codeCharta/state/store/dynamicSettings/margin/margin.actions.ts
@@ -11,7 +11,7 @@ export interface SetMarginAction extends CCAction {
 
 export type MarginAction = SetMarginAction
 
-export function setMargin(margin: number): MarginAction {
+export function setMargin(margin: number = null): MarginAction {
 	return {
 		type: MarginActions.SET_MARGIN,
 		payload: margin
