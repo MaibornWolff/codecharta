195b05f3bb2246620229a334717d361762e85f04
alschmut <aschmutz1@web.de>
2019-12-06 13:10:20 +0100

    Remove tests that do not fit the reducers capabilities anymore #804

--- visualization/app/codeCharta/state/store/appSettings/appSettings.reducer.spec.ts
+++ visualization/app/codeCharta/state/store/appSettings/appSettings.reducer.spec.ts
@@ -1,8 +1,6 @@
-import { AppSettingsAction, setAppSettings } from "./appSettings.actions"
+import { AppSettingsAction } from "./appSettings.actions"
 import appSettings from "./appSettings.reducer"
-import { AppSettings } from "../../../codeCharta.model"
-import { Vector3 } from "three"
-import { DEFAULT_SETTINGS, SETTINGS } from "../../../util/dataMocks"
+import { DEFAULT_SETTINGS } from "../../../util/dataMocks"
 
 describe("appSettings", () => {
 	describe("Default State", () => {
@@ -10,41 +8,6 @@ describe("appSettings", () => {
 			const result = appSettings(undefined, {} as AppSettingsAction)
 
 			expect(result).toEqual(DEFAULT_SETTINGS.appSettings)
-		})
-	})
-
-	describe("Action: SET_APP_SETTINGS", () => {
-		it("should set invertHeight boolean in new appSettings", () => {
-			const partialAppSettings = {
-				invertHeight: true
-			}
-			const result = appSettings({} as AppSettings, setAppSettings(partialAppSettings))
-
-			expect(result.invertHeight).toEqual(true)
-		})
-
-		it("should set default appSettings", () => {
-			const result = appSettings(SETTINGS.appSettings, setAppSettings())
-
-			expect(result).toEqual(DEFAULT_SETTINGS.appSettings)
-		})
-
-		it("should set edgeHeight number in new appSettings", () => {
-			const partialAppSettings = {
-				edgeHeight: 42
-			}
-			const result = appSettings({} as AppSettings, setAppSettings(partialAppSettings))
-
-			expect(result.edgeHeight).toEqual(42)
-		})
-
-		it("should set camera object in new appSettings", () => {
-			const partialAppSettings = {
-				camera: new Vector3(0, 300, 1000)
-			}
-			const result = appSettings({} as AppSettings, setAppSettings(partialAppSettings))
-
-			expect(result.camera).toEqual(new Vector3(0, 300, 1000))
 		})
 	})
 })
--- visualization/app/codeCharta/state/store/dynamicSettings/dynamicSettings.reducer.spec.ts
+++ visualization/app/codeCharta/state/store/dynamicSettings/dynamicSettings.reducer.spec.ts
@@ -1,29 +1,11 @@
-import { DynamicSettings } from "../../../codeCharta.model"
-import { DEFAULT_SETTINGS, STATE } from "../../../util/dataMocks"
+import { DEFAULT_SETTINGS } from "../../../util/dataMocks"
 import dynamicSettings from "./dynamicSettings.reducer"
-import { DynamicSettingsAction, setDynamicSettings } from "./dynamicSettings.actions"
+import { DynamicSettingsAction } from "./dynamicSettings.actions"
 
 describe("dynamicSettings", () => {
 	describe("Default State", () => {
 		it("should initialize the default state", () => {
 			const result = dynamicSettings(undefined, {} as DynamicSettingsAction)
-
-			expect(result).toEqual(DEFAULT_SETTINGS.dynamicSettings)
-		})
-	})
-
-	describe("Action: SET_DYNAMIC_SETTINGS", () => {
-		it("should set margin in new dynamicSettings", () => {
-			const partialDynamicSettings = {
-				margin: 42
-			}
-			const result = dynamicSettings({} as DynamicSettings, setDynamicSettings(partialDynamicSettings))
-
-			expect(result.margin).toEqual(42)
-		})
-
-		it("should set default appSettings", () => {
-			const result = dynamicSettings(STATE.dynamicSettings, setDynamicSettings())
 
 			expect(result).toEqual(DEFAULT_SETTINGS.dynamicSettings)
 		})
