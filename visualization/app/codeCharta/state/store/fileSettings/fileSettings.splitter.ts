292e2d38358884f628a985d2f044f7e9199981eb
alschmut <aschmutz1@web.de>
2019-12-06 11:53:37 +0100

    Add fileSettings properties splitter function #804

---
+++ visualization/app/codeCharta/state/store/fileSettings/fileSettings.splitter.ts
@@ -0,0 +1,27 @@
+import { RecursivePartial, CCAction, FileSettings, AttributeTypes } from "../../../codeCharta.model"
+import { splitMarkedPackagesAction } from "./markedPackages/markedPackages.splitter"
+import { splitEdgesAction } from "./edges/edges.splitter"
+import { splitAttributeTypesAction } from "./attributeTypes/attributeTypes.splitter"
+import { splitBlacklistAction } from "./blacklist/blacklist.splitter"
+
+export function splitFileSettingsActions(payload: RecursivePartial<FileSettings>): CCAction[] {
+	const actions: CCAction[] = []
+
+	if (payload.markedPackages !== undefined) {
+		actions.push(splitMarkedPackagesAction(payload.markedPackages))
+	}
+
+	if (payload.edges !== undefined) {
+		actions.push(splitEdgesAction(payload.edges))
+	}
+
+	if (payload.attributeTypes !== undefined) {
+		actions.push(splitAttributeTypesAction(payload.attributeTypes as AttributeTypes))
+	}
+
+	if (payload.blacklist !== undefined) {
+		actions.push(splitBlacklistAction(payload.blacklist))
+	}
+
+	return actions
+}
