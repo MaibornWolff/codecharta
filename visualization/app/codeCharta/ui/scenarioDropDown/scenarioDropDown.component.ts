81ee5541b15107dfbbe08954684118d965bd3210
alschmut <aschmutz1@web.de>
2019-12-08 12:47:40 +0100

    Fill storeService from scenarioDropDown #804

--- visualization/app/codeCharta/ui/scenarioDropDown/scenarioDropDown.component.ts
+++ visualization/app/codeCharta/ui/scenarioDropDown/scenarioDropDown.component.ts
@@ -7,6 +7,8 @@ import { MetricService, MetricServiceSubscriber } from "../../state/metric.servi
 import { MetricData } from "../../codeCharta.model"
 import { IRootScopeService } from "angular"
 import { ThreeOrbitControlsService } from "../codeMap/threeViewer/threeOrbitControlsService"
+import { StoreService } from "../../state/store.service"
+import { setState } from "../../state/store/state.actions"
 
 export class ScenarioDropDownController implements MetricServiceSubscriber {
 	private _viewModel: {
@@ -18,6 +20,7 @@ export class ScenarioDropDownController implements MetricServiceSubscriber {
 	constructor(
 		private $rootScope: IRootScopeService,
 		private settingsService: SettingsService,
+		private storeService: StoreService,
 		private threeOrbitControlsService: ThreeOrbitControlsService
 	) {
 		MetricService.subscribe(this.$rootScope, this)
@@ -30,8 +33,8 @@ export class ScenarioDropDownController implements MetricServiceSubscriber {
 	public onMetricDataRemoved() {}
 
 	public applyScenario(scenarioName: string) {
-		//TODO: fill StoreService
 		this.settingsService.updateSettings(ScenarioHelper.getScenarioSettingsByName(scenarioName))
+		this.storeService.dispatch(setState(ScenarioHelper.getScenarioSettingsByName(scenarioName)))
 		this.threeOrbitControlsService.autoFitTo()
 	}
 }
