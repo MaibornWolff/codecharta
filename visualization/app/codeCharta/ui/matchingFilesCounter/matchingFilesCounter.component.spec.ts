8898cd7ed0cd78e1184f6889fc1268219923a9cf
alschmut <aschmutz1@web.de>
2019-12-12 19:09:59 +0100

    Read from storeService in matchingFilesCounter #804

--- visualization/app/codeCharta/ui/matchingFilesCounter/matchingFilesCounter.component.spec.ts
+++ visualization/app/codeCharta/ui/matchingFilesCounter/matchingFilesCounter.component.spec.ts
@@ -6,7 +6,7 @@ import { CodeMapNode, BlacklistItem, BlacklistType } from "../../codeCharta.mode
 import { CodeMapHelper } from "../../util/codeMapHelper"
 import { IRootScopeService } from "angular"
 import { NodeSearchService } from "../../state/nodeSearch.service"
-import { SettingsService } from "../../state/settingsService/settings.service"
+import { BlacklistService } from "../../state/store/fileSettings/blacklist/blacklist.service"
 
 describe("MatchingFilesCounterController", () => {
 	let matchingFilesCounterController: MatchingFilesCounterController
@@ -27,21 +27,20 @@ describe("MatchingFilesCounterController", () => {
 	}
 
 	describe("constructor", () => {
-		beforeEach(() => {
-			NodeSearchService.subscribe = jest.fn()
-			SettingsService.subscribeToBlacklist = jest.fn()
-		})
-
 		it("should subscribe to NodeSearchService", () => {
+			NodeSearchService.subscribe = jest.fn()
+
 			rebuildController()
 
 			expect(NodeSearchService.subscribe).toHaveBeenCalledWith($rootScope, matchingFilesCounterController)
 		})
 
-		it("should subscribe to Blacklist-Event", () => {
+		it("should subscribe to BlacklistService", () => {
+			BlacklistService.subscribe = jest.fn()
+
 			rebuildController()
 
-			expect(SettingsService.subscribeToBlacklist).toHaveBeenCalledWith($rootScope, matchingFilesCounterController)
+			expect(BlacklistService.subscribe).toHaveBeenCalledWith($rootScope, matchingFilesCounterController)
 		})
 	})
 
