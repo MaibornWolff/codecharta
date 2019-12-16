e5c117b0a26fe45696e43768d6daf68ed9898f8e
alschmut <aschmutz1@web.de>
2019-12-16 21:44:17 +0100

    Abort push if branch name does not match convention and print useful error message

---
+++ visualization/pre-push.sh
@@ -0,0 +1,24 @@
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ $BRANCH_NAME =~ ^[a-z]+\/([0-9]+\/)?[a-z-]+$ ]]
+then
+    exit 0
+fi
+
+echo "ERROR: Wrong branch naming with $BRANCH_NAME"
+
+if [[ $BRANCH_NAME =~ [A-Z] ]]
+then
+    echo "- Do not use uppercase letters and split words by dash."
+fi
+
+if [[ $BRANCH_NAME =~ [_] ]]
+then
+    echo "- Do not use underscores and split words by dash."
+fi
+
+echo "- The branch must be named using following syntax, while the issue number is optional, in case there is no issue"
+echo "  <branch-type>/[<issue-number>/]<branch-name>"
+echo "  Example: feature/123/my-branch-name"
+
+exit 1
