5ee4c29de919ad30eb70fb639ade6e5b98fdd6b4
alschmut <aschmutz1@web.de>
2019-12-20 19:46:05 +0100

    Reset heightMetric in HeightMetricService #804

--- visualization/app/codeCharta/state/store/dynamicSettings/heightMetric/heightMetric.service.ts
+++ visualization/app/codeCharta/state/store/dynamicSettings/heightMetric/heightMetric.service.ts
@@ -1,22 +1,44 @@
 import { StoreService, StoreSubscriber } from "../../../store.service"
 import { IRootScopeService } from "angular"
-import { HeightMetricActions } from "./heightMetric.actions"
+import { HeightMetricActions, setHeightMetric } from "./heightMetric.actions"
 import _ from "lodash"
+import { MetricData } from "../../../../codeCharta.model"
+import { MetricService, MetricServiceSubscriber } from "../../../metric.service"
+import { getResetMetricName, isAnyMetricAvailable } from "../../../../util/metricHelper"
 
 export interface HeightMetricSubscriber {
 	onHeightMetricChanged(heightMetric: string)
 }
 
-export class HeightMetricService implements StoreSubscriber {
+export class HeightMetricService implements StoreSubscriber, MetricServiceSubscriber {
 	private static HEIGHT_METRIC_CHANGED_EVENT = "height-metric-changed"
 
 	constructor(private $rootScope: IRootScopeService, private storeService: StoreService) {
-		StoreService.subscribe($rootScope, this)
+		StoreService.subscribe(this.$rootScope, this)
+		MetricService.subscribe(this.$rootScope, this)
 	}
 
 	public onStoreChanged(actionType: string) {
 		if (_.values(HeightMetricActions).includes(actionType)) {
 			this.notify(this.select())
+		}
+	}
+
+	public onMetricDataAdded(metricData: MetricData[]) {
+		if (isAnyMetricAvailable(metricData)) {
+			this.reset(metricData)
+		}
+	}
+
+	public onMetricDataRemoved() {}
+
+	public reset(metricData: MetricData[]) {
+		const heightMetric = this.storeService.getState().dynamicSettings.heightMetric
+
+		const newHeightMetric = getResetMetricName(metricData, heightMetric, 1)
+
+		if (newHeightMetric) {
+			this.storeService.dispatch(setHeightMetric(newHeightMetric))
 		}
 	}
 
