686508bcb831086f8a4deca135e9e9ad50113325
Alexander Schmutz <aschmutz1@web.de>
2020-01-03 20:35:07 +0100

    Fix relative path in build_gh_pages script

--- script/build_gh_pages.sh
+++ script/build_gh_pages.sh
@@ -13,12 +13,13 @@ analysis/build/install/codecharta-analysis/bin/ccsh sonarimport -o gh-pages/visu
 # new demo file with edges
 mkdir gh-pages/demo_files
 cd gh-pages/demo_files
+CCSH=../../analysis/build/install/codecharta-analysis/bin/ccsh
 
 git log --numstat --raw --topo-order > git.log
-analysis/build/install/codecharta-analysis/bin/ccsh scmlogparser -o codecharta_git.cc.json --input-format GIT_LOG_NUMSTAT_RAW git.log
-analysis/build/install/codecharta-analysis/bin/ccsh modify --setRoot /root/visualization -o codecharta_git_mod.cc.json codecharta_git.cc.json
+$CCSH scmlogparser -o codecharta_git.cc.json --input-format GIT_LOG_NUMSTAT_RAW git.log
+$CCSH modify --setRoot /root/visualization -o codecharta_git_mod.cc.json codecharta_git.cc.json
 
-analysis/build/install/codecharta-analysis/bin/ccsh sonarimport -o codecharta_sonar.cc.json https://sonarcloud.io de.maibornwolff.codecharta:visualization
-analysis/build/install/codecharta-analysis/bin/ccsh modify --setRoot /root/de.maibornwolff.codecharta/visualization -o codecharta_sonar_mod.cc.json codecharta_sonar.cc.json
+$CCSH sonarimport -o codecharta_sonar.cc.json https://sonarcloud.io de.maibornwolff.codecharta:visualization
+$CCSH modify --setRoot /root/de.maibornwolff.codecharta/visualization -o codecharta_sonar_mod.cc.json codecharta_sonar.cc.json
 
-analysis/build/install/codecharta-analysis/bin/ccsh merge -o ../visualization/app/codecharta_with_edges.cc.json -p CodeCharta codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json
+$CCSH merge -o ../visualization/app/codecharta_with_edges.cc.json -p CodeCharta codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json
