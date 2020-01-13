076761c908c8597ebb82e76b09012389a1d893c4
Alex Hunziker <alexhunziker@sunrise.ch>
2020-01-13 23:38:09 +0100

    Create attribute type builder #365

---
+++ analysis/import/SCMLogParser/src/main/kotlin/de/maibornwolff/codecharta/importer/scmlogparser/input/metrics/AttributeTypeBuilder.kt
@@ -0,0 +1,26 @@
+package de.maibornwolff.codecharta.importer.scmlogparser.input.metrics
+
+import de.maibornwolff.codecharta.model.AttributeTypes
+
+object AttributeTypeBuilder {
+
+    fun createNodeAttributeTypes(metricList: List<Metric>): AttributeTypes {
+        val attributeTypes = AttributeTypes(type = "nodes")
+        metricList.forEach { metric ->
+            attributeTypes.add(metric.metricName(), metric.attributeType())
+        }
+        return attributeTypes
+    }
+
+    fun createEdgeAttributeTypes(metricList: List<Metric>): AttributeTypes {
+        val attributeTypes = AttributeTypes(type = "edges")
+        metricList.forEach { metric ->
+            val name = metric.edgeMetricName() ?: return@forEach
+            val type = metric.edgeAttributeType() ?: return@forEach
+            attributeTypes.add(name, type)
+        }
+        return attributeTypes
+    }
+
+
+}
