da2af08f4581e53e3f6e870c48bd3ed9dd2c8bd8
Alex Hunziker <alexhunziker@sunrise.ch>
2020-01-15 12:50:54 +0100

    removed projectName from crococosmoImporter #772

--- analysis/import/CrococosmoImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/crococosmo/CrococosmoConverter.kt
+++ analysis/import/CrococosmoImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/crococosmo/CrococosmoConverter.kt
@@ -1,32 +1,3 @@
-/*
- * Copyright (c) 2018, MaibornWolff GmbH
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *  * Neither the name of  nor the names of its contributors may be used to
- *    endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 package de.maibornwolff.codecharta.importer.crococosmo
 
 import de.maibornwolff.codecharta.importer.crococosmo.model.Graph
@@ -38,9 +9,9 @@ import de.maibornwolff.codecharta.model.Project
 
 class CrococosmoConverter {
 
-    fun convertToProjectsMap(projectName: String, graph: Graph): Map<String, Project> {
+    fun convertToProjectsMap(graph: Graph): Map<String, Project> {
         return graph.schema.versions.versions
-                .associateBy({ createVersionName(it) }, { createProject(projectName, graph, it.id) })
+                .associateBy({ createVersionName(it) }, { createProject(graph, it.id) })
     }
 
     private fun createVersionName(it: SchemaVersion) =
@@ -50,8 +21,8 @@ class CrococosmoConverter {
                 else                     -> it.id
             }
 
-    fun createProject(projectName: String, graph: Graph, version: String = graph.schema.versions.versions.first().id) =
-            Project(projectName, createNodeListForProject(graph.nodes, version))
+    fun createProject(graph: Graph, version: String = graph.schema.versions.versions.first().id) =
+            Project("", createNodeListForProject(graph.nodes, version))
 
     private fun createNodeListForProject(nodes: List<de.maibornwolff.codecharta.importer.crococosmo.model.Node>,
                                          version: String): List<Node> {
@@ -71,7 +42,7 @@ class CrococosmoConverter {
         return when {
             origin.name.isNullOrEmpty() -> convertToNodeList(origin.children.orEmpty(), version)
             else                        -> listOf(
-                    Node(origin.name!!, getNodeType(origin), createAttributeListForNode(origin.versions, version), "",
+                    Node(origin.name, getNodeType(origin), createAttributeListForNode(origin.versions, version), "",
                             convertToNodeList(origin.children.orEmpty(), version)))
         }
     }
--- analysis/import/CrococosmoImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/crococosmo/CrococosmoImporter.kt
+++ analysis/import/CrococosmoImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/crococosmo/CrococosmoImporter.kt
@@ -1,32 +1,3 @@
-/*
- * Copyright (c) 2018, MaibornWolff GmbH
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *  * Neither the name of  nor the names of its contributors may be used to
- *    endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 package de.maibornwolff.codecharta.importer.crococosmo
 
 import de.maibornwolff.codecharta.serialization.ProjectSerializer
@@ -37,7 +8,7 @@ import java.util.concurrent.Callable
 @CommandLine.Command(
         name = "crococosmoimport",
         description = ["generates cc.json from crococosmo xml file"],
-        footer = ["Copyright(c) 2018, MaibornWolff GmbH"]
+        footer = ["Copyright(c) 2020, MaibornWolff GmbH"]
 )
 class CrococosmoImporter: Callable<Void> {
 
@@ -47,16 +18,13 @@ class CrococosmoImporter: Callable<Void> {
     @CommandLine.Option(names = ["-h", "--help"], usageHelp = true, description = ["displays this help and exits"])
     private var help = false
 
-    @CommandLine.Option(names = ["-p", "--projectName"], description = ["project name"])
-    private var projectName = "CrococosmoImporter"
-
     @CommandLine.Option(names = ["-o", "--outputFile"],
             description = ["output File or prefix for File (or empty for stdout)"])
     private var outputFile: String? = null
 
     override fun call(): Void? {
         val graph = CrococosmoDeserializer().deserializeCrococosmoXML(file!!.inputStream())
-        val projects = CrococosmoConverter().convertToProjectsMap(projectName, graph)
+        val projects = CrococosmoConverter().convertToProjectsMap(graph)
         projects.forEach {
             val suffix = if (projects.isNotEmpty()) "_" + it.key else ""
             ProjectSerializer.serializeProject(it.value, writer(suffix))
