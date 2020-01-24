762dcef3697b03ad0815e442c09c3b77672cbdde
Ben Willenbring <ben-willenbring@t-online.de>
2020-01-24 11:42:37 +0100

    Refactor shallow copy instead of deep clone

--- visualization/app/codeCharta/state/store/fileSettings/markedPackages/markedPackages.reducer.ts
+++ visualization/app/codeCharta/state/store/fileSettings/markedPackages/markedPackages.reducer.ts
@@ -1,12 +1,11 @@
 import { MarkedPackagesAction, MarkedPackagesActions, setMarkedPackages } from "./markedPackages.actions"
 import { MarkedPackage } from "../../../../codeCharta.model"
 import { addItemToArray, removeItemFromArray } from "../../../../util/reduxHelper"
-const clone = require("rfdc")()
 
 export function markedPackages(state: MarkedPackage[] = setMarkedPackages().payload, action: MarkedPackagesAction): MarkedPackage[] {
 	switch (action.type) {
 		case MarkedPackagesActions.SET_MARKED_PACKAGES:
-			return clone(action.payload)
+			return [...action.payload]
 		case MarkedPackagesActions.MARK_PACKAGE:
 			return addItemToArray(state, action.payload)
 		case MarkedPackagesActions.UNMARK_PACKAGE:
