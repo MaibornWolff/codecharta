bc3ded775b2077741d49a65beaaddb4f2ff4816d
Ben Willenbring <ben-willenbring@t-online.de>
2020-01-24 09:51:01 +0100

    Revert "Add example standalone component with env switch in module #815"
    This reverts commit 23f8be70642d8e063bcced77f453e7f90fc22213.
    
    # Conflicts:
    #	visualization/app/codeCharta/ui/downloadButton/downloadButton.standalone.component.ts

--- visualization/app/codeCharta/ui/downloadButton/downloadButton.module.ts
+++ visualization/app/codeCharta/ui/downloadButton/downloadButton.module.ts
@@ -2,13 +2,7 @@ import "../../state/state.module"
 import "../dialog/dialog.module"
 import angular from "angular"
 import { downloadButtonComponent } from "./downloadButton.component"
-import { isStandalone } from "../../util/envDetector"
-import { downloadButtonStandaloneComponent } from "./downloadButton.standalone.component"
 
-const module = angular.module("app.codeCharta.ui.downloadButton", ["app.codeCharta.state", "app.codeCharta.ui.dialog"])
-
-if (isStandalone()) {
-	module.component(downloadButtonStandaloneComponent.selector, downloadButtonStandaloneComponent)
-} else {
-	module.component(downloadButtonComponent.selector, downloadButtonComponent)
-}
+angular
+	.module("app.codeCharta.ui.downloadButton", ["app.codeCharta.state", "app.codeCharta.ui.dialog"])
+	.component(downloadButtonComponent.selector, downloadButtonComponent)
