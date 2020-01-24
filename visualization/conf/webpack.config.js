17d849a75ee9da9e57d09682e089809d20abd948
Ben Willenbring <ben-willenbring@t-online.de>
2020-01-24 10:33:11 +0100

    Fix broken import statement #815

--- visualization/conf/webpack.config.js
+++ visualization/conf/webpack.config.js
@@ -1,35 +1,35 @@
-import { isStandalone } from "../app/codeCharta/util/envDetector"
-
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 const dist = path.resolve(__dirname, "../dist/webpack")
 
-module.exports = {
-	mode: "development",
-	target: isStandalone() ? "node" : "web",
-	entry: "./app/app.ts",
-	output: {
-		filename: "bundle.js",
-		path: dist
-	},
-	devServer: {
-		contentBase: dist,
-		compress: true, // enable gzip compression
-		hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
-		port: 3000,
-		clientLogLevel: "error"
-	},
-	module: require("./webpack.loaders.js"),
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: "./app/index.html"
-		})
-	],
-	devtool: "source-map",
-	resolve: {
-		extensions: [".ts", ".tsx", ".js"]
+module.exports = env => {
+	return {
+		mode: "development",
+		target: JSON.parse(env.STANDALONE) ? "node" : "web",
+		entry: "./app/app.ts",
+		output: {
+			filename: "bundle.js",
+			path: dist
+		},
+		devServer: {
+			contentBase: dist,
+			compress: true, // enable gzip compression
+			hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+			port: 3000,
+			clientLogLevel: "error"
+		},
+		module: require("./webpack.loaders.js"),
+		plugins: [
+			new CleanWebpackPlugin(),
+			new HtmlWebpackPlugin({
+				template: "./app/index.html"
+			})
+		],
+		devtool: "source-map",
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"]
+		}
 	}
 }
