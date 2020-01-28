a19a0afa089122b84a81b417c6c7544c28253d2e
Alex Hunziker <alexhunziker@sunrise.ch>
2020-01-28 09:15:49 +0100

    value argument of addMetric now accepts type Number #660

--- analysis/parser/RawTextParser/src/main/kotlin/de/maibornwolff/codecharta/parser/rawtextparser/model/FileMetrics.kt
+++ analysis/parser/RawTextParser/src/main/kotlin/de/maibornwolff/codecharta/parser/rawtextparser/model/FileMetrics.kt
@@ -3,7 +3,7 @@ package de.maibornwolff.codecharta.parser.rawtextparser.model
 class FileMetrics {
     val metricMap = mutableMapOf<String, Double>()
 
-    fun addMetric(name: String, value: Int): FileMetrics {
+    fun addMetric(name: String, value: Number): FileMetrics {
         metricMap[name] = value.toDouble()
         return this
     }
