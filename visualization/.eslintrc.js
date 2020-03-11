56f6b7f4777ad9d880a8c17d2d8fd7ddd12fb32b
Ben Willenbring <ben-willenbring@t-online.de>
2020-03-11 18:40:27 +0100

    Add eslint prettier config again to override rules #826

--- visualization/.eslintrc.js
+++ visualization/.eslintrc.js
@@ -1,60 +1,61 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": "tsconfig.json",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "@typescript-eslint/class-name-casing": "error",
-        "@typescript-eslint/explicit-member-accessibility": [
-            "error",
-            {
-                "accessibility": "explicit",
-                "overrides": {
-                    "constructors": "no-public"
-                }
-            }
-        ],
-        "@typescript-eslint/triple-slash-reference": "error",
-        "curly": "error",
-        "new-parens": "error",
-        "no-caller": "error",
-        "no-cond-assign": "error",
-        "no-console": [
-            "warn",
-            {
-                "allow": [
-                    "warn",
-                    "dir",
-                    "timeLog",
-                    "assert",
-                    "clear",
-                    "count",
-                    "countReset",
-                    "group",
-                    "groupEnd",
-                    "table",
-                    "dirxml",
-                    "error",
-                    "groupCollapsed",
-                    "Console",
-                    "profile",
-                    "profileEnd",
-                    "timeStamp",
-                    "context"
-                ]
-            }
-        ],
-        "no-empty": "error",
-        "no-eval": "error",
-        "no-var": "error",
-    }
-};
+	"env": {
+		"browser": true,
+		"es6": true,
+		"node": true
+	},
+	"extends": ["prettier"], // prettier must be last in array to override other configs
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"project": "tsconfig.json",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"rules": {
+		"@typescript-eslint/class-name-casing": "error",
+		"@typescript-eslint/explicit-member-accessibility": [
+			"error",
+			{
+				"accessibility": "explicit",
+				"overrides": {
+					"constructors": "no-public"
+				}
+			}
+		],
+		"@typescript-eslint/triple-slash-reference": "error",
+		"curly": "error",
+		"new-parens": "error",
+		"no-caller": "error",
+		"no-cond-assign": "error",
+		"no-console": [
+			"warn",
+			{
+				"allow": [
+					"warn",
+					"dir",
+					"timeLog",
+					"assert",
+					"clear",
+					"count",
+					"countReset",
+					"group",
+					"groupEnd",
+					"table",
+					"dirxml",
+					"error",
+					"groupCollapsed",
+					"Console",
+					"profile",
+					"profileEnd",
+					"timeStamp",
+					"context"
+				]
+			}
+		],
+		"no-empty": "error",
+		"no-eval": "error",
+		"no-var": "error"
+	}
+}
