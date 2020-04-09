4e94ffe52dd65f9a7997b7c4ce8158df2f21db2c
RomanenkoVladimir <volodymyr.romanenko@icloud.com>
2020-04-09 15:57:11 +0200

    Added new validator(jsonscheme), Added function to validate current API Version

--- visualization/app/codeCharta/util/fileValidator.ts
+++ visualization/app/codeCharta/util/fileValidator.ts
@@ -1,6 +1,40 @@
 import { CodeMapNode } from "../codeCharta.model"
+import * as codeCharta from "../../../package.json"
 
 export class FileValidator {
+	public static validate(file: { nodes: CodeMapNode[] }): Array<{ message: string; dataPath: string }> {
+		if (!file) {
+			return [{ message: "file is empty or invalid", dataPath: "empty or invalid file" }]
+		}
+
+		let Validator = require("jsonschema").Validator
+		let valid = new Validator()
+		let validationResult = valid.validate(file, require("./schema.json"))
+		console.log(validationResult.errors)
+
+		this.checkApiVersion(file)
+
+		if (validationResult.errors.length !== 0) {
+			return [
+				{
+					message: validationResult.errors,
+					dataPath: ""
+				}
+			]
+		}
+
+		if (!FileValidator.hasUniqueChildren(file.nodes[0])) {
+			return [
+				{
+					message: "names or ids are not unique",
+					dataPath: "uniqueness"
+				}
+			]
+		}
+
+		return validationResult.errors
+	}
+
 	private static hasUniqueChildren(node: CodeMapNode) {
 		if (!node.children || node.children.length === 0) {
 			return true
@@ -22,23 +56,45 @@ export class FileValidator {
 		return true
 	}
 
-	public static validate(file: { nodes: CodeMapNode[] }): Array<{ message: string; dataPath: string }> {
-		if (!file) {
-			return [{ message: "file is empty or invalid", dataPath: "empty or invalid file" }]
-		}
-		let ajv = require("ajv")()
-		let compare = ajv.compile(require("./schema.json"))
-		let valid = compare(file)
+	private static checkApiVersion(file: { nodes: CodeMapNode[] }) {
+		let apiVersion = this.getCodeChartaApiVersion()
+		let fileApiVersion = this.getFileApiVersion(file)
 
-		if (!FileValidator.hasUniqueChildren(file.nodes[0])) {
-			return [
-				{
-					message: "names or ids are not unique",
-					dataPath: "uniqueness"
+		let apiVersionMajor = apiVersion.split(".")[0]
+		let apiVersionMinor = apiVersion.split(".")[1]
+
+		let majorVersion = fileApiVersion.split(".")[0]
+		let minorVersion = fileApiVersion.split(".")[1]
+
+		if (majorVersion > apiVersionMajor) {
+			console.log("Major Version Wrong")
+			return
+		}
+		if (minorVersion > apiVersionMinor) {
+			console.log("Minor Version Wrong")
+			return
+		}
+	}
+
+	private static getCodeChartaApiVersion() {
+		let schemaCodeCharta = codeCharta
+		let schemaApiVersion
+
+		for (let key in schemaCodeCharta) {
+			if (key === "codecharta") {
+				schemaApiVersion = schemaCodeCharta[key]
+				for (let apiVersionKey in schemaApiVersion) {
+					return schemaApiVersion[apiVersionKey]
 				}
-			]
+			}
 		}
+	}
 
-		return valid ? [] : compare.errors
+	private static getFileApiVersion(file: { nodes: CodeMapNode[] }) {
+		for (let key in file) {
+			if (key === "apiVersion") {
+				return file[key]
+			}
+		}
 	}
 }
