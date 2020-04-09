2503bf80caa295eb5b78c53a94d6676fc6de39ca
alschmut <aschmutz1@web.de>
2020-04-09 16:17:14 +0200

    Add tests for layoutSelection.component #904

--- visualization/app/codeCharta/ui/layoutSelection/layoutSelection.component.spec.ts
+++ visualization/app/codeCharta/ui/layoutSelection/layoutSelection.component.spec.ts
@@ -1,9 +1,16 @@
 import "./layoutSelection.module"
 import { LayoutSelectionController } from "./layoutSelection.component"
-import { instantiateModule } from "../../../../mocks/ng.mockhelper"
+import { getService, instantiateModule } from "../../../../mocks/ng.mockhelper"
+import { IRootScopeService } from "angular"
+import { StoreService } from "../../state/store.service"
+import { LayoutAlgorithmService } from "../../state/store/appSettings/layoutAlgorithm/layoutAlgorithm.service"
+import _ from "lodash"
+import { LayoutAlgorithm } from "../../codeCharta.model"
 
 describe("LayoutSelectionController", () => {
 	let layoutSelectionController: LayoutSelectionController
+	let $rootScope: IRootScopeService
+	let storeService: StoreService
 
 	beforeEach(() => {
 		restartSystem()
@@ -12,13 +19,48 @@ describe("LayoutSelectionController", () => {
 
 	function restartSystem() {
 		instantiateModule("app.codeCharta.ui.layoutSelection")
+
+		$rootScope = getService<IRootScopeService>("$rootScope")
+		storeService = getService<StoreService>("$rootScope")
 	}
 
 	function rebuildController() {
-		layoutSelectionController = new LayoutSelectionController()
+		layoutSelectionController = new LayoutSelectionController($rootScope, storeService)
 	}
 
-	describe("someMethodName", () => {
-		it("should do something", () => {})
+	describe("constructor", () => {
+		it("should subscribe to LayoutAlgorithmService Events", () => {
+			LayoutAlgorithmService.subscribe = jest.fn()
+
+			rebuildController()
+
+			expect(LayoutAlgorithmService.subscribe).toHaveBeenCalledWith($rootScope, layoutSelectionController)
+		})
+
+		it("should initialise viewModel LayoutAlgorithms", () => {
+			rebuildController()
+
+			expect(layoutSelectionController["_viewModel"].layoutAlgorithms).toEqual(_.values(LayoutAlgorithm))
+		})
+	})
+
+	describe("onLayoutAlgorithmChanged", () => {
+		it("should update viewModel", () => {
+			layoutSelectionController["_viewModel"].layoutAlgorithm = null
+
+			layoutSelectionController.onLayoutAlgorithmChanged(LayoutAlgorithm.StreetMap)
+
+			expect(layoutSelectionController["_viewModel"].layoutAlgorithm).toEqual(LayoutAlgorithm.StreetMap)
+		})
+	})
+
+	describe("applyLayoutAlgorithm", () => {
+		it("should set new layoutAlgorithm into redux store", () => {
+			layoutSelectionController["_viewModel"].layoutAlgorithm = LayoutAlgorithm.StreetMap
+
+			layoutSelectionController.applyLayoutAlgorithm()
+
+			expect(storeService.getState().appSettings.layoutAlgorithm).toEqual(LayoutAlgorithm.StreetMap)
+		})
 	})
 })
