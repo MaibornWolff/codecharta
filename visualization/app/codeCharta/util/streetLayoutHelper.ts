155e9c40168fc52b71696e5ae7cebc7635057f7e
lurichte <xp3112@aol.com>
2020-04-09 12:58:07 +0200

    Add check if node has children

--- visualization/app/codeCharta/util/streetLayoutHelper.ts
+++ visualization/app/codeCharta/util/streetLayoutHelper.ts
@@ -2,15 +2,13 @@ import { CodeMapNode } from "../codeCharta.model"
 
 export class StreetLayoutHelper {
 	public static calculateSize(node: CodeMapNode, metricName: string) {
-		let totalSize = 0
+		let totalSize = node.attributes[metricName] || 0
 
-		if (node.children && node.children.length > 0) {
+		if (totalSize === 0 && node.children && node.children.length > 0) {
 			for (const child of node.children) {
 				totalSize += StreetLayoutHelper.calculateSize(child, metricName)
 			}
-			return totalSize
-		} else {
-			return node.attributes[metricName] || 0
 		}
+		return totalSize
 	}
 }
