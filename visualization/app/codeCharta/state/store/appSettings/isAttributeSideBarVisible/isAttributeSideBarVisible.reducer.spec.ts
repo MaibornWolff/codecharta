56a93c371998b985a5d681690e8e198abcbe6df7
Ben Willenbring <ben-willenbring@t-online.de>
2020-04-15 19:27:13 +0200

    Refactor use toBeTruthy and toBeFalsy

--- visualization/app/codeCharta/state/store/appSettings/isAttributeSideBarVisible/isAttributeSideBarVisible.reducer.spec.ts
+++ visualization/app/codeCharta/state/store/appSettings/isAttributeSideBarVisible/isAttributeSideBarVisible.reducer.spec.ts
@@ -11,7 +11,7 @@ describe("isAttributeSideBarVisible", () => {
 		it("should initialize the default state", () => {
 			const result = isAttributeSideBarVisible(undefined, {} as IsAttributeSideBarVisibleAction)
 
-			expect(result).toEqual(false)
+			expect(result).toBeFalsy()
 		})
 	})
 
@@ -19,13 +19,13 @@ describe("isAttributeSideBarVisible", () => {
 		it("should set new isAttributeSideBarVisible", () => {
 			const result = isAttributeSideBarVisible(false, setIsAttributeSideBarVisible(true))
 
-			expect(result).toEqual(true)
+			expect(result).toBeTruthy()
 		})
 
 		it("should set default isAttributeSideBarVisible", () => {
 			const result = isAttributeSideBarVisible(true, setIsAttributeSideBarVisible())
 
-			expect(result).toEqual(false)
+			expect(result).toBeFalsy()
 		})
 	})
 
@@ -33,7 +33,7 @@ describe("isAttributeSideBarVisible", () => {
 		it("should set new isAttributeSideBarVisible", () => {
 			const result = isAttributeSideBarVisible(false, openAttributeSideBar())
 
-			expect(result).toEqual(true)
+			expect(result).toBeTruthy()
 		})
 	})
 
@@ -41,7 +41,7 @@ describe("isAttributeSideBarVisible", () => {
 		it("should set default isAttributeSideBarVisible", () => {
 			const result = isAttributeSideBarVisible(true, closeAttributeSideBar())
 
-			expect(result).toEqual(false)
+			expect(result).toBeFalsy()
 		})
 	})
 })
