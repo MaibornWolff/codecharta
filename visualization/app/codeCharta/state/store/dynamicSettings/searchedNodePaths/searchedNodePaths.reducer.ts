eb9ffec26040af71256fbd80d5ff2fd73d6952c0
Ben Willenbring <ben-willenbring@t-online.de>
2020-04-17 09:36:41 +0200

    Refactor clone set to destroy reference #837

--- visualization/app/codeCharta/state/store/dynamicSettings/searchedNodePaths/searchedNodePaths.reducer.ts
+++ visualization/app/codeCharta/state/store/dynamicSettings/searchedNodePaths/searchedNodePaths.reducer.ts
@@ -3,7 +3,7 @@ import { SearchedNodePathsAction, SearchedNodePathsActions, setSearchedNodePaths
 export function searchedNodePaths(state: Set<string> = setSearchedNodePaths().payload, action: SearchedNodePathsAction): Set<string> {
 	switch (action.type) {
 		case SearchedNodePathsActions.SET_SEARCHED_NODE_PATHS:
-			return action.payload
+			return new Set(Array.from(action.payload))
 		default:
 			return state
 	}
