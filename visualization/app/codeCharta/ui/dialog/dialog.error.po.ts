17b86bc7148e9c0e403282074ca45592f8e8a09d
RomanenkoVladimir <volodymyr.romanenko@icloud.com>
2020-04-22 14:09:46 +0200

    Added Iframe wait

--- visualization/app/codeCharta/ui/dialog/dialog.error.po.ts
+++ visualization/app/codeCharta/ui/dialog/dialog.error.po.ts
@@ -4,8 +4,11 @@ export class DialogErrorPageObject {
 	constructor(private page: Page) {}
 
 	public async getMessage() {
-		await this.page.waitForSelector(".md-dialog-content-body p")
-		return await this.page.$eval(".md-dialog-content-body p", el => el["innerText"])
+		await this.page.waitForSelector("iframe")
+		const iframeElement = await this.page.$("iframe")
+		const frame = await iframeElement.contentFrame()
+		await frame.waitForSelector(".md-dialog-content-body p")
+		return await frame.$eval(".md-dialog-content-body p", el => el["innerText"])
 	}
 
 	public async clickOk() {
