aa7f7840380f6dbbc8f88a8be9b156a85acaeb98
Ben Willenbring <ben-willenbring@t-online.de>
2020-04-24 22:01:08 +0200

    Fix no-unused-var linting errors

--- visualization/app/codeCharta/ui/loadingGif/loadingGif.module.ts
+++ visualization/app/codeCharta/ui/loadingGif/loadingGif.module.ts
@@ -1,7 +1,6 @@
 import "../../state/state.module"
 import angular from "angular"
 import { loadingGifFileComponent, loadingGifMapComponent } from "./loadingGif.component"
-import _ from "lodash"
 
 angular
 	.module("app.codeCharta.ui.loadingGif", ["app.codeCharta.state"])
