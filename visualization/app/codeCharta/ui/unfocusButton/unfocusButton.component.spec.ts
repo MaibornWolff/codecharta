2c8e7873074f90f17f9b53f9a55652d67deed4f0
Ben Willenbring <ben-willenbring@t-online.de>
2020-05-01 02:49:56 +0200

    Update move unfocus-button to the node-context-menu #948

--- visualization/app/codeCharta/ui/unfocusButton/unfocusButton.component.spec.ts
+++ visualization/app/codeCharta/ui/unfocusButton/unfocusButton.component.spec.ts
@@ -1,10 +1,13 @@
 import "./unfocusButton.module"
 import { UnfocusButtonController } from "./unfocusButton.component"
 import { getService, instantiateModule } from "../../../../mocks/ng.mockhelper"
-import { FocusedNodePathService } from "../../state/store/dynamicSettings/focusedNodePath/focusedNodePath.service"
 import { IRootScopeService } from "angular"
 import { StoreService } from "../../state/store.service"
 import { focusNode } from "../../state/store/dynamicSettings/focusedNodePath/focusedNodePath.actions"
+import { CODE_MAP_BUILDING, TEST_DELTA_MAP_A } from "../../util/dataMocks"
+import { setIdToNode } from "../../state/store/lookUp/idToNode/idToNode.actions"
+import { CodeMapMouseEventService } from "../codeMap/codeMap.mouseEvent.service"
+import { NodeDecorator } from "../../util/nodeDecorator"
 
 describe("UnfocusButtonController", () => {
 	let unfocusButtonController: UnfocusButtonController
@@ -21,6 +24,13 @@ describe("UnfocusButtonController", () => {
 
 		$rootScope = getService<IRootScopeService>("$rootScope")
 		storeService = getService<StoreService>("storeService")
+
+		NodeDecorator.preDecorateFile(TEST_DELTA_MAP_A)
+		const map = new Map([
+			[TEST_DELTA_MAP_A.map.children[0].id, TEST_DELTA_MAP_A.map.children[0]],
+			[TEST_DELTA_MAP_A.map.children[1].id, TEST_DELTA_MAP_A.map.children[1]]
+		])
+		storeService.dispatch(setIdToNode(map))
 	}
 
 	function rebuildController() {
@@ -28,26 +38,32 @@ describe("UnfocusButtonController", () => {
 	}
 
 	describe("constructor", () => {
-		it("should subscribe to FocusedNodePathService focus", () => {
-			FocusedNodePathService.subscribeToFocusNode = jest.fn()
+		it("should subscribe to building-right-clicked-event", () => {
+			CodeMapMouseEventService.subscribeToBuildingRightClickedEvents = jest.fn()
 
 			rebuildController()
 
-			expect(FocusedNodePathService.subscribeToFocusNode).toHaveBeenCalledWith($rootScope, unfocusButtonController)
+			expect(CodeMapMouseEventService.subscribeToBuildingRightClickedEvents).toHaveBeenCalledWith($rootScope, unfocusButtonController)
 		})
 	})
 
-	describe("onFocusNode", () => {
-		it("should set focusedNodePath in viewModel", () => {
-			unfocusButtonController.onFocusNode("/root")
+	describe("onBuildingRightClicked", () => {
+		it("should not show the unfocus button when the focusedNodePath equals the right-clicked-buildings path", () => {
+			storeService.dispatch(focusNode(TEST_DELTA_MAP_A.map.children[0].path))
+			CODE_MAP_BUILDING.node.id = TEST_DELTA_MAP_A.map.children[0].id
+			CODE_MAP_BUILDING.node.path = TEST_DELTA_MAP_A.map.children[0].path
+
+			unfocusButtonController.onBuildingRightClicked(CODE_MAP_BUILDING, 0, 0)
 
 			expect(unfocusButtonController["_viewModel"].isNodeFocused).toBeTruthy()
 		})
-	})
 
-	describe("onUnfocusNode", () => {
-		it("should set empty focusedNodePath in viewModel", () => {
-			unfocusButtonController.onUnfocusNode()
+		it("should show the unfocus button when the focusedNodePath does not equal the right-clicked-buildings path", () => {
+			storeService.dispatch(focusNode(TEST_DELTA_MAP_A.map.children[0].path))
+			CODE_MAP_BUILDING.node.id = TEST_DELTA_MAP_A.map.children[1].id
+			CODE_MAP_BUILDING.node.path = TEST_DELTA_MAP_A.map.children[1].path
+
+			unfocusButtonController.onBuildingRightClicked(CODE_MAP_BUILDING, 0, 0)
 
 			expect(unfocusButtonController["_viewModel"].isNodeFocused).toBeFalsy()
 		})
--- visualization/app/codeCharta/ui/unfocusButton/unfocusButton.component.ts
+++ visualization/app/codeCharta/ui/unfocusButton/unfocusButton.component.ts
@@ -1,14 +1,11 @@
 import "./unfocusButton.component.scss"
-import {
-	FocusedNodePathService,
-	FocusNodeSubscriber,
-	UnfocusNodeSubscriber
-} from "../../state/store/dynamicSettings/focusedNodePath/focusedNodePath.service"
 import { IRootScopeService } from "angular"
 import { unfocusNode } from "../../state/store/dynamicSettings/focusedNodePath/focusedNodePath.actions"
 import { StoreService } from "../../state/store.service"
+import { BuildingRightClickedEventSubscriber, CodeMapMouseEventService } from "../codeMap/codeMap.mouseEvent.service"
+import { CodeMapBuilding } from "../codeMap/rendering/codeMapBuilding"
 
-export class UnfocusButtonController implements FocusNodeSubscriber, UnfocusNodeSubscriber {
+export class UnfocusButtonController implements BuildingRightClickedEventSubscriber {
 	private _viewModel: {
 		isNodeFocused: boolean
 	} = {
@@ -17,16 +14,12 @@ export class UnfocusButtonController implements FocusNodeSubscriber, UnfocusNode
 
 	/* @ngInject */
 	constructor(private $rootScope: IRootScopeService, private storeService: StoreService) {
-		FocusedNodePathService.subscribeToFocusNode(this.$rootScope, this)
-		FocusedNodePathService.subscribeToUnfocusNode(this.$rootScope, this)
+		CodeMapMouseEventService.subscribeToBuildingRightClickedEvents(this.$rootScope, this)
 	}
 
-	public onFocusNode(focusedNodePath: string) {
-		this._viewModel.isNodeFocused = true
-	}
-
-	public onUnfocusNode() {
-		this._viewModel.isNodeFocused = false
+	public onBuildingRightClicked(building: CodeMapBuilding, x: number, y: number) {
+		const codeMapNode = this.storeService.getState().lookUp.idToNode.get(building.node.id)
+		this._viewModel.isNodeFocused = codeMapNode.path === this.storeService.getState().dynamicSettings.focusedNodePath
 	}
 
 	public removeFocusedNode() {
