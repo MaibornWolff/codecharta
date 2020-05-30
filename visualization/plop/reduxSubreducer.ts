6720db9e2c06465ccc0f06c8a50229a50dd6666f
Alexander Schmutz <aschmutz1@web.de>
2020-05-30 03:35:55 +0200

    Tech/rewrite plop in typescript (#868)
    * Refactor plop generator to use typescript and extract 5 generator/helper files
    
    * Use typescript plop module declaration
    
    * WIP: Add plop:build npm script and rename script g to plop
    
    * Upgrade plop and fix execution
    
    * Compile plop files with tsc and move into dist folder for execution
    
    * Fix using wrong property to modify file by path and use templates outside the dist folder
    
    * Fix generating state service and refactor path usage
    
    Co-authored-by: Ben Willenbring <ben-willenbring@t-online.de>

---
+++ visualization/plop/reduxSubreducer.ts
@@ -0,0 +1,55 @@
+import { createFileAction, createInputPromt, modifyFileAction } from "./plopHelper"
+
+const TEMPLATE_DIR: string = "reduxSubreducer"
+const DESTINATION_DIR: string = "state/store/{{camelCase name}}"
+
+export const PLOP_REDUX_SUBREDUCER_VARIABLE_PROMPTS = [createInputPromt("name", "Name (e.x. dynamicSettings):")]
+
+export const PLOP_REDUX_SUBREDUCER_FILE_ACTIONS = [
+	createFileAction(TEMPLATE_DIR, DESTINATION_DIR, ["actions", "ts"]),
+	createFileAction(TEMPLATE_DIR, DESTINATION_DIR, ["reducer", "spec", "ts"]),
+	createFileAction(TEMPLATE_DIR, DESTINATION_DIR, ["reducer", "ts"]),
+	createFileAction(TEMPLATE_DIR, DESTINATION_DIR, ["splitter", "ts"]),
+	modifyFileAction({
+		path: "state/store/state.reducer.ts",
+		pattern: /(\/\/ Plop: Import sub-reducer here)/gi,
+		template: '$1\r\nimport {{camelCase name}} from "./{{camelCase name}}/{{camelCase name}}.reducer"'
+	}),
+	modifyFileAction({
+		path: "state/store/state.reducer.ts",
+		pattern: /(\/\/ Plop: Append sub-reducer here)/gi,
+		template: "$1\r\n\t{{camelCase name}},"
+	}),
+	modifyFileAction({
+		path: "state/store/state.actions.ts",
+		pattern: /(\/\/ Plop: Import sub-reducer here)/gi,
+		template: '$1\r\nimport { default{{properCase name}} } from "./{{camelCase name}}/{{camelCase name}}.actions"'
+	}),
+	modifyFileAction({
+		path: "state/store/state.actions.ts",
+		pattern: /(\/\/ Plop: Append sub-reducer here)/gi,
+		template: "$1\r\n\t{{camelCase name}}: default{{properCase name}},"
+	}),
+	modifyFileAction({
+		path: "state/store/state.splitter.ts",
+		pattern: /(\/\/ Plop: Import sub-reducer action here)/gi,
+		template: '$1\r\nimport { {{properCase name}}Actions } from "./{{camelCase name}}/{{camelCase name}}.actions"'
+	}),
+	modifyFileAction({
+		path: "state/store/state.splitter.ts",
+		pattern: /(\/\/ Plop: Import sub-reducer splitter here)/gi,
+		template: '$1\r\nimport { split{{properCase name}}Actions } from "./{{camelCase name}}/{{camelCase name}}.splitter"'
+	}),
+	modifyFileAction({
+		path: "state/store/state.splitter.ts",
+		pattern: /(\/\/ Plop: Propagate sub-reducer here)/gi,
+		template:
+			"$1\r\n\tif (_.values({{properCase name}}Actions).includes(action.type)) {\n\t\treturn split{{properCase name}}Actions(action.payload)\n\t}\n"
+	}),
+	modifyFileAction({
+		path: "state/store/state.splitter.ts",
+		pattern: /(\/\/ Plop: Split into sub-reducer here)/gi,
+		template:
+			"$1\r\n\t\tif (action.payload.{{camelCase name}} !== undefined) {\n\t\t\tactions = actions.concat(...split{{properCase name}}Actions(action.payload.{{camelCase name}}))\n\t\t}\n"
+	})
+]
