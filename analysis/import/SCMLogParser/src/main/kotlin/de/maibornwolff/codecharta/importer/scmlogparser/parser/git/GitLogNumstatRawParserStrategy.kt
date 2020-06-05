c411771ceed315207615f00e15787c90adbda86b
RomanenkoVladimir <volodymyr.romanenko@icloud.com>
2020-06-05 13:37:30 +0200

    Add a Kotlin Linter and Fix the project according to the rules

--- analysis/import/SCMLogParser/src/main/kotlin/de/maibornwolff/codecharta/importer/scmlogparser/parser/git/GitLogNumstatRawParserStrategy.kt
+++ analysis/import/SCMLogParser/src/main/kotlin/de/maibornwolff/codecharta/importer/scmlogparser/parser/git/GitLogNumstatRawParserStrategy.kt
@@ -11,8 +11,7 @@ import java.util.stream.Collector
 import java.util.stream.Stream
 import kotlin.streams.toList
 
-class GitLogNumstatRawParserStrategy: LogParserStrategy {
-
+class GitLogNumstatRawParserStrategy : LogParserStrategy {
     override fun creationCommand(): String {
         return "git log --numstat --raw --topo-order"
     }
@@ -23,41 +22,38 @@ class GitLogNumstatRawParserStrategy: LogParserStrategy {
 
     override fun parseAuthor(commitLines: List<String>): String {
         return commitLines
-                .parallelStream()
-                .filter { commitLine -> commitLine.startsWith(AUTHOR_ROW_INDICATOR) }
-                .map { AuthorParser.parseAuthor(it) }
-                .toList()
-                .first()
+            .parallelStream()
+            .filter { commitLine -> commitLine.startsWith(AUTHOR_ROW_INDICATOR) }
+            .map { AuthorParser.parseAuthor(it) }
+            .toList()
+            .first()
     }
 
     override fun parseModifications(commitLines: List<String>): List<Modification> {
-
         return commitLines
-                .filter { isFileLine(it) }
-                .map { parseModification(it) }
-                .groupingBy { it.filename }
-                .aggregate { _, aggregatedModification: Modification?, currentModification, _ ->
-                    when (aggregatedModification) {
-                        null -> mergeModifications(currentModification)
-                        else -> mergeModifications(aggregatedModification, currentModification)
-                    }
+            .filter { isFileLine(it) }
+            .map { parseModification(it) }
+            .groupingBy { it.filename }
+            .aggregate { _, aggregatedModification: Modification?, currentModification, _ ->
+                when (aggregatedModification) {
+                    null -> mergeModifications(currentModification)
+                    else -> mergeModifications(aggregatedModification, currentModification)
                 }
-                .values
-                .filterNotNull()
-                .toList()
+            }
+            .values
+            .filterNotNull()
+            .toList()
     }
 
     override fun parseDate(commitLines: List<String>): OffsetDateTime {
         return commitLines
-                .filter { commitLine -> commitLine.startsWith(DATE_ROW_INDICATOR) }
-                .map { CommitDateParser.parseCommitDate(it) }
-                .first()
+            .filter { commitLine -> commitLine.startsWith(DATE_ROW_INDICATOR) }
+            .map { CommitDateParser.parseCommitDate(it) }
+            .first()
     }
 
     companion object {
-
         private val GIT_COMMIT_SEPARATOR_TEST = Predicate<String> { logLine -> logLine.startsWith("commit") }
-
         private fun isFileLine(commitLine: String): Boolean {
             return GitLogRawParserStrategy.isFileLine(commitLine) || GitLogNumstatParserStrategy.isFileLine(commitLine)
         }
@@ -73,7 +69,7 @@ class GitLogNumstatRawParserStrategy: LogParserStrategy {
             val additions = a.map { it.additions }.sum()
             val deletions = a.map { it.deletions }.sum()
             val type =
-                    a.map { it.type }.firstOrNull { t -> t != Modification.Type.UNKNOWN } ?: Modification.Type.UNKNOWN
+                a.map { it.type }.firstOrNull { t -> t != Modification.Type.UNKNOWN } ?: Modification.Type.UNKNOWN
 
             if (type == Modification.Type.RENAME) {
                 val oldFilename = a.map { it.oldFilename }.firstOrNull { s -> s.isNotEmpty() } ?: ""
