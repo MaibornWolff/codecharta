ab15a94702e9fb787318e43281e1cbd87c2f08ef
RomanenkoVladimir <volodymyr.romanenko@icloud.com>
2020-06-12 13:35:08 +0200

    Improve Performance Commit Collector, Remove double call to find the versionControlled File

--- analysis/import/SCMLogParser/src/main/kotlin/de/maibornwolff/codecharta/importer/scmlogparser/parser/CommitCollector.kt
+++ analysis/import/SCMLogParser/src/main/kotlin/de/maibornwolff/codecharta/importer/scmlogparser/parser/CommitCollector.kt
@@ -10,43 +10,40 @@ import java.util.function.Supplier
 import java.util.stream.Collector
 
 internal class CommitCollector private constructor(private val metricsFactory: MetricsFactory) {
-
     private fun collectCommit(versionControlledFiles: MutableList<VersionControlledFile>, commit: Commit) {
         if (commit.isEmpty) {
             return
         }
-        commit.filenames
-            .filter { findVersionControlledFileByFilename(versionControlledFiles, it) == null }
-            .forEach {
-                val missingVersionControlledFile = VersionControlledFile(it, metricsFactory)
+
+        for (fileName in commit.filenames) {
+            var versionControlledFile: VersionControlledFile? =
+                versionControlledFiles.firstOrNull { it.filename == fileName }
+            if (versionControlledFile == null) {
+                val missingVersionControlledFile = VersionControlledFile(fileName, metricsFactory)
                 versionControlledFiles.add(missingVersionControlledFile)
+                missingVersionControlledFile.registerCommit(commit)
+            } else {
+                versionControlledFile.registerCommit(commit)
             }
-
-        commit.filenames
-            .mapNotNull { findVersionControlledFileByFilename(versionControlledFiles, it) }
-            .forEach { it.registerCommit(commit) }
+        }
     }
 
-    private fun findVersionControlledFileByFilename(versionControlledFiles: List<VersionControlledFile>,
-                                                    filename: String): VersionControlledFile? {
-        return versionControlledFiles.firstOrNull { it.filename == filename }
-    }
-
-    private fun combineForParallelExecution(firstCommits: MutableList<VersionControlledFile>,
-                                            secondCommits: MutableList<VersionControlledFile>): MutableList<VersionControlledFile> {
+    private fun combineForParallelExecution(
+        firstCommits: MutableList<VersionControlledFile>,
+        secondCommits: MutableList<VersionControlledFile>
+    ): MutableList<VersionControlledFile> {
         throw UnsupportedOperationException("parallel collection of commits not supported")
     }
 
     companion object {
-
         fun create(metricsFactory: MetricsFactory): Collector<Commit, *, MutableList<VersionControlledFile>> {
             val collector = CommitCollector(metricsFactory)
             return Collector.of(Supplier<MutableList<VersionControlledFile>> { ArrayList() },
-                    BiConsumer<MutableList<VersionControlledFile>, Commit> { versionControlledFiles, commit ->
-                        collector.collectCommit(versionControlledFiles, commit)
-                    }, BinaryOperator<MutableList<VersionControlledFile>> { firstCommits, secondCommits ->
-                collector.combineForParallelExecution(firstCommits, secondCommits)
-            })
+                BiConsumer<MutableList<VersionControlledFile>, Commit> { versionControlledFiles, commit ->
+                    collector.collectCommit(versionControlledFiles, commit)
+                }, BinaryOperator<MutableList<VersionControlledFile>> { firstCommits, secondCommits ->
+                    collector.combineForParallelExecution(firstCommits, secondCommits)
+                })
         }
     }
 }
