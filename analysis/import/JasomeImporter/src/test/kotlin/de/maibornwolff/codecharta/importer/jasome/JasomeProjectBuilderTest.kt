f263f6722127d9ff037eacf9be05f4df95ba4120
Ben Willenbring <ben-willenbring@t-online.de>
2020-07-12 04:25:30 +0200

    Update schema in analysis and fix JasomeImporter exporting wrong nodetypes

--- analysis/import/JasomeImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/jasome/JasomeProjectBuilderTest.kt
+++ analysis/import/JasomeImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/jasome/JasomeProjectBuilderTest.kt
@@ -48,9 +48,9 @@ class JasomeProjectBuilderTest : Spek({
             MatcherAssert.assertThat(leaves.keys, hasItem(Path(listOf("com", "package", "name", "ClassName"))))
         }
 
-        it("contains class") {
+        it("contains class exporte to cc.json as file") {
             val node = leaves.values.first()
-            MatcherAssert.assertThat(node.type, `is`(NodeType.Class))
+            MatcherAssert.assertThat(node.type, `is`(NodeType.File))
             MatcherAssert.assertThat(node.name, `is`("ClassName"))
         }
 
