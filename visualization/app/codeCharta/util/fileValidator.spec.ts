1b1387b384261c473722d29802b30263a90bd0b6
Ben Willenbring <ben-willenbring@t-online.de>
2020-07-12 02:36:02 +0200

    Update use assert to verify exceptions

--- visualization/app/codeCharta/util/fileValidator.spec.ts
+++ visualization/app/codeCharta/util/fileValidator.spec.ts
@@ -6,7 +6,8 @@ import {
 	TEST_FILE_CONTENT_NO_API
 } from "./dataMocks"
 import { NodeType } from "../codeCharta.model"
-import { ERROR_MESSAGES, validate } from "./fileValidator"
+import { CCValidationResult, ERROR_MESSAGES, validate } from "./fileValidator"
+import assert from "assert"
 
 describe("FileValidator", () => {
 	let file
@@ -16,102 +17,88 @@ describe("FileValidator", () => {
 		file = TEST_FILE_CONTENT
 	})
 
-	function expectFileToBeValid(errors) {
-		expect(errors.error.length).toBe(0)
-	}
-
-	function expectFileToBeInvalid(errors) {
-		expect(errors.error.length).toBeGreaterThan(0)
-	}
-
-	function expectFileToThrowResult(file) {
-		expect(() => {
-			validate(file)
-		}).toThrow()
-	}
-
-	function expectFileNotToThrowResult(file) {
-		expect(() => {
-			validate(file)
-		}).not.toThrow()
-	}
-
 	it("API version exists in package.json", () => {
 		expect(require("../../../package.json").codecharta.apiVersion).toEqual("1.1")
 	})
 
 	it("should throw on null", () => {
-		expectFileToThrowResult(invalidFile)
-		try {
-			validate(null)
-		} catch (e) {
-			expectFileToBeInvalid(e)
-			expect(e.title).toEqual(ERROR_MESSAGES.fileIsInvalid.title)
-			expect(e.error[0]).toEqual(ERROR_MESSAGES.fileIsInvalid.message)
+		const expectedError: CCValidationResult = {
+			title: ERROR_MESSAGES.fileIsInvalid.title,
+			error: [ERROR_MESSAGES.fileIsInvalid.message],
+			warning: []
 		}
+
+		assert.throws(() => {
+			validate(null)
+		}, expectedError)
 	})
 
 	it("should throw when higher Major API", () => {
 		invalidFile = TEST_FILE_CONTENT_INVALID_MAJOR_API
 
-		expectFileToThrowResult(invalidFile)
-		try {
-			validate(invalidFile)
-		} catch (e) {
-			expectFileToBeInvalid(e)
-			expect(e.title).toEqual(ERROR_MESSAGES.majorApiVersionIsOutdated.title)
-			expect(e.error[0]).toEqual(ERROR_MESSAGES.majorApiVersionIsOutdated.message)
+		const expectedError: CCValidationResult = {
+			title: ERROR_MESSAGES.majorApiVersionIsOutdated.title,
+			error: [ERROR_MESSAGES.majorApiVersionIsOutdated.message],
+			warning: []
 		}
+
+		assert.throws(() => {
+			validate(invalidFile)
+		}, expectedError)
 	})
 
 	it("should throw on warning with higher minor API version", () => {
 		file = TEST_FILE_CONTENT_INVALID_MINOR_API
 
-		expectFileToThrowResult(file)
-		try {
-			validate(file)
-		} catch (e) {
-			expectFileToBeValid(e)
-			expect(e.warning.length).toBeGreaterThan(0)
-			expect(e.title).toEqual(ERROR_MESSAGES.minorApiVersionOutdated.title)
-			expect(e.warning[0]).toEqual(ERROR_MESSAGES.minorApiVersionOutdated.message)
+		const expectedError: CCValidationResult = {
+			title: ERROR_MESSAGES.minorApiVersionOutdated.title,
+			error: [],
+			warning: [ERROR_MESSAGES.minorApiVersionOutdated.message]
 		}
+
+		assert.throws(() => {
+			validate(file)
+		}, expectedError)
 	})
 
 	it("should throw on file missing API version", () => {
 		invalidFile = TEST_FILE_CONTENT_NO_API
 
-		expectFileToThrowResult(invalidFile)
-		try {
-			validate(invalidFile)
-		} catch (e) {
-			expectFileToBeInvalid(e)
-			expect(e.title).toEqual(ERROR_MESSAGES.apiVersionIsInvalid.title)
-			expect(e.error[0]).toEqual(ERROR_MESSAGES.apiVersionIsInvalid.message)
+		const expectedError: CCValidationResult = {
+			title: ERROR_MESSAGES.apiVersionIsInvalid.title,
+			error: [ERROR_MESSAGES.apiVersionIsInvalid.message],
+			warning: []
 		}
+
+		assert.throws(() => {
+			validate(invalidFile)
+		}, expectedError)
 	})
 
 	it("should throw on file with wrong API version", () => {
 		invalidFile = TEST_FILE_CONTENT_INVALID_API
 
-		expectFileToThrowResult(invalidFile)
-		try {
-			validate(invalidFile)
-		} catch (e) {
-			expectFileToBeInvalid(e)
-			expect(e.title).toEqual(ERROR_MESSAGES.apiVersionIsInvalid.title)
-			expect(e.error[0]).toEqual(ERROR_MESSAGES.apiVersionIsInvalid.message)
+		const expectedError: CCValidationResult = {
+			title: ERROR_MESSAGES.apiVersionIsInvalid.title,
+			error: [ERROR_MESSAGES.apiVersionIsInvalid.message],
+			warning: []
 		}
+
+		assert.throws(() => {
+			validate(invalidFile)
+		}, expectedError)
 	})
 
 	it("should throw on string", () => {
-		expectFileToThrowResult(invalidFile)
-		try {
-			validate("" as any)
-		} catch (e) {
-			expect(e.title).toEqual(ERROR_MESSAGES.fileIsInvalid.title)
-			expect(e.error[0]).toEqual(ERROR_MESSAGES.fileIsInvalid.message)
+		const expectedError: CCValidationResult = {
+			title: ERROR_MESSAGES.fileIsInvalid.title,
+			error: [ERROR_MESSAGES.fileIsInvalid.message],
+			warning: []
 		}
+
+		assert.throws(() => {
+			validate("" as any)
+		}, expectedError)
 	})
 
 	it("should not throw on a file with edges", () => {
@@ -126,19 +113,25 @@ describe("FileValidator", () => {
 			}
 		]
 
-		expectFileNotToThrowResult(file)
+		assert.doesNotThrow(() => {
+			validate(file)
+		})
 	})
 
 	it("should not throw on a file without edges", () => {
 		file.edges = undefined
 
-		expectFileNotToThrowResult(file)
+		assert.doesNotThrow(() => {
+			validate(file)
+		})
 	})
 
 	it("should not throw on a file when numbers are floating point values", () => {
 		file.nodes[0].children[0].attributes["rloc"] = 333.4
 
-		expectFileNotToThrowResult(file)
+		assert.doesNotThrow(() => {
+			validate(file)
+		})
 	})
 
 	it("should throw when children are not unique in name+type", () => {
@@ -147,26 +140,29 @@ describe("FileValidator", () => {
 		file.nodes[0].children[1].name = "same"
 		file.nodes[0].children[1].type = NodeType.FILE
 
-		expectFileToThrowResult(invalidFile)
-		try {
-			validate(file)
-		} catch (e) {
-			expectFileToBeInvalid(e)
-			expect(e.title).toEqual(ERROR_MESSAGES.nodesNotUnique.title)
-			expect(e.error[0]).toEqual(ERROR_MESSAGES.nodesNotUnique.message)
+		const expectedError: CCValidationResult = {
+			title: ERROR_MESSAGES.nodesNotUnique.title,
+			error: [ERROR_MESSAGES.nodesNotUnique.message],
+			warning: []
 		}
+
+		assert.throws(() => {
+			validate(file)
+		}, expectedError)
 	})
 
 	it("should throw when nodes are empty", () => {
 		file.nodes[0] = []
 
-		expectFileToThrowResult(invalidFile)
-		try {
-			validate(file)
-		} catch (e) {
-			expectFileToBeInvalid(e)
-			expect(e.title).toEqual(ERROR_MESSAGES.validationError.title)
+		const expectedError: CCValidationResult = {
+			title: ERROR_MESSAGES.validationError.title,
+			error: ["Type error: nodes[0] should be object"],
+			warning: []
 		}
+
+		assert.throws(() => {
+			validate(file)
+		}, expectedError)
 	})
 
 	it("should throw if nodes is not a node and therefore has no name or id", () => {
@@ -174,12 +170,18 @@ describe("FileValidator", () => {
 			something: "something"
 		}
 
-		expectFileToThrowResult(invalidFile)
-		try {
-			validate(file)
-		} catch (e) {
-			expectFileToBeInvalid(e)
+		const expectedError: CCValidationResult = {
+			title: ERROR_MESSAGES.validationError.title,
+			error: [
+				"Required error: nodes[0] should have required property 'name'",
+				"Required error: nodes[0] should have required property 'type'"
+			],
+			warning: []
 		}
+
+		assert.throws(() => {
+			validate(file)
+		}, expectedError)
 	})
 
 	it("attributes should not allow whitespaces", () => {
@@ -187,12 +189,18 @@ describe("FileValidator", () => {
 			"tes t1": 0
 		}
 
-		expectFileToThrowResult(invalidFile)
-		try {
-			validate(file)
-		} catch (e) {
-			expectFileToBeInvalid(e)
+		const expectedError: CCValidationResult = {
+			title: ERROR_MESSAGES.validationError.title,
+			error: [
+				"Required error: nodes[0] should have required property 'name'",
+				"Required error: nodes[0] should have required property 'type'"
+			],
+			warning: []
 		}
+
+		assert.throws(() => {
+			validate(file)
+		}, expectedError)
 	})
 
 	it("attributes should not allow special characters", () => {
@@ -200,11 +208,17 @@ describe("FileValidator", () => {
 			"tes)t1": 0
 		}
 
-		expectFileToThrowResult(invalidFile)
-		try {
-			validate(file)
-		} catch (e) {
-			expectFileToBeInvalid(e)
+		const expectedError: CCValidationResult = {
+			title: ERROR_MESSAGES.validationError.title,
+			error: [
+				"Required error: nodes[0] should have required property 'name'",
+				"Required error: nodes[0] should have required property 'type'"
+			],
+			warning: []
 		}
+
+		assert.throws(() => {
+			validate(file)
+		}, expectedError)
 	})
 })
