f340162d4bbade66d8f1228621b53b046a5c0bd5
Ben Willenbring <ben-willenbring@t-online.de>
2020-07-26 23:53:35 +0200

    Add regression test #1095

--- visualization/app/codeCharta/ui/edgeChooser/edgeChooser.e2e.ts
+++ visualization/app/codeCharta/ui/edgeChooser/edgeChooser.e2e.ts
@@ -2,6 +2,8 @@ import { delay, goto, launch, newPage } from "../../../puppeteer.helper"
 import { Browser, Page } from "puppeteer"
 import { EdgeChooserPageObject } from "./edgeChooser.po"
 import { FileChooserPageObject } from "../fileChooser/fileChooser.po"
+import { MapTreeViewLevelPageObject } from "../mapTreeView/mapTreeView.level.po"
+import { SearchPanelPageObject } from "../searchPanel/searchPanel.po"
 
 describe("MapTreeViewLevel", () => {
 	let browser: Browser
@@ -9,6 +11,8 @@ describe("MapTreeViewLevel", () => {
 
 	let edgeChooser: EdgeChooserPageObject
 	let fileChooser: FileChooserPageObject
+	let mapTreeViewLevel: MapTreeViewLevelPageObject
+	let searchPanel: SearchPanelPageObject
 
 	beforeAll(async () => {
 		browser = await launch()
@@ -22,6 +26,8 @@ describe("MapTreeViewLevel", () => {
 		page = await newPage(browser)
 		edgeChooser = new EdgeChooserPageObject(page)
 		fileChooser = new FileChooserPageObject(page)
+		mapTreeViewLevel = new MapTreeViewLevelPageObject(page)
+		searchPanel = new SearchPanelPageObject(page)
 
 		await goto(page)
 	})
@@ -35,6 +41,21 @@ describe("MapTreeViewLevel", () => {
 			const metrics = await edgeChooser.getMetrics()
 
 			expect(metrics).toHaveLength(2)
+		})
+
+		it("should display the amount of incoming and outgoing edges next to the metric name", async () => {
+			await edgeChooser.selectEdgeMetric("pairingRate")
+			await searchPanel.toggle()
+			await mapTreeViewLevel.hoverNode()
+
+			expect(edgeChooser.isEdgeCountAvailable()).toBeTruthy()
+		})
+
+		it("should not display the amount of incoming and outgoing edges of buildings for the none metric", async () => {
+			await searchPanel.toggle()
+			await mapTreeViewLevel.hoverNode()
+
+			expect(await edgeChooser.isEdgeCountAvailable()).toBeFalsy()
 		})
 	})
 })
