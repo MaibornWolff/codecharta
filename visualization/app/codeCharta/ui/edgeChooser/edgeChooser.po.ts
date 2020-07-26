f6b685a291ad9b604fecea88ad4063d1a409d7f9
Ben Willenbring <ben-willenbring@t-online.de>
2020-07-26 23:38:30 +0200

    Add regression test #1095

--- visualization/app/codeCharta/ui/edgeChooser/edgeChooser.po.ts
+++ visualization/app/codeCharta/ui/edgeChooser/edgeChooser.po.ts
@@ -12,4 +12,19 @@ export class EdgeChooserPageObject {
 	public async getMetrics() {
 		return await this.page.$$eval(".edge-metric", metrics => metrics.map(x => x.textContent))
 	}
+
+	public async selectEdgeMetric(metric: string) {
+		await this.open()
+		await this.page.click(`#edge-metric-${metric}`)
+	}
+
+	public async isEdgeCountAvailable() {
+		const innerText = await this.page.$eval("edge-chooser-component #edge-count", el => el["innerText"])
+
+		function containsNumber(string: string): boolean {
+			return /\d/.test(string)
+		}
+
+		return containsNumber(innerText)
+	}
 }
--- visualization/app/codeCharta/ui/mapTreeView/mapTreeView.level.po.ts
+++ visualization/app/codeCharta/ui/mapTreeView/mapTreeView.level.po.ts
@@ -8,6 +8,11 @@ export class MapTreeViewLevelPageObject {
 		await this.page.click("[id='/root/ParentLeaf/smallLeaf.html']", { button: "right" })
 	}
 
+	public async hoverNode() {
+		await this.page.click("[id='/root/ParentLeaf']")
+		await this.page.hover("[id='/root/ParentLeaf/smallLeaf.html']")
+	}
+
 	public async nodeExists() {
 		return !!(await this.page.$("[id='/root/ParentLeaf/smallLeaf.html']"))
 	}
