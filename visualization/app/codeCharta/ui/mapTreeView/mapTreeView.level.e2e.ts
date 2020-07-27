06fad8b13f731eff1098b9d3b03313832b9b4f07
Ben Willenbring <ben-willenbring@t-online.de>
2020-07-27 14:31:42 +0200

    Refactor use variable for reused strings #1101

--- visualization/app/codeCharta/ui/mapTreeView/mapTreeView.level.e2e.ts
+++ visualization/app/codeCharta/ui/mapTreeView/mapTreeView.level.e2e.ts
@@ -31,12 +31,14 @@ describe("MapTreeViewLevel", () => {
 
 	describe("Blacklist", () => {
 		it("excluding a building should exclude it from the tree-view as well", async () => {
+			const nodePath = "/root/ParentLeaf/smallLeaf.html"
+
 			await searchPanelModeSelector.toggleTreeView()
 			await mapTreeViewLevel.openFolder("/root/ParentLeaf")
-			await mapTreeViewLevel.openContextMenu("/root/ParentLeaf/smallLeaf.html")
+			await mapTreeViewLevel.openContextMenu(nodePath)
 			await nodeContextMenu.exclude()
 
-			expect(await mapTreeViewLevel.nodeExists("/root/ParentLeaf/smallLeaf.html")).toBeFalsy()
+			expect(await mapTreeViewLevel.nodeExists(nodePath)).toBeFalsy()
 		})
 	})
 })
