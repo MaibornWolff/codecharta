04111a7c2a33f3fca58fdc1d989b22d26ad5d044
Ben Willenbring <ben-willenbring@t-online.de>
2020-07-28 20:22:19 +0200

    Remove waiting for x seconds #1101

--- visualization/app/codeCharta/ui/fileChooser/fileChooser.po.ts
+++ visualization/app/codeCharta/ui/fileChooser/fileChooser.po.ts
@@ -1,15 +1,18 @@
+import { waitForElementRemoval } from "../../../puppeteer.helper"
+
 export class FileChooserPageObject {
 	public async openFile(path: string) {
 		const [fileChooser] = await Promise.all([page.waitForFileChooser(), expect(page).toClick("file-chooser-directive .toolbar-button")])
 
 		await fileChooser.accept([path])
-		await page.waitFor(200)
+
+		await page.waitForSelector("#loading-gif-file")
+		await waitForElementRemoval("#loading-gif-file")
 	}
 
 	public async cancelOpeningFile() {
 		const [fileChooser] = await Promise.all([page.waitForFileChooser(), expect(page).toClick("file-chooser-directive .toolbar-button")])
 
 		await fileChooser.cancel()
-		await page.waitFor(200)
 	}
 }
