fdbeab5b03d6f092b31cd065bca8b35c4309e0bd
Ben Willenbring <ben-willenbring@t-online.de>
2020-07-30 03:14:33 +0200

    Add version bumping #783

--- script/make_release.py
+++ script/make_release.py
@@ -1,11 +1,13 @@
 import pathlib
 import git
 import inquirer
+import subprocess
+import fileinput
 
 
 def isRootFolder():
     currentDir = pathlib.Path().absolute()
-    return "visualization" not in currentDir and "analysis" not in currentDir
+    return currentDir[-10:] == "codecharta"
 
 # Requirements
 
@@ -23,37 +25,82 @@ if repo.is_dirty():
     print("Please commit your changes first and/or ignore untracked files in git. Aborting.")
     quit()
 
+
 # Check if we are on master branch
 if repo.head != "master":
     print("You can only release on master branch. Aborting.")
     quit()
 
 # Get latest tag
-latest_tag = sorted(repo.tags)[-1]
+latest_tag = str(
+    sorted(repo.tags, key=lambda t: t.commit.committed_datetime)[-1])
 print(f"Last version tag in git is {latest_tag}")
+
 
 # Precalc new versions
 versions = latest_tag.split(".")
-new_major_version = versions[0]
-new_minor_version = versions[1]
-new_patch_version = versions[2]
+
+major = int(versions[0])
+minor = int(versions[1])
+patch = int(versions[2])
+
+new_major_version = f"{major + 1}.0.0"
+new_minor_version = f"{major}.{minor + 1}.0"
+new_patch_version = f"{major}.{minor}.{patch + 1}"
+
 
 # Get release type and version
 questions = [
-    inquirer.List('version',
+    inquirer.List("version",
                   message=f"Do you want to release a major version [{new_major_version}], minor version [{new_minor_version}] or a patch [{new_patch_version}]?",
                   choices=["Major", "Minor", "Patch"],
                   ),
 ]
-answer = inquirer.prompt(questions)
+release_type = inquirer.prompt(questions)["version"]
+print("release type: ", release_type)
 
 new_version = ""
 
-if answer == "":
+if release_type == None:
     quit()
-if answer == "Major":
+if release_type == "Major":
     new_version = new_major_version
-if answer == "Minor":
+if release_type == "Minor":
     new_version = new_minor_version
-if answer == "Patch":
+if release_type == "Patch":
     new_version = new_patch_version
+
+
+# Confirm release
+confirm = {
+    inquirer.Confirm("confirmed",
+                     message=f"Do you REALLY want to release {new_version}? WARNING: File changes need to be undone manually or through git when done unintentionally!",
+                     default=True),
+}
+confirmation = inquirer.prompt(confirm)["confirmed"]
+
+if confirmation:
+    print(f"Selected {release_type} release. Updating project...")
+else:
+    print("Aborting.")
+    quit()
+
+
+# bump version in gradle.properties
+for line in fileinput.input("./analysis/gradle.properties", inplace=True):
+    if "currentVersion=" in line:
+        print(f"currentVersion={new_version}", end="\n")
+    else:
+        print(line, end="")
+
+print(f"v{new_version}")
+print("incremented version in ./analysis/gradle.properties")
+
+# bump version in package.jsons
+subprocess.run(["npm", "--prefix", "./analysis/node-wrapper",
+                "--no-git-tag-version", "version", f"{new_version}"], shell=True)
+print("incremented version in ./analysis/node-wrapper/package.json + locks")
+
+subprocess.run(["npm", "--prefix", "./visualization",
+                "--no-git-tag-version", "version", f"{new_version}"], shell=True)
+print("incremented version in ./visualization/package.json + locks")
