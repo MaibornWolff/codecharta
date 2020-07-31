50f1d0aba6e0c259a1aa2db20a4d08e34b9fa60b
Ben Willenbring <ben-willenbring@t-online.de>
2020-07-31 12:43:26 +0200

    Update increase stability of mapTreeView actions #1101

--- visualization/app/codeCharta/ui/mapTreeView/mapTreeView.level.po.ts
+++ visualization/app/codeCharta/ui/mapTreeView/mapTreeView.level.po.ts
@@ -1,15 +1,18 @@
 export class MapTreeViewLevelPageObject {
 	public async openContextMenu(path: string) {
 		await expect(page).toClick(`[id='${path}']`, { button: "right", timeout: 3000 })
+		await page.waitForSelector("node-context-menu-component", { visible: true })
 	}
 
 	public async openFolder(path: string) {
 		await expect(page).toClick(`[id='${path}']`, { timeout: 3000 })
+		await page.waitForSelector(`[id='${path}'] span.fa.fa-folder-open`)
 	}
 
 	public async hoverNode(path: string) {
 		await page.waitForSelector(`[id='${path}']`)
 		await page.hover(`[id='${path}']`)
+		await page.waitForSelector(`[id='${path}'] > div .hovered`)
 	}
 
 	public async nodeExists(path: string) {
