e4467a1a59a409f4560fcef0eef22d4e0797d218
Ben Willenbring <ben-willenbring@t-online.de>
2020-08-12 14:35:13 +0200

    Refactor use shorthand notation in lambda function return #901

--- visualization/app/codeCharta/util/reduxHelper.ts
+++ visualization/app/codeCharta/util/reduxHelper.ts
@@ -3,9 +3,7 @@ import _ from "lodash"
 const clone = require("rfdc")()
 
 export function removeItemFromArray(array: any[], item: any): any[] {
-	return array.filter(x => {
-		return !_.isEqual(x, item)
-	})
+	return array.filter(x => !_.isEqual(x, item))
 }
 
 export function addItemToArray(array: any[], item: any): any[] {
