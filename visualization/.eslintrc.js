46cb9d7abe7fef2601ba77effe536c3b2f2b6413
Ben Willenbring <ben-willenbring@t-online.de>
2020-09-04 13:17:11 +0200

    Update replace deprecated camelcase rule

--- visualization/.eslintrc.js
+++ visualization/.eslintrc.js
@@ -1,47 +1,54 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es6": true,
-		"node": true,
-		"jest": true
-	},
-	"globals": {
-		page: true,
-		browser: true,
-		context: true,
-		jestPuppeteer: true,
-	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/eslint-recommended",
-		"plugin:@typescript-eslint/recommended",
-		"prettier",
-		"prettier/@typescript-eslint",
-	], // prettier must be last in array to override other configs
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"project": "tsconfig.json",
-		"sourceType": "module"
-	},
-	"plugins": [
-		"@typescript-eslint"
-	],
-	"rules": {
-		"no-console": ["error", {allow : ["warn", "error"]}],
-		"@typescript-eslint/no-empty-function": ["error", {allow: ["arrowFunctions"]}],
-		"@typescript-eslint/camelcase": ["error", {properties: "never"}],
-		"@typescript-eslint/no-unused-vars": ["error"],
-		"object-shorthand": ["error", "always"],
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true,
+        "jest": true
+    },
+    "globals": {
+        page: true,
+        browser: true,
+        context: true,
+        jestPuppeteer: true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint",
+    ], // prettier must be last in array to override other configs
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "tsconfig.json",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "no-console": ["error", {allow: ["warn", "error"]}],
+        "@typescript-eslint/no-empty-function": ["error", {allow: ["arrowFunctions"]}],
+        "@typescript-eslint/naming-convention": [
+            "error",
+            {
+                "selector": ["variable", "function"],
+                "format": ["camelCase", "UPPER_CASE"],
+                "leadingUnderscore": "allow"
+            }
+        ],
+        "@typescript-eslint/no-unused-vars": ["error"],
+        "object-shorthand": ["error", "always"],
 
-		// Do not apply inappropriate rules below
-		"@typescript-eslint/no-use-before-define": "off",
-		"@typescript-eslint/ban-ts-ignore": "off",
+        // Do not apply inappropriate rules below
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/ban-ts-ignore": "off",
 
-		// TODO fix and remove rules below
-		"@typescript-eslint/explicit-function-return-type": "off",
-		"@typescript-eslint/ban-types": "off",
-		"@typescript-eslint/no-explicit-any": "off",
-		"@typescript-eslint/no-var-requires": "off",
-		"@typescript-eslint/consistent-type-assertions": "off",
-	}
+        // TODO fix and remove rules below
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/consistent-type-assertions": "off",
+    }
 }
