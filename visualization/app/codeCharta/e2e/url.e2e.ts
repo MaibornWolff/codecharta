1ced9cb81b0aae68cb75219c11e69489fcc19997
Ben Willenbring <ben-willenbring@t-online.de>
2020-09-16 15:14:15 +0200

    Add todo

--- visualization/app/codeCharta/e2e/url.e2e.ts
+++ visualization/app/codeCharta/e2e/url.e2e.ts
@@ -38,7 +38,10 @@ describe("codecharta", () => {
 
 	async function handleErrorDialog() {
 		const message = await dialogError.getMessage()
-		expect(message).toEqual("One or more files from the given file URL parameter could not be loaded. Loading sample files instead. undefined")
+		// TODO: Printing undefined should not happen during runtime. Check the mocks again.
+		expect(message).toEqual(
+			"One or more files from the given file URL parameter could not be loaded. Loading sample files instead. undefined"
+		)
 		await page.waitForSelector(".md-dialog-container")
 		await dialogError.clickOk()
 	}
