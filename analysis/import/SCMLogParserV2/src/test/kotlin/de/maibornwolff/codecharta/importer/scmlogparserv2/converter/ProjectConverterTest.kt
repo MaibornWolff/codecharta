37a0d6ffd28a4e29c46e79e3b91d4ecf952f1a69
RomanenkoVladimir <volodymyr.romanenko@icloud.com>
2020-09-25 18:49:41 +0200

    Enable and fit previously disabled ProjectConverterTests

--- analysis/import/SCMLogParserV2/src/test/kotlin/de/maibornwolff/codecharta/importer/scmlogparserv2/converter/ProjectConverterTest.kt
+++ analysis/import/SCMLogParserV2/src/test/kotlin/de/maibornwolff/codecharta/importer/scmlogparserv2/converter/ProjectConverterTest.kt
@@ -1,10 +1,16 @@
 package de.maibornwolff.codecharta.importer.scmlogparserv2.converter
 
+import de.maibornwolff.codecharta.importer.scmlogparserv2.input.Commit
 import de.maibornwolff.codecharta.importer.scmlogparserv2.input.Modification
+import de.maibornwolff.codecharta.importer.scmlogparserv2.input.VersionControlledFile
 import de.maibornwolff.codecharta.importer.scmlogparserv2.input.metrics.MetricsFactory
+import de.maibornwolff.codecharta.importer.scmlogparserv2.parser.VersionControlledFilesList
 import io.mockk.every
 import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
+import org.junit.Test
+import java.time.OffsetDateTime
 
 class ProjectConverterTest {
 
@@ -19,76 +25,66 @@ class ProjectConverterTest {
         return filenames.map { Modification(it) }
     }
 
-    //    @Test
-    //    @Suppress
-    //    @Throws(Exception::class)
-    //    fun canCreateAnEmptyProject() {
-    //        // given
-    //        val projectConverter = ProjectConverter(true)
-    //
-    //        // when
-    //        val project = projectConverter.convert(emptyList(), metricsFactory)
-    //
-    //        // then
-    //        assertThat(project.rootNode.leaves).hasSize(1)
-    //    }
+    @Test
+    @Throws(Exception::class)
+    fun canCreateAnEmptyProject() {
+        // given
+        val projectConverter = ProjectConverter(true)
+        val metricsFactory = MetricsFactory()
+        val vcfList = VersionControlledFilesList(metricsFactory)
+        // when
+        val project = projectConverter.convert(vcfList, metricsFactory, listOf())
 
-    //    @Test
-    //    @Suppress
-    //    fun canConvertProjectWithAuthors() {
-    //        // given
-    //        val projectConverter = ProjectConverter(true)
-    //        val file1 = VersionControlledFile("File 1", metricsFactory)
-    //        file1.registerCommit(Commit("Author", modificationsByFilename("File 1", "File 2"), OffsetDateTime.now()))
-    //
-    //        // when
-    //        val project = projectConverter.convert(Arrays.asList(file1), metricsFactory)
-    //
-    //        // then
-    //        assertThat(project.rootNode.children.toMutableList()[0].attributes.containsKey("authors")).isTrue()
-    //    }
+        // then
+        assertThat(project.rootNode.leaves).hasSize(1)
+    }
 
-    //    @Test
-    //    @Suppress
-    //    fun canConvertProjectWithoutAuthors() {
-    //        // given
-    //        val projectConverter = ProjectConverter(false)
-    //        val file1 = VersionControlledFile("File 1", metricsFactory)
-    //        file1.registerCommit(Commit("Author", modificationsByFilename("File 1", "File 2"), OffsetDateTime.now()))
-    //
-    //        // when
-    //        val project = projectConverter.convert(Arrays.asList(file1), metricsFactory)
-    //
-    //        // then
-    //        assertThat(project.rootNode.children.toMutableList()[0].attributes.containsKey("authors")).isFalse()
-    //    }
+    @Test
+    fun canConvertProjectWithAuthors() {
+        // given
+        val projectConverter = ProjectConverter(true)
+        val file1 = VersionControlledFile("File 1", metricsFactory)
+        val vcfList = VersionControlledFilesList(metricsFactory)
 
-    //    @Test
-    //    fun edgesAreRegisteredInProject() {
-    //        // given
-    //        val projectConverter = ProjectConverter(true)
-    //        val metricsFactory = MetricsFactory().createMetrics()
-    //        val file1 = VersionControlledFile("File 1", metricsFactory)
-    //        val commit = Commit("Author", modificationsByFilename("File 1", "File 2"), OffsetDateTime.now())
-    //        for (i in 0..4) {
-    //            file1.registerCommit(commit)
-    //        }
-    //
-    //        // when
-    //        val project = projectConverter.convert(listOf(file1), MetricsFactory())
-    //
-    //        // then
-    //        assertThat(project.edges.size).isEqualTo(1)
-    //        assertThat(project.edges[0].toNodeName).isEqualTo("/root/File 2")
-    //        assertThat(project.edges[0].fromNodeName).isEqualTo("/root/File 1")
-    //    }
+        vcfList.addFileBy("File 1")
+        file1.registerCommit(
+            Commit("Author", modificationsByFilename("File 1", "File 2"), OffsetDateTime.now()),
+            Modification.EMPTY
+        )
 
-    //    @Test
-    //    @Suppress
-    //    fun attributeTypesAreCreated() {
-    //        val projectConverter = ProjectConverter(false)
-    //        val project = projectConverter.convert(listOf(), MetricsFactory())
-    //
-    //        assertThat(project.attributeTypes).containsKeys("edges", "nodes")
-    //    }
+        // when
+        val project = projectConverter.convert(vcfList, metricsFactory, listOf("File 1"))
+
+        // then
+        assertThat(project.rootNode.children.toMutableList()[0].attributes.containsKey("authors")).isTrue()
+    }
+
+    @Test
+    fun canConvertProjectWithoutAuthors() {
+        // given
+        val projectConverter = ProjectConverter(false)
+        val file1 = VersionControlledFile("File 1", metricsFactory)
+        val vcfList = VersionControlledFilesList(metricsFactory)
+
+        vcfList.addFileBy("File 1")
+        file1.registerCommit(
+            Commit("Author", modificationsByFilename("File 1", "File 2"), OffsetDateTime.now()),
+            Modification.EMPTY
+        )
+
+        // when
+        val project = projectConverter.convert(vcfList, metricsFactory, listOf("File 1"))
+
+        // then
+        assertThat(project.rootNode.children.toMutableList()[0].attributes.containsKey("authors")).isFalse()
+    }
+
+    @Test
+    fun attributeTypesAreCreated() {
+        val projectConverter = ProjectConverter(false)
+        val vcfList = VersionControlledFilesList(metricsFactory)
+        val project = projectConverter.convert(vcfList, MetricsFactory(), listOf())
+
+        assertThat(project.attributeTypes).containsKeys("edges", "nodes")
+    }
 }
