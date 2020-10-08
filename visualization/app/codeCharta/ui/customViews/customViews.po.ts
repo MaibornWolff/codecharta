20ebe3aa83c9d7e044976eb6f3f64e2c49d114f0
Cedrik Bormann <cedrikbormann@gmail.com>
2020-10-08 11:39:12 +0200

    Clear PageObject #1318

--- visualization/app/codeCharta/ui/customViews/customViews.po.ts
+++ visualization/app/codeCharta/ui/customViews/customViews.po.ts
@@ -1,7 +1,3 @@
 export class CustomViewsPageObject {
 
-    public async doSomething() {
-        await page.waitForSelector("CSS_SELECTOR")
-    	  return await page.$eval("A > CSS_SELECTOR", el => el["someAttribute"])
-    }
 }
