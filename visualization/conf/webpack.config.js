c23873df611c3850d0ae320f7e05c886dd942aff
Ben Willenbring <ben-willenbring@t-online.de>
2020-10-27 23:56:46 +0100

    Update scripts and config with new webpack cli

--- visualization/conf/webpack.config.js
+++ visualization/conf/webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const { DefinePlugin } = require("webpack")
 
 const dist = path.resolve(__dirname, "../dist/webpack")
 
@@ -18,13 +19,17 @@ module.exports = env => {
 			compress: true, // enable gzip compression
 			hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
 			port: 3000,
-			clientLogLevel: "error"
+			clientLogLevel: "error",
+			open: true
 		},
 		module: require("./webpack.loaders.js"),
 		plugins: [
 			new CleanWebpackPlugin(),
 			new HtmlWebpackPlugin({
 				template: "./app/index.html"
+			}),
+			new DefinePlugin({
+				"process.env.STANDALONE": JSON.stringify(env.STANDALONE)
 			})
 		],
 		devtool: "source-map",
