d95f2a7aa21452121a1117140bfec00e89e89d53
Cedrik Bormann <cedrikbormann@gmail.com>
2020-11-06 10:21:41 +0100

    Replace CustomView with CustomConfig

--- visualization/app/codeCharta/ui/customConfigs/customConfigs.po.ts
+++ visualization/app/codeCharta/ui/customConfigs/customConfigs.po.ts
@@ -12,10 +12,10 @@ export class CustomConfigsPageObject {
 	}
 
 	async openCustomConfigPanel() {
-		await page.waitForSelector(".custom-views-button", { hidden: false })
-		await expect(page).toClick("custom-views-component md-menu:nth-child(1) .custom-views-button", { timeout: 3000 })
+		await page.waitForSelector(".custom-configs-button", { hidden: false })
+		await expect(page).toClick("custom-configs-component md-menu:nth-child(1) .custom-configs-button", { timeout: 3000 })
 
-		await page.waitForSelector(".custom-views-drop-down", { visible: true })
+		await page.waitForSelector(".custom-configs-drop-down", { visible: true })
 	}
 
 	async addCustomConfig(name: string) {
@@ -30,26 +30,26 @@ export class CustomConfigsPageObject {
 	}
 
 	async openCustomConfigAddDialog() {
-		await page.waitForSelector(".custom-views-button.plus", { visible: true })
-		await expect(page).toClick(".custom-views-button.plus", { timeout: 3000 })
+		await page.waitForSelector(".custom-configs-button.plus", { visible: true })
+		await expect(page).toClick(".custom-configs-button.plus", { timeout: 3000 })
 
-		await page.waitForSelector(".custom-view-dialog")
+		await page.waitForSelector(".custom-config-dialog")
 	}
 
 	async isCustomConfigFeatureDisabled() {
-		await page.waitForSelector(".custom-views-button", { hidden: true })
+		await page.waitForSelector(".custom-configs-button", { hidden: true })
 	}
 
 	async isCustomConfigAddDialogOpen() {
-		await page.waitForSelector(".custom-view-dialog", { visible: true })
+		await page.waitForSelector(".custom-config-dialog", { visible: true })
 	}
 
 	async isCustomConfigAddDialogClosed() {
-		await page.waitForSelector(".custom-view-dialog", { hidden: true })
+		await page.waitForSelector(".custom-config-dialog", { hidden: true })
 	}
 
 	async fillInCustomConfigName(name = "TestViewName") {
-		return expect(page).toFill(".custom-view-input", name, { timeout: 3000 })
+		return expect(page).toFill(".custom-config-input", name, { timeout: 3000 })
 	}
 
 	async submitAddDialog() {
@@ -75,13 +75,13 @@ export class CustomConfigsPageObject {
 	}
 
 	async hasCustomConfigItemGroups() {
-		await page.waitForSelector(".custom-views-drop-down span.collapse-trigger:nth-child(1)")
+		await page.waitForSelector(".custom-configs-drop-down span.collapse-trigger:nth-child(1)")
 	}
 
 	async hasCustomConfigItemGroup(groupName: string, groupIndex: number) {
 		await page.waitForFunction(
 			(groupIndex, groupName) =>
-				document.querySelectorAll(".custom-views-drop-down span.collapse-trigger")[groupIndex].innerHTML.includes(groupName),
+				document.querySelectorAll(".custom-configs-drop-down span.collapse-trigger")[groupIndex].innerHTML.includes(groupName),
 			{},
 			groupIndex,
 			groupName
@@ -90,10 +90,10 @@ export class CustomConfigsPageObject {
 
 	async collapseCustomConfigItemGroup(groupIndex: number) {
 		// +2 to skip two disabled/invisible menu-items
-		await expect(page).toClick(`.custom-views-drop-down .custom-views-item:nth-child(${groupIndex + 2}) .button-hovering`, {
+		await expect(page).toClick(`.custom-configs-drop-down .custom-configs-item:nth-child(${groupIndex + 2}) .button-hovering`, {
 			timeout: 3000
 		})
-		await page.waitForSelector(`.custom-views-drop-down .custom-views-item:nth-child(${groupIndex + 2}) .collapsable`, {
+		await page.waitForSelector(`.custom-configs-drop-down .custom-configs-item:nth-child(${groupIndex + 2}) .collapsable`, {
 			visible: true
 		})
 	}
