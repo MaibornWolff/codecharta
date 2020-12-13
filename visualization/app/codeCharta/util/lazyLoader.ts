b67029bdec276a72d8e1ae7a13854b3005ad42b5
Dmytro Hutkin <dmytro.hutkin@gmail.com>
2020-12-13 14:31:41 +0100

    separate openFile function

--- visualization/app/codeCharta/util/lazyLoader.ts
+++ visualization/app/codeCharta/util/lazyLoader.ts
@@ -2,6 +2,7 @@
 export class LazyLoader {
 
     static openFolderDialog(fileName: string){
+
         if (localStorage.getItem(fileName) === null && confirm("Directory not chosen. Choose now?")){
             const input = document.createElement("input")
             const qualifiedNames = ['type', 'webkitdirectory', 'directory', 'mozdirectory', 'msdirectory', 'odirectory']
@@ -14,9 +15,16 @@ export class LazyLoader {
                 // @ts-ignore
                 const directoryName = input.files[0]?.webkitRelativePath?.split("/")[0]
                 localStorage.setItem(fileName, directoryName)
+                LazyLoader.openFile(input.files[0])
             }
             input.click()
         }
-        //window.open(localStorage.getItem(fileName), "_blank")
+    }
+
+    private static openFile(file: File) {
+        const link = document.createElement('a')
+        link.setAttribute("target", "_blank")
+        link.href = window.URL.createObjectURL(file)
+        link.click()
     }
 }
