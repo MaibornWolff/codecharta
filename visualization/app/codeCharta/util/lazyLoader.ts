78da544d96b9ab0fa9cff1714b5076c913113b46
Dmytro Hutkin <dmytro.hutkin@gmail.com>
2020-12-18 16:15:52 +0100

    fix directory exists validator

--- visualization/app/codeCharta/util/lazyLoader.ts
+++ visualization/app/codeCharta/util/lazyLoader.ts
@@ -1,27 +1,44 @@
 import {isStandalone} from "./envDetector"
-import * as fs from "fs"
-import * as open from 'open'
 
 export class LazyLoader {
 
-    static openFile(fileName: string, nodePath: string){
+    private static _fileName: string
+    private static _nodePath: string
+
+    static openFile(fileName:string = LazyLoader._fileName, nodePath:string = LazyLoader._nodePath){
 
         if (!isStandalone()){
             return
         }
+        LazyLoader._fileName = fileName
+        LazyLoader._nodePath = nodePath
         if (localStorage.getItem(fileName) === null){
-            this.setDirectory(fileName)
+            LazyLoader.setDirectory()
+            return
         }
-        else if (!fs.existsSync(localStorage.getItem(fileName))){
-            alert(`unknown or nonexistent directory: ${localStorage.getItem(fileName)}`)
-            this.setDirectory(fileName)
-        }
-        const path = nodePath.replace("root", localStorage.getItem(fileName))
-        open(`file:///${path}`)
+        LazyLoader.checkDirExists().then((exists) =>{
+            if (!exists){
+                const message = `Unknown or non-existent directory: ${localStorage.getItem(LazyLoader._fileName)}`
+                alert(message)
+                LazyLoader.setDirectory()
+                return
+            }
+        })
+        const path: string = nodePath.replace("root", localStorage.getItem(LazyLoader._fileName))
+        import("open").then(open => {
+            open.default(`file:///${path}`).then(_ => _, error => alert(error))
+        })
     }
 
-    private static setDirectory(fileName: string){
-        const directoryName = prompt("Please enter a root directory path of the project", "C:/")
-        localStorage.setItem(fileName, directoryName)
+    private static setDirectory(){
+        const directoryName = prompt("Please enter a root directory path of the project", "C:\\Users\\")
+        if (directoryName !== null && directoryName !== ""){
+            localStorage.setItem(LazyLoader._fileName, directoryName)
+            LazyLoader.openFile()
+        }
+    }
+
+    private static async checkDirExists(){
+        return import('fs').then(fs => fs.existsSync(localStorage.getItem(LazyLoader._fileName)))
     }
 }
