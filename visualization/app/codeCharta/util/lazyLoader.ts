3fd31957a752ab5b0e9e22c879b61b52f72db84d
Dmytro Hutkin <dmytro.hutkin@gmail.com>
2021-01-13 15:52:49 +0100

    refactor lazyLoader

--- visualization/app/codeCharta/util/lazyLoader.ts
+++ visualization/app/codeCharta/util/lazyLoader.ts
@@ -1,50 +1,49 @@
-import {isStandalone} from "./envDetector"
+import { isStandalone } from "./envDetector"
 
 export class LazyLoader {
+	private static _fileName: string
+	private static _nodePath: string
 
-    private static _fileName: string
-    private static _nodePath: string
+	static async openFile(fileName: string = LazyLoader._fileName, nodePath: string = LazyLoader._nodePath) {
+		if (!isStandalone()) {
+			return
+		}
+		LazyLoader._fileName = fileName
+		LazyLoader._nodePath = nodePath
+		const directory = localStorage.getItem(LazyLoader._fileName)
+		const path = nodePath.replace("root", directory)
+		if (directory === null) {
+			return LazyLoader.setDirectory()
+		}
+		//check if root still exists
+		const exists = await LazyLoader.checkDirExists(directory)
+		if (!exists) {
+			alert(`Unknown or non-existent directory: ${directory}`)
+			return LazyLoader.setDirectory()
+		}
+		const open = await import("open")
+		try {
+			await open.default(`file:///${path}`)
+		} catch (error) {
+			alert(error)
+		}
+	}
 
-    static openFile(fileName:string = LazyLoader._fileName, nodePath:string = LazyLoader._nodePath){
-        if (!isStandalone()){
-            return
-        }
-        LazyLoader._fileName = fileName
-        LazyLoader._nodePath = nodePath
-        const directory = localStorage.getItem(LazyLoader._fileName)
-        const path = nodePath.replace("root", directory)
-        if (directory === null){
-            LazyLoader.setDirectory()
-            return
-        }
-        //check if root still exists
-        LazyLoader.checkDirExists(directory).then(exists =>{
-            if (!exists){
-                alert(`Unknown or non-existent directory: ${directory}`)
-                LazyLoader.setDirectory()
-                return
-            }
-        })
-        import("open").then(open => {
-            open.default(`file:///${path}`).then(_ => _, error => alert(error))
-        })
-    }
+	private static async setDirectory() {
+		let _default = "\\Users\\"
+		if (navigator.userAgent.includes("Windows")) {
+			_default = `C:${_default}`
+		}
+		let directoryName = prompt("Please enter a root directory path of the project", _default)
+		if (directoryName) {
+			directoryName = directoryName.replace(/\\/g, "/")
+			localStorage.setItem(LazyLoader._fileName, directoryName)
+			await LazyLoader.openFile()
+		}
+	}
 
-    private static setDirectory(){
-        let _default = "\\Users\\"
-        if (navigator.userAgent.indexOf("Windows") !== 1){
-            _default = "C:".concat(_default)
-        }
-        let directoryName = prompt("Please enter a root directory path of the project", _default)
-        if (directoryName !== null && directoryName !== ""){
-            directoryName = directoryName.split("\\").join("/")
-            localStorage.setItem(LazyLoader._fileName, directoryName)
-            LazyLoader.openFile()
-        }
-    }
-
-    private static async checkDirExists(path: string){
-        const fs = await import('fs')
-        return fs.existsSync(path)
-    }
+	private static async checkDirExists(path: string) {
+		const fs = await import("fs")
+		return fs.existsSync(path)
+	}
 }
