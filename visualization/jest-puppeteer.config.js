65add2a4041eeba39d86b341b9c086a95d8e1725
RomanenkoVladimir <volodymyr.romanenko@icloud.com>
2021-01-14 17:55:39 +0100

    Revert config changes

--- visualization/jest-puppeteer.config.js
+++ visualization/jest-puppeteer.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	launch: {
 		headless: true,
 		args: ["--allow-file-access-from-files", "--start-maximized"],
-		defaultViewport: { width: 1920, height: 1080 },
-		slowMo: 250
+		defaultViewport: { width: 1920, height: 1080 }
+		//slowMo: 250
 	}
 }
