7bf2c26bc6ecc3cecb14813b8afc0359c7e49629
RomanenkoVladimir <volodymyr.romanenko@icloud.com>
2021-01-14 14:09:49 +0100

    e2e test

--- visualization/app/codeCharta/ui/legendPanel/legendPanel.po.ts
+++ visualization/app/codeCharta/ui/legendPanel/legendPanel.po.ts
@@ -3,10 +3,4 @@ export class LegendPanelObject {
 		await expect(page).toClick("legend-panel-component panel-button", { timeout: 3000 })
 		await page.waitForSelector("block-wrapper cc-shadow visible", { visible: true })
 	}
-
-	async checkNodeColor() {
-		await page.waitForSelector("img[data:image/gif;base64,R0lGODlhAQABAPAAAB2O/////yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==]", {
-			timeout: 3000
-		})
-	}
 }
