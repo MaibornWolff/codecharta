1746a2bfdf3db4b05fe63ef28905e8d2b1967d85
Torsten Knauf <Torsten.Knauf@maibornwolff.de>
2021-01-15 19:34:00 +0100

    1556: Add updating of brush icon's color

--- visualization/app/codeCharta/ui/legendPanel/legendMarkedPackages/legendMarkedPackages.component.tsx
+++ visualization/app/codeCharta/ui/legendPanel/legendMarkedPackages/legendMarkedPackages.component.tsx
@@ -1,18 +1,18 @@
 import "./legendMarkedPackages.component.scss"
-import { IRootScopeService } from "angular"
+import angular, { IRootScopeService } from "angular"
 import { CodeMapActionsService } from "../../codeMap/codeMap.actions.service"
 import { MarkedPackage } from "../../../codeCharta.model"
 import { MarkedPackagesSubscriber, MarkedPackagesService } from "../../../state/store/fileSettings/markedPackages/markedPackages.service"
-import { hasValidHexLength, normalizeHex } from "../../customColorPicker/colorHelper"
+import { getReadableColorForBackground, hasValidHexLength, normalizeHex } from "../../customColorPicker/colorHelper"
 
 type Path = string
-interface MarkedPackageMap {
+interface MarkedPackagesMap {
 	[color: string]: Path[]
 }
 
 export class LegendPanelController implements MarkedPackagesSubscriber {
 	private _viewModel: {
-		packageLists: MarkedPackageMap
+		packageLists: MarkedPackagesMap
 	} = {
 		packageLists: {}
 	}
@@ -20,23 +20,22 @@ export class LegendPanelController implements MarkedPackagesSubscriber {
 	// @ts-ignore TS6133 - used by rendered color-picker
 	private _colorPickerScopes = {}
 
-	constructor(private $rootScope: IRootScopeService, private $scope, private codeMapActionsService: CodeMapActionsService) {
+	constructor(
+		private $rootScope: IRootScopeService,
+		private $scope,
+		private $element,
+		private $timeout,
+		private codeMapActionsService: CodeMapActionsService
+	) {
 		this.$scope._colorPickerScopes = {}
 		MarkedPackagesService.subscribe(this.$rootScope, this)
 	}
 
 	onMarkedPackagesChanged(markedPackages: MarkedPackage[]) {
-		console.log(this.$scope, this._colorPickerScopes)
-		this._viewModel.packageLists = markedPackages.reduce((acc, markedPackage) => {
-			if (!acc.hasOwnProperty(markedPackage.color)) acc[markedPackage.color] = []
-			acc[markedPackage.color].push(markedPackage.path)
-			return acc
-		}, {})
+		this._viewModel.packageLists = this.calculatePackageLists(markedPackages)
 
-		this._colorPickerScopes = Object.entries(this._viewModel.packageLists).reduce((acc, [color, paths]) => {
-			acc[paths[0]] = color
-			return acc
-		}, {})
+		this._colorPickerScopes = this.calculateColorPickerScopes(this._viewModel.packageLists)
+		this.updateBrushesColor()
 	}
 
 	$onInit() {
@@ -45,15 +44,45 @@ export class LegendPanelController implements MarkedPackagesSubscriber {
 				if (!hasValidHexLength(newColor)) return
 
 				newColor = normalizeHex(newColor)
-				const oldColor = api.getElement().scope().color
-				this._viewModel.packageLists[newColor] = this._viewModel.packageLists[oldColor]
-				delete this._viewModel.packageLists[oldColor]
+
+				this.updateViewModelPackageList(api.getElement().scope().color, newColor)
 
 				this._viewModel.packageLists[newColor].forEach(path => {
 					this.codeMapActionsService.markFolder({ path }, newColor)
 				})
 			}
 		}
+	}
+
+	private updateBrushesColor() {
+		// wait for color-pickers to be updated
+		this.$timeout(() => {
+			for (const colorPicker of this.$element[0].querySelectorAll("color-picker")) {
+				const colorPickersColor = normalizeHex((angular.element(colorPicker).scope() as any).color)
+				const brushIcon = colorPicker.querySelector(".cc-color-picker-swatch-brush-icon") as HTMLElement
+				brushIcon.style.color = getReadableColorForBackground(colorPickersColor)
+			}
+		})
+	}
+
+	private calculatePackageLists(markedPackages: MarkedPackage[]) {
+		return markedPackages.reduce((acc, markedPackage) => {
+			if (!acc.hasOwnProperty(markedPackage.color)) acc[markedPackage.color] = []
+			acc[markedPackage.color].push(markedPackage.path)
+			return acc
+		}, {})
+	}
+
+	private calculateColorPickerScopes(packageLists: MarkedPackagesMap) {
+		return Object.entries(packageLists).reduce((acc, [color, paths]) => {
+			acc[paths[0]] = color
+			return acc
+		}, {})
+	}
+
+	private updateViewModelPackageList(oldColor: string, newColor: string) {
+		this._viewModel.packageLists[newColor] = this._viewModel.packageLists[oldColor]
+		delete this._viewModel.packageLists[oldColor]
 	}
 }
 
