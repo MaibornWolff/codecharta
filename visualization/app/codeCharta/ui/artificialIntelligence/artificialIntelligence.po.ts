e144307deb0225283724f618dae9360191471437
Cedrik Bormann <cedrikbormann@gmail.com>
2021-02-04 11:57:49 +0100

    Remove yet not needed e2e test for AI Component #1603

--- visualization/app/codeCharta/ui/artificialIntelligence/artificialIntelligence.po.ts
+++
@@ -1,6 +0,0 @@
-export class ArtificialIntelligencePageObject {
-	public async doSomething() {
-		await page.waitForSelector("CSS_SELECTOR")
-		return await page.$eval("A > CSS_SELECTOR", el => el["someAttribute"])
-	}
-}
