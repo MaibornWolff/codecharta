c186e70eec780d0e48fe6bad96f1437e24ba87c3
Dmytro Hutkin <dmytro.hutkin@gmail.com>
2021-02-11 12:00:52 +0100

    Add check focus depth #1099

--- visualization/app/codeCharta/ui/unfocusButton/unfocusButton.component.ts
+++ visualization/app/codeCharta/ui/unfocusButton/unfocusButton.component.ts
@@ -1,6 +1,6 @@
 import "./unfocusButton.component.scss"
 import { IRootScopeService } from "angular"
-import { unfocusNode } from "../../state/store/dynamicSettings/focusedNodePath/focusedNodePath.actions"
+import { focusNode, unfocusNode } from "../../state/store/dynamicSettings/focusedNodePath/focusedNodePath.actions"
 import { StoreService } from "../../state/store.service"
 import { BuildingRightClickedEventSubscriber, CodeMapMouseEventService } from "../codeMap/codeMap.mouseEvent.service"
 import { CodeMapBuilding } from "../codeMap/rendering/codeMapBuilding"
@@ -10,9 +10,11 @@ export class UnfocusButtonController implements BuildingRightClickedEventSubscri
 	private _viewModel: {
 		isNodeFocused: boolean
 		isParentFocused: boolean
+		focusedNodes: string[]
 	} = {
 		isNodeFocused: false,
-		isParentFocused: false
+		isParentFocused: false,
+		focusedNodes: []
 	}
 
 	/* @ngInject */
@@ -23,12 +25,18 @@ export class UnfocusButtonController implements BuildingRightClickedEventSubscri
 	onBuildingRightClicked(building: CodeMapBuilding) {
 		const codeMapNode = this.storeService.getState().lookUp.idToNode.get(building.node.id)
 		const { focusedNodePath } = this.storeService.getState().dynamicSettings
+		if (focusedNodePath && !this._viewModel.focusedNodes.includes(focusedNodePath)) {
+			this._viewModel.focusedNodes.push(focusedNodePath)
+		}
 		this._viewModel.isNodeFocused = codeMapNode.path === focusedNodePath
 		this._viewModel.isParentFocused = codeMapNode.path.startsWith(focusedNodePath) && codeMapNode.path !== focusedNodePath
 	}
 
 	removeFocusedNode() {
 		this.storeService.dispatch(unfocusNode())
+		this._viewModel.focusedNodes.pop()
+		const lastFocusedNode = this._viewModel.focusedNodes[this._viewModel.focusedNodes.length - 1]
+		this.storeService.dispatch(focusNode(lastFocusedNode))
 		NodeContextMenuController.broadcastHideEvent(this.$rootScope)
 	}
 }
