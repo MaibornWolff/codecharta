103fd679ffaad0c91e3446e8f89fd2c537dc1e8e
RomanenkoVladimir <volodymyr.romanenko@icloud.com>
2021-02-11 19:19:22 +0100

    Tech/1605/move to GitHub actions (#1730)
    * Create github-actions.yml
    
    * Delete .travis.yml
    
    Co-authored-by: JaiJoPa <50145538+JaiJoPa@users.noreply.github.com>

--- visualization/script/npm-publish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd visualization
-
-echo "makeshifting .npmrc with Token from Travis settings"
-./node_modules/.bin/makeshift -t $NPM_TOKEN
-
-echo "publishing to npm"
-npm publish --access public
