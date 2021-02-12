79c75d931ee8e369e20f622f747ca15111ee4d6f
Dmytro Hutkin <dmytro.hutkin@gmail.com>
2021-02-12 14:51:35 +0100

    Update tests #1099

--- visualization/app/codeCharta/ui/unfocusButton/unfocusButton.component.spec.ts
+++ visualization/app/codeCharta/ui/unfocusButton/unfocusButton.component.spec.ts
@@ -71,14 +71,51 @@ describe("UnfocusButtonController", () => {
 			expect(unfocusButtonController["_viewModel"].isParentFocused).toBeTruthy()
 			expect(unfocusButtonController["_viewModel"].isNodeFocused).toBeFalsy()
 		})
+
+		it("should clear focusedNodes if new files were loaded", () => {
+			UnfocusButtonController.NEW_FILES_LOADED = true
+			unfocusButtonController["_viewModel"].focusedNodes = ["/root/app", "/root/app/ui"]
+			CODE_MAP_BUILDING.node.id = TEST_DELTA_MAP_A.map.children[1].children[0].id
+			CODE_MAP_BUILDING.node.path = TEST_DELTA_MAP_A.map.children[1].children[0].path
+
+			unfocusButtonController.onBuildingRightClicked(CODE_MAP_BUILDING)
+
+			expect(unfocusButtonController["_viewModel"].focusedNodes).toEqual([])
+			expect(UnfocusButtonController.NEW_FILES_LOADED).toBe(false)
+		})
+
+		it("should update focusedNodes if new node became focused", () => {
+			unfocusButtonController["_viewModel"].focusedNodes = ["/root"]
+			CODE_MAP_BUILDING.node.id = TEST_DELTA_MAP_A.map.children[1].children[0].id
+			CODE_MAP_BUILDING.node.path = TEST_DELTA_MAP_A.map.children[1].children[0].path
+
+			storeService.dispatch(focusNode("/root/app"))
+
+			unfocusButtonController.onBuildingRightClicked(CODE_MAP_BUILDING)
+
+			expect(unfocusButtonController["_viewModel"].focusedNodes).toEqual(["/root", "/root/app"])
+		})
 	})
 
 	describe("removeFocusedNode", () => {
-		it("should apply empty focusedNodePath in storeService", () => {
-			storeService.dispatch(focusNode("/root"))
+		it("should apply previous focusedNodePath in storeService", () => {
+			unfocusButtonController["_viewModel"].focusedNodes = ["/root", "/root/app"]
+			storeService.dispatch(focusNode("/root/app"))
 
 			unfocusButtonController.removeFocusedNode()
 
+			expect(unfocusButtonController["_viewModel"].focusedNodes.length).toBe(1)
+			expect(unfocusButtonController["_viewModel"].focusedNodes).toEqual(["/root"])
+			//expect(storeService.getState().dynamicSettings.focusedNodePath).toEqual("/root")
+		})
+
+		it("should clear focusedNodePath in storeService", () => {
+			unfocusButtonController["_viewModel"].focusedNodes = ["/root", "/root/app"]
+			storeService.dispatch(focusNode("/root/app"))
+
+			unfocusButtonController.removeFocusedNode(true)
+
+			expect(unfocusButtonController["_viewModel"].focusedNodes.length).toBe(0)
 			expect(storeService.getState().dynamicSettings.focusedNodePath).toBe("")
 		})
 	})
