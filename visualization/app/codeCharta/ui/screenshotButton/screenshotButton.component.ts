002d1f86fb942bdd656015597fce541df4fd1311
Dmytro Hutkin <dmytro.hutkin@gmail.com>
2021-03-04 22:02:14 +0100

    Add png file name creation (#674)

--- visualization/app/codeCharta/ui/screenshotButton/screenshotButton.component.ts
+++ visualization/app/codeCharta/ui/screenshotButton/screenshotButton.component.ts
@@ -1,4 +1,6 @@
 import { Color, WebGLRenderer } from "three"
+import { getVisibleFileStates, isDeltaState, isPartialState, isSingleState } from "../../model/files/files.helper"
+import { StoreService } from "../../state/store.service"
 import { ThreeCameraService } from "../codeMap/threeViewer/threeCameraService"
 import { ThreeRendererService } from "../codeMap/threeViewer/threeRendererService"
 import { ThreeSceneService } from "../codeMap/threeViewer/threeSceneService"
@@ -9,14 +11,27 @@ export class ScreenshotButtonController {
 	constructor(
 		private threeSceneService: ThreeSceneService,
 		private threeCameraService: ThreeCameraService,
-		private threeRendererService: ThreeRendererService
+		private threeRendererService: ThreeRendererService,
+		private storeService: StoreService
 	) {}
 
 	makeScreenshot() {
 		const link = document.createElement("a")
-		link.setAttribute("download", "test.png")
+		link.setAttribute("download", this.makePNGFileName())
 		link.onclick = () => this.loadScript(link, this.threeRendererService.renderer)
 		link.click()
+	}
+
+	private makePNGFileName() {
+		const files = this.storeService.getState().files
+		const jsonFileNames = getVisibleFileStates(files)
+		const state = isSingleState(files) ? "single" : isPartialState(files) ? "partial" : isDeltaState(files) ? "delta" : ""
+		let pngFileName = ""
+		for (const fileState of jsonFileNames) {
+			const fileName = fileState.file.fileMeta.fileName
+			pngFileName += `${fileName.slice(0, fileName.indexOf(".json"))}_`
+		}
+		return `${state}_${pngFileName.slice(0, pngFileName.lastIndexOf("_"))}.png`
 	}
 
 	private loadScript(link: HTMLAnchorElement, renderer: WebGLRenderer) {
