737cb4191279ff60eb83ee3ba8585c8dbb2254de
unknown <JacobK.admin@DELL-G048793.mwea.de>
2021-03-26 20:56:24 +0100

    e2e tests check

--- visualization/app/codeCharta/ui/nodeContextMenu/nodeContextMenu.po.ts
+++ visualization/app/codeCharta/ui/nodeContextMenu/nodeContextMenu.po.ts
@@ -19,13 +19,4 @@ export class NodeContextMenuPageObject {
 	async isClosed() {
 		await page.waitForSelector("#codemap-context-menu", { hidden: true })
 	}
-
-	async areButtonsDisabled() {
-		await page.waitForSelector("#codemap-context-menu", { visible: true })
-		const selector = "#codemap-context-menu > div.button-group"
-		const condition = await page.$eval(selector, element => {
-			return element.classList.contains("div-disabled")
-		})
-		return condition
-	}
 }
