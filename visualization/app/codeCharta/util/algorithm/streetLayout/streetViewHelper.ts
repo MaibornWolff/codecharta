8c08f1c45096e70cbba97e1ad5753e85ff84c496
unknown <JacobK.admin@DELL-G048793.mwea.de>
2021-03-31 19:12:35 +0200

    repair exclusion in Treemap and streetmap layouts (not perfect in Treemap)

--- visualization/app/codeCharta/util/algorithm/streetLayout/streetViewHelper.ts
+++ visualization/app/codeCharta/util/algorithm/streetLayout/streetViewHelper.ts
@@ -19,9 +19,6 @@ function calculateSize(node: CodeMapNode, metricName: string) {
 }
 
 function mergeDirectories(node: CodeMapNode, metricName: string): CodeMapNode {
-	if(metricName !== 'unary'){
-
-	}
 	let mergedNode = node
 	const nodeSize = calculateSize(node, metricName)
 	for (const child of node.children) {
@@ -53,22 +50,19 @@ function getHeightValue(s: State, squaredNode: CodeMapNode, maxHeight: number, f
 function buildNodeFrom(layoutNode: CodeMapNode, heightScale: number, maxHeight: number, s: State, isDeltaState: boolean): Node {
 	const isNodeLeaf = !(layoutNode.children && layoutNode.children.length > 0)
 	const flattened: boolean = isNodeFlat(layoutNode, s)
-	let heightValue: number = getHeightValue(s, layoutNode, maxHeight, flattened)
-	console.log("heightValue : "+heightValue)
-	let height = Math.abs(
+	const heightValue: number = getHeightValue(s, layoutNode, maxHeight, flattened)
+	const height = Math.abs(
 		isNodeLeaf ? Math.max(heightScale * heightValue, TreeMapHelper.MIN_BUILDING_HEIGHT) : TreeMapHelper.FOLDER_HEIGHT
 	)
-	console.log("height : "+height)
-
-	const length = layoutNode.rect ? layoutNode.rect.height : 1
-	const x0 = layoutNode.rect ? layoutNode.rect.topLeft.x : 1
-	const y0 = layoutNode.rect ? layoutNode.rect.topLeft.y : 1
-	const z0 = layoutNode.zOffset ?layoutNode.zOffset * TreeMapHelper.FOLDER_HEIGHT : TreeMapHelper.FOLDER_HEIGHT
-	let width = layoutNode.rect ? layoutNode.rect.width : 1
+	const length = layoutNode.rect ? layoutNode.rect.height : 0.000000000001
+	const x0 = layoutNode.rect ? layoutNode.rect.topLeft.x : 0.000000000001
+	const y0 = layoutNode.rect ? layoutNode.rect.topLeft.y : 0.000000000001
+	const z0 = layoutNode.zOffset ? layoutNode.zOffset * TreeMapHelper.FOLDER_HEIGHT : TreeMapHelper.FOLDER_HEIGHT
+	const width = layoutNode.rect ? layoutNode.rect.width : 0.000000000001
 	return {
 		name: layoutNode.name,
 		id: layoutNode.id,
-		width: width,
+		width,
 		height,
 		length,
 		depth: layoutNode.zOffset ? layoutNode.zOffset : 0,
