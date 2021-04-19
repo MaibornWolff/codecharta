9ad7e633a77afeac68ff9ccb4f043eda8ee564c7
unknown <JacobK.admin@DELL-G048793.mwea.de>
2021-04-19 18:35:40 +0200

    prettier repair #901

--- visualization/app/codeCharta/ui/nodeContextMenu/nodeContextMenu.component.spec.ts
+++ visualization/app/codeCharta/ui/nodeContextMenu/nodeContextMenu.component.spec.ts
@@ -170,8 +170,8 @@ describe("nodeContextMenuController", () => {
 		it("should show everything is excluded if root or everything is excluded", () => {
 			codeMapPreRenderService.getRenderMap = jest.fn().mockReturnValue(VALID_NODE_EVERYTHING_EXCLUDED)
 			nodeContextMenuController.onShowNodeContextMenu("/root", NodeType.FOLDER, 42, 24)
-			expect(nodeContextMenuController["_viewModel"].isAllExcluded).toBeTruthy()
 
+			expect(nodeContextMenuController["_viewModel"].isAllExcluded).toBeTruthy() 
 		})	
 
 		it("should not shorten the path if it has no sub paths", () => {
