939b6e19a244716c6ad60c95c2878624ef8ead52
ChristianE <christian.eberhard@maibornwolff.de>
2021-05-10 13:13:47 +0200

    Fixes tests
    #1952

--- visualization/app/codeCharta/ui/metricChooser/metricChooser.po.ts
+++ visualization/app/codeCharta/ui/metricChooser/metricChooser.po.ts
@@ -15,7 +15,7 @@ export class MetricChooserPageObject {
 	}
 
 	async getAreaMetricValue(): Promise<number> {
-		await page.waitForSelector("area-metric-chooser-component .metric-value")
-		return page.$eval("area-metric-chooser-component .metric-value", element => element["innerText"])
+		await page.waitForSelector("area-metric-chooser-component .metric-value span")
+		return page.$eval("area-metric-chooser-component .metric-value span", element => element["innerText"])
 	}
 }
