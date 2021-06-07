81818c8b49b20e41f5b429a44b52d48e77801118
RomanenkoVladimir <volodymyr.romanenko@icloud.com>
2021-06-07 14:45:12 +0200

    Revert "Fix/353/map rotation not working near cube (#2028)"
    This reverts commit e923b11eb7bee04fa7e97e5319fbc7f9c977ff46.

--- visualization/app/codeCharta/ui/codeMap/codeMap.mouseEvent.service.ts
+++ visualization/app/codeCharta/ui/codeMap/codeMap.mouseEvent.service.ts
@@ -17,7 +17,6 @@ import { Object3D, Raycaster } from "three"
 import { CodeMapLabelService } from "./codeMap.label.service"
 import { LazyLoader } from "../../util/lazyLoader"
 import { CodeMapPreRenderService } from "./codeMap.preRender.service"
-import { ThreeViewerService } from "./threeViewer/threeViewerService"
 
 interface Coordinates {
 	x: number
@@ -77,9 +76,7 @@ export class CodeMapMouseEventService
 		private threeUpdateCycleService: ThreeUpdateCycleService,
 		private storeService: StoreService,
 		private codeMapLabelService: CodeMapLabelService,
-		private codeMapPreRenderService: CodeMapPreRenderService,
-		private viewCubeMouseEventsService: ViewCubeMouseEventsService,
-		private threeViewerService: ThreeViewerService
+		private codeMapPreRenderService: CodeMapPreRenderService
 	) {
 		this.threeUpdateCycleService.register(() => this.threeRendererService.render())
 		MapTreeViewLevelController.subscribeToHoverEvents(this.$rootScope, this)
@@ -115,8 +112,6 @@ export class CodeMapMouseEventService
 		this.threeRendererService.renderer.domElement.addEventListener("mouseup", event => this.onDocumentMouseUp(event))
 		this.threeRendererService.renderer.domElement.addEventListener("mousedown", event => this.onDocumentMouseDown(event))
 		this.threeRendererService.renderer.domElement.addEventListener("dblclick", () => this.onDocumentDoubleClick())
-		this.threeRendererService.renderer.domElement.addEventListener("mouseleave", event => this.onDocumentMouseLeave(event))
-		this.threeRendererService.renderer.domElement.addEventListener("mouseenter", () => this.onDocumentMouseEnter())
 		ViewCubeMouseEventsService.subscribeToEventPropagation(this.$rootScope, this)
 	}
 
@@ -260,23 +255,10 @@ export class CodeMapMouseEventService
 		return labelForBuilding
 	}
 
-	private EnableOrbitalsRotation(isRotation: boolean) {
-		this.threeViewerService.enableRotation(isRotation)
-		this.viewCubeMouseEventsService.enableRotation(isRotation)
-	}
-
-	onDocumentMouseEnter() {
-		this.EnableOrbitalsRotation(true)
-	}
-
-	onDocumentMouseLeave(event: MouseEvent) {
-		if (!(event.relatedTarget instanceof HTMLCanvasElement)) this.EnableOrbitalsRotation(false)
-	}
-
 	onDocumentMouseMove(event: MouseEvent) {
 		this.mouse.x = event.clientX
 		this.mouse.y = event.clientY
-		this.viewCubeMouseEventsService.propagateMovement()
+		this.updateHovering()
 	}
 
 	onDocumentDoubleClick() {
@@ -317,7 +299,6 @@ export class CodeMapMouseEventService
 	}
 
 	onDocumentMouseUp(event: MouseEvent) {
-		this.viewCubeMouseEventsService.resetIsDragging()
 		if (event.button === ClickType.LeftClick) {
 			this.onLeftClick()
 		} else {
