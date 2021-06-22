547b17ab1089ad8dfcb460bbcf7d8c0a7f486028
ngormsen <50167165+ngormsen@users.noreply.github.com>
2021-06-22 11:20:27 +0200

    Add removeFilebutton component to partial state #1954

--- visualization/app/codeCharta/ui/removeFileButton/removeFileButton.component.ts
+++ visualization/app/codeCharta/ui/removeFileButton/removeFileButton.component.ts
@@ -1,5 +1,5 @@
 import "./removeFileButton.component.scss"
-import { removeFile, setSingleByName } from "../../state/store/files/files.actions"
+import { removeFile, setMultipleByNames, setSingleByName } from "../../state/store/files/files.actions"
 import { removeRecentFile } from "../../state/store/dynamicSettings/recentFiles/recentFiles.actions"
 import { StoreService } from "../../state/store.service"
 import { FileSelectionState } from "../../model/files/files"
@@ -7,9 +7,15 @@ import { CodeChartaService } from "../../codeCharta.service"
 
 export class RemoveFileButtonController {
 	private filename: string
+	private state: string
 
 	/* @ngInject */
 	constructor(private storeService: StoreService) {}
+
+	onRemoveFile($event): void {
+		if (this.state === FileSelectionState.Single) this.onSingleRemoveFile($event)
+		else this.onPartialRemoveFile($event)
+	}
 
 	onSingleRemoveFile($event): void {
 		this.storeService.dispatch(removeFile(this.filename))
@@ -22,10 +28,27 @@ export class RemoveFileButtonController {
 		$event.preventDefault()
 	}
 
+	onPartialRemoveFile($event): void {
+		this.storeService.dispatch(removeFile(this.filename))
+		this.storeService.dispatch(removeRecentFile(this.filename))
+
+		const allRemainingFiles = this.storeService.getState().files.map(x => x.file.fileMeta.fileName)
+		this.onPartialFilesChange(allRemainingFiles)
+
+		$event.stopPropagation()
+		$event.preventDefault()
+	}
+
 	onSingleFileChange(singleFileName: string) {
 		this.storeService.dispatch(setSingleByName(singleFileName))
 		const rootName = this.storeService.getState().files.find(x => x.selectedAs === FileSelectionState.Single).file.map.name
 		CodeChartaService.updateRootData(rootName)
+	}
+
+	onPartialFilesChange(partialFileNames: string[]) {
+		if (partialFileNames.length > 0) {
+			this.storeService.dispatch(setMultipleByNames(partialFileNames))
+		}
 	}
 }
 
@@ -33,5 +56,5 @@ export const removeFileButtonComponent = {
 	selector: "removeFileButtonComponent",
 	template: require("./removeFileButton.component.html"),
 	controller: RemoveFileButtonController,
-	bindings: { filename: "@" }
+	bindings: { filename: "@", state: "@" }
 }
