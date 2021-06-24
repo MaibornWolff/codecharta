e3f9717a817e5dc9f5b0edcd9bb20ad80d112751
ngormsen <50167165+ngormsen@users.noreply.github.com>
2021-06-24 10:09:04 +0200

    Add first tests for JasomeImporter #1702

---
+++ analysis/import/JasomeImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/jasome/JasomeImporterTest.kt
@@ -0,0 +1,39 @@
+package de.maibornwolff.codecharta.importer.jasome
+
+import de.maibornwolff.codecharta.importer.jasome.JasomeImporter.Companion.main
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertTrue
+import java.io.File
+
+
+internal class JasomeImporterTest{
+
+    @Test
+    fun `should create json uncompressed file`() {
+        val cliResult = main(arrayOf("src/test/resources/jasome.xml", "-nc", "-o=src/test/resources/jasome.cc.json"))
+        val file = File("src/test/resources/jasome.cc.json")
+        file.deleteOnExit()
+
+        assertTrue(file.exists())
+    }
+
+    @Test
+    fun `should create json gzip file`() {
+        val cliResult = main(arrayOf("src/test/resources/jasome.xml", "-o=src/test/resources/jasome.cc.json"))
+        val file = File("src/test/resources/jasome.cc.json.gz")
+        file.deleteOnExit()
+
+        assertTrue(file.exists())
+    }
+
+    @Test
+    fun `should contain RTLOC of 149`(){
+        val cliResult = main(arrayOf("src/test/resources/jasome.xml", "-nc", "-o=src/test/resources/jasome.cc.json"))
+        val file = File("src/test/resources/jasome.cc.json")
+        file.deleteOnExit()
+
+        assertThat(file.readText()).contains(listOf("\"RTLOC\":149"))
+    }
+
+}
