669d952110976019decdf006502e5299456a42b0
ngormsen <50167165+ngormsen@users.noreply.github.com>
2021-06-24 11:54:27 +0200

    Add tests for UnderstandImporter #1702

---
+++ analysis/import/UnderstandImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/understand/UnderstandImporterTest.kt
@@ -0,0 +1,37 @@
+import de.maibornwolff.codecharta.importer.understand.UnderstandImporter.Companion.main
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertTrue
+import java.io.File
+
+
+internal class CSVImporterTest{
+
+    @Test
+    fun `should create json uncompressed file`() {
+        val cliResult = main(arrayOf("src/test/resources/understand.csv", "-nc", "-o=src/test/resources/understand.cc.json"))
+        val file = File("src/test/resources/understand.cc.json")
+        file.deleteOnExit()
+
+        assertTrue(file.exists())
+    }
+
+    @Test
+    fun `should create json gzip file`() {
+        val cliResult = main(arrayOf("src/test/resources/understand.csv", "-o=src/test/resources/understand.cc.json"))
+        val file = File("src/test/resources/understand.cc.json.gz")
+        file.deleteOnExit()
+
+        assertTrue(file.exists())
+    }
+
+    @Test
+    fun `should contain Lines value of 44`(){
+        val cliResult = main(arrayOf("src/test/resources/understand.csv", "-nc", "-o=src/test/resources/understand.cc.json"))
+        val file = File("src/test/resources/understand.cc.json")
+        file.deleteOnExit()
+
+        assertThat(file.readText()).contains(listOf("\"max_lcom\":50"))
+    }
+
+}
