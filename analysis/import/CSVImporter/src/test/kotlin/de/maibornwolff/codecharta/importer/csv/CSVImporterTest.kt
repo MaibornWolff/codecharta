786ee2b4b34f58c0785d601636e2c56c9b9aa67f
ngormsen <50167165+ngormsen@users.noreply.github.com>
2021-06-25 12:24:34 +0200

    Fix test #1702

--- analysis/import/CSVImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/csv/CSVImporterTest.kt
+++ analysis/import/CSVImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/csv/CSVImporterTest.kt
@@ -8,8 +8,7 @@ internal class CSVImporterTest {
 
     @Test
     fun `should create json uncompressed file`() {
-        val cliResult =
-                main(arrayOf("src/test/resources/sourcemonitor.csv", "-nc",
+        main(arrayOf("src/test/resources/sourcemonitor.csv", "-nc",
                         "-o=src/test/resources/sourcemonitor.cc.json"))
         val file = File("src/test/resources/sourcemonitor.cc.json")
         file.deleteOnExit()
@@ -19,8 +18,7 @@ internal class CSVImporterTest {
 
     @Test
     fun `should create json gzip file`() {
-        val cliResult =
-                main(arrayOf("src/test/resources/sourcemonitor.csv", "-o=src/test/resources/sourcemonitor.cc.json"))
+        main(arrayOf("src/test/resources/sourcemonitor.csv", "-o=src/test/resources/sourcemonitor.cc.json"))
         val file = File("src/test/resources/sourcemonitor.cc.json.gz")
         file.deleteOnExit()
 
@@ -29,8 +27,7 @@ internal class CSVImporterTest {
 
     @Test
     fun `should contain Lines value of 44`() {
-        val cliResult =
-                main(arrayOf("src/test/resources/sourcemonitor.csv", "-nc",
+        main(arrayOf("src/test/resources/sourcemonitor.csv", "-nc",
                         "-o=src/test/resources/sourcemonitor.cc.json"))
         val file = File("src/test/resources/sourcemonitor.cc.json")
         file.deleteOnExit()
