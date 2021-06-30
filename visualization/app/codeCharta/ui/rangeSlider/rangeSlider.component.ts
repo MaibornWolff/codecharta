39f343e65da72c630683bce52bbb006343355449
ngormsen <50167165+ngormsen@users.noreply.github.com>
2021-07-01 00:44:58 +0200

    Fix slider initialisation #2077 (#2081)

--- visualization/app/codeCharta/ui/rangeSlider/rangeSlider.component.ts
+++ visualization/app/codeCharta/ui/rangeSlider/rangeSlider.component.ts
@@ -69,7 +69,7 @@ export class RangeSliderController
 	}
 
 	forceSliderRender() {
-		this.$rootScope.$broadcast("rzSliderForceRender")
+		angular.element(() => this.$rootScope.$broadcast("rzSliderForceRender"))
 	}
 
 	onBlacklistChanged() {
