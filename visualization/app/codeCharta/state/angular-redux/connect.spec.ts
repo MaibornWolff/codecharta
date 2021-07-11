0ae28e48c97b2c07499201d3ac0b4ae5ed34ef6d
shaman-apprentice <Torsten.Knauf@gmx.de>
2021-07-11 12:51:07 +0200

    refactor: add mixin for connecting to redux store
    refs #2291

---
+++ visualization/app/codeCharta/state/angular-redux/connect.spec.ts
@@ -0,0 +1,32 @@
+import { setSortingOrderAscending } from "../store/appSettings/sortingOrderAscending/sortingOrderAscending.actions"
+import { sortingOrderAscendingSelector } from "../store/appSettings/sortingOrderAscending/sortingOrderAscending.selector"
+import { CcState, Store } from "../store/store"
+import { connect } from "./connect"
+
+describe("connect", () => {
+	beforeEach(() => {
+		Store["initialize"]()
+		jest.spyOn(Store["_store"], "dispatch")
+	})
+
+	it("should map state to this", () => {
+		const Connected = connect((state: CcState) => ({ orderAscending: sortingOrderAscendingSelector(state) }))
+		const connected = new (class extends Connected {})()
+		expect(connected.orderAscending).toBe(false)
+	})
+
+	it("should update its mapped state", () => {
+		const Connected = connect((state: CcState) => ({ orderAscending: sortingOrderAscendingSelector(state) }))
+		const connected = new (class extends Connected {})()
+		Store.store.dispatch(setSortingOrderAscending(true))
+		expect(connected.orderAscending).toBe(true)
+	})
+
+	it("should map dispatch to this", () => {
+		const Connected = connect(undefined, { setSortingOrderAscending })
+		const connected = new (class extends Connected {})()
+		expect(typeof connected.setSortingOrderAscending).toBe("function")
+		connected.setSortingOrderAscending(true)
+		expect(Store.store.dispatch).toHaveBeenCalledTimes(1)
+	})
+})
