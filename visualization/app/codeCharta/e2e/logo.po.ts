8c067ea1a9439ad342bb602d40dfca7816518aa5
ChristianE <christian.eberhard@maibornwolff.de>
2021-07-29 11:12:36 +0200

    Fixes tests

--- visualization/app/codeCharta/e2e/logo.po.ts
+++ visualization/app/codeCharta/e2e/logo.po.ts
@@ -1,17 +1,17 @@
 export class LogoPageObject {
 	async getVersion() {
-		await page.waitForSelector(".logo > .version")
-		const versionString = await page.$eval(".logo > .version", element => element["innerText"])
+		await page.waitForSelector("#logo > .version")
+		const versionString = await page.$eval("#logo > .version", element => element["innerText"])
 		return versionString.split(" ")[1]
 	}
 
 	async getLink() {
-		await page.waitForSelector(".logo > a")
-		return page.$eval(".logo > a", element => element["href"])
+		await page.waitForSelector("#logo > a")
+		return page.$eval("#logo > a", element => element["href"])
 	}
 
 	async getImageSrc() {
-		await page.waitForSelector(".logo > a > img")
-		return page.$eval(".logo > a > img", element => element["src"])
+		await page.waitForSelector("#logo > a > img")
+		return page.$eval("#logo > a > img", element => element["src"])
 	}
 }
