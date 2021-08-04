bcf828ee765068557e501072a126cdffafceebf5
IhsenBouallegue <bouallegueihsen@gmail.com>
2021-08-04 12:38:37 +0200

    Show only given version in the Changelog #1315

--- visualization/app/codeCharta/ui/dialog/dialog.changelog.component.ts
+++ visualization/app/codeCharta/ui/dialog/dialog.changelog.component.ts
@@ -1,17 +1,19 @@
 import "./dialog.changelog.component.scss"
 // @ts-ignore
-import md from "../../../../../CHANGELOG.md";
+import md from "../../../../../CHANGELOG.md"
+
 // import parseChangelog from 'changelog-parser'
 
 export class DialogChangelogController {
 	private _viewModel: {
-		title: string
+		version: string
 		md: any
 	} = {
-		title: "",
-		md: null,
+		version: "",
+		md: null
 	}
-    constructor(private $mdDialog) {
+
+	constructor(private $mdDialog) {
 		"ngInject"
 
 		// parseChangelog({
@@ -24,19 +26,29 @@ export class DialogChangelogController {
 		// 	console.error(err)
 		// })
 		// console.log(angular.element(document.querySelector('#change')));
-		console.log(md.split("\n").search(/^<h3>/));
-		this._viewModel.md = md.split("\n").slice(1).join("\n");
-		this._viewModel.title = "x";
+		const changelogLines = md.split("\n")
+		const newVersionLine = this.findVersionLine(changelogLines)
+		const endVersionLine = this.findEndVersionLine(changelogLines, newVersionLine)
+		this._viewModel.md = changelogLines.slice(newVersionLine, endVersionLine).join("\n")
 	}
+
 	hide() {
 		this.$mdDialog.hide()
+	}
+
+	findVersionLine(lines) {
+		return lines.findIndex(element => element.search(/1\.73\.0/) > -1)
+	}
+
+	findEndVersionLine(lines, newVersionLine) {
+		return newVersionLine + lines.slice(newVersionLine + 1).findIndex(element => element.search(/<h2>/) > -1)
 	}
 }
 
 export const dialogChangelogComponent = {
-    selector: "dialogChangelogComponent",
-    template: require("./dialog.changelog.component.html"),
-    controller: DialogChangelogController,
+	selector: "dialogChangelogComponent",
+	template: require("./dialog.changelog.component.html"),
+	controller: DialogChangelogController,
 	clickOutsideToClose: true,
 	controllerAs: "$ctrl"
 }
