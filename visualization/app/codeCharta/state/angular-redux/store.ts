62c9d58af9b1f2b17baa5e28f9bc0d476cecaa4c
shaman-apprentice <Torsten.Knauf@gmx.de>
2021-09-11 20:21:06 +0200

    refactor: (WIP) add tests
    refs: #2318

--- visualization/app/codeCharta/state/angular-redux/store.ts
+++ visualization/app/codeCharta/state/angular-redux/store.ts
@@ -4,23 +4,20 @@ import { Observable } from "rxjs"
 import { CCAction } from "../../codeCharta.model"
 import { CcState, Store as PlainStore } from "../store/store"
 
-type Selector<T> = (state: CcState) => T
-
-// todo: Add tests
-
 @Injectable({
 	providedIn: "root"
 })
 export class Store {
-	select<T>(selector: Selector<T>): Observable<T> {
+	select<T>(selector: (state: CcState) => T): Observable<T> {
 		return new Observable<T>(subscriber => {
 			let lastValue = selector(PlainStore.store.getState())
 			subscriber.next(lastValue)
+
 			return PlainStore.store.subscribe(() => {
-				const newValue = selector(PlainStore.store.getState())
-				if (lastValue !== newValue) {
-					lastValue = newValue
-					subscriber.next(newValue)
+				const value = selector(PlainStore.store.getState())
+				if (lastValue !== value) {
+					lastValue = value
+					subscriber.next(value)
 				}
 			})
 		})
