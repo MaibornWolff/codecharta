d2fe8c9b2d81f62f4f5a65d7ff9a6cffad5eec0d
shaman-apprentice <Torsten.Knauf@gmx.de>
2021-10-03 18:58:37 +0200

    refactor: introduce NgRx like connection to store
    When all connections are mapped, we can more easily replace Redux with
    NgRx, which is best practise in Angular community.
    
    Refs: #2318

--- visualization/app/codeCharta/state/store/appSettings/sortingOrderAscending/sortingOrderAscending.reducer.spec.ts
+++ visualization/app/codeCharta/state/store/appSettings/sortingOrderAscending/sortingOrderAscending.reducer.spec.ts
@@ -1,5 +1,5 @@
 import { sortingOrderAscending } from "./sortingOrderAscending.reducer"
-import { SortingOrderAscendingAction, setSortingOrderAscending } from "./sortingOrderAscending.actions"
+import { SortingOrderAscendingAction, setSortingOrderAscending, toggleSortingOrderAscending } from "./sortingOrderAscending.actions"
 
 describe("sortingOrderAscending", () => {
 	describe("Default State", () => {
@@ -21,6 +21,15 @@ describe("sortingOrderAscending", () => {
 			const result = sortingOrderAscending(true, setSortingOrderAscending())
 
 			expect(result).toEqual(false)
+		})
+	})
+
+	describe("Action: TOGGLE_SORTING_ORDER_ASCENDING", () => {
+		it("should toggle state", () => {
+			const result = sortingOrderAscending(false, setSortingOrderAscending(true))
+			const toggledResult = sortingOrderAscending(result, toggleSortingOrderAscending())
+
+			expect(toggledResult).toBe(!result)
 		})
 	})
 })
