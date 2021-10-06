f91ffad093507f272feaac9ce85669722709a249
Cedrik Bormann <cedrikbormann@gmail.com>
2021-10-06 08:29:04 +0200

    Add FloorLabelHelper Unit Tests #2348

--- visualization/app/codeCharta/ui/codeMap/threeViewer/floorLabels/floorLabelHelper.spec.ts
+++ visualization/app/codeCharta/ui/codeMap/threeViewer/floorLabels/floorLabelHelper.spec.ts
@@ -1,7 +1,43 @@
-describe("FloorLabelHelper", () => {
-	beforeEach(() => {})
+import { FloorLabelHelper } from "./floorLabelHelper"
 
-	describe("", () => {
-		it("please implement me", () => {})
+describe("FloorLabelHelper", () => {
+	function appendMapCanvas(mapCanvasWidth) {
+		const fakeMapCanvas = document.createElement("canvas")
+		fakeMapCanvas.width = fakeMapCanvas.height = mapCanvasWidth
+
+		const fragment = document.createDocumentFragment()
+		fragment.appendChild(fakeMapCanvas)
+
+		document.getElementsByTagName = jest.fn().mockReturnValue(fragment.children)
+	}
+
+	describe("getMapResolutionScaling", () => {
+		it("should not scale since map width is smaller than scaling threshold (four times the display width)", () => {
+			appendMapCanvas(500)
+
+			expect(FloorLabelHelper.getMapResolutionScaling(400)).toBe(1)
+		})
+
+		it("should scale since map width is greater than scaling threshold (four times the display width)", () => {
+			const displayWidth = 1000
+			appendMapCanvas(displayWidth)
+
+			const mapWidth = 5000
+			expect(FloorLabelHelper.getMapResolutionScaling(mapWidth)).toBe((displayWidth * 4) / mapWidth)
+		})
+
+		it("should not scale since map width is smaller than scaling threshold (four times the width of FullHD+)", () => {
+			appendMapCanvas(1000)
+
+			expect(FloorLabelHelper.getMapResolutionScaling(400)).toBe(1)
+		})
+
+		it("should scale since map width is greater than scaling threshold (four times the width of FullHD+)", () => {
+			const fullHdPlusWidth = 2560
+			appendMapCanvas(fullHdPlusWidth * 5)
+
+			const mapWidth = fullHdPlusWidth * 6
+			expect(FloorLabelHelper.getMapResolutionScaling(mapWidth)).toBe((fullHdPlusWidth * 4) / mapWidth)
+		})
 	})
 })
