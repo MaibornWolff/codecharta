f35eb2b658ff1ae79bab97ee9f15d94bb2cafce8
Shivan-997 <shivanoskan.1997@hotmail.com>
2021-10-11 11:19:59 +0200

    add new Methode Signature to createSelector

--- visualization/app/codeCharta/state/angular-redux/createSelector.ts
+++ visualization/app/codeCharta/state/angular-redux/createSelector.ts
@@ -11,6 +11,10 @@ export function createSelector<S1, S2, S3, P, State = CcState>(
 	selectors: [Selector<S1, State>, Selector<S2, State>?, Selector<S3, State>?],
 	projector: (s1: S1, s2?: S2, s3?: S3) => P
 ): (state: State) => P
+export function createSelector<S1, S2, S3, S4, P, State = CcState>(
+	selectors: [Selector<S1, State>, Selector<S2, State>?, Selector<S3, State>?, Selector<S4, State>?],
+	projector: (s1: S1, s2?: S2, s3?: S3, s4?: S4) => P
+): (state: State) => P
 
 export function createSelector<S1, S2, S3, S4, P, State = CcState>(
 	selectors: [Selector<S1, State>, Selector<S2, State>?, Selector<S3, State>?, Selector<S4, State>?],
