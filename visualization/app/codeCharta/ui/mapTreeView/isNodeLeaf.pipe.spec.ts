d2a6081def16d9596bb146610e4f602f3a2f4a26
Torsten Knauf <Torsten.Knauf@maibornwolff.de>
2021-10-11 19:52:10 +0200

    refactor: adjust path of previous moved file
    Refs: #2318

--- visualization/app/codeCharta/ui/mapTreeView/isNodeLeaf.pipe.spec.ts
+++ visualization/app/codeCharta/ui/mapTreeView/isNodeLeaf.pipe.spec.ts
@@ -1,14 +1,14 @@
+import { CodeMapNode } from "../../codeCharta.model"
 import { IsNodeLeafPipe } from "./isNodeLeaf.pipe"
-import { CodeMapNode } from "../../../codeCharta.model"
 
 describe("IsNodeLeafPipe", () => {
 	it("should be true if node is a leaf", () => {
-		const fakeNode = {} as unknown as CodeMapNode
+		const fakeNode = ({} as unknown) as CodeMapNode
 		expect(new IsNodeLeafPipe().transform(fakeNode)).toBeTruthy()
 	})
 
 	it("should be false if node is a folder", () => {
-		const fakeNode = { children: [{}] } as unknown as CodeMapNode
+		const fakeNode = ({ children: [{}] } as unknown) as CodeMapNode
 		expect(new IsNodeLeafPipe().transform(fakeNode)).toBeFalsy()
 	})
 })
