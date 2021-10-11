064470e64870c89689502579b5b0769f3f253913
Torsten Knauf <Torsten.Knauf@maibornwolff.de>
2021-10-11 15:03:21 +0200

    refactor: extract mapTreeViewLevelModule
    This has a better encapsulation.
    
    It will also reduce boiler code in tests to come.
    
    Refs: #2318

--- visualization/app/app.module.ts
+++ visualization/app/app.module.ts
@@ -6,35 +6,13 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic"
 import { UpgradeModule } from "@angular/upgrade/static"
 
 import { SortingButtonComponent } from "./codeCharta/ui/sortingButton/sortingButton.component"
-import { MapTreeViewItemIcon } from "./codeCharta/ui/mapTreeView/mapTreeViewItemIcon/mapTreeViewItemIcon.component"
-import { MapTreeViewItemIconClassPipe } from "./codeCharta/ui/mapTreeView/mapTreeViewItemIcon/mapTreeViewItemIconClass.pipe"
-import { MapTreeViewItemIconColorPipe } from "./codeCharta/ui/mapTreeView/mapTreeViewItemIcon/mapTreeViewItemIconColor.pipe"
-import { MapTreeViewItemOptionButtonsComponent } from "./codeCharta/ui/mapTreeView/mapTreeViewItemOptionButtons/mapTreeViewItemOptionButtons.component"
-import { MapTreeViewItemName } from "./codeCharta/ui/mapTreeView/mapTreeViewItemName/mapTreeViewItemName.component"
-import { MapTreeViewItemSearchedNameHighlightPipe } from "./codeCharta/ui/mapTreeView/mapTreeViewItemName/mapTreeViewItemSearchedNameHighlight.pipe"
-import { IsNodeLeafPipe } from "./codeCharta/ui/mapTreeView/mapTreeViewItemName/isNodeLeaf.pipe"
 import { MapTreeViewLevel } from "./codeCharta/ui/mapTreeView/mapTreeViewLevel/mapTreeViewLevel.component"
+import { MapTreeViewLevelModule } from "./codeCharta/ui/mapTreeView/mapTreeViewLevel.module"
 
 @NgModule({
-	imports: [BrowserModule, UpgradeModule],
-	declarations: [
-		SortingButtonComponent,
-		MapTreeViewItemIcon,
-		MapTreeViewItemIconClassPipe,
-		MapTreeViewItemIconColorPipe,
-		MapTreeViewItemOptionButtonsComponent,
-		MapTreeViewItemName,
-		MapTreeViewLevel,
-		MapTreeViewItemSearchedNameHighlightPipe,
-		IsNodeLeafPipe
-	],
-	entryComponents: [
-		SortingButtonComponent,
-		MapTreeViewItemIcon,
-		MapTreeViewItemOptionButtonsComponent,
-		MapTreeViewItemName,
-		MapTreeViewLevel
-	]
+	imports: [BrowserModule, UpgradeModule, MapTreeViewLevelModule],
+	declarations: [SortingButtonComponent],
+	entryComponents: [SortingButtonComponent, MapTreeViewLevel]
 })
 export class AppModule {
 	constructor(@Inject(UpgradeModule) private upgrade: UpgradeModule) {}
--- visualization/app/codeCharta/ui/mapTreeView/mapTreeViewItemOptionButtons/mapTreeViewItemOptionButtons.component.ts
+++ visualization/app/codeCharta/ui/mapTreeView/mapTreeViewItemOptionButtons/mapTreeViewItemOptionButtons.component.ts
@@ -4,7 +4,7 @@ import { Component, Input } from "@angular/core"
 	selector: "cc-map-tree-view-item-option-buttons",
 	template: require("./mapTreeViewItemOptionButtons.component.html")
 })
-export class MapTreeViewItemOptionButtonsComponent {
+export class MapTreeViewItemOptionButtons {
 	@Input() isHovered: boolean
 	@Input() isFlattened: boolean
 	@Input() openNodeContextMenu: (event: MouseEvent) => unknown
