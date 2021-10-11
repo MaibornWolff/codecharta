750d60116280a69041dc51b4748f57dbdd7e8109
Torsten Knauf <Torsten.Knauf@maibornwolff.de>
2021-10-11 20:14:55 +0200

    refactor: make prettier happy
    Refs: #2318

--- visualization/app/codeCharta/ui/mapTreeView/isNodeLeaf.pipe.spec.ts
+++ visualization/app/codeCharta/ui/mapTreeView/isNodeLeaf.pipe.spec.ts
@@ -3,12 +3,12 @@ import { IsNodeLeafPipe } from "./isNodeLeaf.pipe"
 
 describe("IsNodeLeafPipe", () => {
 	it("should be true if node is a leaf", () => {
-		const fakeNode = ({} as unknown) as CodeMapNode
+		const fakeNode = {} as unknown as CodeMapNode
 		expect(new IsNodeLeafPipe().transform(fakeNode)).toBeTruthy()
 	})
 
 	it("should be false if node is a folder", () => {
-		const fakeNode = ({ children: [{}] } as unknown) as CodeMapNode
+		const fakeNode = { children: [{}] } as unknown as CodeMapNode
 		expect(new IsNodeLeafPipe().transform(fakeNode)).toBeFalsy()
 	})
 })
