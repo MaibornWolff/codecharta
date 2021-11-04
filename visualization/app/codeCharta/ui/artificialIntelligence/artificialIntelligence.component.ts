666a7e448b963bdf776f931bd67fedcef290651c
Maria Hallmann <72517530+Hall-Ma@users.noreply.github.com>
2021-11-04 17:30:42 +0100

    Fix/2434/no-calculations-of-suspicious-metrics-when-experimental-mode-disabled (#2471)
    * Add condition when calculations are needed to be performed
    #2434
    
    * Integrate ExperimentalFeaturesEnabledSubscriber
    #2434
    
    * Tests work in progress
    #2434
    
    * WIP: Add tests #2434
    
    * Add green Tests for testing calculations when experimental features are enabled or disabled
    #2434
    
    * Update implementation, tests and change log
    #2434
    
    * Update CHANGELOG.md
    
    Co-authored-by: Hall-Ma <maria.hallmann@tudent.htw-berlin.de>
    Co-authored-by: IhsenBouallegue <bouallegueihsen@gmail.com>
    Co-authored-by: Cedrik Bormann <26900540+ce-bo@users.noreply.github.com>

--- visualization/app/codeCharta/ui/artificialIntelligence/artificialIntelligence.component.ts
+++ visualization/app/codeCharta/ui/artificialIntelligence/artificialIntelligence.component.ts
@@ -18,6 +18,10 @@ import { ThreeOrbitControlsService } from "../codeMap/threeViewer/threeOrbitCont
 import { ThreeCameraService } from "../codeMap/threeViewer/threeCameraService"
 import { BlacklistService, BlacklistSubscriber } from "../../state/store/fileSettings/blacklist/blacklist.service"
 import { isPathBlacklisted } from "../../util/codeMapHelper"
+import {
+	ExperimentalFeaturesEnabledService,
+	ExperimentalFeaturesEnabledSubscriber
+} from "../../state/store/appSettings/enableExperimentalFeatures/experimentalFeaturesEnabled.service"
 
 interface MetricValues {
 	[metric: string]: number[]
@@ -37,7 +41,9 @@ interface MetricSuggestionParameters {
 	outlierCustomConfigId?: string
 }
 
-export class ArtificialIntelligenceController implements FilesSelectionSubscriber, BlacklistSubscriber {
+export class ArtificialIntelligenceController
+	implements FilesSelectionSubscriber, BlacklistSubscriber, ExperimentalFeaturesEnabledSubscriber
+{
 	private _viewModel: {
 		analyzedProgrammingLanguage: string
 		suspiciousMetricSuggestionLinks: MetricSuggestionParameters[]
@@ -73,10 +79,17 @@ export class ArtificialIntelligenceController implements FilesSelectionSubscribe
 		"ngInject"
 		FilesService.subscribe(this.$rootScope, this)
 		BlacklistService.subscribe(this.$rootScope, this)
+		ExperimentalFeaturesEnabledService.subscribe(this.$rootScope, this)
 
 		this.debounceCalculation = debounce(() => {
 			this.calculate()
 		}, 10)
+	}
+
+	onExperimentalFeaturesEnabledChanged(experimentalFeaturesEnabled: boolean) {
+		if (experimentalFeaturesEnabled) {
+			this.debounceCalculation()
+		}
 	}
 
 	applyCustomConfig(configId: string) {
@@ -103,6 +116,11 @@ export class ArtificialIntelligenceController implements FilesSelectionSubscribe
 	}
 
 	private calculate() {
+		const { experimentalFeaturesEnabled } = this.storeService.getState().appSettings
+		if (!experimentalFeaturesEnabled) {
+			return
+		}
+
 		const mainProgrammingLanguage = this.getMostFrequentLanguage(this.fileState.file.map)
 		this._viewModel.analyzedProgrammingLanguage = mainProgrammingLanguage
 
