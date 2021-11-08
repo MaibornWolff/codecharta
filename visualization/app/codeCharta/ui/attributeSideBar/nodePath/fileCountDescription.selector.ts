c9ad47132be6b18708a93dabe55bd30e654f0ae5
Torsten Knauf <Torsten.Knauf@maibornwolff.de>
2021-11-08 14:19:40 +0100

    refactor: improve type and add tests
    refs #2318

--- visualization/app/codeCharta/ui/attributeSideBar/nodePath/fileCountDescription.selector.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/nodePath/fileCountDescription.selector.ts
@@ -1,11 +1,21 @@
 import { createSelector } from "../../../state/angular-redux/createSelector"
 import { selectedBuildingSelector } from "../../../state/selectors/selectedBuilding.selector"
 
-export const fileCountDescriptionSelector = createSelector([selectedBuildingSelector], selectedBuilding => {
-	if (!selectedBuilding) return
+type BuildingWithFileCountDescription = {
+	node?: {
+		attributes?: {
+			unary?: number
+		}
+	}
+}
 
-	const fileCount = selectedBuilding.node?.attributes?.unary ?? 0
+export const building2fileCountDescription = (building: BuildingWithFileCountDescription | undefined) => {
+	if (!building) return
+
+	const fileCount = building.node?.attributes?.unary ?? 0
 	if (fileCount === 0) return "empty"
 	if (fileCount === 1) return "1 file"
 	return `${fileCount} files`
-})
+}
+
+export const fileCountDescriptionSelector = createSelector([selectedBuildingSelector], building2fileCountDescription)
