4459cd8b00f35154afebb7f8c46aeab2be43060f
Shivan-997 <shivanoskan.1997@hotmail.com>
2021-11-09 17:17:36 +0100

    Fix compatibility md5 with url query #2493

--- visualization/app/codeCharta/util/urlExtractor.ts
+++ visualization/app/codeCharta/util/urlExtractor.ts
@@ -1,7 +1,8 @@
 "use strict"
 import { ILocationService, IHttpService } from "angular"
 import { NameDataPair } from "../codeCharta.model"
-import { ExportCCFile } from "../codeCharta.api.model"
+import { ExportCCFile, ExportWrappedCCFile } from "../codeCharta.api.model"
+import md5 from "md5"
 
 export class UrlExtractor {
 	constructor(private $location: ILocationService, private $http: IHttpService) {}
@@ -39,8 +40,25 @@ export class UrlExtractor {
 			throw new Error(`Filename is missing`)
 		}
 		const response = await this.$http.get(fileName)
+		let content: ExportCCFile
+		try {
+			const fileContent = response.data as ExportWrappedCCFile | ExportCCFile
+
+			if ("data" in fileContent && "checksum" in fileContent) {
+				content = fileContent.data
+
+				content.fileChecksum = fileContent.checksum ? fileContent.checksum : md5(response.data)
+			} else {
+				if (!fileContent.fileChecksum) {
+					fileContent.fileChecksum = md5(response.data)
+				}
+				content = fileContent
+			}
+		} catch {
+			// Explicitly ignored
+		}
 		if (response.status >= 200 && response.status < 300) {
-			return { fileName, fileSize: response.data.toString().length, content: response.data as ExportCCFile }
+			return { fileName, fileSize: response.data.toString().length, content }
 		}
 		throw new Error(`Could not load file "${fileName}"`)
 	}
