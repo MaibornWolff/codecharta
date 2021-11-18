62e09b200f7de1892e6015e57919ea8e521e7ceb
shaman-apprentice <Torsten.Knauf@gmx.de>
2021-11-18 18:37:49 +0100

    ci: remove plop
    - as it is not used
    - as it is setup to AngularJS, which we are migrating away
    
    In the future we might use Angular's `ng generate` tooling
    
    close #2289

--- visualization/app/codeCharta/state/store/treeMap/treeMap.reducer.ts
+++ visualization/app/codeCharta/state/store/treeMap/treeMap.reducer.ts
@@ -1,9 +1,7 @@
-// Plop: Append reducer import here
 import { combineReducers } from "redux"
 import { mapSize } from "./mapSize/mapSize.reducer"
 
 const treeMap = combineReducers({
-	// Plop: Append sub-reducer here
 	mapSize
 })
 
