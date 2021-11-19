e8e867fb139f92c8d53f836ff1f39dbb4acb20be
Hall-Ma <maria.hallmann@tudent.htw-berlin.de>
2021-11-19 14:28:49 +0100

    Update file panel state buttons #2506

--- visualization/app/codeCharta/ui/filePanel/filePanelStateButtons/filePanelStateButtons.component.ts
+++ visualization/app/codeCharta/ui/filePanel/filePanelStateButtons/filePanelStateButtons.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core"
+import { Component, Input } from "@angular/core"
+import { FilePanelState } from "../filePanel.component"
 
 @Component({
 	selector: "cc-file-panel-state-buttons",
 	template: require("./filePanelStateButtons.component.html")
 })
 export class FilePanelStateButtonsComponent {
-	//@Input() state: FilePanelState
+	@Input() state: FilePanelState
 }
