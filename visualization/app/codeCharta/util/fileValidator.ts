e6f4ff3e3b971d8a70d3585db7fea1769de643b6
Maria Hallmann <maria.hallmann@student.htw-berlin.de>
2021-12-08 12:13:57 +0100

    Remove validation method for testing files equality #2480

--- visualization/app/codeCharta/util/fileValidator.ts
+++ visualization/app/codeCharta/util/fileValidator.ts
@@ -4,7 +4,6 @@ import packageJson from "../../../package.json"
 import { ExportCCFile } from "../codeCharta.api.model"
 import jsonSchema from "./generatedSchema.json"
 import { isLeaf } from "./codeMapHelper"
-import { FileState } from "../model/files/files"
 
 const latestApiVersion = packageJson.codecharta.apiVersion
 
@@ -33,9 +32,8 @@ export const ERROR_MESSAGES = {
 	blacklistError: "Excluding all buildings is not possible."
 }
 
-export function validate(nameDataPair: NameDataPair, fileStates: FileState[]) {
+export function validate(nameDataPair: NameDataPair) {
 	const file = nameDataPair?.content
-	const fileName = nameDataPair?.fileName
 	const result: CCValidationResult = { error: [], warning: [] }
 	switch (true) {
 		case !file:
@@ -49,9 +47,6 @@ export function validate(nameDataPair: NameDataPair, fileStates: FileState[]) {
 			break
 		case fileHasHigherMinorVersion(file):
 			result.warning.push(`${ERROR_MESSAGES.minorApiVersionOutdated} Found: ${file.apiVersion}`)
-			break
-		case fileAlreadyExists(fileName, file, fileStates):
-			result.error.push(ERROR_MESSAGES.fileAlreadyExists)
 			break
 	}
 
@@ -91,16 +86,6 @@ function fileHasHigherMajorVersion(file: ExportCCFile) {
 function fileHasHigherMinorVersion(file: ExportCCFile) {
 	const apiVersion = getAsApiVersion(file.apiVersion)
 	return apiVersion.minor > getAsApiVersion(latestApiVersion).minor
-}
-
-function fileAlreadyExists(currentFileName: string, file: ExportCCFile, fileStates: FileState[]) {
-	for (const recentFile of fileStates) {
-		const { fileName, fileChecksum } = recentFile.file.fileMeta
-		if (fileName === currentFileName && fileChecksum === file.fileChecksum) {
-			return true
-		}
-	}
-	return false
 }
 
 export function getAsApiVersion(version: string): ApiVersion {
