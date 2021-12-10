45cd12c3c4fbd463908398f14cb1dbcf21bdc31d
Maria Hallmann <maria.hallmann@student.htw-berlin.de>
2021-12-10 10:25:26 +0100

    Add test that ensures uploading of previously manually fixed files is possible Refactor some variables #2480

--- visualization/app/codeCharta/codeCharta.service.spec.ts
+++ visualization/app/codeCharta/codeCharta.service.spec.ts
@@ -159,68 +159,89 @@ describe("codeChartaService", () => {
 		})
 
 		it("should replace files with equal file name and checksum when loading new files", async () => {
-			const validContent2ndFile = klona(validFileContent)
-			validContent2ndFile.fileChecksum = "validHash_1"
+			const valid2ndFileContent = klona(validFileContent)
+			valid2ndFileContent.fileChecksum = "validHash_1"
 
 			await codeChartaService.loadFiles([{ fileName: "FirstFile", content: validFileContent, fileSize: 42 }])
 
 			await codeChartaService.loadFiles([
 				{ fileName: "FirstFile", content: validFileContent, fileSize: 42 },
-				{ fileName: "SecondFile", content: validContent2ndFile, fileSize: 42 }
+				{ fileName: "SecondFile", content: valid2ndFileContent, fileSize: 42 }
 			])
 
-			expect(getCCFiles(storeService.getState().files).length).toEqual(2)
-			expect(getCCFiles(storeService.getState().files)[0].fileMeta.fileName).toEqual("FirstFile")
-			expect(getCCFiles(storeService.getState().files)[0].fileMeta.fileChecksum).toEqual("invalid-md5-sample")
-			expect(getCCFiles(storeService.getState().files)[1].fileMeta.fileName).toEqual("SecondFile")
-			expect(getCCFiles(storeService.getState().files)[1].fileMeta.fileChecksum).toEqual("validHash_1")
+			const CCFilesUnderTest = getCCFiles(storeService.getState().files)
+
+			expect(CCFilesUnderTest.length).toEqual(2)
+			expect(CCFilesUnderTest[0].fileMeta.fileName).toEqual("FirstFile")
+			expect(CCFilesUnderTest[0].fileMeta.fileChecksum).toEqual("invalid-md5-sample")
+			expect(CCFilesUnderTest[1].fileMeta.fileName).toEqual("SecondFile")
+			expect(CCFilesUnderTest[1].fileMeta.fileChecksum).toEqual("validHash_1")
 		})
 
 		it("should load files with equal file name but different checksum and rename uploaded files ", async () => {
-			const validContent2ndFile = klona(validFileContent)
-			validContent2ndFile.fileChecksum = "validHash_1"
-			const validContent3ndFile = klona(validFileContent)
-			validContent3ndFile.fileChecksum = "validHash_2"
+			const valid2ndFileContent = klona(validFileContent)
+			valid2ndFileContent.fileChecksum = "validHash_1"
+			const valid3rdFileContent = klona(validFileContent)
+			valid3rdFileContent.fileChecksum = "validHash_2"
 
 			await codeChartaService.loadFiles([{ fileName: "FirstFile", content: validFileContent, fileSize: 42 }])
 
 			await codeChartaService.loadFiles([
-				{ fileName: "FirstFile", content: validContent2ndFile, fileSize: 42 },
-				{ fileName: "FirstFile", content: validContent3ndFile, fileSize: 42 }
+				{ fileName: "FirstFile", content: valid2ndFileContent, fileSize: 42 },
+				{ fileName: "FirstFile", content: valid3rdFileContent, fileSize: 42 }
 			])
 
-			expect(getCCFiles(storeService.getState().files).length).toEqual(3)
-			expect(getCCFiles(storeService.getState().files)[0].fileMeta.fileName).toEqual("FirstFile")
-			expect(getCCFiles(storeService.getState().files)[0].fileMeta.fileChecksum).toEqual("invalid-md5-sample")
-			expect(getCCFiles(storeService.getState().files)[1].fileMeta.fileName).toEqual("FirstFile_1")
-			expect(getCCFiles(storeService.getState().files)[1].fileMeta.fileChecksum).toEqual("validHash_1")
-			expect(getCCFiles(storeService.getState().files)[2].fileMeta.fileName).toEqual("FirstFile_2")
-			expect(getCCFiles(storeService.getState().files)[2].fileMeta.fileChecksum).toEqual("validHash_2")
+			const CCFilesUnderTest = getCCFiles(storeService.getState().files)
+
+			expect(CCFilesUnderTest.length).toEqual(3)
+			expect(CCFilesUnderTest[0].fileMeta.fileName).toEqual("FirstFile")
+			expect(CCFilesUnderTest[0].fileMeta.fileChecksum).toEqual("invalid-md5-sample")
+			expect(CCFilesUnderTest[1].fileMeta.fileName).toEqual("FirstFile_1")
+			expect(CCFilesUnderTest[1].fileMeta.fileChecksum).toEqual("validHash_1")
+			expect(CCFilesUnderTest[2].fileMeta.fileName).toEqual("FirstFile_2")
+			expect(CCFilesUnderTest[2].fileMeta.fileChecksum).toEqual("validHash_2")
 		})
 
 		it("should not load files with equal file name and checksum when there was a renaming of file names in previous uploads", async () => {
-			const validContent2ndFile = klona(validFileContent)
-			validContent2ndFile.fileChecksum = "validHash_1"
-			const validContent3ndFile = klona(validFileContent)
-			validContent3ndFile.fileChecksum = "validHash_2"
+			const valid2ndFileContent = klona(validFileContent)
+			valid2ndFileContent.fileChecksum = "validHash_1"
+			const valid3rdFileContent = klona(validFileContent)
+			valid3rdFileContent.fileChecksum = "validHash_2"
 
 			await codeChartaService.loadFiles([
 				{ fileName: "FirstFile", content: validFileContent, fileSize: 42 },
-				{ fileName: "FirstFile", content: validContent2ndFile, fileSize: 42 }
+				{ fileName: "FirstFile", content: valid2ndFileContent, fileSize: 42 }
 			])
 
 			await codeChartaService.loadFiles([
-				{ fileName: "FirstFile", content: validContent3ndFile, fileSize: 42 },
-				{ fileName: "FirstFile", content: validContent2ndFile, fileSize: 42 }
+				{ fileName: "FirstFile", content: valid3rdFileContent, fileSize: 42 },
+				{ fileName: "FirstFile", content: valid2ndFileContent, fileSize: 42 }
 			])
 
-			expect(getCCFiles(storeService.getState().files).length).toEqual(3)
-			expect(getCCFiles(storeService.getState().files)[0].fileMeta.fileName).toEqual("FirstFile")
-			expect(getCCFiles(storeService.getState().files)[0].fileMeta.fileChecksum).toEqual("invalid-md5-sample")
-			expect(getCCFiles(storeService.getState().files)[1].fileMeta.fileName).toEqual("FirstFile_1")
-			expect(getCCFiles(storeService.getState().files)[1].fileMeta.fileChecksum).toEqual("validHash_1")
-			expect(getCCFiles(storeService.getState().files)[2].fileMeta.fileName).toEqual("FirstFile_2")
-			expect(getCCFiles(storeService.getState().files)[2].fileMeta.fileChecksum).toEqual("validHash_2")
+			const CCFilesUnderTest = getCCFiles(storeService.getState().files)
+
+			expect(CCFilesUnderTest.length).toEqual(3)
+			expect(CCFilesUnderTest[0].fileMeta.fileName).toEqual("FirstFile")
+			expect(CCFilesUnderTest[0].fileMeta.fileChecksum).toEqual("invalid-md5-sample")
+			expect(CCFilesUnderTest[1].fileMeta.fileName).toEqual("FirstFile_1")
+			expect(CCFilesUnderTest[1].fileMeta.fileChecksum).toEqual("validHash_1")
+			expect(CCFilesUnderTest[2].fileMeta.fileName).toEqual("FirstFile_2")
+			expect(CCFilesUnderTest[2].fileMeta.fileChecksum).toEqual("validHash_2")
+		})
+
+		it("should not load broken file but after fixing this problem manually it should possible to load this file again", async () => {
+			const invalidFileContent = klona(validFileContent)
+			invalidFileContent.apiVersion = ""
+
+			await codeChartaService.loadFiles([{ fileName: "FirstFile", content: invalidFileContent, fileSize: 42 }])
+
+			await codeChartaService.loadFiles([{ fileName: "FirstFile", content: validFileContent, fileSize: 42 }])
+
+			const CCFilesUnderTest = getCCFiles(storeService.getState().files)
+
+			expect(CCFilesUnderTest.length).toEqual(1)
+			expect(CCFilesUnderTest[0].fileMeta.fileName).toEqual("FirstFile")
+			expect(CCFilesUnderTest[0].fileMeta.fileChecksum).toEqual("invalid-md5-sample")
 		})
 
 		it("should select the newly added file", async () => {
