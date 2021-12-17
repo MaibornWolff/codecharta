f440ab030ee0f71ef6b7abd644d6f3c75e145b8e
IhsenBouallegue <bouallegueihsen@gmail.com>
2021-12-17 16:25:21 +0100

    Fix css and subscribe issues #475

--- visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.component.ts
+++ visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.component.ts
@@ -1,5 +1,5 @@
 import "./export3DMapButton.component.scss"
-import { Component, Inject } from "@angular/core"
+import { Component, Inject, OnDestroy } from "@angular/core"
 import { FileDownloader } from "../../util/fileDownloader"
 import { STLExporter } from "three/examples/jsm/exporters/STLExporter"
 import { ThreeSceneService } from "../codeMap/threeViewer/threeSceneService"
@@ -15,18 +15,20 @@ import { Mesh } from "three"
 	selector: "cc-export-threed-map-button",
 	template: require("./export3DMapButton.component.html")
 })
-export class Export3DMapButtonComponent {
+export class Export3DMapButtonComponent implements OnDestroy {
 	private fileName: string
 	private files: FileState[]
 	private exporter = new STLExporter()
-
+	private storeSubscriptions = []
 	constructor(@Inject(Store) store: Store) {
-		store.select(accumulatedDataSelector).subscribe(accumulatedData => {
-			this.fileName = accumulatedData.unifiedFileMeta?.fileName
-		})
-		store.select(filesSelector).subscribe(files => {
-			this.files = files
-		})
+		this.storeSubscriptions.push(
+			store.select(accumulatedDataSelector).subscribe(accumulatedData => {
+				this.fileName = accumulatedData.unifiedFileMeta?.fileName
+			}),
+			store.select(filesSelector).subscribe(files => {
+				this.files = files
+			})
+		)
 	}
 
 	downloadStlFile() {
@@ -34,5 +36,9 @@ export class Export3DMapButtonComponent {
 		const exportedBinaryFile = this.exporter.parse(threeMesh, { binary: true })
 		const fileName = `${FileNameHelper.getNewFileName(this.fileName, isDeltaState(this.files))}.stl`
 		FileDownloader.downloadData(exportedBinaryFile, fileName)
+	}
+
+	ngOnDestroy(): void {
+		for (const subscription of this.storeSubscriptions) subscription.unsubscribe()
 	}
 }
