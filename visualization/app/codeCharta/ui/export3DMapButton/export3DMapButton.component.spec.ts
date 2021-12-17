87932d46659c97ae2d60ff329a773ade48c0f5f1
Torsten Knauf <Torsten.Knauf@maibornwolff.de>
2021-12-17 17:38:35 +0100

    test: add basic test for template
    ref #475

--- visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.component.spec.ts
+++ visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from "@angular/core/testing"
 import { Export3DMapButtonComponent } from "./export3DMapButton.component"
-import { render } from "@testing-library/angular"
+import { fireEvent, render } from "@testing-library/angular"
 import { Export3DMapButtonModule } from "./export3DMapButton.module"
 
 describe("Export3DMapButtonComponent", () => {
@@ -9,9 +9,21 @@ describe("Export3DMapButtonComponent", () => {
 			imports: [Export3DMapButtonModule]
 		})
 	})
-	it("should show 3d file export button", async function () {
-		const { container } = await render(Export3DMapButtonComponent, { excludeComponentDeclaration: true })
 
-		expect(container.querySelector(".export-3d-button")).not.toBe(null)
+	it("should start download on click", async function () {
+		const { container } = await render(Export3DMapButtonComponent, { excludeComponentDeclaration: true })
+		// mock download and therefore only verify the Angular binding.
+		// A better approach would be, if the component would only fire an action
+		// and an https://ngrx.io/guide/effects would do the side effect and logic.
+		// Then the we could test the logic in the effect without mocking a lot
+		// and the component wouldn't need to know anything about store values
+		// @ts-ignore
+		const mockedDownload = jest.spyOn(ng.probe(container).componentInstance, "downloadStlFile").mockImplementation(() => null)
+
+		const downloadButton = container.querySelector(".export-3d-button")
+		expect(downloadButton).not.toBe(null)
+
+		fireEvent.click(downloadButton)
+		expect(mockedDownload).toHaveBeenCalledTimes(1)
 	})
 })
