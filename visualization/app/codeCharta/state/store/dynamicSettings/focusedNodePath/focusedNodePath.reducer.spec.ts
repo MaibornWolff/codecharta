ac09bd086bc739e02890a8fc612c3cf7f0cb14d3
shaman-apprentice <Torsten.Knauf@gmx.de>
2021-12-26 22:30:33 +0100

    refactor: migrate tests for focusedNodePath.reducer
    ref #2318

--- visualization/app/codeCharta/state/store/dynamicSettings/focusedNodePath/focusedNodePath.reducer.spec.ts
+++ visualization/app/codeCharta/state/store/dynamicSettings/focusedNodePath/focusedNodePath.reducer.spec.ts
@@ -1,40 +1,48 @@
 import { focusedNodePath } from "./focusedNodePath.reducer"
-import { FocusedNodePathAction, focusNode, unfocusNode } from "./focusedNodePath.actions"
+import { FocusedNodePathAction, focusNode, unfocusAllNodes, unfocusNode } from "./focusedNodePath.actions"
 
 describe("focusedNodePath", () => {
 	describe("Default State", () => {
 		it("should initialize the default state", () => {
 			const result = focusedNodePath(undefined, {} as FocusedNodePathAction)
 
-			expect(result).toEqual("")
+			expect(result).toEqual([])
 		})
 	})
 
 	describe("Action: FOCUS_NODE", () => {
 		it("should set new focusedNodePath", () => {
-			const result = focusedNodePath("", focusNode("some/path/*.ts"))
+			const result = focusedNodePath([], focusNode("some/path/*.ts"))
 
-			expect(result).toEqual("some/path/*.ts")
+			expect(result).toEqual(["some/path/*.ts"])
 		})
 
-		it("should set default focusedNodePath", () => {
-			const result = focusedNodePath("some/path/*.ts", focusNode())
+		it("should add focusedNodePath", () => {
+			const result = focusedNodePath(["some/path/*.ts"], focusNode("foo.ts"))
 
-			expect(result).toEqual("")
+			expect(result).toEqual(["foo.ts", "some/path/*.ts"])
 		})
 
 		it("should not allow to focus root folder", () => {
-			const result = focusedNodePath("/root", focusNode())
+			const result = focusedNodePath([], focusNode("/root"))
 
-			expect(result).toEqual("")
+			expect(result).toEqual([])
 		})
 	})
 
 	describe("Action: UNFOCUS_NODE", () => {
 		it("should remove focusedNodePath", () => {
-			const result = focusedNodePath("some/path/*.ts", unfocusNode())
+			const result = focusedNodePath(["some/path/*.ts", "foo.ts"], unfocusNode())
 
-			expect(result).toEqual("")
+			expect(result).toEqual(["foo.ts"])
+		})
+	})
+
+	describe("Action: UNFOCUS_ALL_NODES", () => {
+		it("should remove all focusedNodePaths", () => {
+			const result = focusedNodePath(["some/path/*.ts", "foo.ts"], unfocusAllNodes())
+
+			expect(result).toEqual([])
 		})
 	})
 })
