755b9fbaf0bf3bbbd62fe8f6f1828243ce934f3a
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-01-01 17:43:40 +0100

    refactor: move duplicated logic into cc-color-picker
    ref #2318

--- visualization/app/codeCharta/ui/labelledColorPicker/labelledColorPicker.module.ts
+++ visualization/app/codeCharta/ui/labelledColorPicker/labelledColorPicker.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
 
-import { MaterialModule } from "../../../material/material.module"
 import { ColorPickerModule } from "../colorPicker/colorPicker.module"
 import { LabelledColorPickerComponent } from "./labelledColorPicker.component"
 import { ReadableColorForBackgroundPipe } from "./readableColorForBackground.pipe"
 
 @NgModule({
-	imports: [CommonModule, ColorPickerModule, MaterialModule],
+	imports: [CommonModule, ColorPickerModule],
 	declarations: [LabelledColorPickerComponent, ReadableColorForBackgroundPipe],
 	exports: [LabelledColorPickerComponent]
 })
