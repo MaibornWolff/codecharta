7ff1b328e73f48c9f887fd99f2043974ea772bad
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-01-10 13:53:03 +0100

    fix JsonProperty #2592

--- analysis/import/JasomeImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/jasome/model/Class.kt
+++ analysis/import/JasomeImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/jasome/model/Class.kt
@@ -5,5 +5,5 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
 
 class Class(
     @JsonProperty("name") var name: String?,
-    @JacksonXmlElementWrapper(localName = "Metrics") @JsonProperty("Metric") var metrics: List<Metric>?
+    @JacksonXmlElementWrapper(localName = "Metrics") @JsonProperty("Metrics") var metrics: List<Metric>?
 )
