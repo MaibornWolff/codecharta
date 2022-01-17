ea4a5f70437b8777566b8e8640caad86d0e5b2c0
Shivan-997 <shivanoskan.1997@hotmail.com>
2022-01-17 16:01:04 +0100

    fix showing images in what is new

--- visualization/app/codeCharta/ui/dialog/dialog.changelog.component.ts
+++ visualization/app/codeCharta/ui/dialog/dialog.changelog.component.ts
@@ -1,5 +1,6 @@
 import markdownFile from "../../../../../CHANGELOG.md"
 import packageJson from "../../../../package.json"
+import "./test.scss"
 
 export class DialogChangelogController {
 	private _viewModel: {
@@ -35,7 +36,12 @@ export class DialogChangelogController {
 				const start = lineIndex + 2
 				const end = this.findEndChangesLine(changelogLines, lineIndex)
 				for (const changeLine of changelogLines.slice(start, end)) {
-					changelogTypes.push(changeLine)
+					if (!changeLine.includes("<img")) {
+						const newChangeLine = `${changeLine}<br>`
+						changelogTypes.push(newChangeLine)
+					} else {
+						changelogTypes.push(`${changeLine}<br>`)
+					}
 				}
 			}
 			if (changelogTypes.length > 0) changes[title] = changelogTypes.join("\n")
