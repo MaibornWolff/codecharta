7c7a8ae84a3da22dbed8405eae1267b529acf9a1
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-01-21 09:45:46 +0100

    Add tests to dialog.service #2094

---
+++ visualization/app/codeCharta/ui/dialog/dialog.service.spec.ts
@@ -0,0 +1,68 @@
+import "./dialog.module.ts"
+import { getService, instantiateModule } from "../../../../mocks/ng.mockhelper"
+import { DialogService } from "./dialog.service"
+import { CCFileValidationResult } from "../../util/fileValidator"
+
+describe("DialogService", () => {
+	let dialogService: DialogService
+	let $mdDialog
+
+	beforeEach(() => {
+		restartSystem()
+		rebuildController()
+		dialogService.showCustomDialog = jest.fn()
+		dialogService.showErrorDialog = jest.fn()
+	})
+
+	function restartSystem() {
+		instantiateModule("app.codeCharta.ui.dialog")
+		$mdDialog = getService("$mdDialog")
+	}
+
+	function rebuildController() {
+		dialogService = new DialogService($mdDialog)
+	}
+
+	it("should show AddCustomConfigSettings", function () {
+		dialogService.showAddCustomConfigSettings()
+
+		expect(dialogService.showCustomDialog).toHaveBeenCalled()
+	})
+
+	it("should show DownloadDialog", function () {
+		dialogService.showDownloadDialog()
+
+		expect(dialogService.showCustomDialog).toHaveBeenCalled()
+	})
+
+	it("should show AddScenarioSettings", function () {
+		dialogService.showAddScenarioSettings()
+
+		expect(dialogService.showCustomDialog).toHaveBeenCalled()
+	})
+
+	it("should show GlobalSettingsDialog", function () {
+		dialogService.showGlobalSettingsDialog()
+
+		expect(dialogService.showCustomDialog).toHaveBeenCalled()
+	})
+
+	it("should show ChangelogDialog", function () {
+		dialogService.showChangelogDialog()
+
+		expect(dialogService.showCustomDialog).toHaveBeenCalled()
+	})
+
+	it("should show ValidationDialog", function () {
+		const fileValidationResults: CCFileValidationResult[] = [
+			{ fileName: "file_1", errors: ["Error"], warnings: [] },
+			{ fileName: "file_2", errors: [], warnings: ["Warning"] }
+		]
+		dialogService.showValidationDialog(fileValidationResults)
+
+		expect(dialogService.showErrorDialog).toHaveBeenCalledWith(
+			`<h2>Errors</h2><p><strong>file_1:</strong> <p><i class="fa fa-exclamation-circle"></i> Error</p></p><h2>Warnings</h2><p><strong>file_2:</strong> <p><i class="fa fa-exclamation-triangle"></i> Warning</p></p>`,
+			"Something is wrong with the uploaded file(s)"
+		)
+	})
+})
