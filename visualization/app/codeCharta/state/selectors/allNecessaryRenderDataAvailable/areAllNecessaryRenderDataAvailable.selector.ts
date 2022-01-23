b26450347776b8f29da39c996e557f503ff8bf6a
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-01-23 12:30:18 +0100

    refactor: extract areAllNecessaryRenderDataAvailableSelector
    - which improves separation of concerns
    - which enables memorization for this calculation
    
    ref #2318

---
+++ visualization/app/codeCharta/state/selectors/allNecessaryRenderDataAvailable/areAllNecessaryRenderDataAvailable.selector.ts
@@ -0,0 +1,30 @@
+import { fileStatesAvailable } from "../../../model/files/files.helper"
+import { createSelector } from "../../angular-redux/createSelector"
+import { areaMetricSelector } from "../../store/dynamicSettings/areaMetric/areaMetric.selector"
+import { colorMetricSelector } from "../../store/dynamicSettings/colorMetric/colorMetric.selector"
+import { dynamicSettingsSelector } from "../../store/dynamicSettings/dynamicSettings.selector"
+import { heightMetricSelector } from "../../store/dynamicSettings/heightMetric/heightMetric.selector"
+import { filesSelector } from "../../store/files/files.selector"
+import { nodeMetricDataSelector } from "../accumulatedData/metricData/nodeMetricData.selector"
+import { areDynamicSettingsAvailable } from "./utils/areDynamicSettingsAvailable"
+import { areMetricsAvailable } from "./utils/areMetricsAvailable"
+
+const areFileStatesAvailableSelector = createSelector([filesSelector], files => fileStatesAvailable(files))
+
+const areChosenMetricsAvailableSelector = createSelector(
+	[nodeMetricDataSelector, areaMetricSelector, colorMetricSelector, heightMetricSelector],
+	(nodeMetricData, areaMetric, colorMetric, heightMetric) => areMetricsAvailable(nodeMetricData, [areaMetric, colorMetric, heightMetric])
+)
+
+const areDynamicSettingsAvailableSelector = createSelector([dynamicSettingsSelector], dynamicSettings =>
+	areDynamicSettingsAvailable(dynamicSettings)
+)
+
+export const areAllNecessaryRenderDataAvailableSelector = createSelector(
+	[nodeMetricDataSelector, areFileStatesAvailableSelector, areChosenMetricsAvailableSelector, areDynamicSettingsAvailableSelector],
+	(nodeMetricData, areFileStatesAvailable, areChosenMetricsAvailable, areDynamicSettingsAvailable) => {
+		if (nodeMetricData === null || !areFileStatesAvailable || !areChosenMetricsAvailable || !areDynamicSettingsAvailable) return false
+
+		return true
+	}
+)
