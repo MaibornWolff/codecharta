efae257335bdd0b336f9d7ce56197fb2f19fb907
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-01-25 19:39:43 +0100

    refactor: fine tune styling
    ref #2318

--- visualization/app/codeCharta/ui/nodeContextMenu/flattenButtons/flattenButtons.module.ts
+++ visualization/app/codeCharta/ui/nodeContextMenu/flattenButtons/flattenButtons.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
+import { MaterialModule } from "../../../../material/material.module"
 import { FlattenButtonsComponent } from "./flattenButtons.component"
 
 @NgModule({
-	imports: [CommonModule],
+	imports: [CommonModule, MaterialModule],
 	declarations: [FlattenButtonsComponent],
 	exports: [FlattenButtonsComponent]
 })
