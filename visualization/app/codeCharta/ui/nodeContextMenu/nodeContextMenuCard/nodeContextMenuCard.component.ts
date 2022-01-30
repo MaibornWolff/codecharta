a25a9a82a404d370c0f21ebfdecc6efee1089f8f
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-01-30 20:05:01 +0100

    refactor: use ngOnInit
    as the ctx menu is not always present it is beneficial to not do it in the constructor
    
    ref #2318

--- visualization/app/codeCharta/ui/nodeContextMenu/nodeContextMenuCard/nodeContextMenuCard.component.ts
+++ visualization/app/codeCharta/ui/nodeContextMenu/nodeContextMenuCard/nodeContextMenuCard.component.ts
@@ -1,16 +1,22 @@
 import "./nodeContextMenuCard.component.scss"
-import { Component, Inject } from "@angular/core"
+import { Component, Inject, OnInit } from "@angular/core"
 import { Store } from "../../../state/angular-redux/store"
 import { rightClickedCodeMapNodeSelector } from "../rightClickedCodeMapNode.selector"
+import { Observable } from "rxjs"
+import { CodeMapNode } from "../../../codeCharta.model"
 
 @Component({
 	selector: "cc-node-context-menu-card",
 	template: require("./nodeContextMenuCard.component.html")
 })
-export class NodeContextMenuCardComponent {
-	codeMapNode$ = this.store.select(rightClickedCodeMapNodeSelector)
+export class NodeContextMenuCardComponent implements OnInit {
+	codeMapNode$: Observable<CodeMapNode>
 
 	constructor(@Inject(Store) private store: Store) {}
+
+	ngOnInit(): void {
+		this.codeMapNode$ = this.store.select(rightClickedCodeMapNodeSelector)
+	}
 
 	excludeNode() {
 		// console.log("excludeNode")
