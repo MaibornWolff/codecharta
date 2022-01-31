9a4db39f040a8f0b9b6c6cefd759d783270c82e8
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-01-31 14:24:37 +0100

    Rename aggregation type pipe #2545

--- visualization/app/codeCharta/ui/attributeSideBar/attributeTypeSelector/aggregationSymbol.pipe.spec.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeTypeSelector/aggregationSymbol.pipe.spec.ts
@@ -1,19 +1,19 @@
 import { AttributeTypeValue } from "../../../codeCharta.model"
-import { AggregationSymbolPipe } from "./aggregationSymbol.pipe"
+import { AggregationTypePipePipe } from "./aggregationType.pipe"
 
 describe("aggregationSymbolPipe", () => {
 	it("should map `AttributeTypeValue.relative` to x͂", () => {
 		const getAttributeTypeOfNodesByMetricSelector = () => AttributeTypeValue.relative
-		expect(new AggregationSymbolPipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("x͂")
+		expect(new AggregationTypePipePipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("x͂")
 	})
 
 	it("should map `AttributeTypeValue.absolute` to Σ", () => {
 		const getAttributeTypeOfNodesByMetricSelector = () => AttributeTypeValue.absolute
-		expect(new AggregationSymbolPipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("Σ")
+		expect(new AggregationTypePipePipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("Σ")
 	})
 
 	it("should default to Σ", () => {
 		const getAttributeTypeOfNodesByMetricSelector = () => "" as AttributeTypeValue
-		expect(new AggregationSymbolPipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("Σ")
+		expect(new AggregationTypePipePipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("Σ")
 	})
 })
--- visualization/app/codeCharta/ui/attributeSideBar/attributeTypeSelector/aggregationType.pipe.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeTypeSelector/aggregationType.pipe.ts
@@ -3,8 +3,8 @@ import { Pipe, PipeTransform } from "@angular/core"
 import { AttributeTypeValue } from "../../../codeCharta.model"
 import { GetAttributeTypeOfNodesByMetric } from "../../../state/selectors/getAttributeTypeOfNodesByMetric.selector"
 
-@Pipe({ name: "aggregationSymbolPipe" })
-export class AggregationSymbolPipe implements PipeTransform {
+@Pipe({ name: "aggregationTypePipe" })
+export class AggregationTypePipePipe implements PipeTransform {
 	transform(metricName: string, getAttributeTypeOfNodesByMetricSelector: GetAttributeTypeOfNodesByMetric): string {
 		const type = getAttributeTypeOfNodesByMetricSelector(metricName)
 		switch (type) {
