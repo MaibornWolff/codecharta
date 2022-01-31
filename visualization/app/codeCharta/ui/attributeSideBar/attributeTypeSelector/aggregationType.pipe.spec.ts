8b39001b1b4cfd5b93e58efbc06284a9a5364955
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-01-31 14:48:29 +0100

    Adjust tests for toggle buttons #2545

--- visualization/app/codeCharta/ui/attributeSideBar/attributeTypeSelector/aggregationType.pipe.spec.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeTypeSelector/aggregationType.pipe.spec.ts
@@ -2,18 +2,18 @@ import { AttributeTypeValue } from "../../../codeCharta.model"
 import { AggregationTypePipePipe } from "./aggregationType.pipe"
 
 describe("aggregationSymbolPipe", () => {
-	it("should map `AttributeTypeValue.relative` to x͂", () => {
+	it("should map `AttributeTypeValue.relative` to median", () => {
 		const getAttributeTypeOfNodesByMetricSelector = () => AttributeTypeValue.relative
-		expect(new AggregationTypePipePipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("x͂")
+		expect(new AggregationTypePipePipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("median")
 	})
 
-	it("should map `AttributeTypeValue.absolute` to Σ", () => {
+	it("should map `AttributeTypeValue.absolute` to sum", () => {
 		const getAttributeTypeOfNodesByMetricSelector = () => AttributeTypeValue.absolute
-		expect(new AggregationTypePipePipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("Σ")
+		expect(new AggregationTypePipePipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("sum")
 	})
 
-	it("should default to Σ", () => {
+	it("should default to sum", () => {
 		const getAttributeTypeOfNodesByMetricSelector = () => "" as AttributeTypeValue
-		expect(new AggregationTypePipePipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("Σ")
+		expect(new AggregationTypePipePipe().transform("some-metric-name", getAttributeTypeOfNodesByMetricSelector)).toBe("sum")
 	})
 })
