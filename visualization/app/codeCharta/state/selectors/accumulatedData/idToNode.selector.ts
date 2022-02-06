f3d8b5ed00ceef8235cd5ce96fcc8ff35bfac0ae
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-02-06 16:21:37 +0100

    refactor: extract idToNode via a selector
    as store should only contain atomic non derivable data.
    
    This also "migrates" / removes idToNode.service
    
    ref #2318

---
+++ visualization/app/codeCharta/state/selectors/accumulatedData/idToNode.selector.ts
@@ -0,0 +1,16 @@
+import { hierarchy } from "d3-hierarchy"
+import { CodeMapNode } from "../../../codeCharta.model"
+import { createSelector } from "../../angular-redux/createSelector"
+import { unifiedMapNodeSelector } from "./unifiedMapNode.selector"
+
+export const _calculateIdToNode = (unifiedMapNode: CodeMapNode | undefined): Map<number, CodeMapNode> => {
+	if (!unifiedMapNode) return new Map()
+
+	const idToNode: Map<number, CodeMapNode> = new Map([[unifiedMapNode.id, unifiedMapNode]])
+	for (const { data } of hierarchy(unifiedMapNode)) {
+		idToNode.set(data.id, data)
+	}
+	return idToNode
+}
+
+export const idToNodeSelector = createSelector([unifiedMapNodeSelector], _calculateIdToNode)
