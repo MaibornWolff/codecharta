fd21662f19f6486f3788385e2d03abb5c405cc6d
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-02-07 17:48:29 +0100

    Add test validation for adding a new custom config #2489

--- visualization/app/codeCharta/ui/customConfigs/addCustomConfigButton/addCustomConfigButton.module.ts
+++ visualization/app/codeCharta/ui/customConfigs/addCustomConfigButton/addCustomConfigButton.module.ts
@@ -3,9 +3,10 @@ import { MaterialModule } from "../../../../material/material.module"
 import { AddCustomConfigButtonComponent } from "./addCustomConfigButton.component"
 import { AddCustomConfigDialogComponent } from "./addCustomConfigDialog/addCustomConfigDialog.component"
 import { FormsModule, ReactiveFormsModule } from "@angular/forms"
+import { CommonModule } from "@angular/common"
 
 @NgModule({
-	imports: [MaterialModule, ReactiveFormsModule, FormsModule],
+	imports: [MaterialModule, ReactiveFormsModule, FormsModule, CommonModule],
 	declarations: [AddCustomConfigButtonComponent, AddCustomConfigDialogComponent],
 	exports: [AddCustomConfigButtonComponent]
 })
--- visualization/app/codeCharta/ui/customConfigs/addCustomConfigButton/addCustomConfigDialog/addCustomConfigDialog.component.ts
+++ visualization/app/codeCharta/ui/customConfigs/addCustomConfigButton/addCustomConfigDialog/addCustomConfigDialog.component.ts
@@ -5,5 +5,18 @@ import { FormControl, Validators } from "@angular/forms"
 	template: require("./addCustomConfigDialog.component.html")
 })
 export class AddCustomConfigDialogComponent {
-	customConfigName = new FormControl("", [Validators.required, Validators.email])
+	customConfigName = new FormControl("", [Validators.required, validateYolo])
+
+	getErrorMessage() {
+		if (this.customConfigName.hasError("required")) {
+			return "Halt Stopp!"
+		}
+		return this.customConfigName.hasError("A") ? this.customConfigName.getError("A") : ""
+	}
+}
+
+function validateYolo(control: FormControl) {
+	const value = control.value
+	if (value === "Yolo") return null
+	return { A: "B" }
 }
