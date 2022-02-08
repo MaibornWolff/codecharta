45961498cb9154cd84a4f4f70ae79d021ea7efcb
Cedrik Bormann <cedrikbormann@gmail.com>
2022-02-08 17:51:29 +0100

    Disable LayzLoader feature #1314, #2554

--- visualization/app/codeCharta/util/envDetector.ts
+++ visualization/app/codeCharta/util/envDetector.ts
@@ -1,7 +1,3 @@
-export function isStandalone(): boolean {
-	return false
-}
-
 export function isDevelopment(): boolean {
 	return process.env.DEV === undefined ? false : JSON.parse(process.env.DEV)
 }
