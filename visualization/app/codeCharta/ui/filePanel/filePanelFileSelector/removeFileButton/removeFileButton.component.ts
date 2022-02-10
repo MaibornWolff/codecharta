596bacbb8129a906d738f87b5899763733b74c49
cschild <clara.schild@gmail.com>
2022-02-10 09:02:01 +0100

    fix remove file button only visible when at least 2 maps are loaded

--- visualization/app/codeCharta/ui/filePanel/filePanelFileSelector/removeFileButton/removeFileButton.component.ts
+++ visualization/app/codeCharta/ui/filePanel/filePanelFileSelector/removeFileButton/removeFileButton.component.ts
@@ -1,12 +1,11 @@
 import "./removeFileButton.component.scss"
 import { Component, Input } from "@angular/core"
-import { FileState } from "../../../../model/files/files"
+
 @Component({
 	selector: "cc-remove-file-button",
 	template: require("./removeFileButton.component.html")
 })
 export class RemoveFileButtonComponent {
 	@Input() filename: string
-	@Input() files: FileState[]
 	@Input() onRemoveFile: (filename: string, event) => void
 }
