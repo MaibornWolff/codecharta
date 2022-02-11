9d1679abb8c930057023fee83b0a347609bb7b7f
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-02-11 18:45:31 +0100

    reorder functions #2617

--- visualization/app/codeCharta/util/gameObjectsParser/gameObjectsImporter.ts
+++ visualization/app/codeCharta/util/gameObjectsParser/gameObjectsImporter.ts
@@ -1,29 +1,41 @@
 import md5 from "md5"
 
-function isFile(names: string[]) {
-	return names.length === 0
-}
+export function parseGameObjectsFile(data) {
+	const { gameObjectPositions, cycles = [] } = JSON.parse(data)
 
-function nodeAlreadyExists(nodes, name) {
-	return nodes.some(node => node.name === name)
-}
+	const ccJson = {
+		checksum: "",
+		data: {
+			projectName: "ScriptProjectName",
+			fileChecksum: "",
+			apiVersion: "1.3",
+			nodes: []
+		}
+	}
 
-function calculateFixedFolderPosition(node, name, parent, gameObjectPosition) {
-	if (node.type === "Folder" && name !== "base") {
-		const cornerXofParent = parent.position.x - parent.scale.x / 2
-		const cornerZofParent = parent.position.z - parent.scale.z / 2
-		const cornerXofChild = gameObjectPosition.position.x - gameObjectPosition.scale.x / 2
-		const cornerZofChild = gameObjectPosition.position.z - gameObjectPosition.scale.z / 2
-		const top = Math.floor(((cornerXofChild - cornerXofParent) / parent.scale.x) * 100)
-		const left = Math.floor(((cornerZofChild - cornerZofParent) / parent.scale.z) * 100)
-		const width = Math.floor((gameObjectPosition.scale.z / parent.scale.z) * 100)
-		const height = Math.floor((gameObjectPosition.scale.x / parent.scale.x) * 100)
-		node.fixedPosition = { left, top, width, height }
+	// we add a dummy node so that base and root folders don't get merged and the fixed position of root won't be ignored
+	const dummyNode = {
+		name: "dummy",
+		type: "Folder",
+		attributes: {},
+		children: [],
+		fixedPosition: { top: 0, left: 0, width: 0, height: 0 }
 	}
-	if (name === "root") {
-		node.fixedPosition.top = Math.floor(50 - node.fixedPosition.height / 2)
-		node.fixedPosition.left = 0
+	const nodes = [{ name: "base", type: "Folder", attributes: {}, children: [dummyNode] }]
+	gameObjectPositions.push(createBaseGameObjectPosition(gameObjectPositions))
+
+	for (const gameObjectPosition of gameObjectPositions) {
+		const nodeNames = gameObjectPosition.name.split(".")
+		if (nodeNames[0] !== "base") {
+			addNodeRecursively(nodeNames, nodes[0].children, gameObjectPosition, "base", gameObjectPositions)
+		}
 	}
+
+	ccJson.data.nodes = nodes
+	ccJson.data["edges"] = cycles.map(cycle => createEdge(cycle))
+	ccJson.data["attributeTypes"] = createAttributeTypes()
+	ccJson.checksum = md5(JSON.stringify(ccJson.data))
+	return ccJson
 }
 
 function addNodeRecursively(nodeNames, nodes, gameObjectPosition, parentNodeName, gameObjectPositions) {
@@ -56,6 +68,32 @@ function addNodeRecursively(nodeNames, nodes, gameObjectPosition, parentNodeName
 
 	const newParentName = parentNodeName === "base" ? node.name : `${parentNodeName}.${node.name}`
 	addNodeRecursively(nodeNames, node["children"], gameObjectPosition, newParentName, gameObjectPositions)
+}
+
+function isFile(names: string[]) {
+	return names.length === 0
+}
+
+function nodeAlreadyExists(nodes, name) {
+	return nodes.some(node => node.name === name)
+}
+
+function calculateFixedFolderPosition(node, name, parent, gameObjectPosition) {
+	if (node.type === "Folder" && name !== "base") {
+		const cornerXofParent = parent.position.x - parent.scale.x / 2
+		const cornerZofParent = parent.position.z - parent.scale.z / 2
+		const cornerXofChild = gameObjectPosition.position.x - gameObjectPosition.scale.x / 2
+		const cornerZofChild = gameObjectPosition.position.z - gameObjectPosition.scale.z / 2
+		const top = Math.floor(((cornerXofChild - cornerXofParent) / parent.scale.x) * 100)
+		const left = Math.floor(((cornerZofChild - cornerZofParent) / parent.scale.z) * 100)
+		const width = Math.floor((gameObjectPosition.scale.z / parent.scale.z) * 100)
+		const height = Math.floor((gameObjectPosition.scale.x / parent.scale.x) * 100)
+		node.fixedPosition = { left, top, width, height }
+	}
+	if (name === "root") {
+		node.fixedPosition.top = Math.floor(50 - node.fixedPosition.height / 2)
+		node.fixedPosition.left = 0
+	}
 }
 
 function addWrappedFolderName(filePath) {
@@ -97,42 +135,4 @@ function createBaseGameObjectPosition(gameObjectPositions) {
 			z: longEdge
 		}
 	}
-}
-
-export function parseGameObjectsFile(data) {
-	const { gameObjectPositions, cycles = [] } = JSON.parse(data)
-
-	const ccJson = {
-		checksum: "",
-		data: {
-			projectName: "ScriptProjectName",
-			fileChecksum: "",
-			apiVersion: "1.3",
-			nodes: []
-		}
-	}
-
-	// we add a dummy node so that base and root folders don't get merged and the fixed position of root won't be ignored
-	const dummyNode = {
-		name: "dummy",
-		type: "Folder",
-		attributes: {},
-		children: [],
-		fixedPosition: { top: 0, left: 0, width: 0, height: 0 }
-	}
-	const nodes = [{ name: "base", type: "Folder", attributes: {}, children: [dummyNode] }]
-	gameObjectPositions.push(createBaseGameObjectPosition(gameObjectPositions))
-
-	for (const gameObjectPosition of gameObjectPositions) {
-		const nodeNames = gameObjectPosition.name.split(".")
-		if (nodeNames[0] !== "base") {
-			addNodeRecursively(nodeNames, nodes[0].children, gameObjectPosition, "base", gameObjectPositions)
-		}
-	}
-
-	ccJson.data.nodes = nodes
-	ccJson.data["edges"] = cycles.map(cycle => createEdge(cycle))
-	ccJson.data["attributeTypes"] = createAttributeTypes()
-	ccJson.checksum = md5(JSON.stringify(ccJson.data))
-	return ccJson
 }
