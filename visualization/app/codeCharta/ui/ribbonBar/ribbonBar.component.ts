3150a68246165ed0206bf8431485a4a77d63c3aa
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-02-16 19:48:47 +0100

    Implement showing edge metric panel when file has edge metrics #2066

--- visualization/app/codeCharta/ui/ribbonBar/ribbonBar.component.ts
+++ visualization/app/codeCharta/ui/ribbonBar/ribbonBar.component.ts
@@ -1,7 +1,7 @@
 import "./ribbonBar.component.scss"
 import { IRootScopeService } from "angular"
 import { StoreService } from "../../state/store.service"
-import { PanelSelection } from "../../codeCharta.model"
+import { EdgeMetricData, PanelSelection } from "../../codeCharta.model"
 import { setPanelSelection } from "../../state/store/appSettings/panelSelection/panelSelection.actions"
 import { PanelSelectionService, PanelSelectionSubscriber } from "../../state/store/appSettings/panelSelection/panelSelection.service"
 import { CodeChartaMouseEventService } from "../../codeCharta.mouseEvent.service"
@@ -12,8 +12,9 @@ import {
 import { FileState } from "../../model/files/files"
 import { isDeltaState } from "../../model/files/files.helper"
 import { FilesService } from "../../state/store/files/files.service"
+import { EdgeMetricDataService } from "../../state/store/metricData/edgeMetricData/edgeMetricData.service"
 
-export class RibbonBarController implements PanelSelectionSubscriber, ExperimentalFeaturesEnabledSubscriber {
+export class RibbonBarController implements PanelSelectionSubscriber, ExperimentalFeaturesEnabledSubscriber, EdgeMetricData {
 	constructor(
 		private storeService: StoreService,
 		private $rootScope: IRootScopeService,
@@ -23,20 +24,30 @@ export class RibbonBarController implements PanelSelectionSubscriber, Experiment
 		PanelSelectionService.subscribe(this.$rootScope, this)
 		ExperimentalFeaturesEnabledService.subscribe(this.$rootScope, this)
 		FilesService.subscribe(this.$rootScope, this)
+		EdgeMetricDataService.subscribe(this.$rootScope, this)
 	}
 
+	name: string
+	maxValue: number
+	minValue: number
 	private _viewModel: {
 		panelSelection: PanelSelection
 		panelSelectionValues: typeof PanelSelection
 		experimentalFeaturesEnabled: boolean
 		isDeltaState: boolean
+		hasEdgeMetric: boolean
 		files: FileState[]
 	} = {
 		panelSelection: PanelSelection.NONE,
 		panelSelectionValues: PanelSelection,
 		experimentalFeaturesEnabled: false,
 		isDeltaState: null,
+		hasEdgeMetric: false,
 		files: null
+	}
+
+	onEdgeMetricDataChanged(edgeMetricData: EdgeMetricData[]) {
+		this._viewModel.hasEdgeMetric = edgeMetricData.length > 0
 	}
 
 	onFilesSelectionChanged(files: FileState[]) {
