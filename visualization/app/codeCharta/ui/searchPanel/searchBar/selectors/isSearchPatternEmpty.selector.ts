8dd405260635c896d36aa43b198a240a53f681c8
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-02-20 20:23:08 +0100

    refactor: add first test
    ref #2318

--- visualization/app/codeCharta/ui/searchPanel/searchBar/selectors/isSearchPatternEmpty.selector.ts
+++ visualization/app/codeCharta/ui/searchPanel/searchBar/selectors/isSearchPatternEmpty.selector.ts
@@ -1,7 +1,6 @@
 import { createSelector } from "../../../../state/angular-redux/createSelector"
 import { searchPatternSelector } from "../../../../state/store/dynamicSettings/searchPattern/searchPattern.selector"
 
-export const isSearchPatternEmptySelector = createSelector(
-	[searchPatternSelector],
-	searchPattern => searchPattern === "" || searchPattern === "!" || searchPattern === ","
-)
+export const _isSearchPatternEmpty = (searchPattern: string) => searchPattern === "" || searchPattern === "!" || searchPattern === ","
+
+export const isSearchPatternEmptySelector = createSelector([searchPatternSelector], _isSearchPatternEmpty)
