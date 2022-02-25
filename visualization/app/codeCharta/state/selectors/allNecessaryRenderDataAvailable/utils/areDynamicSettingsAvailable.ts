3a61599897472e05384bf1b97a83296ef6d6e0f9
Torsten Knauf <Torsten.Knauf@gmx.de>
2022-02-25 09:21:02 +0100

    fix: loading of files without edge metric never finished (#2683)
    ref #2680

--- visualization/app/codeCharta/state/selectors/allNecessaryRenderDataAvailable/utils/areDynamicSettingsAvailable.ts
+++ visualization/app/codeCharta/state/selectors/allNecessaryRenderDataAvailable/utils/areDynamicSettingsAvailable.ts
@@ -1,10 +1,11 @@
 import { ColorRange, DynamicSettings } from "../../../../codeCharta.model"
 
-const isDynamicSettingAvailable = (setting: null | string | number | string[] | ColorRange) => {
+export const _isDynamicSettingAvailable = (name: string, setting: null | string | number | string[] | ColorRange) => {
+	if (name === "edgeMetric") return true
 	if (setting === null) return false
 	if (typeof setting !== "object") return true
 	return Object.values(setting).every(v => v !== null)
 }
 
 export const areDynamicSettingsAvailable = (dynamicSettings: Partial<DynamicSettings>) =>
-	Object.values(dynamicSettings).every(value => isDynamicSettingAvailable(value))
+	Object.entries(dynamicSettings).every(([name, value]) => _isDynamicSettingAvailable(name, value))
