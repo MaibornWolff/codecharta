4ee9fc24023080295e8caffe65d574784c05fcf9
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-03-04 17:15:14 +0100

    Adjust tests in nodePath component #95

--- visualization/app/codeCharta/ui/attributeSideBar/attributeSideBarHeaderSection/nodePath/nodePath.component.spec.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeSideBarHeaderSection/nodePath/nodePath.component.spec.ts
@@ -1,19 +1,23 @@
 import { render } from "@testing-library/angular"
 import { CodeMapNode } from "../../../../codeCharta.model"
-
 import { selectedNodeSelector } from "../../../../state/selectors/selectedNode.selector"
 import { NodePathComponent } from "./nodePath.component"
+import { isDeltaStateSelector } from "../../../../state/selectors/isDeltaState.selector"
 
 jest.mock("../../../../state/selectors/selectedNode.selector", () => ({
 	selectedNodeSelector: jest.fn()
 }))
+jest.mock("../../../../state/selectors/isDeltaState.selector", () => ({
+	isDeltaStateSelector: jest.fn()
+}))
+
 const selectedNodeSelectorMock = selectedNodeSelector as jest.Mock
+const isDeltaStateSelectorMock = isDeltaStateSelector as jest.Mock
 
 describe("nodePathComponent", () => {
 	it("should display an empty p tag, if no building is selected", async () => {
 		const { container } = await render(NodePathComponent, { componentProperties: { node: undefined } })
 		const pTag = container.querySelector("p")
-
 		expect(pTag).not.toBe(null)
 		expect(pTag.textContent).toBe("")
 	})
@@ -26,15 +30,38 @@ describe("nodePathComponent", () => {
 		expect(container.textContent).toContain("some/file.ts")
 	})
 
-	it("should display node path and fileCountDescription,, when a folder is selected", async () => {
+	it("should display node path and amount of files, when a folder is selected and delta mode is not enabled", async () => {
 		const node = {
 			children: [{}] as CodeMapNode[],
 			path: "some/folder",
-			attributes: { unary: 2 }
+			attributes: { unary: 2 },
+			deltas: {
+				addedFiles: 1,
+				removedFiles: 2
+			}
 		}
 		selectedNodeSelectorMock.mockImplementation(() => node)
 
 		const { container } = await render(NodePathComponent, { componentProperties: { node } })
-		expect(container.textContent.replace(/\s+/g, " ")).toContain("some/folder (2 files)")
+		//TODO: adjust string when white space is fixed in nodePath template
+		expect(container.textContent.replace(/\s+/g, " ")).toContain("some/folder (2 files )")
+	})
+
+	it("should display node path,amount of files, added and removed files, when a folder is selected and delta mode is enabled", async () => {
+		const node = {
+			children: [{}] as CodeMapNode[],
+			path: "some/folder",
+			attributes: { unary: 2 },
+			deltas: {
+				addedFiles: 1,
+				removedFiles: 2
+			}
+		}
+		isDeltaStateSelectorMock.mockImplementation(() => true)
+		selectedNodeSelectorMock.mockImplementation(() => node)
+
+		const { container } = await render(NodePathComponent, { componentProperties: { node } })
+		//TODO: adjust string when white space is fixed in nodePath template
+		expect(container.textContent.replace(/\s+/g, " ")).toContain("some/folder (2 files | Δ1 | Δ2 )")
 	})
 })
