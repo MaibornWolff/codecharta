0465c83d55c9677b3504fc86da6f30a91706d6e1
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-03-04 16:40:03 +0100

    Adjust tests for file count selector #95

--- visualization/app/codeCharta/ui/attributeSideBar/attributeSideBarHeaderSection/nodePath/fileCountDescription.selector.spec.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeSideBarHeaderSection/nodePath/fileCountDescription.selector.spec.ts
@@ -1,22 +1,17 @@
 import { getFileCount } from "./fileCountSelector"
 
-describe("building2fileCountDescription", () => {
+describe("getFileCount", () => {
 	it("should return nothing when it receives no building", () => {
 		const node = undefined
 		expect(getFileCount(node)).toBe(undefined)
 	})
 
-	it("should return 'empty' when it receives a building with no unary value", () => {
-		const node = { attributes: { unary: 0 } }
-		expect(getFileCount(node)).toBe("empty")
+	it("should return file count object with default values when it receives a building with no attributes and deltas", () => {
+		const node = {}
+		expect(getFileCount(node)).toEqual({ fileCount: 0, added: 0, removed: 0 })
 	})
 
-	it("should return '1 file' when it receives a building with a unary value of 1", () => {
-		const node = { attributes: { unary: 1 } }
-		expect(getFileCount(node)).toBe("1 file")
-	})
-
-	it("should return '4 files' when it receives a building with a unary value of 4", () => {
+	it("should return file count object that has the same values as the value assignments of the received attributes and deltas", () => {
 		const node = { attributes: { unary: 4 }, deltas: { addedFiles: 2, removedFiles: 1 } }
 		expect(getFileCount(node)).toEqual({ fileCount: 4, added: 2, removed: 1 })
 	})
