1c888a8a0f1327e5997935be3c402ad5e36cfa51
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-03-07 11:36:48 +0100

    WIP add tests for add custom config dialog #2489

--- visualization/app/codeCharta/ui/customConfigs/addCustomConfigButton/addCustomConfigDialog/addCustomConfigDialog.spec.ts
+++ visualization/app/codeCharta/ui/customConfigs/addCustomConfigButton/addCustomConfigDialog/addCustomConfigDialog.spec.ts
@@ -1,26 +1,51 @@
 import { TestBed } from "@angular/core/testing"
 import { AddCustomConfigButtonModule } from "../addCustomConfigButton.module"
-import { MatDialog } from "@angular/material/dialog"
 import { render, screen } from "@testing-library/angular"
 import { AddCustomConfigDialogComponent } from "./addCustomConfigDialog.component"
 import { CustomConfigHelper } from "../../../../util/customConfigHelper"
+import userEvent from "@testing-library/user-event"
+import { MatDialog } from "@angular/material/dialog"
 
 describe("addCustomConfigDialogComponent", () => {
-	const mockedDialog = { open: jest.fn() }
+	jest.spyOn(CustomConfigHelper, "getConfigNameSuggestionByFileState").mockReturnValue("new custom view name")
 
 	beforeEach(async () => {
+		const mockedDialog = { open: jest.fn() }
 		TestBed.configureTestingModule({
 			imports: [AddCustomConfigButtonModule],
 			providers: [{ provide: MatDialog, useValue: mockedDialog }]
 		})
 	})
 
-	it("should suggest a valid custom view name", async () => {
-		jest.spyOn(CustomConfigHelper, "getConfigNameSuggestionByFileState").mockReturnValue("new custom view name")
+	it("should suggest a valid custom view name and 'add' button is enabled", async () => {
 		await render(AddCustomConfigDialogComponent, { excludeComponentDeclaration: true })
 
 		const input = screen.getByRole("textbox") as HTMLInputElement
 
 		expect(input.value).toBe("new custom view name")
+		expect((screen.getByRole("button") as HTMLButtonElement).disabled).toBe(false)
+	})
+
+	it("should show error message when input field is empty and disable 'add' button", async () => {
+		await render(AddCustomConfigDialogComponent, { excludeComponentDeclaration: true })
+
+		const input = screen.getByRole("textbox") as HTMLInputElement
+		userEvent.clear(input)
+		input.blur()
+
+		expect(await screen.findByText("Please enter a view name.")).not.toBeNull()
+		expect((screen.getByRole("button") as HTMLButtonElement).disabled).toBe(true)
+	})
+
+	it("should show error message when file name already exists and disable 'add' button", async () => {
+		await render(AddCustomConfigDialogComponent, { excludeComponentDeclaration: true })
+		jest.spyOn(CustomConfigHelper, "hasCustomConfigByName").mockReturnValue(true)
+
+		const input = screen.getByRole("textbox") as HTMLInputElement
+		userEvent.type(input, "file name already exists")
+		input.blur()
+
+		expect(await screen.findByText("A Custom View with this name already exists.")).not.toBeNull()
+		expect((screen.getByRole("button") as HTMLButtonElement).disabled).toBe(true)
 	})
 })
