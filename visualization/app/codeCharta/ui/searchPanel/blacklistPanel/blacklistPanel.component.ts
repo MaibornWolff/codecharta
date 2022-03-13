0b5c626645f372c518ed5a3e91615487b90017d1
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-03-13 14:44:31 +0100

    refactor: remover code duplication and make code better testable
    ref #2318

--- visualization/app/codeCharta/ui/searchPanel/blacklistPanel/blacklistPanel.component.ts
+++ visualization/app/codeCharta/ui/searchPanel/blacklistPanel/blacklistPanel.component.ts
@@ -1,18 +1,17 @@
 import "./blacklistPanel.component.scss"
 import { Component, Inject } from "@angular/core"
-import { BlacklistItem } from "../../../codeCharta.model"
+import { BlacklistItem, BlacklistType } from "../../../codeCharta.model"
 import { Store } from "../../../state/angular-redux/store"
 import { removeBlacklistItem } from "../../../state/store/fileSettings/blacklist/blacklist.actions"
-import { excludedItemsSelector } from "./selectors/excludedItems.selector"
-import { flattenedItemsSelector } from "./selectors/flattenedItems.selector"
+import { createBlacklistItemSelector } from "./createBlacklistItemSelector"
 
 @Component({
 	selector: "cc-blacklist-panel",
 	template: require("./blacklistPanel.component.html")
 })
 export class BlacklistPanelComponent {
-	flattenedItems$ = this.store.select(flattenedItemsSelector)
-	excludedItems$ = this.store.select(excludedItemsSelector)
+	flattenedItems$ = this.store.select(createBlacklistItemSelector(BlacklistType.flatten))
+	excludedItems$ = this.store.select(createBlacklistItemSelector(BlacklistType.exclude))
 
 	constructor(@Inject(Store) private store: Store) {}
 
