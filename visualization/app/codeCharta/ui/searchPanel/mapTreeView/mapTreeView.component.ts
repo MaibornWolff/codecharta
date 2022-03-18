ac59097381224f4b83b679011fea47275de3ca14
Torsten Knauf <Torsten.Knauf@gmx.de>
2022-03-18 10:24:24 +0100

    Refactor/2318/file structure (#2726)
    * refactor: move searchPanel components into searchPanel folder
    
    ref #2318
    
    * refactor: remove obsolete entry point
    
    as all its parent components are already migrated
    
    ref #2318
    
    * refactor: remove obsolete downgrades
    
    as all its parent components are already migrated
    
    ref #2318
    
    * refactor: adjust test import paths
    
    ref #2318
    
    * refactor: remove duplicated selector declaration
    
    ref #2318

---
+++ visualization/app/codeCharta/ui/searchPanel/mapTreeView/mapTreeView.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject } from "@angular/core"
+import { Observable } from "rxjs"
+
+import { CodeMapNode } from "../../../codeCharta.model"
+import { Store } from "../../../state/angular-redux/store"
+import { mapTreeViewNodeSelector } from "./mapTreeViewNodeSelector/mapTreeViewNode.selector"
+
+import "./mapTreeView.component.scss"
+
+@Component({
+	selector: "cc-map-tree-view",
+	template: require("./mapTreeView.component.html")
+})
+export class MapTreeViewComponent {
+	mapTreeViewNode$: Observable<CodeMapNode>
+
+	constructor(@Inject(Store) store: Store) {
+		this.mapTreeViewNode$ = store.select(mapTreeViewNodeSelector)
+	}
+}
---
+++ visualization/app/codeCharta/ui/searchPanel/matchingFilesCounter/matchingFilesCounter.component.ts
@@ -0,0 +1,17 @@
+import { Component, Inject } from "@angular/core"
+import { Observable } from "rxjs"
+import { Store } from "../../../state/angular-redux/store"
+import "./matchingFilesCounter.component.scss"
+import { MatchingFilesCounter, matchingFilesCounterSelector } from "./selectors/matchingFilesCounter.selector"
+
+@Component({
+	selector: "cc-matching-files-counter",
+	template: require("./matchingFilesCounter.component.html")
+})
+export class MatchingFilesCounterComponent {
+	matchingFileCounters$: Observable<MatchingFilesCounter>
+
+	constructor(@Inject(Store) store: Store) {
+		this.matchingFileCounters$ = store.select(matchingFilesCounterSelector)
+	}
+}
