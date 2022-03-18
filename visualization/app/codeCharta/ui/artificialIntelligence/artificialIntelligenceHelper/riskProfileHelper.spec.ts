915a7e84d4f03c5ac92aecd968121d8a16ba12e7
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-03-18 19:11:15 +0100

    Add test for calculations of percentages of risk profile #2710

--- visualization/app/codeCharta/ui/artificialIntelligence/artificialIntelligenceHelper/riskProfileHelper.spec.ts
+++ visualization/app/codeCharta/ui/artificialIntelligence/artificialIntelligenceHelper/riskProfileHelper.spec.ts
@@ -1,10 +1,10 @@
-import { calculateRiskProfile, RiskProfile } from "./riskProfileHelper"
+import { calculateRiskProfile, getPercentagesOfRiskProfile, RiskProfile } from "./riskProfileHelper"
 import { CodeMapNode, NodeType } from "../../../codeCharta.model"
 import { metricThresholdsByLanguage } from "./artificialIntelligence.metricThresholds"
 
 describe("riskProfileHelper", () => {
 	it("should calculate risk profile for java files", () => {
-		const actualRiskProfile: RiskProfile = { lowRisk: 0, moderateRisk: 0, highRisk: 0, veryHighRisk: 0 }
+		const actualRlocRisk: RiskProfile = { lowRisk: 0, moderateRisk: 0, highRisk: 0, veryHighRisk: 0 }
 		const fileExtension = "java"
 		const nodes: CodeMapNode[] = [
 			{ name: "javaFile1", type: NodeType.FILE, attributes: { rloc: 1, mcc: 48 } },
@@ -14,19 +14,31 @@ describe("riskProfileHelper", () => {
 		]
 
 		for (const node of nodes) {
-			calculateRiskProfile(node, actualRiskProfile, fileExtension)
+			calculateRiskProfile(node, actualRlocRisk, fileExtension)
 		}
 
-		expect(actualRiskProfile).toEqual({ lowRisk: 1, moderateRisk: 10, highRisk: 100, veryHighRisk: 1000 })
+		expect(actualRlocRisk).toEqual({ lowRisk: 1, moderateRisk: 10, highRisk: 100, veryHighRisk: 1000 })
 	})
 
 	it("should calculate risk profile for all occurring programming languages in files", () => {
 		const actualRiskProfile: RiskProfile = { lowRisk: 0, moderateRisk: 0, highRisk: 0, veryHighRisk: 0 }
 		const fileExtensions = ["java", "java", "java", "ts"]
 		const nodes: CodeMapNode[] = [
-			{ name: "javaFile1", type: NodeType.FILE, attributes: { rloc: 1, mcc: metricThresholdsByLanguage.java["mcc"].percentile70 } },
-			{ name: "javaFile2", type: NodeType.FILE, attributes: { rloc: 10, mcc: metricThresholdsByLanguage.java["mcc"].percentile80 } },
-			{ name: "javaFile3", type: NodeType.FILE, attributes: { rloc: 100, mcc: metricThresholdsByLanguage.java["mcc"].percentile90 } },
+			{
+				name: "javaFile1",
+				type: NodeType.FILE,
+				attributes: { rloc: 1, mcc: metricThresholdsByLanguage.java["mcc"].percentile70 }
+			},
+			{
+				name: "javaFile2",
+				type: NodeType.FILE,
+				attributes: { rloc: 10, mcc: metricThresholdsByLanguage.java["mcc"].percentile80 }
+			},
+			{
+				name: "javaFile3",
+				type: NodeType.FILE,
+				attributes: { rloc: 100, mcc: metricThresholdsByLanguage.java["mcc"].percentile90 }
+			},
 			{
 				name: "tsFile",
 				type: NodeType.FILE,
@@ -39,5 +51,17 @@ describe("riskProfileHelper", () => {
 		}
 
 		expect(actualRiskProfile).toEqual({ lowRisk: 1, moderateRisk: 10, highRisk: 1100, veryHighRisk: 0 })
+	})
+
+	it("should get percentages of risk profile", () => {
+		const rlocRisk: RiskProfile = { lowRisk: 1000, moderateRisk: 0, highRisk: 0, veryHighRisk: 1000 }
+		const totalRloc = Object.values(rlocRisk).reduce((a, b) => a + b)
+		const veryHighRiskRloc = totalRloc - rlocRisk.veryHighRisk
+		const veryHighRisk = Math.round((veryHighRiskRloc / totalRloc) * 100)
+		const expectedRiskProfile = { highRisk: 0, lowRisk: 100 - veryHighRisk, moderateRisk: 0, veryHighRisk }
+
+		const actualRiskProfile = getPercentagesOfRiskProfile(rlocRisk)
+
+		expect(actualRiskProfile).toEqual(expectedRiskProfile)
 	})
 })
