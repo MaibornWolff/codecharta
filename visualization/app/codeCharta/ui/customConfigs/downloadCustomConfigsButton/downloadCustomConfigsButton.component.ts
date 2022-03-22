ae73d88b5b87e796ff47cfb9a3b3be865442e596
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-03-22 18:24:23 +0100

    Improve selector for downloadable custom configs Note: When custom config menu is clicked several times the new selector is not active because there is no file change #2734

--- visualization/app/codeCharta/ui/customConfigs/downloadCustomConfigsButton/downloadCustomConfigsButton.component.ts
+++ visualization/app/codeCharta/ui/customConfigs/downloadCustomConfigsButton/downloadCustomConfigsButton.component.ts
@@ -3,7 +3,7 @@ import { CustomConfigHelper } from "../../../util/customConfigHelper"
 import { CustomConfigFileStateConnector } from "../customConfigFileStateConnector"
 import { State } from "../../../state/angular-redux/state"
 import { filesSelector } from "../../../state/store/files/files.selector"
-import { CustomConfig, ExportCustomConfig } from "../../../model/customConfig/customConfig.api.model"
+import { ExportCustomConfig } from "../../../model/customConfig/customConfig.api.model"
 import { downloadableCustomConfigsSelector } from "../selectors/downloadableCustomConfigs.selector"
 
 import { Subscription } from "rxjs"
@@ -12,9 +12,7 @@ import { Store } from "../../../state/angular-redux/store"
 @Component({ template: require("./downloadCustomConfigsButton.component.html") })
 export class DownloadCustomConfigsButtonComponent implements OnInit, OnDestroy {
 	private customConfigFileStateConnector: CustomConfigFileStateConnector
-	private downloadableConfigs: Map<string, ExportCustomConfig> = new Map()
-	testDownloadableConfigs: Map<string, number>
-	hasDownloadableConfigs = false
+	private downloadableConfigs: Map<string, ExportCustomConfig>
 	subscription: Subscription
 
 	constructor(@Inject(State) private state: State, @Inject(Store) private store: Store) {}
@@ -22,46 +20,20 @@ export class DownloadCustomConfigsButtonComponent implements OnInit, OnDestroy {
 	ngOnInit(): void {
 		const files = filesSelector(this.state.getValue())
 		this.subscription = this.store.select(downloadableCustomConfigsSelector).subscribe(downloadableConfigs => {
-			this.testDownloadableConfigs = downloadableConfigs
+			this.downloadableConfigs = downloadableConfigs
 		})
 
 		this.customConfigFileStateConnector = new CustomConfigFileStateConnector(files)
-		this.preloadDownloadableConfigs()
 	}
 
 	ngOnDestroy(): void {
 		this.subscription.unsubscribe()
 	}
 
-	private isConfigApplicableForUploadedMaps(customConfig: CustomConfig) {
-		const mapChecksumsOfConfig = customConfig.mapChecksum.split(";")
-		for (const checksumOfConfig of mapChecksumsOfConfig) {
-			if (this.customConfigFileStateConnector.isMapAssigned(checksumOfConfig)) {
-				return true
-			}
-		}
-		return false
-	}
-
-	private preloadDownloadableConfigs() {
-		this.downloadableConfigs.clear()
-		const customConfigs = CustomConfigHelper.getCustomConfigs()
-
-		for (const [key, value] of customConfigs.entries()) {
-			// Only Configs which are applicable for at least one of the uploaded maps should be downloaded.
-			if (this.isConfigApplicableForUploadedMaps(value)) {
-				this.downloadableConfigs.set(key, CustomConfigHelper.createExportCustomConfigFromConfig(value))
-			}
-		}
-
-		this.hasDownloadableConfigs = this.downloadableConfigs.size > 0
-	}
-
 	downloadPreloadedCustomConfigs() {
 		if (this.downloadableConfigs.size === 0) {
 			return
 		}
-
 		CustomConfigHelper.downloadCustomConfigs(this.downloadableConfigs, this.customConfigFileStateConnector)
 	}
 }
--- visualization/app/codeCharta/ui/customConfigs/selectors/downloadableCustomConfigs.selector.ts
+++ visualization/app/codeCharta/ui/customConfigs/selectors/downloadableCustomConfigs.selector.ts
@@ -1,16 +1,43 @@
 import { CcState } from "../../../state/store/store"
 import { createSelector } from "../../../state/angular-redux/createSelector"
-import { filesSelector } from "../../../state/store/files/files.selector"
 import { FileState } from "../../../model/files/files"
+import { CustomConfig, ExportCustomConfig } from "../../../model/customConfig/customConfig.api.model"
+import { CustomConfigHelper } from "../../../util/customConfigHelper"
+import { visibleFileStatesSelector } from "../../../state/selectors/visibleFileStates.selector"
 
-export type DownloadableConfigs = Map<string, number>
+export type DownloadableConfigs = Map<string, ExportCustomConfig>
 
-export const getDownloadableCustomViews = (files: FileState[]): DownloadableConfigs => {
-	const downloadableConfigs: DownloadableConfigs = new Map([["test", files.length]])
+export const getDownloadableCustomViews = (files: FileState[]): DownloadableConfigs | undefined => {
+	if (files === undefined) {
+		return
+	}
+
+	const downloadableConfigs = new Map() as DownloadableConfigs
+	const customConfigs = CustomConfigHelper.getCustomConfigs()
+	const mapChecksums = []
+
+	files.map(file => mapChecksums.push(file.file.fileMeta.fileChecksum))
+
+	for (const [checksum, customConfig] of customConfigs.entries()) {
+		// Only Configs which are applicable for at least one of the uploaded maps should be downloaded.
+		if (isConfigApplicableForUploadedMaps(customConfig, mapChecksums)) {
+			downloadableConfigs.set(checksum, CustomConfigHelper.createExportCustomConfigFromConfig(customConfig))
+		}
+	}
 	return downloadableConfigs
 }
 
+function isConfigApplicableForUploadedMaps(customConfig: CustomConfig, mapChecksums: string[]) {
+	const mapChecksumsOfConfig = customConfig.mapChecksum.split(";")
+	for (const checksumOfConfig of mapChecksumsOfConfig) {
+		if (mapChecksums.includes(checksumOfConfig)) {
+			return true
+		}
+	}
+	return false
+}
+
 export const downloadableCustomConfigsSelector: (state: CcState) => DownloadableConfigs = createSelector(
-	[filesSelector],
+	[visibleFileStatesSelector],
 	getDownloadableCustomViews
 )
