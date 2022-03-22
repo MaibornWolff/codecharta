9ea7ba70ea8f9da6f771265e7a5b42412e764ec6
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-03-22 12:40:59 +0100

    Move download functions to new component Note: customConfigFileStateConnector doesn't recognize changes when files change ToDo: Implement new selector that returns downloadable custom views, so that fileStateConnector is not necessary anymore #2734

--- visualization/app/codeCharta/ui/customConfigs/downloadCustomConfigsButton/downloadCustomConfigsButton.component.ts
+++ visualization/app/codeCharta/ui/customConfigs/downloadCustomConfigsButton/downloadCustomConfigsButton.component.ts
@@ -1,4 +1,53 @@
-import { Component } from "@angular/core"
+import { Component, Inject, OnInit } from "@angular/core"
+import { CustomConfigHelper } from "../../../util/customConfigHelper"
+import { CustomConfigFileStateConnector } from "../customConfigFileStateConnector"
+import { State } from "../../../state/angular-redux/state"
+import { filesSelector } from "../../../state/store/files/files.selector"
+import { CustomConfig, ExportCustomConfig } from "../../../model/customConfig/customConfig.api.model"
 
 @Component({ template: require("./downloadCustomConfigsButton.component.html") })
-export class DownloadCustomConfigsButtonComponent {}
+export class DownloadCustomConfigsButtonComponent implements OnInit {
+	private customConfigFileStateConnector: CustomConfigFileStateConnector
+	private downloadableConfigs: Map<string, ExportCustomConfig> = new Map()
+	hasDownloadableConfigs = false
+
+	constructor(@Inject(State) private state: State) {}
+
+	ngOnInit(): void {
+		const files = filesSelector(this.state.getValue())
+		this.customConfigFileStateConnector = new CustomConfigFileStateConnector(files)
+		this.preloadDownloadableConfigs()
+	}
+
+	private isConfigApplicableForUploadedMaps(customConfig: CustomConfig) {
+		const mapChecksumsOfConfig = customConfig.mapChecksum.split(";")
+		for (const checksumOfConfig of mapChecksumsOfConfig) {
+			if (this.customConfigFileStateConnector.isMapAssigned(checksumOfConfig)) {
+				return true
+			}
+		}
+		return false
+	}
+
+	private preloadDownloadableConfigs() {
+		this.downloadableConfigs.clear()
+		const customConfigs = CustomConfigHelper.getCustomConfigs()
+
+		for (const [key, value] of customConfigs.entries()) {
+			// Only Configs which are applicable for at least one of the uploaded maps should be downloaded.
+			if (this.isConfigApplicableForUploadedMaps(value)) {
+				this.downloadableConfigs.set(key, CustomConfigHelper.createExportCustomConfigFromConfig(value))
+			}
+		}
+
+		this.hasDownloadableConfigs = this.downloadableConfigs.size > 0
+	}
+
+	downloadPreloadedCustomConfigs() {
+		if (this.downloadableConfigs.size === 0) {
+			return
+		}
+
+		CustomConfigHelper.downloadCustomConfigs(this.downloadableConfigs, this.customConfigFileStateConnector)
+	}
+}
