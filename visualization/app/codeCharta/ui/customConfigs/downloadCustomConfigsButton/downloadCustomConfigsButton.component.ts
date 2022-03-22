7c66e9c7198772dd4e352e0c8a54496d23f3a9d5
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-03-22 18:58:43 +0100

    Remove FileStateConnector from component and remove unused state call #2734

--- visualization/app/codeCharta/ui/customConfigs/downloadCustomConfigsButton/downloadCustomConfigsButton.component.ts
+++ visualization/app/codeCharta/ui/customConfigs/downloadCustomConfigsButton/downloadCustomConfigsButton.component.ts
@@ -1,29 +1,22 @@
 import { Component, Inject, OnDestroy, OnInit } from "@angular/core"
-import { CustomConfigHelper } from "../../../util/customConfigHelper"
-import { CustomConfigFileStateConnector } from "../customConfigFileStateConnector"
-import { State } from "../../../state/angular-redux/state"
-import { filesSelector } from "../../../state/store/files/files.selector"
 import { ExportCustomConfig } from "../../../model/customConfig/customConfig.api.model"
 import { downloadableCustomConfigsSelector } from "../selectors/downloadableCustomConfigs.selector"
 
 import { Subscription } from "rxjs"
 import { Store } from "../../../state/angular-redux/store"
+import { downloadCustomConfigs } from "./downloadCustomConfigHelper"
 
 @Component({ template: require("./downloadCustomConfigsButton.component.html") })
 export class DownloadCustomConfigsButtonComponent implements OnInit, OnDestroy {
-	private customConfigFileStateConnector: CustomConfigFileStateConnector
 	private downloadableConfigs: Map<string, ExportCustomConfig>
 	subscription: Subscription
 
-	constructor(@Inject(State) private state: State, @Inject(Store) private store: Store) {}
+	constructor(@Inject(Store) private store: Store) {}
 
 	ngOnInit(): void {
-		const files = filesSelector(this.state.getValue())
 		this.subscription = this.store.select(downloadableCustomConfigsSelector).subscribe(downloadableConfigs => {
 			this.downloadableConfigs = downloadableConfigs
 		})
-
-		this.customConfigFileStateConnector = new CustomConfigFileStateConnector(files)
 	}
 
 	ngOnDestroy(): void {
@@ -34,6 +27,6 @@ export class DownloadCustomConfigsButtonComponent implements OnInit, OnDestroy {
 		if (this.downloadableConfigs.size === 0) {
 			return
 		}
-		CustomConfigHelper.downloadCustomConfigs(this.downloadableConfigs, this.customConfigFileStateConnector)
+		downloadCustomConfigs(this.downloadableConfigs)
 	}
 }
