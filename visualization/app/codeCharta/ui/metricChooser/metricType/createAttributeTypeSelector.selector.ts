946f2deaa4d2b6697d6da916c3d70fa0d1277e67
Torsten Knauf <Torsten.Knauf@gmx.de>
2022-03-23 08:58:01 +0100

    fix:  handling of attribute type (#2746)
    * fix:  handling of attribute type
    
    - set initial primary edges metric correctly
    - dispatch secondary metric type correctly
    
    ref #2731
    ref #2630
    
    * refactor: simplify extraction of attribute type
    
    ref #2731
    ref #2630

--- visualization/app/codeCharta/ui/metricChooser/metricType/createAttributeTypeSelector.selector.ts
+++ visualization/app/codeCharta/ui/metricChooser/metricType/createAttributeTypeSelector.selector.ts
@@ -1,9 +1,10 @@
 import { AttributeTypes, PrimaryMetrics } from "../../../codeCharta.model"
 import { createSelector } from "../../../state/angular-redux/createSelector"
-import { getAttributeTypeSelector } from "../../../state/selectors/getAttributeTypeOfNodesByMetric.selector"
+import { attributeTypesSelector } from "../../../state/store/fileSettings/attributeTypes/attributeTypes.selector"
 import { primaryMetricNamesSelector } from "../../attributeSideBar/attributeSideBarPrimaryMetrics/primaryMetricNames.selector"
 
 export const createAttributeTypeSelector = (metricType: keyof AttributeTypes, metricFor: keyof PrimaryMetrics) =>
-	createSelector([primaryMetricNamesSelector, getAttributeTypeSelector], (primaryMetricNames, getAttributeType) =>
-		getAttributeType(metricType, primaryMetricNames[metricFor]) === "relative" ? "x͂" : "Σ"
-	)
+	createSelector([primaryMetricNamesSelector, attributeTypesSelector], (primaryMetricNames, attributeTypes) => {
+		const metricName = primaryMetricNames[metricFor]
+		return attributeTypes[metricType][metricName] === "relative" ? "x͂" : "Σ"
+	})
