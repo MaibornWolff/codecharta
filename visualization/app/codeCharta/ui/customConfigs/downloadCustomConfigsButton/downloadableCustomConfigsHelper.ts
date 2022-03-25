e190db31f9dd8d42daf15d9ebd92c6f3d7894136
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-03-25 10:52:49 +0100

    Fix bug notified from sonar + do some renaming + refactoring #2734

--- visualization/app/codeCharta/ui/customConfigs/downloadCustomConfigsButton/downloadableCustomConfigsHelper.ts
+++ visualization/app/codeCharta/ui/customConfigs/downloadCustomConfigsButton/downloadableCustomConfigsHelper.ts
@@ -11,25 +11,22 @@ export const getDownloadableCustomConfigs = (files: FileState[]): DownloadableCo
 
 	const downloadableConfigs = new Map() as DownloadableConfigs
 	const customConfigs = CustomConfigHelper.getCustomConfigs()
-	const mapChecksums = []
+	const mapChecksumsOfFiles = []
 
-	files.map(file => mapChecksums.push(file.file.fileMeta.fileChecksum))
+	for (const file of files) {
+		mapChecksumsOfFiles.push(file.file.fileMeta.fileChecksum)
+	}
 
 	for (const [checksum, customConfig] of customConfigs.entries()) {
 		// Only Configs which are applicable for at least one of the uploaded maps should be downloaded.
-		if (isConfigApplicableForUploadedMaps(customConfig, mapChecksums)) {
+		if (isConfigApplicableForUploadedMaps(customConfig, mapChecksumsOfFiles)) {
 			downloadableConfigs.set(checksum, CustomConfigHelper.createExportCustomConfigFromConfig(customConfig))
 		}
 	}
 	return downloadableConfigs
 }
 
-function isConfigApplicableForUploadedMaps(customConfig: CustomConfig, mapChecksums: string[]) {
+function isConfigApplicableForUploadedMaps(customConfig: CustomConfig, mapChecksumsOfFiles: string[]) {
 	const mapChecksumsOfConfig = customConfig.mapChecksum.split(";")
-	for (const checksumOfConfig of mapChecksumsOfConfig) {
-		if (mapChecksums.includes(checksumOfConfig)) {
-			return true
-		}
-	}
-	return false
+	return mapChecksumsOfConfig.some(mapChecksumConfig => mapChecksumsOfFiles.includes(mapChecksumConfig))
 }
