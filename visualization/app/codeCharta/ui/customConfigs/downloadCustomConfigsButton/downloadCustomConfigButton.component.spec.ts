da1e42af69b72ba8760ea0915f766ececb5514a2
Maria Hallmann <72517530+Hall-Ma@users.noreply.github.com>
2022-03-30 10:13:09 +0200

    Refactor/2734/migrate download button for custom configs (#2747)
    * Add new Angular component
    #2734
    
    * Move download functions to new component
    Note: customConfigFileStateConnector doesn't recognize changes when files change
    ToDo: Implement new selector that returns downloadable custom views, so that fileStateConnector is not necessary anymore
    #2734
    
    * Implement prototype selector for downloadable custom configs
    Next step: implement correct type in Map
    #2734
    
    * Improve selector for downloadable custom configs
    Note: When custom config menu is clicked several times the new selector is not active because there is no file change
    #2734
    
    * Remove FileStateConnector from component and remove unused state call
    #2734
    
    * Combine observables to get downloadableConfigs when file state or local storage has changed
    #2734
    
    * Remove unused download button in custom configs component
    #2734
    
    * Adjust style for download button
    #2734
    
    * Adjust disabled button style for download custom config button
    #2734
    
    * Implement service for get downloadable configs
    #2734
    
    * Fix bug notified from sonar + do some renaming + refactoring
    #2734
    
    * Add new test with mocked service -> WIP
    #2734
    
    * Introduce new module for downloading custom configs + refactor downloadCustomConfig service
    #2734
    
    * Change function signature of getDownloadableCustomConfigs$
    #2734
    
    * Add tests
    #2734
    
    * Remove getter function because observable is already read only
    #2734
    
    * Delete unused tests and function in custom config component
    #2734
    
    * Add tests for get downloadable custom configs
    #2734

---
+++ visualization/app/codeCharta/ui/customConfigs/downloadCustomConfigsButton/downloadCustomConfigButton.component.spec.ts
@@ -0,0 +1,36 @@
+import { DownloadCustomConfigsButtonComponent } from "./downloadCustomConfigsButton.component"
+import { DownloadCustomConfigService } from "./downloadCustomConfig.service"
+import { of } from "rxjs"
+import { TestBed } from "@angular/core/testing"
+import { render, screen } from "@testing-library/angular"
+import userEvent from "@testing-library/user-event"
+import { FileDownloader } from "../../../util/fileDownloader"
+
+const mockedDownloadCustomConfigService = { downloadableCustomConfigs$: null }
+
+describe("DownloadCustomConfigButtonComponent", () => {
+	beforeEach(() => {
+		FileDownloader.downloadData = jest.fn()
+		TestBed.configureTestingModule({
+			providers: [{ provide: DownloadCustomConfigService, useValue: mockedDownloadCustomConfigService }]
+		})
+	})
+
+	it("should disable download button when no custom configs are available ", async () => {
+		mockedDownloadCustomConfigService.downloadableCustomConfigs$ = of(new Map())
+		await render(DownloadCustomConfigsButtonComponent)
+
+		expect((screen.getByRole("button") as HTMLButtonElement).disabled).toBe(true)
+	})
+
+	it("should click download button when custom configs are available ", async () => {
+		mockedDownloadCustomConfigService.downloadableCustomConfigs$ = of(new Map([["invalid-md5", {}]]))
+		await render(DownloadCustomConfigsButtonComponent)
+
+		expect((screen.getByRole("button") as HTMLButtonElement).disabled).toBe(false)
+
+		userEvent.click(screen.getByRole("button") as HTMLButtonElement)
+
+		expect(FileDownloader.downloadData).toHaveBeenCalledTimes(1)
+	})
+})
---
+++ visualization/app/codeCharta/ui/customConfigs/downloadCustomConfigsButton/getDownloadableCustomConfigs.spec.ts
@@ -0,0 +1,42 @@
+import { getDownloadableCustomConfigs } from "./getDownloadableCustomConfigs"
+import { FILE_STATES_JAVA } from "../../../util/dataMocks"
+import { CustomConfig, CustomConfigMapSelectionMode } from "../../../model/customConfig/customConfig.api.model"
+import { CustomConfigHelper } from "../../../util/customConfigHelper"
+
+describe("getDownloadableCustomConfigs", () => {
+	it("should get an empty map when file state is undefined", () => {
+		const actualDownloadableCustomConfigs = getDownloadableCustomConfigs(undefined)
+
+		expect(actualDownloadableCustomConfigs.size).toBe(0)
+	})
+
+	it("should get an empty map when no applicable custom configs are available", () => {
+		const customConfigStub = {
+			id: "invalid-md5-checksum",
+			name: "stubbedConfig2",
+			mapChecksum: "invalid-md5-checksum",
+			mapSelectionMode: CustomConfigMapSelectionMode.SINGLE,
+			assignedMaps: ["test.cc.json"],
+			stateSettings: {}
+		} as CustomConfig
+		CustomConfigHelper.getCustomConfigs = jest.fn().mockReturnValue(new Map([["invalid-md5-checksum", customConfigStub]]))
+		const actualDownloadableCustomConfigs = getDownloadableCustomConfigs(FILE_STATES_JAVA)
+
+		expect(actualDownloadableCustomConfigs.size).toBe(0)
+	})
+
+	it("should get a map with downloadable custom configs when no applicable custom configs are available", () => {
+		const customConfigStub = {
+			id: "md5-fileA",
+			name: "stubbedConfig2",
+			mapChecksum: "md5-fileA",
+			mapSelectionMode: CustomConfigMapSelectionMode.SINGLE,
+			assignedMaps: ["test.cc.json"],
+			stateSettings: {}
+		} as CustomConfig
+		CustomConfigHelper.getCustomConfigs = jest.fn().mockReturnValue(new Map([["md5-fileA", customConfigStub]]))
+		const actualDownloadableCustomConfigs = getDownloadableCustomConfigs(FILE_STATES_JAVA)
+
+		expect(actualDownloadableCustomConfigs.size).toBe(1)
+	})
+})
