91fb0ad4347720374e07d0fd270c7570cdc8c89d
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-04-04 09:44:33 +0200

    update footer in ccsh #2341

--- analysis/tools/ccsh/src/main/kotlin/de/maibornwolff/codecharta/tools/ccsh/Ccsh.kt
+++ analysis/tools/ccsh/src/main/kotlin/de/maibornwolff/codecharta/tools/ccsh/Ccsh.kt
@@ -109,7 +109,7 @@ class Ccsh : Callable<Void?> {
             return arrayOf(
                 Ccsh::class.java.`package`.implementationTitle + "\n" +
                     "version \"" + Ccsh::class.java.`package`.implementationVersion + "\"\n" +
-                    "Copyright(c) 2020, MaibornWolff GmbH"
+                    "Copyright(c) 2022, MaibornWolff GmbH"
             )
         }
     }
