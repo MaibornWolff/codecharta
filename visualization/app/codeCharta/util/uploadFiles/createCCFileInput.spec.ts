711c553cd2b53433cc8c05568ad9bf48b495a7bb
Torsten Knauf <Torsten.Knauf@gmx.de>
2022-04-06 18:03:29 +0200

    Update visualization/app/codeCharta/util/uploadFiles/createCCFileInput.spec.ts
    Co-authored-by: Maria Hallmann <72517530+Hall-Ma@users.noreply.github.com>

--- visualization/app/codeCharta/util/uploadFiles/createCCFileInput.spec.ts
+++ visualization/app/codeCharta/util/uploadFiles/createCCFileInput.spec.ts
@@ -1,7 +1,7 @@
 import { createCCFileInput } from "./createCCFileInput"
 
 describe("createCCFileInput", () => {
-	it("should create a file input for json or gs files", () => {
+	it("should create a file input for json or gz files", () => {
 		const ccFileInput = createCCFileInput()
 		expect(ccFileInput.nodeName).toBe("INPUT")
 		expect(ccFileInput.type).toBe("file")
