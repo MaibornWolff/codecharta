41f52418e2de1e5d28375fa8572f2ae09ae176ee
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-04-10 11:55:08 +0200

    Implement apply custom config function, still WIP #2734

--- visualization/app/codeCharta/ui/customConfigs/customConfigList/customConfigItemGroup.component.ts
+++ visualization/app/codeCharta/ui/customConfigs/customConfigList/customConfigItemGroup.component.ts
@@ -1,6 +1,14 @@
-import { Component, Input } from "@angular/core"
+import { Component, Inject, Input } from "@angular/core"
 import { CustomConfigHelper } from "../../../util/customConfigHelper"
 import { CustomConfigItemGroup } from "../customConfigs.component"
+import { Store } from "../../../state/angular-redux/store"
+import { setState } from "../../../state/store/state.actions"
+import { setColorRange } from "../../../state/store/dynamicSettings/colorRange/colorRange.actions"
+import { ColorRange } from "../../../codeCharta.model"
+import { setMargin } from "../../../state/store/dynamicSettings/margin/margin.actions"
+import { setCamera } from "../../../state/store/appSettings/camera/camera.actions"
+import { setCameraTarget } from "../../../state/store/appSettings/cameraTarget/cameraTarget.actions"
+import { Vector3 } from "three"
 
 @Component({
 	selector: "cc-custom-config-item-group",
@@ -11,9 +19,20 @@ export class CustomConfigItemGroupComponent {
 	@Input() isApplicable: boolean
 	isCollapsed = false
 
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	constructor(@Inject(Store) private store: Store) {}
+
 	applyCustomConfig(configId: string) {
-		//CustomConfigHelper.applyCustomConfig(configId, this.storeService, this.threeCameraService, this.threeOrbitControlsService)
+		const customConfig = CustomConfigHelper.getCustomConfigSettings(configId)
+
+		this.store.dispatch(setState(customConfig.stateSettings))
+		this.store.dispatch(setColorRange(customConfig.stateSettings.dynamicSettings.colorRange as ColorRange))
+		this.store.dispatch(setMargin(customConfig.stateSettings.dynamicSettings.margin))
+
+		//Now camera position is not set yet
+		this.store.dispatch(setCamera(customConfig.stateSettings.appSettings.camera as Vector3))
+		this.store.dispatch(setCameraTarget(customConfig.stateSettings.appSettings.cameraTarget as Vector3))
+
+		//CustomConfigHelper.applyCustomConfig(configId, this.store, this.threeCameraService, this.threeOrbitControlsService)
 	}
 
 	removeCustomConfig(configId: string) {
