b1092b800e610baf4c0441d5be0d052cb262d01e
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-04-26 12:38:55 +0200

    add more tests to ParserService and fix ccsh tests #2341

--- analysis/tools/ccsh/src/test/kotlin/de/maibornwolff/codecharta/ccsh/CcshTest.kt
+++ analysis/tools/ccsh/src/test/kotlin/de/maibornwolff/codecharta/ccsh/CcshTest.kt
@@ -2,13 +2,30 @@ package de.maibornwolff.codecharta.ccsh
 
 import de.maibornwolff.codecharta.tools.ccsh.Ccsh
 import de.maibornwolff.codecharta.tools.ccsh.parser.ParserService
-import io.mockk.*
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockkObject
+import io.mockk.verify
+import io.mockk.unmockkAll
 import org.assertj.core.api.Assertions
+import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import picocli.CommandLine
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
+import java.io.PrintWriter
+import java.io.StringWriter
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CcshTest {
+
+    @AfterAll
+    fun afterTest(){
+        unmockkAll()
+    }
 
     @Test
     fun `should convert parameters to kebab-case`() {
@@ -20,6 +37,18 @@ class CcshTest {
 
         Assertions.assertThat(outStream.toString()).contains("--default-excludes-s=AbC")
         System.setOut(originalOut)
+    }
+
+    @Test
+    fun `should return help message`() {
+        val ccshCLI = CommandLine(Ccsh())
+        val contentOutput = StringWriter()
+        ccshCLI.out = PrintWriter(contentOutput)
+
+        val exitCode = ccshCLI.execute("-h")
+
+        Assertions.assertThat(exitCode).isEqualTo(0)
+        Assertions.assertThat(contentOutput.toString()).contains("Usage: ccsh [-hv] [COMMAND]","Command Line Interface for CodeCharta analysis")
     }
 
     @Test
@@ -41,11 +70,11 @@ class CcshTest {
     fun `should call interactive parser`() {
         mockkObject(ParserService)
         every {
-            ParserService.executeSelectedParser(any())
-        } just Runs
-        every {
             ParserService.selectParser(any())
         } returns "someparser"
+        every {
+            ParserService.executeSelectedParser(any())
+        } just Runs
 
         Ccsh.main(emptyArray())
 
--- analysis/tools/ccsh/src/test/kotlin/de/maibornwolff/codecharta/ccsh/parser/ParserServiceTest.kt
+++ analysis/tools/ccsh/src/test/kotlin/de/maibornwolff/codecharta/ccsh/parser/ParserServiceTest.kt
@@ -12,12 +12,29 @@ import io.mockk.mockkObject
 import io.mockk.mockkStatic
 import io.mockk.verify
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import picocli.CommandLine
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ParserServiceTest {
+
+    private val outContent = ByteArrayOutputStream()
+    private val originalOut = System.out
+
+    @BeforeAll
+    fun setUpStreams() {
+        System.setOut(PrintStream(outContent))
+    }
+
+    @AfterAll
+    fun restoreStreams() {
+        System.setOut(originalOut)
+    }
 
     @Test
     fun `should return the selected parser name`() {
@@ -33,35 +50,95 @@ class ParserServiceTest {
 
     @Test
     fun `should execute check parser`() {
-        val outStream = ByteArrayOutputStream()
-        val originalOut = System.out
-        System.setOut(PrintStream(outStream))
-
         ParserService.executeSelectedParser("check")
 
-        Assertions.assertThat(outStream.toString()).contains("Not implemented yet")
-        System.setOut(originalOut)
+        Assertions.assertThat(outContent.toString()).contains("Not implemented yet")
     }
 
     @Test
-    fun `execute merge`() {
-        val outStream = ByteArrayOutputStream()
-        val originalOut = System.out
-        System.setOut(PrintStream(outStream))
-
+    fun `should execute merge parser`() {
         ParserService.executeSelectedParser("merge")
 
-        Assertions.assertThat(outStream.toString()).contains("Not implemented yet")
-        System.setOut(originalOut)
+        Assertions.assertThat(outContent.toString()).contains("Not implemented yet")
     }
 
     @Test
-    fun `execute edgefilter`() {
+    fun `should execute edgefilter parser`() {
         mockkObject(EdgeFilter)
         every {
             EdgeFilter.main(any())
         } just Runs
         ParserService.executeSelectedParser("edgefilter")
         verify { EdgeFilter.main(emptyArray()) }
+    }
+
+    @Test
+    fun `should execute modify parser`() {
+        ParserService.executeSelectedParser("modify")
+
+        Assertions.assertThat(outContent.toString()).contains("Not implemented yet")
+    }
+
+    @Test
+    fun `should execute sonarimport parser`() {
+        ParserService.executeSelectedParser("sonarimport")
+
+        Assertions.assertThat(outContent.toString()).contains("Not implemented yet")
+    }
+
+    @Test
+    fun `should execute sourcemonitorimport parser`() {
+        ParserService.executeSelectedParser("sourcemonitorimport")
+
+        Assertions.assertThat(outContent.toString()).contains("Not implemented yet")
+    }
+
+    @Test
+    fun `should execute gitlogparser parser`() {
+        ParserService.executeSelectedParser("gitlogparser")
+
+        Assertions.assertThat(outContent.toString()).contains("Not implemented yet")
+    }
+
+    @Test
+    fun `should execute svnlogparser parser`() {
+        ParserService.executeSelectedParser("svnlogparser")
+
+        Assertions.assertThat(outContent.toString()).contains("Not implemented yet")
+    }
+
+    @Test
+    fun `should execute csvexport parser`() {
+        ParserService.executeSelectedParser("csvexport")
+
+        Assertions.assertThat(outContent.toString()).contains("Not implemented yet")
+    }
+
+    @Test
+    fun `should execute sourcecodeparser parser`() {
+        ParserService.executeSelectedParser("sourcecodeparser")
+
+        Assertions.assertThat(outContent.toString()).contains("Not implemented yet")
+    }
+
+    @Test
+    fun `should execute tokeiimporter parser`() {
+        ParserService.executeSelectedParser("tokeiimporter")
+
+        Assertions.assertThat(outContent.toString()).contains("Not implemented yet")
+    }
+
+    @Test
+    fun `should execute rawtextparser parser`() {
+        ParserService.executeSelectedParser("rawtextparser")
+
+        Assertions.assertThat(outContent.toString()).contains("Not implemented yet")
+    }
+
+    @Test
+    fun `should not execute any parser`() {
+        ParserService.executeSelectedParser("unkownparser")
+
+        Assertions.assertThat(outContent.toString()).contains("No valid parser was selected.")
     }
 }
