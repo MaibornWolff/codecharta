e56f30e0568715dac2e39840b2e92ec792a2ce7c
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-04-28 21:15:12 +0200

    refactor: migrate functionality of heightSettingsPanel
    ref #2318

--- visualization/app/codeCharta/ui/heightSettingsPanel/heightSettingsPanel.component.ts
+++ visualization/app/codeCharta/ui/heightSettingsPanel/heightSettingsPanel.component.ts
@@ -1,33 +1,11 @@
 import { IRootScopeService } from "angular"
 import { Vector3 } from "three"
 import { StoreService } from "../../state/store.service"
-import { setInvertHeight } from "../../state/store/appSettings/invertHeight/invertHeight.actions"
-import { setScaling } from "../../state/store/appSettings/scaling/scaling.actions"
-import debounce from "lodash.debounce"
-import { ScalingService, ScalingSubscriber } from "../../state/store/appSettings/scaling/scaling.service"
-import { InvertHeightService, InvertHeightSubscriber } from "../../state/store/appSettings/invertHeight/invertHeight.service"
 import { FilesService, FilesSelectionSubscriber } from "../../state/store/files/files.service"
 import { isDeltaState } from "../../model/files/files.helper"
 import { FileState } from "../../model/files/files"
-import {
-	LabelShowNodeNameService,
-	ShowMetricLabelNameValueSubscriber
-} from "../../state/store/appSettings/showMetricLabelNameValue/labelShowNodeNameService"
-import { setShowMetricLabelNameValue } from "../../state/store/appSettings/showMetricLabelNameValue/showMetricLabelNameValue.actions"
-import {
-	LabelShowMetricValueService,
-	ShowMetricLabelNodeNameSubscriber
-} from "../../state/store/appSettings/showMetricLabelNodeName/labelShowMetricValueService"
-import { setShowMetricLabelNodeName } from "../../state/store/appSettings/showMetricLabelNodeName/showMetricLabelNodeName.actions"
 
-export class HeightSettingsPanelController
-	implements
-		FilesSelectionSubscriber,
-		ScalingSubscriber,
-		InvertHeightSubscriber,
-		ShowMetricLabelNameValueSubscriber,
-		ShowMetricLabelNodeNameSubscriber
-{
+export class HeightSettingsPanelController implements FilesSelectionSubscriber {
 	private static DEBOUNCE_TIME = 400
 	private readonly applyDebouncedScaling: (newScaling: Vector3) => void
 
@@ -35,66 +13,19 @@ export class HeightSettingsPanelController
 		scalingY: number
 		invertHeight: boolean
 		isDeltaState: boolean
-		showMetricValue: boolean
-		showNodeName: boolean
 	} = {
 		scalingY: null,
 		invertHeight: null,
-		isDeltaState: null,
-		showMetricValue: true,
-		showNodeName: true
+		isDeltaState: null
 	}
 
 	constructor(private $rootScope: IRootScopeService, private storeService: StoreService) {
 		"ngInject"
-		ScalingService.subscribe(this.$rootScope, this)
-		InvertHeightService.subscribe(this.$rootScope, this)
 		FilesService.subscribe(this.$rootScope, this)
-		LabelShowNodeNameService.subscribe(this.$rootScope, this)
-		LabelShowMetricValueService.subscribe(this.$rootScope, this)
-
-		this.applyDebouncedScaling = debounce(newScaling => {
-			this.storeService.dispatch(setScaling(newScaling))
-		}, HeightSettingsPanelController.DEBOUNCE_TIME)
-	}
-
-	onInvertHeightChanged(invertHeight: boolean) {
-		this._viewModel.invertHeight = invertHeight
-	}
-
-	onShowMetricLabelNameValueChanged(showMetricValue: boolean) {
-		this._viewModel.showMetricValue = showMetricValue
-	}
-
-	onShowMetricLabelNodeNameChanged(showMetricLabelNodeName: boolean) {
-		this._viewModel.showNodeName = showMetricLabelNodeName
-	}
-
-	onScalingChanged(scaling) {
-		this._viewModel.scalingY = scaling.y
 	}
 
 	onFilesSelectionChanged(files: FileState[]) {
 		this._viewModel.isDeltaState = isDeltaState(files)
-	}
-
-	applySettingsMetricLabelValueChanged() {
-		this.storeService.dispatch(setShowMetricLabelNameValue(this._viewModel.showMetricValue))
-	}
-
-	applySettingsMetricLabelNodeNameChanged() {
-		this.storeService.dispatch(setShowMetricLabelNodeName(this._viewModel.showNodeName))
-	}
-
-	applySettingsInvertHeight() {
-		this.storeService.dispatch(setInvertHeight(this._viewModel.invertHeight))
-	}
-
-	applySettingsScaling() {
-		const oldScaling = this.storeService.getState().appSettings.scaling
-		const newScaling = new Vector3(oldScaling.x, this._viewModel.scalingY, oldScaling.z)
-
-		this.applyDebouncedScaling(newScaling)
 	}
 }
 
--- visualization/app/codeCharta/ui/ribbonBar/heightSettingsPanel/heightSettingsPanel.component.ts
+++ visualization/app/codeCharta/ui/ribbonBar/heightSettingsPanel/heightSettingsPanel.component.ts
@@ -6,6 +6,16 @@ import { amountOfTopLabelsSelector } from "../../../state/store/appSettings/amou
 import { isLabelsSliderDisabledSelector } from "./selectors/isLabelsSliderDisabled.selector"
 import { debounce } from "lodash"
 import { setAmountOfTopLabels } from "../../../state/store/appSettings/amountOfTopLabels/amountOfTopLabels.actions"
+import { showMetricLabelNodeNameSelector } from "../../../state/store/appSettings/showMetricLabelNameValue/showMetricLabelNodeName.selector"
+import { showMetricLabelNodeValueSelector } from "../../../state/store/appSettings/showMetricLabelNameValue/showMetricLabelNameValue.selector"
+import { MatCheckboxChange } from "@angular/material/checkbox"
+import { setShowMetricLabelNodeName } from "../../../state/store/appSettings/showMetricLabelNodeName/showMetricLabelNodeName.actions"
+import { setShowMetricLabelNameValue } from "../../../state/store/appSettings/showMetricLabelNameValue/showMetricLabelNameValue.actions"
+import { scalingSelector } from "../../../state/store/appSettings/scaling/scaling.selector"
+import { setScaling } from "../../../state/store/appSettings/scaling/scaling.actions"
+import { invertHeightSelector } from "../../../state/store/appSettings/invertHeight/invertHeight.selector"
+import { setInvertHeight } from "../../../state/store/appSettings/invertHeight/invertHeight.actions"
+import { isDeltaStateSelector } from "../../../state/selectors/isDeltaState.selector"
 
 @Component({
 	selector: "cc-height-settings-panel",
@@ -16,10 +26,31 @@ export class HeightSettingsPanelComponent {
 
 	amountOfTopLabels$ = this.store.select(amountOfTopLabelsSelector)
 	isLabelsSliderDisabled$ = this.store.select(isLabelsSliderDisabledSelector)
+	showMetricLabelNodeName$ = this.store.select(showMetricLabelNodeNameSelector)
+	showMetricLabelNodeValue$ = this.store.select(showMetricLabelNodeValueSelector)
+	scaling$ = this.store.select(scalingSelector)
+	invertHeight$ = this.store.select(invertHeightSelector)
+	isDeltaState$ = this.store.select(isDeltaStateSelector)
+
+	constructor(@Inject(Store) private store: Store) {}
 
 	applyDebouncedTopLabels = debounce((amountOfTopLabels: number) => {
 		this.store.dispatch(setAmountOfTopLabels(amountOfTopLabels))
 	}, HeightSettingsPanelComponent.DEBOUNCE_TIME)
 
-	constructor(@Inject(Store) private store: Store) {}
+	applyDebouncedScalingY = debounce((y: number) => {
+		this.store.dispatch(setScaling({ y }))
+	}, HeightSettingsPanelComponent.DEBOUNCE_TIME)
+
+	setShowMetricLabelNodeName(event: MatCheckboxChange) {
+		this.store.dispatch(setShowMetricLabelNodeName(event.checked))
+	}
+
+	setShowMetricLabelNameValue(event: MatCheckboxChange) {
+		this.store.dispatch(setShowMetricLabelNameValue(event.checked))
+	}
+
+	setInvertHeight(event: MatCheckboxChange) {
+		this.store.dispatch(setInvertHeight(event.checked))
+	}
 }
