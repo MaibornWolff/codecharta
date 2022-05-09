783b7cc768dfeab5dab3272af21a2a8129662654
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-05-09 22:16:40 +0200

    refactor: aling labels and hide min max labels in case of overlapping
    ref #2318

--- visualization/app/codeCharta/ui/colorSettingsPanel/rangeSlider/rangeSliderLabels/rangeSliderLabels.component.ts
+++ visualization/app/codeCharta/ui/colorSettingsPanel/rangeSlider/rangeSliderLabels/rangeSliderLabels.component.ts
@@ -1,21 +1,55 @@
 import "./rangeSliderLabels.component.scss"
-import { Component, ElementRef, Input, ViewChild } from "@angular/core"
+import { AfterViewChecked, Component, ElementRef, Input, OnChanges, ViewChild } from "@angular/core"
 import { SliderRangePosition } from "../utils/SliderRangePosition"
+import { sliderWidth } from "../rangeSlider.component"
+
+const minDistanceBetweenLabels = 4
 
 @Component({
 	selector: "cc-range-slider-labels",
 	template: require("./rangeSliderLabels.component.html")
 })
-export class RangeSliderLabelsComponent {
+export class RangeSliderLabelsComponent implements OnChanges, AfterViewChecked {
 	@Input() minValue: number
 	@Input() maxValue: number
 	@Input() currentLeftValue: number
 	@Input() currentRightValue: number
 	@Input() sliderRangePosition: SliderRangePosition
-	@Input() thumbRadius: number
 
-	@ViewChild("minValueLabel") minValueLabel: ElementRef<HTMLDivElement>
-	@ViewChild("maxValueLabel") maxValueLabel: ElementRef<HTMLDivElement>
-	@ViewChild("currentLeftValueLabel") currentLeftValueLabel: ElementRef<HTMLDivElement>
-	@ViewChild("currentRightValueLabel") currentRightValueLabel: ElementRef<HTMLDivElement>
+	@ViewChild("minLabel") minLabel: ElementRef<HTMLDivElement>
+	@ViewChild("maxLabel") maxLabel: ElementRef<HTMLDivElement>
+	@ViewChild("currentLeftLabel") currentLeftLabel: ElementRef<HTMLDivElement>
+	@ViewChild("currentRightLabel") currentRightLabel: ElementRef<HTMLDivElement>
+
+	hideMinLabel = false
+	hideMaxLabel = false
+	currentLeftLabelLeftPosition
+	currentRightLabelLeftPosition
+
+	private hasUnhandledChanges = false
+
+	ngOnChanges(): void {
+		this.hasUnhandledChanges = true
+	}
+
+	ngAfterViewChecked(): void {
+		if (this.hasUnhandledChanges) {
+			this.updateLabelDisplays()
+		}
+	}
+
+	private updateLabelDisplays() {
+		const minLabelRightPosition = this.minLabel.nativeElement.getBoundingClientRect().width
+		this.currentLeftLabelLeftPosition =
+			this.sliderRangePosition.leftEnd - this.currentLeftLabel.nativeElement.getBoundingClientRect().width / 2
+
+		const currentRightLabelWidth = this.currentRightLabel.nativeElement.getBoundingClientRect().width
+		const maxLabelLeftPosition = sliderWidth - this.maxLabel.nativeElement.getBoundingClientRect().width
+		this.currentRightLabelLeftPosition = this.sliderRangePosition.rightStart - currentRightLabelWidth / 2
+
+		this.hideMinLabel = this.currentLeftLabelLeftPosition <= minLabelRightPosition + minDistanceBetweenLabels
+		this.hideMaxLabel = this.currentRightLabelLeftPosition + currentRightLabelWidth + minDistanceBetweenLabels >= maxLabelLeftPosition
+
+		this.hasUnhandledChanges = false
+	}
 }
