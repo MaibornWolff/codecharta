9c22e76eab361995a1da3a006b041ab76e1a0fd7
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-05-09 23:51:25 +0200

    refactor: wip connecting
    ref #2318

--- visualization/app/codeCharta/ui/colorSettingsPanel/metricColorRangeSlider/metricColorRangeSlider.component.ts
+++ visualization/app/codeCharta/ui/colorSettingsPanel/metricColorRangeSlider/metricColorRangeSlider.component.ts
@@ -1,18 +1,28 @@
 import { Component, Inject } from "@angular/core"
 import { Store } from "../../../state/angular-redux/store"
 import { HandleValueChange } from "./rangeSlider/rangeSlider.component"
+import { colorRangeSelector } from "../../../state/store/dynamicSettings/colorRange/colorRange.selector"
+import { setColorRange } from "../../../state/store/dynamicSettings/colorRange/colorRange.actions"
 
 @Component({
 	selector: "cc-metric-color-range-slider",
 	template: require("./metricColorRangeSlider.component.html")
 })
 export class MetricColorRangeSliderComponent {
+	colorRange$ = this.store.select(colorRangeSelector)
+
 	constructor(@Inject(Store) private store: Store) {}
 
-	currentLeftValue = 62
-	currentRightValue = 80
 	handleValueChange: HandleValueChange = ({ currentLeftValue, currentRightValue }) => {
-		this.currentLeftValue = currentLeftValue
-		this.currentRightValue = currentRightValue
+		// todo check initial reducing from
+		// todo add debounce
+		this.store.dispatch(
+			setColorRange({
+				from: currentLeftValue,
+				to: currentRightValue,
+				min: 0,
+				max: 100 // todo make action partial
+			})
+		)
 	}
 }
--- visualization/app/codeCharta/ui/colorSettingsPanel/metricColorRangeSlider/metricColorRangeSlider.module.ts
+++ visualization/app/codeCharta/ui/colorSettingsPanel/metricColorRangeSlider/metricColorRangeSlider.module.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
 import { MetricColorRangeSliderComponent } from "./metricColorRangeSlider.component"
 import { RangeSliderModule } from "./rangeSlider/rangeSlider.module"
 
 @NgModule({
-	imports: [RangeSliderModule],
+	imports: [CommonModule, RangeSliderModule],
 	declarations: [MetricColorRangeSliderComponent],
 	exports: [MetricColorRangeSliderComponent],
 	entryComponents: [MetricColorRangeSliderComponent]
