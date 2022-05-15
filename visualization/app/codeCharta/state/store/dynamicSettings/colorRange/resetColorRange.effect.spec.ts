6dcd41066c1f68475f75dce2b48e26c2c2703d76
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-05-15 12:26:19 +0200

    refactor: align naming
    ref #2318

--- visualization/app/codeCharta/state/store/dynamicSettings/colorRange/resetColorRange.effect.spec.ts
+++ visualization/app/codeCharta/state/store/dynamicSettings/colorRange/resetColorRange.effect.spec.ts
@@ -1,6 +1,6 @@
 import { _nodeMetricRangeToInitialColorRange } from "./resetColorRange.effect"
 
-describe("ColorRangeEffect", () => {
+describe("ResetColorRangeColorRangeEffect", () => {
 	it("should split positive, neutral, negative to a third each", () => {
 		expect(_nodeMetricRangeToInitialColorRange({ minValue: 20, maxValue: 120 })).toEqual({ from: 53, to: 86 })
 	})
