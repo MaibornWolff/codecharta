590b6137ecf590f10ec50d0f0ce4f6a55ac86098
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-05-18 15:57:05 +0200

    switch to parameterized test #2341

--- analysis/import/GitLogParser/src/test/kotlin/de/maibornwolff/codecharta/importer/gitlogparser/ParserDialogTest.kt
+++ analysis/import/GitLogParser/src/test/kotlin/de/maibornwolff/codecharta/importer/gitlogparser/ParserDialogTest.kt
@@ -8,8 +8,10 @@ import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ParserDialogTest {
@@ -19,15 +21,16 @@ class ParserDialogTest {
         unmockkAll()
     }
 
-    @Test
-    fun `should output correct arguments for file without extension`() {
-        val fileName = "git"
-        val outputFileName = "codecharta.cc.json"
-        val fileNameList = "file-name-list.txt"
-        val isCompressed = false
-        val isSilent = false
-        val addAuthor = false
-
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    fun `should output correct arguments`(
+        fileName: String,
+        outputFileName: String,
+        fileNameList: String,
+        isCompressed: Boolean,
+        isSilent: Boolean,
+        addAuthor: Boolean
+    ) {
         mockkStatic("com.github.kinquirer.components.InputKt")
         every {
             KInquirer.promptInput(any(), any(), any())
@@ -51,35 +54,16 @@ class ParserDialogTest {
         )
     }
 
-    @Test
-    fun `should output correct arguments for file with correct extension`() {
-        val fileName = "git.log"
-        val outputFileName = "codecharta.cc.json"
-        val fileNameList = "file-name-list.txt"
-        val isCompressed = false
-        val isSilent = false
-        val addAuthor = false
-
-        mockkStatic("com.github.kinquirer.components.InputKt")
-        every {
-            KInquirer.promptInput(any(), any(), any())
-        } returns fileName andThen outputFileName andThen fileNameList
-        mockkStatic("com.github.kinquirer.components.ConfirmKt")
-        every {
-            KInquirer.promptConfirm(any(), any())
-        } returns isCompressed andThen isSilent andThen addAuthor
-
-        val parserArguments = ParserDialog.collectParserArgs()
-
-        Assertions.assertThat(parserArguments).isEqualTo(
-            listOf(
-                fileName,
-                "-o $outputFileName",
-                "--file-name-list=$fileNameList",
-                "--not-compressed=$isCompressed",
-                "--silent=$isSilent",
-                "--add-author=$addAuthor"
+    companion object {
+        @JvmStatic
+        fun provideArguments(): List<Arguments> {
+            return listOf(
+                Arguments.of(
+                    "git", "codecharta.cc.json", "file-name-list.txt", false, false, false
+                ), Arguments.of(
+                    "git.log", "codecharta.cc.json", "file-name-list.txt", false, false, false
+                )
             )
-        )
+        }
     }
 }
