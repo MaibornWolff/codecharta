72dd66c78031336b2673427c9ce560dfe968cae2
Torsten Knauf <Torsten.Knauf@maibornwolff.de>
2022-05-20 12:31:50 +0200

    refactor: add reset button
    ref #2318

--- visualization/app/codeCharta/ui/toolBar/globalConfigurationButton/globalConfigurationButton.module.ts
+++ visualization/app/codeCharta/ui/toolBar/globalConfigurationButton/globalConfigurationButton.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core"
 import { MaterialModule } from "../../../../material/material.module"
+import { ResetSettingsButtonModule } from "../../resetSettingsButton/resetSettingsButton.module"
 import { GlobalConfigurationButtonComponent } from "./globalConfigurationButton.component"
 import { GlobalConfigurationDialogComponent } from "./globalConfigurationDialog/globalConfigurationDialog.component"
 
 @NgModule({
-	imports: [MaterialModule],
+	imports: [MaterialModule, ResetSettingsButtonModule],
 	declarations: [GlobalConfigurationButtonComponent, GlobalConfigurationDialogComponent],
 	exports: [GlobalConfigurationButtonComponent],
 	entryComponents: [GlobalConfigurationButtonComponent, GlobalConfigurationDialogComponent]
