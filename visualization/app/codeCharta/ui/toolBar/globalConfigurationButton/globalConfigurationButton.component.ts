d365e4c692d45ec7f77bdaa646ee0b6383a9bcc1
Torsten Knauf <Torsten.Knauf@maibornwolff.de>
2022-05-20 10:55:06 +0200

    refactor: style button
    ref #2318

--- visualization/app/codeCharta/ui/toolBar/globalConfigurationButton/globalConfigurationButton.component.ts
+++ visualization/app/codeCharta/ui/toolBar/globalConfigurationButton/globalConfigurationButton.component.ts
@@ -1,7 +1,12 @@
+import "./globalConfigurationButton.component.scss"
 import { Component } from "@angular/core"
 
 @Component({
 	selector: "cc-global-configuration-button",
 	template: require("./globalConfigurationButton.component.html")
 })
-export class GlobalConfigurationButtonComponent {}
+export class GlobalConfigurationButtonComponent {
+	showGlobalConfiguration() {
+		// open dialog
+	}
+}
