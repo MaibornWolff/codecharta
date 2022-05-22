29dbf7cab5de801bfc4188f4b2ceb0e5c6c8f001
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-05-22 13:55:44 +0200

    refactor: add display quality selection
    ref #2318

--- visualization/app/codeCharta/ui/toolBar/globalConfigurationButton/globalConfigurationButton.module.ts
+++ visualization/app/codeCharta/ui/toolBar/globalConfigurationButton/globalConfigurationButton.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core"
 import { MaterialModule } from "../../../../material/material.module"
 import { ResetSettingsButtonModule } from "../../resetSettingsButton/resetSettingsButton.module"
 import { GlobalConfigurationButtonComponent } from "./globalConfigurationButton.component"
+import { DisplayQualitySelectionModule } from "./globalConfigurationDialog/displayQualitySelection/displayQualitySelection.module"
 import { GlobalConfigurationDialogComponent } from "./globalConfigurationDialog/globalConfigurationDialog.component"
 import { MapLayoutSelectionModule } from "./globalConfigurationDialog/mapLayoutSelection/mapLayoutSelection.module"
 
 @NgModule({
-	imports: [MaterialModule, ResetSettingsButtonModule, MapLayoutSelectionModule],
+	imports: [MaterialModule, ResetSettingsButtonModule, MapLayoutSelectionModule, DisplayQualitySelectionModule],
 	declarations: [GlobalConfigurationButtonComponent, GlobalConfigurationDialogComponent],
 	exports: [GlobalConfigurationButtonComponent],
 	entryComponents: [GlobalConfigurationButtonComponent, GlobalConfigurationDialogComponent]
