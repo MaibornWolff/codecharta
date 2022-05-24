bcb64f38c65f2650cf90052cd9fb2ddad72e3ef3
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-05-24 14:25:31 +0200

    add tests WIP #2318

--- visualization/app/codeCharta/ui/ribbonBar/areaSettingsPanel/areaSettingsPanel.component.spec.ts
+++ visualization/app/codeCharta/ui/ribbonBar/areaSettingsPanel/areaSettingsPanel.component.spec.ts
@@ -1,105 +1,26 @@
-import "./areaSettingsPanel.module"
-import "../codeMap/codeMap.module"
-import "../../codeCharta.module"
-import { AreaSettingsPanelController } from "./areaSettingsPanel.component"
-import { IRootScopeService } from "angular"
-import { getService, instantiateModule } from "../../../../mocks/ng.mockhelper"
-import { StoreService } from "../../state/store.service"
-import { DynamicMarginService } from "../../state/store/appSettings/dynamicMargin/dynamicMargin.service"
-import { MarginService } from "../../state/store/dynamicSettings/margin/margin.service"
-import { FilesService } from "../../state/store/files/files.service"
+import { AreaSettingsPanelComponent } from "./areaSettingsPanel.component"
+import { render, screen } from "@testing-library/angular"
+import { AreaSettingsPanelModule } from "./areaSettingsPanel.module"
+import { Store } from "../../../state/store/store"
+import { TestBed } from "@angular/core/testing"
+import userEvent from "@testing-library/user-event"
+import { wait } from "../../../util/testUtils/wait"
 
-describe("AreaSettingsPanelController", () => {
-	let areaSettingsPanelController: AreaSettingsPanelController
-	let $rootScope: IRootScopeService
-	let storeService: StoreService
-
-	const SOME_EXTRA_TIME = 400
-
+describe("AreaSettingsPanelComponent", () => {
 	beforeEach(() => {
-		restartSystem()
-		rebuildController()
-	})
-
-	function restartSystem() {
-		instantiateModule("app.codeCharta.ui.areaSettingsPanel")
-
-		$rootScope = getService<IRootScopeService>("$rootScope")
-		storeService = getService<StoreService>("storeService")
-	}
-
-	function rebuildController() {
-		areaSettingsPanelController = new AreaSettingsPanelController($rootScope, storeService)
-	}
-
-	describe("constructor", () => {
-		it("should subscribe to DynamicMarginService", () => {
-			DynamicMarginService.subscribe = jest.fn()
-
-			rebuildController()
-
-			expect(DynamicMarginService.subscribe).toHaveBeenCalledWith($rootScope, areaSettingsPanelController)
-		})
-
-		it("should subscribe to MarginService", () => {
-			MarginService.subscribe = jest.fn()
-
-			rebuildController()
-
-			expect(MarginService.subscribe).toHaveBeenCalledWith($rootScope, areaSettingsPanelController)
-		})
-
-		it("should subscribe to FilesService", () => {
-			FilesService.subscribe = jest.fn()
-
-			rebuildController()
-
-			expect(FilesService.subscribe).toHaveBeenCalledWith($rootScope, areaSettingsPanelController)
+		Store["initialize"]()
+		TestBed.configureTestingModule({
+			imports: [AreaSettingsPanelModule]
 		})
 	})
 
-	describe("onDynamicMarginChanged", () => {
-		it("should set the dynamicMargin in viewModel", () => {
-			areaSettingsPanelController.onDynamicMarginChanged(true)
+	it("should handle default margin", async function () {
+		const { container } = await render(AreaSettingsPanelComponent, { excludeComponentDeclaration: true })
+		expect(screen.queryByRole("checkbox", { checked: true, name: "Default margin (50px)" })).not.toBe(null)
 
-			expect(areaSettingsPanelController["_viewModel"].dynamicMargin).toBeTruthy()
-		})
-	})
-
-	describe("onMarginChanged", () => {
-		it("should set margin in viewModel", () => {
-			areaSettingsPanelController.onMarginChanged(28)
-
-			expect(areaSettingsPanelController["_viewModel"].margin).toBe(28)
-		})
-	})
-
-	describe("onFilesSelectionChanged", () => {
-		it("should set dynamicMargin to true", () => {
-			areaSettingsPanelController.onFilesSelectionChanged()
-
-			expect(storeService.getState().appSettings.dynamicMargin).toBeTruthy()
-		})
-
-		it("should update margin and dynamicMargin in store", () => {
-			areaSettingsPanelController.onFilesSelectionChanged()
-
-			expect(storeService.getState().appSettings.dynamicMargin).toBeTruthy()
-		})
-	})
-
-	describe("onChangeMarginSlider", () => {
-		it("should update margin and dynamicMargin in store", done => {
-			areaSettingsPanelController["_viewModel"].dynamicMargin = false
-			areaSettingsPanelController["_viewModel"].margin = 28
-
-			areaSettingsPanelController.applyDebouncedMargin(42)
-
-			setTimeout(() => {
-				expect(storeService.getState().dynamicSettings.margin).toEqual(42)
-				expect(storeService.getState().appSettings.dynamicMargin).toBeFalsy()
-				done()
-			}, AreaSettingsPanelController["DEBOUNCE_TIME"] + SOME_EXTRA_TIME)
-		})
+		const marginInput = container.querySelector("input")
+		userEvent.type(marginInput, "1")
+		await wait(AreaSettingsPanelComponent.DEBOUNCE_TIME)
+		expect(screen.queryByRole("checkbox", { checked: false, name: "Default margin (50px)" })).not.toBe(null)
 	})
 })
