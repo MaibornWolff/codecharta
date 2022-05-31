10772fe395cda35d771190c0163685152e78a2a5
Mira Weller <mira.weller@maibornwolff.de>
2022-05-31 15:22:58 +0200

    fix: use print for user interaction

--- analysis/import/SVNLogParser/src/main/kotlin/de/maibornwolff/codecharta/importer/svnlogparser/ParserDialog.kt
+++ analysis/import/SVNLogParser/src/main/kotlin/de/maibornwolff/codecharta/importer/svnlogparser/ParserDialog.kt
@@ -12,17 +12,15 @@ class ParserDialog {
     companion object : ParserDialogInterface {
 
         private const val EXTENSION = "log"
-        private val extensionPattern = Regex(".($EXTENSION)$")
 
         override fun collectParserArgs(): List<String> {
-            logger.info { "You can generate this file with: svn log --verbose > svn.log" }
+            print("You can generate this file with: svn log --verbose > svn.log")
             val defaultInputFileName = "svn.$EXTENSION"
-            var inputFileName = KInquirer.promptInput(
+            val inputFileName = KInquirer.promptInput(
                 message = "What is the $EXTENSION file that has to be parsed?",
                 hint = defaultInputFileName,
                 default = defaultInputFileName
             )
-            logger.info { "File path: $inputFileName" }
 
             val defaultOutputFileName = getOutputFileName(inputFileName)
             val outputFileName: String = KInquirer.promptInput(
