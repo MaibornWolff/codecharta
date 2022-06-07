e036a81634613ec8722f3b26fe7f7425b5a385e4
phanlezz <se.wo96@gmx.de>
2022-06-07 11:43:51 +0200

    Refactor tests from spek to internal

--- analysis/model/src/test/kotlin/de/maibornwolff/codecharta/serialization/CompressedStreamHandlerTest.kt
+++ analysis/model/src/test/kotlin/de/maibornwolff/codecharta/serialization/CompressedStreamHandlerTest.kt
@@ -1,47 +1,47 @@
 package de.maibornwolff.codecharta.serialization
 
-import org.hamcrest.MatcherAssert
-import org.hamcrest.Matchers
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
+import org.junit.jupiter.api.Test
+import kotlin.test.assertSame
 
-class CompressedStreamHandlerTest : Spek({
-    val EXAMPLE_COMPRESSED = "example.txt.gz"
-    val EXAMPLE_UNCOMPRESSED = "example.txt"
+class CompressedStreamHandlerTest {
+    val EXAMPLE_COMPRESSED = "exampleCompressed.txt.gz"
+    val EXAMPLE_UNCOMPRESSED = "exampleUncompressed.txt"
     val EXAMPLE_EMPTY = "exampleEmpty"
     val EXAMPLE_ONE_BYTE = "exampleOneByte"
 
-    describe("A GZ-Compression InputStream Checker") {
-        it("should be able to read text from compressed input stream") {
-            var input = this.javaClass.classLoader.getResourceAsStream(EXAMPLE_COMPRESSED)
-            input = CompressedStreamHandler.handleInput(input)
-            val content = input.reader().readText()
-            MatcherAssert.assertThat(content, Matchers.`is`("hello world\n"))
-            input.close()
-        }
-
-        it("should be able to read text from uncompressed input stream") {
-            var input = this.javaClass.classLoader.getResourceAsStream(EXAMPLE_UNCOMPRESSED)
-            input = CompressedStreamHandler.handleInput(input)
-            val content = input.reader().readText()
-            MatcherAssert.assertThat(content, Matchers.`is`("hello world\n"))
-            input.close()
-        }
-
-        it("should be able to handle empty files") {
-            var input = this.javaClass.classLoader.getResourceAsStream(EXAMPLE_EMPTY)
-            input = CompressedStreamHandler.handleInput(input)
-            val content = input.reader().readText()
-            MatcherAssert.assertThat(content, Matchers.`is`(""))
-            input.close()
-        }
-
-        it("should be able to handle one byte files") {
-            var input = this.javaClass.classLoader.getResourceAsStream(EXAMPLE_ONE_BYTE)
-            input = CompressedStreamHandler.handleInput(input)
-            val content = input.read()
-            MatcherAssert.assertThat(content, Matchers.`is`(0))
-            input.close()
-        }
+    @Test
+    fun `should be able to read text from compressed input stream`() {
+        var input = this.javaClass.classLoader.getResourceAsStream(EXAMPLE_COMPRESSED)
+        input = CompressedStreamHandler.handleInput(input)
+        val content = input.reader().readText()
+        assertSame("hello world\n", content)
+        input.close()
     }
-})
+
+    @Test
+    fun `should be able to read text from uncompressed input stream`() {
+        var input = this.javaClass.classLoader.getResourceAsStream(EXAMPLE_UNCOMPRESSED)
+        input = CompressedStreamHandler.handleInput(input)
+        val content = input.reader().readText()
+        assertSame("hello world\n", content)
+        input.close()
+    }
+
+    @Test
+    fun `should be able to handle empty files`() {
+        var input = this.javaClass.classLoader.getResourceAsStream(EXAMPLE_EMPTY)
+        input = CompressedStreamHandler.handleInput(input)
+        val content = input.reader().readText()
+        assertSame("", content)
+        input.close()
+    }
+
+    @Test
+    fun `should be able to handle one byte files`() {
+        var input = this.javaClass.classLoader.getResourceAsStream(EXAMPLE_ONE_BYTE)
+        input = CompressedStreamHandler.handleInput(input)
+        val content = input.read()
+        assertSame(0, content)
+        input.close()
+    }
+}
