14982d4a803b2f44a5d616b1cb08e2e309e4e418
Chrisp485 <cspahn@gmx.net>
2022-06-30 14:48:22 +0200

    add interactive dialog, adjust gradle #2619

--- analysis/import/MetricGardenerImporter/src/main/Kotlin/de/maibornwolff/codecharta/importer/metricgardenerimporter/MetricGardenerImporter.kt
+++ analysis/import/MetricGardenerImporter/src/main/Kotlin/de/maibornwolff/codecharta/importer/metricgardenerimporter/MetricGardenerImporter.kt
@@ -1,5 +1,7 @@
 package de.maibornwolff.codecharta.importer.metricgardenerimporter
 
+import de.maibornwolff.codecharta.tools.interactiveparser.InteractiveParser
+import de.maibornwolff.codecharta.tools.interactiveparser.ParserDialogInterface
 import picocli.CommandLine
 import java.io.File
 import java.util.concurrent.Callable
@@ -10,10 +12,10 @@ import java.util.concurrent.Callable
         footer = ["Copyright(c) 2022, MaibornWolff GmbH"]
 )
 
-class MetricGardenerImporter: Callable<Void> {
+class MetricGardenerImporter : Callable<Void>, InteractiveParser {
 
     @CommandLine.Option(names = ["-h", "--help"], usageHelp = true, description = [
-      "Specify: path/to/input/folder/or/file -o path/to/outputfile.json"])
+        "Specify : path/to/input/folder/or/file -o path/to/outputfile.json"])
     private var help = false
 
     @CommandLine.Option(names = ["-nc", "--not-compressed"], description = ["save uncompressed output File"])
@@ -25,9 +27,8 @@ class MetricGardenerImporter: Callable<Void> {
     @CommandLine.Option(names = ["-o", "--output-file"], description = ["path for the outputfile"])
     private var outputFile: File? = null
 
-
-
     override fun call(): Void {
         TODO("Not yet implemented")
     }
+    override fun getDialog(): ParserDialogInterface = ParserDialog
 }
