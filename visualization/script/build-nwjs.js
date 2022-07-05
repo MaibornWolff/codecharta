a6c6bd4c5fc4679626c4290e530cd5281db635c7
Mira Weller <mira.weller@maibornwolff.de>
2022-07-05 12:22:30 +0200

    fix: switch to nw-builder
    issue #2823

---
+++ visualization/script/build-nwjs.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const NwBuilder = require('nw-builder');
+const nw = new NwBuilder({
+  macIcns: './app/assets/icon.icns',
+  winIco:'./app/assets/icon.ico',
+  buildDir: "./dist/packages/",
+  files: [
+    "./dist/webpack/**/*",
+    "package.json"
+  ],
+  version: "latest",
+  platforms: ["osx64", "win32", "win64", "linux32", "linux64"],
+  appName: null,
+  appVersion: null,
+  cacheDir: "build",
+  buildType: "versioned",
+  zip: true,
+  useRcedit: true,
+  winVersionString: {
+    'CompanyName': 'MaibornWolff GmbH',
+    'FileDescription': 'CodeCharta Visualization',
+    'ProductName': 'codecharta-visualization',
+    'LegalCopyright': 'Copyright MaibornWolff GmbH',
+  }
+});
+nw.build().then(function () {
+  console.log('done');
+}).catch(function (error) {
+  console.error('nw-builder error: '+error);
+})
+
