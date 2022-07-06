72b602b71351a9d65b238babd0dc602aa614b651
phanlezz <se.wo96@gmx.de>
2022-07-06 23:45:15 +0200

    Remove zip option, add clean up of build and package dirs, change to bestzip for zip process

--- visualization/script/build-nwjs.js
+++ visualization/script/build-nwjs.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const zip = require('bestzip');
+const path = require('node:path');
+
+fs.rmSync('./build', { recursive: true, force: true });
+fs.rmSync('dist/packages', { recursive: true, force: true });
+
 const NwBuilder = require('nw-builder');
 const nw = new NwBuilder({
-  buildDir: "./dist/packages/",
+  buildDir: "dist/packages",
   cacheDir: "build",
   forceDownload: true,
   files: [
@@ -14,9 +21,9 @@ const nw = new NwBuilder({
   platforms: ["osx64", "win32", "win64", "linux32", "linux64"],
   appName: null,
   appVersion: null,
-  buildType: "versioned",
-  zip: true,
+  buildType: function () { return this.appName + "-" + this.appVersion },
   useRcedit: true,
+  zip: false,
   macIcns: './app/assets/icon.icns',
   winIco:'./app/assets/icon.ico',
   winVersionString: {
@@ -27,8 +34,35 @@ const nw = new NwBuilder({
   }
 });
 nw.build().then(function () {
-  console.log('done');
+  console.log("nw-build done!");
+  fs.mkdirSync(nw.options.buildDir, { recursive: true });
+  const baseZipPath = path.resolve(nw.options.buildDir, nw.options.appName + "-" + nw.options.appVersion + "-");
+  nw._forEachPlatform(function (name, platform) {
+    zip({
+      source:name,
+      destination: baseZipPath + name + ".zip",
+      cwd: path.join(platform.releasePath, "..")
+    }).then(function () {
+      console.log(name + " zipped!")
+    }).catch(function (err) {
+      console.error(err.stack)
+      process.exit(1)
+    });
+  });
+
+  zip({
+    source: path.join("..", "webpack"),
+    destination: baseZipPath + "web.zip",
+    cwd: nw.options.buildDir
+  }).then(function () {
+    console.log("web zipped!")
+  }).catch(function (err) {
+    console.error(err.stack)
+    process.exit(1)
+  });
+  // console.log('done');
 }).catch(function (error) {
   console.error('nw-builder error: '+error);
-})
+});
+
 
