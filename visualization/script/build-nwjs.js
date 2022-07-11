03c5d37c86059c6694e6818595d7ed19c797ef66
MW-Friedrich <108457762+MW-Friedrich@users.noreply.github.com>
2022-07-11 14:58:08 +0200

    refactor build-nwjs.js #2881

--- visualization/script/build-nwjs.js
+++ visualization/script/build-nwjs.js
@@ -4,10 +4,13 @@ const fs = require('fs');
 const zip = require('bestzip');
 const path = require('node:path');
 
-fs.rmSync('./build', { recursive: true, force: true });
-fs.rmSync('dist/packages', { recursive: true, force: true });
+(async () => {
+  fs.rmSync('./build', {recursive: true, force: true});
+  fs.rmSync('dist/packages', {recursive: true, force: true});
+})();
 
 const NwBuilder = require('nw-builder');
+const {log} = require("util");
 const nw = new NwBuilder({
   buildDir: "dist/packages",
   cacheDir: "build",
@@ -21,11 +24,13 @@ const nw = new NwBuilder({
   platforms: ["osx64", "win32", "win64", "linux32", "linux64"],
   appName: null,
   appVersion: null,
-  buildType: function () { return this.appName + "-" + this.appVersion },
+  buildType: function () {
+    return this.appName + "-" + this.appVersion
+  },
   useRcedit: true,
   zip: false,
   macIcns: './app/assets/icon.icns',
-  winIco:'./app/assets/icon.ico',
+  winIco: './app/assets/icon.ico',
   winVersionString: {
     'CompanyName': 'MaibornWolff GmbH',
     'FileDescription': 'CodeCharta Visualization',
@@ -33,36 +38,26 @@ const nw = new NwBuilder({
     'LegalCopyright': 'Copyright MaibornWolff GmbH'
   }
 });
-nw.build().then(function () {
+
+(async () => {
+  await nw.build();
   console.log("nw-build done!");
-  fs.mkdirSync(nw.options.buildDir, { recursive: true });
+  fs.mkdirSync(nw.options.buildDir, {recursive: true});
   const baseZipPath = path.resolve(nw.options.buildDir, nw.options.appName + "-" + nw.options.appVersion + "-");
-  nw._forEachPlatform(function (name, platform) {
+  await Promise.all([
+    nw._forEachPlatform((name, platform) => {
+      zip({
+        source: name,
+        destination: baseZipPath + name + ".zip",
+        cwd: path.join(platform.releasePath, "..")
+      })
+      console.log('%s zipped', name)
+    }),
     zip({
-      source:name,
-      destination: baseZipPath + name + ".zip",
-      cwd: path.join(platform.releasePath, "..")
-    }).then(function () {
-      console.log(name + " zipped!")
-    }).catch(function (err) {
-      console.error(err.stack)
-      process.exit(1)
-    });
-  });
-
-  zip({
-    source: path.join("..", "webpack"),
-    destination: baseZipPath + "web.zip",
-    cwd: nw.options.buildDir
-  }).then(function () {
-    console.log("web zipped!")
-  }).catch(function (err) {
-    console.error(err.stack)
-    process.exit(1)
-  });
-  // console.log('done');
-}).catch(function (error) {
-  console.error('nw-builder error: '+error);
-});
-
-
+      source: path.join("..", "webpack"),
+      destination: baseZipPath + "web.zip",
+      cwd: nw.options.buildDir
+    })
+  ]);
+  console.log('web zipped')
+})();
