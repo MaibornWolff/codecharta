40b33cb56f85a8a5cb70fadca535d5bc521eb8b0
knoffi <matthiastutor@outlook.de>
2022-07-21 17:48:58 +0200

    Fix fileCount selector spec #2708

--- visualization/app/codeCharta/ui/attributeSideBar/attributeSideBarHeaderSection/nodePath/fileCount.selector.spec.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeSideBarHeaderSection/nodePath/fileCount.selector.spec.ts
@@ -8,11 +8,11 @@ describe("getFileCount", () => {
 
 	it("should return file count object with default values when it receives a building with no attributes and changed files", () => {
 		const node = {}
-		expect(getFileCount(node)).toEqual({ all: 0, added: 0, removed: 0 })
+		expect(getFileCount(node)).toEqual({ all: 0, added: 0, removed: 0, metricsChanged: 0 })
 	})
 
 	it("should return file count object that has the same values as the value assignments of the received attributes and changed files", () => {
-		const node = { attributes: { unary: 4 }, fileCount: { added: 2, removed: 1 } }
-		expect(getFileCount(node)).toEqual({ all: 4, added: 2, removed: 1 })
+		const node = { attributes: { unary: 4 }, fileCount: { added: 2, removed: 1, metricsChanged: 3 } }
+		expect(getFileCount(node)).toEqual({ all: 4, added: 2, removed: 1, metricsChanged: 3 })
 	})
 })
