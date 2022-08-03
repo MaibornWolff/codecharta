786b1a22fc1155dc5fd1c89cfb3e7b8d23a767c1
Alex Ander <alexander.roidl@maibornwolff.de>
2022-08-03 15:36:54 +0200

    Created presentation mode button component Removed "Presentation mode" from toolbar component

---
+++ visualization/app/codeCharta/ui/toolBar/presentationModeButton/presentationModeButton.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core"
+
+@Component({
+	selector: "cc-presentation-mode-button",
+	template: require("./presentationModeButton.component.html")
+})
+export class PresentationModeButtonComponent {
+	constructor() {
+		// DO NOTHING
+	}
+}
