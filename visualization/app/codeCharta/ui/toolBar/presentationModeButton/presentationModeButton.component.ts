4b6deaa10c467237e10a8ba2ca15086b45cdfaf5
Alex Ander <alexander.roidl@maibornwolff.de>
2022-08-03 16:42:08 +0200

    Migrated presentation mode button for Angular Implemented "isPresentationMode" selector

--- visualization/app/codeCharta/ui/toolBar/presentationModeButton/presentationModeButton.component.ts
+++ visualization/app/codeCharta/ui/toolBar/presentationModeButton/presentationModeButton.component.ts
@@ -1,11 +1,23 @@
-import { Component } from "@angular/core"
+import { Component, Inject } from "@angular/core"
+import { MatSlideToggleChange } from "@angular/material/slide-toggle";
+import { Store } from "../../../state/angular-redux/store";
+import { setPresentationMode } from "../../../state/store/appSettings/isPresentationMode/isPresentationMode.actions";
+import { isPresentationModeSelector } from "../../../state/store/appSettings/isPresentationMode/isPresentationMode.selector";
+import "./presentationModeButton.component.scss"
 
 @Component({
 	selector: "cc-presentation-mode-button",
 	template: require("./presentationModeButton.component.html")
 })
 export class PresentationModeButtonComponent {
-	constructor() {
+  isPresentationModeEnabled$ = this.store.select(isPresentationModeSelector)
+
+	constructor(@Inject(Store) private readonly store: Store) {
 		// DO NOTHING
 	}
+
+  setPresentationModeEnabled(event: MatSlideToggleChange) {
+    debugger 
+    this.store.dispatch(setPresentationMode(event.checked))
+  }
 }
