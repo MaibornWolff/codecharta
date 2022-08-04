059d27583a6304e7b62094c95da343e35e0911f1
knoffi <matthiastutor@outlook.de>
2022-08-04 17:53:13 +0200

    Save wip for file name extraction #2744

--- visualization/app/codeCharta/ui/copyToClipboardButton/copyToClipboardButton.component.ts
+++ visualization/app/codeCharta/ui/copyToClipboardButton/copyToClipboardButton.component.ts
@@ -1,19 +1,19 @@
 import "./copyToClipboardButton.component.scss"
 import { Component, Inject } from "@angular/core"
 import { Store } from "../../state/angular-redux/store"
-import { FileState } from "../../model/files/files"
-import { visibleFileStatesSelector } from "../../state/selectors/visibleFileStates.selector"
 import { getFilenamesWithHighestMetrics } from "./getFilenamesWithHighestMetrics"
+import { accumulatedDataSelector } from "../../state/selectors/accumulatedData/accumulatedData.selector"
+import { CodeMapNode } from "../../codeCharta.model"
 
 @Component({
 	selector: "cc-copy-to-clipboard-button",
 	template: require("./copyToClipboardButton.component.html")
 })
 export class CopyToClipboardButtonComponent {
-	private files: FileState[]
+	private files: CodeMapNode
 	constructor(@Inject(Store) store: Store) {
-		store.select(visibleFileStatesSelector).subscribe(files => {
-			this.files = files
+		store.select(accumulatedDataSelector).subscribe(files => {
+			this.files = files.unifiedMapNode
 		})
 	}
 
--- visualization/app/codeCharta/ui/copyToClipboardButton/getFilenamesWithHighestMetrics.ts
+++ visualization/app/codeCharta/ui/copyToClipboardButton/getFilenamesWithHighestMetrics.ts
@@ -1,5 +1,45 @@
-import { FileState } from "../../model/files/files"
+import { hierarchy } from "d3-hierarchy"
+import { CodeMapNode, NodeType } from "../../codeCharta.model"
 
-export function getFilenamesWithHighestMetrics(fileStates: FileState[]) {
-	return fileStates
+export function getFilenamesWithHighestMetrics(fileStates: CodeMapNode) {
+	return fileStates[0].file.map
+}
+
+export function parse(map: CodeMapNode): FileNamesByMetrics {
+	const fileNamesByMetrics = new Map<string, FileToValue[]>()
+	for (const { data } of hierarchy(map)) {
+		if (data.type === NodeType.FILE && data.attributes) {
+			for (const [key, value] of Object.entries(data.attributes)) {
+				maybeAddToMap(key, value, data.name, fileNamesByMetrics)
+			}
+		}
+	}
+	return new Map<string, string[]>()
+}
+
+type FileNamesByMetrics = Map<string, string[]>
+type FileToValue = { name: string; value: number }
+//sry Ruben
+function maybeAddToMap(key: string, value: number, fileName: string, map: Map<string, FileToValue[]>) {
+	const keyIsNew = !map.has(key)
+	if (keyIsNew) {
+		map.set(key, [{ name: fileName, value }])
+	} else {
+		const previousValues = map.get(key)
+		if (previousValues.length < 10) {
+			map.set(key, [...previousValues, { name: fileName, value }])
+		} else {
+			const minValue = Math.min(...previousValues.map(pair => pair.value), value)
+			const shouldAddToMap = minValue < value
+			if (shouldAddToMap) {
+				const minValuePair = previousValues.find(pair => (pair.value = minValue))
+				// checking name and value, because different files could have the same name
+				const filteredPreviousValues = previousValues.filter(
+					pair => pair.name !== minValuePair.name || pair.value !== minValuePair.value
+				)
+				const newPairs = [...filteredPreviousValues, { name: fileName, value }]
+				map.set(key, newPairs)
+			}
+		}
+	}
 }
