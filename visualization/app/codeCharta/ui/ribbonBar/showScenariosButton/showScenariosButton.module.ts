2bdd26923a4bd0a815f36b504342bf0739841e87
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-08-07 12:18:47 +0200

    refactor: replace component through styles
    as the component did only add styles and a button without click handler. Second is bad practise
    
    ref #2318

--- visualization/app/codeCharta/ui/ribbonBar/showScenariosButton/showScenariosButton.module.ts
+++ visualization/app/codeCharta/ui/ribbonBar/showScenariosButton/showScenariosButton.module.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
 import { MaterialModule } from "../../../../material/material.module"
-import { RibbonBarButtonModule } from "../ribbonBarButton/ribbonBarButton.module"
 import { AddCustomScenarioModule } from "./addCustomScenario/addCustomScenario.module"
 import { ScenarioService } from "./scenario.service"
 import { ScenariosComponent } from "./scenarios/scenarios.component"
 import { ShowScenariosButtonComponent } from "./showScenariosButton.component"
 
 @NgModule({
-	imports: [CommonModule, MaterialModule, RibbonBarButtonModule, AddCustomScenarioModule],
+	imports: [CommonModule, MaterialModule, AddCustomScenarioModule],
 	providers: [ScenarioService],
 	declarations: [ShowScenariosButtonComponent, ScenariosComponent],
 	exports: [ShowScenariosButtonComponent]
