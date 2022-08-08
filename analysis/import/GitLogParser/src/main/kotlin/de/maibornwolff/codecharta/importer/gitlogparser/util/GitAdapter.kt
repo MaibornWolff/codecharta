1d0b99a7c30d5ea019701c66a27a566391e5e09f
Hannah Magin <hannah.magin@maibornwolff.de>
2022-08-08 11:31:05 +0200

    feat: refactor and add proper commands
    2952

--- analysis/import/GitLogParser/src/main/kotlin/de/maibornwolff/codecharta/importer/gitlogparser/util/GitAdapter.kt
+++ analysis/import/GitLogParser/src/main/kotlin/de/maibornwolff/codecharta/importer/gitlogparser/util/GitAdapter.kt
@@ -7,35 +7,22 @@ import java.util.stream.Stream
 class GitAdapter(private val gitDirectory: String) {
 
     fun getGitLog(): Stream<String> {
-        val output = mutableListOf<String>()
-        val process = ProcessBuilder(
-                "git", "ls-files")
-        process.directory(File(gitDirectory))
-        val runningProcess = process.start()
-        runningProcess.inputStream.reader(Charsets.UTF_8).use {
-            output += it.readLines()
-        }
-        runningProcess.waitFor(10, TimeUnit.SECONDS)
-        return output.stream()
+        val process = ProcessBuilder("git", "log", "--numstat", "--raw", "--topo-order", "--reverse", "-m")
+        return executeProcess(process).stream()
     }
 
     fun isGitInstalled(): Boolean {
-        val output = mutableListOf<String>()
-        val process = ProcessBuilder(
-                "git", "ls-files")
-        process.directory(File(gitDirectory))
-        val runningProcess = process.start()
-        runningProcess.inputStream.reader(Charsets.UTF_8).use {
-            output += it.readLines()
-        }
-        runningProcess.waitFor(10, TimeUnit.SECONDS)
-        return true
+        val process = ProcessBuilder("git", "--version")
+        return executeProcess(process).any { it.startsWith("git version") }
     }
 
     fun getGitFiles(): List<String> {
+        val process = ProcessBuilder("git", "ls-files")
+        return executeProcess(process)
+    }
+
+    private fun executeProcess(process: ProcessBuilder): MutableList<String> {
         val output = mutableListOf<String>()
-        val process = ProcessBuilder(
-                "git", "ls-files")
         process.directory(File(gitDirectory))
         val runningProcess = process.start()
         runningProcess.inputStream.reader(Charsets.UTF_8).use {
