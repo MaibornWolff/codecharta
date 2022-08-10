a7f2ac9f30b57fcd720bf953ec134f45cbd47f56
Torsten Knauf <Torsten.Knauf@maibornwolff.de>
2022-08-10 11:30:14 +0200

    test: add test for component
    #2318

--- visualization/app/codeCharta/ui/toolBar/downloadButton/downloadButton.component.spec.ts
+++ visualization/app/codeCharta/ui/toolBar/downloadButton/downloadButton.component.spec.ts
@@ -3,7 +3,9 @@ import { render, waitFor } from "@testing-library/angular"
 import { screen } from "@testing-library/dom"
 import userEvent from "@testing-library/user-event"
 import { stubDate } from "../../../../../mocks/dateMock.helper"
-import { FILE_META, VALID_NODE_WITH_PATH_AND_EXTENSION } from "../../../util/dataMocks"
+import { State } from "../../../state/angular-redux/state"
+import { FILE_META, VALID_EDGES, VALID_NODE_WITH_PATH_AND_EXTENSION } from "../../../util/dataMocks"
+import { FileDownloader } from "../../../util/fileDownloader"
 import { DownloadButtonComponent } from "./downloadButton.component"
 import { DownloadButtonModule } from "./downloadButton.module"
 
@@ -14,16 +16,38 @@ jest.mock("../../../state/selectors/accumulatedData/accumulatedData.selector", (
 
 describe("downloadButtonComponent", () => {
 	stubDate(new Date(Date.UTC(2018, 11, 14, 9, 39)))
+	const mockedFileSettings = {
+		attributeTypes: {
+			nodes: { rloc: "absolute" },
+			edges: { pairingRate: "relative", avgCommits: "absolute" }
+		},
+		blacklist: [],
+		edges: VALID_EDGES,
+		markedPackages: []
+	}
 
 	beforeEach(() => {
 		mockedAccumulatedData["unifiedMapNode"] = VALID_NODE_WITH_PATH_AND_EXTENSION
 		mockedAccumulatedData["unifiedFileMeta"] = FILE_META
 		TestBed.configureTestingModule({
-			imports: [DownloadButtonModule]
+			imports: [DownloadButtonModule],
+			providers: [
+				{
+					provide: State,
+					useValue: {
+						getValue: () => ({
+							files: [], // we only need to mock the key for the test to pass
+							fileSettings: mockedFileSettings
+						})
+					}
+				}
+			]
 		})
 	})
 
 	it("should download a map with custom settings", async () => {
+		const mockedDownload = jest.fn()
+		FileDownloader.downloadCurrentMap = mockedDownload
 		await render(DownloadButtonComponent, { excludeComponentDeclaration: true })
 		userEvent.click(screen.getByRole("button"))
 
@@ -32,7 +56,27 @@ describe("downloadButtonComponent", () => {
 		)
 
 		const checkboxes = document.querySelectorAll("mat-checkbox")
+
 		expect(checkboxes[0].textContent).toContain("Nodes (8)")
-		expect(checkboxes[1].textContent).toContain("AttributeTypes (0)")
+		expect(checkboxes[0].querySelector("input").checked).toBe(true)
+		expect(checkboxes[0].querySelector("input").disabled).toBe(true)
+
+		expect(checkboxes[1].textContent).toContain("AttributeTypes (3)")
+		expect(checkboxes[1].querySelector("input").checked).toBe(true)
+		expect(checkboxes[1].querySelector("input").disabled).toBe(true)
+
+		expect(checkboxes[2].textContent).toContain("Edges (3)")
+		expect(checkboxes[2].querySelector("input").checked).toBe(true)
+		expect(checkboxes[2].querySelector("input").disabled).toBe(false)
+
+		userEvent.click(checkboxes[2].querySelector("input"))
+		expect(checkboxes[2].querySelector("input").checked).toBe(false)
+
+		userEvent.click(screen.getByText("SAVE"))
+		expect(mockedDownload).toHaveBeenCalled()
+		expect(mockedDownload.mock.calls[0][0]).toBe(mockedAccumulatedData["unifiedMapNode"])
+		expect(mockedDownload.mock.calls[0][1]).toBe(mockedAccumulatedData["unifiedFileMeta"])
+		expect(mockedDownload.mock.calls[0][3].length).toBe(2)
+		expect(mockedDownload.mock.calls[0][4]).toBe("fileA_2018-12-14_09-39")
 	})
 })
