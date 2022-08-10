8e52ca5c9fa9248e9e693cb703bcc346a631b77a
Torsten Knauf <Torsten.Knauf@gmx.de>
2022-08-10 10:38:00 +0200

    Refactor/2318/migrate scenario dropdown (#2960)

--- visualization/app/codeCharta/ui/ui.ts
+++ visualization/app/codeCharta/ui/ui.ts
@@ -10,7 +10,6 @@ import "./codeMap/codeMap.module"
 import "./dialog/dialog.module"
 import "./resetSettingsButton/resetSettingsButton.module"
 import "./ribbonBar/ribbonBar.module"
-import "./scenarioDropDown/scenarioDropDown.module"
 import "./toolBar/toolBar.module"
 import "./viewCube/viewCube.module"
 import { Export3DMapButtonComponent } from "./export3DMapButton/export3DMapButton.component"
@@ -27,7 +26,6 @@ angular
 		"app.codeCharta.ui.dialog",
 		"app.codeCharta.ui.resetSettingsButton",
 		"app.codeCharta.ui.ribbonBar",
-		"app.codeCharta.ui.scenarioDropDown",
 		"app.codeCharta.ui.toolBar",
 		"app.codeCharta.ui.viewCube"
 	])
