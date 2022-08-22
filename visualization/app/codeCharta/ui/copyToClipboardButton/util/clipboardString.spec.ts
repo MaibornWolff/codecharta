0bdd32068dafa6dfa7ddb61890924dcba8691ea8
LuisGuenther <93578491+LuisGuenther@users.noreply.github.com>
2022-08-22 12:00:20 +0200

    Feature/2744/copy filenames to clipboard (#2942)
    * Introduce new component for copying filenames to clipboard #2744
    
    * Add method for copying text to clipboard #2744
    
    * Add subscription of button to file state #2744
    
    * Save wip for file name extraction #2744
    
    * test map mutating function #2744
    
    * fix clipboard text construction (wip) #2744
    
    * refactor clipboard component (wip) #2744
    
    * Fix while loop in insertInFullDescendingList, plus adjust renaming, plus adjust test
    #2744
    
    * test if clipboard button uses clipboard #2744
    
    * test if clipboard receives text from service #2744
    
    * fix test description of component #2744
    
    * test clipboard service #2744
    
    * test string building for clipboard #2744
    
    * test getFilenamesWithHighestMetrics #2744
    
    * rename function maybeAddToMap #2744
    
    * document changelog #2744
    
    * document changes via picture #2744
    
    * refactor method for finding top 10 violators #2744
    [Maintenability was prefered over best perfomance]
    
    * document changelog with better text #2744
    
    * remove redundant test #2744
    
    * refactor string construction for clipboard #2744
    
    * refactor variable naming #2744
    
    * refactor function name #2744
    
    * remove comment and documentation #2744
    
    * rename text suite #2744
    
    * refactor new lines after each it in test #2744
    
    * fix left margin of copy-to-clipboard-button #2744
    
    * change button title of copy-to-clipboard #2744
    
    * rename test description #2744
    
    * refactor updateAttributeMap #2744
    
    * change test description #2744
    
    * refactor free lines in test suite #2744
    
    * describe test suite #2744
    
    * test copyToClipBoardButton with tiny setup #2744
    
    * test CopyToClipboardService with tiny setup #2744
    
    Co-authored-by: luig59 <luig59@mi.fu-berlin.de>
    Co-authored-by: knoffi <matthiastutor@outlook.de>
    Co-authored-by: Hall-Ma <maria.hallmann@student.htw-berlin.de>
    Co-authored-by: Matthias Möser <46388280+knoffi@users.noreply.github.com>

---
+++ visualization/app/codeCharta/ui/copyToClipboardButton/util/clipboardString.spec.ts
@@ -0,0 +1,37 @@
+import { buildTextOfFiles } from "./clipboardString"
+import { FileToValue } from "./getFilenamesWithHighestMetrics"
+
+describe("buildTextOfFiles", () => {
+	it("should return only header line if there is only one attribute without files", () => {
+		const result = buildTextOfFiles(WITH_ONE_FILELESS_ATTRIBUTE)
+
+		expect(result).toBe("FUNCTIONS\n")
+	})
+
+	it("should return valid string if there two attributes", () => {
+		const result = buildTextOfFiles(WITH_TWO_ATTRIBUTES)
+
+		expect(result).toBe(`RLOC\n` + `\t• fileA (12)\n` + `COMMENTS\n` + `\t• fileA (14)\n`)
+	})
+
+	it("should return valid string if there is one attribute with many files", () => {
+		const result = buildTextOfFiles(WITH_ONE_ATTRIBUTE)
+
+		expect(result).toBe(`MCC\n` + `\t• file1 (100)\n` + `\t• file2 (84)\n` + `\t• file3 (122)\n`)
+	})
+})
+const WITH_ONE_ATTRIBUTE = new Map<string, FileToValue[]>([
+	[
+		"mcc",
+		[
+			{ name: "file1", value: 100 },
+			{ name: "file2", value: 84 },
+			{ name: "file3", value: 122 }
+		]
+	]
+])
+const WITH_TWO_ATTRIBUTES = new Map<string, FileToValue[]>([
+	["rloc", [{ name: "fileA", value: 12 }]],
+	["comments", [{ name: "fileA", value: 14 }]]
+])
+const WITH_ONE_FILELESS_ATTRIBUTE = new Map<string, FileToValue[]>([["functions", []]])
---
+++ visualization/app/codeCharta/ui/copyToClipboardButton/util/getFilenamesWithHighestMetric.spec.ts
@@ -0,0 +1,136 @@
+import { NodeType } from "../../../codeCharta.model"
+import { FileToValue, getFilenamesWithHighestMetrics, updateAttributeMap } from "./getFilenamesWithHighestMetrics"
+
+let MAP: Map<string, FileToValue[]>
+
+beforeEach(() => {
+	MAP = new Map<string, FileToValue[]>([
+		[
+			"mcc",
+			[
+				{ name: "file2", value: 500 },
+				{ name: "file3", value: 400 },
+				{ name: "file1", value: 300 },
+				{ name: "file1", value: 200 },
+				{ name: "file1", value: 100 },
+				{ name: "file1", value: 50 },
+				{ name: "file1", value: 20 },
+				{ name: "file1", value: 15 },
+				{ name: "file1", value: 10 },
+				{ name: "file1", value: 5 }
+			]
+		]
+	])
+})
+
+describe("getFilenamesWithHighestMetrics", () => {
+	it("should return a map with keys for exactly two attribute", () => {
+		const resultMap = getFilenamesWithHighestMetrics(BIG_NODE_WITH_TWO_ATTRIBUTES)
+		const mccResults = resultMap.get("mcc")
+		const rlocResults = resultMap.get("rloc")
+
+		expect(resultMap.size).toBe(2)
+		expect(mccResults).toBeTruthy()
+		expect(rlocResults).toBeTruthy()
+	})
+
+	it("should return correct values for rloc and mcc", () => {
+		const resultMap = getFilenamesWithHighestMetrics(BIG_NODE_WITH_TWO_ATTRIBUTES)
+		const mccResults = resultMap.get("mcc")
+		const rlocResults = resultMap.get("rloc")
+
+		expect(rlocResults).toEqual([{ name: "leaf1", value: 5 }])
+		expect(mccResults).toEqual([
+			{ name: "leaf1", value: 10 },
+			{ name: "leaf2", value: 9 },
+			{ name: "leaf3", value: 8 },
+			{ name: "leaf4", value: 7 },
+			{ name: "leaf5", value: 6 },
+			{ name: "leaf6", value: 5 },
+			{ name: "leaf7", value: 4 },
+			{ name: "leaf8", value: 3 },
+			{ name: "leaf9", value: 2 },
+			{ name: "leaf10", value: 1 }
+		])
+	})
+
+	it("should ignore folders", () => {
+		const map = getFilenamesWithHighestMetrics(CONTAINS_FOLDER_WITH_ATTRIBUTES)
+
+		expect(map.get("rloc")).toBeUndefined()
+		expect(map.get("mcc")).toEqual([{ name: "file", value: 0 }])
+	})
+})
+
+describe("updateAttributeMap", () => {
+	it("should not add item to attribute if value is lower than old values", () => {
+		updateAttributeMap("mcc", 0, "file with low value", MAP)
+
+		expect(MAP.get("mcc")).not.toContainEqual({ name: "file with low value", value: 0 })
+		expect(MAP.get("mcc").length).toBe(10)
+	})
+
+	it("should add item to attribute if value is higher than old values", () => {
+		updateAttributeMap("mcc", 9001, "file with highest value", MAP)
+
+		expect(MAP.get("mcc")).toContainEqual({ name: "file with highest value", value: 9001 })
+		expect(MAP.get("mcc").length).toBe(10)
+	})
+
+	it("should add attribute and attribute gets item if map is empty", () => {
+		const EMPTY_MAP = new Map<string, FileToValue[]>()
+
+		updateAttributeMap("mcc", 0, "first file", EMPTY_MAP)
+
+		expect(EMPTY_MAP.get("mcc")).toContainEqual({ name: "first file", value: 0 })
+		expect(EMPTY_MAP.get("mcc").length).toBe(1)
+	})
+
+	it("should add item to attribute if attribute has only one item", () => {
+		const TINY_MAP = new Map<string, FileToValue[]>([["mcc", [{ name: "first file", value: 424_242 }]]])
+
+		updateAttributeMap("mcc", 0, "second file", TINY_MAP)
+
+		expect(TINY_MAP.get("mcc")).toContainEqual({ name: "second file", value: 0 })
+		expect(TINY_MAP.get("mcc").length).toBe(2)
+	})
+
+	it("should add attribute and attribute gets item if attribute is new", () => {
+		updateAttributeMap("rloc", 0, "first rloc file", MAP)
+
+		expect(MAP.get("rloc")).toEqual([{ name: "first rloc file", value: 0 }])
+		expect(MAP.get("mcc").length).toBe(10)
+		expect(MAP.size).toBe(2)
+	})
+})
+
+const BIG_NODE_WITH_TWO_ATTRIBUTES = {
+	name: "root",
+	type: NodeType.FOLDER,
+	children: [
+		{
+			name: "folder",
+			type: NodeType.FOLDER,
+			children: [
+				{ name: "leaf2", attributes: { ["mcc"]: 9 }, type: NodeType.FILE },
+				{ name: "leaf3", attributes: { ["mcc"]: 8 }, type: NodeType.FILE },
+				{ name: "leaf4", attributes: { ["mcc"]: 7 }, type: NodeType.FILE },
+				{ name: "leaf5", attributes: { ["mcc"]: 6 }, type: NodeType.FILE },
+				{ name: "leaf6", attributes: { ["mcc"]: 5 }, type: NodeType.FILE },
+				{ name: "leaf7", attributes: { ["mcc"]: 4 }, type: NodeType.FILE },
+				{ name: "leaf8", attributes: { ["mcc"]: 3 }, type: NodeType.FILE },
+				{ name: "leaf9", attributes: { ["mcc"]: 2 }, type: NodeType.FILE },
+				{ name: "leaf10", attributes: { ["mcc"]: 1 }, type: NodeType.FILE },
+				{ name: "leaf11", attributes: { ["mcc"]: 0 }, type: NodeType.FILE }
+			]
+		},
+		{ name: "leaf1", attributes: { ["mcc"]: 10, ["rloc"]: 5 }, type: NodeType.FILE }
+	]
+}
+
+const CONTAINS_FOLDER_WITH_ATTRIBUTES = {
+	name: "root_folder",
+	type: NodeType.FOLDER,
+	attributes: { mcc: 111, rloc: 15 },
+	children: [{ name: "file", type: NodeType.FILE, attributes: { mcc: 0 } }]
+}
