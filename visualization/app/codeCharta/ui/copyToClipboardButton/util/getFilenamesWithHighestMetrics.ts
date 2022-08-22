7138391dba7c26acb0cb2d6fa2455f84aa611032
knoffi <matthiastutor@outlook.de>
2022-08-22 18:08:42 +0200

    change clipboard to show file path + no name #2992

--- visualization/app/codeCharta/ui/copyToClipboardButton/util/getFilenamesWithHighestMetrics.ts
+++ visualization/app/codeCharta/ui/copyToClipboardButton/util/getFilenamesWithHighestMetrics.ts
@@ -1,24 +1,26 @@
 import { hierarchy } from "d3-hierarchy"
 import { CodeMapNode, NodeType } from "../../../codeCharta.model"
 
-export type FileToValue = { name: string; value: number }
+export type FileToValue = { filePath: string; value: number }
 
 const MAX_ENTRIES = 10
 
-export function getFilenamesWithHighestMetrics(node: Pick<CodeMapNode, "children" | "type" | "attributes" | "name">) {
+type ReducedCodeMapNode = Pick<CodeMapNode, "type" | "attributes" | "path"> & { children?: ReducedCodeMapNode[] }
+
+export function getFilenamesWithHighestMetrics(node: ReducedCodeMapNode) {
 	const fileToValueByAttributes = new Map<string, FileToValue[]>()
 	for (const { data } of hierarchy(node)) {
 		if (data.type === NodeType.FILE && data.attributes) {
 			for (const [key, value] of Object.entries(data.attributes)) {
-				updateAttributeMap(key, value, data.name, fileToValueByAttributes)
+				updateAttributeMap(key, value, data.path, fileToValueByAttributes)
 			}
 		}
 	}
 	return fileToValueByAttributes
 }
 
-export function updateAttributeMap(key: string, value: number, fileName: string, map: Map<string, FileToValue[]>) {
-	const newPair = { name: fileName, value }
+export function updateAttributeMap(key: string, value: number, filePath: string, map: Map<string, FileToValue[]>) {
+	const newPair = { filePath, value }
 
 	const keyIsNew = !map.has(key)
 	if (keyIsNew) {
