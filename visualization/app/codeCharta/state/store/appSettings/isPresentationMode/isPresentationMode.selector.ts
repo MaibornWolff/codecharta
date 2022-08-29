b4c21cb0d5d1a5d5ec7012916e82eb12f7c23edb
Alex Ander <alexander.roidl@maibornwolff.de>
2022-08-29 11:43:16 +0200

    Made Prettier happy again Changed <div> to <button> so font is displayed correctly

--- visualization/app/codeCharta/state/store/appSettings/isPresentationMode/isPresentationMode.selector.ts
+++ visualization/app/codeCharta/state/store/appSettings/isPresentationMode/isPresentationMode.selector.ts
@@ -1,4 +1,4 @@
-import { createSelector } from "../../../angular-redux/createSelector";
-import { appSettingsSelector } from "../appSettings.selector";
+import { createSelector } from "../../../angular-redux/createSelector"
+import { appSettingsSelector } from "../appSettings.selector"
 
-export const isPresentationModeSelector = createSelector([appSettingsSelector], (appSettings) => appSettings.isPresentationMode)
+export const isPresentationModeSelector = createSelector([appSettingsSelector], appSettings => appSettings.isPresentationMode)
