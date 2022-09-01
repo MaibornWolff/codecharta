89edc48883085fb63c8ca6a0ea72e57fcd1ea2a3
MW-Friedrich <108457762+MW-Friedrich@users.noreply.github.com>
2022-09-01 16:18:07 +0200

    Feature/2619/run metric gardener automatically (#3015)
    * Add option to automatically run MG in the ccsh
    #2619
    
    * Add turtle dependency
    Remove unused gradle node plugin
    #2619
    
    * Add tests
    #2619
    
    * Add metric-gardener as a package.json-dependency
    WIP
    #2619
    
    * Add dummy file for golden-test.sh
    #2619
    
    * fix: parameter logic, tests
    
    * fix: always check input file exists
    
    * fix: install metric-gardener on demand
    
    * fix: remove metric-gardener from package.json
    
    * fix: golden test
    
    * Update CHANGELOG.md
    #2619
    
    Co-authored-by: Mira Weller <mira.weller@maibornwolff.de>

---
+++ analysis/import/MetricGardenerImporter/src/test/resources/MetricGardenerRawFile.kt
@@ -0,0 +1,40 @@
+package de.maibornwolff.codecharta.importer.metricgardenerimporter.model
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import de.maibornwolff.codecharta.model.Path
+
+@JsonIgnoreProperties("types")
+class MetricGardenerRawFile(
+    @JsonProperty("name") var name: String?,
+    @JsonProperty("type") var type: String?,
+    @JsonProperty("metrics") var metrics: Map<String, Any>
+) {
+
+    fun getPathWithoutFileName(): Path {
+        if (!name.isNullOrBlank()) {
+            return Path(name!!.split("\\").dropLast(1))
+        }
+        return Path(emptyList())
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MetricGardenerRawFile
+
+        if (name != other.name) return false
+        if (type != other.type) return false
+        if (metrics != other.metrics) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name?.hashCode() ?: 0
+        result = 31 * result + (type?.hashCode() ?: 0)
+        result = 31 * result + metrics.hashCode()
+        return result
+    }
+}
