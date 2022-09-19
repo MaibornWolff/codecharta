ed66a89baa8efb224f142b7705da3a6172aeb5c7
RomanenkoVladimir <volodymyr.romanenko@icloud.com>
2022-09-19 16:03:41 +0200

    Fix prettier, add test

--- visualization/app/codeCharta/util/uploadFiles/createCCFileInput.spec.ts
+++ visualization/app/codeCharta/util/uploadFiles/createCCFileInput.spec.ts
@@ -2,10 +2,12 @@ import { createCCFileInput } from "./createCCFileInput"
 
 describe("createCCFileInput", () => {
 	it("should create a file input for json or gz files", () => {
+		jest.spyOn(document.body, 'appendChild');
 		const ccFileInput = createCCFileInput()
 		expect(ccFileInput.nodeName).toBe("INPUT")
 		expect(ccFileInput.type).toBe("file")
 		expect(ccFileInput.accept).toBe(".json,.gz")
 		expect(ccFileInput.multiple).toBe(true)
+		expect(document.body.appendChild).toHaveBeenCalledTimes(1)
 	})
 })
--- visualization/app/codeCharta/util/uploadFiles/createCCFileInput.ts
+++ visualization/app/codeCharta/util/uploadFiles/createCCFileInput.ts
@@ -1,8 +1,8 @@
 export const createCCFileInput = () => {
 	const fileInput = document.createElement("INPUT") as HTMLInputElement
-	fileInput.setAttribute('type', "file")
-	fileInput.setAttribute('accept', ".json,.gz")
-	fileInput.setAttribute('multiple', "")
-	document.body.appendChild(fileInput);
+	fileInput.setAttribute("type", "file")
+	fileInput.setAttribute("accept", ".json,.gz")
+	fileInput.setAttribute("multiple", "")
+	document.body.appendChild(fileInput)
 	return fileInput
-	}
+}
