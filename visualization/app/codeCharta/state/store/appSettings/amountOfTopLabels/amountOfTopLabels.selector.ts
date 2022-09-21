c4dfce7bcdef8c60abc756f333aa2131d9c8526e
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-09-21 14:07:16 +0200

    Adjust amountOfTopLabels selector to adjust dynamically the number of top labels, event handling is still WIP #2584

--- visualization/app/codeCharta/state/store/appSettings/amountOfTopLabels/amountOfTopLabels.selector.ts
+++ visualization/app/codeCharta/state/store/appSettings/amountOfTopLabels/amountOfTopLabels.selector.ts
@@ -1,4 +1,23 @@
 import { createSelector } from "../../../angular-redux/createSelector"
 import { appSettingsSelector } from "../appSettings.selector"
+import { AppSettings } from "../../../../codeCharta.model"
 
-export const amountOfTopLabelsSelector = createSelector([appSettingsSelector], appSettings => appSettings.amountOfTopLabels)
+import { numberOfFilesSelector } from "./numberOfFiles.selector"
+
+const NUMBER_OF_BUILDINGS = 100
+const DEFAULT_NUMBER_OF_LABELS = 1
+const MAX_NUMBER_OF_LABELS = 10
+
+export const getAmountOfTopLabels = (appSettings: Pick<AppSettings, "amountOfTopLabels">, numberOfFiles: any): number => {
+	const { amountOfTopLabels } = appSettings
+	const numberOfLabels = Math.floor(numberOfFiles.length / NUMBER_OF_BUILDINGS)
+	if (numberOfLabels <= DEFAULT_NUMBER_OF_LABELS) {
+		return amountOfTopLabels
+	}
+	if (numberOfLabels > DEFAULT_NUMBER_OF_LABELS && numberOfLabels <= MAX_NUMBER_OF_LABELS) {
+		return numberOfLabels
+	}
+	return MAX_NUMBER_OF_LABELS
+}
+
+export const amountOfTopLabelsSelector = createSelector([appSettingsSelector, numberOfFilesSelector], getAmountOfTopLabels)
