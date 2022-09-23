6444a49ab915da981af908c5876cb76f016a1a75
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-09-23 14:12:02 +0200

    Update tests #2584

--- visualization/app/codeCharta/state/effects/updateVisibleTopLabels/getNumberOfTopLabels.ts
+++ visualization/app/codeCharta/state/effects/updateVisibleTopLabels/getNumberOfTopLabels.ts
@@ -1,10 +1,9 @@
 import { defaultAmountOfTopLabels } from "../../store/appSettings/amountOfTopLabels/amountOfTopLabels.actions"
-import { CodeMapNode } from "../../../codeCharta.model"
 
 const BUILDINGS_PER_LABEL = 100
 const MAX_NUMBER_OF_LABELS = 10
 
-export const getNumberOfTopLabels = (codeMapNodes: CodeMapNode[]) => {
+export const getNumberOfTopLabels = (codeMapNodes: { length: number }) => {
 	const numberOfLabels = Math.floor(codeMapNodes.length / BUILDINGS_PER_LABEL)
 	if (numberOfLabels <= defaultAmountOfTopLabels) {
 		return defaultAmountOfTopLabels
--- visualization/app/codeCharta/state/effects/updateVisibleTopLabels/updateVisibleTopLabels.effect.spec.ts
+++ visualization/app/codeCharta/state/effects/updateVisibleTopLabels/updateVisibleTopLabels.effect.spec.ts
@@ -5,11 +5,8 @@ import { UpdateVisibleTopLabelsEffect } from "./updateVisibleTopLabels.effect"
 import { setAmountOfTopLabels } from "../../store/appSettings/amountOfTopLabels/amountOfTopLabels.actions"
 import { codeMapNodesSelector } from "../../selectors/accumulatedData/codeMapNodesSelector"
 import { Subject } from "rxjs"
-
 import { visibleFileStatesSelector } from "../../selectors/visibleFileStates.selector"
-import { FileState } from "../../../model/files/files"
 import { Store } from "../../angular-redux/store"
-import { CodeMapNode } from "../../../codeCharta.model"
 import { Store as PlainStoreUsedByEffects } from "../../store/store"
 
 describe("updateVisibleTopLabelsEffect", () => {
@@ -47,26 +44,21 @@ describe("updateVisibleTopLabelsEffect", () => {
 		mockedCodeMapNodesSelector.complete()
 	})
 
-	it("should do nothing when only FileState changes", () => {
-		mockedVisibleFileStatesSelector.next([] as FileState[])
-		expect(mockedStore.dispatch).not.toHaveBeenCalled()
-	})
-
 	it("should set setAmountOfTopLabels to 1 on FileState change when number of nodes are equal or less than 100", () => {
-		mockedCodeMapNodesSelector.next(Array.from({ length: 10 }).fill({}) as CodeMapNode[])
-		mockedVisibleFileStatesSelector.next([] as FileState[])
+		mockedCodeMapNodesSelector.next(Array.from({ length: 10 }).fill({}))
+		mockedVisibleFileStatesSelector.next([])
 		expect(mockedStore.dispatch).toHaveBeenCalledWith(setAmountOfTopLabels(1))
 	})
 
 	it("should set setAmountOfTopLabels to 2 on FileState change when number of nodes are greater than or equal to 200 and less than 300", () => {
-		mockedCodeMapNodesSelector.next(Array.from({ length: 200 }).fill({}) as CodeMapNode[])
-		mockedVisibleFileStatesSelector.next([] as FileState[])
+		mockedCodeMapNodesSelector.next(Array.from({ length: 200 }).fill({}))
+		mockedVisibleFileStatesSelector.next([])
 		expect(mockedStore.dispatch).toHaveBeenCalledWith(setAmountOfTopLabels(2))
 	})
 
 	it("should set setAmountOfTopLabels to 10 (max limit for displayed top labels) on FileState change when number of nodes are greater than 1000", () => {
-		mockedCodeMapNodesSelector.next(Array.from({ length: 1001 }).fill({}) as CodeMapNode[])
-		mockedVisibleFileStatesSelector.next([] as FileState[])
+		mockedCodeMapNodesSelector.next(Array.from({ length: 1001 }).fill({}))
+		mockedVisibleFileStatesSelector.next([])
 		expect(mockedStore.dispatch).toHaveBeenCalledWith(setAmountOfTopLabels(10))
 	})
 })
