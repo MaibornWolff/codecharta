2096fa1a39e52a570864687fa06eb48de04db0b0
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-09-24 14:08:31 +0200

    refactor: migrate tests
    ref #2318

--- visualization/app/codeCharta/ui/codeMap/codeMap.component.ts
+++ visualization/app/codeCharta/ui/codeMap/codeMap.component.ts
@@ -1,21 +1,31 @@
 import "./codeMap.component.scss"
-import { Component, Inject, AfterViewInit, ElementRef } from "@angular/core"
+import { Component, Inject, AfterViewInit, ElementRef, OnDestroy } from "@angular/core"
 import { Store } from "../../state/angular-redux/store"
 import { isLoadingFileSelector } from "../../state/store/appSettings/isLoadingFile/isLoadingFile.selector"
 import { isAttributeSideBarVisibleSelector } from "../../state/store/appSettings/isAttributeSideBarVisible/isAttributeSideBarVisible.selector"
 import { ThreeViewerService } from "./threeViewer/threeViewerService"
 import { CodeMapMouseEventServiceToken, ThreeViewerServiceToken } from "../../services/ajs-upgraded-providers"
-import { onStoreChanged } from "../../state/angular-redux/onStoreChanged/onStoreChanged"
 import { sharpnessModeSelector } from "../../state/store/appSettings/sharpnessMode/sharpnessMode.selector"
 import { CodeMapMouseEventService } from "./codeMap.mouseEvent.service"
+import { skip, tap } from "rxjs"
 
 @Component({
 	selector: "cc-code-map",
 	template: require("./codeMap.component.html")
 })
-export class CodeMapComponent implements AfterViewInit {
+export class CodeMapComponent implements AfterViewInit, OnDestroy {
 	isLoadingFile$ = this.store.select(isLoadingFileSelector)
 	isAttributeSidebarVisible$ = this.store.select(isAttributeSideBarVisibleSelector)
+	restartOnSharpnessModeChangesSubscription = this.store
+		.select(sharpnessModeSelector)
+		.pipe(
+			skip(1),
+			tap(() => {
+				this.threeViewerService.restart(this.elementReference.nativeElement.querySelector("#codeMap"))
+				this.codeMapMouseEventService.start()
+			})
+		)
+		.subscribe()
 
 	constructor(
 		@Inject(Store) private store: Store,
@@ -27,12 +37,9 @@ export class CodeMapComponent implements AfterViewInit {
 	ngAfterViewInit(): void {
 		this.threeViewerService.init(this.elementReference.nativeElement.querySelector("#codeMap"))
 		this.codeMapMouseEventService.start()
+	}
 
-		onStoreChanged(sharpnessModeSelector, oldValue => {
-			if (oldValue) {
-				this.threeViewerService.restart(this.elementReference.nativeElement.querySelector("#codeMap"))
-				this.codeMapMouseEventService.start()
-			}
-		})
+	ngOnDestroy(): void {
+		this.restartOnSharpnessModeChangesSubscription.unsubscribe()
 	}
 }
