a438e6a96a9a4c45ca5ce86c0b20be06183b56e0
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-09-24 14:19:11 +0200

    refactor: remove not anymore needed downgrades
    ref #2318

--- visualization/app/codeCharta/ui/codeMap/codeMap.module.ts
+++ visualization/app/codeCharta/ui/codeMap/codeMap.module.ts
@@ -1,6 +1,6 @@
 import angular from "angular"
 import camelCase from "lodash.camelcase"
-import { downgradeComponent, downgradeInjectable } from "@angular/upgrade/static"
+import { downgradeInjectable } from "@angular/upgrade/static"
 
 import "./threeViewer/threeViewer.module"
 import "../../state/state.module"
@@ -8,8 +8,6 @@ import { CodeMapMouseEventService } from "./codeMap.mouseEvent.service"
 import { CodeMapRenderService } from "./codeMap.render.service"
 import { CodeMapLabelService } from "./codeMap.label.service"
 import { CodeMapArrowService } from "./codeMap.arrow.service"
-import { AttributeSideBarComponent } from "../attributeSideBar/attributeSideBar.component"
-import { ViewCubeComponent } from "../viewCube/viewCube.component"
 import { ViewCubeMouseEventsService } from "../viewCube/viewCube.mouseEvents.service"
 import { NgModule } from "@angular/core"
 import { CommonModule } from "@angular/common"
@@ -20,12 +18,10 @@ import { CodeMapComponent } from "./codeMap.component"
 angular
 	.module("app.codeCharta.ui.codeMap", ["app.codeCharta.state", "app.codeCharta", "app.codeCharta.ui.codeMap.threeViewer"])
 	.factory("viewCubeMouseEvents", downgradeInjectable(ViewCubeMouseEventsService))
-	.directive("ccAttributeSideBar", downgradeComponent({ component: AttributeSideBarComponent }))
 	.service(camelCase(CodeMapRenderService.name), CodeMapRenderService)
 	.service(camelCase(CodeMapMouseEventService.name), CodeMapMouseEventService)
 	.service(camelCase(CodeMapLabelService.name), CodeMapLabelService)
 	.service(camelCase(CodeMapArrowService.name), CodeMapArrowService)
-	.directive("ccViewCube", downgradeComponent({ component: ViewCubeComponent }))
 
 @NgModule({
 	imports: [CommonModule, ViewCubeModule, AttributeSideBarModule],
