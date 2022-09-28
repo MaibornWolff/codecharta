49f8e9ec244a7ce6eb8b38fa24f9a50526f4aec3
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-09-28 12:40:37 +0200

    Adjust effect in a better way #2507

--- visualization/app/codeCharta/state/effects/coupleHeightAndColorMetric/coupleHeightAndColorMetric.effect.ts
+++ visualization/app/codeCharta/state/effects/coupleHeightAndColorMetric/coupleHeightAndColorMetric.effect.ts
@@ -1,26 +1,20 @@
 import { Inject, Injectable } from "@angular/core"
 import { Store } from "../../angular-redux/store"
 import { createEffect } from "../../angular-redux/effects/createEffect"
-import { combineLatest, map } from "rxjs"
+import { combineLatest, filter, map } from "rxjs"
 import { isHeightAndColorMetricCoupledSelector } from "../../store/appSettings/isHeightAndColorMetricCoupled/isHeightAndColorMetricCoupled.selector"
 
 import { setColorMetric } from "../../store/dynamicSettings/colorMetric/colorMetric.actions"
 import { heightMetricSelector } from "../../store/dynamicSettings/heightMetric/heightMetric.selector"
-import { colorMetricSelector } from "../../store/dynamicSettings/colorMetric/colorMetric.selector"
 
 @Injectable()
 export class CoupleHeightAndColorMetricEffect {
 	constructor(@Inject(Store) private store: Store) {}
 
 	coupleHeightAndColorMetric$ = createEffect(() =>
-		combineLatest([
-			this.store.select(heightMetricSelector),
-			this.store.select(isHeightAndColorMetricCoupledSelector),
-			this.store.select(colorMetricSelector)
-		]).pipe(
-			map(([heightMetric, isCouplingEnabled, colorMetric]) => {
-				return isCouplingEnabled ? setColorMetric(heightMetric) : setColorMetric(colorMetric)
-			})
+		combineLatest([this.store.select(heightMetricSelector), this.store.select(isHeightAndColorMetricCoupledSelector)]).pipe(
+			filter(([, isCouplingEnabled]) => isCouplingEnabled),
+			map(([heightMetric]) => setColorMetric(heightMetric))
 		)
 	)
 }
