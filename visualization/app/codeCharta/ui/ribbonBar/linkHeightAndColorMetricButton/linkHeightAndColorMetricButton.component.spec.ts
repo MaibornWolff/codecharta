9547759205321a689843cf7b37d6ce7fc0eed699
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-09-29 15:50:48 +0200

    Adjust toggle link metric button style (WIP) #2507

--- visualization/app/codeCharta/ui/ribbonBar/linkHeightAndColorMetricButton/linkHeightAndColorMetricButton.component.spec.ts
+++ visualization/app/codeCharta/ui/ribbonBar/linkHeightAndColorMetricButton/linkHeightAndColorMetricButton.component.spec.ts
@@ -1,12 +1,10 @@
-import {render, screen} from "@testing-library/angular"
+import { render, screen } from "@testing-library/angular"
 import userEvent from "@testing-library/user-event"
-import {LinkHeightAndColorMetricButtonComponent} from "./linkHeightAndColorMetricButton.component"
-import {Store} from "../../../state/store/store"
-import {
-	isHeightAndColorMetricLinkedSelector
-} from "../../../state/store/appSettings/isHeightAndColorMetricLinked/isHeightAndColorMetricLinked.selector"
-import {TestBed} from "@angular/core/testing"
-import {CommonModule} from "@angular/common"
+import { LinkHeightAndColorMetricButtonComponent } from "./linkHeightAndColorMetricButton.component"
+import { Store } from "../../../state/store/store"
+import { isHeightAndColorMetricLinkedSelector } from "../../../state/store/appSettings/isHeightAndColorMetricLinked/isHeightAndColorMetricLinked.selector"
+import { TestBed } from "@angular/core/testing"
+import { CommonModule } from "@angular/common"
 
 describe("linkHeightAndColorMetricComponent", () => {
 	beforeEach(() => {
@@ -22,11 +20,14 @@ describe("linkHeightAndColorMetricComponent", () => {
 
 		expect(container.getElementsByClassName("fa fa-chain-broken")[0].className).toEqual("fa fa-chain-broken")
 		expect(container.getElementsByClassName("fa fa-link")[0]).toBeUndefined()
+		expect(screen.queryByText("Unlink Height And Color Metric")).toBe(null)
 
 		await userEvent.click(screen.getByTitle("Link Height And Color Metric"))
 
 		expect(container.getElementsByClassName("fa fa-link")[0].className).toEqual("fa fa-link")
 		expect(container.getElementsByClassName("fa fa-chain-broken")[0]).toBeUndefined()
+		expect(screen.getByTitle("Unlink Height And Color Metric")).not.toBe(null)
+		expect(screen.queryByText("Link Height And Color Metric")).toBe(null)
 		expect(isHeightAndColorMetricLinkedSelector(Store.store.getState())).toBe(!isHeightAndColorMetricLinked)
 	})
 })
