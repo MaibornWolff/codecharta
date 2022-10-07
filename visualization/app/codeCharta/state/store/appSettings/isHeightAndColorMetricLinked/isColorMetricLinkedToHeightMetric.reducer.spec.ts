292ec5623026160f25f11057d9b478a2c28fbe91
Hall-Ma <maria.hallmann@student.htw-berlin.de>
2022-10-07 16:43:26 +0200

    Add test for new action #2057

--- visualization/app/codeCharta/state/store/appSettings/isHeightAndColorMetricLinked/isColorMetricLinkedToHeightMetric.reducer.spec.ts
+++ visualization/app/codeCharta/state/store/appSettings/isHeightAndColorMetricLinked/isColorMetricLinkedToHeightMetric.reducer.spec.ts
@@ -1,10 +1,11 @@
 import {
 	IsColorMetricLinkedToHeightMetricAction,
+	setIsColorMetricLinkedToHeightMetricAction,
 	toggleIsColorMetricLinkedToHeightMetric
 } from "./isColorMetricLinkedToHeightMetric.actions"
 import { isColorMetricLinkedToHeightMetric } from "./isColorMetricLinkedToHeightMetric.reducer"
 
-describe("isHeightAndColorMetricLinked", () => {
+describe("isColorMetricLinkedToHeightMetric", () => {
 	describe("Default State", () => {
 		it("should initialize the default state", () => {
 			const result = isColorMetricLinkedToHeightMetric(undefined, {} as IsColorMetricLinkedToHeightMetricAction)
@@ -13,12 +14,21 @@ describe("isHeightAndColorMetricLinked", () => {
 		})
 	})
 
-	describe("Action: TOGGLE_IS_HEIGHT_AND_COLOR_METRIC_LINKED", () => {
+	describe("Action: TOGGLE_IS_COLOR_METRIC_LINKED_TO_HEIGHT_METRIC", () => {
 		it("should toggle state", () => {
 			const result = isColorMetricLinkedToHeightMetric(false, toggleIsColorMetricLinkedToHeightMetric())
 			const toggleResult = isColorMetricLinkedToHeightMetric(result, toggleIsColorMetricLinkedToHeightMetric())
 
 			expect(result).toBe(!toggleResult)
+		})
+	})
+
+	describe("Action: SET_IS_COLOR_METRIC_LINKED_TO_HEIGHT_METRIC", () => {
+		it("should set new isColorMetricLinkedToHeightMetric state", () => {
+			const isLinked = isColorMetricLinkedToHeightMetric(false, setIsColorMetricLinkedToHeightMetricAction(true))
+			const isNotLinked = isColorMetricLinkedToHeightMetric(isLinked, setIsColorMetricLinkedToHeightMetricAction(false))
+
+			expect(isLinked).not.toBe(isNotLinked)
 		})
 	})
 })
