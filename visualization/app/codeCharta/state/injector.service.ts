418fda281f2e86e13ff5119898dfed9be469e837
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-10-16 22:22:30 +0200

    refactor: Remove now obsolete isLoadingFile.service
    ref #2318

--- visualization/app/codeCharta/state/injector.service.ts
+++ visualization/app/codeCharta/state/injector.service.ts
@@ -1,7 +1,6 @@
 //@ts-nocheck
 
 import { FilesService } from "./store/files/files.service"
-import { IsLoadingFileService } from "./store/appSettings/isLoadingFile/isLoadingFile.service"
 import { MapSizeService } from "./store/treeMap/mapSize/mapSize.service"
 import { MarkedPackagesService } from "./store/fileSettings/markedPackages/markedPackages.service"
 import { EdgesService } from "./store/fileSettings/edges/edges.service"
@@ -9,12 +8,11 @@ import { BlacklistService } from "./store/fileSettings/blacklist/blacklist.servi
 export class InjectorService {
 	constructor(
 		// We have to inject the services somewhere
-		private isLoadingFileService: IsLoadingFileService,
 		private filesService: FilesService,
 		private mapSizeService: MapSizeService,
 		private markedPackagesService: MarkedPackagesService,
 		private edgesService: EdgesService,
-		private blacklistService: BlacklistService,
+		private blacklistService: BlacklistService
 	) {
 		"ngInject"
 	}
