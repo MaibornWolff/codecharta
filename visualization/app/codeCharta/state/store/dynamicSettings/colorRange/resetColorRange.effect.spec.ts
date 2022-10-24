818c08de6a508ae88557b3d51792c92654c0de35
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-10-24 20:35:04 +0200

    fix: don't reset when changing blacklist items
    ref #2318

--- visualization/app/codeCharta/state/store/dynamicSettings/colorRange/resetColorRange.effect.spec.ts
+++
@@ -1,7 +0,0 @@
-import { _metricMinMaxToInitialColorRange } from "./resetColorRange.effect"
-
-describe("ResetColorRangeColorRangeEffect", () => {
-	it("should split positive, neutral, negative to a third each", () => {
-		expect(_metricMinMaxToInitialColorRange({ minValue: 20, maxValue: 120 })).toEqual({ from: 53, to: 86 })
-	})
-})
