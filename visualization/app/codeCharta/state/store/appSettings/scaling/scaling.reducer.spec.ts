3444dda5e8f23f033756281eba0364327e92683a
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-10-24 22:22:07 +0200

    refactor: adjust test
    ref #2318

--- visualization/app/codeCharta/state/store/appSettings/scaling/scaling.reducer.spec.ts
+++ visualization/app/codeCharta/state/store/appSettings/scaling/scaling.reducer.spec.ts
@@ -1,27 +1,26 @@
 import { scaling } from "./scaling.reducer"
 import { ScalingAction, setScaling } from "./scaling.actions"
-import { Vector3 } from "three"
 
 describe("scaling", () => {
 	describe("Default State", () => {
 		it("should initialize the default state", () => {
 			const result = scaling(undefined, {} as ScalingAction)
 
-			expect(result).toEqual(new Vector3(1, 1, 1))
+			expect(result).toEqual({ x: 1, y: 1, z: 1 })
 		})
 	})
 
 	describe("Action: SET_SCALING", () => {
 		it("should set new scaling", () => {
-			const result = scaling(new Vector3(1, 1, 1), setScaling(new Vector3(2, 2, 2)))
+			const result = scaling({ x: 1, y: 1, z: 1 }, setScaling({ x: 2, y: 2, z: 2 }))
 
-			expect(result).toEqual(new Vector3(2, 2, 2))
+			expect(result).toEqual({ x: 2, y: 2, z: 2 })
 		})
 
 		it("should update partial scaling", () => {
-			const result = scaling(new Vector3(2, 2, 2), setScaling({ y: 1 }))
+			const result = scaling({ x: 2, y: 2, z: 2 }, setScaling({ y: 1 }))
 
-			expect(result).toEqual(new Vector3(2, 1, 2))
+			expect(result).toEqual({ x: 2, y: 1, z: 2 })
 		})
 	})
 })
