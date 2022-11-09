0ed207ad9ceac7e1146db0e0c784197dbeaba669
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-11-09 23:27:05 +0100

    refactor: split unit and e2e configs
    #2300

--- visualization/conf/setupJestUnit.ts
+++ visualization/conf/setupJestUnit.ts
@@ -1,7 +1,4 @@
-// e2e tests must not include these - hot fix for issue #2300
-if (process.env.TEST_TYPE !== "e2e") {
-	require("jest-preset-angular/setup-jest")
-}
+require("jest-preset-angular/setup-jest")
 
 const numberToLocaleString = Number.prototype.toLocaleString
 Number.prototype.toLocaleString = function (locale = "en-US") {
