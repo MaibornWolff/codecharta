c490e376f4b551a1529d192b231a5189f32bba25
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-11-10 22:28:51 +0100

    refactor: update babel-loader
    #2300

--- visualization/conf/setupJestUnit.ts
+++ visualization/conf/setupJestUnit.ts
@@ -1,4 +1,4 @@
-require("jest-preset-angular/setup-jest")
+import "jest-preset-angular/setup-jest"
 
 const numberToLocaleString = Number.prototype.toLocaleString
 Number.prototype.toLocaleString = function (locale = "en-US") {
