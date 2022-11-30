34620d65c66b40c77dc41b456e23d878ffbfa69e
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2022-11-30 16:55:26 +0100

    Add metric name translator to metricgardener to uniform common metric names rloc and loc

--- analysis/import/MetricGardenerImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/metricgardenerimporter/AttributeDescriptors.kt
+++ analysis/import/MetricGardenerImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/metricgardenerimporter/AttributeDescriptors.kt
@@ -6,12 +6,12 @@ fun getAttributeDescriptors(): Map<String, AttributeDescriptor> {
     val descriptors = mutableMapOf<String, AttributeDescriptor>()
     val npmLink = "https://www.npmjs.com/package/metric-gardener"
 
-    descriptors["mcc"] = AttributeDescriptor(description = "Cyclomatic complexity", link = npmLink)
-    descriptors["functions"] = AttributeDescriptor(description = "Number of functions", link = npmLink)
-    descriptors["classes"] = AttributeDescriptor(description = "Number of classes", link = npmLink)
-    descriptors["lines_of_code"] = AttributeDescriptor(description = "Lines of code", link = npmLink)
-    descriptors["comment_lines "] = AttributeDescriptor(description = "Number of code lines with comments", link = npmLink)
-    descriptors["real_lines_of_code"] = AttributeDescriptor(description = "Real lines of code", link = npmLink)
+    descriptors["mcc"] = AttributeDescriptor(title = "Maximum Cyclic Complexity", description = "Maximum cyclic complexity", link = npmLink)
+    descriptors["functions"] = AttributeDescriptor(title = "Number of Functions", description = "Number of functions", link = npmLink)
+    descriptors["classes"] = AttributeDescriptor(title = "Number of Classes", description = "Number of classes", link = npmLink)
+    descriptors["lines_of_code"] = AttributeDescriptor(title = "Lines of Code", description = "Lines of code including empty lines and comments", link = npmLink)
+    descriptors["comment_lines"] = AttributeDescriptor(title = "Comment Lines", description = "Number of lines containing either comment or commented-out code", link = npmLink)
+    descriptors["real_lines_of_code"] = AttributeDescriptor(title = "Real Lines of Code", description = "Number of physical lines that contain at least one character which is neither a whitespace nor a tabulation nor part of a comment", link = npmLink)
 
     return descriptors.toMap()
 }
