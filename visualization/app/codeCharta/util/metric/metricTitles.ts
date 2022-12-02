fc4b9063cd1ba9ba3bebfecc829dbb5a6d3125a3
BenediktMehl <b.mehl14@outlook.de>
2022-12-02 09:29:56 +0100

    Refactor: metricTitles cases to fit to new attribute descriptors #1066

--- visualization/app/codeCharta/util/metric/metricTitles.ts
+++ visualization/app/codeCharta/util/metric/metricTitles.ts
@@ -3,17 +3,17 @@ This file contains descriptions of the metrics
 */
 
 export const metricTitles: Map<string, string> = new Map([
-	["loc", "lines of code"],
-	["rloc", "real lines of code"],
-	["comment_lines", "number of code lines with comments"],
-	["mcc", "cyclomatic complexity"],
-	["avgCommits", "average number of commits from this file"],
-	["functions", "number of functions"],
-	["pairingRate", "pairing rate of selected building"],
+	["loc", "Lines of Code"],
+	["rloc", "Real Lines of Code"],
+	["comment_lines", "Number of Code Lines with Comments"],
+	["mcc", "Cyclomatic Complexity"],
+	["avgCommits", "Average Number of Commits from this file"],
+	["functions", "Number of Functions"],
+	["pairingRate", "Pairing Rate of selected building"],
 	["unary", "-"],
-	["line_coverage", "number of code lines covered by tests"],
-	["sonar_code_smells", "number of smells Sonar has identified"],
-	["avg_code_churn", "average number of lines added or removed from this file"],
-	["number_of_authors", "number of authors that have edited this file"],
-	["statements", "number of statements"]
+	["line_coverage", "Number of Code Lines covered by tests"],
+	["sonar_code_smells", "Number of Smells Sonar has identified"],
+	["avg_code_churn", "Average Number of Lines added or removed from this file"],
+	["number_of_authors", "Number of Authors that have edited this file"],
+	["statements", "Number of Statements"]
 ])
