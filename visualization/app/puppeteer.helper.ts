b9d6a1f4746f49e2878ec8113a7c158970548c80
MW-Friedrich <108457762+MW-Friedrich@users.noreply.github.com>
2022-12-08 12:37:06 +0100

    Chore: remove now unused method from puppeteer helper

--- visualization/app/puppeteer.helper.ts
+++ visualization/app/puppeteer.helper.ts
@@ -1,25 +1,16 @@
 import path from "path"
 import { setDefaultOptions } from "expect-puppeteer"
-import { ConsoleMessage } from "puppeteer"
 
 export const CC_URL = `file:${path.join(__dirname, "../dist/webpack/index.html")}`
 // expect-puppeteer toClick timeout does not work it might be the reason of flaky tests,
 // changed default options globally
 // [toClick issue](https://github.com/smooth-code/jest-puppeteer/issues/202)
 setDefaultOptions({ timeout: 6000 })
+
 export async function goto(url = CC_URL) {
 	await page.goto(url)
 	await page.waitForSelector("#loading-gif-file")
 	await page.waitForSelector("#loading-gif-file", { visible: false })
-}
-
-export async function enableConsole() {
-	page.on("console", async (message: ConsoleMessage) => {
-		const data = await Promise.all(message.args().map(async argument => argument.jsonValue()))
-		// @ts-ignore
-		// eslint-disable-next-line no-console
-		console[message._type](...data)
-	})
 }
 
 export const clickButtonOnPageElement = async (selectorString: string, expectToClickOptions?) => {
