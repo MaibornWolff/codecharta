486f65d2ca2bd7a667627195cd713716809ee03c
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2022-12-23 14:57:07 +0100

    Refactor first half of the model from spek to junit

--- analysis/model/src/test/kotlin/de/maibornwolff/codecharta/model/TreeTest.kt
+++ analysis/model/src/test/kotlin/de/maibornwolff/codecharta/model/TreeTest.kt
@@ -1,89 +1,100 @@
 package de.maibornwolff.codecharta.model
 
 import de.maibornwolff.codecharta.model.TreeCreator.createTree
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.hasItem
-import org.hamcrest.Matchers.`is`
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import kotlin.test.assertFailsWith
 
-class TreeTest : Spek({
-    describe("a tree of depth 0") {
+class TreeTest{
+
+    @Nested
+    @DisplayName("a tree of depth 0")
+    inner class ZeroDepthTree {
         val tree = createTree()
 
-        it("nodes should be only the tree itself") {
+        @Test
+        fun `nodes should be only the tree itself`() {
             val nodes = tree.nodes
-            assertThat(nodes.size, `is`(1))
-            assertThat(nodes.keys, hasItem(Path.TRIVIAL))
-            assertThat(nodes.values, hasItem(tree))
+            assertThat(nodes.size).isEqualTo(1)
+            assertThat(nodes.keys).contains(Path.TRIVIAL)
+            assertThat(nodes.values).contains(tree.asTreeNode())
         }
 
-        it("leaves should be only the tree itself") {
+        @Test
+        fun `leaves should be only the tree itself`() {
             val leaves = tree.leaves
-            assertThat(leaves.size, `is`(1))
-            assertThat(leaves.keys, hasItem(Path.TRIVIAL))
-            assertThat(leaves.values, hasItem(tree))
+            assertThat(leaves.size).isEqualTo(1)
+            assertThat(leaves.keys).contains(Path.TRIVIAL)
+            assertThat(leaves.values).contains(tree.asTreeNode())
         }
 
-        it("getNodeBy should return same tree on trivial path") {
-            assertThat(tree.getNodeBy(Path.TRIVIAL), `is`(tree))
+        @Test
+        fun `getNodeBy should return same tree on trivial path`() {
+            assertThat(tree.getNodeBy(Path.TRIVIAL)).isEqualTo(tree)
         }
 
-        it("getNodeBy should throw exception on path not contained in tree") {
+        @Test
+        fun `getNodeBy should throw exception on path not contained in tree`() {
             assertFailsWith(NoSuchElementException::class) {
                 tree.getNodeBy(Path("nonexistingpath"))
             }
         }
-
-        context("when merging") {
-            tree.merge(listOf(createTree().asTreeNode()))
-
-            it("should do nothing by default") {
-                val leaves = tree.leaves
-
-                assertThat(leaves.size, `is`(1))
-                assertThat(leaves.keys, hasItem(Path.TRIVIAL))
-                assertThat(leaves.values, hasItem(tree))
-            }
-        }
     }
 
-    describe("a tree of depth 1") {
+    @Test
+    fun `merging should do nothing by default`() {
+        val tree = createTree()
+        tree.merge(listOf(createTree().asTreeNode()))
+        val leaves = tree.leaves
+
+        assertThat(leaves).hasSize(1)
+        assertThat(leaves.keys).contains(Path.TRIVIAL)
+        assertThat(leaves.values).contains(tree.asTreeNode())
+    }
+
+    @Nested
+    @DisplayName("a tree of depth 1")
+    inner class OneDepthTree {
         val innerTree = createTree()
         val pathToInnerTree = Path("bla")
         val tree = createTree(pathToInnerTree, innerTree)
 
-        it("nodes should contain exactly itself and the subtree") {
+        @Test
+        fun `nodes should contain exactly itself and the subtree`() {
             val nodes = tree.nodes
-
-            assertThat(nodes.size, `is`(2))
-            assertThat(nodes.keys, hasItem(Path.TRIVIAL))
-            assertThat(nodes.keys, hasItem(pathToInnerTree))
-            assertThat(nodes.values, hasItem(tree))
-            assertThat(nodes.values, hasItem(innerTree))
+            assertThat(nodes).hasSize(2)
+            assertThat(nodes.keys).contains(Path.TRIVIAL)
+            assertThat(nodes.keys).contains(pathToInnerTree)
+            assertThat(nodes.values).contains(tree.asTreeNode())
+            assertThat(nodes.values).contains(innerTree.asTreeNode())
         }
 
-        it("leaves should return the inner tree") {
+        @Test
+        fun `leaves should return the inner tree`() {
             val leaves = tree.leaves
 
-            assertThat(leaves.size, `is`(1))
-            assertThat(leaves.keys, hasItem(pathToInnerTree))
-            assertThat(leaves.values, hasItem(innerTree))
+            assertThat(leaves).hasSize(1)
+            assertThat(leaves.keys).contains(pathToInnerTree)
+            assertThat(leaves.values).contains(innerTree.asTreeNode())
         }
 
-        it("getNodeBy should return same tree on trivial path") {
-            assertThat(tree.getNodeBy(Path.TRIVIAL), `is`(tree))
+        @Test
+        fun `getNodeBy should return same tree on trivial path`(){
+            assertThat(tree.getNodeBy(Path.TRIVIAL)).isEqualTo(tree)
         }
 
-        it("getNodeBy should return inner tree on pathToInnerTree") {
-            assertThat(tree.getNodeBy(pathToInnerTree), `is`(innerTree))
+        @Test
+        fun `getNodeBy should return inner tree on pathToInnerTree`() {
+            assertThat(tree.getNodeBy(pathToInnerTree)).isEqualTo(innerTree)
         }
 
-        it("getNodeBy should throw exception on path not contained in tree") {
+        @Test
+        fun `getNodeBy should throw exception on path not contained in tree`() {
             assertFailsWith(NoSuchElementException::class) {
                 tree.getNodeBy(Path("nonexistingpath"))
             }
         }
     }
-})
+}
--- analysis/model/src/test/kotlin/de/maibornwolff/codecharta/serialization/ProjectSerializerTest.kt
+++ analysis/model/src/test/kotlin/de/maibornwolff/codecharta/serialization/ProjectSerializerTest.kt
@@ -4,10 +4,9 @@ import de.maibornwolff.codecharta.model.Project
 import io.mockk.called
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.jupiter.api.Test
 import org.skyscreamer.jsonassert.JSONAssert
 import org.skyscreamer.jsonassert.JSONCompareMode
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
@@ -17,49 +16,49 @@ import kotlin.io.path.absolute
 import kotlin.io.path.createTempDirectory
 import kotlin.test.assertTrue
 
-class ProjectSerializerTest : Spek({
-
+class ProjectSerializerTest {
     val EXAMPLE_JSON_VERSION_1_3 = "example_api_version_1.3.cc.json"
     val tempDir = createTempDirectory()
     val filename = tempDir.absolute().toString() + "test.cc.json"
+    val project = mockk<Project>()
 
-    describe("ProjectSerializer") {
-        it("should serialize project") {
-            val jsonReader = this.javaClass.classLoader.getResourceAsStream(EXAMPLE_JSON_VERSION_1_3)!!.reader()
-            val expectedJsonString = this.javaClass.classLoader.getResource("example_api_version_1.3.cc.json")!!.readText()
-            val testProject = ProjectDeserializer.deserializeProject(jsonReader)
+    @Test
+    fun `should serialize project`() {
+        val jsonReader = this.javaClass.classLoader.getResourceAsStream(EXAMPLE_JSON_VERSION_1_3)!!.reader()
+        val expectedJsonString = this.javaClass.classLoader.getResource("example_api_version_1.3.cc.json")!!.readText()
+        val testProject = ProjectDeserializer.deserializeProject(jsonReader)
 
-            ProjectSerializer.serializeProject(testProject, FileOutputStream(filename), false)
+        ProjectSerializer.serializeProject(testProject, FileOutputStream(filename), false)
 
-            val testJsonString = File(filename).readText()
-            System.err.println(testJsonString)
-            System.err.println(expectedJsonString)
-            JSONAssert.assertEquals(testJsonString, expectedJsonString, JSONCompareMode.NON_EXTENSIBLE)
-        }
+        val testJsonString = File(filename).readText()
+        System.err.println(testJsonString)
+        System.err.println(expectedJsonString)
+        JSONAssert.assertEquals(testJsonString, expectedJsonString, JSONCompareMode.NON_EXTENSIBLE)
     }
 
-    describe("serializeToFileOrStream") {
-        val project = mockk<Project>()
-
-        it("should create a gz file") {
-            val mockStream = mockk<OutputStream>()
-            ProjectSerializer.serializeToFileOrStream(project, "test.cc.json", mockStream, true)
-            assertTrue { File("test.cc.json.gz").exists() }
-            File("test.cc.json.gz").delete()
-            verify { mockStream wasNot called }
-        }
-        it("should create a json file") {
-            val mockStream = mockk<OutputStream>()
-            ProjectSerializer.serializeToFileOrStream(project, "test.cc.json", mockStream, false)
-            assertTrue { File("test.cc.json").exists() }
-            File("test.cc.json").delete()
-            verify { mockStream wasNot called }
-        }
-        it("should write to stream") {
-            val stream = ByteArrayOutputStream()
-            ProjectSerializer.serializeToFileOrStream(project, "", stream, true)
-            val result = stream.toString(UTF_8)
-            assertTrue { result.startsWith("{") }
-        }
+    @Test
+    fun `serializeToFileOrStream should create a gz file`() {
+        val mockStream = mockk<OutputStream>()
+        ProjectSerializer.serializeToFileOrStream(project, "test.cc.json", mockStream, true)
+        assertTrue { File("test.cc.json.gz").exists() }
+        File("test.cc.json.gz").delete()
+        verify { mockStream wasNot called }
     }
-})
+
+    @Test
+    fun `serializeToFileOrStream should create a json file`() {
+        val mockStream = mockk<OutputStream>()
+        ProjectSerializer.serializeToFileOrStream(project, "test.cc.json", mockStream, false)
+        assertTrue { File("test.cc.json").exists() }
+        File("test.cc.json").delete()
+        verify { mockStream wasNot called }
+    }
+
+    @Test
+    fun `serializeToFileOrStream should write to stream`() {
+        val stream = ByteArrayOutputStream()
+        ProjectSerializer.serializeToFileOrStream(project, "", stream, true)
+        val result = stream.toString(UTF_8)
+        assertTrue { result.startsWith("{") }
+    }
+}
