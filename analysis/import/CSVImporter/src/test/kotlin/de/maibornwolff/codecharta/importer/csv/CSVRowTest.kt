c9bb5b23fbdc5f55ef5d8a385ea575b5b1a371b8
phanlezz <65733509+phanlezz@users.noreply.github.com>
2023-01-19 17:07:04 +0100

    Tech/3036/remove spek (#3202)
    * Refactor from spek / hamcrest to junit5 / assertj
    
    * Remove hamcrest assertions
    
    * Remove blank lines in gradle files
    
    * Remove spek2 references in gradle files
    
    * Remove hamcrest and hamkrest from gradle & test methods

--- analysis/import/CSVImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/csv/CSVRowTest.kt
+++ analysis/import/CSVImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/csv/CSVRowTest.kt
@@ -1,77 +1,75 @@
 package de.maibornwolff.codecharta.importer.csv
 
 import de.maibornwolff.codecharta.model.Path
-import org.hamcrest.CoreMatchers.hasItem
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.not
-import org.hamcrest.CoreMatchers.nullValue
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.hasSize
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import kotlin.test.assertFailsWith
 
 private const val PATH_SEPARATOR = '\\'
 
-class CSVRowTest : Spek({
+class CSVRowTest {
+    private val header = CSVHeader(arrayOf("head1", "head2", "head3", "path", "attrib", "attrib2", ""))
 
-    describe("Using a header with path column") {
-        val header = CSVHeader(arrayOf("head1", "head2", "head3", "path", "attrib", "attrib2", ""))
+    @Test
+    fun `path column header should be used as node name`() {
+        val nameExpectedFilenameMap = mapOf(
+            Pair("someNodeName", "someNodeName"),
+            Pair("someDir\\someName", "someName"),
+            Pair("someDir\\anotherDir\\anotherName", "anotherName")
+        )
 
-        it("name of node should be filename from this columnn") {
-            val nameExpectedFilenameMap = mapOf(
-                Pair("someNodeName", "someNodeName"),
-                Pair("someDir\\someName", "someName"),
-                Pair("someDir\\anotherDir\\anotherName", "anotherName")
-            )
-
-            for (name in nameExpectedFilenameMap.keys) {
-                val node = CSVRow(arrayOf("projectName", "blubb2", "blubb3", name), header, PATH_SEPARATOR).asNode()
-                assertThat(node.name, `is`(nameExpectedFilenameMap[name]))
-            }
-        }
-
-        it("path in Tree should be absolute file name from this column") {
-            val nameExpectedFolderWithFileMap = mapOf(
-                Pair("someNodeName", Path.TRIVIAL),
-                Pair("someDir\\someName", Path(listOf("someDir"))),
-                Pair("someDir\\anotherDir\\anotherName", Path(listOf("someDir", "anotherDir")))
-            )
-
-            for (name in nameExpectedFolderWithFileMap.keys) {
-                val path = CSVRow(arrayOf("projectName", "blubb2", "blubb3", name), header, PATH_SEPARATOR).pathInTree()
-                assertThat(path, `is`<Path>(nameExpectedFolderWithFileMap[name]))
-            }
-        }
-
-        it("should throw exception if no path column present") {
-            assertFailsWith(IllegalArgumentException::class) {
-                CSVRow(arrayOf("", ""), header, PATH_SEPARATOR)
-            }
-        }
-
-        it("should ignore columns if no attribute name in header") {
-            val rawRow = arrayOf<String?>("1", "2", "3", "file", "4", "5", "6", "7")
-            val node = CSVRow(rawRow, header, PATH_SEPARATOR).asNode()
-            assertThat(node.attributes.keys, hasSize(5))
-        }
-
-        it("should ignore column if not in row") {
-            val rawRow = arrayOf<String?>("blubb1", "blubb2", "blubb3", "path")
-            val node = CSVRow(rawRow, header, PATH_SEPARATOR).asNode()
-            assertThat(node.attributes.keys, not(hasItem("attrib")))
-        }
-
-        it("should have attribute for metric columns") {
-            val rawRow = arrayOf<String?>("3,2", "2", "3", "file")
-            val node = CSVRow(rawRow, header, PATH_SEPARATOR).asNode()
-            assertThat(node.attributes["head1"] as Double, `is`<Double>(3.2))
-        }
-
-        it("should have NO attribute for non-metric columns") {
-            val rawRow = arrayOf<String?>("bla", "2", "3", "file")
-            val node = CSVRow(rawRow, header, PATH_SEPARATOR).asNode()
-            assertThat(node.attributes["head1"], nullValue())
+        for (name in nameExpectedFilenameMap.keys) {
+            val node = CSVRow(arrayOf("projectName", "blubb2", "blubb3", name), header, PATH_SEPARATOR).asNode()
+            assertThat(node.name).isEqualTo(nameExpectedFilenameMap[name])
         }
     }
-})
+
+    @Test
+    fun `path in Tree should be absolute file name from column header`() {
+        val nameExpectedFolderWithFileMap = mapOf(
+            Pair("someNodeName", Path.TRIVIAL),
+            Pair("someDir\\someName", Path(listOf("someDir"))),
+            Pair("someDir\\anotherDir\\anotherName", Path(listOf("someDir", "anotherDir")))
+        )
+
+        for (name in nameExpectedFolderWithFileMap.keys) {
+            val path = CSVRow(arrayOf("projectName", "blubb2", "blubb3", name), header, PATH_SEPARATOR).pathInTree()
+            assertThat(path).isEqualTo(nameExpectedFolderWithFileMap[name])
+        }
+    }
+
+    @Test
+    fun `should throw exception if no path column present`() {
+        assertFailsWith(IllegalArgumentException::class) {
+            CSVRow(arrayOf("", ""), header, PATH_SEPARATOR)
+        }
+    }
+
+    @Test
+    fun `should ignore columns if no attribute name in header`() {
+        val rawRow = arrayOf<String?>("1", "2", "3", "file", "4", "5", "6", "7")
+        val node = CSVRow(rawRow, header, PATH_SEPARATOR).asNode()
+        assertThat(node.attributes.keys).hasSize(5)
+    }
+
+    @Test
+    fun `should ignore column if not in row`() {
+        val rawRow = arrayOf<String?>("blubb1", "blubb2", "blubb3", "path")
+        val node = CSVRow(rawRow, header, PATH_SEPARATOR).asNode()
+        assertThat(node.attributes.keys).doesNotContain("attrib")
+    }
+
+    @Test
+    fun `should have attribute for metric columns`() {
+        val rawRow = arrayOf<String?>("3,2", "2", "3", "file")
+        val node = CSVRow(rawRow, header, PATH_SEPARATOR).asNode()
+        assertThat(node.attributes["head1"] as Double).isEqualTo(3.2)
+    }
+
+    @Test
+    fun `should have NO attribute for non-metric columns`() {
+        val rawRow = arrayOf<String?>("bla", "2", "3", "file")
+        val node = CSVRow(rawRow, header, PATH_SEPARATOR).asNode()
+        assertThat(node.attributes["head1"]).isNull()
+    }
+}
--- analysis/model/src/test/kotlin/de/maibornwolff/codecharta/model/ProjectBuilderTest.kt
+++ analysis/model/src/test/kotlin/de/maibornwolff/codecharta/model/ProjectBuilderTest.kt
@@ -1,57 +1,44 @@
 package de.maibornwolff.codecharta.model
 
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers
-import org.hamcrest.Matchers.hasSize
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
-class ProjectBuilderTest : Spek({
+class ProjectBuilderTest {
 
-    describe("ProjectBuilder without root node") {
+    @Test
+    fun `without root inserting a new node should insert as child of root`() {
         val projectBuilder = ProjectBuilder()
+        val nodeForInsertion = MutableNode("someNode", NodeType.File)
+        projectBuilder.insertByPath(Path.trivialPath(), nodeForInsertion)
 
-        context("inserting new node") {
-            val nodeForInsertion = MutableNode("someNode", NodeType.File)
-            projectBuilder.insertByPath(Path.trivialPath(), nodeForInsertion)
-
-            it("has node as child of root") {
-                val root = projectBuilder.build().rootNode
-                assertThat(root.children, hasSize(1))
-                assertThat(root.children.toMutableList()[0], NodeMatcher.matchesNode(nodeForInsertion.toNode()))
-            }
-        }
+        val root = projectBuilder.build().rootNode
+        assertThat(root.children).hasSize(1)
+        assertThat(root.children.toMutableList()[0].toString()).isEqualTo(nodeForInsertion.toNode().toString())
     }
 
-    describe("ProjectBuilder with root node") {
+    @Test
+    fun `with root inserting a new node should create project with root`() {
         val root = MutableNode("root", NodeType.Folder)
         val projectBuilder = ProjectBuilder(listOf(root))
 
-        context("inserting new node") {
-            val nodeForInsertion = MutableNode("someNode", NodeType.File)
-            projectBuilder.insertByPath(Path.trivialPath(), nodeForInsertion)
+        val nodeForInsertion = MutableNode("someNode", NodeType.File)
+        projectBuilder.insertByPath(Path.trivialPath(), nodeForInsertion)
 
-            it("creates a Project with root node") {
-                val project = projectBuilder.build()
-                assertThat(project.rootNode, NodeMatcher.matchesNode(root.toNode()))
-                assertThat(root.children, hasSize(1))
-                assertThat(root.children.toMutableList()[0], Matchers.`is`(nodeForInsertion))
-            }
-        }
+        val project = projectBuilder.build()
+        assertThat(project.rootNode.toString()).isEqualTo(root.toNode().toString())
+        assertThat(root.children).hasSize(1)
+        assertThat(root.children.toMutableList()[0]).isEqualTo(nodeForInsertion)
     }
 
-    describe("ProjectBuilder with empty folders") {
+    @Test
+    fun `building with empty folders should filter them`() {
         val projectBuilder = ProjectBuilder()
         val nodeForInsertion = MutableNode("someNode", NodeType.Folder)
         projectBuilder.insertByPath(Path.trivialPath(), nodeForInsertion)
 
-        context("building") {
-            val project = projectBuilder.build()
+        val project = projectBuilder.build()
 
-            it("should filter empty folders") {
-                val root = project.rootNode
-                assertThat(root.children, hasSize(0))
-            }
-        }
+        val root = project.rootNode
+        assertThat(root.children).hasSize(0)
     }
-})
+}
