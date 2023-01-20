f9dc0e0e7ab573464420f39ed1f25c4245487a88
shaman-apprentice <torsten.knauf@gmx.de>
2023-01-20 16:59:50 +0100

    fix: format changelog as html again
    ref #3116

--- visualization/conf/customWebpack.config.js
+++ visualization/conf/customWebpack.config.js
@@ -1,27 +1,27 @@
+const RemarkHTML = require("remark-html")
+
 module.exports = config => {
-	config.module.rules.push({
-		test: /\.glsl$/,
-		use: ["webpack-glsl-loader"]
-	})
-
-	// TODO NG - typescript compilation should already take care of it (also of declaration within index.d.ts)
-	config.module.rules.push({
-		test: /\.md$/,
-
-		use: [
-			{ loader: "html-loader" }
-			// TODO NG - why remark-loader?
-			// {
-			// 	loader: "remark-loader",
-			// 	options: {
-			// 		removeFrontMatter: false,
-			// 		remarkOptions: {
-			// 			plugins: [RemarkHTML]
-			// 		}
-			// 	}
-			// }
-		]
-	})
+	config.module.rules.push(
+		{
+			test: /\.glsl$/,
+			use: ["webpack-glsl-loader"]
+		},
+		{
+			test: /\.md$/,
+			use: [
+				{ loader: "html-loader" },
+				{
+					loader: "remark-loader",
+					options: {
+						removeFrontMatter: false,
+						remarkOptions: {
+							plugins: [RemarkHTML]
+						}
+					}
+				}
+			]
+		}
+	)
 
 	return config
 }
