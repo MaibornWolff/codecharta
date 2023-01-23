e6001732da90804a7485814d7270351d2c1bd5a5
TorstenKnauf <Torsten.Knauf-extern@deutschebahn.com>
2023-01-23 16:32:23 +0100

    refactor: remove cast which is not necessary with new TS version
    ref #3116

--- visualization/app/codeCharta/services/version/version.service.spec.ts
+++ visualization/app/codeCharta/services/version/version.service.spec.ts
@@ -11,7 +11,7 @@ describe("versionService", () => {
 		mockedDialog = { open: jest.fn() } as unknown as MatLegacyDialog
 		mockedSetLocalStorageItem.mockReset()
 		versionService = new VersionService(mockedDialog)
-		;(versionService as any).version = "1.42.0"
+		versionService.version = "1.42.0"
 	})
 
 	describe("synchronizeLocalCodeChartaVersion", () => {
