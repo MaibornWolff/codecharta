4a5a3915ee89d0bce5fe81ef6a4772f794e01f74
Torsten Knauf <Torsten.Knauf@gmx.de>
2023-02-06 19:11:56 +0100

    Update visualization/app/codeCharta/util/EventEmitter.ts
    Co-authored-by: Ruben Bridgewater <ruben@bridgewater.de>

--- visualization/app/codeCharta/util/EventEmitter.ts
+++ visualization/app/codeCharta/util/EventEmitter.ts
@@ -10,7 +10,7 @@ export class EventEmitter<EventMap extends AbstractEventMap> {
 	}
 
 	emit<EventType extends keyof EventMap>(event: EventType, data?: Parameters<EventMap[EventType]>[0]) {
-		if (!this.listeners[event]) {
+		if (!this.listeners[event]?.length) {
 			return false
 		}
 
