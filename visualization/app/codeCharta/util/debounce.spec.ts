094d4875b9bb04a9321d3dcf802f6cc342f231bc
shaman-apprentice <torsten.knauf@gmx.de>
2023-02-06 18:51:05 +0100

    refactor:
    ref #3116

--- visualization/app/codeCharta/util/debounce.spec.ts
+++ visualization/app/codeCharta/util/debounce.spec.ts
@@ -13,12 +13,4 @@ describe("debounce", () => {
 		await wait(1)
 		expect(f).toHaveBeenCalledTimes(1)
 	})
-
-	it("should have type safe parameters", async () => {
-		const debouncedSquare = debounce((n: number) => n * n, 0)
-		// @ts-expect-error
-		debouncedSquare("2")
-		debouncedSquare(2)
-		await wait(0)
-	})
 })
