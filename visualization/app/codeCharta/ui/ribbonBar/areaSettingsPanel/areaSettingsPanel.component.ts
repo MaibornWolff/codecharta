389022d0d8f557c1f2dd899c878611b5357c7214
shaman-apprentice <torsten.knauf@gmx.de>
2023-03-07 20:49:12 +0100

    refactor: remove unused code from component
    ref #3225

--- visualization/app/codeCharta/ui/ribbonBar/areaSettingsPanel/areaSettingsPanel.component.ts
+++ visualization/app/codeCharta/ui/ribbonBar/areaSettingsPanel/areaSettingsPanel.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewEncapsulation } from "@angular/core"
 import { Store } from "../../../state/angular-redux/store"
 import { marginSelector } from "../../../state/store/dynamicSettings/margin/margin.selector"
 import { setMargin } from "../../../state/store/dynamicSettings/margin/margin.actions"
-import { dynamicMarginSelector } from "../../../state/store/appSettings/dynamicMargin/dynamicMargin.selector"
 import { setDynamicMargin } from "../../../state/store/appSettings/dynamicMargin/dynamicMargin.actions"
 import { MatCheckboxChange } from "@angular/material/checkbox"
 import { setEnableFloorLabels } from "../../../state/store/appSettings/enableFloorLabels/enableFloorLabels.actions"
@@ -19,21 +18,16 @@ export class AreaSettingsPanelComponent {
 	static DEBOUNCE_TIME = 400
 
 	margin$ = this.store.select(marginSelector)
-	dynamicMargin$ = this.store.select(dynamicMarginSelector)
 	enableFloorLabels$ = this.store.select(enableFloorLabelsSelector)
+
+	constructor(private store: Store) {}
+
+	setEnableFloorLabel(event: MatCheckboxChange) {
+		this.store.dispatch(setEnableFloorLabels(event.checked))
+	}
 
 	applyDebouncedMargin = debounce((margin: number) => {
 		this.store.dispatch(setMargin(margin))
 		this.store.dispatch(setDynamicMargin(false))
 	}, AreaSettingsPanelComponent.DEBOUNCE_TIME)
-
-	constructor(private store: Store) {}
-
-	setDynamicMargin($event: MatCheckboxChange) {
-		this.store.dispatch(setDynamicMargin($event.checked))
-	}
-
-	setEnableFloorLabel(event: MatCheckboxChange) {
-		this.store.dispatch(setEnableFloorLabels(event.checked))
-	}
 }
