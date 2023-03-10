bc69a7aaf1068539bb023a49e6dba4d380bdc61f
shaman-apprentice <torsten.knauf@gmx.de>
2023-03-10 16:43:46 +0100

    test: improve tests to catch the bug
    ref #3243

--- visualization/app/codeCharta/ui/ribbonBar/areaSettingsPanel/areaSettingsPanel.component.spec.ts
+++ visualization/app/codeCharta/ui/ribbonBar/areaSettingsPanel/areaSettingsPanel.component.spec.ts
@@ -1,6 +1,10 @@
+import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed"
 import { TestBed } from "@angular/core/testing"
+import { MatInputHarness } from "@angular/material/input/testing"
 import { render, screen } from "@testing-library/angular"
-import { Store } from "../../../state/store/store"
+import userEvent from "@testing-library/user-event"
+import { State } from "../../../state/angular-redux/state"
+import { CcState, Store } from "../../../state/store/store"
 import { AreaSettingsPanelComponent } from "./areaSettingsPanel.component"
 import { AreaSettingsPanelModule } from "./areaSettingsPanel.module"
 
@@ -12,8 +16,31 @@ describe("AreaSettingsPanelComponent", () => {
 		})
 	})
 
-	it("should display enableFloorLabels-checkbox", async () => {
-		await render(AreaSettingsPanelComponent, { excludeComponentDeclaration: true })
-		expect(screen.queryByText("Enable Floor Labels")).not.toBe(null)
+	it("should allow for change and resetting of 'margin', 'enable floor label' and 'invert area'", async () => {
+		const { fixture } = await render(AreaSettingsPanelComponent, { excludeComponentDeclaration: true })
+		const loader = TestbedHarnessEnvironment.loader(fixture)
+		const state = TestBed.inject(State)
+		const initialValues = extractRelatedValues(state.getValue())
+
+		const marginInput = await loader.getHarness(MatInputHarness.with({ ancestor: 'cc-slider[label="Margin"]' }))
+		await marginInput.setValue(String(initialValues.margin + 1))
+		await userEvent.click(await screen.findByText("Enable Floor Labels"))
+		await userEvent.click(await screen.findByText("Invert Area"))
+		const changedValues = extractRelatedValues(state.getValue())
+		expect(changedValues.margin).toBe(initialValues.margin + 1)
+		expect(changedValues.enableFloorLabels).toBe(!initialValues.enableFloorLabels)
+		expect(changedValues.invertArea).toBe(!initialValues.invertArea)
+
+		await userEvent.click(await screen.findByText("Reset area metric settings"))
+		const resetValues = extractRelatedValues(state.getValue())
+		expect(resetValues).toEqual(initialValues)
 	})
+
+	function extractRelatedValues(state: CcState) {
+		return {
+			margin: state.dynamicSettings.margin,
+			enableFloorLabels: state.appSettings.enableFloorLabels,
+			invertArea: state.appSettings.invertArea
+		}
+	}
 })
