17440e13260c9b5dca029963b054546c3c29b678
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2023-03-23 13:32:54 +0100

    Add tests for the sidebar that validates tooltips(titles) and edge selector info

--- visualization/app/codeCharta/ui/attributeSideBar/attributeSideBar.component.spec.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeSideBar.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from "@angular/core/testing"
 import { render } from "@testing-library/angular"
 import { klona } from "klona"
 import { expect } from "@jest/globals"
-import { TEST_NODE_FOLDER, TEST_NODE_LEAF } from "../../util/dataMocks"
+import { TEST_NODE_FOLDER, TEST_NODE_LEAF, TEST_ATTRIBUTE_DESCRIPTORS } from "../../util/dataMocks"
 import { selectedNodeSelector } from "../../state/selectors/selectedNode.selector"
 import { AttributeSideBarComponent } from "./attributeSideBar.component"
 import { AttributeSideBarModule } from "./attributeSideBar.module"
@@ -13,13 +13,19 @@ jest.mock("../../state/selectors/selectedNode.selector", () => ({
 }))
 const mockedSelectedNodeSelector = selectedNodeSelector as jest.Mock
 
+const selectedMetricNames = {
+	areaMetric: "a",
+	heightMetric: "a",
+	colorMetric: "a",
+	edgeMetric: "c"
+}
+
 jest.mock("../../state/selectors/primaryMetrics/primaryMetricNames.selector", () => ({
-	primaryMetricNamesSelector: jest.fn(() => ({
-		areaMetric: "a",
-		heightMetric: "a",
-		colorMetric: "a",
-		edgeMetric: "c"
-	}))
+	primaryMetricNamesSelector: jest.fn(() => selectedMetricNames)
+}))
+
+jest.mock("../../state/store/fileSettings/attributeDescriptors/attributeDescriptors.selector", () => ({
+	attributeDescriptorsSelector: jest.fn(() => TEST_ATTRIBUTE_DESCRIPTORS)
 }))
 
 describe("AttributeSideBarComponent", () => {
@@ -108,6 +114,52 @@ describe("AttributeSideBarComponent", () => {
 		const attributeTypeSelectorWithinSecondaryMetrics = container.querySelectorAll(
 			"cc-attribute-side-bar-secondary-metrics cc-attribute-type-selector"
 		)
+		expect(attributeTypeSelectorWithinSecondaryMetrics.length).toBe(1)
+	})
+
+	it("should contain 'no edge metric available' note", async () => {
+		mockedSelectedNodeSelector.mockImplementation(() => {
+			const node = klona(TEST_NODE_FOLDER)
+			node["children"] = [{}]
+			return node
+		})
+
+		selectedMetricNames.edgeMetric = undefined
+
+		const { container } = await render(AttributeSideBarComponent, { excludeComponentDeclaration: true })
+
+		const primaryMetricsWithoutEdgeMetric = container.querySelectorAll("cc-attribute-side-bar-primary-metrics td")
+		// if this turns out to be flaky, iterate through all results
+		expect(primaryMetricsWithoutEdgeMetric[3].textContent === "No edge metric available")
+	})
+
+	it("should contain primary/secondary metrics with title/tooltip attribute/metric descriptor information", async () => {
+		selectedMetricNames.areaMetric = "a"
+		selectedMetricNames.heightMetric = "b"
+		selectedMetricNames.colorMetric = "someColor"
+		selectedMetricNames.edgeMetric = "someEdge"
+
+		mockedSelectedNodeSelector.mockImplementation(() => {
+			const node = klona(TEST_NODE_FOLDER)
+			node["children"] = [{}]
+			node["attributes"] = { a: 3, b: 1, c: 4, someColor: 10 }
+			node["edgeAttributes"] = { someEdge: { incoming: 20, outgoing: 60 } }
+			return node
+		})
+
+		const { container } = await render(AttributeSideBarComponent, { excludeComponentDeclaration: true })
+
+		const attributeTypeSelectorWithinPrimaryMetrics = container.querySelectorAll("cc-attribute-side-bar-primary-metrics td")
+		const attributeTypeSelectorWithinSecondaryMetrics = container.querySelectorAll(
+			"cc-attribute-side-bar-secondary-metrics .metric-row"
+		)
+
+		expect(attributeTypeSelectorWithinPrimaryMetrics[0].getAttribute("title")).toBe("a_testTitle (a):\na_testDescription")
+		expect(attributeTypeSelectorWithinPrimaryMetrics[1].getAttribute("title")).toBe("b_testTitle (b)")
+		expect(attributeTypeSelectorWithinPrimaryMetrics[2].getAttribute("title")).toBe("someColor")
+		expect(attributeTypeSelectorWithinPrimaryMetrics[3].getAttribute("title")).toBe("someEdge")
+
+		expect(attributeTypeSelectorWithinSecondaryMetrics[0].getAttribute("title")).toBe("c:\nc_testDescription")
 		expect(attributeTypeSelectorWithinSecondaryMetrics.length).toBe(1)
 	})
 })
