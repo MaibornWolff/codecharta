7e9e63622c27e5494505b55cf5f8e26cd6f51e2d
Maria Hallmann <72517530+Hall-Ma@users.noreply.github.com>
2023-03-24 16:50:14 +0100

    Fix/accurate testing of dialogs (#3255)
    * Enable safari compatibility and add babel class-static-blcok package
    
    * Add Custom Config Dialog Component, Add note to custom config model, Add text field to create custom config for notes
    
    * Fix addCustomButton tests
    
    * Move note edit below title, change button behaviour, display note preview on custom config select screen, CSS magic for select screen layout
    
    * Update old tests
    
    * Add and fix remaining tests
    
    * Update Changelog
    
    * Apply suggested scss changes
    
    * Fix bottmo maring for note preview
    
    * Re-Enable previous click-on-title-to-apply-custom-config behaviour and add test
    
    * Remove angular legacy dialogs #3213
    
    * Fix view of custom config list, make pipe for truncate text more reusable, add little adjustments in css styling
    #3213
    
    * Adjust styling for editing custom config notes in dialog, restructure directories for custom config notes, adjust tests
    #3213
    
    * Adjust tests
    #3213
    
    * Update changelog
    #3213
    
    * Custom config note property is now optional, adjust tests and data mocks
    #3213
    
    * Update tests
    #3213
    
    * refactor: remove not needed mock
    
    ref #3213
    
    * Adapt tests to check open and closed dialogues
    
    ---------
    
    Co-authored-by: RomanenkoVladimir <vladimir.romanenko@maibornwolff.de>
    Co-authored-by: RomanenkoVladimir <volodymyr.romanenko@icloud.com>
    Co-authored-by: Cedrik Bormann <cedrik.bormann@maibornwolff.de>
    Co-authored-by: TorstenKnauf <Torsten.Knauf-extern@deutschebahn.com>

--- visualization/app/codeCharta/ui/customConfigs/addCustomConfigButton/addCustomConfigButton.component.spec.ts
+++ visualization/app/codeCharta/ui/customConfigs/addCustomConfigButton/addCustomConfigButton.component.spec.ts
@@ -7,6 +7,8 @@ import { CustomConfigHelper } from "../../../util/customConfigHelper"
 import { Vector3 } from "three"
 import { ThreeCameraService } from "../../codeMap/threeViewer/threeCamera.service"
 import { ThreeOrbitControlsService } from "../../codeMap/threeViewer/threeOrbitControls.service"
+import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed"
+import { MatDialogHarness } from "@angular/material/dialog/testing"
 
 describe("addCustomConfigButtonComponent", () => {
 	beforeEach(async () => {
@@ -14,25 +16,30 @@ describe("addCustomConfigButtonComponent", () => {
 			imports: [AddCustomConfigButtonModule],
 			providers: [
 				{ provide: ThreeCameraService, useValue: { camera: { position: new Vector3(0, 300, 1000) } } },
-				{
-					provide: ThreeOrbitControlsService,
-					useValue: { controls: { target: new Vector3(0, 0, 0) } }
-				}
+				{ provide: ThreeOrbitControlsService, useValue: { controls: { target: new Vector3(0, 0, 0) } } }
 			]
 		})
 	})
 
 	it("should let a user save a custom config", async () => {
-		const addCustomConfigSpy = jest.spyOn(CustomConfigHelper, "addCustomConfig")
-		await render(AddCustomConfigButtonComponent, { excludeComponentDeclaration: true })
 		const configName = "myCustomConfig"
 		const configNote = "My Custom Note"
+		const addCustomConfigSpy = jest.spyOn(CustomConfigHelper, "addCustomConfig")
+		const { fixture } = await render(AddCustomConfigButtonComponent, { excludeComponentDeclaration: true })
+		const loader = TestbedHarnessEnvironment.documentRootLoader(fixture)
+		let currentlyOpenedDialogs = await loader.getAllHarnesses(MatDialogHarness)
+		expect(currentlyOpenedDialogs.length).toBe(0)
 
 		await userEvent.click(screen.getByRole("button"))
+		currentlyOpenedDialogs = await loader.getAllHarnesses(MatDialogHarness)
+		expect(currentlyOpenedDialogs.length).toBe(1)
+
 		await userEvent.type(screen.getAllByRole("textbox")[0], configName)
 		await userEvent.type(screen.getAllByRole("textbox")[1], configNote)
 		await userEvent.click(screen.getByRole("button", { name: "ADD" }))
+		currentlyOpenedDialogs = await loader.getAllHarnesses(MatDialogHarness)
 
+		expect(currentlyOpenedDialogs.length).toBe(0)
 		expect(addCustomConfigSpy).toHaveBeenCalledTimes(1)
 	})
 })
