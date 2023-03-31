4554f1796afcfd60c34c0c2c44f671ec612b160a
shaman-apprentice <torsten.knauf@gmx.de>
2023-03-31 16:17:04 +0200

    refactor: WIP fix initial color reset
    ref #3250

--- visualization/app/codeCharta/state/store/dynamicSettings/colorRange/colorRange.reducer.ts
+++ visualization/app/codeCharta/state/store/dynamicSettings/colorRange/colorRange.reducer.ts
@@ -3,7 +3,6 @@ import { setColorRange } from "./colorRange.actions"
 import { ColorRange } from "../../../../codeCharta.model"
 
 export const colorRange = createReducer(
-	// TODO this PR maybe 0, 0 better default?
 	{ from: null, to: null } as ColorRange,
 	on(setColorRange, (state, payload) => ({ ...state, ...payload.value }))
 )
--- visualization/app/codeCharta/state/store/dynamicSettings/colorRange/resetColorRange.effect.ts
+++ visualization/app/codeCharta/state/store/dynamicSettings/colorRange/resetColorRange.effect.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core"
 import { Store } from "@ngrx/store"
 import { Actions, createEffect, ofType } from "@ngrx/effects"
-import { map, withLatestFrom } from "rxjs"
+import { map, skip, switchMap, take } from "rxjs"
 import { selectedColorMetricDataSelector } from "../../../selectors/accumulatedData/metricData/selectedColorMetricData.selector"
 import { calculateInitialColorRange } from "./calculateInitialColorRange"
 import { setColorRange } from "./colorRange.actions"
@@ -10,22 +10,15 @@ import { State } from "../../../../codeCharta.model"
 
 @Injectable()
 export class ResetColorRangeEffect {
-	private resetActions$ = this.actions$.pipe(
-		ofType(...fileActions)
-		// TODO or case
-		// filter action =>
-		// 	isActionOfType(action.type, FilesSelectionActions) ||
-		// 	(isAction<SetColorRangeAction>(action, ColorRangeActions.SET_COLOR_RANGE) &&
-		// 		action.payload.from === null &&
-		// 		action.payload.to === null)
-	)
-
 	constructor(private actions$: Actions, private store: Store<State>) {}
 
 	resetColorRange$ = createEffect(() =>
-		this.resetActions$.pipe(
-			withLatestFrom(this.store.select(selectedColorMetricDataSelector)),
-			map(([, selectedColorMetricData]) => setColorRange({ value: calculateInitialColorRange(selectedColorMetricData) }))
+		this.actions$.pipe(
+			ofType(...fileActions),
+			switchMap(() => this.store.select(selectedColorMetricDataSelector).pipe(skip(1), take(1))),
+			map(selectedColorMetricData => setColorRange({ value: calculateInitialColorRange(selectedColorMetricData) }))
 		)
 	)
+
+	// todo { from: null, to: null } := reset
 }
