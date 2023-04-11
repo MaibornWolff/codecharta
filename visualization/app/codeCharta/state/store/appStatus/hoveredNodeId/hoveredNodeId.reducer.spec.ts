62d7d5c0f7322b8352a1f17d232937e055907c4b
shaman-apprentice <torsten.knauf@gmx.de>
2023-04-12 00:03:30 +0200

    test: adjust some more store tests
    ref #3250

--- visualization/app/codeCharta/state/store/appStatus/hoveredNodeId/hoveredNodeId.reducer.spec.ts
+++ visualization/app/codeCharta/state/store/appStatus/hoveredNodeId/hoveredNodeId.reducer.spec.ts
@@ -1,12 +1,8 @@
-import { setHoveredNodeId, SetHoveredNodeIdAction, SetHoveredNodeIdPayload } from "./hoveredNodeId.actions"
+import { setHoveredNodeId } from "./hoveredNodeId.actions"
 import { hoveredNodeId } from "./hoveredNodeId.reducer"
 
 describe("hoveredNodeId", () => {
-	it("should be initialized to null", () => {
-		expect(hoveredNodeId(undefined as SetHoveredNodeIdPayload, {} as SetHoveredNodeIdAction)).toBe(null)
-	})
-
 	it("should set hovered node id", () => {
-		expect(hoveredNodeId(null, setHoveredNodeId(42))).toBe(42)
+		expect(hoveredNodeId(null, setHoveredNodeId({ value: 42 }))).toBe(42)
 	})
 })
--- visualization/app/codeCharta/state/store/files/files.reducer.spec.ts
+++ visualization/app/codeCharta/state/store/files/files.reducer.spec.ts
@@ -1,8 +1,5 @@
 import {
 	addFile,
-	defaultFiles,
-	FilesAction,
-	FilesSelectionActions,
 	invertStandard,
 	removeFile,
 	setAll,
@@ -15,52 +12,32 @@ import {
 	switchReferenceAndComparison
 } from "./files.actions"
 import { TEST_DELTA_MAP_A, TEST_DELTA_MAP_B } from "../../../util/dataMocks"
-import files from "./files.reducer"
 import { isDeltaState, isPartialState } from "../../../model/files/files.helper"
 import { FileSelectionState, FileState } from "../../../model/files/files"
 import { clone } from "../../../util/clone"
-import { isActionOfType } from "../../../util/arrayHelper"
+import { files } from "./files.reducer"
 
 describe("files", () => {
 	let state: FileState[] = []
 
 	beforeEach(() => {
-		state = []
-		state = files(state, addFile(TEST_DELTA_MAP_A))
-		state = files(state, addFile(TEST_DELTA_MAP_B))
-	})
-
-	describe("Default State", () => {
-		it("should initialize the default state", () => {
-			const result = files(undefined, {} as FilesAction)
-
-			expect(result).toEqual(defaultFiles)
-		})
+		state = files([], addFile({ file: TEST_DELTA_MAP_A }))
+		state = files(state, addFile({ file: TEST_DELTA_MAP_B }))
 	})
 
 	describe("Action: SET_FILES", () => {
 		it("should set new files", () => {
 			const newFiles: FileState[] = [{ file: TEST_DELTA_MAP_A, selectedAs: FileSelectionState.Partial }]
 
-			const result = files(state, setFiles(newFiles))
+			const result = files(state, setFiles({ value: newFiles }))
 
 			expect(result).toEqual(newFiles)
-		})
-
-		it("should set default files", () => {
-			const result = files(state, setFiles())
-
-			expect(result).toEqual(defaultFiles)
-		})
-
-		it("should be a file selection action, as file selections changes when all files are set", () => {
-			expect(isActionOfType(setFiles().type, FilesSelectionActions)).toBe(true)
 		})
 	})
 
 	describe("Action: SET_DELTA", () => {
 		it("should select a file as reference and another as comparison", () => {
-			const result = files(state, setDelta(TEST_DELTA_MAP_A, TEST_DELTA_MAP_B))
+			const result = files(state, setDelta({ referenceFile: TEST_DELTA_MAP_A, comparisonFile: TEST_DELTA_MAP_B }))
 
 			expect(isDeltaState(result)).toBeTruthy()
 		})
@@ -68,7 +45,7 @@ describe("files", () => {
 
 	describe("Action: SET_STANDARD", () => {
 		it("should select two files to view in multiple mode", () => {
-			const result = files(state, setStandard([TEST_DELTA_MAP_A, TEST_DELTA_MAP_B]))
+			const result = files(state, setStandard({ files: [TEST_DELTA_MAP_A, TEST_DELTA_MAP_B] }))
 
 			expect(isPartialState(result)).toBeTruthy()
 		})
@@ -76,7 +53,10 @@ describe("files", () => {
 
 	describe("Action: SET_STANDARD_BY_NAMES", () => {
 		it("should select two files by name to view in multiple mode", () => {
-			const result = files(state, setStandardByNames([TEST_DELTA_MAP_A.fileMeta.fileName, TEST_DELTA_MAP_B.fileMeta.fileName]))
+			const result = files(
+				state,
+				setStandardByNames({ fileNames: [TEST_DELTA_MAP_A.fileMeta.fileName, TEST_DELTA_MAP_B.fileMeta.fileName] })
+			)
 
 			expect(isPartialState(result)).toBeTruthy()
 		})
@@ -84,7 +64,7 @@ describe("files", () => {
 
 	describe("Action: REMOVE_FILE", () => {
 		it("should remove a file", () => {
-			const result = files(state, removeFile(TEST_DELTA_MAP_A.fileMeta.fileName))
+			const result = files(state, removeFile({ fileName: TEST_DELTA_MAP_A.fileMeta.fileName }))
 
 			expect(result[0].file).toEqual(TEST_DELTA_MAP_B)
 			expect(result.length).toBe(1)
@@ -92,7 +72,7 @@ describe("files", () => {
 
 		it("should select first file as partial when there is no other file selected", () => {
 			state[1].selectedAs = FileSelectionState.None
-			const result = files(state, removeFile(state[0].file.fileMeta.fileName))
+			const result = files(state, removeFile({ fileName: state[0].file.fileMeta.fileName }))
 			expect(result.length).toBe(1)
 			expect(result[0].selectedAs).toBe(FileSelectionState.Partial)
 		})
@@ -112,7 +92,7 @@ describe("files", () => {
 		it("should set delta reference file", () => {
 			state[0].selectedAs = FileSelectionState.Partial
 			state[1].selectedAs = FileSelectionState.Partial
-			const result = files(state, setDeltaReference(state[0].file))
+			const result = files(state, setDeltaReference({ file: state[0].file }))
 			expect(result[0].selectedAs).toBe(FileSelectionState.Reference)
 			expect(result[1].selectedAs).toBe(FileSelectionState.None)
 		})
@@ -122,7 +102,7 @@ describe("files", () => {
 			state[1].selectedAs = FileSelectionState.Comparison
 			state[2] = { file: clone(state[1].file), selectedAs: FileSelectionState.None }
 			state[2].file.fileMeta.fileChecksum += "1"
-			const result = files(state, setDeltaReference(state[2].file))
+			const result = files(state, setDeltaReference({ file: state[2].file }))
 			expect(result[0].selectedAs).toBe(FileSelectionState.None)
 			expect(result[1].selectedAs).toBe(FileSelectionState.Comparison)
 			expect(result[2].selectedAs).toBe(FileSelectionState.Reference)
@@ -133,7 +113,7 @@ describe("files", () => {
 		it("should set delta comparison file", () => {
 			state[0].selectedAs = FileSelectionState.Partial
 			state[1].selectedAs = FileSelectionState.Reference
-			const result = files(state, setDeltaComparison(state[0].file))
+			const result = files(state, setDeltaComparison({ file: state[0].file }))
 			expect(result[0].selectedAs).toBe(FileSelectionState.Comparison)
 			expect(result[1].selectedAs).toBe(FileSelectionState.Reference)
 		})
@@ -143,7 +123,7 @@ describe("files", () => {
 			state[1].selectedAs = FileSelectionState.Comparison
 			state[2] = { file: clone(state[1].file), selectedAs: FileSelectionState.None }
 			state[2].file.fileMeta.fileChecksum += "1"
-			const result = files(state, setDeltaComparison(state[2].file))
+			const result = files(state, setDeltaComparison({ file: state[2].file }))
 			expect(result[0].selectedAs).toBe(FileSelectionState.Reference)
 			expect(result[1].selectedAs).toBe(FileSelectionState.None)
 			expect(result[2].selectedAs).toBe(FileSelectionState.Comparison)
