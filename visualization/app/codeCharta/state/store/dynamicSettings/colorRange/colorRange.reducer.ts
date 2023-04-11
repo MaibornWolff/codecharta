3e403fbc0bd839ecfe0a2d01c625e59029b4d2b2
shaman-apprentice <torsten.knauf@gmx.de>
2023-04-11 22:14:23 +0200

    refactor: inline return
    ref #3250

--- visualization/app/codeCharta/state/store/dynamicSettings/colorRange/colorRange.reducer.ts
+++ visualization/app/codeCharta/state/store/dynamicSettings/colorRange/colorRange.reducer.ts
@@ -5,7 +5,5 @@ import { ColorRange } from "../../../../codeCharta.model"
 export const defaultColorRange: ColorRange = { from: 0, to: 0 }
 export const colorRange = createReducer(
 	defaultColorRange,
-	on(setColorRange, (state, action) => {
-		return { ...state, ...action.value }
-	})
+	on(setColorRange, (state, action) => ({ ...state, ...action.value }))
 )
