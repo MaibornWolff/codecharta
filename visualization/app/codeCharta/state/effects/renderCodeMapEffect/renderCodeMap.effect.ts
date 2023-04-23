874a36ac8228c9f2881a96df7c62492983938533
shaman-apprentice <torsten.knauf@gmx.de>
2023-04-23 11:03:51 +0200

    revert: be more explicit about loading indicators
    ref #3250

--- visualization/app/codeCharta/state/effects/renderCodeMapEffect/renderCodeMap.effect.ts
+++ visualization/app/codeCharta/state/effects/renderCodeMapEffect/renderCodeMap.effect.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core"
 import { Actions, createEffect, ofType } from "@ngrx/effects"
 import { Store } from "@ngrx/store"
-import { asyncScheduler, combineLatest, filter, map, share, tap, throttleTime } from "rxjs"
+import { asyncScheduler, combineLatest, filter, share, tap, throttleTime } from "rxjs"
 import { CcState } from "../../../codeCharta.model"
 import { CodeMapRenderService } from "../../../ui/codeMap/codeMap.render.service"
 import { ThreeRendererService } from "../../../ui/codeMap/threeViewer/threeRenderer.service"
@@ -26,26 +26,32 @@ export class RenderCodeMapEffect {
 
 	private actionsRequiringRender$ = this.actions$.pipe(ofType(...actionsRequiringRerender))
 
-	renderCodeMap$ = createEffect(() =>
-		combineLatest([this.store.select(accumulatedDataSelector), this.actionsRequiringRender$]).pipe(
-			filter(([accumulatedData]) => Boolean(accumulatedData.unifiedMapNode)),
-			throttleTime(maxFPS, asyncScheduler, { leading: false, trailing: true }),
-			tap(([accumulatedData, action]) => {
-				this.codeMapRenderService.render(accumulatedData.unifiedMapNode)
-				this.threeRendererService.render()
-				if (action.type === setScaling.type) {
-					this.codeMapRenderService.scaleMap()
-				}
-			}),
-			map(() => setIsLoadingMap({ value: false })),
-			share()
-		)
+	renderCodeMap$ = createEffect(
+		() =>
+			combineLatest([this.store.select(accumulatedDataSelector), this.actionsRequiringRender$]).pipe(
+				filter(([accumulatedData]) => Boolean(accumulatedData.unifiedMapNode)),
+				throttleTime(maxFPS, asyncScheduler, { leading: false, trailing: true }),
+				tap(([accumulatedData, action]) => {
+					this.codeMapRenderService.render(accumulatedData.unifiedMapNode)
+					this.threeRendererService.render()
+					if (action.type === setScaling.type) {
+						this.codeMapRenderService.scaleMap()
+					}
+				}),
+				share()
+			),
+		{ dispatch: false }
 	)
 
-	removeLoadingIndicatorAfterRender$ = createEffect(() =>
-		this.renderCodeMap$.pipe(
-			filter(() => !this.uploadFilesService.isUploading),
-			map(() => setIsLoadingFile({ value: false }))
-		)
+	removeLoadingIndicatorAfterRender$ = createEffect(
+		() =>
+			this.renderCodeMap$.pipe(
+				filter(() => !this.uploadFilesService.isUploading),
+				tap(() => {
+					this.store.dispatch(setIsLoadingFile({ value: false }))
+					this.store.dispatch(setIsLoadingMap({ value: false }))
+				})
+			),
+		{ dispatch: false }
 	)
 }
