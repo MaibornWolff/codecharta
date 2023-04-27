c58f749c3120951f5b70022969bdde6d57131781
Phanlezz <65733509+phanlezz@users.noreply.github.com>
2023-04-27 13:00:14 +0200

    Refactor attributeSidebar's attributeDescriptor Tooltips

--- visualization/app/codeCharta/ui/attributeSideBar/attributeSideBarPrimaryMetrics/attributeSideBarPrimaryMetrics.component.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeSideBarPrimaryMetrics/attributeSideBarPrimaryMetrics.component.ts
@@ -4,6 +4,8 @@ import { Observable } from "rxjs"
 import { Store } from "../../../state/angular-redux/store"
 import { showAttributeTypeSelectorSelector } from "../util/showAttributeTypeSelector.selector"
 import { PrimaryMetrics, primaryMetricsSelector } from "../../../state/selectors/primaryMetrics/primaryMetrics.selector"
+import { AttributeDescriptors } from "../../../codeCharta.model"
+import { attributeDescriptorsSelector } from "../../../state/store/fileSettings/attributeDescriptors/attributeDescriptors.selector"
 
 @Component({
 	selector: "cc-attribute-side-bar-primary-metrics",
@@ -13,9 +15,11 @@ import { PrimaryMetrics, primaryMetricsSelector } from "../../../state/selectors
 export class AttributeSideBarPrimaryMetricsComponent {
 	primaryMetrics$: Observable<PrimaryMetrics>
 	showAttributeTypeSelector$: Observable<boolean>
+	attributeDescriptors: AttributeDescriptors
 
 	constructor(store: Store) {
 		this.primaryMetrics$ = store.select(primaryMetricsSelector)
 		this.showAttributeTypeSelector$ = store.select(showAttributeTypeSelectorSelector)
+		store.select(attributeDescriptorsSelector).subscribe(descriptors => (this.attributeDescriptors = descriptors))
 	}
 }
