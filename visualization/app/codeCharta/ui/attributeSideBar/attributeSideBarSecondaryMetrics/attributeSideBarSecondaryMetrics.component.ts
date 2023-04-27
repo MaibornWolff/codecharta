04140647f1d1617d56ba68f47268658b7a9c9c90
Phanlezz <65733509+phanlezz@users.noreply.github.com>
2023-04-27 14:01:56 +0200

    Refactor attributeSidebar's attributeDescriptor Tooltips

--- visualization/app/codeCharta/ui/attributeSideBar/attributeSideBarSecondaryMetrics/attributeSideBarSecondaryMetrics.component.ts
+++ visualization/app/codeCharta/ui/attributeSideBar/attributeSideBarSecondaryMetrics/attributeSideBarSecondaryMetrics.component.ts
@@ -6,6 +6,8 @@ import { Metric } from "../util/metric"
 import { showAttributeTypeSelectorSelector } from "../util/showAttributeTypeSelector.selector"
 import { showDeltaValueSelector } from "../util/showDeltaValueSelector"
 import { secondaryMetricsSelector } from "./secondaryMetrics.selector"
+import { attributeDescriptorsSelector } from "../../../state/store/fileSettings/attributeDescriptors/attributeDescriptors.selector"
+import { AttributeDescriptors } from "../../../codeCharta.model"
 
 @Component({
 	selector: "cc-attribute-side-bar-secondary-metrics",
@@ -16,10 +18,12 @@ export class AttributeSideBarSecondaryMetricsComponent {
 	secondaryMetrics$: Observable<Metric[]>
 	showAttributeTypeSelector$: Observable<boolean>
 	showDeltaValue$: Observable<boolean>
+	attributeDescriptors: AttributeDescriptors
 
 	constructor(store: Store) {
 		this.secondaryMetrics$ = store.select(secondaryMetricsSelector)
 		this.showAttributeTypeSelector$ = store.select(showAttributeTypeSelectorSelector)
 		this.showDeltaValue$ = store.select(showDeltaValueSelector)
+		store.select(attributeDescriptorsSelector).subscribe(descriptors => (this.attributeDescriptors = descriptors))
 	}
 }
