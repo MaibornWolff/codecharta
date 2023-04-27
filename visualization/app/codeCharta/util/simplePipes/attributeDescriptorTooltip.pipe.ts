22d3d1f1d902049c1bf16b8d7c00069054986bb9
Phanlezz <65733509+phanlezz@users.noreply.github.com>
2023-04-27 11:13:38 +0200

    Add formatting for attributeDescriptor-based tooltip in metricChoosers and fallback

--- visualization/app/codeCharta/util/simplePipes/attributeDescriptorTooltip.pipe.ts
+++ visualization/app/codeCharta/util/simplePipes/attributeDescriptorTooltip.pipe.ts
@@ -1,12 +1,20 @@
 import { Pipe, PipeTransform } from "@angular/core"
 import { AttributeDescriptor } from "../../codeCharta.model"
+import { metricTitles } from "../metric/metricTitles"
 
 @Pipe({ name: "attributeDescriptorTooltip" })
 export class AttributeDescriptorTooltipPipe implements PipeTransform {
 	transform(attributeDescriptor: AttributeDescriptor, key: string): string {
 		if (attributeDescriptor == null) {
-			return ""
+			return metricTitles.get(key) ?? ""
 		}
-		return key + attributeDescriptor.description
+		const title: string = attributeDescriptor.title ? `${attributeDescriptor.title} (${key})` : `${key}`
+
+		let subtitle: string = attributeDescriptor.description ? `\n${attributeDescriptor.description}` : ""
+		subtitle += attributeDescriptor.hintHighValue ? `\nHigh Values: ${attributeDescriptor.hintHighValue}` : ""
+		subtitle += attributeDescriptor.hintLowValue ? `\nLow Values: ${attributeDescriptor.hintLowValue}` : ""
+		subtitle += attributeDescriptor.link ? `\n${attributeDescriptor.link}` : ""
+
+		return subtitle.length > 0 ? `${title}:${subtitle}` : title
 	}
 }
