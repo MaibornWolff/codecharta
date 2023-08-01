4e76382ede9756c5b1d60f534c851cc29d548c02
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2023-08-01 16:32:23 +0200

    Fix script formatting

--- analysis/node-wrapper/install.js
+++ analysis/node-wrapper/install.js
@@ -1,32 +1,34 @@
-const fs = require("fs")
-const path = require("path")
+#!/usr/bin/env node
 
-if (process.env.npm_package_name === "codecharta-analysis") {
-	;(async () => {
-		const bins = ["ccsh", "ccsh.ps1", "ccsh.cmd"]
-		const globalOrLocal = process.env.npm_config_global === "true"
-		const scriptLocation = globalOrLocal ? "binScripts/global" : "binScripts/local"
+const fs = require("fs");
+const path = require("path");
+
+(async () => {
+    if (process.env.npm_package_name === "codecharta-analysis") {
+		const bins = ["ccsh", "ccsh.ps1", "ccsh.cmd"];
+		const globalOrLocal = process.env.npm_config_global === "true";
+		const scriptLocation = globalOrLocal ? "binScripts/global" : "binScripts/local";
 		const scriptTarget = globalOrLocal
 			? process.env.npm_config_global_prefix
-			: path.join(process.env.npm_config_local_prefix, "node_modules/.bin")
-		var deletionPromises = []
+			: path.join(process.env.npm_config_local_prefix, "node_modules/.bin");
+		var deletionPromises = [];
 
 		bins.forEach(file => {
 			deletionPromises.push(
 				new Promise((res, rej) => {
 					try {
 						fs.copyFile(path.join(scriptLocation, file), path.join(scriptTarget, file), err => {
-							if (err) throw err
-							console.log(`${file} was moved`)
-							res()
+							if (err) throw err;
+							console.log(`${file} was moved`);
+							res();
 						})
 					} catch (err) {
-						console.error(err)
-						rej(err)
+						console.error(err);
+						rej(err);
 					}
 				})
 			)
 		})
-		await Promise.allSettled(deletionPromises)
-	})()
-}
+		await Promise.allSettled(deletionPromises);
+	}
+})();
