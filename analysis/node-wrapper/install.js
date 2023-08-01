e3d459dd4e3f34b86c374c992156a65dbe637f86
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2023-08-01 15:57:28 +0200

    Fix ccsh cli problems by providings custom bin-links for the executables
    #2600

---
+++ analysis/node-wrapper/install.js
@@ -0,0 +1,32 @@
+const fs = require("fs")
+const path = require("path")
+
+if (process.env.npm_package_name === "codecharta-analysis") {
+	;(async () => {
+		const bins = ["ccsh", "ccsh.ps1", "ccsh.cmd"]
+		const globalOrLocal = process.env.npm_config_global === "true"
+		const scriptLocation = globalOrLocal ? "binScripts/global" : "binScripts/local"
+		const scriptTarget = globalOrLocal
+			? process.env.npm_config_global_prefix
+			: path.join(process.env.npm_config_local_prefix, "node_modules/.bin")
+		var deletionPromises = []
+
+		bins.forEach(file => {
+			deletionPromises.push(
+				new Promise((res, rej) => {
+					try {
+						fs.copyFile(path.join(scriptLocation, file), path.join(scriptTarget, file), err => {
+							if (err) throw err
+							console.log(`${file} was moved`)
+							res()
+						})
+					} catch (err) {
+						console.error(err)
+						rej(err)
+					}
+				})
+			)
+		})
+		await Promise.allSettled(deletionPromises)
+	})()
+}
