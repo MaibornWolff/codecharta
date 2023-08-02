e5c7cf415bb51f540288000417a9fe9b007e3331
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2023-08-02 13:32:03 +0200

    Refactor try/catch, add early return, remove is-windows package for check

--- analysis/node-wrapper/install.js
+++ analysis/node-wrapper/install.js
@@ -2,25 +2,22 @@
 
 const fs = require("fs");
 const path = require("path");
-var isWindows = require("is-windows");
 
-if (isWindows() === true && process.env.npm_package_name === "codecharta-analysis") {
-  (async () => {
-      const bins = ["ccsh.ps1", "ccsh.cmd"];
-      const globalOrLocal = process.env.npm_config_global === "true";
-      const scriptLocation = globalOrLocal ? "binScripts/global" : "binScripts/local";
-      const scriptTarget = globalOrLocal
-        ? process.env.npm_config_global_prefix
-        : path.join(process.env.npm_config_local_prefix, "node_modules/.bin");
-
-      await Promise.all(bins.map((async (file) => {
-        try {
-          await fs.promises.copyFile(path.join(scriptLocation, file), path.join(scriptTarget, file));
-          console.log(`${file} was moved to ${scriptTarget}`);
-        } catch (err) {
-          console.error("File could not get copied")
-          throw err
-        }
-      })));
-  })();
+if (process.platform !== 'win32' || process.env.npm_package_name !== "codecharta-analysis") {
+  return;
 }
+
+(async () => {
+    const bins = ["ccsh.ps1", "ccsh.cmd"];
+    const globalOrLocal = process.env.npm_config_global === "true";
+    const scriptLocation = globalOrLocal ? "binScripts/global" : "binScripts/local";
+    const scriptTarget = globalOrLocal
+      ? process.env.npm_config_global_prefix
+      : path.join(process.env.npm_config_local_prefix, "node_modules/.bin");
+
+    await Promise.all(bins.map((async (file) => {
+      await fs.promises.copyFile(path.join(scriptLocation, file), path.join(scriptTarget, file));
+      console.log(`${file} was moved to ${scriptTarget}`);
+    })));
+})();
+
