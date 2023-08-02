9cbac5ccc62f26cefae0df7ad6f60c9e711d4f0f
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2023-08-02 11:43:14 +0200

    Change copy logic to properly use promises

--- analysis/node-wrapper/install.js
+++ analysis/node-wrapper/install.js
@@ -2,33 +2,25 @@
 
 const fs = require("fs");
 const path = require("path");
+var isWindows = require("is-windows");
 
-(async () => {
-    if (process.env.npm_package_name === "codecharta-analysis") {
-		const bins = ["ccsh.ps1", "ccsh.cmd"];
-		const globalOrLocal = process.env.npm_config_global === "true";
-		const scriptLocation = globalOrLocal ? "binScripts/global" : "binScripts/local";
-		const scriptTarget = globalOrLocal
-			? process.env.npm_config_global_prefix
-			: path.join(process.env.npm_config_local_prefix, "node_modules/.bin");
-		var deletionPromises = [];
+if (isWindows() === true && process.env.npm_package_name === "codecharta-analysis") {
+  (async () => {
+      const bins = ["ccsh.ps1", "ccsh.cmd"];
+      const globalOrLocal = process.env.npm_config_global === "true";
+      const scriptLocation = globalOrLocal ? "binScripts/global" : "binScripts/local";
+      const scriptTarget = globalOrLocal
+        ? process.env.npm_config_global_prefix
+        : path.join(process.env.npm_config_local_prefix, "node_modules/.bin");
 
-		bins.forEach(file => {
-			deletionPromises.push(
-				new Promise((res, rej) => {
-					try {
-						fs.copyFile(path.join(scriptLocation, file), path.join(scriptTarget, file), err => {
-							if (err) throw err;
-							console.log(`${file} was moved`);
-							res();
-						})
-					} catch (err) {
-						console.error(err);
-						rej(err);
-					}
-				})
-			)
-		})
-		await Promise.allSettled(deletionPromises);
-	}
-})();
+      await Promise.all(bins.map((async (file) => {
+        try {
+          await fs.promises.copyFile(path.join(scriptLocation, file), path.join(scriptTarget, "d",file));
+          console.log(`${file} was moved to ${scriptTarget}`);
+        } catch (err) {
+          console.error("File could not get copied")
+          throw err
+        }
+      })));
+  })();
+}
