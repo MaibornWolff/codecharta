b093d4c2a9d1dba119ec8bcfb218fe03e39af33d
phanlezz <65733509+phanlezz@users.noreply.github.com>
2023-09-27 10:57:10 +0200

    Fix bugs listed by sonar to pass quality gate (#3370)
    * Fix nullpointer "bugs"
    
    * Fix equals and hashCode function
    
    * Fix non-null assertion call for map access
    
    * Fix no toString issue kotlin:S6218

--- analysis/model/src/test/kotlin/de/maibornwolff/codecharta/model/ProjectBuilderTest.kt
+++ analysis/model/src/test/kotlin/de/maibornwolff/codecharta/model/ProjectBuilderTest.kt
@@ -1,9 +1,17 @@
 package de.maibornwolff.codecharta.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalStateException
 import org.junit.jupiter.api.Test
 
 class ProjectBuilderTest {
+
+    @Test
+    fun `should throw exception on wrong initialization`() {
+        assertThatIllegalStateException()
+            .isThrownBy { ProjectBuilder(listOf()) }
+            .withMessageContaining("No unique root node was found, instead 0 candidates identified.")
+    }
 
     @Test
     fun `without root inserting a new node should insert as child of root`() {
@@ -40,5 +48,21 @@ class ProjectBuilderTest {
 
         val root = project.rootNode
         assertThat(root.children).hasSize(0)
+    }
+
+    @Test
+    fun `should add new attributeTypes`() {
+        val projectBuilder = ProjectBuilder(attributeTypes = mutableMapOf("nodes" to mutableMapOf("nodeMetric" to AttributeType.absolute)))
+        val attributeTypesToAdd = AttributeTypes(mutableMapOf("edgeMetric" to AttributeType.absolute), "edges")
+        val attributeTypesToAddExisting = AttributeTypes(mutableMapOf("nodeMetric2" to AttributeType.relative), "nodes")
+        projectBuilder.addAttributeTypes(attributeTypesToAdd)
+        projectBuilder.addAttributeTypes(attributeTypesToAddExisting)
+        assertThat(projectBuilder.toString()).contains("edges={edgeMetric=absolute}", "nodes={nodeMetric=absolute, nodeMetric2=relative}")
+    }
+
+    @Test
+    fun `should print content keys`() {
+        val projectBuilder = ProjectBuilder()
+        assertThat(projectBuilder.toString()).contains("edges=[]", "attributeTypes={}", "attributeDescriptors={}", "blacklist=[]")
     }
 }
