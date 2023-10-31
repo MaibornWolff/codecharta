cf6b051566091510122a7d15eca95bfef5b7254b
nereboss <niklas.kneissl@gmail.com>
2023-10-31 09:09:21 +0100

    Update exception class #3333
    Fixes build issues

--- analysis/import/MetricGardenerImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/metricgardenerimporter/model/MetricGardenerException.kt
+++ analysis/import/MetricGardenerImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/metricgardenerimporter/model/MetricGardenerException.kt
@@ -1,8 +1,3 @@
 package de.maibornwolff.codecharta.importer.metricgardenerimporter.model
 
-class MetricGardenerException : Exception {
-    constructor() : super()
-    constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
-    constructor(cause: Throwable) : super(cause)
-}
+class MetricGardenerException(message: String) : Exception(message)
