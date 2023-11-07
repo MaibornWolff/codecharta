2a36c29778727107a8e91ed9c699074d6cc8bbc6
nereboss <niklas.kneissl@gmail.com>
2023-11-07 11:51:56 +0100

    Remove download button #3157
    Remove both the files and the calls of the toolbar

--- visualization/app/codeCharta/ui/toolBar/toolBar.module.ts
+++ visualization/app/codeCharta/ui/toolBar/toolBar.module.ts
@@ -6,7 +6,6 @@ import { HoveredNodePathPanelModule } from "./hoveredNodePathPanel/hoveredNodePa
 import { ScreenshotButtonModule } from "../screenshotButton/screenshotButton.module"
 import { FilePanelModule } from "../filePanel/filePanel.module"
 import { CopyToClipboardButtonModule } from "../copyToClipboardButton/copyToClipboardButton.module"
-import { DownloadButtonModule } from "./downloadButton/downloadButton.module"
 import { PresentationModeButtonModule } from "./presentationModeButton/presentationModeButton.module"
 import { ToolBarComponent } from "./toolBar.component"
 import { Export3DMapButtonModule } from "../export3DMapButton/export3DMapButton.module"
@@ -16,7 +15,6 @@ import { LoadingMapProgressSpinnerModule } from "./loadingMapProgressSpinner/loa
 	imports: [
 		CommonModule,
 		UploadFilesButtonModule,
-		DownloadButtonModule,
 		ScreenshotButtonModule,
 		CopyToClipboardButtonModule,
 		Export3DMapButtonModule,
