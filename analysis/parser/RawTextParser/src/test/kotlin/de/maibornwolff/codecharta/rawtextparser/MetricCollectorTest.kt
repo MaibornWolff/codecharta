82e2e01f69cdb97168ee23ba72659a71f1b6dd85
nereboss <niklas.kneissl@gmail.com>
2023-11-09 10:43:03 +0100

    Add more tests for MetricCollector #3316
    Specifically about the file extension option

--- analysis/parser/RawTextParser/src/test/kotlin/de/maibornwolff/codecharta/rawtextparser/MetricCollectorTest.kt
+++ analysis/parser/RawTextParser/src/test/kotlin/de/maibornwolff/codecharta/rawtextparser/MetricCollectorTest.kt
@@ -33,7 +33,7 @@ class MetricCollectorTest {
     }
 
     @Test
-    fun `Should exlude regex patterns`() {
+    fun `Should exclude regex patterns`() {
         val result = MetricCollector(File("src/test/resources/sampleproject").absoluteFile, exclude = arrayOf(".*\\.xyz", "foobar")).parse()
 
         Assertions.assertThat(result.size).isEqualTo(1)
@@ -41,10 +41,44 @@ class MetricCollectorTest {
     }
 
     @Test
-    fun `Should include only spedified File extensions`() {
+    fun `Should include only specified File extension with one given`() {
         val result = MetricCollector(File("src/test/resources/sampleproject").absoluteFile, fileExtensions = arrayOf("wrong")).parse()
 
         Assertions.assertThat(result.size).isEqualTo(1)
         Assertions.assertThat(result).containsKey("/spaces/spaces_xyz.wrong")
+        Assertions.assertThat(result).doesNotContainKey("/spaces/spaces_3.xyz")
+        Assertions.assertThat(result).doesNotContainKey("tabs.xyz")
+    }
+
+    @Test
+    fun `Should include only specified File extensions with multiple given`() {
+        val result = MetricCollector(File("src/test/resources/sampleproject").absoluteFile, fileExtensions = arrayOf("wrong", "xyz")).parse()
+
+        Assertions.assertThat(result.size).isEqualTo(4)
+        Assertions.assertThat(result).containsKey("/spaces/spaces_xyz.wrong")
+        Assertions.assertThat(result).doesNotContainKey("/spaces/spaces_3.xyz")
+    }
+
+    @Test
+    fun `Should include all Files when no specific extensions were given (default for interactive mode)`() {
+        val result = MetricCollector(File("src/test/resources/sampleproject").absoluteFile, fileExtensions = arrayOf("")).parse()
+
+        Assertions.assertThat(result.size).isEqualTo(4)
+        Assertions.assertThat(result).containsKey("/spaces/spaces_xyz.wrong")
+        Assertions.assertThat(result).containsKey("/spaces/spaces_3.xyz")
+        Assertions.assertThat(result).containsKey("/spaces/spaces_4.xyz")
+        Assertions.assertThat(result).containsKey("/tabs.xyz")
+    }
+
+    @Test
+    fun `Should not produce an output and notify the user if the only specified extension was not found in the folder`() {
+        val result = MetricCollector(File("src/test/resources/sampleproject").absoluteFile, fileExtensions = arrayOf("wrong, abc")).parse()
+
+        Assertions.assertThat(result.size).isEqualTo(0)
+    }
+
+    @Test
+    fun `Should warn the user if one of the specified extensions was not found in the folder`() {
+
     }
 }
