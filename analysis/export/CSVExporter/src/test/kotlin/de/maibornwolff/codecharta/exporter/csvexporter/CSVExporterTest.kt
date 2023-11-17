49012743248f28ce1aa1aa41d6504f8b51a39ad5
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2023-11-17 11:17:00 +0100

    Add test without implementation for csvexporter #3338

--- analysis/export/CSVExporter/src/test/kotlin/de/maibornwolff/codecharta/exporter/csvexporter/CSVExporterTest.kt
+++ analysis/export/CSVExporter/src/test/kotlin/de/maibornwolff/codecharta/exporter/csvexporter/CSVExporterTest.kt
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import picocli.CommandLine
 import java.io.ByteArrayOutputStream
+import java.io.File
 import java.io.PrintStream
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -21,6 +22,15 @@ class CSVExporterTest {
     @AfterAll
     fun afterTest() {
         unmockkAll()
+    }
+
+    @Test
+    fun `should handle single valid input file`() {
+
+    }
+
+    @Test
+    fun `should handle two valid input files with appending`() {
     }
 
     @Test
@@ -35,5 +45,52 @@ class CSVExporterTest {
         System.setErr(originalErr)
 
         Assertions.assertThat(errContent.toString()).contains("Input invalid file for CSVExporter, stopping execution")
+    }
+
+    @Test
+    fun `should handle mix of valid and invalid input files`() {
+        // Implement the test logic
+    }
+
+    @Test
+    fun `should handle nodes with empty values`() {
+        // Implement the test logic
+    }
+
+    @Test
+    fun `should handle nodes with null values`() {
+        // Implement the test logic
+    }
+
+    @Test
+    fun `should handle nodes with different attribute types`() {
+        // Implement the test logic
+    }
+
+    @Test
+    fun `should handle no output file specified (stdout case)`() {
+        // Implement the test logic
+    }
+
+    @Test
+    fun `should handle output file specified`() {
+        // Implement the test logic
+    }
+
+    @Test
+    fun `should handle existing output file with data (test overwriting)`() {
+        // Implement the test logic
+    }
+
+    @Test
+    fun `should stop execution if depth-of-hierarchy is negative`() {
+        val filePath = "../../test/data/codecharta/csvexport_input.cc.json"
+        Assertions.assertThat(File(filePath).exists())
+        val maxHierarchy = -1
+        System.setErr(PrintStream(errContent))
+        CommandLine(CSVExporter()).execute("filePath", "--depth-of-hierarchy", "$maxHierarchy").toString()
+        System.setErr(originalErr)
+
+        Assertions.assertThat(errContent.toString()).contains("depth-of-hierarchy must not be negative")
     }
 }
