0c67715e6b5dd1adf8bc2fd604fc0349d3116eae
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2023-11-22 18:12:58 +0100

    Fix output file not getting deleted #3338

--- analysis/import/CodeMaatImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/codemaat/CodeMaatImporterTest.kt
+++ analysis/import/CodeMaatImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/codemaat/CodeMaatImporterTest.kt
@@ -18,71 +18,83 @@ import java.io.PrintStream
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CodeMaatImporterTest {
-    val errContent = ByteArrayOutputStream()
-    val originalErr = System.err
+    private var errContent = ByteArrayOutputStream()
+    private val originalErr = System.err
 
     @AfterEach
     fun afterTest() {
         unmockkAll()
+        errContent = ByteArrayOutputStream()
     }
 
     @Test
     fun `should create json uncompressed file`() {
-        main(
-            arrayOf(
-                "src/test/resources/coupling-codemaat.csv",
-                "-nc",
-                "-o=src/test/resources/coupling-codemaat.cc.json"
-                   )
-            )
-        val file = File("src/test/resources/coupling-codemaat.cc.json")
-        file.deleteOnExit()
+        // given
+        val inputFilePath = "src/test/resources/coupling-codemaat.csv"
+        val outputFilePath = "src/test/resources/coupling-codemaat.cc.json"
+        val outputFile = File(outputFilePath)
+        outputFile.deleteOnExit()
 
-        assertTrue(file.exists())
+        // when
+        main(arrayOf(inputFilePath, "-nc", "-o=$outputFilePath"))
+
+        assertTrue(outputFile.exists())
     }
 
     @Test
     fun `should create json gzip file`() {
-        main(
-            arrayOf(
-                "src/test/resources/coupling-codemaat.csv",
-                "-o=src/test/resources/coupling-codemaat.cc.json"
-                   )
-            )
-        val file = File("src/test/resources/coupling-codemaat.cc.json.gz")
-        file.deleteOnExit()
+        // given
+        val inputFilePath = "src/test/resources/coupling-codemaat.csv"
+        val outputFilePath = "src/test/resources/coupling-codemaat.cc.json"
+        val outputFilePathCompressed = "src/test/resources/coupling-codemaat.cc.json.gz"
+        val outputFile = File(outputFilePathCompressed)
+        outputFile.deleteOnExit()
 
-        assertTrue(file.exists())
+        // when
+        main(arrayOf(inputFilePath, "-o=$outputFilePath"))
+
+        // then
+        assertTrue(outputFile.exists())
     }
 
     @Test
     fun `should contain expected content`() {
-        main(
-            arrayOf(
-                "src/test/resources/coupling-codemaat.csv",
-                "-nc",
-                "-o=src/test/resources/coupling-codemaat-content.cc.json"
-                   )
-            )
-        val file = File("src/test/resources/coupling-codemaat-content.cc.json")
+        // given
+        val inputFilePath = "src/test/resources/coupling-codemaat.csv"
+        val outputFilePath = "src/test/resources/coupling-codemaat-content.cc.json"
+        val file = File(outputFilePath)
         file.deleteOnExit()
 
+        // when
+        main(arrayOf(inputFilePath, "-nc", "-o=$outputFilePath"))
+
+        // then
         assertThat(file.readText()).contains("\"avgCommits\":5")
         assertThat(file.readText()).contains(listOf("attributeDescriptors", "\"description\":\"Average"))
-        assertEquals(ProjectDeserializer.deserializeProject(file.reader()).attributeDescriptors, getAttributeDescriptors())
+        file.reader().use {
+            assertEquals(ProjectDeserializer.deserializeProject(it).attributeDescriptors, getAttributeDescriptors())
+        }
     }
 
     @Test
     fun `should stop execution if input file is invalid`() {
+        // given
+        val nonExistentInputFilePath = "thisDoesNotExist.cc.json"
+
         mockkObject(InputHelper)
         every {
             InputHelper.isInputValid(any(), any())
         } returns false
 
         System.setErr(PrintStream(errContent))
-        main(arrayOf("thisDoesNotExist.cc.json"))
-        System.setErr(originalErr)
 
+        // when
+        main(arrayOf(nonExistentInputFilePath))
+
+        // then
         assertThat(errContent.toString()).contains("Input invalid file for CodeMaatImporter, stopping execution")
+
+        // clean up
+        System.setErr(originalErr)
     }
 }
