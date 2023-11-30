0f253cb9aba420133957ed1793e1fe1b0a2a6dc1
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2023-11-30 22:54:08 +0100

    Add tests for filemetrics and projectmetrics #3406

--- analysis/parser/RawTextParser/src/main/kotlin/de/maibornwolff/codecharta/parser/rawtextparser/ProjectMetrics.kt
+++ analysis/parser/RawTextParser/src/main/kotlin/de/maibornwolff/codecharta/parser/rawtextparser/ProjectMetrics.kt
@@ -2,7 +2,7 @@ package de.maibornwolff.codecharta.parser.rawtextparser
 
 import java.util.concurrent.ConcurrentHashMap
 
-class ProjectMetrics() {
+class ProjectMetrics {
 
     val metricsMap = ConcurrentHashMap<String, FileMetrics>()
     fun addFileMetrics(filePath: String, fileMetrics: FileMetrics): ProjectMetrics {
