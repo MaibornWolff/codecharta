3e8eccc268d8a90c2aaca72b30dc3f2470b69686
fritschldwg <149768491+fritschldwg@users.noreply.github.com>
2023-12-06 16:00:56 +0100

    Revert "Remove unused imports #3406"
    This reverts commit 6cca68650994a89d3d5ff34f627d9976050a04bf.

--- analysis/model/src/main/kotlin/de/maibornwolff/codecharta/serialization/ProjectInputReader.kt
+++ analysis/model/src/main/kotlin/de/maibornwolff/codecharta/serialization/ProjectInputReader.kt
@@ -5,6 +5,7 @@ import java.io.BufferedInputStream
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.io.Reader
 import java.nio.charset.StandardCharsets
 import java.util.Scanner
 import java.util.zip.GZIPInputStream
@@ -105,4 +106,5 @@ object ProjectInputReader {
         }
         return false
     }
+
 }
