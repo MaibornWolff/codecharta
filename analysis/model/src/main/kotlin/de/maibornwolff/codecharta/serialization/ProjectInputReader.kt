596f16b7bce90788af97cc079ac31860d9a46f87
fritschldwg <149768491+fritschldwg@users.noreply.github.com>
2023-12-06 16:00:56 +0100

    Revert "Check sync signal and project start #3406"
    This reverts commit 42f90cd0cfee085c1922209d65bd600d7f45d64f.

--- analysis/model/src/main/kotlin/de/maibornwolff/codecharta/serialization/ProjectInputReader.kt
+++ analysis/model/src/main/kotlin/de/maibornwolff/codecharta/serialization/ProjectInputReader.kt
@@ -1,6 +1,5 @@
 package de.maibornwolff.codecharta.serialization
 
-import de.maibornwolff.codecharta.tools.pipeableparser.PipeableParserSyncFlag
 import java.io.BufferedInputStream
 import java.io.InputStream
 import java.io.InputStreamReader
@@ -28,12 +27,16 @@ object ProjectInputReader {
             return extractProjectString(BufferedInputStream(input))
         }
 
-        val isSyncSignalContained = isSyncSignalContained(input, availableBytes)
-        val isProjectAtFrontOfStream = isJsonObjectAtFrontOfStream(input, availableBytes)
-
-        if (!isSyncSignalContained and !isProjectAtFrontOfStream) {
-            return ""
+        input.mark(availableBytes)
+        val buffer = CharArray(availableBytes)
+        val reader = InputStreamReader(input, StandardCharsets.UTF_8)
+        val bytesRead = reader.read(buffer, 0, availableBytes)
+        val content = StringBuilder().appendRange(buffer, 0, bytesRead).toString()
+        val newLineCharacter = '\n'
+        if (content[content.length - 1] == newLineCharacter) {
+            return content.replace(newLineCharacter.toString(), "")
         }
+        input.reset()
 
         val scanner = Scanner(input)
         val stringBuilder = StringBuilder()
@@ -42,71 +45,6 @@ object ProjectInputReader {
             stringBuilder.append(scanner.nextLine())
         }
 
-        return extractJsonObjectFromEndOfStream(stringBuilder.toString())
+        return stringBuilder.toString()
     }
-
-    private fun isSyncSignalContained(input: InputStream, availableBytes: Int): Boolean {
-        val maxBufferSize = 1024
-        val bufferSize = minOf(availableBytes, maxBufferSize)
-        val buffer = ByteArray(bufferSize)
-        input.mark(bufferSize)
-        input.read(buffer, 0, bufferSize)
-
-        val syncFlag = PipeableParserSyncFlag.SYNC_FLAG.value
-        val syncSignalBytes = syncFlag.toByteArray()
-        input.reset()
-        return isSubarray(syncSignalBytes, buffer)
-    }
-
-    private fun isJsonObjectAtFrontOfStream(input: InputStream, availableBytes: Int): Boolean {
-        input.mark(availableBytes)
-        val buffer = CharArray(availableBytes)
-        val reader = InputStreamReader(input, StandardCharsets.UTF_8)
-        val bytesRead = reader.read(buffer, 0, availableBytes)
-        val content = StringBuilder().appendRange(buffer, 0, bytesRead).toString()
-        val openingBracket = "{".toCharArray()[0]
-        val closingBracket = "}".toCharArray()[0]
-        val lastClosingBracketIndex = content.lastIndexOf(closingBracket)
-        val firstOpeningBracketIndex = content.lastIndexOf(openingBracket)
-        input.reset()
-        return firstOpeningBracketIndex == 0 && lastClosingBracketIndex == -1
-    }
-
-    private fun extractJsonObjectFromEndOfStream(streamContent: String): String {
-        var count = 0
-        val openingBracket = "{".toCharArray()[0]
-        val closingBracket = "}".toCharArray()[0]
-        val lastClosingBracketIndex = streamContent.lastIndexOf(closingBracket)
-        if (lastClosingBracketIndex == -1) {
-            return streamContent
-        }
-
-        var index = lastClosingBracketIndex
-        while (index >= 0) {
-            when (streamContent[index]) {
-                closingBracket -> count++
-                openingBracket -> count--
-            }
-            if (count == 0) {
-                break
-            }
-            index--
-        }
-
-        return if (count == 0) {
-            streamContent.substring(index, lastClosingBracketIndex + 1)
-        } else {
-            streamContent
-        }
-    }
-
-    private fun isSubarray(subarray: ByteArray, buffer: ByteArray): Boolean {
-        for (i in 0 until buffer.size - subarray.size + 1) {
-            if (buffer.copyOfRange(i, i + subarray.size).contentEquals(subarray)) {
-                return true
-            }
-        }
-        return false
-    }
-
 }
