987779f31a445da2a4d63839db42179b3829cd89
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2023-12-12 21:50:07 +0100

    Update test naming #2866

--- analysis/filter/StructureModifier/src/test/kotlin/de/maibornwolff/codecharta/filter/structuremodifier/NodeRemoverTest.kt
+++ analysis/filter/StructureModifier/src/test/kotlin/de/maibornwolff/codecharta/filter/structuremodifier/NodeRemoverTest.kt
@@ -1,6 +1,5 @@
 package de.maibornwolff.codecharta.filter.structuremodifier
 
-import de.maibornwolff.codecharta.model.Path
 import de.maibornwolff.codecharta.model.Project
 import de.maibornwolff.codecharta.serialization.ProjectDeserializer
 import org.assertj.core.api.Assertions
@@ -21,53 +20,58 @@ class NodeRemoverTest {
     }
 
     @Test
-    fun `Non existent path leads to replication of the project`() {
+    fun `Should replicate project when non existent path specified`() {
+        // when
         val subProjectExtractor = NodeRemover(sampleProject)
-
         val result = subProjectExtractor.remove(arrayOf("/root/somethig"))
 
+        // then
         Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(sampleProject)
     }
 
     @Test
-    fun `Non affected nodes are kept`() {
+    fun `Should keep non affected nodes when removal is specified`() {
+        // when
         val subProjectExtractor = NodeRemover(sampleProject)
-
         val result = subProjectExtractor.remove(arrayOf("/root/src/main"))
-
         val testFolder = result.rootNode.children.first().children.first()
+
+        // then
         Assertions.assertThat(testFolder.name).isEqualTo("test")
     }
 
     @Test
-    fun `specified nodes are removed`() {
+    fun `Should remove correct nodes when single node is specified for removal`() {
+        // when
         val subProjectExtractor = NodeRemover(sampleProject)
-
         val result = subProjectExtractor.remove(arrayOf("/root/src/main"))
-
         val srcContent = result.rootNode.children.first().children
+
+        // then
         Assertions.assertThat(srcContent.size).isEqualTo(2)
         Assertions.assertThat(srcContent.map { it.name }).doesNotContain("main")
     }
 
     @Test
-    fun `multiple nodes are removed`() {
+    fun `Should remove correct nodes when multiple nodes are specified for removal`() {
+        // when
         val subProjectExtractor = NodeRemover(sampleProject)
-
         val result = subProjectExtractor.remove(arrayOf("/root/src/main/file1.java", "root/src/folder3/"))
-
         val srcContent = result.rootNode.children.first().children
         val mainContent = srcContent.first().children
+
+        // then
         Assertions.assertThat(srcContent.map { it.name }).doesNotContain("folder3")
         Assertions.assertThat(mainContent.map { it.name }).containsOnly("file2.java")
     }
 
     @Test
-    fun `Affected edges are removed`() {
+    fun `Should remove affected edges when removal are specified`() {
+        // when
         val subProjectExtractor = NodeRemover(sampleProject)
-
         val result = subProjectExtractor.remove(arrayOf("/root/foo"))
 
+        // then
         Assertions.assertThat(result.edges.size).isEqualTo(1)
         Assertions.assertThat(result.edges.first()).usingRecursiveComparison().isEqualTo(sampleProject.edges.last())
         Assertions.assertThat(result.edges.map { it.fromNodeName }).doesNotContain("/root/foo")
@@ -75,40 +79,54 @@ class NodeRemoverTest {
     }
 
     @Test
-    fun `Affected edges are removed for multiple removals`() {
-        val subProjectExtractor = NodeRemover(sampleProject)
+    fun `Should remove affected edges when multiple removals are specified`() {
+        // given
         val toExclude = arrayOf("/root/foo/file1", "root/else/")
+        val shouldBeExcluded = listOf("/root/foo/file1", "root/else/file1", "root/else")
 
+        // when
+        val subProjectExtractor = NodeRemover(sampleProject)
         val result = subProjectExtractor.remove(toExclude)
 
-        val shouldBeExcluded = listOf("/root/foo/file1", "root/else/file1", "root/else")
+        // then
         Assertions.assertThat(result.edges.size).isEqualTo(2)
         Assertions.assertThat(result.edges.map { it.fromNodeName }).doesNotContainAnyElementsOf(shouldBeExcluded)
         Assertions.assertThat(result.edges.map { it.toNodeName }).doesNotContainAnyElementsOf(shouldBeExcluded)
     }
 
     @Test
-    fun `Affected blacklist items are removed`() {
+    fun `Should remove correct blacklist items when node removal specified`() {
+        // when
         val subProjectExtractor = NodeRemover(sampleProject)
-
         val result = subProjectExtractor.remove(arrayOf("/root/foo"))
 
+        // then
         Assertions.assertThat(result.blacklist.map { it.path }).doesNotContainSubsequence("/root/foo")
     }
 
     @Test
-    fun `given a remove action attributeDescriptors should get copied if attributes don't change`() {
+    fun `Should keep attributes when only node removal specified`() {
+        // given
         val input = InputStreamReader(this.javaClass.classLoader.getResourceAsStream(DESCRIPTOR_TEST_PATH)!!)
         val attributeProject = ProjectDeserializer.deserializeProject(input)
+
+        // when
         val resultProject = NodeRemover(attributeProject).remove(arrayOf("/root/bigLeaf.ts"))
+
+        // then
         assertEquals(attributeProject.attributeDescriptors.size, resultProject.attributeDescriptors.size)
     }
 
     @Test
-    fun `should remove unused attributeDescriptors if nodes are removed`() {
+    fun `Should remove unused attributeDescriptors when nodes are removed`() {
+        // given
         val input = InputStreamReader(this.javaClass.classLoader.getResourceAsStream(DESCRIPTOR_TEST_PATH)!!)
         val attributeProject = ProjectDeserializer.deserializeProject(input)
+
+        // when
         val resultProject = NodeRemover(attributeProject).remove(arrayOf("/root/AnotherParentLeaf"))
+
+        // then
         assertEquals(resultProject.attributeDescriptors.size, 3)
         assertEquals(resultProject.attributeDescriptors["rloc"]!!.description, "1")
         assertEquals(resultProject.attributeDescriptors["yrloc"], null)
