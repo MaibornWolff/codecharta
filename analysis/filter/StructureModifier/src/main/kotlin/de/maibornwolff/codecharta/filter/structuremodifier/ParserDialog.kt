2a17d628d83dc2e26c80d6e0fa28871c3f7fd414
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2023-12-14 09:38:43 +0100

    Update readme #2866

--- analysis/filter/StructureModifier/src/main/kotlin/de/maibornwolff/codecharta/filter/structuremodifier/ParserDialog.kt
+++ analysis/filter/StructureModifier/src/main/kotlin/de/maibornwolff/codecharta/filter/structuremodifier/ParserDialog.kt
@@ -29,37 +29,37 @@ class ParserDialog {
             )
 
             return when (selectedAction) {
-                StructureModifierAction.PRINT_STRUCTURE.descripton  -> collectPrintArguments(inputFolderName)
-                StructureModifierAction.EXTRACT_PATH.descripton     -> collectExtractPathArguments(inputFolderName)
-                StructureModifierAction.MOVES_NODES.descripton      -> collectMoveNodesArguments(inputFolderName)
-                StructureModifierAction.REMOVE_NODES.descripton     -> collectRemoveNodesArguments(inputFolderName)
-                else                                                -> listOf()
+                StructureModifierAction.PRINT_STRUCTURE.descripton -> collectPrintArguments(inputFolderName)
+                StructureModifierAction.EXTRACT_PATH.descripton -> collectExtractPathArguments(inputFolderName)
+                StructureModifierAction.MOVES_NODES.descripton -> collectMoveNodesArguments(inputFolderName)
+                StructureModifierAction.REMOVE_NODES.descripton -> collectRemoveNodesArguments(inputFolderName)
+                else -> listOf()
             }
         }
 
-        private fun collectPrintArguments(inputFolderName: String):List<String> {
+        private fun collectPrintArguments(inputFolderName: String): List<String> {
             val printLevels: BigDecimal =
                     KInquirer.promptInputNumber(message = "How many print levels do you want to print? (Optional)", default = "0", hint = "0")
             return listOf(inputFolderName, "--print-levels=$printLevels")
         }
 
-        private fun collectExtractPathArguments(inputFolderName: String):List<String> {
+        private fun collectExtractPathArguments(inputFolderName: String): List<String> {
             val setRoot: String =
                     KInquirer.promptInput(message = "What path within project to be extracted?")
             val outputFileName = collectOutputFileName()
             return listOf(inputFolderName, "--set-root=$setRoot", "--output-file=$outputFileName")
         }
 
-        private fun collectMoveNodesArguments(inputFolderName: String):List<String> {
+        private fun collectMoveNodesArguments(inputFolderName: String): List<String> {
             val moveFrom: String =
                     KInquirer.promptInput(message = "What are the nodes to be moved?")
             val moveTo: String =
-                    KInquirer.promptInput(message ="Where to move them?")
+                    KInquirer.promptInput(message = "Where to move them?")
             val outputFileName = collectOutputFileName()
             return listOf(inputFolderName, "--move-from=$moveFrom", "--move-to=$moveTo", "--output-file=$outputFileName")
         }
 
-        private fun collectRemoveNodesArguments(inputFolderName: String):List<String> {
+        private fun collectRemoveNodesArguments(inputFolderName: String): List<String> {
             val remove: String =
                     KInquirer.promptInput(message = "What are the nodes to be removed?")
             val outputFileName = collectOutputFileName()
