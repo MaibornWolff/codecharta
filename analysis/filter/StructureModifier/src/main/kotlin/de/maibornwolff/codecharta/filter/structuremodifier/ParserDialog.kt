c585e27fca113c0afd69cd09307d4e8da4140ed2
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2023-12-14 08:57:11 +0100

    Make user choose single action for modify #2866

--- analysis/filter/StructureModifier/src/main/kotlin/de/maibornwolff/codecharta/filter/structuremodifier/ParserDialog.kt
+++ analysis/filter/StructureModifier/src/main/kotlin/de/maibornwolff/codecharta/filter/structuremodifier/ParserDialog.kt
@@ -3,6 +3,7 @@ package de.maibornwolff.codecharta.filter.structuremodifier
 import com.github.kinquirer.KInquirer
 import com.github.kinquirer.components.promptInput
 import com.github.kinquirer.components.promptInputNumber
+import com.github.kinquirer.components.promptList
 import de.maibornwolff.codecharta.tools.interactiveparser.ParserDialogInterface
 import java.io.File
 import java.math.BigDecimal
@@ -15,40 +16,58 @@ class ParserDialog {
             val inputFolderName =
                     KInquirer.promptInput(
                             message = "What is the cc.json file that has to be modified?",
-                            hint = Paths.get("").toAbsolutePath().toString() + File.separator + "yourInput.cc.json"
-                                         )
+                            hint = Paths.get("").toAbsolutePath().toString() + File.separator + "yourInput.cc.json")
 
-            val outputFileName: String = KInquirer.promptInput(
-                    message = "What is the name of the output file?"
+            val selectedAction: String = KInquirer.promptList(
+                    message = "Which action do you want to perform?",
+                    choices = listOf(
+                            StructureModifierAction.PRINT_STRUCTURE.descripton,
+                            StructureModifierAction.EXTRACT_PATH.descripton,
+                            StructureModifierAction.MOVES_NODES.descripton,
+                            StructureModifierAction.REMOVE_NODES.descripton
+                    )
             )
 
-            val setRoot: String =
-                    KInquirer.promptInput(message = "What path within project to be extracted? (Optional)", default = "")
+            return when (selectedAction) {
+                StructureModifierAction.PRINT_STRUCTURE.descripton  -> collectPrintArguments(inputFolderName)
+                StructureModifierAction.EXTRACT_PATH.descripton     -> collectExtractPathArguments(inputFolderName)
+                StructureModifierAction.MOVES_NODES.descripton      -> collectMoveNodesArguments(inputFolderName)
+                StructureModifierAction.REMOVE_NODES.descripton     -> collectRemoveNodesArguments(inputFolderName)
+                else                                                -> listOf()
+            }
+        }
 
+        private fun collectPrintArguments(inputFolderName: String):List<String> {
             val printLevels: BigDecimal =
                     KInquirer.promptInputNumber(message = "How many print levels do you want to print? (Optional)", default = "0", hint = "0")
+            return listOf(inputFolderName, "--print-levels=$printLevels")
+        }
 
-            val moveFrom: String = KInquirer.promptInput(
-                    message = "What are the nodes to be moved? (Optional)"
-            )
-            var moveTo: String = ""
-            if (moveFrom.isNotBlank()) {
-                moveTo = KInquirer.promptInput(
-                        message = "Where to move them?"
-                )
-            }
-            val remove: String = KInquirer.promptInput(
-                    message = "What are the nodes to be removed? (Optional)"
-            )
-            return listOfNotNull(
-                    inputFolderName,
-                    "--output-file=$outputFileName",
-                    if (printLevels.toInt() != 0) "--print-levels=$printLevels" else null,
-                    if (setRoot.isNotBlank()) "--set-root=$setRoot" else null,
-                    if (moveFrom.isNotBlank()) "--move-from=$moveFrom" else null,
-                    if (moveFrom.isNotBlank()) "--move-to=$moveTo" else null,
-                    if (remove.isNotBlank()) "--remove=$remove" else null,
-            )
+        private fun collectExtractPathArguments(inputFolderName: String):List<String> {
+            val setRoot: String =
+                    KInquirer.promptInput(message = "What path within project to be extracted?")
+            val outputFileName = collectOutputFileName()
+            return listOf(inputFolderName, "--set-root=$setRoot", "--output-file=$outputFileName")
+        }
+
+        private fun collectMoveNodesArguments(inputFolderName: String):List<String> {
+            val moveFrom: String =
+                    KInquirer.promptInput(message = "What are the nodes to be moved?")
+            val moveTo: String =
+                    KInquirer.promptInput(message ="Where to move them?")
+            val outputFileName = collectOutputFileName()
+            return listOf(inputFolderName, "--move-from=$moveFrom", "--move-to=$moveTo", "--output-file=$outputFileName")
+        }
+
+        private fun collectRemoveNodesArguments(inputFolderName: String):List<String> {
+            val remove: String =
+                    KInquirer.promptInput(message = "What are the nodes to be removed?")
+            val outputFileName = collectOutputFileName()
+            return listOf(inputFolderName, "--remove=$remove", "--output-file=$outputFileName")
+        }
+
+        private fun collectOutputFileName(): String {
+            return KInquirer.promptInput(message = "What is the name of the output file?")
         }
     }
 }
---
+++ analysis/filter/StructureModifier/src/main/kotlin/de/maibornwolff/codecharta/filter/structuremodifier/StructureModifierAction.kt
@@ -0,0 +1,8 @@
+package de.maibornwolff.codecharta.filter.structuremodifier
+
+enum class StructureModifierAction(val descripton: String) {
+    PRINT_STRUCTURE("Print the structure of the project"),
+    EXTRACT_PATH("Extract a subproject"),
+    MOVES_NODES("Reorder nodes inside the project"),
+    REMOVE_NODES("Remove nodes"),
+}
