db1e262e6d20f86f8ac2e7a25285a7dc86b7769a
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2023-12-14 11:27:30 +0100

    Remove absolute file path handling #2866

--- analysis/filter/StructureModifier/src/main/kotlin/de/maibornwolff/codecharta/filter/structuremodifier/NodeRemover.kt
+++ analysis/filter/StructureModifier/src/main/kotlin/de/maibornwolff/codecharta/filter/structuremodifier/NodeRemover.kt
@@ -31,17 +31,26 @@ class NodeRemover(private val project: Project) {
 
     private fun removeNodes(paths: List<List<String>>): List<MutableNode> {
         val rootNode = project.rootNode.toMutableNode()
-
         for (path in paths) {
             var currentNode = rootNode
-            val rootElementIndex = path.indexOfFirst { it == "root" }
-            val pathSegmentsWithoutRoot = path.subList(rootElementIndex + 1, path.size)
-            for ((index, pathSegment) in pathSegmentsWithoutRoot.withIndex()) {
-                val isLastSegment = index == pathSegmentsWithoutRoot.lastIndex
+            if (path[0] != "root") {
+                logger.warn("Path to node has to start with root: ${path.joinToString("/")}")
+                continue
+            }
+
+            val pathWithoutRoot = path.drop(1)
+            for ((index, pathSegment) in pathWithoutRoot.withIndex()) {
+                val isLastSegment = index == pathWithoutRoot.lastIndex
                 if (isLastSegment) {
                     currentNode.children.removeIf { it.name == pathSegment }
                 } else {
-                    currentNode = currentNode.children.find { it.name == pathSegment } ?: break
+                    val childNode: MutableNode? = currentNode.children.find { it.name == pathSegment }
+                    if (childNode != null) {
+                        currentNode = childNode
+                    } else {
+                        logger.warn("Path to node not found: ${path.joinToString("/")}")
+                        break
+                    }
                 }
             }
         }
