880b4c7a143662c3cc93b71710a18fd16d10c6b5
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2024-01-17 12:01:21 +0100

    Add pipe to check if area is valid (attribute>0 or delta<0 value must be present)

---
+++ visualization/app/codeCharta/ui/searchPanel/mapTreeView/areaMetricValidPipe.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from "@angular/core"
+
+import { CodeMapNode } from "../../../codeCharta.model"
+
+@Pipe({ name: "areaMetricValid" })
+export class AreaMetricValidPipe implements PipeTransform {
+	transform(node: CodeMapNode, areaMetric: string): boolean {
+		return isAreaValid(node, areaMetric)
+	}
+}
+
+export function isAreaValid(node: CodeMapNode, areaMetric: string): boolean {
+	return node.deltas?.[areaMetric] < 0 || node.attributes?.[areaMetric] > 0
+}
