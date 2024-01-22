5c5beb196be31b8a2b964141ee8ff48c21c6278b
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2024-01-22 09:11:09 +0100

    Fix import format and unused imports

--- analysis/tools/InteractiveParser/src/main/kotlin/de/maibornwolff/codecharta/tools/interactiveparser/Inquirer.kt
+++ analysis/tools/InteractiveParser/src/main/kotlin/de/maibornwolff/codecharta/tools/interactiveparser/Inquirer.kt
@@ -1,8 +1,14 @@
 package de.maibornwolff.codecharta.tools.interactiveparser
 
-import com.varabyte.kotter.foundation.*
-import com.varabyte.kotter.foundation.input.*
-import com.varabyte.kotter.foundation.text.*
+import com.varabyte.kotter.foundation.input.Completions
+import com.varabyte.kotter.foundation.input.input
+import com.varabyte.kotter.foundation.input.onInputEntered
+import com.varabyte.kotter.foundation.input.runUntilInputEntered
+import com.varabyte.kotter.foundation.session
+import com.varabyte.kotter.foundation.text.bold
+import com.varabyte.kotter.foundation.text.green
+import com.varabyte.kotter.foundation.text.text
+import com.varabyte.kotter.foundation.text.textLine
 
 class Inquirer {
     companion object {
@@ -11,10 +17,10 @@ class Inquirer {
             session {
                 run {
                     section {
-                        bold { green { text("? ") }; textLine(message) };
+                        bold { green { text("? ") }; textLine(message) }
                         text("> "); input(Completions(hint), initialText = "")
                     }.runUntilInputEntered {
-                        onInputEntered { returnValue = input; return@onInputEntered}
+                        onInputEntered { returnValue = input; return@onInputEntered }
                     }
                 }
             }
