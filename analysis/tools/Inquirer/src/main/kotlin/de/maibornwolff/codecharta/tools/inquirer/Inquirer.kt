47ab5d9d1757f1490d8dcb37745daef4b73fb40a
nereboss <niklas.kneissl@gmail.com>
2024-01-23 15:41:49 +0100

    Add promptConfirm function #3483

--- analysis/tools/Inquirer/src/main/kotlin/de/maibornwolff/codecharta/tools/inquirer/Inquirer.kt
+++ analysis/tools/Inquirer/src/main/kotlin/de/maibornwolff/codecharta/tools/inquirer/Inquirer.kt
@@ -1,10 +1,13 @@
 package de.maibornwolff.codecharta.tools.inquirer
 
 import com.varabyte.kotter.foundation.input.Completions
+import com.varabyte.kotter.foundation.input.Keys
 import com.varabyte.kotter.foundation.input.input
 import com.varabyte.kotter.foundation.input.onInputChanged
 import com.varabyte.kotter.foundation.input.onInputEntered
+import com.varabyte.kotter.foundation.input.onKeyPressed
 import com.varabyte.kotter.foundation.input.runUntilInputEntered
+import com.varabyte.kotter.foundation.liveVarOf
 import com.varabyte.kotter.foundation.runUntilSignal
 import com.varabyte.kotter.foundation.session
 import com.varabyte.kotter.foundation.text.bold
@@ -18,13 +21,11 @@ class Inquirer {
         fun myPromptInput(message: String, hint: String = ""): String { //todo, add flag to accept empty input or not
             var returnValue = ""
             session {
-                run {
-                    section {
-                        bold { green { text("? ") }; textLine(message) }
-                        text("> "); input(Completions(hint), initialText = "")
-                    }.runUntilInputEntered {
-                        onInputEntered { returnValue = input; return@onInputEntered }
-                    }
+                section {
+                    bold { green { text("? ") }; textLine(message) }
+                    text("> "); input(Completions(hint), initialText = "")
+                }.runUntilInputEntered {
+                    onInputEntered { returnValue = input; return@onInputEntered }
                 }
             }
             return returnValue
@@ -38,28 +39,46 @@ class Inquirer {
             var returnValue = ""
             var showError = false
             session {
-                run {
-                    section {
-                        bold {
-                            green { text("? ") }; text(message)
-                            if (showError) red { textLine(" $invalidInputMessage") } else textLine("")
+                section {
+                    bold {
+                        green { text("? ") }; text(message)
+                        if (showError) red { textLine(" $invalidInputMessage") } else textLine("")
+                    }
+                    text("> "); input(Completions(hint), initialText = "")
+                }.runUntilSignal {
+                    onInputChanged { showError = false; input = input.filter { it.isDigit() } }
+                    onInputEntered {
+                        if (allowEmpty || input.isNotEmpty()) {
+                            returnValue = input
+                            signal()
                         }
-                        text("> "); input(Completions(hint), initialText = "")
-                    }.runUntilSignal {
-                        onInputChanged { showError = false; input = input.filter { it.isDigit() } }
-                        onInputEntered {
-                            if (allowEmpty || input.isNotEmpty()) {
-                                returnValue = input
-                                signal()
-                            }
-                            else {
-                                showError = true
-                            }
+                        else {
+                            showError = true
                         }
                     }
                 }
             }
             return returnValue
+        }
+
+        fun myPromptConfirm(message: String): Boolean {
+            var result = true
+            session {
+                var res by liveVarOf(true)
+                section {
+                    green { text("? ") }; text(message)
+                    if (res) textLine(" [Yes] No ") else textLine("  Yes [No]")
+                }.runUntilSignal{
+                    onKeyPressed {
+                        when(key) {
+                            Keys.LEFT -> res = true
+                            Keys.RIGHT -> res = false
+                            Keys.ENTER -> { result = res; signal() }
+                        }
+                    }
+                }
+            }
+            return result
         }
     }
 }
