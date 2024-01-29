e86f8b25443f1af357fe9541f5cf5839b8a2514a
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2024-01-29 10:13:07 +0100

    Apply config exhaustively #2056

--- visualization/app/codeCharta/state/store/appSettings/isEdgeMetricVisible/isEdgeMetricVisible.reducer.ts
+++ visualization/app/codeCharta/state/store/appSettings/isEdgeMetricVisible/isEdgeMetricVisible.reducer.ts
@@ -1,8 +1,9 @@
 import { createReducer, on } from "@ngrx/store"
-import { toggleEdgeMetricVisible } from "./isEdgeMetricVisible.actions"
+import { setIsEdgeMetricVisible, toggleEdgeMetricVisible } from "./isEdgeMetricVisible.actions"
 
 export const defaultIsEdgeMetricVisible = true
 export const isEdgeMetricVisible = createReducer(
 	defaultIsEdgeMetricVisible,
+	on(setIsEdgeMetricVisible, (_state, action) => action.value),
 	on(toggleEdgeMetricVisible, state => !state)
 )
