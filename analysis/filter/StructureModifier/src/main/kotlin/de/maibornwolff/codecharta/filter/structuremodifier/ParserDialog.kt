c6f21849ed56ffe4c0fcf64468938c7f316703aa
nereboss <niklas.kneissl@gmail.com>
2024-02-02 10:12:29 +0100

    Test Inquirer on StructureModifier; WIP Commit #3483
    Still a lot of todos that need to be closed and comments that need to be implemented, as well as finding a solution for the session problem

--- analysis/filter/StructureModifier/src/main/kotlin/de/maibornwolff/codecharta/filter/structuremodifier/ParserDialog.kt
+++ analysis/filter/StructureModifier/src/main/kotlin/de/maibornwolff/codecharta/filter/structuremodifier/ParserDialog.kt
@@ -2,9 +2,9 @@ package de.maibornwolff.codecharta.filter.structuremodifier
 
 import com.github.kinquirer.KInquirer
 import com.github.kinquirer.components.promptInput
-import com.github.kinquirer.components.promptInputNumber
 import com.github.kinquirer.components.promptList
 import com.varabyte.kotter.foundation.session
+import com.varabyte.kotter.runtime.Session
 import de.maibornwolff.codecharta.tools.inquirer.myPromptCheckbox
 import de.maibornwolff.codecharta.tools.inquirer.myPromptConfirm
 import de.maibornwolff.codecharta.tools.inquirer.myPromptInput
@@ -13,110 +13,100 @@ import de.maibornwolff.codecharta.tools.inquirer.myPromptList
 import de.maibornwolff.codecharta.tools.interactiveparser.ParserDialogInterface
 import de.maibornwolff.codecharta.util.InputHelper
 import java.io.File
-import java.math.BigDecimal
 import java.nio.file.Paths
 
 class ParserDialog {
     companion object : ParserDialogInterface {
         override fun collectParserArgs(): List<String> {
             var inputFileName: String
+            var result: List<String> = listOf()
 
             // temporary test for kotter methods TODO: remove
             session {
-                myPromptInput("Input a test input:")
+                println("inside tester")
+                myPromptInput("Input a test input:", Paths.get("").toAbsolutePath().toString() + File.separator + "yourInput.cc.json")
                 myPromptInputNumber("input a test number:", "42")
                 myPromptConfirm("Confirm the test?")
                 myPromptList("select any", listOf("a", "b", "c", "1", "2", "3"))
                 myPromptCheckbox("select any", listOf("a", "b", "c", "1", "2", "3"))
             }
+            println("after tester")
 
-            do {
-                inputFileName =
-                    KInquirer.promptInput(
-                        message = "What is the cc.json file that has to be modified?",
-                        hint = Paths.get("").toAbsolutePath().toString() + File.separator + "yourInput.cc.json"
+            println("test before session")
+            session {
+                println("test inside session")
+                do {
+                    inputFileName = myPromptInput(
+                            message = "What is the cc.json file that has to be modified?",
+                            hint = Paths.get("").toAbsolutePath().toString() + File.separator + "yourInput.cc.json",
+                            canContainFolders = false
                     )
-            } while (!InputHelper.isInputValidAndNotNull(arrayOf(File(inputFileName)), canInputContainFolders = false))
+                } while (!InputHelper.isInputValidAndNotNull(arrayOf(File(inputFileName)), canInputContainFolders = false))
 
-            val selectedAction: String =
-                KInquirer.promptList(
-                    message = "Which action do you want to perform?",
-                    choices =
-                        listOf(
-                            StructureModifierAction.PRINT_STRUCTURE.descripton,
-                            StructureModifierAction.MIGRATE_MCC.descripton,
-                            StructureModifierAction.SET_ROOT.descripton,
-                            StructureModifierAction.MOVE_NODES.descripton,
-                            StructureModifierAction.REMOVE_NODES.descripton
+                val selectedAction: String = myPromptList(
+                        message = "Which action do you want to perform?",
+                        choices = listOf(
+                                StructureModifierAction.PRINT_STRUCTURE.descripton,
+                                StructureModifierAction.MIGRATE_MCC.descripton,
+                                StructureModifierAction.SET_ROOT.descripton,
+                                StructureModifierAction.MOVE_NODES.descripton,
+                                StructureModifierAction.REMOVE_NODES.descripton
                         )
                 )
 
-            return when (selectedAction) {
-                StructureModifierAction.PRINT_STRUCTURE.descripton -> listOf(inputFileName, *collectPrintArguments())
-                StructureModifierAction.MIGRATE_MCC.descripton -> listOf(inputFileName, *collectMCCArguments())
-                StructureModifierAction.SET_ROOT.descripton -> listOf(inputFileName, *collectSetRootArguments())
-                StructureModifierAction.MOVE_NODES.descripton ->
-                    listOf(
-                        inputFileName,
-                        *collectMoveNodesArguments()
-                    )
-
-                StructureModifierAction.REMOVE_NODES.descripton ->
-                    listOf(
-                        inputFileName,
-                        *collectRemoveNodesArguments()
-                    )
-
-                else -> listOf()
+                result = when (selectedAction) {
+                    StructureModifierAction.PRINT_STRUCTURE.descripton -> listOf(inputFileName, *collectPrintArguments())
+                    StructureModifierAction.MIGRATE_MCC.descripton -> listOf(inputFileName, *collectMCCArguments())
+                    StructureModifierAction.SET_ROOT.descripton -> listOf(inputFileName, *collectSetRootArguments())
+                    StructureModifierAction.MOVE_NODES.descripton -> listOf(inputFileName, *collectMoveNodesArguments())
+                    StructureModifierAction.REMOVE_NODES.descripton -> listOf(inputFileName, *collectRemoveNodesArguments())
+                    else -> listOf()
+                }
             }
         }
-
-        private fun collectPrintArguments(): Array<String> {
-            val printLevels: BigDecimal =
-                KInquirer.promptInputNumber(
-                    message = "How many print levels do you want to print?",
-                    default = "0",
-                    hint = "0"
-                )
-            return arrayOf("--print-levels=$printLevels")
-        }
-
-        private fun collectMCCArguments(): Array<String> {
-            val newName: String =
-                KInquirer.promptList(
-                    message = "This action will rename the mcc metric. Which should the new name be?",
-                    choices = listOf("complexity", "sonar_complexity")
-                )
-            val renameParam = if (newName == "sonar_complexity") "--rename-mcc=sonar" else "--rename-mcc"
-            val outputFileName = collectOutputFileName()
-            return arrayOf(renameParam, "--output-file=$outputFileName")
-        }
-
-        private fun collectSetRootArguments(): Array<String> {
-            val setRoot: String =
-                KInquirer.promptInput(message = "What path within the project should be extracted as the new root?")
-            val outputFileName = collectOutputFileName()
-            return arrayOf("--set-root=$setRoot", "--output-file=$outputFileName")
-        }
-
-        private fun collectMoveNodesArguments(): Array<String> {
-            val moveFrom: String =
-                KInquirer.promptInput(
-                    message = "What path should be moved (contained children will be moved as well)?"
-                )
-            val moveTo: String = KInquirer.promptInput(message = "What is the target path to move them?")
-            val outputFileName = collectOutputFileName()
-            return arrayOf("--move-from=$moveFrom", "--move-to=$moveTo", "--output-file=$outputFileName")
-        }
-
-        private fun collectRemoveNodesArguments(): Array<String> {
-            val remove: String = KInquirer.promptInput(message = "What are the paths of the nodes to be removed?")
-            val outputFileName = collectOutputFileName()
-            return arrayOf("--remove=$remove", "--output-file=$outputFileName")
-        }
-
-        private fun collectOutputFileName(): String {
-            return KInquirer.promptInput(message = "What is the name of the output file?")
-        }
     }
+}
+
+private fun Session.collectPrintArguments(): Array<String> { //diese funktionen als extension vom section scope setzen? können wir sie dann noch private lassen?
+    val printLevels: String =
+            myPromptInputNumber(message = "How many print levels do you want to print?", hint = "0")
+    return arrayOf("--print-levels=$printLevels")
+}
+
+private fun collectMCCArguments(): Array<String> {
+    val newName: String =
+        KInquirer.promptList(
+            message = "This action will rename the mcc metric. Which should the new name be?",
+            choices = listOf("complexity", "sonar_complexity")
+        )
+    val renameParam = if (newName == "sonar_complexity") "--rename-mcc=sonar" else "--rename-mcc"
+    val outputFileName = collectOutputFileName()
+    return arrayOf(renameParam, "--output-file=$outputFileName")
+}
+
+private fun Session.collectSetRootArguments(): Array<String> {
+    val setRoot: String =
+            myPromptInput(message = "What path within the project should be extracted as the new root?")
+    val outputFileName = collectOutputFileName()
+    return arrayOf("--set-root=$setRoot", "--output-file=$outputFileName")
+}
+
+private fun Session.collectMoveNodesArguments(): Array<String> {
+    val moveFrom: String =
+            myPromptInput(message = "What path should be moved (contained children will be moved as well)?")
+    val moveTo: String =
+            myPromptInput(message = "What is the target path to move them?")
+    val outputFileName = collectOutputFileName()
+    return arrayOf("--move-from=$moveFrom", "--move-to=$moveTo", "--output-file=$outputFileName")
+}
+
+private fun Session.collectRemoveNodesArguments(): Array<String> {
+    val remove: String =
+            myPromptInput(message = "What are the paths of the nodes to be removed?")
+    val outputFileName = collectOutputFileName()
+    return arrayOf("--remove=$remove", "--output-file=$outputFileName")
+}
+
+private fun collectOutputFileName(): String {
+    return KInquirer.promptInput(message = "What is the name of the output file?")
 }
