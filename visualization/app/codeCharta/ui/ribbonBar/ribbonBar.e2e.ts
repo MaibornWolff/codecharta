61a4f34fb62b4061a4a9b04469ecb76b14b82f3e
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2024-02-21 09:34:59 +0100

    Group actions requiring saving of state #401

--- visualization/app/codeCharta/ui/ribbonBar/ribbonBar.e2e.ts
+++ visualization/app/codeCharta/ui/ribbonBar/ribbonBar.e2e.ts
@@ -2,22 +2,14 @@ import { CC_URL, clearIndexedDB, goto } from "../../../puppeteer.helper"
 import { RibbonBarPageObject } from "./ribbonBar.po"
 import { SearchPanelPageObject } from "../searchPanel/searchPanel.po"
 
-//Commented out flaky test
-
 describe("RibbonBar", () => {
 	const sampleMap = `sample3.cc.json`
 	let searchPanel: SearchPanelPageObject
-	//let searchPanelModeSelector: SearchPanelModeSelectorPageObject
 	let ribbonBar: RibbonBarPageObject
-	//let metricChooser: MetricChooserPageObject
-	//let mapTreeViewLevel: MapTreeViewLevelPageObject
 
 	beforeEach(async () => {
 		searchPanel = new SearchPanelPageObject()
-		//searchPanelModeSelector = new SearchPanelModeSelectorPageObject()
 		ribbonBar = new RibbonBarPageObject()
-		//metricChooser = new MetricChooserPageObject()
-		//mapTreeViewLevel = new MapTreeViewLevelPageObject()
 
 		await goto()
 	})
@@ -25,15 +17,6 @@ describe("RibbonBar", () => {
 	afterEach(async () => {
 		await clearIndexedDB()
 	})
-
-	/*it("hovering over a folder should display the sum of metric of all children", async () => {
-		await searchPanelModeSelector.toggleTreeView()
-
-		await mapTreeViewLevel.hoverNode("/root")
-
-		const actual = await metricChooser.getAreaMetricValue()
-		expect(actual).toContain("600")
-	})*/
 
 	it("should open a section, open the search bar and close the section again automatically", async () => {
 		const areaPanel = "area-metric"
