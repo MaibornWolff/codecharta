b0063ae99c1cadc2d190bd7caa2ebb2459615f45
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2024-03-04 09:59:37 +0100

    Remove transition from screenshot-button #401

--- visualization/app/codeCharta/ui/codeMap/codeMap.module.ts
+++ visualization/app/codeCharta/ui/codeMap/codeMap.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core"
 import { CommonModule } from "@angular/common"
 import { ViewCubeModule } from "../viewCube/viewCube.module"
 import { CodeMapComponent } from "./codeMap.component"
+import { AttributeSideBarModule } from "../attributeSideBar/attributeSideBar.module"
 
 @NgModule({
-	imports: [CommonModule, ViewCubeModule],
+	imports: [CommonModule, ViewCubeModule, AttributeSideBarModule],
 	declarations: [CodeMapComponent],
 	exports: [CodeMapComponent]
 })
