01a4cd641c9c91ff416eea84a21268f4f5a7ac9d
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2024-03-18 17:27:31 +0100

    Display correct node type for top level node #1267

--- visualization/app/codeCharta/ui/metricChooser/metricChooserType/isNodeALeaf.selector.ts
+++
@@ -1,7 +0,0 @@
-import { createSelector } from "@ngrx/store"
-import { hoveredNodeSelector } from "../../../state/selectors/hoveredNode.selector"
-import { isLeaf } from "../../../util/codeMapHelper"
-
-export const _isHoveredNodeALeaf = hoveredNode => hoveredNode && isLeaf(hoveredNode)
-
-export const isHoveredNodeALeafSelector = createSelector(hoveredNodeSelector, _isHoveredNodeALeaf)
--- visualization/app/codeCharta/ui/metricChooser/metricChooserType/metricChooserType.component.spec.ts
+++ visualization/app/codeCharta/ui/metricChooser/metricChooserType/metricChooserType.component.spec.ts
@@ -1,28 +1,32 @@
-import { render } from "@testing-library/angular"
-import { isHoveredNodeALeafSelector } from "./isNodeALeaf.selector"
-import { MetricChooserTypeComponent } from "./metricChooserType.component"
 import { TestBed } from "@angular/core/testing"
-import { MockStore, provideMockStore } from "@ngrx/store/testing"
+import { provideMockStore } from "@ngrx/store/testing"
+import { render } from "@testing-library/angular"
+import { Observable, of } from "rxjs"
+import { DIFFERENT_NODE } from "../../../util/dataMocks"
+import { NodeSelectionService } from "../nodeSelection.service"
+import { MetricChooserTypeComponent } from "./metricChooserType.component"
 
-describe("metricChooserType", () => {
+describe("metricChooserTypeComponent", () => {
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			providers: [provideMockStore({ selectors: [{ selector: isHoveredNodeALeafSelector, value: true }] })]
+			providers: [
+				{ provide: NodeSelectionService, useValue: { createNodeObservable: jest.fn(() => new Observable(null)) } },
+				provideMockStore()
+			]
 		})
 	})
+	it("should not be hidden, when hovered node is a folder", async () => {
+		const { container } = await render(MetricChooserTypeComponent)
+
+		expect(container.querySelector("span").hidden).toBe(false)
+	})
+
 	it("should be hidden, when hovered node is a leaf", async () => {
+		TestBed.overrideProvider(NodeSelectionService, {
+			useValue: { createNodeObservable: jest.fn().mockReturnValue(of(DIFFERENT_NODE)) }
+		})
 		const { container } = await render(MetricChooserTypeComponent)
 
 		expect(container.querySelector("span").hidden).toBe(true)
-	})
-
-	it("should not be hidden, when hovered node is a folder", async () => {
-		const { container, detectChanges } = await render(MetricChooserTypeComponent)
-		const store = TestBed.inject(MockStore)
-		store.overrideSelector(isHoveredNodeALeafSelector, false)
-		store.refreshState()
-		detectChanges()
-
-		expect(container.querySelector("span").hidden).toBe(false)
 	})
 })
