de2970a1c9235a2b1ff681ebd0ec5fb141509602
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2024-03-18 13:43:07 +0100

    Display metric values for top level node #1267

--- visualization/app/codeCharta/ui/metricChooser/metricChooserValue/metricChooserValue.component.ts
+++ visualization/app/codeCharta/ui/metricChooser/metricChooserValue/metricChooserValue.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input, ViewEncapsulation } from "@angular/core"
 import { Store } from "@ngrx/store"
-import { Observable } from "rxjs"
-import { CodeMapNode, PrimaryMetrics, CcState } from "../../../codeCharta.model"
+import { Observable, combineLatest, filter, map } from "rxjs"
+import { CcState, CodeMapNode, Node, PrimaryMetrics } from "../../../codeCharta.model"
+import { AccumulatedData, accumulatedDataSelector } from "../../../state/selectors/accumulatedData/accumulatedData.selector"
 import { hoveredNodeSelector } from "../../../state/selectors/hoveredNode.selector"
 import { primaryMetricNamesSelector } from "../../../state/selectors/primaryMetrics/primaryMetricNames.selector"
+import { dynamicSettingsSelector } from "../../../state/store/dynamicSettings/dynamicSettings.selector"
+import { CodeMapRenderService } from "../../codeMap/codeMap.render.service"
 
 @Component({
 	selector: "cc-metric-chooser-value",
@@ -13,11 +16,34 @@ import { primaryMetricNamesSelector } from "../../../state/selectors/primaryMetr
 export class MetricChooserValueComponent {
 	@Input() metricFor: keyof PrimaryMetrics
 
-	hoveredNode$: Observable<CodeMapNode | undefined>
+	node$: Observable<CodeMapNode | Node>
 	primaryMetricNames$: Observable<PrimaryMetrics>
 
-	constructor(store: Store<CcState>) {
-		this.hoveredNode$ = store.select(hoveredNodeSelector)
-		this.primaryMetricNames$ = store.select(primaryMetricNamesSelector)
+	constructor(private store: Store<CcState>, private codeMapRenderService: CodeMapRenderService) {
+		const hoveredNode$ = this.store.select(hoveredNodeSelector)
+		const topLevelNode$ = this.createTopLevelNodeObservable()
+
+		this.node$ = this.combineHoveredAndTopLevelNodes(hoveredNode$, topLevelNode$)
+		this.primaryMetricNames$ = this.store.select(primaryMetricNamesSelector)
+	}
+
+	private createTopLevelNodeObservable(): Observable<Node> {
+		return combineLatest([this.store.select(accumulatedDataSelector), this.store.select(dynamicSettingsSelector)]).pipe(
+			filter(([accumulatedData]) => Boolean(accumulatedData.unifiedMapNode)),
+			map(([accumulatedData]) => this.findTopLevelNode(accumulatedData))
+		)
+	}
+
+	private findTopLevelNode(accumulatedData: AccumulatedData): Node {
+		const nodes = this.codeMapRenderService.getNodes(accumulatedData.unifiedMapNode)
+		const visibleSortedNodes = this.codeMapRenderService.getVisibleNodes(nodes)
+		return visibleSortedNodes.reduce((previous, current) => (previous.attributes.unary > current.attributes.unary ? previous : current))
+	}
+
+	private combineHoveredAndTopLevelNodes(
+		hoveredNode$: Observable<CodeMapNode>,
+		topLevelNode$: Observable<Node>
+	): Observable<CodeMapNode | Node> {
+		return combineLatest([hoveredNode$, topLevelNode$]).pipe(map(([hoveredNode, topLevelNode]) => hoveredNode ?? topLevelNode))
 	}
 }
