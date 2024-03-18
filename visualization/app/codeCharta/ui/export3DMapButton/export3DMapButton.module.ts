5ca316bdb57976cfb9b98aa1e6af909c241749af
Benedikt Mehl <benedikt.mehl@maibornwolff.de>
2024-03-18 18:05:31 +0100

    Add baseplate

--- visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.module.ts
+++ visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
 import { Export3DMapButtonComponent } from "./export3DMapButton.component"
 import { ActionIconModule } from "../actionIcon/actionIcon.module"
+import { HttpClientModule } from "@angular/common/http"
 
 @NgModule({
-	imports: [CommonModule, ActionIconModule],
+	imports: [CommonModule, ActionIconModule, HttpClientModule],
 	declarations: [Export3DMapButtonComponent],
 	exports: [Export3DMapButtonComponent]
 })
