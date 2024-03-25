05de0a08bfbfa934616e8fa0dc228211471d2f82
Benedikt Mehl <benedikt.mehl@maibornwolff.de>
2024-03-25 17:00:22 +0100

    feat: Add print preview

--- visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.module.ts
+++ visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from "@angular/core"
 import { Export3DMapButtonComponent } from "./export3DMapButton.component"
 import { ActionIconModule } from "../actionIcon/actionIcon.module"
 import { HttpClientModule } from "@angular/common/http"
+import { MaterialModule } from "../../../material/material.module"
+import { Export3DMapDialogComponent } from "./export3DMapDialog/export3DMapDialog.component"
+import { DisplayQualitySelectionModule } from "../toolBar/globalConfigurationButton/globalConfigurationDialog/displayQualitySelection/displayQualitySelection.module"
+import { MapLayoutSelectionModule } from "../toolBar/globalConfigurationButton/globalConfigurationDialog/mapLayoutSelection/mapLayoutSelection.module"
+import { ResetSettingsButtonModule } from "../resetSettingsButton/resetSettingsButton.module"
 
 @NgModule({
-	imports: [CommonModule, ActionIconModule, HttpClientModule],
-	declarations: [Export3DMapButtonComponent],
+	imports: [
+		CommonModule,
+		ActionIconModule,
+		HttpClientModule,
+		MaterialModule,
+		DisplayQualitySelectionModule,
+		MapLayoutSelectionModule,
+		ResetSettingsButtonModule
+	],
+	declarations: [Export3DMapButtonComponent, Export3DMapDialogComponent],
 	exports: [Export3DMapButtonComponent]
 })
 export class Export3DMapButtonModule {}
