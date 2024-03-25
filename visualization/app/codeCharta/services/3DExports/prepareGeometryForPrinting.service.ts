45c8ecc1b093a10be7b68aff61e93e08f781de26
Benedikt Mehl <benedikt.mehl@maibornwolff.de>
2024-03-25 14:23:42 +0100

    feat: Add MW Logo

--- visualization/app/codeCharta/services/3DExports/prepareGeometryForPrinting.service.ts
+++ visualization/app/codeCharta/services/3DExports/prepareGeometryForPrinting.service.ts
@@ -1,4 +1,6 @@
-import { BufferGeometry, ExtrudeGeometry, FontLoader, Mesh, MeshBasicMaterial, Shape, TextGeometry } from "three"
+import { BufferGeometry, DoubleSide, ExtrudeGeometry, FontLoader, Mesh, MeshBasicMaterial, Shape, TextGeometry } from "three"
+import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader"
+import { BufferGeometryUtils } from "three/examples/jsm/utils/BufferGeometryUtils"
 
 export interface GeometryOptions {
 	width: number
@@ -33,6 +35,9 @@ export async function prepareGeometryForPrinting(mapMesh: Mesh, geometryOptions:
 			console.error("Error creating text:", error)
 		}
 	}
+
+	const mwLogo = await createMWLogo(geometryOptions)
+	printMesh.attach(mwLogo)
 
 	return printMesh
 }
@@ -123,69 +128,54 @@ async function createTextGeometry(text: string): Promise<TextGeometry> {
 	})
 }
 
-/*
+async function createMWLogo(geometryOptions): Promise<Mesh> {
+	const mwLogoGeometry = await createSvgGeometry("mw_logo.svg", 1)
 
-baseplate = (x, y, z, mapSideOffset, roundRadius = Number.MAX_VALUE): Geom3 => {
-	const maxRoundRadius = Math.sqrt(2 * Math.pow(mapSideOffset, 2)) / (Math.sqrt(2) - 1) - 1
-	if (maxRoundRadius < roundRadius) {
-		roundRadius = maxRoundRadius
-	}
+	mwLogoGeometry.rotateZ(Math.PI)
+	mwLogoGeometry.scale(0.13, 0.13, 1)
+	mwLogoGeometry.translate(
+		geometryOptions.width + geometryOptions.mapSideOffset + 5,
+		-geometryOptions.width + geometryOptions.mapSideOffset + 43,
+		geometryOptions.baseplateHeight
+	)
 
-	const flatBaselate = roundedRectangle({ size: [x, y], roundRadius, center: [x / 2 - mapSideOffset, y / 2 - mapSideOffset] })
-	let baseplate = extrudeLinear({ height: z }, flatBaselate)
-	baseplate = colorize(colorNameToRgb("gray"), baseplate)
-	return baseplate
+	const material = new MeshBasicMaterial({ color: 0x00_00_ff, side: DoubleSide })
+	const logoMesh = new Mesh(mwLogoGeometry, material)
+	logoMesh.name = "MW Logo"
+	return logoMesh
 }
 
-const print = (txt, x, y, z, size, t, font = "Arial", halign = "left", valign = "baseline") => {
-	const txtModel = text({text: txt, height: t, align: halign, valign, font, size});
-	return translate([x, y, z], txtModel);
-}
+async function createSvgGeometry(fileName: string, depth): Promise<BufferGeometry> {
+	const filePath = `codeCharta/assets/${fileName}`
+	//load the svg file
+	const loader = new SVGLoader()
+	return new Promise((resolve, reject) => {
+		loader.load(
+			filePath,
+			function (data) {
+				const paths = data.paths
+				const geometries: BufferGeometry[] = []
 
+				for (const path of paths) {
+					const shapes = path.toShapes(false, false)
 
+					for (const shape of shapes) {
+						const geometry = new ExtrudeGeometry(shape, {
+							depth,
+							bevelEnabled: false
+						})
+						geometries.push(geometry)
+					}
+				}
 
-const backside = (thickness = 0.6) => {
-	const back = union(
-		logo("logos/mw_logo_text.svg", -20, 10, 0, thickness),
-		translate([15, 60, 0],
-			union(
-				logo("icons/area.svg", 0, 30-1, 0, 8, 8, thickness),
-				logo("icons/height.svg", 0, 15-1, 0, 8, 8, thickness),
-				logo("icons/color.svg", 0, -1, 0, 8, 8, thickness),
-				print("rloc (total: 338,540 real lines)", 15, 30, 0, 6, thickness, "Liberation Mono:style=Bold"),
-				print("mcc  (min: 0, max: 338)", 15, 15, 0, 6, thickness, "Liberation Mono:style=Bold"),
-				print("test line coverage", 15, 0, 0, 6, thickness, "Liberation Mono:style=Bold"),
-				print("     (0-33% / 33-66% / 66-100%)", 15, -10, 0, 6, thickness, "Liberation Mono:style=Bold")
-			)
-		),
-		print("IT Stabilization & Modernization", baseplate_x / 2, 135, 0, 8, thickness, "Arial:style=Bold", "center"),
-		print("maibornwolff.de/service/it-sanierung", baseplate_x / 2, 122, 0, 6, thickness, "Arial:style=Bold", "center"),
-		print("maibornwolff.github.io/codecharta", baseplate_x / 2, 20, 0, 6, thickness, "Arial:style=Bold", "center")
-	);
-	return translate([baseplate_x, 0, -0.01], mirror([1,0,0], back));
-}
-
-const mcolor = (c) => {
-	return (geometry) => color(c, geometry);
-}
-
-
-logo = async (file: string, positionOfUpperLeftCorner: Vec3, size: Vec3, rotate = 0, color = "white"): Promise<Geom3> => {
-	const svgData = await firstValueFrom(this.httpClient.get(`codeCharta/assets/${file}`, { responseType: "text" }).pipe(timeout(5000)))
-	const svgModels: Geom2[] = deserialize({ output: "geometry", addMetaData: false, target: "geom2", pathSelfClosed: "trim" }, svgData)
-	const models: Geom3[] = svgModels.map(object => {
-		return extrudeLinear({ height: size[2] }, object)
+				const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries(geometries)
+				resolve(mergedGeometry)
+			},
+			undefined,
+			function (error) {
+				console.error(`Error loading ${fileName}`)
+				reject(error)
+			}
+		)
 	})
-
-	let model: Geom3 = union(models) //normally I would have used "union" here - but somehow union and intersect seem to be mistakenly swapped in this library
-	model = scale([size[0], size[1], 1], model)
-	model = rotateZ((rotate / 180) * Math.PI, model)
-	const centerPosition: Vec3 = [
-		positionOfUpperLeftCorner[0] - size[0] * 20,
-		positionOfUpperLeftCorner[1],
-		positionOfUpperLeftCorner[2] + size[2] / 2
-	]
-	model = center({ relativeTo: centerPosition }, model)
-	model = colorize(colorNameToRgb(color), model)
-	return model
-}*/
+}
