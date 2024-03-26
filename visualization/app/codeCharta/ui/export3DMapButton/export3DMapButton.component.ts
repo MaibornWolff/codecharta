7222ba18606d1858a2795496db2d0ff31028168e
Benedikt Mehl <benedikt.mehl@maibornwolff.de>
2024-03-26 15:03:27 +0100

    feat: Check if ColorMode is absolute

--- visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.component.ts
+++ visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.component.ts
@@ -1,6 +1,11 @@
 import { Component, ViewEncapsulation } from "@angular/core"
 import { MatDialog } from "@angular/material/dialog"
 import { Export3DMapDialogComponent } from "./export3DMapDialog/export3DMapDialog.component"
+import { Store } from "@ngrx/store"
+import { CcState, ColorMode } from "../../codeCharta.model"
+import { colorModeSelector } from "../../state/store/dynamicSettings/colorMode/colorMode.selector"
+import { take } from "rxjs"
+import { ErrorDialogComponent } from "../dialogs/errorDialog/errorDialog.component"
 
 @Component({
 	selector: "cc-export-threed-map-button",
@@ -8,11 +13,34 @@ import { Export3DMapDialogComponent } from "./export3DMapDialog/export3DMapDialo
 	encapsulation: ViewEncapsulation.None
 })
 export class Export3DMapButtonComponent {
-	constructor(private dialog: MatDialog) {}
+	constructor(private dialog: MatDialog, private store: Store<CcState>) {}
 
 	export3DMap() {
-		this.dialog.open(Export3DMapDialogComponent, {
-			panelClass: ".cc-export-3D-map-dialog"
-		})
+		let colorMode: ColorMode
+		this.store
+			.select(colorModeSelector)
+			.pipe(take(1))
+			.subscribe(colorModeObs => {
+				colorMode = colorModeObs
+			})
+
+		if (colorMode !== ColorMode.absolute) {
+			this.dialog.open(ErrorDialogComponent, {
+				data: this.buildErrorDialog()
+			})
+		} else {
+			this.dialog.open(Export3DMapDialogComponent, {
+				panelClass: ".cc-export-3D-map-dialog"
+			})
+		}
+	}
+
+	buildErrorDialog(): { title; message } {
+		const title = "Map could not be exported"
+		const message =
+			"<p>3D map can only be exported when <strong>color mode</strong> is set to <strong>absolute</strong>.<br>" +
+			'<i class="fa fa-info-circle"></i> You can change this under Color Metric Options.<p>'
+
+		return { title, message }
 	}
 }
