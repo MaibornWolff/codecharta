887961748ea0155959452ea4a4f764d63290b397
Benedikt Mehl <benedikt.mehl@maibornwolff.de>
2024-03-27 15:18:11 +0100

    feat: Add size slider

--- visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.module.ts
+++ visualization/app/codeCharta/ui/export3DMapButton/export3DMapButton.module.ts
@@ -1,25 +1,17 @@
-import { CommonModule } from "@angular/common"
-import { NgModule } from "@angular/core"
-import { Export3DMapButtonComponent } from "./export3DMapButton.component"
-import { ActionIconModule } from "../actionIcon/actionIcon.module"
-import { HttpClientModule } from "@angular/common/http"
-import { MaterialModule } from "../../../material/material.module"
-import { Export3DMapDialogComponent } from "./export3DMapDialog/export3DMapDialog.component"
-import { DisplayQualitySelectionModule } from "../toolBar/globalConfigurationButton/globalConfigurationDialog/displayQualitySelection/displayQualitySelection.module"
-import { MapLayoutSelectionModule } from "../toolBar/globalConfigurationButton/globalConfigurationDialog/mapLayoutSelection/mapLayoutSelection.module"
-import { ResetSettingsButtonModule } from "../resetSettingsButton/resetSettingsButton.module"
+import {CommonModule} from "@angular/common"
+import {NgModule} from "@angular/core"
+import {Export3DMapButtonComponent} from "./export3DMapButton.component"
+import {ActionIconModule} from "../actionIcon/actionIcon.module"
+import {HttpClientModule} from "@angular/common/http"
+import {MaterialModule} from "../../../material/material.module"
+import {Export3DMapDialogComponent} from "./export3DMapDialog/export3DMapDialog.component"
+import {MatSliderModule} from "@angular/material/slider";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-	imports: [
-		CommonModule,
-		ActionIconModule,
-		HttpClientModule,
-		MaterialModule,
-		DisplayQualitySelectionModule,
-		MapLayoutSelectionModule,
-		ResetSettingsButtonModule
-	],
+	imports: [CommonModule, ActionIconModule, HttpClientModule, MaterialModule, MatSliderModule, FormsModule],
 	declarations: [Export3DMapButtonComponent, Export3DMapDialogComponent],
 	exports: [Export3DMapButtonComponent]
 })
-export class Export3DMapButtonModule {}
+export class Export3DMapButtonModule {
+}
