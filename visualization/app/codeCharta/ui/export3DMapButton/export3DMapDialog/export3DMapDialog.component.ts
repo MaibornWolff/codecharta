eceeb4f5a06a2fed99444bdb8b0fe70fa724827c
Benedikt Mehl <benedikt.mehl@maibornwolff.de>
2024-04-10 15:12:57 +0200

    refactor: preview3DPrintMesh has printMesh now instead of the dialog

--- visualization/app/codeCharta/ui/export3DMapButton/export3DMapDialog/export3DMapDialog.component.ts
+++ visualization/app/codeCharta/ui/export3DMapButton/export3DMapDialog/export3DMapDialog.component.ts
@@ -1,20 +1,24 @@
-import {filesSelector} from "../../../state/store/files/files.selector"
-import {accumulatedDataSelector} from "../../../state/selectors/accumulatedData/accumulatedData.selector"
-import {FileNameHelper} from "../../../util/fileNameHelper"
-import {isDeltaState} from "../../../model/files/files.helper"
-import {FileDownloader} from "../../../util/fileDownloader"
-import {Component, ElementRef, Input, ViewChild, ViewEncapsulation} from "@angular/core"
-import {State, Store} from "@ngrx/store"
-import {CcState, NodeMetricData} from "../../../codeCharta.model"
-import {ThreeSceneService} from "../../codeMap/threeViewer/threeSceneService"
-import {Color, Mesh, PerspectiveCamera, Scene, ShaderMaterial, Vector3, WebGLRenderer} from "three"
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
-import {STLExporter} from "three/examples/jsm/exporters/STLExporter"
-import {metricTitles} from "../../../util/metric/metricTitles"
-import {serialize3mf} from "../../../services/3DExports/serialize3mf.service"
-import {firstValueFrom, map} from "rxjs"
-import {metricDataSelector} from "../../../state/selectors/accumulatedData/metricData/metricData.selector"
-import {GeometryOptions, preview3DPrintMeshBuilder} from "../../../services/3DExports/preview3DPrintMeshBuilder"
+import { filesSelector } from "../../../state/store/files/files.selector"
+import { accumulatedDataSelector } from "../../../state/selectors/accumulatedData/accumulatedData.selector"
+import { FileNameHelper } from "../../../util/fileNameHelper"
+import { isDeltaState } from "../../../model/files/files.helper"
+import { FileDownloader } from "../../../util/fileDownloader"
+import { Component, ElementRef, Input, ViewChild, ViewEncapsulation } from "@angular/core"
+import { State, Store } from "@ngrx/store"
+import { CcState, NodeMetricData } from "../../../codeCharta.model"
+import { ThreeSceneService } from "../../codeMap/threeViewer/threeSceneService"
+import { Color, Mesh, PerspectiveCamera, Scene, ShaderMaterial, Vector3, WebGLRenderer } from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { STLExporter } from "three/examples/jsm/exporters/STLExporter"
+import { metricTitles } from "../../../util/metric/metricTitles"
+import { serialize3mf } from "../../../services/3DExports/serialize3mf.service"
+import { firstValueFrom, map } from "rxjs"
+import { metricDataSelector } from "../../../state/selectors/accumulatedData/metricData/metricData.selector"
+import {
+	calculateMaxPossibleWidthForPreview3DPrintMesh,
+	GeometryOptions,
+	Preview3DPrintMesh
+} from "../../../services/3DExports/preview3DPrintMesh"
 
 interface printer {
 	x: number
@@ -30,29 +34,27 @@ interface printer {
 })
 export class Export3DMapDialogComponent {
 	@ViewChild("rendererContainer") rendererContainer: ElementRef
-	@Input() logoColor: string = '#ffffff'
+	@Input() logoColor = "#ffffff"
 
 	private printPreviewScene: Scene
-	private previewMeshBuilder: preview3DPrintMeshBuilder
+	currentSize: Vector3
 
-	isFileSelected: boolean = false;
+	isFileSelected = false
 	isPrintMeshLoaded = false
 	frontText = "CodeCharta"
 
 	printerOptions = ["prusaMk3s", "bambuA1", "prusaXL"]
 	printers: { [key: string]: printer } = {
-		prusaMk3s: {x: 245, y: 205, z: 205, numberOfColors: 1},
-		bambuA1: {x: 251, y: 251, z: 251, numberOfColors: 4},
-		prusaXL: {x: 355, y: 355, z: 355, numberOfColors: 5}
+		prusaMk3s: { x: 245, y: 205, z: 205, numberOfColors: 1 },
+		bambuA1: { x: 251, y: 251, z: 251, numberOfColors: 4 },
+		prusaXL: { x: 355, y: 355, z: 355, numberOfColors: 5 }
 	}
 	selectedPrinter = this.printerOptions[2]
 	private currentNumberOfColors: number
 
 	maxWidth: number
 	wantedWidth: number
-	currentWidth: number
-	currentDepth: number
-	currentHeight: number
+	private previewMesh: Preview3DPrintMesh
 
 	areaMetric: string
 	heightMetric: string
@@ -60,103 +62,93 @@ export class Export3DMapDialogComponent {
 	nodeMetricData: NodeMetricData[]
 
 	constructor(private store: Store<CcState>, private state: State<CcState>, private threeSceneService: ThreeSceneService) {
-		this.previewMeshBuilder = new preview3DPrintMeshBuilder()
-
-		this.maxWidth = this.previewMeshBuilder.calculateMaxWidth(new Vector3(this.printers[this.selectedPrinter].x, this.printers[this.selectedPrinter].y, this.printers[this.selectedPrinter].z), this.threeSceneService.getMapMesh().getThreeMesh())
-		this.currentWidth = this.maxWidth
+		//console.log(this.threeSceneService)
+		this.maxWidth = calculateMaxPossibleWidthForPreview3DPrintMesh(
+			new Vector3(
+				this.printers[this.selectedPrinter].x,
+				this.printers[this.selectedPrinter].y,
+				this.printers[this.selectedPrinter].z
+			),
+			this.threeSceneService.getMapMesh().getThreeMesh()
+		)
+		this.currentSize = new Vector3()
+		this.currentSize.x = this.maxWidth
 		this.wantedWidth = this.maxWidth
 		this.currentNumberOfColors = this.printers[this.selectedPrinter].numberOfColors
+		this.isPrintMeshLoaded = false
 	}
 
-	async ngOnInit(): Promise<void> {
+	ngOnInit() {
 		this.areaMetric = this.state.getValue().dynamicSettings.areaMetric
 		this.heightMetric = this.state.getValue().dynamicSettings.heightMetric
 		this.colorMetric = this.state.getValue().dynamicSettings.colorMetric
 
-		this.nodeMetricData = await firstValueFrom(
-			this.store
-				.select(metricDataSelector)
-				.pipe(map(metricData => metricData.nodeMetricData))
-		)
-
-		this.nodeMetricData = this.nodeMetricData.filter(
-			metric => metric.name === this.areaMetric || metric.name === this.heightMetric || metric.name === this.colorMetric
-		);
-
-		const geometryOptions = this.initGeometryOptions()
-		const initMeshBuilder = this.previewMeshBuilder.initialize(geometryOptions)
-
-		await initMeshBuilder
-		await this.createScene()
+		firstValueFrom(this.store.select(metricDataSelector).pipe(map(metricData => metricData.nodeMetricData))).then(nodeMetricData => {
+			this.nodeMetricData = nodeMetricData.filter(
+				metric => metric.name === this.areaMetric || metric.name === this.heightMetric || metric.name === this.colorMetric
+			)
+			this.createScene().then(() => (this.isPrintMeshLoaded = true))
+		})
 	}
 
 	onScaleChange() {
-		const printMesh = (this.printPreviewScene as Scene).getObjectByName("PrintMesh") as Mesh
-		this.previewMeshBuilder.updateMapSize(printMesh, this.currentWidth, this.wantedWidth)
-		this.calculateCurrentSize(printMesh)
+		this.previewMesh.updateMapSize(this.wantedWidth)
+		this.currentSize = this.previewMesh.getSize()
 	}
 	onFrontTextChange() {
-		const printMesh = (this.printPreviewScene as Scene).getObjectByName("PrintMesh") as Mesh
-		this.previewMeshBuilder.updateFrontText(printMesh, this.frontText)
+		this.previewMesh.updateFrontText(this.frontText)
 	}
 	onFileSelected(event) {
-		const printMesh = this.printPreviewScene.getObjectByName("PrintMesh") as Mesh;
-		const file: File = event.target.files[0];
+		const file: File = event.target.files[0]
 		if (file) {
-			this.isFileSelected = true; // Set isFileSelected to true when a file is selected
-			const reader = new FileReader();
-			reader.readAsDataURL(file);
+			this.isFileSelected = true // Set isFileSelected to true when a file is selected
+			const reader = new FileReader()
+			reader.readAsDataURL(file)
 			reader.onload = () => {
-				this.previewMeshBuilder.addCustomLogo(printMesh, reader.result as string);
-			};
+				this.previewMesh.addCustomLogo(reader.result as string)
+			}
 		} else {
-			this.isFileSelected = false; // Set isFileSelected to false when no file is selected
+			this.isFileSelected = false
 		}
 	}
-
 	onRotateLogo() {
-		this.previewMeshBuilder.rotateCustomLogo(this.printPreviewScene.getObjectByName("PrintMesh") as Mesh)
+		this.previewMesh.rotateCustomLogo()
 	}
 
 	onFlipLogo() {
-		this.previewMeshBuilder.flipCustomLogo(this.printPreviewScene.getObjectByName("PrintMesh") as Mesh)
+		this.previewMesh.flipCustomLogo()
 	}
-
 	onSelectedPrinterChange() {
 		const wantedNumberOfColors = this.printers[this.selectedPrinter].numberOfColors
 		if (this.currentNumberOfColors !== wantedNumberOfColors) {
-			const printMesh = this.printPreviewScene.getObjectByName("PrintMesh") as Mesh
 			const originalMesh = this.threeSceneService.getMapMesh().getThreeMesh()
-			this.previewMeshBuilder.updateNumberOfColors(originalMesh, printMesh, wantedNumberOfColors)
+			this.previewMesh.updateNumberOfColors(originalMesh, wantedNumberOfColors)
 			this.currentNumberOfColors = wantedNumberOfColors
 		}
-		this.makeMapMaxSize(this.printPreviewScene.getObjectByName("PrintMesh") as Mesh)
+		this.makeMapMaxSize()
 		this.updateCameraPosition(this.printPreviewScene.getObjectByName("camera") as PerspectiveCamera)
 	}
 	onLogoColorChange(newColor: string) {
-		this.logoColor = newColor;
-		const printMesh = this.printPreviewScene.getObjectByName("PrintMesh") as Mesh;
-		this.previewMeshBuilder.updateCustomLogoColor(printMesh, this.logoColor);
+		this.logoColor = newColor
+		this.previewMesh.updateCustomLogoColor(this.logoColor)
 	}
 
 	async createScene() {
 		const printPreviewScene = new Scene()
-		printPreviewScene.background = new Color(0xec_ed_dc)
+		this.printPreviewScene = printPreviewScene
+		printPreviewScene.background = new Color(0xec_ed_dc) //TODO: can I somehow get the current background color from the store or something?
 		const lights = this.threeSceneService.scene.clone().children[3]
 		lights.name = "lights"
 		printPreviewScene.add(lights)
 
 		printPreviewScene.name = "printPreviewScene"
-		printPreviewScene.add(await this.initPrintMesh())
 
 		const renderer = new WebGLRenderer()
 		this.rendererContainer.nativeElement.appendChild(renderer.domElement)
 
 		const camera = new PerspectiveCamera(45, 1.15, 50, 200_000)
 		camera.name = "camera"
-		//camera.position.set(0, 0, -300) //To directly see the backside of the map: uncomment this line and comment the next two lines
-		this.updateCameraPosition(camera)
-		camera.up = new Vector3(0, 0, 1)
+
 		printPreviewScene.rotateZ(Math.PI * 2)
 		printPreviewScene.add(camera)
 
@@ -170,24 +162,19 @@ export class Export3DMapDialogComponent {
 
 		animate()
 
-		this.printPreviewScene = printPreviewScene
+		this.previewMesh = new Preview3DPrintMesh()
+		await this.previewMesh.initialize(this.initGeometryOptions())
+		this.currentSize = this.previewMesh.getSize()
+
+		//camera.position.set(0, 0, -300) //To directly see the backside of the map: uncomment this line and comment the next two lines
+		this.updateCameraPosition(camera)
+		camera.up = new Vector3(0, 0, 1)
+
+		printPreviewScene.add(this.previewMesh.getThreeMesh())
 	}
 
 	private updateCameraPosition(camera: PerspectiveCamera) {
-		camera.position.set(-this.currentWidth * 0.2, -this.currentDepth * 1.2, this.currentHeight * 5)
-	}
-
-	async initPrintMesh() {
-		this.isPrintMeshLoaded = false
-
-		const printMesh = await this.previewMeshBuilder.createPrintPreviewMesh(
-			this.threeSceneService.getMapMesh().getThreeMesh()
-		)
-
-		this.calculateCurrentSize(printMesh)
-
-		this.isPrintMeshLoaded = true
-		return printMesh
+		camera.position.set(-this.currentSize.x * 0.2, -this.currentSize.y * 1.2, this.currentSize.z * 5)
 	}
 
 	async download3MFFile() {
@@ -195,25 +182,18 @@ export class Export3DMapDialogComponent {
 		this.downloadFile(compressed3mf, "3mf")
 	}
 
-	private makeMapMaxSize(printMesh: Mesh) {
-		this.wantedWidth = this.previewMeshBuilder.calculateMaxWidth(new Vector3(this.printers[this.selectedPrinter].x, this.printers[this.selectedPrinter].y, this.printers[this.selectedPrinter].z), this.threeSceneService.getMapMesh().getThreeMesh())
-		this.previewMeshBuilder.updateMapSize(printMesh, this.currentWidth, this.wantedWidth)
-		this.calculateCurrentSize(printMesh)
-		this.maxWidth = this.currentWidth
-	}
-
-	private calculateCurrentSize(printMesh: Mesh) {
-		const baseplate = printMesh.getObjectByName("Baseplate") as Mesh
-		baseplate.geometry.computeBoundingBox()
-		const boundingBoxBaseplate = baseplate.geometry.boundingBox
-
-		const map = printMesh.getObjectByName("Map") as Mesh
-		map.geometry.computeBoundingBox()
-		const boundingBoxMap = map.geometry.boundingBox
-
-		this.currentWidth = boundingBoxBaseplate.max.x - boundingBoxBaseplate.min.x
-		this.currentDepth = boundingBoxBaseplate.max.y - boundingBoxBaseplate.min.y
-		this.currentHeight = boundingBoxBaseplate.max.z - boundingBoxBaseplate.min.z + boundingBoxMap.max.z - boundingBoxMap.min.z
+	private makeMapMaxSize() {
+		this.wantedWidth = calculateMaxPossibleWidthForPreview3DPrintMesh(
+			new Vector3(
+				this.printers[this.selectedPrinter].x,
+				this.printers[this.selectedPrinter].y,
+				this.printers[this.selectedPrinter].z
+			),
+			this.threeSceneService.getMapMesh().getThreeMesh()
+		)
+		this.previewMesh.updateMapSize(this.wantedWidth)
+		this.currentSize = this.previewMesh.getSize()
+		this.maxWidth = this.currentSize.x
 	}
 
 	private initGeometryOptions(): GeometryOptions {
@@ -225,7 +205,8 @@ export class Export3DMapDialogComponent {
 		const colorMetricTitle = attributeDescriptors[this.colorMetric]?.title || fallbackTitles.get(this.colorMetric)
 
 		return {
-			width: this.wantedWidth,
+			mapMesh: this.threeSceneService.getMapMesh().getThreeMesh(),
+			wantedWidth: this.wantedWidth,
 			areaMetricTitle,
 			areaMetricData: this.nodeMetricData.find(metric => metric.name === this.areaMetric),
 			heightMetricTitle,
@@ -234,7 +215,7 @@ export class Export3DMapDialogComponent {
 			colorMetricData: this.nodeMetricData.find(metric => metric.name === this.colorMetric),
 			colorRange: this.state.getValue().dynamicSettings.colorRange,
 			frontText: this.frontText,
-			defaultMaterial: (this.threeSceneService.getMapMesh().getThreeMesh().material[0].clone() as ShaderMaterial),
+			defaultMaterial: this.threeSceneService.getMapMesh().getThreeMesh().material[0].clone() as ShaderMaterial,
 			numberOfColors: this.currentNumberOfColors
 		}
 	}
