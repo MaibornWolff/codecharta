9084ff4a489319059116b890b477587ecd927658
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2024-04-11 15:31:59 +0200

    Set first item active on search of metric #719

--- visualization/app/codeCharta/ui/metricChooser/metricChooser.component.ts
+++ visualization/app/codeCharta/ui/metricChooser/metricChooser.component.ts
@@ -1,9 +1,11 @@
-import { Component, ElementRef, Input, OnInit, ViewChild, ViewEncapsulation } from "@angular/core"
+import { Component, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren, ViewEncapsulation } from "@angular/core"
 import { map, Observable } from "rxjs"
 import { EdgeMetricData, NodeMetricData, CcState } from "../../codeCharta.model"
 import { metricDataSelector } from "../../state/selectors/accumulatedData/metricData/metricData.selector"
 import { attributeDescriptorsSelector } from "../../state/store/fileSettings/attributeDescriptors/attributeDescriptors.selector"
 import { Store } from "@ngrx/store"
+import { MatSelect } from "@angular/material/select"
+import { MatOption } from "@angular/material/core"
 
 type MetricChooserType = "node" | "edge"
 
@@ -20,6 +22,8 @@ export class MetricChooserComponent implements OnInit {
 	@Input() type: MetricChooserType = "node"
 	@Input() isDisabled = false
 	@ViewChild("searchTermInput") searchTermInput: ElementRef<HTMLInputElement>
+	@ViewChild("matSelect") matSelect: MatSelect
+	@ViewChildren(MatOption) matOptions: QueryList<MatOption>
 	searchTerm = ""
 	metricData$: Observable<NodeMetricData[] | EdgeMetricData[]>
 	attributeDescriptors$ = this.store.select(attributeDescriptorsSelector)
@@ -31,6 +35,22 @@ export class MetricChooserComponent implements OnInit {
 		this.metricData$ = this.store
 			.select(metricDataSelector)
 			.pipe(map(metricData => (this.type === "node" ? metricData.nodeMetricData : metricData.edgeMetricData)))
+	}
+
+	ngAfterViewInit() {
+		this.matOptions.changes.subscribe((options: QueryList<MatOption>) => {
+			this.setFirstItemActiveOnSearch(options)
+		})
+	}
+
+	setFirstItemActiveOnSearch(options: QueryList<MatOption>) {
+		const selectedOptions = options.filter(option => option["_selected"])
+		const matchingOptions = options.filter(option => option.value.toLowerCase().startsWith(this.searchTerm.toLowerCase()))
+		const searchTermExists = this.searchTerm.trim().length > 0
+
+		if (searchTermExists && selectedOptions.length === 0 && matchingOptions.length === 0) {
+			setTimeout(() => this.matSelect._keyManager.setActiveItem(0))
+		}
 	}
 
 	handleOpenedChanged(opened: boolean) {
