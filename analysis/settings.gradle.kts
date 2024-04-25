6f4ed87c13bb9fd3c7afd6d617a7f13a6ffa49aa
Ludwig Fritsch <ludwig.fritsch@maibornwolff.de>
2024-04-25 11:14:13 +0200

    Move versions file to gradle folder #3025

--- analysis/settings.gradle.kts
+++ analysis/settings.gradle.kts
@@ -2,14 +2,6 @@ plugins {
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-dependencyResolutionManagement {
-  versionCatalogs {
-    create("libs") {
-      from(files("./libs.versions.toml"))
-    }
-  }
-}
-
 include("model")
 include("filter:MergeFilter", "filter:EdgeFilter", "filter:StructureModifier")
 include(
