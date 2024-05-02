8045a52b43dd99b43604580c2fdf608b9a663b39
Benedikt Mehl <benedikt.mehl@maibornwolff.de>
2024-05-02 12:55:35 +0200

    test: add first printMesh test

---
+++ visualization/app/codeCharta/services/3DExports/preview3DPrintMesh.spec.ts
@@ -0,0 +1,41 @@
+import { Preview3DPrintMesh } from "./preview3DPrintMesh"
+
+jest.mock("three", () => {
+	const originalModule = jest.requireActual("three")
+
+	// Mock FontLoader class
+	originalModule.FontLoader = jest.fn().mockImplementation(() => {
+		return {
+			load: jest.fn((url, onLoad) => {
+				const mockFont = {} // Replace this with a mock font object if needed
+				onLoad(mockFont)
+			})
+		}
+	})
+	return originalModule
+})
+
+jest.mock("three/examples/jsm/loaders/SVGLoader", () => {
+	return jest.fn().mockImplementation(() => {
+		return {
+			load: jest.fn((url, onLoad) => {
+				const mockData = {
+					paths: [] // Replace this with mock data if needed
+				}
+				onLoad(mockData)
+			})
+		}
+	})
+})
+
+describe("Preview3DPrintMesh", () => {
+	let preview3DPrintMesh: Preview3DPrintMesh
+
+	beforeEach(() => {
+		preview3DPrintMesh = new Preview3DPrintMesh()
+	})
+
+	it("should load font without error", async () => {
+		await expect(preview3DPrintMesh["loadFont"]()).resolves.not.toThrow()
+	})
+})
