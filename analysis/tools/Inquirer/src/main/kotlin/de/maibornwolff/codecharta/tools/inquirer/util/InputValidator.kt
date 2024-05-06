3222aa9ff0530d49ab425dc011f4918430b658f0
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2024-05-06 15:22:31 +0200

    Add build gradle and format

--- analysis/tools/Inquirer/src/main/kotlin/de/maibornwolff/codecharta/tools/inquirer/util/InputValidator.kt
+++ analysis/tools/Inquirer/src/main/kotlin/de/maibornwolff/codecharta/tools/inquirer/util/InputValidator.kt
@@ -3,10 +3,8 @@ package de.maibornwolff.codecharta.tools.inquirer.util
 import java.io.File
 
 class InputValidator {
-
-    companion object {
-
-        //TODO what kind of methods do we need here?
+companion object {
+// TODO what kind of methods do we need here?
         // -> create all that are currently needed for the project when integrating kotter into each parser
         //
         // needed checkers:
@@ -15,22 +13,27 @@ class InputValidator {
         // sometimes input is list of files (or folders), check if they are all existing files
         // (add more when going through the parsers)
 
-        fun isInputAnExistingFile(vararg allowedFiletypes: String): (String) -> Boolean = { input ->
+        fun isInputAnExistingFile(vararg allowedFiletypes: String): (String) -> Boolean =
+        { input ->
             val file = File(input)
             val isFileCorrectType = allowedFiletypes.isEmpty() || allowedFiletypes.any { input.endsWith(it) }
             file.exists() && file.isFile && isFileCorrectType
         }
 
-        //TODO write this method --- make parameter accept multiple strings
-        fun isInputAnExistingFileOrFolder(filetype: String = ""): (String) -> Boolean = { input ->
+        // TODO write this method --- make parameter accept multiple strings
+        fun isInputAnExistingFileOrFolder(filetype: String = ""): (String) -> Boolean =
+        { input ->
             true
         }
 
         // not sure if this one is needed, its currently there for testing
-        fun isInputBetweenNumbers(minValue: Int, maxValue: Int): (String) -> Boolean = { input ->
+        fun isInputBetweenNumbers(
+        minValue: Int,
+        maxValue: Int,
+        ): (String) -> Boolean =
+        { input ->
             val inputNumber = input.toInt()
             minValue < inputNumber && inputNumber < maxValue
         }
     }
-
 }
