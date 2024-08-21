0a615d968aea7a130500976decdd55059dd97cd8
IdrisL <idrislaabidi@gmail.com>
2024-08-21 15:39:00 +0100

    fix ExpressionChangedAfterItHasBeenCheckedError #1539

--- visualization/app/codeCharta/ui/viewCube/zoomSlider/zoomSlider.component.spec.ts
+++ visualization/app/codeCharta/ui/viewCube/zoomSlider/zoomSlider.component.spec.ts
@@ -3,30 +3,36 @@ import { fireEvent, render, screen } from "@testing-library/angular"
 import userEvent from "@testing-library/user-event"
 import { ZoomSliderComponent } from "./zoomSlider.component"
 import { ThreeMapControlsService } from "../../codeMap/threeViewer/threeMapControls.service"
-import { of } from "rxjs"
+import { BehaviorSubject } from "rxjs"
 
 describe("ZoomSliderComponent", () => {
     const mockedThreeMapControlsService = {
         MAX_ZOOM: 200,
         MIN_ZOOM: 10,
-        zoomPercentage$: of(100),
+        zoomPercentage$: new BehaviorSubject(100),
         setZoomPercentage: jest.fn()
     }
 
     beforeEach(() => {
-        mockedThreeMapControlsService.zoomPercentage$.subscribe = jest.fn()
         TestBed.configureTestingModule({
+            declarations: [ZoomSliderComponent],
             providers: [{ provide: ThreeMapControlsService, useValue: mockedThreeMapControlsService }]
         })
     })
 
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
     it("should subscribe to the zoom observable and set min max properties", async () => {
+        jest.spyOn(mockedThreeMapControlsService.zoomPercentage$, "subscribe")
         const { fixture } = await render(ZoomSliderComponent)
         const zoomSliderComponent = fixture.componentInstance
 
         expect(zoomSliderComponent.maxZoom).toBe(200)
         expect(zoomSliderComponent.minZoom).toBe(10)
         expect(mockedThreeMapControlsService.zoomPercentage$.subscribe).toHaveBeenCalled()
+        expect(zoomSliderComponent.zoomPercentage).toBe(100)
     })
 
     it("should call setZoomPercentage on input change", async () => {
@@ -40,7 +46,9 @@ describe("ZoomSliderComponent", () => {
     })
 
     it("should increase the zoom percentage when zoomIn is called", async () => {
-        await render(ZoomSliderComponent)
+        mockedThreeMapControlsService.zoomPercentage$.next(100)
+        const { fixture } = await render(ZoomSliderComponent)
+        fixture.detectChanges()
 
         await userEvent.click(screen.getByTestId("zoomIn"))
 
@@ -48,7 +56,9 @@ describe("ZoomSliderComponent", () => {
     })
 
     it("should decrease the zoom percentage when zoomOut is called", async () => {
-        await render(ZoomSliderComponent)
+        mockedThreeMapControlsService.zoomPercentage$.next(100)
+        const { fixture } = await render(ZoomSliderComponent)
+        fixture.detectChanges()
 
         await userEvent.click(screen.getByTestId("zoomOut"))
 
@@ -56,8 +66,9 @@ describe("ZoomSliderComponent", () => {
     })
 
     it("should not exceed the maximum zoom", async () => {
-        mockedThreeMapControlsService.zoomPercentage$ = of(200)
-        await render(ZoomSliderComponent)
+        mockedThreeMapControlsService.zoomPercentage$.next(200)
+        const { fixture } = await render(ZoomSliderComponent)
+        fixture.detectChanges()
 
         await userEvent.click(screen.getByTestId("zoomIn"))
 
@@ -65,8 +76,9 @@ describe("ZoomSliderComponent", () => {
     })
 
     it("should not go below the minimum zoom", async () => {
-        mockedThreeMapControlsService.zoomPercentage$ = of(10)
-        await render(ZoomSliderComponent)
+        mockedThreeMapControlsService.zoomPercentage$.next(10)
+        const { fixture } = await render(ZoomSliderComponent)
+        fixture.detectChanges()
 
         await userEvent.click(screen.getByTestId("zoomOut"))
 
--- visualization/app/codeCharta/ui/viewCube/zoomSlider/zoomSlider.component.ts
+++ visualization/app/codeCharta/ui/viewCube/zoomSlider/zoomSlider.component.ts
@@ -1,21 +1,33 @@
-import { Component } from "@angular/core"
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from "@angular/core"
 import { ThreeMapControlsService } from "../../codeMap/threeViewer/threeMapControls.service"
 
 @Component({
     selector: "cc-zoom-slider",
     templateUrl: "./zoomSlider.component.html",
-    styleUrls: ["./zoomSlider.component.scss"]
+    styleUrls: ["./zoomSlider.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ZoomSliderComponent {
-    zoomPercentage = 100
+export class ZoomSliderComponent implements OnInit {
+    zoomPercentage: number
     maxZoom: number
     minZoom: number
 
-    constructor(private threeMapControlsService: ThreeMapControlsService) {
+    constructor(
+        private threeMapControlsService: ThreeMapControlsService,
+        private cdr: ChangeDetectorRef
+    ) {}
+
+    ngOnInit() {
         this.maxZoom = this.threeMapControlsService.MAX_ZOOM
         this.minZoom = this.threeMapControlsService.MIN_ZOOM
+
         this.threeMapControlsService.zoomPercentage$.subscribe(zoom => {
             this.zoomPercentage = zoom
+
+            // Manually mark the component for change detection to prevent
+            // ExpressionChangedAfterItHasBeenCheckedError thrown by angular
+            // https://angular.dev/errors/NG0100
+            this.cdr.detectChanges()
         })
     }
 
