1b0feb6a935dce1b024ce0243cd3688bc72047c0
lisa.walter <lisa.walter@maibornwolff.de>
2024-09-09 15:58:06 +0200

    chore(visualization): enable view encapsulation in addCustomConfigButton

--- visualization/app/codeCharta/ui/customConfigs/addCustomConfigButton/addCustomConfigButton.component.ts
+++ visualization/app/codeCharta/ui/customConfigs/addCustomConfigButton/addCustomConfigButton.component.ts
@@ -1,11 +1,10 @@
-import { Component, ViewEncapsulation } from "@angular/core"
+import { Component } from "@angular/core"
 import { MatDialog } from "@angular/material/dialog"
 import { AddCustomConfigDialogComponent } from "./addCustomConfigDialog/addCustomConfigDialog.component"
 
 @Component({
     selector: "cc-add-custom-config-button",
-    templateUrl: "./addCustomConfigButton.component.html",
-    encapsulation: ViewEncapsulation.None
+    templateUrl: "./addCustomConfigButton.component.html"
 })
 export class AddCustomConfigButtonComponent {
     constructor(private dialog: MatDialog) {}
