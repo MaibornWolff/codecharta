3a54f3d9e96c3308197ddd870f5c25680a9d848e
Benedikt Mehl <b.mehl14@outlook.de>
2024-09-24 14:06:49 +0200

    Move deleting of sample files to loadFile.service.ts #2498

--- visualization/app/codeCharta/state/store/files/files.reducer.ts
+++ visualization/app/codeCharta/state/store/files/files.reducer.ts
@@ -1,7 +1,6 @@
 import {
     addFile,
     invertStandard,
-    removeAllFiles,
     removeFile,
     setAll,
     setDelta,
@@ -24,7 +23,6 @@ export const files = createReducer(
     on(setFiles, setState(defaultFiles)),
     on(addFile, (state, action) => [...state, { file: action.file, selectedAs: FileSelectionState.None }]),
     on(removeFile, (state, action) => removeFileFromState(state, action.fileName)),
-    on(removeAllFiles, () => []),
     on(setDelta, (state, action) => setDeltaState(state, action.referenceFile, action.comparisonFile)),
     on(setDeltaReference, (state, action) => setDeltaReferenceState(state, action.file)),
     on(setDeltaComparison, (state, action) => setDeltaComparisonState(state, action.file)),
