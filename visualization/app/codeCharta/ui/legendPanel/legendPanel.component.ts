c4180f993f987e4d666c6d9e8a3e077f8c54ee01
lisa.walter <lisa.walter@maibornwolff.de>
2024-09-24 15:30:06 +0200

    fix(visualization): close legend panel on outside click

--- visualization/app/codeCharta/ui/legendPanel/legendPanel.component.ts
+++ visualization/app/codeCharta/ui/legendPanel/legendPanel.component.ts
@@ -6,14 +6,14 @@ import { heightMetricSelector } from "../../state/store/dynamicSettings/heightMe
 import { areaMetricSelector } from "../../state/store/dynamicSettings/areaMetric/areaMetric.selector"
 import { colorMetricSelector } from "../../state/store/dynamicSettings/colorMetric/colorMetric.selector"
 import { edgeMetricSelector } from "../../state/store/dynamicSettings/edgeMetric/edgeMetric.selector"
-import { Component } from "@angular/core"
+import { Component, ElementRef, OnInit } from "@angular/core"
 
 @Component({
     selector: "cc-legend-panel",
     templateUrl: "./legendPanel.component.html",
     styleUrls: ["./legendPanel.component.scss"]
 })
-export class LegendPanelComponent {
+export class LegendPanelComponent implements OnInit {
     isLegendVisible = false
     isDeltaState$ = this.store.select(isDeltaStateSelector)
     heightMetric$ = this.store.select(heightMetricSelector)
@@ -23,10 +23,31 @@ export class LegendPanelComponent {
 
     constructor(
         private store: Store<CcState>,
-        public isAttributeSideBarVisibleService: IsAttributeSideBarVisibleService
+        public isAttributeSideBarVisibleService: IsAttributeSideBarVisibleService,
+        private readonly elementReference: ElementRef
     ) {}
 
     toggleIsLegendVisible() {
         this.isLegendVisible = !this.isLegendVisible
+    }
+
+    private mouseDownListener?: (event: MouseEvent) => void
+
+    ngOnInit(): void {
+        this.mouseDownListener = (event: MouseEvent) => this.collapseOnOutsideClick(event)
+        document.addEventListener("mousedown", this.mouseDownListener)
+    }
+
+    private collapseOnOutsideClick(event: MouseEvent) {
+        const target = event.target as Node
+        if (this.isLegendVisible) {
+            const clickedInside = this.elementReference.nativeElement.contains(target)
+            const overlayPaneElement = document.querySelector(".cdk-overlay-container")
+            const clickedWithinOverlayPane = overlayPaneElement ? overlayPaneElement.contains(target) : false
+
+            if (!(clickedInside || clickedWithinOverlayPane)) {
+                this.isLegendVisible = false
+            }
+        }
     }
 }
