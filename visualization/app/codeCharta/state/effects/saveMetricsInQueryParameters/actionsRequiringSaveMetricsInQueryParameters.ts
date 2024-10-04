78e12d1e5e71ec85b636ed1e49c03f42f1f6293d
Benedikt Mehl <b.mehl14@outlook.de>
2024-10-04 11:29:26 +0200

    Add isSample file to url query parameters #2498

--- visualization/app/codeCharta/state/effects/saveMetricsInQueryParameters/actionsRequiringSaveMetricsInQueryParameters.ts
+++ visualization/app/codeCharta/state/effects/saveMetricsInQueryParameters/actionsRequiringSaveMetricsInQueryParameters.ts
@@ -2,5 +2,12 @@ import { setAreaMetric } from "../../store/dynamicSettings/areaMetric/areaMetric
 import { setColorMetric } from "../../store/dynamicSettings/colorMetric/colorMetric.actions"
 import { setEdgeMetric } from "../../store/dynamicSettings/edgeMetric/edgeMetric.actions"
 import { setHeightMetric } from "../../store/dynamicSettings/heightMetric/heightMetric.actions"
+import { setCurrentFilesAreSampleFiles } from "../../store/appStatus/currentFilesAreSampleFiles/currentFilesAreSampleFiles.actions"
 
-export const actionsRequiringSaveMetricsInQueryParameters = [setEdgeMetric, setHeightMetric, setColorMetric, setAreaMetric]
+export const actionsRequiringSaveMetricsInQueryParameters = [
+    setEdgeMetric,
+    setHeightMetric,
+    setColorMetric,
+    setAreaMetric,
+    setCurrentFilesAreSampleFiles
+]
