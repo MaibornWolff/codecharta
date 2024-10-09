c865338162ac742cf07f6a02ee2a69cfe56b76e1
VictoriaG <victoria.gordeeva@maibornwolff.de>
2024-10-09 11:00:54 +0200

    #3745 remove comments in MergeFilterTest

--- analysis/filter/MergeFilter/src/test/kotlin/de/maibornwolff/codecharta/filter/mergefilter/MergeFilterTest.kt
+++ analysis/filter/MergeFilter/src/test/kotlin/de/maibornwolff/codecharta/filter/mergefilter/MergeFilterTest.kt
@@ -27,8 +27,6 @@ class MergeFilterTest {
     @Test
     fun `should merge all files in a folder correctly`() {
         val projectLocation = "src/test/resources/mergeFolderTest"
-        // val valueInFile1 = "SourceMonCsvConverterTest.java"
-        // val valueInFile2 = "SourceMonCsvConverter.java"
         val invalidFile = "invalid.json"
 
         System.setOut(PrintStream(outContent))
@@ -39,10 +37,6 @@ class MergeFilterTest {
 
         // should ignore files starting with a dot
         assertThat(outContent.toString()).doesNotContain("ShouldNotAppear.java")
-
-        // should merge all valid projects in folder
-        // assertThat(outContent.toString()).contains(valueInFile1)
-        // assertThat(outContent.toString()).contains(valueInFile2)
 
         // should warn about skipped files
         assertThat(errContent.toString()).contains(invalidFile)
