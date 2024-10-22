55da03bacb37b10551e07ef570e56f999d16ece6
Benedikt Mehl <b.mehl14@outlook.de>
2024-10-22 10:09:31 +0200

    Add button for applying file selection #3637

--- visualization/app/codeCharta/state/store/files/files.actions.ts
+++ visualization/app/codeCharta/state/store/files/files.actions.ts
@@ -4,11 +4,9 @@ import { FileState } from "../../../model/files/files"
 
 export const setFiles = createAction("SET_FILES", props<{ value: FileState[] }>())
 export const addFile = createAction("ADD_FILE", props<{ file: CCFile }>())
-export const removeFile = createAction("REMOVE_FILE", props<{ fileName: string }>())
+export const removeFiles = createAction("REMOVE_FILES", props<{ fileNames: string[] }>())
 export const setStandard = createAction("SET_STANDARD", props<{ files: CCFile[] }>())
 export const setStandardByNames = createAction("SET_STANDARD_BY_NAMES", props<{ fileNames: string[] }>())
-export const setAll = createAction("SET_ALL")
-export const invertStandard = createAction("INVERT_STANDARD")
 export const switchReferenceAndComparison = createAction("SWITCH_REFERENCE_AND_COMPARISON")
 export const setDeltaComparison = createAction("SET_DELTA_COMPARISON", props<{ file: CCFile }>())
 export const setDeltaReference = createAction("SET_DELTA_REFERENCE", props<{ file: CCFile }>())
@@ -16,11 +14,9 @@ export const setDelta = createAction("SET_DELTA", props<{ referenceFile: CCFile;
 export const fileActions = [
     setFiles,
     addFile,
-    removeFile,
+    removeFiles,
     setStandard,
     setStandardByNames,
-    setAll,
-    invertStandard,
     switchReferenceAndComparison,
     setDeltaComparison,
     setDeltaReference,
