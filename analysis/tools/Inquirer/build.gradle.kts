5307e933f63c0223d2dfb61dccd5d778d198dd92
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2024-12-18 10:52:30 +0100

    feat(analysis): remove temporary test methods, format, fix tests (#3483)

--- analysis/tools/Inquirer/build.gradle.kts
+++ analysis/tools/Inquirer/build.gradle.kts
@@ -1,17 +1,17 @@
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-  implementation(project(":model"))
+    implementation(project(":model"))
 
-  implementation(libs.kotter.test)
-  implementation(libs.kotter)
+    implementation(libs.kotter.test)
+    implementation(libs.kotter)
 
-  testImplementation(libs.kotlin.test)
-  testImplementation(libs.assertj.core)
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.assertj.core)
 }
 
 tasks.test {
-  useJUnitPlatform()
+    useJUnitPlatform()
 }
