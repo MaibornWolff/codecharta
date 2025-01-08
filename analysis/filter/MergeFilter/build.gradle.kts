488cd444829f503889819e52b19a66c41ef6a604
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2025-01-08 14:46:01 +0100

    feat(analysis): transform mergeFilter dialog to Kotter (#3483)

--- analysis/filter/MergeFilter/build.gradle.kts
+++ analysis/filter/MergeFilter/build.gradle.kts
@@ -1,9 +1,11 @@
 dependencies {
     implementation(project(":model"))
     implementation(project(":tools:InteractiveParser"))
+    implementation(project(":tools:Inquirer"))
 
+    implementation(libs.kotter)
+    implementation(libs.kotter.test)
     implementation(libs.picocli)
-    implementation(libs.kotlin.inquirer)
     testImplementation(libs.kotlin.test)
 
     testRuntimeOnly(libs.kotlin.reflect)
