61365cc7d3265a5eb313a73e1634e63f33411818
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2025-01-14 10:10:17 +0100

    feat(analysis): transform edgefilter dialog to kotter  (#3483)

--- analysis/filter/EdgeFilter/src/main/kotlin/de/maibornwolff/codecharta/filter/edgefilter/ParserDialog.kt
+++ analysis/filter/EdgeFilter/src/main/kotlin/de/maibornwolff/codecharta/filter/edgefilter/ParserDialog.kt
@@ -1,40 +1,51 @@
 package de.maibornwolff.codecharta.filter.edgefilter
 
-import com.github.kinquirer.KInquirer
-import com.github.kinquirer.components.promptInput
+import com.varabyte.kotter.foundation.session
+import com.varabyte.kotter.runtime.RunScope
+import com.varabyte.kotter.runtime.Session
+import de.maibornwolff.codecharta.serialization.FileExtension
+import de.maibornwolff.codecharta.tools.inquirer.myPromptDefaultFileFolderInput
+import de.maibornwolff.codecharta.tools.inquirer.myPromptInput
 import de.maibornwolff.codecharta.tools.interactiveparser.InputType
 import de.maibornwolff.codecharta.tools.interactiveparser.ParserDialogInterface
-import de.maibornwolff.codecharta.util.InputHelper
 import de.maibornwolff.codecharta.util.Logger
-import java.io.File
 
 class ParserDialog {
     companion object : ParserDialogInterface {
         override fun collectParserArgs(): List<String> {
-            var inputFileName: String
-            do {
-                inputFileName = getInputFileName("cc.json", InputType.FILE)
-            } while (!InputHelper.isInputValidAndNotNull(arrayOf(File(inputFileName)), canInputContainFolders = false))
+            var res = listOf<String>()
+            session { res = myCollectParserArgs() }
+            return res
+        }
+
+        internal fun Session.myCollectParserArgs(
+            fileCallback: suspend RunScope.() -> Unit = {},
+            outFileCallback: suspend RunScope.() -> Unit = {},
+            separatorCallback: suspend RunScope.() -> Unit = {}
+        ): List<String> {
+            val inputFileName: String = myPromptDefaultFileFolderInput(
+                allowEmptyInput = false,
+                inputType = de.maibornwolff.codecharta.tools.inquirer.InputType.FILE,
+                fileExtensionList = listOf(FileExtension.CCJSON, FileExtension.CCGZ),
+                onInputReady = fileCallback
+            )
 
             Logger.info {
                 "File path: $inputFileName"
             }
 
-            val defaultOutputFileName = getOutputFileName(inputFileName)
-            val outputFileName: String =
-                KInquirer.promptInput(
-                    message = "What is the name of the output file?",
-                    hint = defaultOutputFileName,
-                    default = defaultOutputFileName
-                )
+            val outputFileName: String = myPromptInput(
+                message = "What is the name of the output file?",
+                onInputReady = outFileCallback
+            )
 
             val defaultPathSeparator = "/"
-            val pathSeparator: String =
-                KInquirer.promptInput(
-                    message = "What is the path separator?",
-                    hint = defaultPathSeparator,
-                    default = defaultPathSeparator
-                )
+            val pathSeparator: String = myPromptInput(
+                message = "What is the path separator?",
+                hint = defaultPathSeparator,
+                invalidInputMessage = "Please specify a valid path separator like '/' or '\\'",
+                onInputReady = separatorCallback
+            )
 
             return listOf(inputFileName, "--output-file=$outputFileName", "--path-separator=$pathSeparator")
         }
