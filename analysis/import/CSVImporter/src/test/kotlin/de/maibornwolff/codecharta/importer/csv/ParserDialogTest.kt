5d9a2a8234e7bf3b550f701cb8669b1c025a6ccf
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2025-01-16 13:12:26 +0100

    feat(analysis): adapt CSVimporter to kotter dialog and tests (#3483)

--- analysis/import/CSVImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/csv/ParserDialogTest.kt
+++ analysis/import/CSVImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/csv/ParserDialogTest.kt
@@ -2,66 +2,78 @@
 
 package de.maibornwolff.codecharta.importer.csv
 
-import com.github.kinquirer.KInquirer
-import com.github.kinquirer.components.promptConfirm
-import com.github.kinquirer.components.promptInput
-import de.maibornwolff.codecharta.util.InputHelper
-import io.mockk.every
-import io.mockk.mockkObject
-import io.mockk.mockkStatic
-import io.mockk.unmockkAll
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.AfterEach
+import com.varabyte.kotter.foundation.input.Keys
+import com.varabyte.kotter.runtime.terminal.inmemory.press
+import com.varabyte.kotter.runtime.terminal.inmemory.type
+import com.varabyte.kotterx.test.foundation.testSession
+import de.maibornwolff.codecharta.importer.csv.ParserDialog.Companion.myCollectParserArgs
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.Timeout
 import picocli.CommandLine
 import java.io.File
 
+@Timeout(120)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ParserDialogTest {
-    @AfterEach
-    fun afterTest() {
-        unmockkAll()
-    }
+    private val testResourceBaseFolder = "src/test/resources/"
+    private val inputFileName = "${testResourceBaseFolder}csvimporter.csv"
+    private val outputFileName = "out.cc.json"
+    private val pathColumnName = "path"
+    private val pathSeparator = "/"
 
     @Test
-    fun `should output correct arguments when valid input is provided`() { // given
-        val fileName = "in.csv"
-        val outputFileName = "out.cc.json"
-        val pathColumnName = "path"
+    fun `should output correct arguments when valid input is provided`() {
         val delimiter = ";"
-        val pathSeparator = "/"
         val isCompressed = false
 
-        mockkObject(InputHelper)
-        every { InputHelper.isInputValidAndNotNull(any(), any()) } returns true
+        testSession { terminal ->
+            val parserArguments = myCollectParserArgs(
+                fileCallback = {
+                    terminal.type(inputFileName)
+                    terminal.press(Keys.ENTER)
+                },
+                outFileCallback = {
+                    terminal.type(outputFileName)
+                    terminal.press(Keys.ENTER)
+                },
+                columnCallback = {
+                    terminal.type(pathColumnName)
+                    terminal.press(Keys.ENTER)
+                },
+                delimiterCallback = {
+                    terminal.type(delimiter)
+                    terminal.press(Keys.ENTER)
+                },
+                separatorCallback = {
+                    terminal.type(pathSeparator)
+                    terminal.press(Keys.ENTER)
+                },
+                compressCallback = {
+                    terminal.press(Keys.RIGHT)
+                    terminal.press(Keys.ENTER)
+                }
+            )
 
-        mockkStatic("com.github.kinquirer.components.InputKt")
-        every {
-            KInquirer.promptInput(any(), any(), any(), any(), any())
-        } returns fileName andThen "" andThen outputFileName andThen pathColumnName andThen delimiter andThen pathSeparator
-        mockkStatic("com.github.kinquirer.components.ConfirmKt")
-        every {
-            KInquirer.promptConfirm(any(), any())
-        } returns isCompressed
+            val cmdLine = CommandLine(CSVImporter())
+            val parseResult = cmdLine.parseArgs(*parserArguments.toTypedArray())
 
-        // when
-        val parserArguments = ParserDialog.collectParserArgs()
-        val cmdLine = CommandLine(CSVImporter())
-        val parseResult = cmdLine.parseArgs(*parserArguments.toTypedArray())
-
-        // then
-        Assertions.assertThat(parseResult.matchedOption("output-file").getValue<String>()).isEqualTo(outputFileName)
-        Assertions.assertThat(parseResult.matchedOption("path-column-name").getValue<String>())
-            .isEqualTo(pathColumnName)
-        Assertions.assertThat(parseResult.matchedOption("delimiter").getValue<Char>()).isEqualTo(delimiter[0])
-        Assertions.assertThat(parseResult.matchedOption("path-separator").getValue<Char>()).isEqualTo(pathSeparator[0])
-        Assertions.assertThat(parseResult.matchedOption("not-compressed").getValue<Boolean>()).isEqualTo(isCompressed)
-        Assertions.assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[0].name).isEqualTo(fileName)
+            assertThat(parseResult.matchedOption("output-file").getValue<String>()).isEqualTo(outputFileName)
+            assertThat(parseResult.matchedOption("path-column-name").getValue<String>())
+                .isEqualTo(pathColumnName)
+            assertThat(parseResult.matchedOption("delimiter").getValue<Char>()).isEqualTo(delimiter[0])
+            assertThat(parseResult.matchedOption("path-separator").getValue<Char>()).isEqualTo(pathSeparator[0])
+            assertThat(parseResult.matchedOption("not-compressed").getValue<Boolean>()).isEqualTo(isCompressed)
+            assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[0].name).isEqualTo(File(inputFileName).name)
+        }
     }
 
+    @Disabled
     @Test
     fun `should output correct arguments when multiple files are provided`() { // given
+        // New multi input separated by comma, NotImplementedYet
         val fileName = "in.csv"
         val fileName2 = "in2.csv"
         val fileName3 = "in3.csv"
@@ -71,17 +83,28 @@ class ParserDialogTest {
         val pathSeparator = "/"
         val isCompressed = true
 
-        mockkObject(InputHelper)
-        every { InputHelper.isInputValidAndNotNull(any(), any()) } returns true andThen true andThen true
-
-        mockkStatic("com.github.kinquirer.components.InputKt")
-        every {
-            KInquirer.promptInput(any(), any(), any(), any(), any(), any())
-        } returns fileName andThen fileName2 andThen fileName3 andThen "" andThen outputFileName andThen pathColumnName andThen delimiter andThen pathSeparator
-        mockkStatic("com.github.kinquirer.components.ConfirmKt")
-        every {
-            KInquirer.promptConfirm(any(), any())
-        } returns isCompressed
+        testSession { terminal ->
+            val parserArguments = myCollectParserArgs(
+                fileCallback = {
+                    terminal.press(Keys.ENTER)
+                },
+                outFileCallback = {
+                    terminal.press(Keys.ENTER)
+                },
+                columnCallback = {
+                    terminal.press(Keys.ENTER)
+                },
+                delimiterCallback = {
+                    terminal.press(Keys.ENTER)
+                },
+                separatorCallback = {
+                    terminal.press(Keys.ENTER)
+                },
+                compressCallback = {
+                    terminal.press(Keys.ENTER)
+                }
+            )
+        }
 
         // when
         val parserArguments = ParserDialog.collectParserArgs()
@@ -89,55 +112,61 @@ class ParserDialogTest {
         val parseResult = cmdLine.parseArgs(*parserArguments.toTypedArray())
 
         // then
-        Assertions.assertThat(parseResult.matchedOption("output-file").getValue<String>()).isEqualTo(outputFileName)
-        Assertions.assertThat(parseResult.matchedOption("path-column-name").getValue<String>())
+        assertThat(parseResult.matchedOption("output-file").getValue<String>()).isEqualTo(outputFileName)
+        assertThat(parseResult.matchedOption("path-column-name").getValue<String>())
             .isEqualTo(pathColumnName)
-        Assertions.assertThat(parseResult.matchedOption("delimiter").getValue<Char>()).isEqualTo(delimiter[0])
-        Assertions.assertThat(parseResult.matchedOption("path-separator").getValue<Char>()).isEqualTo(pathSeparator[0])
-        Assertions.assertThat(parseResult.matchedOption("not-compressed")).isNull()
-        Assertions.assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[0].name).isEqualTo(fileName)
-        Assertions.assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[1].name).isEqualTo(fileName2)
-        Assertions.assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[2].name).isEqualTo(fileName3)
+        assertThat(parseResult.matchedOption("delimiter").getValue<Char>()).isEqualTo(delimiter[0])
+        assertThat(parseResult.matchedOption("path-separator").getValue<Char>()).isEqualTo(pathSeparator[0])
+        assertThat(parseResult.matchedOption("not-compressed")).isNull()
+        assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[0].name).isEqualTo(fileName)
+        assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[1].name).isEqualTo(fileName2)
+        assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[2].name).isEqualTo(fileName3)
     }
 
     @Test
-    fun `should prompt user twice for input file when first input file is invalid`() { // given
-        val invalidFileName1 = "invalidFileName"
-        val invalidFileName2 = "invalidFileName2"
-        val validFileName1 = "in.csv"
-        val validFileName2 = "in2.csv"
-        val validFileName3 = "in3.csv"
-        val outputFileName = "out.cc.json"
-        val pathColumnName = "path"
-        val delimiter = ";"
-        val pathSeparator = "/"
-        val isCompressed = true
+    fun `should prompt user twice for input file when first input file is invalid`() {
+        val invalidFileName1 = "inv"
+        val delimiter = ","
+        val separator = "\\"
 
-        mockkObject(InputHelper)
-        every {
-            InputHelper.isInputValidAndNotNull(any(), any())
-        } returns false andThen true andThen false andThen true andThen true
+        testSession { terminal ->
+            val parserArguments = myCollectParserArgs(
+                fileCallback = {
+                    terminal.type(invalidFileName1)
+                    terminal.press(Keys.ENTER)
+                    terminal.press(Keys.BACKSPACE,Keys.BACKSPACE,Keys.BACKSPACE)
+                    terminal.type(inputFileName)
+                    terminal.press(Keys.ENTER)
+                },
+                outFileCallback = {
+                    terminal.type(outputFileName)
+                    terminal.press(Keys.ENTER)
+                },
+                columnCallback = {
+                    terminal.type(pathColumnName)
+                    terminal.press(Keys.ENTER)
+                },
+                delimiterCallback = {
+                    terminal.press(Keys.RIGHT)
+                    terminal.press(Keys.ENTER)
+                },
+                separatorCallback = {
+                    terminal.type(separator)
+                    terminal.press(Keys.ENTER)
+                },
+                compressCallback = {
+                    terminal.press(Keys.ENTER)
+                }
+            )
 
-        mockkStatic("com.github.kinquirer.components.InputKt")
-        every {
-            KInquirer.promptInput(any(), any(), any(), any(), any(), any())
-        } returns invalidFileName1 andThen validFileName1 andThen invalidFileName2 andThen validFileName2 andThen validFileName3 andThen "" andThen outputFileName andThen pathColumnName andThen delimiter andThen pathSeparator
-        mockkStatic("com.github.kinquirer.components.ConfirmKt")
-        every {
-            KInquirer.promptConfirm(any(), any())
-        } returns isCompressed
+            val cmdLine = CommandLine(CSVImporter())
+            val parseResult = cmdLine.parseArgs(*parserArguments.toTypedArray())
 
-        // when
-        val parserArguments = ParserDialog.collectParserArgs()
-        val cmdLine = CommandLine(CSVImporter())
-        val parseResult = cmdLine.parseArgs(*parserArguments.toTypedArray())
-
-        // then
-        Assertions.assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[0].name)
-            .isEqualTo(validFileName1)
-        Assertions.assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[1].name)
-            .isEqualTo(validFileName2)
-        Assertions.assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[2].name)
-            .isEqualTo(validFileName3)
+            assertThat(parseResult.matchedPositional(0).getValue<ArrayList<File>>()[0].name)
+                .isEqualTo(File(inputFileName).name)
+            assertThat(parseResult.matchedOption("delimiter").getValue<Char>()).isEqualTo(delimiter[0])
+            assertThat(parseResult.matchedOption("path-separator").getValue<Char>()).isEqualTo(separator[0])
+            assertThat(parseResult.matchedOption("not-compressed")).isNull()
+        }
     }
 }
