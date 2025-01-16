81caefb8236de59937283b583430c5363ea2879b
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2025-01-16 13:12:26 +0100

    feat(analysis): adapt CSVimporter to kotter dialog and tests (#3483)

--- analysis/import/CSVImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/csv/ParserDialog.kt
+++ analysis/import/CSVImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/csv/ParserDialog.kt
@@ -1,43 +1,70 @@
 package de.maibornwolff.codecharta.importer.csv
 
-import com.github.kinquirer.KInquirer
-import com.github.kinquirer.components.promptConfirm
-import com.github.kinquirer.components.promptInput
+import com.varabyte.kotter.foundation.session
+import com.varabyte.kotter.runtime.RunScope
+import com.varabyte.kotter.runtime.Session
+import de.maibornwolff.codecharta.serialization.FileExtension
+import de.maibornwolff.codecharta.tools.inquirer.InputType
+import de.maibornwolff.codecharta.tools.inquirer.myPromptConfirm
+import de.maibornwolff.codecharta.tools.inquirer.myPromptDefaultFileFolderInput
+import de.maibornwolff.codecharta.tools.inquirer.myPromptInput
 import de.maibornwolff.codecharta.tools.interactiveparser.ParserDialogInterface
-import de.maibornwolff.codecharta.util.CSVFileAggregation.Companion.getInputFiles
 
 class ParserDialog {
     companion object : ParserDialogInterface {
         override fun collectParserArgs(): List<String> {
-            val inputFileNames = getInputFiles("Please specify the name of the first CSV file to be parsed.")
+            var res = listOf<String>()
+            session { res = myCollectParserArgs() }
+            return res
+        }
 
-            val outputFileName: String = KInquirer.promptInput(
+        internal fun Session.myCollectParserArgs(
+            fileCallback: suspend RunScope.() -> Unit = {},
+            outFileCallback: suspend RunScope.() -> Unit = {},
+            columnCallback: suspend RunScope.() -> Unit = {},
+            delimiterCallback: suspend RunScope.() -> Unit = {},
+            separatorCallback: suspend RunScope.() -> Unit = {},
+            compressCallback: suspend RunScope.() -> Unit = {}
+        ): List<String> {
+            val inputFileName: String = myPromptDefaultFileFolderInput(
+                allowEmptyInput = false,
+                inputType = InputType.FILE,
+                fileExtensionList = listOf(FileExtension.CSV),
+                onInputReady = fileCallback
+            )
+
+            val outputFileName: String = myPromptInput(
                 message = "What is the name of the output file?",
-                hint = "output.cc.json"
+                allowEmptyInput = true,
+                onInputReady = outFileCallback
             )
 
-            val pathColumnName: String = KInquirer.promptInput(
+            val pathColumnName: String = myPromptInput(
                 message = "What is the name of the path column name?",
-                default = "path"
+                hint = "path",
+                allowEmptyInput = false,
+                onInputReady = columnCallback
             )
 
-            val delimiter = KInquirer.promptInput(
+            val delimiter: String = myPromptInput(
                 message = "Which column delimiter is used in the CSV file?",
                 hint = ",",
-                default = ","
+                onInputReady = delimiterCallback
             )
 
-            val pathSeparator = KInquirer.promptInput(
+            val pathSeparator: String = myPromptInput(
                 message = "Which path separator is used in the path names?",
                 hint = "/",
-                default = "/"
+                onInputReady = separatorCallback
             )
 
-            val isCompressed = outputFileName.isEmpty() || KInquirer.promptConfirm(
-                message = "Do you want to compress the output file?", default = true
+            val isCompressed = outputFileName.isEmpty() || myPromptConfirm(
+                message = "Do you want to compress the output file?",
+                onInputReady = compressCallback
             )
 
-            return inputFileNames + listOfNotNull(
+            return listOfNotNull(
+                inputFileName,
                 "--output-file=$outputFileName",
                 "--path-column-name=$pathColumnName",
                 "--delimiter=$delimiter",
