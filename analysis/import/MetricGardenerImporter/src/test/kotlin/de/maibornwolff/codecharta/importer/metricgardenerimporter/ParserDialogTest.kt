d8e1069c82153a87cde92c989133c33852848f17
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2025-01-17 12:16:28 +0100

    feat(analysis): adapt MetricGardenerImport to kotter dialog and tests (#3483)

--- analysis/import/MetricGardenerImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/metricgardenerimporter/ParserDialogTest.kt
+++ analysis/import/MetricGardenerImporter/src/test/kotlin/de/maibornwolff/codecharta/importer/metricgardenerimporter/ParserDialogTest.kt
@@ -1,118 +1,126 @@
 package de.maibornwolff.codecharta.importer.metricgardenerimporter
 
-import com.github.kinquirer.KInquirer
-import com.github.kinquirer.components.promptConfirm
-import com.github.kinquirer.components.promptInput
-import de.maibornwolff.codecharta.util.InputHelper
-import io.mockk.every
-import io.mockk.mockkObject
-import io.mockk.mockkStatic
-import io.mockk.unmockkAll
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.AfterEach
+import com.varabyte.kotter.foundation.input.Keys
+import com.varabyte.kotter.runtime.terminal.inmemory.press
+import com.varabyte.kotter.runtime.terminal.inmemory.type
+import com.varabyte.kotterx.test.foundation.testSession
+import de.maibornwolff.codecharta.importer.metricgardenerimporter.ParserDialog.Companion.myCollectParserArgs
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.Timeout
 import picocli.CommandLine
 import java.io.File
 
+@Timeout(120)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ParserDialogTest {
-    @AfterEach
-    fun afterTest() {
-        unmockkAll()
-    }
+    private val testResourceBaseFolder = "src/test/resources/"
+    private val inputFileName = "${testResourceBaseFolder}metricgardener-analysis.json"
+    private val outputFileName = "out.cc.json"
+    private val inputFolderName = "${testResourceBaseFolder}my"
 
     @Test
     fun `should output correct arguments when compress-flag is not set`() {
-        // given
-        val fileName = "metricGardenIn.json"
-        val outputFileName = "out.cc.json"
         val isCompressed = false
         val isJsonFile = true
 
-        mockkObject(InputHelper)
-        every { InputHelper.isInputValidAndNotNull(any(), any()) } returns true
+        testSession { terminal ->
+            val parserArguments = myCollectParserArgs(
+                jsonCallback = {
+                    terminal.press(Keys.ENTER)
+                },
+                fileCallback = {
+                    terminal.type(inputFileName)
+                    terminal.press(Keys.ENTER)
+                },
+                outFileCallback = {
+                    terminal.type(outputFileName)
+                    terminal.press(Keys.ENTER)
+                },
+                compressCallback = {
+                    terminal.press(Keys.RIGHT)
+                    terminal.press(Keys.ENTER)
+                }
+            )
 
-        mockkStatic("com.github.kinquirer.components.InputKt")
-        every {
-            KInquirer.promptInput(any(), any(), any(), any())
-        } returns fileName andThen outputFileName
-        mockkStatic("com.github.kinquirer.components.ConfirmKt")
-        every {
-            KInquirer.promptConfirm(any(), any())
-        } returns isJsonFile andThen isCompressed
+            val cmdLine = CommandLine(MetricGardenerImporter())
+            val parseResult = cmdLine.parseArgs(*parserArguments.toTypedArray())
 
-        // when
-        val parserArguments = ParserDialog.collectParserArgs()
-        val cmdLine = CommandLine(MetricGardenerImporter())
-        val parseResult = cmdLine.parseArgs(*parserArguments.toTypedArray())
-
-        // then
-        Assertions.assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(fileName)
-        Assertions.assertThat(parseResult.matchedOption("output-file").getValue<String>().equals(outputFileName))
-        Assertions.assertThat(parseResult.matchedOption("not-compressed").getValue<Boolean>()).isEqualTo(isCompressed)
-        Assertions.assertThat(parseResult.matchedOption("is-json-file").getValue<Boolean>()).isEqualTo(isJsonFile)
+            assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(File(inputFileName).name)
+            assertThat(parseResult.matchedOption("output-file").getValue<String>().equals(outputFileName))
+            assertThat(parseResult.matchedOption("not-compressed").getValue<Boolean>()).isEqualTo(isCompressed)
+            assertThat(parseResult.matchedOption("is-json-file").getValue<Boolean>()).isEqualTo(isJsonFile)
+        }
     }
 
     @Test
     fun `should output correct arguments when compress-flag is set`() {
-        // given
-        val fileName = "metricGardenIn.json"
-        val outputFileName = "out.cc.json"
-        val isCompressed = true
-        val isJsonFile = false
+        val isCompressed = false
 
-        mockkObject(InputHelper)
-        every { InputHelper.isInputValidAndNotNull(any(), any()) } returns true
+        testSession { terminal ->
+            val parserArguments = myCollectParserArgs(
+                jsonCallback = {
+                    terminal.press(Keys.RIGHT)
+                    terminal.press(Keys.ENTER)
+                },
+                fileCallback = {
+                    terminal.type(inputFolderName)
+                    terminal.press(Keys.ENTER)
+                },
+                outFileCallback = {
+                    terminal.type(outputFileName)
+                    terminal.press(Keys.ENTER)
+                },
+                compressCallback = {
+                    terminal.press(Keys.RIGHT)
+                    terminal.press(Keys.ENTER)
+                }
+            )
 
-        mockkStatic("com.github.kinquirer.components.InputKt")
-        every {
-            KInquirer.promptInput(any(), any(), any(), any())
-        } returns fileName andThen outputFileName
-        mockkStatic("com.github.kinquirer.components.ConfirmKt")
-        every {
-            KInquirer.promptConfirm(any(), any())
-        } returns isJsonFile andThen isCompressed
+            val cmdLine = CommandLine(MetricGardenerImporter())
+            val parseResult = cmdLine.parseArgs(*parserArguments.toTypedArray())
 
-        // when
-        val parserArguments = ParserDialog.collectParserArgs()
-        val cmdLine = CommandLine(MetricGardenerImporter())
-        val parseResult = cmdLine.parseArgs(*parserArguments.toTypedArray())
-
-        // then
-        Assertions.assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(fileName)
-        Assertions.assertThat(parseResult.matchedOption("output-file").getValue<String>().equals(outputFileName))
-        Assertions.assertThat(parseResult.matchedOption("not-compressed")).isNull()
-        Assertions.assertThat(parseResult.matchedOption("is-json-file")).isNull()
+            assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(File(inputFolderName).name)
+            assertThat(parseResult.matchedOption("output-file").getValue<String>().equals(outputFileName))
+            assertThat(parseResult.matchedOption("not-compressed").getValue<Boolean>()).isEqualTo(isCompressed)
+            assertThat(parseResult.matchedOption("is-json-file")).isNull()
+        }
     }
 
     @Test
     fun `should prompt user twice for input file when first input file is invalid`() {
-        // given
-        val invalidFileName = "invalidFileName"
-        val validFileName = "metricGardenIn.json"
-        val outputFileName = "out.cc.json"
+        val invalidFileName = "inv"
         val isCompressed = true
-        val isJsonFile = false
 
-        mockkObject(InputHelper)
-        every { InputHelper.isInputValidAndNotNull(any(), any()) } returns false andThen true
+        testSession { terminal ->
+            val parserArguments = myCollectParserArgs(
+                jsonCallback = {
+                    terminal.press(Keys.RIGHT)
+                    terminal.press(Keys.ENTER)
+                },
+                fileCallback = {
+                    terminal.type(invalidFileName)
+                    terminal.press(Keys.ENTER)
+                    terminal.press(Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE)
+                    terminal.type(inputFolderName)
+                    terminal.press(Keys.ENTER)
+                },
+                outFileCallback = {
+                    terminal.type(outputFileName)
+                    terminal.press(Keys.ENTER)
+                },
+                compressCallback = {
+                    terminal.press(Keys.ENTER)
+                }
+            )
 
-        mockkStatic("com.github.kinquirer.components.InputKt")
-        every {
-            KInquirer.promptInput(any(), any(), any(), any())
-        } returns invalidFileName andThen validFileName andThen outputFileName
-        mockkStatic("com.github.kinquirer.components.ConfirmKt")
-        every {
-            KInquirer.promptConfirm(any(), any())
-        } returns isJsonFile andThen isCompressed
+            val cmdLine = CommandLine(MetricGardenerImporter())
+            val parseResult = cmdLine.parseArgs(*parserArguments.toTypedArray())
 
-        // when
-        val parserArguments = ParserDialog.collectParserArgs()
-        val cmdLine = CommandLine(MetricGardenerImporter())
-        val parseResult = cmdLine.parseArgs(*parserArguments.toTypedArray())
-
-        // then
-        Assertions.assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(validFileName)
+            assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(File(inputFolderName).name)
+            assertThat(parseResult.matchedOption("not-compressed")).isNull()
+            assertThat(parseResult.matchedOption("is-json-file")).isNull()
+        }
     }
 }
