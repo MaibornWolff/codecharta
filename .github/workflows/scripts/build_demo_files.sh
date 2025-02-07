f9e37c1ded0b76fa14a6c775b1cb816efe85b1ee
Benedikt Mehl <b.mehl14@outlook.de>
2025-02-07 11:12:35 +0100

    Remove copying of dist/webpack #3910

--- .github/workflows/scripts/build_demo_files.sh
+++ .github/workflows/scripts/build_demo_files.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
-# Copy current build into gh-pages folder
-mkdir -p gh-pages/visualization/
-cp -R visualization/dist/coverage gh-pages/visualization/
-cp -R visualization/dist/webpack/ gh-pages/visualization/app/
-
 analysis/gradlew -p analysis/ installDist
 
-mkdir gh-pages/temp_dir
+mkdir temp_dir
 
-git ls-files > gh-pages/temp_dir/file-name-list.txt
-git log --numstat --raw --topo-order --reverse -m > gh-pages/temp_dir/git.log
+git ls-files > temp_dir/file-name-list.txt
+git log --numstat --raw --topo-order --reverse -m > temp_dir/git.log
 
-cd gh-pages/temp_dir || exit
+cd temp_dir || exit
 CCSH=../../analysis/build/install/codecharta-analysis/bin/ccsh
 
 # Data for for both visualization and analysis
@@ -24,18 +19,18 @@ $CCSH modify --set-root root/visualization -o codecharta_git_mod.cc.json codecha
 $CCSH sonarimport -nc -o codecharta_sonar.cc.json https://sonarcloud.io maibornwolff-gmbh_codecharta_visualization
 $CCSH modify --set-root root/maibornwolff-gmbh_codecharta_visualization -o codecharta_sonar_mod.cc.json codecharta_sonar.cc.json
 
-$CCSH merge -o ../visualization/app/codecharta.cc.json codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json -nc
+$CCSH merge -o ../visualization/dist/webpack/codecharta.cc.json codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json -nc
 # Zipped map for pipeline build
-$CCSH merge -o ../visualization/app/codecharta.cc.json codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json
+$CCSH merge -o ../visualization/dist/webpack/codecharta.cc.json codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json
 
 # Map for analysis
 $CCSH sonarimport -nc -o codecharta_sonar_analysis.cc.json https://sonarcloud.io maibornwolff-gmbh_codecharta_analysis
 $CCSH modify --set-root root/maibornwolff-gmbh_codecharta_analysis -o codecharta_sonar_mod.cc.json codecharta_sonar_analysis.cc.json
 $CCSH modify --set-root root/analysis -o codecharta_git_mod.cc.json codecharta_git.cc.json
-$CCSH merge -o ../visualization/app/codecharta_analysis.cc.json codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json -nc
+$CCSH merge -o ../visualization/dist/webpack/codecharta_analysis.cc.json codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json -nc
 # Zipped map for pipeline build
-$CCSH merge -o ../visualization/app/codecharta_analysis.cc.json codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json
+$CCSH merge -o ../visualization/dist/webpack/codecharta_analysis.cc.json codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json
 
 
-cd ../..
-rm -r gh-pages/temp_dir
+cd ..
+rm -r temp_dir
