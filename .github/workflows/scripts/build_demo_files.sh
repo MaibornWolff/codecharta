00a067fc6089e9913de882631f3067b5d5052b3f
Benedikt Mehl <b.mehl14@outlook.de>
2025-02-11 13:25:36 +0100

    Adjust to sonar importer changes #3910

--- .github/workflows/scripts/build_demo_files.sh
+++ .github/workflows/scripts/build_demo_files.sh
@@ -2,7 +2,6 @@
 
 analysis/gradlew -p analysis/ installDist
 
-<<<<<<<< HEAD:.github/workflows/scripts/build_demo_files.sh
 mkdir temp_dir
 
 git ls-files > temp_dir/file-name-list.txt
@@ -10,15 +9,6 @@ git log --numstat --raw --topo-order --reverse -m > temp_dir/git.log
 
 cd temp_dir || exit
 CCSH=../analysis/build/install/codecharta-analysis/bin/ccsh
-========
-mkdir gh-pages/temp_dir
-
-git ls-files > gh-pages/temp_dir/file-name-list.txt
-git log --numstat --raw --topo-order --reverse -m > gh-pages/temp_dir/git.log
-
-cd gh-pages/temp_dir || exit
-CCSH=../../analysis/build/install/codecharta-analysis/bin/ccsh
->>>>>>>> bb1a54a27 (Rename build demo file script #3910):script/build_demo_files.sh
 
 # Data for for both visualization and analysis
 $CCSH gitlogparser log-scan --git-log git.log --repo-files file-name-list.txt -o codecharta_git.cc.json -nc
@@ -27,7 +17,7 @@ $CCSH gitlogparser log-scan --git-log git.log --repo-files file-name-list.txt -o
 $CCSH modify --set-root root/visualization -o codecharta_git_mod.cc.json codecharta_git.cc.json
 
 $CCSH sonarimport -nc -o codecharta_sonar.cc.json https://sonarcloud.io maibornwolff-gmbh_codecharta_visualization
-$CCSH modify --set-root root/maibornwolff-gmbh_codecharta_visualization -o codecharta_sonar_mod.cc.json codecharta_sonar.cc.json
+$CCSH modify --set-root root/visualization -o codecharta_sonar_mod.cc.json codecharta_sonar.cc.json
 
 $CCSH merge -o ../visualization/dist/webpack/codecharta.cc.json codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json -nc
 # Zipped map for pipeline build
@@ -35,17 +25,12 @@ $CCSH merge -o ../visualization/dist/webpack/codecharta.cc.json codecharta_sonar
 
 # Map for analysis
 $CCSH sonarimport -nc -o codecharta_sonar_analysis.cc.json https://sonarcloud.io maibornwolff-gmbh_codecharta_analysis
-$CCSH modify --set-root root/maibornwolff-gmbh_codecharta_analysis -o codecharta_sonar_mod.cc.json codecharta_sonar_analysis.cc.json
+$CCSH modify --set-root root/analysis -o codecharta_sonar_mod.cc.json codecharta_sonar_analysis.cc.json
 $CCSH modify --set-root root/analysis -o codecharta_git_mod.cc.json codecharta_git.cc.json
 $CCSH merge -o ../visualization/dist/webpack/codecharta_analysis.cc.json codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json -nc
 # Zipped map for pipeline build
 $CCSH merge -o ../visualization/dist/webpack/codecharta_analysis.cc.json codecharta_sonar_mod.cc.json codecharta_git_mod.cc.json
 
 
-<<<<<<<< HEAD:.github/workflows/scripts/build_demo_files.sh
 cd ..
 rm -r temp_dir
-========
-cd ../..
-rm -r gh-pages/temp_dir
->>>>>>>> bb1a54a27 (Rename build demo file script #3910):script/build_demo_files.sh
