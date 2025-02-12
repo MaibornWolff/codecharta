7303ac35bcf6e8abe23ab1c2b9f58f3ee9e3a42c
Benedikt Mehl <b.mehl14@outlook.de>
2025-02-12 19:19:12 +0100

    Add tests for sorting #3710

--- visualization/app/codeCharta/state/selectors/accumulatedData/metricData/sortedNodeEdgeMetricsMap.selector.spec.ts
+++ visualization/app/codeCharta/state/selectors/accumulatedData/metricData/sortedNodeEdgeMetricsMap.selector.spec.ts
@@ -7,6 +7,8 @@ import { appReducers } from "../../../store/state.manager"
 import { setFiles } from "../../../store/files/files.actions"
 import { NodeEdgeMetricsMap } from "./edgeMetricData.calculator"
 import { firstValueFrom } from "rxjs"
+import { setShowOutgoingEdges } from "../../../store/appSettings/showEdges/outgoing/showOutgoingEdges.actions"
+import { setShowIncomingEdges } from "../../../store/appSettings/showEdges/incoming/showIncomingEdges.actions"
 
 describe("sortedNodeEdgeMetricsMapSelector", () => {
     let store: Store<CcState>
@@ -47,6 +49,47 @@ describe("sortedNodeEdgeMetricsMapSelector", () => {
                 ["/root/Parent Leaf/small leaf", { incoming: 1, outgoing: 0 }]
             ])
         )
+    })
+
+    it("should only return nodes with incoming edges if show outgoing edges is false", async () => {
+        store.dispatch(setShowOutgoingEdges({ value: false }))
+        await firstValueFrom(store.select(sortedNodeEdgeMetricsMapSelector))
+        store.dispatch(setFiles({ value: FILE_STATES }))
+
+        const result: NodeEdgeMetricsMap = await firstValueFrom(store.select(sortedNodeEdgeMetricsMapSelector))
+
+        expect(result.get("pairingRate")).toEqual(new Map([["/root/Parent Leaf/small leaf", { incoming: 2, outgoing: 0 }]]))
+        expect(result.get("avgCommits")).toEqual(new Map([["/root/Parent Leaf/small leaf", { incoming: 1, outgoing: 0 }]]))
+        expect(result.get("otherMetric")).toEqual(new Map([["/root/Parent Leaf/small leaf", { incoming: 1, outgoing: 0 }]]))
+    })
+
+    it("should only return nodes with outgoing edges if show incoming edges is false", async () => {
+        store.dispatch(setShowIncomingEdges({ value: false }))
+        await firstValueFrom(store.select(sortedNodeEdgeMetricsMapSelector))
+        store.dispatch(setFiles({ value: FILE_STATES }))
+
+        const result: NodeEdgeMetricsMap = await firstValueFrom(store.select(sortedNodeEdgeMetricsMapSelector))
+
+        expect(result.get("pairingRate")).toEqual(
+            new Map([
+                ["/root/big leaf", { incoming: 0, outgoing: 1 }],
+                ["/root/Parent Leaf/other small leaf", { incoming: 0, outgoing: 1 }]
+            ])
+        )
+        expect(result.get("avgCommits")).toEqual(new Map([["/root/big leaf", { incoming: 0, outgoing: 1 }]]))
+        expect(result.get("otherMetric")).toEqual(new Map([["/root/Parent Leaf/other small leaf", { incoming: 0, outgoing: 1 }]]))
+    })
+
+    it("should return an empty map if incoming and outgoing edges are disabled", async () => {
+        store.dispatch(setShowIncomingEdges({ value: false }))
+        await firstValueFrom(store.select(sortedNodeEdgeMetricsMapSelector))
+        store.dispatch(setShowOutgoingEdges({ value: false }))
+        await firstValueFrom(store.select(sortedNodeEdgeMetricsMapSelector))
+        store.dispatch(setFiles({ value: FILE_STATES }))
+
+        const result: NodeEdgeMetricsMap = await firstValueFrom(store.select(sortedNodeEdgeMetricsMapSelector))
+
+        expect(result).toEqual(new Map())
     })
 
     it("should return an empty map if there are no edges", async () => {
