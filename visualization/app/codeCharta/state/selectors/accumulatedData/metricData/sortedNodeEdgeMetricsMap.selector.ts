5ae0b30cd0f62023cfe1ddf65524f8ee373d2223
Benedikt Mehl <b.mehl14@outlook.de>
2025-02-13 10:19:14 +0100

    refactor: clean up sorting algorithm #3710

--- visualization/app/codeCharta/state/selectors/accumulatedData/metricData/sortedNodeEdgeMetricsMap.selector.ts
+++ visualization/app/codeCharta/state/selectors/accumulatedData/metricData/sortedNodeEdgeMetricsMap.selector.ts
@@ -1,9 +1,9 @@
 import { createSelector } from "@ngrx/store"
 import { visibleFileStatesSelector } from "../../visibleFileStates/visibleFileStates.selector"
 import { blacklistSelector } from "../../../store/fileSettings/blacklist/blacklist.selector"
-import { calculateEdgeMetricData, NodeEdgeMetricsMap } from "./edgeMetricData.calculator"
+import { calculateEdgeMetricData, EdgeMetricCountMap, NodeEdgeMetricsMap } from "./edgeMetricData.calculator"
 import { FileState } from "../../../../model/files/files"
-import { BlacklistItem } from "../../../../codeCharta.model"
+import { BlacklistItem, EdgeMetricCount } from "../../../../codeCharta.model"
 import { showIncomingEdgesSelector } from "../../../store/appSettings/showEdges/incoming/showIncomingEdges.selector"
 import { showOutgoingEdgesSelector } from "../../../store/appSettings/showEdges/outgoing/showOutgoingEdges.selector"
 
@@ -22,29 +22,48 @@ function sortNodeEdgeMetricsMap(
     showOutgoingEdges: boolean
 ): NodeEdgeMetricsMap {
     const { nodeEdgeMetricsMap } = calculateEdgeMetricData(visibleFileStates, blacklist)
+
     if (!showIncomingEdges && !showOutgoingEdges) {
         return new Map()
     }
-    const sortedNodeEdgeMetricsMap = new Map()
 
-    for (const [metric, nodeEdgeMetricCounts] of nodeEdgeMetricsMap) {
-        const newSortedEdgeMetricEntry = new Map(
-            [...nodeEdgeMetricCounts.entries()]
-                .filter(nodeEdgeMetricCounts => {
-                    const incomingEdgesVisible = showIncomingEdges && nodeEdgeMetricCounts[1].incoming > 0
-                    const outgoingEdgesVisible = showOutgoingEdges && nodeEdgeMetricCounts[1].outgoing > 0
-                    return incomingEdgesVisible || outgoingEdgesVisible
-                })
-                .sort((a, b) => {
-                    const aCountIncoming = showIncomingEdges ? a[1].incoming : 0
-                    const aCountOutgoing = showOutgoingEdges ? a[1].outgoing : 0
-                    const bCountIncoming = showIncomingEdges ? b[1].incoming : 0
-                    const bCountOutgoing = showOutgoingEdges ? b[1].outgoing : 0
-                    return bCountIncoming + bCountOutgoing - (aCountIncoming + aCountOutgoing)
-                })
-        )
-        sortedNodeEdgeMetricsMap.set(metric, newSortedEdgeMetricEntry)
+    const sortedMetricsMap = new Map()
+
+    for (const [metricName, nodeMetrics] of nodeEdgeMetricsMap) {
+        const filteredNodes = filterVisibleNodes(nodeMetrics, showIncomingEdges, showOutgoingEdges)
+        const sortedNodes = sortNodesByEdgeCounts(filteredNodes, showIncomingEdges, showOutgoingEdges)
+        sortedMetricsMap.set(metricName, new Map(sortedNodes))
     }
 
-    return sortedNodeEdgeMetricsMap
+    return sortedMetricsMap
+}
+
+function filterVisibleNodes(
+    nodeMetrics: EdgeMetricCountMap,
+    showIncoming: boolean,
+    showOutgoing: boolean
+): Array<[string, EdgeMetricCount]> {
+    return Array.from(nodeMetrics.entries()).filter(([_, metrics]) => {
+        const hasAtLeastOneIncoming = metrics.incoming > 0
+        const hasVisibleIncoming = showIncoming && hasAtLeastOneIncoming
+        const hasAtLeastOneOutgoing = metrics.outgoing > 0
+        const hasVisibleOutgoing = showOutgoing && hasAtLeastOneOutgoing
+        return hasVisibleIncoming || hasVisibleOutgoing
+    })
+}
+
+function sortNodesByEdgeCounts(
+    nodes: Array<[string, EdgeMetricCount]>,
+    showIncoming: boolean,
+    showOutgoing: boolean
+): Array<[string, EdgeMetricCount]> {
+    return nodes.sort(([_, metricCountsA], [__, metricCountsB]) => {
+        const totalA = calculateVisibleTotal(metricCountsA, showIncoming, showOutgoing)
+        const totalB = calculateVisibleTotal(metricCountsB, showIncoming, showOutgoing)
+        return totalB - totalA
+    })
+}
+
+function calculateVisibleTotal(metrics: EdgeMetricCount, includeIncoming: boolean, includeOutgoing: boolean): number {
+    return (includeIncoming ? metrics.incoming : 0) + (includeOutgoing ? metrics.outgoing : 0)
 }
