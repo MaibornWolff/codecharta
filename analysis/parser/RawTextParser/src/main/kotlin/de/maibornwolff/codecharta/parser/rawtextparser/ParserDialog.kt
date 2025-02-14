5b789f79277b43b10736826db4a1c32d05d73016
Benedikt Mehl <b.mehl14@outlook.de>
2025-02-14 15:47:48 +0100

    refactor: rawtext parser for testing #3495

--- analysis/parser/RawTextParser/src/main/kotlin/de/maibornwolff/codecharta/parser/rawtextparser/ParserDialog.kt
+++ analysis/parser/RawTextParser/src/main/kotlin/de/maibornwolff/codecharta/parser/rawtextparser/ParserDialog.kt
@@ -13,90 +13,79 @@ import de.maibornwolff.codecharta.tools.interactiveparser.ParserDialogInterface
 class ParserDialog {
     companion object : ParserDialogInterface {
         override fun collectParserArgs(): List<String> {
-            var res = listOf<String>()
-            session { res = myCollectParserArgs() }
+            val res = listOf<String>()
+            session { collectParserArgs2(this) }
             return res
         }
 
-        internal fun Session.myCollectParserArgs(
-            fileCallback: suspend RunScope.() -> Unit = {},
-            outFileCallback: suspend RunScope.() -> Unit = {},
-            compressCallback: suspend RunScope.() -> Unit = {},
-            verboseCallback: suspend RunScope.() -> Unit = {},
-            metricCallback: suspend RunScope.() -> Unit = {},
-            tabCallback: suspend RunScope.() -> Unit = {},
-            indentationCallback: suspend RunScope.() -> Unit = {},
-            excludeCallback: suspend RunScope.() -> Unit = {},
-            extensionCallback: suspend RunScope.() -> Unit = {},
-            defaultCallback: suspend RunScope.() -> Unit = {}
-        ): List<String> {
-            val inputFileName = myPromptDefaultFileFolderInput(
+        fun collectParserArgs2(session: Session): List<String> {
+            val inputFileName = session.myPromptDefaultFileFolderInput(
                 inputType = InputType.FOLDER_AND_FILE,
                 fileExtensionList = listOf(),
-                onInputReady = fileCallback
+                onInputReady = fileCallback()
             )
 
-            val outputFileName: String = myPromptInput(
-                message = "What is the name of the output file?",
+            val outputFileName: String = session.myPromptInput(
+                message = "xxWhat is the name of the output file?",
                 allowEmptyInput = true,
-                onInputReady = outFileCallback
+                onInputReady = outFileCallback()
             )
 
             val isCompressed =
                 (outputFileName.isEmpty()) ||
-                    myPromptConfirm(
+                    session.myPromptConfirm(
                         message = "Do you want to compress the output file?",
-                        onInputReady = compressCallback
+                        onInputReady = compressCallback()
                     )
 
-            val verbose: Boolean = myPromptConfirm(
+            val verbose: Boolean = session.myPromptConfirm(
                 message = "Do you want to suppress command line output?",
-                onInputReady = verboseCallback
+                onInputReady = verboseCallback()
             )
 
-            val metrics: String = myPromptInput(
+            val metrics: String = session.myPromptInput(
                 message = "What are the metrics to import (comma separated)?",
                 hint = "metric1,metric2,metric3 (leave empty for all metrics)",
                 allowEmptyInput = true,
-                onInputReady = metricCallback
+                onInputReady = metricCallback()
             )
 
-            val tabWidth: String = myPromptInputNumber(
+            val tabWidth: String = session.myPromptInputNumber(
                 message = "How many spaces represent one indentation level when using spaces for indentation (estimated if empty)?",
                 allowEmptyInput = true,
-                onInputReady = tabCallback
+                onInputReady = tabCallback()
             )
 
             val tabWidthValue = tabWidth.toIntOrNull() ?: 0
 
-            val maxIndentationLevel: Int = myPromptInputNumber(
+            val maxIndentationLevel: Int = session.myPromptInputNumber(
                 message = "What is the maximum Indentation Level?",
                 hint = "10",
                 allowEmptyInput = false,
-                onInputReady = indentationCallback
+                onInputReady = indentationCallback()
             ).toInt()
 
             val exclude: String =
-                myPromptInput(
+                session.myPromptInput(
                     message = "Do you want to exclude file/folder according to regex pattern?",
                     hint = "regex1, regex2.. (leave empty if you don't want to exclude anything)",
                     allowEmptyInput = true,
-                    onInputReady = excludeCallback
+                    onInputReady = excludeCallback()
                 )
 
             val fileExtensions: String =
-                myPromptInput(
+                session.myPromptInput(
                     message = "Do you only want to parse files with specific file-extensions? ",
                     hint = "fileType1, fileType2... (leave empty to include all file-extensions)",
                     allowEmptyInput = true,
-                    onInputReady = extensionCallback
+                    onInputReady = extensionCallback()
                 )
 
             val withoutDefaultExcludes: Boolean =
-                myPromptConfirm(
+                session.myPromptConfirm(
                     message = "Do you want to include build, target, dist, resources" +
                         " and out folders as well as files/folders starting with '.'?",
-                    onInputReady = defaultCallback
+                    onInputReady = defaultCallback()
                 )
 
             return listOfNotNull(
@@ -111,6 +100,47 @@ class ParserDialog {
                 "--file-extensions=$fileExtensions",
                 "--without-default-excludes=$withoutDefaultExcludes"
             )
+        }
+
+        // For testing purposes
+        internal fun fileCallback(): suspend RunScope.() -> Unit {
+            return {}
+        }
+
+        internal fun outFileCallback(): suspend RunScope.() -> Unit {
+            return {}
+        }
+
+        internal fun compressCallback(): suspend RunScope.() -> Unit {
+            return {}
+        }
+
+        internal fun verboseCallback(): suspend RunScope.() -> Unit {
+            return {}
+        }
+
+        internal fun metricCallback(): suspend RunScope.() -> Unit {
+            return {}
+        }
+
+        internal fun tabCallback(): suspend RunScope.() -> Unit {
+            return {}
+        }
+
+        internal fun indentationCallback(): suspend RunScope.() -> Unit {
+            return {}
+        }
+
+        internal fun excludeCallback(): suspend RunScope.() -> Unit {
+            return {}
+        }
+
+        internal fun extensionCallback(): suspend RunScope.() -> Unit {
+            return {}
+        }
+
+        internal fun defaultCallback(): suspend RunScope.() -> Unit {
+            return {}
         }
     }
 }
--- analysis/parser/RawTextParser/src/test/kotlin/de/maibornwolff/codecharta/rawtextparser/ParserDialogTest.kt
+++ analysis/parser/RawTextParser/src/test/kotlin/de/maibornwolff/codecharta/rawtextparser/ParserDialogTest.kt
@@ -4,8 +4,11 @@ import com.varabyte.kotter.foundation.input.Keys
 import com.varabyte.kotter.runtime.terminal.inmemory.press
 import com.varabyte.kotter.runtime.terminal.inmemory.type
 import com.varabyte.kotterx.test.foundation.testSession
-import de.maibornwolff.codecharta.parser.rawtextparser.ParserDialog.Companion.myCollectParserArgs
+import de.maibornwolff.codecharta.parser.rawtextparser.ParserDialog
+import de.maibornwolff.codecharta.parser.rawtextparser.ParserDialog.Companion.collectParserArgs2
 import de.maibornwolff.codecharta.parser.rawtextparser.RawTextParser
+import io.mockk.every
+import io.mockk.mockkObject
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -34,66 +37,70 @@ class ParserDialogTest {
         val exclude = "file1"
         val withoutDefaultExcludes = false
 
-        testSession { terminal ->
-            val parserArguments = myCollectParserArgs(
-                fileCallback = {
-                    terminal.type(inputFileName)
-                    terminal.press(Keys.ENTER)
-                },
-                outFileCallback = {
-                    terminal.type(outputFileName)
-                    terminal.press(Keys.ENTER)
-                },
-                compressCallback = {
-                    terminal.press(Keys.RIGHT)
-                    terminal.press(Keys.ENTER)
-                },
-                verboseCallback = {
-                    terminal.press(Keys.RIGHT)
-                    terminal.press(Keys.ENTER)
-                },
-                metricCallback = {
-                    terminal.type(metrics)
-                    terminal.press(Keys.ENTER)
-                },
-                tabCallback = {
-                    terminal.type(tabWidth)
-                    terminal.press(Keys.ENTER)
-                },
-                indentationCallback = {
-                    terminal.type(maxIndentLvl.toString())
-                    terminal.press(Keys.ENTER)
-                },
-                excludeCallback = {
-                    terminal.type(exclude)
-                    terminal.press(Keys.ENTER)
-                },
-                extensionCallback = {
-                    terminal.press(Keys.ENTER)
-                },
-                defaultCallback = {
-                    terminal.press(Keys.RIGHT)
-                    terminal.press(Keys.ENTER)
-                }
-            )
-            val commandLine = CommandLine(RawTextParser())
-            val parseResult = commandLine.parseArgs(*parserArguments.toTypedArray())
+        mockkObject(ParserDialog.Companion)
 
-            assertThat(parseResult.matchedOption("output-file").getValue<String>().equals(outputFileName))
-            assertThat(parseResult.matchedOption("not-compressed").getValue<Boolean>()).isEqualTo(isCompressed)
-            assertThat(parseResult.matchedOption("metrics").getValue<List<String>>()).isEqualTo(listOf(metrics))
-            assertThat(parseResult.matchedOption("max-indentation-level").getValue<Int>())
-                .isEqualTo(maxIndentLvl)
-            assertThat(parseResult.matchedOption("tab-width").getValue<Int>()).isEqualTo(tabWidthValue)
-            assertThat(parseResult.matchedOption("file-extensions").getValue<List<String>>())
-                .isEqualTo(listOf<String>())
-            assertThat(parseResult.matchedOption("without-default-excludes").getValue<Boolean>())
-                .isEqualTo(withoutDefaultExcludes)
-            assertThat(parseResult.matchedOption("verbose").getValue<Boolean>())
-                .isEqualTo(verbose)
-            assertThat(parseResult.matchedOption("exclude").getValue<List<String>>()).isEqualTo(listOf(exclude))
-            assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(File(inputFileName).name)
+        var parserArguments: List<String> = emptyList()
+
+        testSession { terminal ->
+            every { ParserDialog.Companion.fileCallback() } returns {
+                terminal.type(inputFileName)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.outFileCallback() } returns {
+                terminal.type(outputFileName)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.compressCallback() } returns {
+                terminal.press(Keys.RIGHT)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.verboseCallback() } returns {
+                terminal.press(Keys.RIGHT)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.metricCallback() } returns {
+                terminal.type(metrics)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.tabCallback() } returns {
+                terminal.type(tabWidth)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.indentationCallback() } returns {
+                terminal.type(maxIndentLvl.toString())
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.excludeCallback() } returns {
+                terminal.type(exclude)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.extensionCallback() } returns {
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.defaultCallback() } returns {
+                terminal.press(Keys.RIGHT)
+                terminal.press(Keys.ENTER)
+            }
+
+            parserArguments = collectParserArgs2(this)
         }
+        val commandLine = CommandLine(RawTextParser())
+        val parseResult = commandLine.parseArgs(*parserArguments.toTypedArray())
+
+        assertThat(parseResult.matchedOption("output-file").getValue<String>().equals(outputFileName))
+        assertThat(parseResult.matchedOption("not-compressed").getValue<Boolean>()).isEqualTo(isCompressed)
+        assertThat(parseResult.matchedOption("metrics").getValue<List<String>>()).isEqualTo(listOf(metrics))
+        assertThat(parseResult.matchedOption("max-indentation-level").getValue<Int>())
+            .isEqualTo(maxIndentLvl)
+        assertThat(parseResult.matchedOption("tab-width").getValue<Int>()).isEqualTo(tabWidthValue)
+        assertThat(parseResult.matchedOption("file-extensions").getValue<List<String>>())
+            .isEqualTo(listOf<String>())
+        assertThat(parseResult.matchedOption("without-default-excludes").getValue<Boolean>())
+            .isEqualTo(withoutDefaultExcludes)
+        assertThat(parseResult.matchedOption("verbose").getValue<Boolean>())
+            .isEqualTo(verbose)
+        assertThat(parseResult.matchedOption("exclude").getValue<List<String>>()).isEqualTo(listOf(exclude))
+        assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(File(inputFileName).name)
     }
 
     @ParameterizedTest
@@ -107,66 +114,70 @@ class ParserDialogTest {
         val exclude = "file1"
         val withoutDefaultExcludes = false
 
-        testSession { terminal ->
-            val parserArguments = myCollectParserArgs(
-                fileCallback = {
-                    terminal.type(inputFileName)
-                    terminal.press(Keys.ENTER)
-                },
-                outFileCallback = {
-                    terminal.type(outputFileName)
-                    terminal.press(Keys.ENTER)
-                },
-                compressCallback = {
-                    terminal.press(Keys.RIGHT)
-                    terminal.press(Keys.ENTER)
-                },
-                verboseCallback = {
-                    terminal.press(Keys.RIGHT)
-                    terminal.press(Keys.ENTER)
-                },
-                metricCallback = {
-                    terminal.type(metrics)
-                    terminal.press(Keys.ENTER)
-                },
-                tabCallback = {
-                    terminal.type(invalidTabWidth)
-                    terminal.press(Keys.ENTER)
-                },
-                indentationCallback = {
-                    terminal.type(maxIndentLvl.toString())
-                    terminal.press(Keys.ENTER)
-                },
-                excludeCallback = {
-                    terminal.type(exclude)
-                    terminal.press(Keys.ENTER)
-                },
-                extensionCallback = {
-                    terminal.press(Keys.ENTER)
-                },
-                defaultCallback = {
-                    terminal.press(Keys.RIGHT)
-                    terminal.press(Keys.ENTER)
-                }
-            )
-            val commandLine = CommandLine(RawTextParser())
-            val parseResult = commandLine.parseArgs(*parserArguments.toTypedArray())
+        mockkObject(ParserDialog.Companion)
 
-            assertThat(parseResult.matchedOption("output-file").getValue<String>().equals(outputFileName))
-            assertThat(parseResult.matchedOption("not-compressed").getValue<Boolean>()).isEqualTo(isCompressed)
-            assertThat(parseResult.matchedOption("metrics").getValue<List<String>>()).isEqualTo(listOf(metrics))
-            assertThat(parseResult.matchedOption("max-indentation-level").getValue<Int>())
-                .isEqualTo(maxIndentLvl)
-            assertThat(parseResult.matchedOption("tab-width").getValue<Int>()).isEqualTo(tabWidthValue)
-            assertThat(parseResult.matchedOption("file-extensions").getValue<List<String>>())
-                .isEqualTo(listOf<String>())
-            assertThat(parseResult.matchedOption("without-default-excludes").getValue<Boolean>())
-                .isEqualTo(withoutDefaultExcludes)
-            assertThat(parseResult.matchedOption("verbose").getValue<Boolean>())
-                .isEqualTo(verbose)
-            assertThat(parseResult.matchedOption("exclude").getValue<List<String>>()).isEqualTo(listOf(exclude))
-            assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(File(inputFileName).name)
+        var parserArguments: List<String> = emptyList()
+
+        testSession { terminal ->
+            every { ParserDialog.Companion.fileCallback() } returns {
+                terminal.type(inputFileName)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.outFileCallback() } returns {
+                terminal.type(outputFileName)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.compressCallback() } returns {
+                terminal.press(Keys.RIGHT)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.verboseCallback() } returns {
+                terminal.press(Keys.RIGHT)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.metricCallback() } returns {
+                terminal.type(metrics)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.tabCallback() } returns {
+                terminal.type(invalidTabWidth)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.indentationCallback() } returns {
+                terminal.type(maxIndentLvl.toString())
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.excludeCallback() } returns {
+                terminal.type(exclude)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.extensionCallback() } returns {
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.defaultCallback() } returns {
+                terminal.press(Keys.RIGHT)
+                terminal.press(Keys.ENTER)
+            }
+
+            parserArguments = collectParserArgs2(this)
         }
+        val commandLine = CommandLine(RawTextParser())
+        val parseResult = commandLine.parseArgs(*parserArguments.toTypedArray())
+
+        assertThat(parseResult.matchedOption("output-file").getValue<String>().equals(outputFileName))
+        assertThat(parseResult.matchedOption("not-compressed").getValue<Boolean>()).isEqualTo(isCompressed)
+        assertThat(parseResult.matchedOption("metrics").getValue<List<String>>()).isEqualTo(listOf(metrics))
+        assertThat(parseResult.matchedOption("max-indentation-level").getValue<Int>())
+            .isEqualTo(maxIndentLvl)
+        assertThat(parseResult.matchedOption("tab-width").getValue<Int>()).isEqualTo(tabWidthValue)
+        assertThat(parseResult.matchedOption("file-extensions").getValue<List<String>>())
+            .isEqualTo(listOf<String>())
+        assertThat(parseResult.matchedOption("without-default-excludes").getValue<Boolean>())
+            .isEqualTo(withoutDefaultExcludes)
+        assertThat(parseResult.matchedOption("verbose").getValue<Boolean>())
+            .isEqualTo(verbose)
+        assertThat(parseResult.matchedOption("exclude").getValue<List<String>>()).isEqualTo(listOf(exclude))
+        assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(File(inputFileName).name)
     }
 
     @Test
@@ -177,53 +188,57 @@ class ParserDialogTest {
         val maxIndentLvl = 10
         val exclude = "file1"
 
-        testSession { terminal ->
-            val parserArguments = myCollectParserArgs(
-                fileCallback = {
-                    terminal.type(invalidFileName)
-                    terminal.press(Keys.ENTER)
-                    terminal.press(Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE)
-                    terminal.type(inputFileName)
-                    terminal.press(Keys.ENTER)
-                },
-                outFileCallback = {
-                    terminal.type(outputFileName)
-                    terminal.press(Keys.ENTER)
-                },
-                compressCallback = {
-                    terminal.press(Keys.ENTER)
-                },
-                verboseCallback = {
-                    terminal.press(Keys.ENTER)
-                },
-                metricCallback = {
-                    terminal.type(metrics)
-                    terminal.press(Keys.ENTER)
-                },
-                tabCallback = {
-                    terminal.type(tabWidth)
-                    terminal.press(Keys.ENTER)
-                },
-                indentationCallback = {
-                    terminal.type(maxIndentLvl.toString())
-                    terminal.press(Keys.ENTER)
-                },
-                excludeCallback = {
-                    terminal.type(exclude)
-                    terminal.press(Keys.ENTER)
-                },
-                extensionCallback = {
-                    terminal.press(Keys.ENTER)
-                },
-                defaultCallback = {
-                    terminal.press(Keys.ENTER)
-                }
-            )
-            val commandLine = CommandLine(RawTextParser())
-            val parseResult = commandLine.parseArgs(*parserArguments.toTypedArray())
+        mockkObject(ParserDialog.Companion)
 
-            assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(File(inputFileName).name)
+        var parserArguments: List<String> = emptyList()
+
+        testSession { terminal ->
+            every { ParserDialog.Companion.fileCallback() } returns {
+                terminal.type(invalidFileName)
+                terminal.press(Keys.ENTER)
+                terminal.press(Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE)
+                terminal.type(inputFileName)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.outFileCallback() } returns {
+                terminal.type(outputFileName)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.compressCallback() } returns {
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.verboseCallback() } returns {
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.metricCallback() } returns {
+                terminal.type(metrics)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.tabCallback() } returns {
+                terminal.type(tabWidth)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.indentationCallback() } returns {
+                terminal.type(maxIndentLvl.toString())
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.excludeCallback() } returns {
+                terminal.type(exclude)
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.extensionCallback() } returns {
+                terminal.press(Keys.ENTER)
+            }
+            every { ParserDialog.Companion.defaultCallback() } returns {
+                terminal.press(Keys.ENTER)
+            }
+
+            parserArguments = collectParserArgs2(this)
         }
+        val commandLine = CommandLine(RawTextParser())
+        val parseResult = commandLine.parseArgs(*parserArguments.toTypedArray())
+
+        assertThat(parseResult.matchedPositional(0).getValue<File>().name).isEqualTo(File(inputFileName).name)
     }
 
     private fun provideInvalidTabWidth(): List<Arguments> {
