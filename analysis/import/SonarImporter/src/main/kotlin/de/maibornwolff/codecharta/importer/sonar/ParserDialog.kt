bf4ad5374115c64474295b9877b7a79506a37061
Benedikt Mehl <b.mehl14@outlook.de>
2025-02-19 13:47:24 +0100

    test(analysis): improve sonar importer cov #3483

--- analysis/import/SonarImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/sonar/ParserDialog.kt
+++ analysis/import/SonarImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/sonar/ParserDialog.kt
@@ -14,7 +14,7 @@ class ParserDialog {
                 hint = "https://sonarcloud.io/",
                 allowEmptyInput = false,
                 invalidInputMessage = "Empty hostUrl is not allowed!",
-                onInputReady = hostCallback()
+                onInputReady = testCallback()
             )
 
             val projectKey = session.myPromptInput(
@@ -22,35 +22,35 @@ class ParserDialog {
                 hint = "Unique identifier of your project",
                 allowEmptyInput = false,
                 invalidInputMessage = "Empty projectKey is not allowed!",
-                onInputReady = projectCallback()
+                onInputReady = testCallback()
             )
 
             val userToken: String = session.myPromptInput(
                 message = "What is the sonar user token (sonar.login) required to connect to the remote Sonar instance?",
                 hint = "sqp_0a81f6490875e062f79ccdeace23ac3c68dac6e",
                 allowEmptyInput = true,
-                onInputReady = tokenCallback()
+                onInputReady = testCallback()
             )
 
             val outputFileName: String = session.myPromptInput(
                 message = "What is the name of the output file?",
                 allowEmptyInput = true,
-                onInputReady = outFileCallback()
+                onInputReady = testCallback()
             )
 
             val metrics: String = session.myPromptInput(
                 message = "What are the metrics to import (comma separated)?",
                 hint = "metric1,metric2,metric3 (leave empty for all metrics)",
                 allowEmptyInput = true,
-                onInputReady = metricCallback()
+                onInputReady = testCallback()
             )
 
             val isCompressed: Boolean = (outputFileName.isEmpty()) || session.myPromptConfirm(
-                message = "Do you want to compress the output file?", onInputReady = compressCallback()
+                message = "Do you want to compress the output file?", onInputReady = testCallback()
             )
 
             val mergeModules: Boolean =
-                session.myPromptConfirm(message = "Do you want to merge modules in multi-module projects?", onInputReady = mergeCallback())
+                session.myPromptConfirm(message = "Do you want to merge modules in multi-module projects?", onInputReady = testCallback())
 
             return listOfNotNull(
                 hostUrl,
@@ -65,18 +65,6 @@ class ParserDialog {
 
         private fun eraseWhitespace(input: String) = input.replace(" ", "")
 
-        internal fun hostCallback(): suspend RunScope.() -> Unit = {}
-
-        internal fun projectCallback(): suspend RunScope.() -> Unit = {}
-
-        internal fun tokenCallback(): suspend RunScope.() -> Unit = {}
-
-        internal fun outFileCallback(): suspend RunScope.() -> Unit = {}
-
-        internal fun metricCallback(): suspend RunScope.() -> Unit = {}
-
-        internal fun compressCallback(): suspend RunScope.() -> Unit = {}
-
-        internal fun mergeCallback(): suspend RunScope.() -> Unit = {}
+        internal fun testCallback(): suspend RunScope.() -> Unit = {}
     }
 }
