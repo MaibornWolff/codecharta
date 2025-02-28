7db12ce30eedf1e05d6990ffd1cb220ed88d20fc
Benedikt Mehl <b.mehl14@outlook.de>
2025-02-28 17:33:54 +0100

    test(analysis): add CoverageImporterTest (#3985)

--- analysis/import/CoverageImporter/src/test/kotlin/languages/JavaScriptTypeScriptStrategyTest.kt
+++ analysis/import/CoverageImporter/src/test/kotlin/languages/JavaScriptTypeScriptStrategyTest.kt
@@ -3,33 +3,25 @@ package languages
 import de.maibornwolff.codecharta.importer.tokeiimporter.strategy.JavaScriptTypeScriptStrategy
 import de.maibornwolff.codecharta.model.ProjectBuilder
 import de.maibornwolff.codecharta.serialization.ProjectDeserializer
-import de.maibornwolff.codecharta.serialization.ProjectSerializer
 import io.mockk.unmockkAll
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.skyscreamer.jsonassert.JSONAssert
-import org.skyscreamer.jsonassert.JSONCompareMode
 import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.OutputStreamWriter
-import java.io.PrintStream
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class JavaScriptTypeScriptStrategyTest {
-    private var errContent = ByteArrayOutputStream()
-    private val originalErr = System.err
 
     @AfterEach
     fun afterTest() {
         unmockkAll()
-        errContent = ByteArrayOutputStream()
     }
 
     @Test
     fun `should contain minimal expected content`() {
-        val expectedFilePath = "src/test/resources/languages/JavaScriptTypeScript/minimal_output.cc.json"
+        val expectedFilePath = "src/test/resources/languages/JavaScriptTypeScript/minimal_expected_output.cc.json"
         val expectedProject = ProjectDeserializer.deserializeProject(File(expectedFilePath).inputStream())
         val coverageReport = File("src/test/resources/languages/JavaScriptTypeScript/minimal_lcov.info")
         val projectBuilder = ProjectBuilder()
@@ -39,5 +31,4 @@ class JavaScriptTypeScriptStrategyTest {
         val project = projectBuilder.build()
         assertThat(project.toString()).isEqualTo(expectedProject.toString())
     }
-
 }
