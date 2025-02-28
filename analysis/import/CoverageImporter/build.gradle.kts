c0226081d365785a641b812b5241daad5c670bf2
Benedikt Mehl <b.mehl14@outlook.de>
2025-02-28 12:55:58 +0100

    chore(analysis): adjust gradle (#3985)

--- analysis/import/CoverageImporter/build.gradle.kts
+++ analysis/import/CoverageImporter/build.gradle.kts
@@ -1,10 +1,12 @@
 dependencies {
     implementation(project(":model"))
+    implementation(project(":tools:Inquirer"))
     implementation(project(":tools:InteractiveParser"))
+    implementation(project(":tools:PipeableParser"))
 
-    implementation(libs.univocity.parsers)
     implementation(libs.picocli)
-    implementation(libs.kotlin.inquirer)
+    implementation(libs.kotter)
+    implementation(libs.kotter.test)
 
     testImplementation(libs.junit.jupiter.api)
     testImplementation(libs.kotlin.test)
