3b3535447c3ca3073ca40bb697f1a11747cab9e7
Benedikt Mehl <b.mehl14@outlook.de>
2025-02-28 23:21:29 +0100

    test(analysis): add ParserDialog (#3985)

---
+++ analysis/import/CoverageImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/coverage/ParserDialog.kt
@@ -0,0 +1,60 @@
+package de.maibornwolff.codecharta.importer.coverageimporter
+
+import com.varabyte.kotter.runtime.RunScope
+import com.varabyte.kotter.runtime.Session
+import de.maibornwolff.codecharta.importer.coverage.languages.getFileExtensionsForLanguage
+import de.maibornwolff.codecharta.importer.coverage.languages.getLanguageForLanguageChoice
+import de.maibornwolff.codecharta.importer.coverage.languages.languageChoicesToLanguage
+import de.maibornwolff.codecharta.tools.inquirer.InputType
+import de.maibornwolff.codecharta.tools.inquirer.myPromptConfirm
+import de.maibornwolff.codecharta.tools.inquirer.myPromptDefaultFileFolderInput
+import de.maibornwolff.codecharta.tools.inquirer.myPromptInput
+import de.maibornwolff.codecharta.tools.inquirer.myPromptList
+import de.maibornwolff.codecharta.tools.interactiveparser.ParserDialogInterface
+import java.util.Locale
+
+class ParserDialog {
+    companion object : ParserDialogInterface {
+        override fun collectParserArgs(session: Session): List<String> {
+            val languageChoice: String = session.myPromptList(
+                message = "Specify the language of the coverage report",
+                choices = languageChoicesToLanguage.keys.toList(),
+                onInputReady = testCallback()
+            ).lowercase(Locale.getDefault())
+
+            val language = getLanguageForLanguageChoice(languageChoice)
+
+            val reportFile: String = session.myPromptDefaultFileFolderInput(
+                inputType = InputType.FILE,
+                fileExtensionList = getFileExtensionsForLanguage(language),
+                onInputReady = testCallback()
+            )
+
+            val outputFileName: String = session.myPromptInput(
+                message = "What is the name of the output file?",
+                hint = "coverage.cc.json",
+                allowEmptyInput = true,
+                onInputReady = testCallback()
+            )
+
+            val isCompressed = (outputFileName.isEmpty()) || session.myPromptConfirm(
+                message = "Do you want to compress the output file?",
+                onInputReady = testCallback()
+            )
+
+            val languageParam = "--language=$language"
+            val reportFileParam = "--report-file=$reportFile"
+            val outputFileParam = if (outputFileName.isNotEmpty()) "--outputFile=$outputFileName" else null
+            val notCompressedParam = if (isCompressed) null else "--not-compressed"
+
+            return listOfNotNull(
+                languageParam,
+                reportFileParam,
+                outputFileParam,
+                notCompressedParam
+            )
+        }
+
+        internal fun testCallback(): suspend RunScope.() -> Unit = {}
+    }
+}
