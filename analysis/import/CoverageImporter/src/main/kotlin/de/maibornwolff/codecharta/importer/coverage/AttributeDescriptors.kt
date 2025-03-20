b70662d6c272f87fdcabd84ba652ca35fa6ba0ce
Benedikt Mehl <b.mehl14@outlook.de>
2025-03-20 15:56:45 +0100

    refactor(analysis): use enums (#3985)

--- analysis/import/CoverageImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/coverage/AttributeDescriptors.kt
+++ analysis/import/CoverageImporter/src/main/kotlin/de/maibornwolff/codecharta/importer/coverage/AttributeDescriptors.kt
@@ -4,19 +4,25 @@ import de.maibornwolff.codecharta.model.AttributeDescriptor
 import de.maibornwolff.codecharta.model.AttributeType
 import de.maibornwolff.codecharta.model.AttributeTypes
 
+enum class CoverageAttributes(val attributeName: String) {
+    LINE_COVERAGE("line_coverage"),
+    BRANCH_COVERAGE("branch_coverage"),
+    STATEMENT_COVERAGE("statement_coverage")
+}
+
 fun getAttributeDescriptors(): Map<String, AttributeDescriptor> {
     return mapOf(
-        "line_coverage" to AttributeDescriptor(
+        CoverageAttributes.LINE_COVERAGE.attributeName to AttributeDescriptor(
             title = "Line Coverage",
             description = "Percentage of lines covered by tests",
             direction = 1
         ),
-        "branch_coverage" to AttributeDescriptor(
+        CoverageAttributes.BRANCH_COVERAGE.attributeName to AttributeDescriptor(
             title = "Branch Coverage",
             description = "Percentage of branches covered by tests",
             direction = 1
         ),
-        "statement_coverage" to AttributeDescriptor(
+        CoverageAttributes.STATEMENT_COVERAGE.attributeName to AttributeDescriptor(
             title = "Statement Coverage",
             description = "Percentage of statements covered by tests",
             direction = 1
@@ -26,7 +32,7 @@ fun getAttributeDescriptors(): Map<String, AttributeDescriptor> {
 
 fun getAttributeTypes(): AttributeTypes {
     return AttributeTypes(type = "nodes")
-        .add("line_coverage", AttributeType.RELATIVE)
-        .add("branch_coverage", AttributeType.RELATIVE)
-        .add("statement_coverage", AttributeType.RELATIVE)
+        .add(CoverageAttributes.LINE_COVERAGE.attributeName, AttributeType.RELATIVE)
+        .add(CoverageAttributes.BRANCH_COVERAGE.attributeName, AttributeType.RELATIVE)
+        .add(CoverageAttributes.STATEMENT_COVERAGE.attributeName, AttributeType.RELATIVE)
 }
