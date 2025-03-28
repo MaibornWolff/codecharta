e4869c48c9f4018037a607e5da25a8094f8f8aee
lisa.walter <lisa.walter@maibornwolff.de>
2025-03-28 10:17:18 +0100

    test(analysis): fix unit test (#3990)

--- analysis/analysers/tools/ValidationTool/src/test/kotlin/de/maibornwolff/codecharta/tools/validation/EveritValidatorTest.kt
+++ analysis/analysers/tools/ValidationTool/src/test/kotlin/de/maibornwolff/codecharta/tools/validation/EveritValidatorTest.kt
@@ -29,46 +29,46 @@ class EveritValidatorTest {
 
     @Test
     fun `should extract and validate a valid file`() {
-        validator.validate(this.javaClass.classLoader.getResourceAsStream("validCompressed.gz"))
+        validator.validate(this.javaClass.classLoader.getResourceAsStream("validCompressed.gz")!!)
     }
 
     @Test
     fun `should throw exception if extracted file is invalid json`() {
         assertFailsWith(JSONException::class) {
-            validator.validate(this.javaClass.classLoader.getResourceAsStream("invalidJSONCompressed.gz"))
+            validator.validate(this.javaClass.classLoader.getResourceAsStream("invalidJSONCompressed.gz")!!)
         }
     }
 
     @Test
     fun `should throw exception on compressed json with no project`() {
         assertFailsWith(ValidationException::class) {
-            validator.validate(this.javaClass.classLoader.getResourceAsStream("invalidProjectCompressed.gz"))
+            validator.validate(this.javaClass.classLoader.getResourceAsStream("invalidProjectCompressed.gz")!!)
         }
     }
 
     @Test
     fun `should validate valid File`() {
-        validator.validate(this.javaClass.classLoader.getResourceAsStream("validFile.cc.json"))
+        validator.validate(this.javaClass.classLoader.getResourceAsStream("validFile.cc.json")!!)
     }
 
     @Test
     fun `should throw exception on missing node name`() {
         assertFailsWith(ValidationException::class) {
-            validator.validate(this.javaClass.classLoader.getResourceAsStream("missingNodeNameFile.json"))
+            validator.validate(this.javaClass.classLoader.getResourceAsStream("missingNodeNameFile.json")!!)
         }
     }
 
     @Test
     fun `should throw exception on missing project`() {
         assertFailsWith(ValidationException::class) {
-            validator.validate(this.javaClass.classLoader.getResourceAsStream("invalidFile.json"))
+            validator.validate(this.javaClass.classLoader.getResourceAsStream("invalidFile.json")!!)
         }
     }
 
     @Test
     fun `should throw exception if no json file`() {
         assertFailsWith(JSONException::class) {
-            validator.validate(this.javaClass.classLoader.getResourceAsStream("invalidJson.json"))
+            validator.validate(this.javaClass.classLoader.getResourceAsStream("invalidJson.json")!!)
         }
     }
 
