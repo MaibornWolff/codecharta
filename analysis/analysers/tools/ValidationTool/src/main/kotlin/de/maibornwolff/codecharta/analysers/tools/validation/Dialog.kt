7c21a1c450eed82b289dd0e966576670a2fbfb95
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2025-04-28 10:20:02 +0200

    feat(analysis): wip autocomplete trial (#3923)

--- analysis/analysers/tools/ValidationTool/src/main/kotlin/de/maibornwolff/codecharta/analysers/tools/validation/Dialog.kt
+++ analysis/analysers/tools/ValidationTool/src/main/kotlin/de/maibornwolff/codecharta/analysers/tools/validation/Dialog.kt
@@ -5,20 +5,30 @@ import com.varabyte.kotter.runtime.Session
 import de.maibornwolff.codecharta.analysers.analyserinterface.AnalyserDialogInterface
 import de.maibornwolff.codecharta.dialogProvider.InputType
 import de.maibornwolff.codecharta.dialogProvider.promptDefaultFileFolderInput
+import de.maibornwolff.codecharta.dialogProvider.promptInputComplete
 import de.maibornwolff.codecharta.serialization.FileExtension
 
 class Dialog {
     companion object : AnalyserDialogInterface {
         override fun collectAnalyserArgs(session: Session): List<String> {
             print("Which file do you want to validate?")
-            val inputFileName: String = session.promptDefaultFileFolderInput(
-                inputType = InputType.FILE,
-                fileExtensionList = listOf(
-                    FileExtension.CCJSON,
-                    FileExtension.CCGZ
-                ),
+            val inputFileName: String = session.promptInputComplete(
+                message = "Validate file",
+                hint = "someInput",
+                allowEmptyInput = false,
+                invalidInputMessage = "Input Invalid",
                 onInputReady = testCallback()
             )
+
+
+//                session.promptDefaultFileFolderInput(
+//                inputType = InputType.FILE,
+//                fileExtensionList = listOf(
+//                    FileExtension.CCJSON,
+//                    FileExtension.CCGZ
+//                ),
+//                onInputReady = testCallback()
+//            )
 
             return listOf(inputFileName)
         }
--- analysis/dialogProvider/src/main/kotlin/de/maibornwolff/codecharta/dialogProvider/DialogDrawing.kt
+++ analysis/dialogProvider/src/main/kotlin/de/maibornwolff/codecharta/dialogProvider/DialogDrawing.kt
@@ -2,6 +2,7 @@ package de.maibornwolff.codecharta.dialogProvider
 
 import com.varabyte.kotter.foundation.collections.LiveList
 import com.varabyte.kotter.foundation.input.Completions
+import com.varabyte.kotter.foundation.input.InputCompleter
 import com.varabyte.kotter.foundation.input.input
 import com.varabyte.kotter.foundation.text.black
 import com.varabyte.kotter.foundation.text.bold
@@ -11,6 +12,11 @@ import com.varabyte.kotter.foundation.text.red
 import com.varabyte.kotter.foundation.text.text
 import com.varabyte.kotter.foundation.text.textLine
 import com.varabyte.kotter.runtime.MainRenderScope
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.isDirectory
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.listDirectoryEntries
 
 fun MainRenderScope.drawInput(
     message: String,
@@ -18,7 +24,8 @@ fun MainRenderScope.drawInput(
     isInputValid: Boolean,
     allowEmptyInput: Boolean,
     invalidInputMessage: String,
-    lastInputEmpty: Boolean
+    lastInputEmpty: Boolean,
+    inputCompleter: InputCompleter
 ) {
     bold {
         green { text("? ") }
@@ -30,8 +37,94 @@ fun MainRenderScope.drawInput(
         }
     }
     text("> ")
-    input(Completions(hint), initialText = "")
+    input(inputCompleter, initialText = "")
 }
+
+fun MainRenderScope.drawInputWithInfo(
+    message: String,
+    hint: String,
+    isInputValid: Boolean,
+    allowEmptyInput: Boolean,
+    invalidInputMessage: String,
+    lastInputEmpty: Boolean,
+    lastInput: String,
+    allowedExtension: String = ".cc.json"
+) {
+    var inputCompleter = Completions("")
+    bold {
+        green { text("? ") }
+        text(message)
+        if (isInputValid) {
+            black(isBright = true) { textLine(if (allowEmptyInput) "  Empty input is allowed" else "") }
+        } else {
+            red { textLine(if (lastInputEmpty) "  Empty input is not allowed!" else "  $invalidInputMessage") }
+        }
+    }
+
+
+    // Get input
+    // Check if input is directory or file with allowed extension
+    // if dir -> listDirectoryEntries
+    // else use last valid result and filter by
+
+    val currentPosition = try {
+        Paths.get(lastInput)
+    } catch (e: Exception) {
+        Paths.get(lastInput.substringBeforeLast("/"))
+    }
+    textLine(currentPosition.toString())
+    if(currentPosition.isDirectory()) {
+        lastGood = currentPosition.listDirectoryEntries()
+        inputCompleter = Completions(*lastGood.map { it.toString() }.toTypedArray())
+    }
+    val floatingInput = lastInput.substringAfterLast("/")
+    val possibleMatches = lastGood.filter { it.startsWith(floatingInput) && ( it.isDirectory() || ( it.isRegularFile()) && it.endsWith(allowedExtension) ) }
+//    val filter = if (lastInput.isEmpty() ) "*" else lastInput + "*"
+//    val currentFolders = cwd.listDirectoryEntries(filter).filter { it.isDirectory() }
+//    if (currentFolders.size == 1) {
+//        inputCompleter = Completions(currentFolders.first().toString()+ "/")
+//    }
+
+
+    val someResults = getSomeEntries(possibleMatches)
+
+    text("> ")
+    input(inputCompleter, initialText = "")
+    textLine("")
+
+    black(isBright = true) {
+        textLine(someResults)
+    }
+
+}
+
+fun getSomeEntries(paths: List<Path>, charLimit : Int = 120): String {
+    var usedChars = 0
+    var secondLine = false
+    var availableFolders = ""
+    paths.forEach {
+        val pathLength = it.toString().length
+        if (usedChars + pathLength > charLimit) {
+            if (secondLine) {
+                return availableFolders
+            } else {
+                secondLine = true
+                if (pathLength > charLimit) {
+                    return availableFolders
+                } else {
+                    availableFolders += "\n" + "${it}/ "
+                    usedChars = pathLength
+                }
+            }
+        } else {
+            availableFolders += "${it}/ "
+            usedChars += pathLength
+        }
+    }
+    return availableFolders
+}
+
+
 
 fun MainRenderScope.drawConfirm(message: String, hint: String, choice: Boolean) {
     bold {
