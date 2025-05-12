df0247897b4fb86cc302ecb3c93c2af6d2d807d5
nereboss <niklas.kneissl@gmail.com>
2025-05-12 12:01:27 +0200

    fix(visualisation): change back object to string[] in case browser doesnt (#4060)

--- visualization/app/codeCharta/state/effects/nodeContextMenu/markFolderRow/selectors/markFolderItems.selector.ts
+++ visualization/app/codeCharta/state/effects/nodeContextMenu/markFolderRow/selectors/markFolderItems.selector.ts
@@ -13,6 +13,8 @@ export type MarkFolderItem = {
 const markingColorsSelector = createSelector(mapColorsSelector, mapColors => mapColors.markingColors)
 
 export const _getMarkFolderItems = (node: { path?: string } | null, markingColors: string[], markedPackages: MarkedPackage[]) => {
+    // some browsers (e.g. arc v. 1.94.0) set markingColors as an object (not string[]) due to a bug, so we need to manually adjust this
+    if (!Array.isArray(markingColors)) markingColors = Object.values(markingColors)
     if (node === null) {
         return markingColors.map(color => ({ color, isMarked: false }))
     }
