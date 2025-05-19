e2471d1ce4558d1f9693ab627bd1dacf0b5fadc6
nereboss <niklas.kneissl@gmail.com>
2025-05-19 09:42:11 +0200

    refactor(analysis): WIP commit, changing architecture of unifiedParser (#4048)

--- analysis/analysers/parsers/UnifiedParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/UnifiedParser.kt
+++ analysis/analysers/parsers/UnifiedParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/UnifiedParser.kt
@@ -3,15 +3,20 @@ package de.maibornwolff.codecharta.analysers.parsers.unified
 import de.maibornwolff.codecharta.analysers.analyserinterface.AnalyserDialogInterface
 import de.maibornwolff.codecharta.analysers.analyserinterface.AnalyserInterface
 import de.maibornwolff.codecharta.analysers.analyserinterface.util.CodeChartaConstants
-import de.maibornwolff.codecharta.analysers.parsers.unified.metriccollectors.TypescriptCollector
-import de.maibornwolff.codecharta.analysers.parsers.unified.metricqueries.typescript.TypescriptQueries
+import de.maibornwolff.codecharta.analysers.filters.mergefilter.MergeFilter
 import de.maibornwolff.codecharta.model.AttributeDescriptor
 import de.maibornwolff.codecharta.model.AttributeGenerator
-import java.io.InputStream
-import java.io.PrintStream
+import de.maibornwolff.codecharta.model.ProjectBuilder
+import de.maibornwolff.codecharta.serialization.ProjectDeserializer
+import de.maibornwolff.codecharta.serialization.ProjectSerializer
+import de.maibornwolff.codecharta.util.CommaSeparatedParameterPreprocessor
+import de.maibornwolff.codecharta.util.CommaSeparatedStringToListConverter
+import de.maibornwolff.codecharta.util.FileExtensionConverter
+import de.maibornwolff.codecharta.util.InputHelper
 import picocli.CommandLine
 import java.io.File
-import org.treesitter.*
+import java.io.InputStream
+import java.io.PrintStream
 
 @CommandLine.Command(
     name = UnifiedParser.NAME,
@@ -32,15 +37,38 @@ class UnifiedParser(
     @CommandLine.Option(names = ["-o", "--output-file"], description = ["output File (or empty for stdout)"])
     private var outputFile: String? = null
 
-
     @CommandLine.Option(names = ["--verbose"], description = ["verbose mode"])
     private var verbose = false
 
     @CommandLine.Option(names = ["-nc", "--not-compressed"], description = ["save uncompressed output File"])
     private var compress = true
 
-    // TODO: add more options when the skeleton walks
+    @CommandLine.Option(
+        names = ["-e", "--exclude"],
+        description = [
+            "comma-separated list of regex patterns to exclude files/folders " +
+                "(when using powershell, the list either can't contain spaces or has to be in quotes)"
+        ],
+        converter = [(CommaSeparatedStringToListConverter::class)],
+        preprocessor = CommaSeparatedParameterPreprocessor::class
+    )
+    private var exclude: List<String> = listOf()
 
+    @CommandLine.Option(
+        names = ["--without-default-excludes"],
+        description = ["include build, target, dist, resources and out folders as well as files/folders starting with '.' "]
+    )
+    private var withoutDefaultExcludes = false
+
+    @CommandLine.Option(
+        names = ["-fe", "--file-extensions"],
+        description = ["comma-separated list of file-extensions to parse only those files (default: any)"],
+        converter = [(FileExtensionConverter::class)],
+        preprocessor = CommaSeparatedParameterPreprocessor::class
+    )
+    private var fileExtensions: List<String> = listOf()
+
+    // TODO: add more options when the skeleton walks (e.g. metrics, exclude, file-extensions, without-default-excludes; all taken from rawTextParser)
 
     override val name = NAME
     override val description = DESCRIPTION
@@ -52,27 +80,36 @@ class UnifiedParser(
         private val DEFAULT_EXCLUDES = arrayOf("/out/", "/build/", "/target/", "/dist/", "/resources/", "/\\..*")
     }
 
-
     override fun call(): Unit? {
-        //TODO: this function should handle going through the files and call the correct parsers for each file type
+        logExecutionStartedSyncSignal()
 
-        println("calling UnifiedParser...")
+        require(InputHelper.isInputValidAndNotNull(arrayOf(inputFile), canInputContainFolders = true)) {
+            "Input invalid file for UnifiedParser, stopping execution..."
+        }
 
-        val fileContent = inputFile!!.readText()
-        val nodesTree = parseCode(fileContent)
+        if (!withoutDefaultExcludes) exclude += DEFAULT_EXCLUDES
 
-        val complexity = TypescriptCollector.getMetricFromQuery(nodesTree, TypescriptQueries.complexityQuery)
+        val filesList = inputFile!!.walk().filter { it.isFile }.toList()
+        filesList.forEach { file -> println(file.toString()) }
 
-        println("---------$complexity------------")
+        val projectBuilder = ProjectBuilder()
+
+        // TODO: call functions here that walk through the input and fill the cc.json
+        //parseInputProject(inputFile!!, projectBuilder)
+        val projectScanner = ProjectScanner(inputFile!!, projectBuilder, exclude)
+        projectScanner.traverseInputProject()
+        projectBuilder.addAttributeDescriptions(getAttributeDescriptors())
+
+        var project = projectBuilder.build()
+
+        val pipedProject = ProjectDeserializer.deserializeProject(input)
+        if (pipedProject != null) {
+            project = MergeFilter.mergePipedWithCurrentProject(pipedProject, project)
+        }
+
+        ProjectSerializer.serializeToFileOrStream(project, outputFile, output, compress)
 
         return null
-    }
-
-    private fun parseCode(typescriptCode: String): TSNode {
-        val parser = TSParser()
-        parser.language = TreeSitterTypescript()
-        val tree = parser.parseString(null, typescriptCode)
-        return tree.rootNode
     }
 
     override fun getDialog(): AnalyserDialogInterface = Dialog
@@ -84,5 +121,4 @@ class UnifiedParser(
     override fun getAttributeDescriptorMaps(): Map<String, AttributeDescriptor> {
         TODO("Not yet implemented")
     }
-
 }
--- analysis/analysers/parsers/UnifiedParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/metriccollectors/TypescriptCollector.kt
+++ analysis/analysers/parsers/UnifiedParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/metriccollectors/TypescriptCollector.kt
@@ -1,19 +1,28 @@
 package de.maibornwolff.codecharta.analysers.parsers.unified.metriccollectors
 
+import de.maibornwolff.codecharta.analysers.parsers.unified.metricqueries.typescript.TypescriptQueries
 import org.treesitter.TSNode
+import org.treesitter.TSParser
 import org.treesitter.TSQuery
 import org.treesitter.TSQueryCursor
 import org.treesitter.TreeSitterTypescript
 
-class TypescriptCollector {
-    companion object {
-        fun getMetricFromQuery(node: TSNode, query: String): Int {
+class TypescriptCollector : MetricCollector<TSNode>(TypescriptQueries()) {
+
+    override fun createTreeNodes(code: String): TSNode {
+        val parser = TSParser()
+        parser.language = TreeSitterTypescript()
+        val tree = parser.parseString(null, code)
+        return tree.rootNode
+    }
+
+    //TODO: maybe function should be in metricCollector with wildcard type for node
+    override fun getMetricFromQuery(node: TSNode, query: String): Int {
             val tsQuery = TSQuery(TreeSitterTypescript(), query)
             val cursor = TSQueryCursor()
             cursor.exec(tsQuery, node)
             var metricHits = 0
             for (hit in cursor.matches) metricHits++
             return metricHits
-        }
     }
 }
