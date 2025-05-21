177c9547e59a3bfb6bb2a951d8fafb74ff43e4d0
nereboss <niklas.kneissl@gmail.com>
2025-05-21 11:45:56 +0200

    feat(analysis): add new flags to dialog of unified parser #4048

--- analysis/analysers/parsers/UnifiedParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/Dialog.kt
+++ analysis/analysers/parsers/UnifiedParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/Dialog.kt
@@ -7,6 +7,7 @@ import de.maibornwolff.codecharta.dialogProvider.InputType
 import de.maibornwolff.codecharta.dialogProvider.promptConfirm
 import de.maibornwolff.codecharta.dialogProvider.promptDefaultFileFolderInput
 import de.maibornwolff.codecharta.dialogProvider.promptInput
+import de.maibornwolff.codecharta.dialogProvider.promptList
 
 class Dialog {
     companion object : AnalyserDialogInterface {
@@ -30,6 +31,45 @@ class Dialog {
                         onInputReady = testCallback()
                     )
 
+            val askExcludeInclude = session.promptList(
+                message = "Do you want to exclude files/folders based on regex patterns or limit the analysis to specific file extensions?",
+                choices = listOf(
+                    "Only exclude",
+                    "Only include",
+                    "Both",
+                    "None"
+                ),
+                onInputReady = testCallback()
+            )
+
+            val exclude: String = if (askExcludeInclude == "Only exclude" || askExcludeInclude == "Both") {
+                session.promptInput(
+                    message = "Which regex patterns do you want to use to exclude files/folders from the analysis",
+                    hint = "regex1, regex2.. (leave empty if you don't want to exclude anything)",
+                    allowEmptyInput = true,
+                    onInputReady = testCallback()
+                )
+            } else {
+                ""
+            }
+
+            val fileExtensions: String = if (askExcludeInclude == "Only include" || askExcludeInclude == "Both") {
+                session.promptInput(
+                    message = "Which file extensions do you want to include in the analysis?",
+                    hint = "fileType1, fileType2... (leave empty to include all file-extensions)",
+                    allowEmptyInput = true,
+                    onInputReady = testCallback()
+                )
+            } else {
+                ""
+            }
+
+            val withoutDefaultExcludes: Boolean = session.promptConfirm(
+                message = "Do you want to include build, target, dist, resources" +
+                    " and out folders as well as files/folders starting with '.'?",
+                onInputReady = testCallback()
+            )
+
             val verbose: Boolean = session.promptConfirm(
                 message = "Do you want to suppress command line output?",
                 onInputReady = testCallback()
@@ -39,7 +79,10 @@ class Dialog {
                 inputFileName,
                 "--output-file=$outputFileName",
                 if (isCompressed) null else "--not-compressed",
-                "--verbose=$verbose",
+                "--verbose=${!verbose}",
+                "--exclude=$exclude",
+                "--file-extensions=$fileExtensions",
+                if (withoutDefaultExcludes) "--without-default-excludes" else null,
             )
         }
 
