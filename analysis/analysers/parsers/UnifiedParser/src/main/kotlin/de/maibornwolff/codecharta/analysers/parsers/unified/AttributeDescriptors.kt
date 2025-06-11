f3191da7f3eb25a61bf13e81f263d09e81a9b96e
nereboss <niklas.kneissl@gmail.com>
2025-06-11 13:39:11 +0200

    chore(analysis): improve naming of attributeDescriptors #4048

--- analysis/analysers/parsers/UnifiedParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/AttributeDescriptors.kt
+++ analysis/analysers/parsers/UnifiedParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/AttributeDescriptors.kt
@@ -7,7 +7,13 @@ internal fun getAttributeDescriptors(): Map<String, AttributeDescriptor> {
     return mapOf(
         "complexity" to AttributeDescriptor(
             title = "Complexity",
-            description = "TODO", // TODO: which complexity is it exactly?
+            description = "Complexity of the file based on the number of paths through the code (McCabe Complexity)",
+            link = "https://en.wikipedia.org/wiki/Cyclomatic_complexity",
+            direction = -1
+        ),
+        "comment_lines" to AttributeDescriptor(
+            title = "Comment lines",
+            description = "Number of lines containing either a comment or commented-out code",
             link = ghLink,
             direction = -1
         )
