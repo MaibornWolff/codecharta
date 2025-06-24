3271834cf1941888f3ec192ba0ca00a906c9c43c
nereboss <niklas.kneissl@gmail.com>
2025-06-24 10:59:46 +0200

    chore(analysis):apply ktlint #4087

--- analysis/analysers/parsers/UnifiedParser/src/test/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/UnifiedParserTest.kt
+++ analysis/analysers/parsers/UnifiedParser/src/test/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/UnifiedParserTest.kt
@@ -47,7 +47,7 @@ class UnifiedParserTest {
     fun `Should produce correct output for a single source file of each supported language`(language: String, fileExtension: String) {
         // given
         val pipedProject = ""
-        val inputFilePath = "${testResourceBaseFolder}${language}Sample${fileExtension}"
+        val inputFilePath = "${testResourceBaseFolder}${language}Sample$fileExtension"
         val expectedResultFile = File("${testResourceBaseFolder}${language}Sample.cc.json")
 
         // when
