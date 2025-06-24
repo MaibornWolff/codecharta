702932ab0f0611f95a9f5bc7741ccc41ad2b238c
Sebastian Wolf <65733509+phanlezz@users.noreply.github.com>
2025-06-24 13:23:22 +0200

    feat(analysis): add tests to the navigator wip (#3923)

--- analysis/dialogProvider/src/test/kotlin/de/maibornwolff/codecharta/dialogProvider/DirectoryNavigatorTest.kt
+++ analysis/dialogProvider/src/test/kotlin/de/maibornwolff/codecharta/dialogProvider/DirectoryNavigatorTest.kt
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test
 import java.io.File
 
 class DirectoryNavigatorTest {
+    private val slash = File.separatorChar
+
     @Test
     fun `this should provide a good change directory experience`() {
         val uut = DirectoryNavigator(InputType.FOLDER_AND_FILE, listOf(FileExtension.CCJSON), false)
@@ -13,4 +15,47 @@ class DirectoryNavigatorTest {
         uut.prepareMatches("")
         assertThat(uut.getHints()).isEqualTo(arrayOf("build$systemSlash", "src$systemSlash"))
     }
+
+    @Test
+    fun `should set input types correctly`() {
+        val navigatorFiles = DirectoryNavigator(InputType.FILE, listOf(), false)
+        val navigatorFolderAndFiles = DirectoryNavigator(InputType.FOLDER_AND_FILE, listOf(), false)
+        val navigatorFolders = DirectoryNavigator(InputType.FOLDER, listOf(), false)
+
+        assertThat(navigatorFiles.filesAllowed).isTrue()
+        assertThat(navigatorFolderAndFiles.filesAllowed).isTrue()
+        assertThat(navigatorFolders.filesAllowed).isFalse()
+    }
+
+    @Test
+    fun `should filter possible file options correctly`() {
+        val navigator = DirectoryNavigator(InputType.FILE, listOf(), false)
+        assertThat(navigator.currentDirectoryContent.size).isEqualTo(3)
+        assertThat(navigator.possibleDirectories.size).isEqualTo(2)
+        assertThat(navigator.possibleFiles.size).isEqualTo(1)
+    }
+
+    @Test
+    fun `should filter unaccepted file options`() {
+        val navigator = DirectoryNavigator(InputType.FOLDER, listOf(), false)
+        assertThat(navigator.currentDirectoryContent.size).isEqualTo(3)
+        assertThat(navigator.possibleDirectories.size).isEqualTo(2)
+        assertThat(navigator.possibleFiles.size).isEqualTo(0)
+    }
+
+    @Test
+    fun `should perform an update cycle as expected `() {
+        val resourcePath = "src/test/resources/"
+        val matchingFile = "${resourcePath}validExtension.cc.json"
+        val navigator = DirectoryNavigator(InputType.FOLDER_AND_FILE, listOf(FileExtension.CCJSON), false)
+
+        navigator.prepareMatches(File(resourcePath).toString()+slash)
+        val hints = navigator.getHints()
+        val matchesString = navigator.getMatches()
+
+        assertThat(hints.size).isEqualTo(1)
+        assertThat(hints).containsExactly(File(matchingFile).toString())
+        assertThat(matchesString).isEqualTo("validExtension.cc.json")
+    }
+
 }
