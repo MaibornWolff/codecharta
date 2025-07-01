d717aee0fc66e823809d1a304f9b34741ee5aa6b
nereboss <niklas.kneissl@gmail.com>
2025-07-01 16:40:11 +0200

    test(analysis): add tests for each language collector #4108

---
+++ analysis/analysers/parsers/UnifiedParser/src/test/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/metriccollectors/JavaCollectorTest.kt
@@ -0,0 +1,123 @@
+package de.maibornwolff.codecharta.analysers.parsers.unified.metriccollectors
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.treesitter.TSParser
+import org.treesitter.TreeSitterJava
+
+class JavaCollectorTest {
+    private var parser = TSParser()
+    private val collector = JavaCollector()
+
+    @BeforeEach
+    fun setUp() {
+        parser.setLanguage(TreeSitterJava())
+    }
+
+    @Test
+    fun `should count lambda expressions for complexity`() {
+        // given
+        val input = """int sum = (int x, int y) -> x + y;"""
+        val node = parser.parseString(null, input).rootNode
+
+        // when
+        val complexity = collector.getComplexity(node)
+
+        // then
+        Assertions.assertThat(complexity).isEqualTo(1)
+    }
+
+    @Test
+    fun `should count ternary operator for complexity`() {
+        // given
+        val input = """x = (y>0) ? 1 : -1;"""
+        val node = parser.parseString(null, input).rootNode
+
+        // when
+        val complexity = collector.getComplexity(node)
+
+        // then
+        Assertions.assertThat(complexity).isEqualTo(1)
+    }
+
+    @Test
+    fun `should count enhanced for loop for complexity`() {
+        // given
+        val input = """
+            for (char item: vowels) {
+                System.out.println(item);
+            }
+        """.trimIndent()
+        val node = parser.parseString(null, input).rootNode
+
+        // when
+        val complexity = collector.getComplexity(node)
+
+        // then
+        Assertions.assertThat(complexity).isEqualTo(1)
+    }
+
+    @Test
+    fun `should count line and block comments for comment_lines`() {
+        // given
+        val input = """
+            /**
+             * docstring comment
+             * over
+             * multiple lines
+             */
+            public void helloWorld() {
+                // line comment
+                System.out.println("Hello"); /* comment in code */ System.out.println("world");
+            }
+        """.trimIndent()
+        val node = parser.parseString(null, input).rootNode
+
+        // when
+        val commentLines = collector.getCommentLines(node)
+
+        // then
+        Assertions.assertThat(commentLines).isEqualTo(7)
+    }
+
+    @Test
+    fun `should not include comments or empty lines for rloc`() {
+        // given
+        val input = """
+            if (x == 2) {
+                // comment
+
+
+                return true;
+            }
+        """.trimIndent()
+        val node = parser.parseString(null, input).rootNode
+
+        // when
+        val rloc = collector.getRealLinesOfCode(node)
+
+        // then
+        Assertions.assertThat(rloc).isEqualTo(3)
+    }
+
+    @Test
+    fun `should count empty lines and comments for loc`() {
+        // given
+        val input = """
+            if (x == 2) {
+                // comment
+
+
+                return true;
+            }
+        """.trimIndent() + "\n" //this newline simulates end of file
+        val node = parser.parseString(null, input).rootNode
+
+        // when
+        val loc = collector.getLinesOfCode(node)
+
+        // then
+        Assertions.assertThat(loc).isEqualTo(6)
+    }
+}
