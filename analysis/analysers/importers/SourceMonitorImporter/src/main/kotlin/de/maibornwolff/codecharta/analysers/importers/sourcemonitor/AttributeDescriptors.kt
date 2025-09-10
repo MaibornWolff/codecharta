e219136a240db311e6cd44d7058e7bf852992dc1
nereboss <niklas.kneissl@gmail.com>
2025-09-10 15:13:54 +0200

    feat: update attributeDescriptors for remaining analyzers #4148

--- analysis/analysers/importers/SourceMonitorImporter/src/main/kotlin/de/maibornwolff/codecharta/analysers/importers/sourcemonitor/AttributeDescriptors.kt
+++ analysis/analysers/importers/SourceMonitorImporter/src/main/kotlin/de/maibornwolff/codecharta/analysers/importers/sourcemonitor/AttributeDescriptors.kt
@@ -4,78 +4,114 @@ import de.maibornwolff.codecharta.model.AttributeDescriptor
 
 internal fun getAttributeDescriptors(): Map<String, AttributeDescriptor> {
     val ghLink = "https://github.com/SourceMonitor/SM-Info"
+    val analyzerName = setOf("sourceMonitorImporter")
     val descriptors: MutableMap<String, AttributeDescriptor> = mutableMapOf()
     descriptors["loc"] =
         AttributeDescriptor(
             title = "Lines of Code",
-            description = "Lines of code including empty lines and comments", link = ghLink, direction = -1
+            description = "Lines of code including empty lines and comments",
+            link = ghLink,
+            direction = -1,
+            analyzers = analyzerName
         )
     descriptors["rloc"] =
         AttributeDescriptor(
             title = "Real Lines of Code",
             description = "Number of code lines that contain" +
                 " at least one character which is neither a whitespace nor a tabulation nor part of a comment",
-            link = ghLink, direction = -1
+            link = ghLink,
+            direction = -1,
+            analyzers = analyzerName
         )
     descriptors["classes"] =
         AttributeDescriptor(
-            title = "Number of Classes", description = "Number of classes", link = ghLink,
-            direction = -1
+            title = "Number of Classes",
+            description = "Number of classes",
+            link = ghLink,
+            direction = -1,
+            analyzers = analyzerName
         )
     descriptors["functions_per_class"] =
         AttributeDescriptor(
-            title = "Functions per Class", description = "Number of functions per class",
-            link = ghLink, direction = -1
+            title = "Functions per Class",
+            description = "Number of functions per class",
+            link = ghLink,
+            direction = -1,
+            analyzers = analyzerName
         )
     descriptors["average_statements_per_function"] =
         AttributeDescriptor(
             title = "Average Statements per Function",
-            description = "Average number of statements per function", link = ghLink, direction = -1
+            description = "Average number of statements per function",
+            link = ghLink,
+            direction = -1,
+            analyzers = analyzerName
         )
     descriptors["max_function_complexity"] =
         AttributeDescriptor(
             title = "Maximum Function Complexity",
             description = "Maximum cyclomatic complexity based on the number of paths through a function",
             link = ghLink,
-            direction = -1
+            direction = -1,
+            analyzers = analyzerName
         )
     descriptors["max_block_depth"] =
         AttributeDescriptor(
-            title = "Maximum Block Depth", description = "Maximum nested block depth found",
-            link = ghLink, direction = -1
+            title = "Maximum Block Depth",
+            description = "Maximum nested block depth found",
+            link = ghLink,
+            direction = -1,
+            analyzers = analyzerName
         )
     descriptors["average_block_depth"] =
         AttributeDescriptor(
-            title = "Average Block Depth", description = "Average nested block depth found",
-            link = ghLink, direction = -1
+            title = "Average Block Depth",
+            description = "Average nested block depth found",
+            link = ghLink,
+            direction = -1,
+            analyzers = analyzerName
         )
     descriptors["average_function_complexity"] =
         AttributeDescriptor(
             title = "Average Function Complexity",
             description = "Average cyclomatic complexity based on the number of paths through a function",
-            link = ghLink, direction = -1
+            link = ghLink,
+            direction = -1,
+            analyzers = analyzerName
         )
     descriptors["sm_percent_branch_statements"] =
         AttributeDescriptor(
             title = "Percentage of Branch Statements",
-            description = "Percentage of branch statements", link = ghLink, direction = -1
+            description = "Percentage of branch statements",
+            link = ghLink,
+            direction = -1,
+            analyzers = analyzerName
         )
     descriptors["sm_method_call_statements"] =
         AttributeDescriptor(
-            title = "Method Calls", description = "Number of method call statements", link = ghLink,
-            direction = -1
+            title = "Method Calls",
+            description = "Number of method call statements",
+            link = ghLink,
+            direction = -1,
+            analyzers = analyzerName
         )
     descriptors["sm_percent_lines_with_comments"] =
         AttributeDescriptor(
             title = "Percentage of Comment Lines",
-            description = "Percentage of code lines that contain comments", link = ghLink, direction = -1
+            description = "Percentage of code lines that contain comments",
+            link = ghLink,
+            direction = -1,
+            analyzers = analyzerName
         )
 
     for (i in 0..9) {
         descriptors["statements_at_level_$i"] =
             AttributeDescriptor(
                 title = "Statements at Block Level $i",
-                description = "Statements at block level $i", link = ghLink, direction = -1
+                description = "Statements at block level $i",
+                link = ghLink,
+                direction = -1,
+                analyzers = analyzerName
             )
     }
 
--- analysis/analysers/parsers/GitLogParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/gitlog/AttributeDescriptors.kt
+++ analysis/analysers/parsers/GitLogParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/gitlog/AttributeDescriptors.kt
@@ -4,61 +4,87 @@ import de.maibornwolff.codecharta.model.AttributeDescriptor
 
 internal fun getAttributeDescriptors(): Map<String, AttributeDescriptor> {
     val ghLink = "https://codecharta.com/docs/parser/git-log"
+    val analyzerName = setOf("gitLogParser")
     return mapOf(
         "age_in_weeks" to
             AttributeDescriptor(
-                title = "Age in Weeks", description = "File age in weeks",
-                link = ghLink, direction = -1
+                title = "Age in Weeks",
+                description = "File age in weeks",
+                link = ghLink,
+                direction = -1,
+                analyzers = analyzerName
             ),
         "number_of_authors" to
             AttributeDescriptor(
-                title = "Number of Authors", description = "Number of authors",
-                link = ghLink, direction = -1
+                title = "Number of Authors",
+                description = "Number of authors",
+                link = ghLink,
+                direction = -1,
+                analyzers = analyzerName
             ),
         "number_of_commits" to
             AttributeDescriptor(
-                title = "Number of Commits", description = "Number of commits",
-                link = ghLink, direction = -1
+                title = "Number of Commits",
+                description = "Number of commits",
+                link = ghLink,
+                direction = -1,
+                analyzers = analyzerName
             ),
         "number_of_renames" to
             AttributeDescriptor(
                 title = "Number of Renames",
-                description = "Number of file renames", link = ghLink, direction = -1
+                description = "Number of file renames",
+                link = ghLink,
+                direction = -1,
+                analyzers = analyzerName
             ),
         "range_of_weeks_with_commits" to
             AttributeDescriptor(
                 title = "Week Range of Commits",
-                description = "Average number of weeks between commits", link = ghLink, direction = -1
+                description = "Average number of weeks between commits",
+                link = ghLink,
+                direction = -1,
+                analyzers = analyzerName
             ),
         "successive_weeks_with_commits" to
             AttributeDescriptor(
                 title = "Successive Weeks with Commits",
                 description = "Number of successive weeks in which the file was included in a commit",
-                link = ghLink, direction = -1
+                link = ghLink,
+                direction = -1,
+                analyzers = analyzerName
             ),
         "weeks_with_commits" to
             AttributeDescriptor(
                 title = "Weeks with Commits",
-                description = "Number of weeks in which commits occurred", link = ghLink, direction = -1
+                description = "Number of weeks in which commits occurred",
+                link = ghLink,
+                direction = -1,
+                analyzers = analyzerName
             ),
         "highly_coupled_files" to
             AttributeDescriptor(
                 title = "Highly Coupled Files",
                 description = "Number of highly coupled files (>=35% of times modified the same time) with this file",
-                link = ghLink, direction = -1
+                link = ghLink,
+                direction = -1,
+                analyzers = analyzerName
             ),
         "median_coupled_files" to
             AttributeDescriptor(
                 title = "Median Coupled Files",
                 description = "Median of number of other files that where committed with this file",
                 link = ghLink,
-                direction = -1
+                direction = -1,
+                analyzers = analyzerName
             ),
         "temporal_coupling" to
             AttributeDescriptor(
                 title = "Temporal Coupling",
-                description = "The degree of temporal coupling between two files (>=35%)", link = ghLink,
-                direction = -1
+                description = "The degree of temporal coupling between two files (>=35%)",
+                link = ghLink,
+                direction = -1,
+                analyzers = analyzerName
             )
     )
 }
