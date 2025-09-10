8198e9b0deed5fb06cb666e76c0cd322020ea74a
nereboss <niklas.kneissl@gmail.com>
2025-09-10 15:13:54 +0200

    feat: update attributeDescriptors for remaining analyzers #4148

--- analysis/analysers/parsers/UnifiedParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/AttributeDescriptors.kt
+++ analysis/analysers/parsers/UnifiedParser/src/main/kotlin/de/maibornwolff/codecharta/analysers/parsers/unified/AttributeDescriptors.kt
@@ -4,27 +4,27 @@ import de.maibornwolff.codecharta.model.AttributeDescriptor
 
 internal fun getAttributeDescriptors(): Map<String, AttributeDescriptor> {
     val ghLink = "https://codecharta.com/docs/parser/unified"
-    val parserName = setOf("unifiedParser")
+    val analyzerName = setOf("unifiedParser")
     return mapOf(
         "complexity" to AttributeDescriptor(
             title = "Complexity",
             description = "Complexity of the file based on the number of paths through the code (McCabe Complexity)",
             link = "https://en.wikipedia.org/wiki/Cyclomatic_complexity",
             direction = -1,
-            analyzers = parserName
+            analyzers = analyzerName
         ),
         "comment_lines" to AttributeDescriptor(
             title = "Comment lines",
             description = "Number of lines containing either a comment or commented-out code",
             link = ghLink,
             direction = -1,
-            analyzers = parserName
+            analyzers = analyzerName
         ),
         "loc" to AttributeDescriptor(
             title = "Lines of Code",
             description = "Lines of code including empty lines and comments", link = ghLink,
             direction = -1,
-            analyzers = parserName
+            analyzers = analyzerName
         ),
         "rloc" to AttributeDescriptor(
             title = "Real Lines of Code",
@@ -32,7 +32,7 @@ internal fun getAttributeDescriptors(): Map<String, AttributeDescriptor> {
                 "which is neither a whitespace nor a tabulation nor part of a comment",
             link = ghLink,
             direction = -1,
-            analyzers = parserName
+            analyzers = analyzerName
         ),
         "number_of_functions" to AttributeDescriptor(
             title = "Number of functions",
@@ -40,7 +40,7 @@ internal fun getAttributeDescriptors(): Map<String, AttributeDescriptor> {
                 "Does not include anonymous or lambda functions.",
             link = ghLink,
             direction = -1,
-            analyzers = parserName
+            analyzers = analyzerName
         )
     )
 }
