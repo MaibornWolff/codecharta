631caa7d5053d7e51cc534be460d6a2f4e100098
Christian <christian.huehn@outlook.com>
2025-10-12 14:28:47 +0200

    ci: fail pipelines when there are warnings or errors

--- visualization/conf/setupJestUnit.ts
+++ visualization/conf/setupJestUnit.ts
@@ -7,4 +7,14 @@ Number.prototype.toLocaleString = function (locale = "en-US") {
     return numberToLocaleString.call(this, locale)
 }
 
+const CONSOLE_FAIL_TYPES = ["warn", "error"]
+
+// Throw errors when a `console.error` or `console.warn` happens
+// by overriding the functions
+CONSOLE_FAIL_TYPES.forEach(type => {
+    console[type] = message => {
+        throw new Error(`Failing due to console.${type} while running test!\n\n${message}`)
+    }
+})
+
 window["__TEST_ENVIRONMENT__"] = true
