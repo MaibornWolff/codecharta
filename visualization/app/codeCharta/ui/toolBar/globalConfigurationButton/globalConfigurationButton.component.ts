35f616b87fca6aa2ed2d2a6e9b09f49e3d5eef7c
Christian Hühn <christian.huehn@maibornwolff.de>
2025-11-06 17:13:28 +0100

    refactor: remove unused matbutton import

--- visualization/app/codeCharta/ui/toolBar/globalConfigurationButton/globalConfigurationButton.component.ts
+++ visualization/app/codeCharta/ui/toolBar/globalConfigurationButton/globalConfigurationButton.component.ts
@@ -1,13 +1,12 @@
 import { Component, viewChild } from "@angular/core"
 import { GlobalConfigurationDialogComponent } from "./globalConfigurationDialog/globalConfigurationDialog.component"
 import { ActionIconComponent } from "../../actionIcon/actionIcon.component"
-import { MatButton } from "@angular/material/button"
 
 @Component({
     selector: "cc-global-configuration-button",
     templateUrl: "./globalConfigurationButton.component.html",
     styleUrl: "./globalConfigurationButton.component.scss",
-    imports: [GlobalConfigurationDialogComponent, MatButton, ActionIconComponent]
+    imports: [GlobalConfigurationDialogComponent, ActionIconComponent]
 })
 export class GlobalConfigurationButtonComponent {
     dialog = viewChild.required<GlobalConfigurationDialogComponent>("configDialog")
