a1835d5bb941c03c567c70c1800ba83cfad2ce69
lina.medina <lina.medina@maibornwolff.de>
2025-11-22 21:02:17 +0100

    feat(visualization): migrate layout from Angular Material/SCSS to DaisyUI with zoneless signals

--- visualization/app/codeCharta/ui/codeMap/codeMap.component.ts
+++ visualization/app/codeCharta/ui/codeMap/codeMap.component.ts
@@ -1,34 +1,25 @@
-import { Component, AfterViewInit, ElementRef, OnDestroy } from "@angular/core"
+import { Component, AfterViewInit, ElementRef, effect, Signal } from "@angular/core"
+import { toSignal } from "@angular/core/rxjs-interop"
 import { isLoadingFileSelector } from "../../state/store/appSettings/isLoadingFile/isLoadingFile.selector"
 import { ThreeViewerService } from "./threeViewer/threeViewer.service"
 import { GlobalSettingsFacade } from "../../features/globalSettings/facade"
 import { CodeMapMouseEventService } from "./codeMap.mouseEvent.service"
-import { skip, tap } from "rxjs"
 import { IsAttributeSideBarVisibleService } from "../../services/isAttributeSideBarVisible.service"
 import { Store } from "@ngrx/store"
 import { CcState } from "../../codeCharta.model"
 import { ViewCubeComponent } from "../viewCube/viewCube.component"
 import { AttributeSideBarComponent } from "../attributeSideBar/attributeSideBar.component"
-import { AsyncPipe } from "@angular/common"
 
 @Component({
     selector: "cc-code-map",
     templateUrl: "./codeMap.component.html",
-    styleUrls: ["./codeMap.component.scss"],
-    imports: [ViewCubeComponent, AttributeSideBarComponent, AsyncPipe]
+    imports: [ViewCubeComponent, AttributeSideBarComponent]
 })
-export class CodeMapComponent implements AfterViewInit, OnDestroy {
-    isLoadingFile$ = this.store.select(isLoadingFileSelector)
-    restartOnSharpnessModeChangesSubscription = this.globalSettingsFacade
-        .sharpnessMode$()
-        .pipe(
-            skip(1),
-            tap(() => {
-                this.threeViewerService.restart(this.elementReference.nativeElement.querySelector("#codeMap"))
-                this.codeMapMouseEventService.start()
-            })
-        )
-        .subscribe()
+export class CodeMapComponent implements AfterViewInit {
+    isLoadingFile: Signal<boolean | undefined>
+    sharpnessMode: Signal<string | undefined>
+
+    private isFirstSharpnessModeChange = true
 
     constructor(
         public isAttributeSideBarVisibleService: IsAttributeSideBarVisibleService,
@@ -37,14 +28,23 @@ export class CodeMapComponent implements AfterViewInit, OnDestroy {
         private readonly codeMapMouseEventService: CodeMapMouseEventService,
         private readonly elementReference: ElementRef,
         private readonly globalSettingsFacade: GlobalSettingsFacade
-    ) {}
+    ) {
+        this.isLoadingFile = toSignal(this.store.select(isLoadingFileSelector))
+        this.sharpnessMode = toSignal(this.globalSettingsFacade.sharpnessMode$())
+
+        effect(() => {
+            this.sharpnessMode()
+            if (this.isFirstSharpnessModeChange) {
+                this.isFirstSharpnessModeChange = false
+                return
+            }
+            this.threeViewerService.restart(this.elementReference.nativeElement.querySelector("#codeMap"))
+            this.codeMapMouseEventService.start()
+        })
+    }
 
     ngAfterViewInit(): void {
         this.threeViewerService.init(this.elementReference.nativeElement.querySelector("#codeMap"))
         this.codeMapMouseEventService.start()
-    }
-
-    ngOnDestroy(): void {
-        this.restartOnSharpnessModeChangesSubscription.unsubscribe()
     }
 }
