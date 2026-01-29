079cb02f87eb805ec63787841f0cd0e83e2bb5e0
Christian Hühn <christian.huehn@maibornwolff.de>
2026-01-29 09:01:05 +0100

    chore: bump angular version to 21

--- visualization/app/main.ts
+++ visualization/app/main.ts
@@ -1,6 +1,7 @@
+import { provideZoneChangeDetection } from "@angular/core"
 import "zone.js"
 import { bootstrapApplication } from "@angular/platform-browser"
 import { CodeChartaComponent } from "app/codeCharta/codeCharta.component"
 import { appConfig } from "app/app.config"
 
-bootstrapApplication(CodeChartaComponent, appConfig)
+bootstrapApplication(CodeChartaComponent, { ...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers] })
